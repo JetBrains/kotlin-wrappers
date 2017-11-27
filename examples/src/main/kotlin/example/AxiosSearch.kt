5464cc38a7db644e06dbf834ee71ae920affc197
Scott Huang(ZhiLiang) <scott_huang@qq.com>
2017-11-27 11:10:42 +0800

    Continue enhance axios typing
    Add more typing for axios config/response/error
    Now, people should be able leverage typed axios lib to more easily  co-work with Kotlin-React to create a real app.

--- examples/src/main/kotlin/example/AxiosSearch.kt
+++ examples/src/main/kotlin/example/AxiosSearch.kt
@@ -8,7 +8,7 @@ package example
  * Date: Nov 25, 2017
  */
 
-import kotlinext.js.js
+import kotlinext.js.jsObject
 import kotlinx.html.*
 import kotlinx.html.js.*
 import org.w3c.dom.HTMLInputElement
@@ -29,10 +29,32 @@ interface AxiosState : RState {
 @JsModule("axios")
 external fun axios(config: AxiosConfigSettings): dynamic
 
-//add simple type for this example
+//add enhanced typing for axios
 external interface AxiosConfigSettings {
     var url: String
+    var method: String  //get, post, head, delete, put, patch and so on.  The default is get if you not set any info
+    var baseUrl: String
     var timeout: Number
+    var data: dynamic
+    var transferRequest: dynamic
+    var transferResponse: dynamic
+    var headers: dynamic
+    var params: dynamic
+    var withCredentials: Boolean
+    var adapter: dynamic
+    var auth: dynamic
+    var responseType: String //defauls is json
+    var xsrfCookieName: String
+    var xsrfHeaderName: String
+    var onUploadProgress: dynamic
+    var onDowndloadProgress: dynamic
+    var maxContentLength: Number
+    var validateStatus: (Number)->Boolean
+    var maxRedirects: Number
+    var httpAgent: dynamic
+    var httpsAgent: dynamic
+    var proxy: dynamic
+    var cancelToken: dynamic
 }
 
 external interface AxiosError {
@@ -43,7 +65,7 @@ external interface AxiosResponse {
     var data: dynamic
     var status: Number
     var statusText: String
-    var header: dynamic
+    var headers: dynamic
     var config: AxiosConfigSettings
 }
 
@@ -62,10 +84,16 @@ class AxiosSearch(props: AxiosProps) : RComponent<AxiosProps, AxiosState>(props)
             setState {
                 zipResult = ZipResult(response.data.country, response.data.state, response.data.city)
             }
+            console.log(response.status)
+            console.log(response.statusText)
+            console.log(response.config)
+            console.log(response.headers)
+            console.log(response.data)
         }.catch { error: AxiosError ->
             setState {
                 zipResult = ZipResult("Find error:", error.message, ". Please open your console to learn detail")
             }
+            console.log(error.message)
             console.log(error)
         }
     }
