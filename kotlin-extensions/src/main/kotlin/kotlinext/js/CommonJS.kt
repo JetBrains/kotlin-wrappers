ec2ca0e7ea293d78eeeea96ba3d0197a7139f9f3
hypnos <Filipp.Riabchun@jetbrains.com>
2017-12-04 22:53:15 +0300

    CRKA-57 Missing js bindings for require.context

--- kotlin-extensions/src/main/kotlin/kotlinext/js/CommonJS.kt
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/CommonJS.kt
@@ -1,7 +1,23 @@
 package kotlinext.js
 
+import kotlin.js.RegExp
+
+external interface Context: JsFunction1<String, dynamic> {
+    fun resolve(module: String): String
+    fun keys(): Array<String>
+    val id: Int
+}
+
+fun requireAll(context: Context) = context.keys().forEach(context::invoke)
+
 external object require {
     fun resolve(module: String): String
+    // webpack-specific
+    fun context(directory: String, useSubdirectories: Boolean, regExp: RegExp): Context
+}
+
+fun foo() {
+    require.context("src", true, js("/\\.css$/"))
 }
 
 inline operator fun require.invoke(module: String) = asDynamic()(module)
