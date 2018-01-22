ee50b2a567d38a3773507d9d6242d338ad454b4e
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-01-22 20:38:02 +0100

    Fix order of parameters

--- kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
+++ kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
@@ -3,10 +3,10 @@ package react.dom
 import org.w3c.dom.*
 import react.*
 
-fun render(container: Element?, handler: RBuilder.() -> Unit, callback: () -> Unit) =
+fun render(container: Element?, callback: () -> Unit = {}, handler: RBuilder.() -> Unit) =
     render(buildElements(handler), container, callback)
 
-fun hydrate(container: Element?, handler: RBuilder.() -> Unit, callback: () -> Unit) =
+fun hydrate(container: Element?, callback: () -> Unit = {}, handler: RBuilder.() -> Unit) =
     hydrate(buildElements(handler), container, callback)
 
 fun createPortal(container: Element?, handler: RBuilder.() -> Unit) =
