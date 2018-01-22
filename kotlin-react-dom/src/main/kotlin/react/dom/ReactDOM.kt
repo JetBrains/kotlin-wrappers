ee78c7129a35c2d9901fac50ba148598ea829d69
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-01-22 17:21:33 +0100

    Fix #33

--- kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
+++ kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
@@ -3,11 +3,11 @@ package react.dom
 import org.w3c.dom.*
 import react.*
 
-fun render(container: Element?, handler: RBuilder.() -> Unit) =
-    render(buildElements(handler), container)
+fun render(container: Element?, handler: RBuilder.() -> Unit, callback: () -> Unit) =
+    render(buildElements(handler), container, callback)
 
-fun hydrate(container: Element?, handler: RBuilder.() -> Unit) =
-    hydrate(buildElements(handler), container)
+fun hydrate(container: Element?, handler: RBuilder.() -> Unit, callback: () -> Unit) =
+    hydrate(buildElements(handler), container, callback)
 
 fun createPortal(container: Element?, handler: RBuilder.() -> Unit) =
     createPortal(buildElements(handler), container)
