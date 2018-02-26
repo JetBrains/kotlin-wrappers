1037dd69e1e009c3a25a22666fecd9694ac9b137
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-02-26 12:43:48 +0100

    Move TemplateTag to kotlin-extensions

---
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/TemplateTag.kt
@@ -0,0 +1,11 @@
+package kotlinext.js
+
+external interface TemplateTag<in T, out R> {
+    fun call(ctx: Any?, strings: Array<String>, vararg values: T): R
+}
+
+operator fun <T, R> TemplateTag<T, R>.invoke(strings: Array<String>, vararg values: T) =
+        this.call(null, strings, *values)
+
+operator fun <T, R> TemplateTag<T, R>.invoke(string: String, vararg values: T) = this(arrayOf(string), *values)
+operator fun <T, R> TemplateTag<T, R>.invoke(vararg values: T) = this(emptyArray(), *values)
--- kotlin-styled/src/main/kotlin/styled/StyledComponents.kt
+++ kotlin-styled/src/main/kotlin/styled/StyledComponents.kt
@@ -90,16 +90,6 @@ inline fun CustomStyledProps.css(noinline handler: RuleSet) {
 
 inline fun <P : CustomStyledProps> RElementBuilder<P>.css(noinline handler: RuleSet) = attrs.css(handler)
 
-external interface TemplateTag<in T, out R> {
-    fun call(ctx: Any?, strings: Array<String>, vararg values: T): R
-}
-
-operator fun <T, R> TemplateTag<T, R>.invoke(strings: Array<String>, vararg values: T) =
-    this.call(null, strings, *values)
-
-operator fun <T, R> TemplateTag<T, R>.invoke(string: String, vararg values: T) = this(arrayOf(string), *values)
-operator fun <T, R> TemplateTag<T, R>.invoke(vararg values: T) = this(emptyArray(), *values)
-
 external interface StylerConfig {
     var displayName: String
     var componentId: String
