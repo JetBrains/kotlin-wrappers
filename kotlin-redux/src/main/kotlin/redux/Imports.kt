6f454409cfb22092a79dae6c217b91c18074f124
Aidan Heller <asfheller@gmail.com>
2018-03-17 13:14:03 -0700

    Use function instead of class for createStore

--- kotlin-redux/src/main/kotlin/redux/Imports.kt
+++ kotlin-redux/src/main/kotlin/redux/Imports.kt
@@ -1,9 +1,11 @@
 @file:JsModule("redux")
 package redux
 
-@JsName("createStore")
-internal external class Store<out S>(reducer: (S, WrapperAction) -> S, preloadedState: S) {
+external fun <S, A> createStore(reducer: (S, A) -> S, preloadedState: S): Store<S, A>
+
+external interface Store<S, A> {
     fun getState(): S
-    fun dispatch(action: WrapperAction)
+    fun dispatch(action: A): A
     fun subscribe(listener: () -> Unit): () -> Unit
+    fun replaceReducer(nextReducer: (S, A) -> S)
 }
