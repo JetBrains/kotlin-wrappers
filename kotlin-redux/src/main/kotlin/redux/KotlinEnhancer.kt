58457569887cb58a37bdbf8c87abf8a56cec39ff
Aidan Heller <asfheller@gmail.com>
2018-03-25 19:41:02 -0700

    Use lambda to wrap reducer

--- kotlin-redux/src/main/kotlin/redux/KotlinEnhancer.kt
+++ kotlin-redux/src/main/kotlin/redux/KotlinEnhancer.kt
@@ -4,9 +4,10 @@ import kotlinext.js.js
 
 fun <S> kotlinEnhancer(next: StoreCreator<S, WrapperAction>): StoreCreator<S, KotlinAction> {
     return { reducer, preloadedState ->
-        var wrapperReducer: Reducer<S, WrapperAction> = { state, action -> reducer(state, action.action) }
-        val store = next(wrapperReducer, preloadedState)
-object: Store<S, KotlinAction> {
+        fun wrapReducer(reducer: Reducer<S, KotlinAction>): Reducer<S, WrapperAction> =
+                { state, action -> reducer(state, action.action) }
+        val store = next(wrapReducer(reducer), preloadedState)
+        object: Store<S, KotlinAction> {
             override fun getState() = store.getState()
             override fun dispatch(action: KotlinAction): KotlinAction {
                 @Suppress("UnsafeCastFromDynamic") // Needed because explicit cast causes illegal cast exception
@@ -18,8 +19,7 @@ object: Store<S, KotlinAction> {
             }
             override fun subscribe(listener: () -> Unit) = store.subscribe(listener)
             override fun replaceReducer(nextReducer: Reducer<S, KotlinAction>) {
-                wrapperReducer = { state, action -> nextReducer(state, action.action) }
-                store.replaceReducer(wrapperReducer)
+                store.replaceReducer(wrapReducer(nextReducer))
             }
         }
     }
