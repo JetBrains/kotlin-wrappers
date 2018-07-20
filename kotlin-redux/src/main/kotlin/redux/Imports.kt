a3f873f10732005f572307b5f795d06352c637f7
Aidan Heller <asfheller@gmail.com>
2018-07-19 21:33:18 -0700

    Add type annotations to compose function

--- kotlin-redux/src/main/kotlin/redux/Imports.kt
+++ kotlin-redux/src/main/kotlin/redux/Imports.kt
@@ -13,4 +13,8 @@ external fun <S, A1, A2> applyMiddleware(middleware: Middleware<S, A1, A2>)
 
 external fun <S, A1, A2, A3> applyMiddleware(middleware1: Middleware<S, A1, A3>, middleware2: Middleware<S, A1, A2>)
 
-external fun compose(vararg functions: dynamic): dynamic
+external fun <A, T1, R> compose(function1: (T1) -> R , function2: (A) -> T1): (A) -> R
+
+external fun <A, T1, T2, R> compose(function1: (T2) -> R , function2: (T1) -> T2, function3: (A) -> T1): (A) -> R
+
+external fun <A, T1, T2, T3, R> compose(function1: (T3) -> R, function2: (T2) -> T3, function3: (T1) -> T2, function4: (A) -> T1): (A) -> R
