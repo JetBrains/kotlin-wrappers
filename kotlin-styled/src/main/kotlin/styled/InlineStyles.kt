1cd66d2b28c2cd722ff55b5508f9609a7b31ae55
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-09-11 11:46:30 +0200

    InlineStyles: update to version 5.0 API

--- kotlin-styled/src/main/kotlin/styled/InlineStyles.kt
+++ kotlin-styled/src/main/kotlin/styled/InlineStyles.kt
@@ -5,17 +5,29 @@ import kotlinx.css.*
 import react.dom.*
 
 @JsModule("inline-style-prefixer")
-external class InlineStylePrefixer {
-    fun prefix(src: dynamic): dynamic
-}
+external object InlineStylePrefixer {
+    /**
+     * https://github.com/rofrischmann/inline-style-prefixer/blob/master/docs/api/prefix.md
+     *
+     * data: an object containing a valid prefixMap and a plugins list
+     * Returns: custom prefix function
+     */
+    fun createPrefixer(data: dynamic): dynamic
 
-private val prefixer = InlineStylePrefixer()
+    /**
+     * https://github.com/rofrischmann/inline-style-prefixer/blob/master/docs/api/createPrefixer.md
+     *
+     * style: an object containing valid style declarations
+     * Returns: an object containing all style declarations with vendor prefixes
+     */
+    fun prefix(style: dynamic): dynamic
+}
 
 // Inserts vendor prefixes (using inline-style-prefixer) and sets the style attribute
 fun RDOMBuilder<*>.inlineStyles(builder: StyledElement.() -> Unit) {
     val styles = StyledElement()
     builder(styles)
-    setProp("style", prefixer.prefix(styles.toStyle()))
+    setProp("style", InlineStylePrefixer.prefix(styles.toStyle()))
 }
 
 fun StyledElement.toStyle(): Any {
@@ -28,5 +40,5 @@ fun StyledElement.toStyle(): Any {
             res[it.key] = it.value
         }
     }
-    return prefixer.prefix(res)
+    return InlineStylePrefixer.prefix(res)
 }
