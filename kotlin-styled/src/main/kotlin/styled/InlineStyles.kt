d216d1ba49af8422d7caa3ea4e67dd175112d791
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-10-16 14:13:12 +0200

    Fix StyledElement.toStyle(), it didn't work properly with array values; make prefixing optional

--- kotlin-styled/src/main/kotlin/styled/InlineStyles.kt
+++ kotlin-styled/src/main/kotlin/styled/InlineStyles.kt
@@ -24,13 +24,13 @@ external object InlineStylePrefixer {
 }
 
 // Inserts vendor prefixes (using inline-style-prefixer) and sets the style attribute
-fun RDOMBuilder<*>.inlineStyles(builder: StyledElement.() -> Unit) {
+fun RDOMBuilder<*>.inlineStyles(prefix: Boolean = true, builder: StyledElement.() -> Unit) {
     val styles = StyledElement()
     builder(styles)
-    setProp("style", InlineStylePrefixer.prefix(styles.toStyle()))
+    setProp("style", styles.toStyle(prefix))
 }
 
-fun StyledElement.toStyle(): Any {
+fun StyledElement.toStyle(prefix: Boolean = true): Any {
     val res = js { }
     declarations.forEach {
         if ((it.value !is String) && (it.value !is Number)) {
@@ -40,5 +40,31 @@ fun StyledElement.toStyle(): Any {
             res[it.key] = it.value
         }
     }
-    return InlineStylePrefixer.prefix(res)
+
+    if (!prefix) {
+        return res
+    }
+
+    val prefixed = InlineStylePrefixer.prefix(res) as Object
+
+    // https://inline-style-prefixer.js.org/docs/guides/ResolvingArrays.html
+    Object.keys(prefixed).forEach {
+        if (prefixed.asJsObject().hasOwnProperty(it)) {
+            @Suppress("UNUSED_VARIABLE")
+            val value = prefixed.asDynamic()[it]
+
+            @Suppress("UnsafeCastFromDynamic")
+            if (js("(value instanceof Array)")) {
+                // TODO make this work
+                // val displayValue = CssInJsUtils.resolveArrayValue(it, prefixed.asDynamic()[it])
+
+                // Find the unprefixed value in the array and use it: multiple values don't work for some reason
+                val displayValue = js("(value.find(function(element) { return !element.startsWith('-') }))")
+
+                prefixed.asDynamic()[it] = displayValue
+            }
+        }
+    }
+
+    return prefixed
 }
