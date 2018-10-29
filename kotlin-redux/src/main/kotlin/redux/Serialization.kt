19559cc1f8733b3bef71dc60995854ed82c7e3ef
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2018-10-29 16:58:50 +0100

    Kotlin 1.3

--- kotlin-redux/src/main/kotlin/redux/Serialization.kt
+++ kotlin-redux/src/main/kotlin/redux/Serialization.kt
@@ -6,10 +6,12 @@ import kotlinx.serialization.*
 import kotlinx.serialization.internal.*
 import kotlinx.serialization.json.*
 
+@UseExperimental(ImplicitReflectionSerializer::class)
 inline fun <reified A : RAction> serializeAction(action: A): String {
     return "{\"type\":\"${A::class.simpleName}\",${JSON.stringify(action).drop(1)}"
 }
 
+@UseExperimental(ImplicitReflectionSerializer::class)
 inline fun <reified A : RAction> deserializeAction(action: String): A? {
     val map = JSON.parse((StringSerializer to StringSerializer).map, action)
     return if (map["type"] == A::class.simpleName) JSON.nonstrict.parse(action) else null
