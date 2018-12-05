0584fa718802763946b5c4239e5049522405a27a
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-12-05 22:14:12 +0100

    Rename `assign` to `objectAssign` to avoid clashing with `assign(obj: T, builder: T.() -> Unit)` [publish]

--- kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
@@ -9,8 +9,12 @@ inline fun <T : Any> jsObject(builder: T.() -> Unit): T {
 
 inline fun js(builder: dynamic.() -> Unit): dynamic = jsObject(builder)
 
-fun <T : Any> clone(obj: T) = assign(jsObject<T> {}, obj)
+fun <T : Any> clone(obj: T) = objectAssign(jsObject<T> {}, obj)
 inline fun <T : Any> assign(obj: T, builder: T.() -> Unit) = clone(obj).apply(builder)
+fun <T, R : T> assign(dest: R, src: T): R {
+    console.warn("kotlinext.js.assign is deprecated, use kotlinext.js.objectAssign instead")
+    return objectAssign(dest, src)
+}
 
 fun toPlainObjectStripNull(obj: Any) = js {
     for (key in Object.keys(obj)) {
