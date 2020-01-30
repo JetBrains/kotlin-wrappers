5fb960c3a2ce5e44e2add2c7d3bb19d6939d83b0
Jolan Rensen <j.j.r.rensen@student.tue.nl>
2020-01-30 17:52:56 +0100

    Added RPureComponent to ReactComponent.kt

--- kotlin-react/src/main/kotlin/react/ReactComponent.kt
+++ kotlin-react/src/main/kotlin/react/ReactComponent.kt
@@ -105,3 +105,25 @@ abstract class RComponent<P : RProps, S : RState> : Component<P, S> {
 
     override fun render() = buildElements { render() }
 }
+
+abstract class RPureComponent<P : RProps, S : RState> : PureComponent<P, S> {
+    constructor() : super() {
+        state = jsObject { init() }
+    }
+
+    constructor(props: P) : super(props) {
+        state = jsObject { init(props) }
+    }
+
+    open fun S.init() {}
+    // if you use this method, don't forget to pass props to the constructor first
+    open fun S.init(props: P) {}
+
+    fun RBuilder.children() {
+        props.children()
+    }
+
+    abstract fun RBuilder.render()
+
+    override fun render() = buildElements { render() }
+}
