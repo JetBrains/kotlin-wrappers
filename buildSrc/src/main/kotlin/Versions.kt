252bce41454c31445e532cccae50460114d0aa31
Victor Turansky <victor.turansky@gmail.com>
2020-04-09 01:47:02 +0300

    Automatic publish version calculation

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,20 @@
+@file:JvmName("Versions")
+
+import org.gradle.api.Project
+
+private fun Project.version(target: String): String =
+    property("${target}_version") as String
+
+fun Project.publishVersion(): String {
+    val target = name
+        .removePrefix("kotlin-")
+        .let {
+            when {
+                it.startsWith("css") -> "css"
+                it == "extensions" -> "kotlinext"
+                else -> it.replace("-", "_")
+            }
+        }
+
+    return "${version(target)}-kotlin-${version("kotlin")}"
+}
