14cbbd70f1c54f972120c0f2f651029fc6faa6c9
Victor Turansky <victor.turansky@gmail.com>
2020-04-16 18:21:23 +0300

    Add build constants

--- buildSrc/src/main/kotlin/npm-publishing-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/npm-publishing-conventions.gradle.kts
@@ -4,26 +4,29 @@ plugins {
     id("com.github.node-gradle.node")
 }
 
+val BUILD_NPM = "build/npm"
+val PACKAGE_JSON = "package.json"
+
 tasks {
     val processPkg = register<Copy>("processPkg") {
         from(".")
-        into("build/npm")
-        include("package.json")
+        into(BUILD_NPM)
+        include(PACKAGE_JSON)
         expand(versionMap())
     }
 
     val prepublish = register<Copy>("prepublish") {
         from(".")
-        into("build/npm")
-        exclude("package.json")
-        exclude("build/npm")
+        into(BUILD_NPM)
+        exclude(PACKAGE_JSON)
+        exclude(BUILD_NPM)
 
         dependsOn(named("build"))
     }
 
     named<NpmTask>("npm_publish") {
         setArgs(listOf("--access", "public"))
-        setWorkingDir(file("build/npm"))
+        setWorkingDir(file(BUILD_NPM))
 
         dependsOn(prepublish)
         dependsOn(processPkg)
--- kotlin-css/build.gradle.kts
+++ kotlin-css/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform")
-    `maven-publishing-conventions`
+    `publishing-conventions`
 }
 
 kotlin {
