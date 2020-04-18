7003efe7b21d189d97966cdbce6f13cf9e17183c
Victor Turansky <victor.turansky@gmail.com>
2020-04-19 00:07:26 +0300

    Gradle NPM dependencies as NPM peer dependencies

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -1,5 +1,6 @@
 import org.gradle.api.Project
 import org.gradle.api.internal.artifacts.dependencies.DefaultProjectDependency
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
 
 private val IMPLEMENTATION = "implementation"
 
@@ -11,6 +12,15 @@ internal fun Project.relatedProjects(): List<Project> {
         .allDependencies
         .filterIsInstance<DefaultProjectDependency>()
         .map { it.dependencyProject }
+}
+
+internal fun Project.nmpDependencies(): List<NpmDependency> {
+    val configuration = configurations.findByName(IMPLEMENTATION)
+        ?: return emptyList()
+
+    return configuration
+        .allDependencies
+        .filterIsInstance<NpmDependency>()
 }
 
 internal fun Project.version(target: String): String =
