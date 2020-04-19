30caad8edfa8c75c95408033bb6a1cd99a67a738
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2020-04-19 17:56:10 +0200

    pre.99

--- kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
@@ -13,12 +13,6 @@ fun <T : Any> clone(obj: T) = Object.assign(jsObject(), obj)
 
 inline fun <T : Any> assign(obj: T, builder: T.() -> Unit) = clone(obj).apply(builder)
 
-@Deprecated(
-    message = "Use Object.assign instead",
-    replaceWith = ReplaceWith("Object.assign(dest, src)", "kotlinext.js")
-)
-fun <T, R : T> assign(dest: R, src: T): R = Object.assign(dest, src)
-
 fun toPlainObjectStripNull(obj: Any) = js {
     for (key in Object.keys(obj)) {
         val value = obj.asDynamic()[key]
