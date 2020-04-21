5017cf3045332d1c5267a2ec07606847907cd74e
Victor Turansky <victor.turansky@gmail.com>
2020-04-21 03:04:51 +0300

    Remove Children undocumented method

--- kotlin-react/src/main/kotlin/react/Imports.kt
+++ kotlin-react/src/main/kotlin/react/Imports.kt
@@ -13,9 +13,7 @@ external fun isValidElement(element: Any): Boolean
 
 external object Children {
     fun <T> map(children: Any?, handler: (Child) -> T): Array<out T>?
-    fun <T> map(children: Any?, handler: (Child) -> T, context: Any?): Array<out T>?
     fun forEach(children: Any?, handler: (Child) -> Unit)
-    fun forEach(children: Any?, handler: (Child) -> Unit, context: Any?)
     fun count(children: Any?): Int
     fun only(children: Any?): ReactElement
     fun toArray(children: Any?): Array<out Child>
