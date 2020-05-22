04dcc3f8394048f1152d010b581c2223c7a9ee45
Leonid Khachaturov <leonidkhachaturov@gmail.com>
2020-05-22 17:38:52 +0200

    Revert the useState refactoring

--- kotlin-react/src/main/kotlin/react/hooks.kt
+++ kotlin-react/src/main/kotlin/react/hooks.kt
@@ -8,32 +8,31 @@ typealias RDependenciesList = List<dynamic>
 
 typealias RSetState<T> = (value: T) -> Unit
 
-fun <T> useState(initValue: T): RStateDelegate<T> {
-    val (state, setState) = rawUseState(initValue)
-    return RStateDelegate(
-        state.unsafeCast<T>(),
-        setState.unsafeCast<RSetState<T>>()
-    )
+fun <T> useState(initValue: T): Pair<T, RSetState<T>> {
+    val jsTuple = rawUseState(initValue)
+    val currentValue = jsTuple[0] as T
+    val setState = jsTuple[1] as RSetState<T>
+    return currentValue to setState
 }
 
-fun <T> useState(valueInitializer: () -> T): RStateDelegate<T> {
-    val (state, setState) = rawUseState(valueInitializer)
-    return RStateDelegate(
-        state.unsafeCast<T>(),
-        setState.unsafeCast<RSetState<T>>()
-    )
+fun <T> useState(valueInitializer: () -> T): Pair<T, RSetState<T>> {
+    val jsTuple = rawUseState(valueInitializer)
+    val currentValue = jsTuple[0] as T
+    val setState = jsTuple[1] as RSetState<T>
+    return currentValue to setState
 }
 
-class RStateDelegate<T>
-internal constructor(
-    private val state: T,
-    private val setState: RSetState<T>
-) : ReadWriteProperty<Any?, T> {
-    @Deprecated("Use delegate instead. Example: `var count by useState(13)`")
-    operator fun component1(): T = state
+operator fun <T> Pair<T, RSetState<T>>.getValue(thisRef: Any?, property: KProperty<*>): T {
+    return first
+}
 
-    @Deprecated("Use delegate instead. Example: `var count by useState(42)`")
-    operator fun component2(): RSetState<T> = setState
+operator fun <T> Pair<T, RSetState<T>>.setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+    second(value)
+}
+
+private class ReactStateDelegate<T>(useState: Pair<T, RSetState<T>>) : ReadWriteProperty<Any?, T> {
+    private val state = useState.first
+    private val setState = useState.second
 
     override operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
         state
@@ -43,16 +42,26 @@ internal constructor(
     }
 }
 
+@Deprecated("Use useState delegate", ReplaceWith("useState(initValue)"))
+fun <T> state(initValue: T): ReadWriteProperty<Any?, T> =
+    ReactStateDelegate(useState(initValue))
+
+@Deprecated("Use useState delegate", ReplaceWith("useState(valueInitializer)"))
+fun <T> state(valueInitializer: () -> T): ReadWriteProperty<Any?, T> =
+    ReactStateDelegate(useState(valueInitializer))
+
 typealias RReducer<S, A> = (state: S, action: A) -> S
 typealias RDispatch<A> = (action: A) -> Unit
 
 fun <S, A> useReducer(reducer: RReducer<S, A>, initState: S, initialAction: A? = null): Pair<S, RDispatch<A>> {
-    val (currentState, dispatch) = if (initialAction != null) {
+    val jsTuple = if (initialAction != null) {
         rawUseReducer(reducer, initState, initialAction)
     } else {
         rawUseReducer(reducer, initState)
     }
-    return currentState.unsafeCast<S>() to dispatch.unsafeCast<RDispatch<A>>()
+    val currentState = jsTuple[0] as S
+    val dispatch = jsTuple[1] as RDispatch<A>
+    return currentState to dispatch
 }
 
 fun <S, A> useReducer(reducer: RReducer<S?, A>): Pair<S?, RDispatch<A>> {
