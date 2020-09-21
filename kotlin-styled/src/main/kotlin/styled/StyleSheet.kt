f726d30184a73be2d16dce3d81dcd47145f37265
Leonid Khachaturov <leonidkhachaturov@gmail.com>
2020-09-21 14:36:26 +0200

    styled-components: the useRef() hack is only needed in development mode

--- kotlin-styled/src/main/kotlin/styled/StyleSheet.kt
+++ kotlin-styled/src/main/kotlin/styled/StyleSheet.kt
@@ -39,11 +39,9 @@ open class StyleSheet(var name: String, val isStatic: Boolean = false) {
             }
 
             injectGlobal(builder.toString())
-        } else {
-            // for styled-components v5
-            // inject global uses useRef() to check if it's created dynamically,
-            // but using hooks should be without any condition in render flow
-            // so it should be placed in else too
+        } else if (js("process.env.NODE_ENV !== 'production'")) {
+            // styled-components 5.x uses useRef() in development mode to check if a component has been created
+            // dynamically, so we include a corresponding call here to have a matching count of hook invocations
             try {
                 useRef(undefined)
             } catch (e: Throwable) {}
