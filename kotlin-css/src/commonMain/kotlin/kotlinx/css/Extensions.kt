7bb3921eda9152708cdb131f4c2954e1476dce27
Victor Turansky <victor.turansky@gmail.com>
2021-04-28 12:49:31 +0300

    Kotlin 1.5.0 (#440)
    Kotlin 1.5.0

--- kotlin-css/src/commonMain/kotlin/kotlinx/css/Extensions.kt
+++ kotlin-css/src/commonMain/kotlin/kotlinx/css/Extensions.kt
@@ -4,7 +4,7 @@ private val CAPITAL_LETTER = Regex("[A-Z]")
 
 fun String.hyphenize(): String =
     replace(CAPITAL_LETTER) {
-        "-${it.value.toLowerCase()}"
+        "-${it.value.lowercase()}"
     }
 
 operator fun String.times(n: Int): String = repeat(n)
