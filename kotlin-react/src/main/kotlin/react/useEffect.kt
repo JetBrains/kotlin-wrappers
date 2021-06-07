f3b3be57d1ac373ccd3cc0b8002ada9dce67eeac
sergeigrishchenko <s.i.grishchenko@gmail.com>
2021-06-07 23:06:47 +0300

    New effect signatures are added, old signatures are deprecated

--- kotlin-react/src/main/kotlin/react/useEffect.kt
+++ kotlin-react/src/main/kotlin/react/useEffect.kt
@@ -9,6 +9,43 @@ typealias RCleanup = () -> Unit
  * Only works inside [functionalComponent]
  * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
  */
+fun useEffect(
+    effect: EffectBuilder.() -> Unit,
+) {
+    val effectCallback = useEffectCallback(effect)
+    rawUseEffect(effectCallback)
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useEffect(
+    vararg dependencies: dynamic,
+    effect: EffectBuilder.() -> Unit,
+) {
+    val effectCallback = useEffectCallback(effect)
+    rawUseEffect(effectCallback, dependencies)
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useEffectOnce(
+    effect: EffectBuilder.() -> Unit,
+) {
+    val effectCallback = useEffectCallback(effect)
+    rawUseEffect(effectCallback, emptyArray())
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+@Deprecated(
+    message = "Inconsistent hooks API",
+)
 fun useEffectWithCleanup(
     dependencies: RDependenciesList? = null,
     effect: () -> RCleanup,
@@ -24,17 +61,17 @@ fun useEffectWithCleanup(
  * Only works inside [functionalComponent]
  * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
  */
+@Deprecated(
+    message = "Inconsistent hooks API",
+)
 fun useEffect(
     dependencies: RDependenciesList? = null,
-    effect: () -> Unit,
+    effect: EffectBuilder.() -> Unit,
 ) {
-    val rawEffect = {
-        effect()
-        undefined
-    }
+    val effectCallback = useEffectCallback(effect)
     if (dependencies != null) {
-        rawUseEffect(rawEffect, dependencies.toTypedArray())
+        rawUseEffect(effectCallback, dependencies.toTypedArray())
     } else {
-        rawUseEffect(rawEffect)
+        rawUseEffect(effectCallback)
     }
 }
