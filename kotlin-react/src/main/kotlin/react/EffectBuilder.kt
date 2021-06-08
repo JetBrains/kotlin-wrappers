a5dfd6b911d0773749eb84694918e2fdf1ec0ec0
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2021-06-08 05:17:41 +0300

    Consistency of hook dependencies v2 (#476)

--- kotlin-react/src/main/kotlin/react/EffectBuilder.kt
+++ kotlin-react/src/main/kotlin/react/EffectBuilder.kt
@@ -12,14 +12,13 @@ private constructor() {
     }
 }
 
-internal fun useEffectCallback(
+internal fun createEffectCallback(
     effect: EffectBuilder.() -> Unit,
-): () -> RCleanup? =
-    useCallback(effect) {
-        val cleanups = arrayOf<RCleanup>()
-        effect(cleanups.unsafeCast<EffectBuilder>())
-        buildCleanup(cleanups)
-    }
+): () -> RCleanup? = {
+    val cleanups = arrayOf<RCleanup>()
+    effect(cleanups.unsafeCast<EffectBuilder>())
+    buildCleanup(cleanups)
+}
 
 private fun buildCleanup(
     cleanups: Array<out RCleanup>,
--- kotlin-react/src/main/kotlin/react/useLayoutEffect.kt
+++ kotlin-react/src/main/kotlin/react/useLayoutEffect.kt
@@ -4,6 +4,43 @@ package react
  * Only works inside [functionalComponent]
  * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
  */
+fun useLayoutEffect(
+    effect: EffectBuilder.() -> Unit,
+) {
+    val callback = createEffectCallback(effect)
+    rawUseLayoutEffect(callback)
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useLayoutEffect(
+    vararg dependencies: dynamic,
+    effect: EffectBuilder.() -> Unit,
+) {
+    val callback = createEffectCallback(effect)
+    rawUseLayoutEffect(callback, dependencies)
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useLayoutEffectOnce(
+    effect: EffectBuilder.() -> Unit,
+) {
+    val callback = createEffectCallback(effect)
+    rawUseLayoutEffect(callback, emptyArray())
+}
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+@Deprecated(
+    message = "Inconsistent hooks API",
+)
 fun useLayoutEffectWithCleanup(
     dependencies: RDependenciesList? = null,
     effect: () -> RCleanup,
@@ -19,17 +56,17 @@ fun useLayoutEffectWithCleanup(
  * Only works inside [functionalComponent]
  * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
  */
+@Deprecated(
+    message = "Inconsistent hooks API",
+)
 fun useLayoutEffect(
     dependencies: RDependenciesList? = null,
-    effect: () -> Unit,
+    effect: EffectBuilder.() -> Unit,
 ) {
-    val rawEffect = {
-        effect()
-        undefined
-    }
+    val callback = createEffectCallback(effect)
     if (dependencies != null) {
-        rawUseLayoutEffect(rawEffect, dependencies.toTypedArray())
+        rawUseLayoutEffect(callback, dependencies.toTypedArray())
     } else {
-        rawUseLayoutEffect(rawEffect)
+        rawUseLayoutEffect(callback)
     }
 }
