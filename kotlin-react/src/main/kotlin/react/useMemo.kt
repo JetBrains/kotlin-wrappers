14ee86ac8be1a0e622ab6814213b0c24841c3005
sergeigrishchenko <s.i.grishchenko@gmail.com>
2021-06-12 22:48:48 +0300

    useMemo and useCallback hooks with array dependencies are deprecated, only hooks with vararg dependencies are recommended for usage

--- kotlin-react/src/main/kotlin/react/useMemo.kt
+++ kotlin-react/src/main/kotlin/react/useMemo.kt
@@ -10,4 +10,18 @@ inline fun <T> useMemo(
     vararg dependencies: dynamic,
     noinline callback: () -> T,
 ): T =
-    useMemo(callback, dependencies)
+    rawUseMemo(callback, dependencies)
+
+/**
+ * Only works inside [functionalComponent]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+@Deprecated(
+    message = "Inconsistent hooks API",
+)
+inline fun <T> useMemo(
+    noinline callback: () -> T,
+    dependencies: RDependenciesArray
+): T =
+    rawUseMemo(callback, dependencies)
+
