40b03598446fd70f5ac10b90a89b06e5d62525b0
Alex Popoff <32072831+aerialist7@users.noreply.github.com>
2021-07-06 00:44:10 +0400

    Alphabetic modules sorting (#500)

--- kotlin-wrappers-bom/build.gradle.kts
+++ kotlin-wrappers-bom/build.gradle.kts
@@ -10,9 +10,9 @@ dependencies {
         api(project(":kotlin-react"))
         api(project(":kotlin-react-dom"))
         api(project(":kotlin-react-redux"))
-        api(project(":kotlin-redux"))
         api(project(":kotlin-react-router-dom"))
         api(project(":kotlin-react-table"))
+        api(project(":kotlin-redux"))
         api(project(":kotlin-ring-ui"))
         api(project(":kotlin-styled"))
     }
