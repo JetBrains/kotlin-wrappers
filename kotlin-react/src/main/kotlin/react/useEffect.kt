0a24987cf8168ec18bdb6d1b6ad48cbf7006cec6
Victor Turansky <victor.turansky@gmail.com>
2021-07-12 13:05:05 +0300

    #513 Remove deprecated effect hooks (#514)
    * #513 Remove deprecated effect hooks
    
    * #513 Remove deprecated effect hooks

--- kotlin-react/src/main/kotlin/react/useEffect.kt
+++ kotlin-react/src/main/kotlin/react/useEffect.kt
@@ -1,7 +1,6 @@
 package react
 
 typealias RDependenciesArray = Array<out dynamic>
-typealias RDependenciesList = List<dynamic>
 
 typealias RCleanup = () -> Unit
 
@@ -37,41 +36,4 @@ fun useEffectOnce(
 ) {
     val callback = createEffectCallback(effect)
     rawUseEffect(callback, emptyArray())
-}
-
-/**
- * Only works inside [functionalComponent]
- * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
- */
-@Deprecated(
-    message = "Inconsistent hooks API",
-)
-fun useEffectWithCleanup(
-    dependencies: RDependenciesList? = null,
-    effect: () -> RCleanup,
-) {
-    if (dependencies != null) {
-        rawUseEffect(effect, dependencies.toTypedArray())
-    } else {
-        rawUseEffect(effect)
-    }
-}
-
-/**
- * Only works inside [functionalComponent]
- * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
- */
-@Deprecated(
-    message = "Inconsistent hooks API",
-)
-fun useEffect(
-    dependencies: RDependenciesList? = null,
-    effect: EffectBuilder.() -> Unit,
-) {
-    val callback = createEffectCallback(effect)
-    if (dependencies != null) {
-        rawUseEffect(callback, dependencies.toTypedArray())
-    } else {
-        rawUseEffect(callback)
-    }
 }
