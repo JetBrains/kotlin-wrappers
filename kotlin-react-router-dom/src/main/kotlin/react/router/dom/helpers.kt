aacff01dedfe065691420e5c6c626225d35a7a6b
Alex Popoff <32072831+aerialist7@users.noreply.github.com>
2021-07-26 00:29:30 +0400

    Deprecate `RClass` (#573)

--- kotlin-react-router-dom/src/main/kotlin/react/router/dom/helpers.kt
+++ kotlin-react-router-dom/src/main/kotlin/react/router/dom/helpers.kt
@@ -4,7 +4,7 @@ import react.*
 import kotlin.reflect.KClass
 
 fun <P : RProps> withRouter(klazz: KClass<out Component<P, *>>): ComponentClass<P> =
-    rawWithRouter(klazz.rClass)
+    rawWithRouter(klazz.react)
 
 fun <P : RProps> withRouter(component: FC<P>): ComponentClass<P> =
     rawWithRouter(component)
