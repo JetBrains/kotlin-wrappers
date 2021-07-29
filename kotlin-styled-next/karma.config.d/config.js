1d125dda071502fbc3fb9a4f50e0d23928cec203
Leonid Khachaturov <leonidkhachaturov@gmail.com>
2021-07-29 17:50:11 +0200

    Configure a custom launcher

--- kotlin-styled-next/karma.config.d/config.js
+++ kotlin-styled-next/karma.config.d/config.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer');
 process.env.CHROME_BIN = puppeteer.executablePath();
 
+config.customLaunchers = {
+    ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+    }
+}
+
+config.browsers = config.browsers.map(b => b === 'ChromeHeadless' ? 'ChromeHeadlessNoSandbox' : b)
+
 config.client.mocha = config.client.mocha || {}
 config.client.mocha.timeout = 60000
 
