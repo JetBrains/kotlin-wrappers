3b4cbfbf809c81d0e46218efea21a4273fdda130
Victor Turansky <victor.turansky@gmail.com>
2021-08-14 11:11:46 +0300

    Fix native event aliases

--- kotlin-react-dom/src/main/generated/react/dom/NativeEvents.kt
+++ kotlin-react-dom/src/main/generated/react/dom/NativeEvents.kt
@@ -8,7 +8,7 @@ typealias NativeClipboardEvent = org.w3c.dom.clipboard.ClipboardEvent
 
 typealias NativeCompositionEvent = org.w3c.dom.events.CompositionEvent
 
-typealias NativeDragEvent = org.w3c.dom.events.DragEvent
+typealias NativeDragEvent = org.w3c.dom.DragEvent
 
 typealias NativeFocusEvent = org.w3c.dom.events.FocusEvent
 
@@ -16,7 +16,7 @@ typealias NativeKeyboardEvent = org.w3c.dom.events.KeyboardEvent
 
 typealias NativeMouseEvent = org.w3c.dom.events.MouseEvent
 
-typealias NativeTouchEvent = org.w3c.dom.events.TouchEvent
+typealias NativeTouchEvent = org.w3c.dom.TouchEvent
 
 typealias NativePointerEvent = org.w3c.dom.pointerevents.PointerEvent
 
