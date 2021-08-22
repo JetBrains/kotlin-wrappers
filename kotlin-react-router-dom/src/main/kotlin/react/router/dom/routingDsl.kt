9ff4f7306b1f54a94bf8c5398a9aa1488af2540d
aerialist7 <aerialist7@github.com>
2021-08-22 21:09:28 +0400

    Remove redundant `RProps` usages

--- kotlin-react-router-dom/src/main/kotlin/react/router/dom/routingDsl.kt
+++ kotlin-react-router-dom/src/main/kotlin/react/router/dom/routingDsl.kt
@@ -13,7 +13,7 @@ fun RBuilder.hashRouter(
     basename: String = "",
     hashType: HashType = HashType.slash,
     getUserConfirmation: GetUserConfirmation? = null,
-    handler: RHandler<RProps>,
+    handler: RHandler<*>,
 ) {
     HashRouter {
         attrs {
@@ -31,7 +31,7 @@ fun RBuilder.browserRouter(
     getUserConfirmation: GetUserConfirmation? = null,
     forceRefresh: Boolean = false,
     keyLength: Int = 6,
-    handler: RHandler<RProps>,
+    handler: RHandler<*>,
 ) {
     BrowserRouter {
         attrs {
@@ -46,7 +46,7 @@ fun RBuilder.browserRouter(
 }
 
 fun RBuilder.switch(
-    handler: RHandler<RProps>,
+    handler: RHandler<*>,
 ) {
     Switch(handler)
 }
@@ -117,7 +117,7 @@ fun RBuilder.routeLink(
     to: String,
     replace: Boolean = false,
     className: String? = null,
-    handler: RHandler<RProps>?,
+    handler: RHandler<*>?,
 ) {
     Link {
         attrs {
