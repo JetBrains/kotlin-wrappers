ef4528c5a54827cc690a53b771f23d49b6e1116d
Victor Turansky <victor.turansky@gmail.com>
2021-08-23 17:00:50 +0300

    Strict type for `VirtualItem.measureRef`

--- kotlin-react-virtual/src/main/kotlin/react/virtual/VirtualItem.kt
+++ kotlin-react-virtual/src/main/kotlin/react/virtual/VirtualItem.kt
@@ -1,11 +1,12 @@
 package react.virtual
 
 import org.w3c.dom.HTMLElement
+import react.RefCallback
 
 external interface VirtualItem {
     val index: Int
     val start: Int
     val size: Int
     val end: Int
-    val measureRef: (el: HTMLElement?) -> Unit
+    val measureRef: RefCallback<HTMLElement>
 }
