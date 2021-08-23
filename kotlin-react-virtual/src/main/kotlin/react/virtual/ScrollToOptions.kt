c56ed96d97c5fd370a42d1709ae3fb19b2d5ba51
Victor Turansky <victor.turansky@gmail.com>
2021-08-23 12:34:23 +0300

    Fix React Virtual signatures (#651)
    * Fix React Virtual signatures
    
    * Add `RangeExtractor` typealias

---
+++ kotlin-react-virtual/src/main/kotlin/react/virtual/ScrollToOptions.kt
@@ -0,0 +1,8 @@
+package react.virtual
+
+external interface ScrollToOptions {
+    var align: ScrollAlign
+}
+
+external interface ScrollToIndexOptions : ScrollToOptions
+external interface ScrollToOffsetOptions : ScrollToOptions
--- kotlin-react-virtual/src/main/kotlin/react/virtual/Size.kt
+++ kotlin-react-virtual/src/main/kotlin/react/virtual/Size.kt
@@ -1,6 +1,17 @@
 package react.virtual
 
+import kotlinext.js.jsObject
+
 external interface Size {
     var width: Int
     var height: Int
 }
+
+fun Size(
+    width: Int,
+    height: Int,
+): Size =
+    jsObject {
+        this.width = width
+        this.height = height
+    }
