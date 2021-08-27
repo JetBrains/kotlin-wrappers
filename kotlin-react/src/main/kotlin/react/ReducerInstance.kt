0b0844fc2699718f791d64ca2f1366f12ee9396b
Alex Popoff <32072831+aerialist7@users.noreply.github.com>
2021-08-27 17:39:49 +0400

    Update ReducerInstance.kt

--- kotlin-react/src/main/kotlin/react/ReducerInstance.kt
+++ kotlin-react/src/main/kotlin/react/ReducerInstance.kt
@@ -7,18 +7,6 @@ import kotlinext.js.Tuple
 typealias Reducer<S, A> = (state: S, action: A) -> S
 typealias Dispatch<A> = (action: A) -> Unit
 
-@Deprecated(
-    message = "Legacy type alias",
-    replaceWith = ReplaceWith("Reducer", "react.Reducer"),
-)
-typealias RReducer<S, A> = Reducer<S, A>
-
-@Deprecated(
-    message = "Legacy type alias",
-    replaceWith = ReplaceWith("Dispatch", "react.Dispatch"),
-)
-typealias RDispatch<A> = Dispatch<A>
-
 /**
  * Only works inside [fc]
  * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
