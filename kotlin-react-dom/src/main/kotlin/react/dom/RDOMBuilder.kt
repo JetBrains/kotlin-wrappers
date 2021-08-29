073a024012ca335611bdfd7fdfa84c4c0e006520
aerialist7 <aerialist7@github.com>
2021-08-29 22:02:33 +0400

    Styled: No more useless `WithClassName`

--- kotlin-react-dom/src/main/kotlin/react/dom/RDOMBuilder.kt
+++ kotlin-react-dom/src/main/kotlin/react/dom/RDOMBuilder.kt
@@ -12,7 +12,8 @@ external interface InnerHTML {
 external interface WithClassName : PropsWithChildren {
     var className: String?
 }
-external interface DOMProps : WithClassName {
+
+external interface DOMProps : PropsWithClassName, PropsWithChildren {
     var dangerouslySetInnerHTML: InnerHTML?
 }
 
