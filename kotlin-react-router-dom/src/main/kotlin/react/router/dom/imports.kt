0dc4c95c624965867950edc1bb2415e56a1ba713
aerialist7 <aerialist7@github.com>
2021-08-30 23:51:46 +0400

    React Router DOM: `withRouter` separation

--- kotlin-react-router-dom/src/main/kotlin/react/router/dom/imports.kt
+++ kotlin-react-router-dom/src/main/kotlin/react/router/dom/imports.kt
@@ -3,9 +3,6 @@
 
 package react.router.dom
 
-import react.ComponentClass
-import react.Props
-
 external fun useHistory(): History
 
 external fun useLocation(): Location
@@ -18,5 +15,3 @@ external fun rawUseRouteMatch(options: dynamic): Match?
 @JsName("matchPath")
 external fun rawMatchPath(pathName: String, options: dynamic): Match?
 
-@JsName("withRouter")
-external fun <T : Props> rawWithRouter(component: dynamic): ComponentClass<T>
