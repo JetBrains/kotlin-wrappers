cb88d8a6d68d2cd0c185b05d245a2acdf59fd3a5
Alex Popoff <32072831+aerialist7@users.noreply.github.com>
2021-08-31 01:24:34 +0400

    React Router DOM: `withRouter` separation (#683)
    Co-authored-by: Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>

--- kotlin-react-router-dom/src/main/kotlin/react/router/dom/imports.kt
+++ kotlin-react-router-dom/src/main/kotlin/react/router/dom/imports.kt
@@ -3,9 +3,6 @@
 
 package react.router.dom
 
-import react.ComponentClass
-import react.Props
-
 external fun useHistory(): History
 
 external fun useLocation(): Location
@@ -15,5 +12,3 @@ external fun useParams(): Params
 @JsName("useRouteMatch")
 external fun rawUseRouteMatch(options: dynamic): Match?
 
-@JsName("withRouter")
-external fun <T : Props> rawWithRouter(component: dynamic): ComponentClass<T>
