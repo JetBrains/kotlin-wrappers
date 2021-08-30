0743b9c0c6add018400a26dd99d1cc65252bfbb1
aerialist7 <aerialist7@github.com>
2021-08-31 00:01:16 +0400

    Use `Cleanup`

--- kotlin-react/src/main/kotlin/react/Raw.kt
+++ kotlin-react/src/main/kotlin/react/Raw.kt
@@ -4,19 +4,21 @@
 package react
 
 @JsName("forwardRef")
-external fun <P : Props> rawForwardRef(forward: (props: P, ref: Ref<*>) -> dynamic): ComponentType<P>
+external fun <P : Props> rawForwardRef(
+    forward: (props: P, ref: Ref<*>) -> dynamic,
+): ComponentType<P>
 
 // Effect Hook (16.8+)
 @JsName("useEffect")
 external fun rawUseEffect(
-    effect: () -> dynamic,
+    effect: () -> Cleanup?,
     dependencies: RDependenciesArray = definedExternally,
 )
 
 // Layout Effect Hook (16.8+)
 @JsName("useLayoutEffect")
 external fun rawUseLayoutEffect(
-    effect: () -> dynamic,
+    effect: () -> Cleanup?,
     dependencies: RDependenciesArray = definedExternally,
 )
 
