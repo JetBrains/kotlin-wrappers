2af3f390869ae503d378206001d05af3acd61543
Pavel Matusevich <neitex@protonmail.com>
2021-08-31 22:55:09 +0300

    Applied changes request

--- kotlin-ring-ui/src/main/kotlin/ringui/Loader.ext.kt
+++ kotlin-ring-ui/src/main/kotlin/ringui/Loader.ext.kt
@@ -1,11 +1,5 @@
 package ringui
 
-interface Color {
-    var r: Short
-    var g: Short
-    var b: Short
-}
-
 var LoaderProps.dataTest: String
     get() = asDynamic()["data-test"] as? String ?: ""
     set(value) {
