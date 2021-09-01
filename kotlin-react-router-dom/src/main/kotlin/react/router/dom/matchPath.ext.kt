391d7a1ec97d1afeec00f5432a762913eac5a30b
sergeigrishchenko <s.i.grishchenko@gmail.com>
2021-09-01 21:50:20 +0300

    more overloads for MatchOptions builders

--- kotlin-react-router-dom/src/main/kotlin/react/router/dom/matchPath.ext.kt
+++ kotlin-react-router-dom/src/main/kotlin/react/router/dom/matchPath.ext.kt
@@ -4,7 +4,13 @@ import kotlinext.js.jso
 
 fun matchPath(
     pathName: String,
-    parent: Match? = null,
+    block: MatchOptions.() -> Unit,
+): Match? =
+    matchPath(pathName, options = jso(block))
+
+fun matchPath(
+    pathName: String,
+    parent: Match,
     block: MatchOptions.() -> Unit,
 ): Match? =
     matchPath(pathName, options = jso(block), parent)
