dab76d71cb02ed7b58bb90e58d679bab4693adac
Victor Turansky <victor.turansky@gmail.com>
2021-09-02 23:28:43 +0300

    Add `useRefState` hook

---
+++ kotlin-react/src/main/kotlin/react/useRefState.kt
@@ -0,0 +1,10 @@
+@file:JsModule("react")
+
+package react
+
+import kotlinext.js.Tuple
+
+// [Example](https://popper.js.org/react-popper/v2/#example)
+
+@JsName("useState")
+external fun <T : Any> useRefState(): Tuple<T?, RefCallback<T>>
