254bc3e73d10ee4b1d61b8e14ea6873d73b49555
Victor Turansky <victor.turansky@gmail.com>
2021-09-14 20:53:36 +0300

    CSSType. Missed `AbsoluteSize` [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/AbsoluteSize.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+package csstype
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""({large: 'large', medium: 'medium', small: 'small', xLarge: 'x-large', xSmall: 'x-small', xxLarge: 'xx-large', xxSmall: 'xx-small', xxxLarge: 'xxx-large'})""")
+external enum class AbsoluteSize {
+    large,
+    medium,
+    small,
+    xLarge,
+    xSmall,
+    xxLarge,
+    xxSmall,
+    xxxLarge,
+
+    ;
+}
