2fb67213124d98499b1e55290e4c93503b2de558
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 04:28:42 +0300

    CSSType. Property unions #4 [generated]

--- kotlin-csstype/src/main/generated/csstype/AccentColor.kt
+++ kotlin-csstype/src/main/generated/csstype/AccentColor.kt
@@ -1,6 +1,17 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | DataType.Color | "auto"
-sealed external interface AccentColor
+// ColorProperty
+// language=JavaScript
+@JsName("""({auto: 'auto'})""")
+sealed external interface AccentColor {
+    companion object {
+        val auto: AccentColor
+    }
+}
--- kotlin-csstype/src/main/generated/csstype/CaretColor.kt
+++ kotlin-csstype/src/main/generated/csstype/CaretColor.kt
@@ -1,6 +1,17 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | DataType.Color | "auto"
-sealed external interface CaretColor
+// ColorProperty
+// language=JavaScript
+@JsName("""({auto: 'auto'})""")
+sealed external interface CaretColor {
+    companion object {
+        val auto: CaretColor
+    }
+}
