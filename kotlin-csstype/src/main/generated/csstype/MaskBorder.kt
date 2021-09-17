5bc563255b219cbb632e59d9725e8fce78e89fd0
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 10:24:25 +0300

    CSSType. Property unions #5 [generated] (#762)

--- kotlin-csstype/src/main/generated/csstype/MaskBorder.kt
+++ kotlin-csstype/src/main/generated/csstype/MaskBorder.kt
@@ -1,6 +1,23 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | "alpha" | "luminance" | "none" | "repeat" | "round" | "space" | "stretch" | (string & {}) | (number & {})
-sealed external interface MaskBorder
+// Globals
+// language=JavaScript
+@JsName("""({alpha: 'alpha', luminance: 'luminance', none: 'none', repeat: 'repeat', round: 'round', space: 'space', stretch: 'stretch'})""")
+sealed external interface MaskBorder {
+    companion object {
+        val alpha: MaskBorder
+        val luminance: MaskBorder
+        val none: MaskBorder
+        val repeat: MaskBorder
+        val round: MaskBorder
+        val space: MaskBorder
+        val stretch: MaskBorder
+    }
+}
