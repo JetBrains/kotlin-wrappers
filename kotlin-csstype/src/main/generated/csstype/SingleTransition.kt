bba9b9479c77b585c5c47911a58297bca290408d
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 10:24:57 +0300

    CSSType. Denormalization #5 [generated] (#763)

--- kotlin-csstype/src/main/generated/csstype/SingleTransition.kt
+++
@@ -1,6 +0,0 @@
-// Automatically generated - do not modify!
-
-package csstype
-
-// EasingFunction | TTime | "all" | "none" | (string & {})
-sealed external interface SingleTransition
--- kotlin-csstype/src/main/generated/csstype/TransitionTimingFunction.kt
+++ kotlin-csstype/src/main/generated/csstype/TransitionTimingFunction.kt
@@ -1,6 +1,23 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | DataType.EasingFunction | (string & {})
-sealed external interface TransitionTimingFunction
+// Globals
+// language=JavaScript
+@JsName("""({ease: 'ease', easeIn: 'ease-in', easeInOut: 'ease-in-out', easeOut: 'ease-out', stepEnd: 'step-end', stepStart: 'step-start', linear: 'linear'})""")
+sealed external interface TransitionTimingFunction {
+    companion object {
+        val ease: TransitionTimingFunction
+        val easeIn: TransitionTimingFunction
+        val easeInOut: TransitionTimingFunction
+        val easeOut: TransitionTimingFunction
+        val stepEnd: TransitionTimingFunction
+        val stepStart: TransitionTimingFunction
+        val linear: TransitionTimingFunction
+    }
+}
