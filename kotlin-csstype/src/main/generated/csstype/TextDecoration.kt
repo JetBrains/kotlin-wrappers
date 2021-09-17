d433a0d1517721a7c1a988357eddfba8c8ad4f0b
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 05:19:50 +0300

    CSSType. Property unions #5 [generated]

--- kotlin-csstype/src/main/generated/csstype/TextDecoration.kt
+++ kotlin-csstype/src/main/generated/csstype/TextDecoration.kt
@@ -1,25 +1,30 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-/*
-| Globals
-| DataType.Color
-| TLength
-| "auto"
-| "blink"
-| "dashed"
-| "dotted"
-| "double"
-| "from-font"
-| "grammar-error"
-| "line-through"
-| "none"
-| "overline"
-| "solid"
-| "spelling-error"
-| "underline"
-| "wavy"
-| (string & {})
-*/
-sealed external interface TextDecoration
+// ColorProperty
+// language=JavaScript
+@JsName("""({auto: 'auto', blink: 'blink', dashed: 'dashed', dotted: 'dotted', double: 'double', fromFont: 'from-font', grammarError: 'grammar-error', lineThrough: 'line-through', none: 'none', overline: 'overline', solid: 'solid', spellingError: 'spelling-error', underline: 'underline', wavy: 'wavy'})""")
+sealed external interface TextDecoration {
+    companion object {
+        val auto: TextDecoration
+        val blink: TextDecoration
+        val dashed: TextDecoration
+        val dotted: TextDecoration
+        val double: TextDecoration
+        val fromFont: TextDecoration
+        val grammarError: TextDecoration
+        val lineThrough: TextDecoration
+        val none: TextDecoration
+        val overline: TextDecoration
+        val solid: TextDecoration
+        val spellingError: TextDecoration
+        val underline: TextDecoration
+        val wavy: TextDecoration
+    }
+}
