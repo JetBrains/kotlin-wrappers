1c6807fa64df41123b24988f6b4eb55bd0ee8677
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 10:22:10 +0300

    CSSType. Denormalization 2 [generated] (#759)

--- kotlin-csstype/src/main/generated/csstype/Content.kt
+++ kotlin-csstype/src/main/generated/csstype/Content.kt
@@ -1,6 +1,23 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | DataType.ContentList | "none" | "normal" | (string & {})
-sealed external interface Content
+// Globals
+// language=JavaScript
+@JsName("""({closeQuote: 'close-quote', noCloseQuote: 'no-close-quote', noOpenQuote: 'no-open-quote', openQuote: 'open-quote', contents: 'contents', none: 'none', normal: 'normal'})""")
+sealed external interface Content {
+    companion object {
+        val closeQuote: Content
+        val noCloseQuote: Content
+        val noOpenQuote: Content
+        val openQuote: Content
+        val contents: Content
+        val none: Content
+        val normal: Content
+    }
+}
