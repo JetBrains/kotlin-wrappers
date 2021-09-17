0e8c3a6e4532dd1df79af9073b4b75849625a6e4
Victor Turansky <victor.turansky@gmail.com>
2021-09-17 06:52:54 +0300

    CSSType. Property unions #6 [generated]

--- kotlin-csstype/src/main/generated/csstype/TransformOrigin.kt
+++ kotlin-csstype/src/main/generated/csstype/TransformOrigin.kt
@@ -1,6 +1,21 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | TLength | "bottom" | "center" | "left" | "right" | "top" | (string & {})
-sealed external interface TransformOrigin
+// LengthProperty
+// language=JavaScript
+@JsName("""({bottom: 'bottom', center: 'center', left: 'left', right: 'right', top: 'top'})""")
+sealed external interface TransformOrigin {
+    companion object {
+        val bottom: TransformOrigin
+        val center: TransformOrigin
+        val left: TransformOrigin
+        val right: TransformOrigin
+        val top: TransformOrigin
+    }
+}
