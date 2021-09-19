bd23c7c9bc02370fbedad547587b2c6271647f0d
Victor Turansky <victor.turansky@gmail.com>
2021-09-19 14:16:20 +0300

    CSSType. Actualize `LengthProperty` [generated]

--- kotlin-csstype/src/main/generated/csstype/LengthProperty.kt
+++ kotlin-csstype/src/main/generated/csstype/LengthProperty.kt
@@ -4,13 +4,18 @@ package csstype
 
 // Globals | TLength
 sealed external interface LengthProperty :
+    BackgroundPosition,
     BackgroundPositionX,
     BackgroundPositionY,
+    BackgroundSize,
     Flex,
     FlexBasis,
+    FontSize,
+    FontSmooth,
     Gap,
     LetterSpacing,
     MaskPosition,
+    MaskSize,
     ObjectPosition,
     Offset,
     OffsetAnchor,
