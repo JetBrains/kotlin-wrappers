734968abd5f2fb13abd0373775fcf35957f030e3
Victor Turansky <victor.turansky@gmail.com>
2021-09-19 13:24:19 +0300

    CSSType. "Number like" type support #1 [generated]

--- kotlin-csstype/src/main/generated/csstype/ColumnCount.kt
+++ kotlin-csstype/src/main/generated/csstype/ColumnCount.kt
@@ -1,6 +1,20 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
+)
+
 package csstype
 
-// Globals | "auto" | (number & {})
-sealed external interface ColumnCount
+// language=JavaScript
+@JsName("""({auto: 'auto'})""")
+sealed external interface ColumnCount {
+    companion object {
+        val auto: ColumnCount
+    }
+}
+
+inline fun ColumnCount(value: Int): ColumnCount =
+    value.unsafeCast<ColumnCount>()
