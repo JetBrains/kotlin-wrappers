1ad110e19eee7619a6287ed231ac98c0a72aaa93
Victor Turansky <victor.turansky@gmail.com>
2021-09-21 04:12:20 +0300

    CSSType. Denormalization #9 [generated]

--- kotlin-csstype/src/main/generated/csstype/OverflowClipMargin.kt
+++ kotlin-csstype/src/main/generated/csstype/OverflowClipMargin.kt
@@ -1,6 +1,18 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | DataType.VisualBox | TLength | (string & {})
-sealed external interface OverflowClipMargin
+// language=JavaScript
+@JsName("""({borderBox: 'border-box', contentBox: 'content-box', paddingBox: 'padding-box'})""")
+sealed external interface OverflowClipMargin {
+    companion object {
+        val borderBox: OverflowClipMargin
+        val contentBox: OverflowClipMargin
+        val paddingBox: OverflowClipMargin
+    }
+}
