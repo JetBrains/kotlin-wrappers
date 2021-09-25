8b4dbc9b73624ae53be60dc221533c0f002fc3ba
Victor Turansky <victor.turansky@gmail.com>
2021-09-26 02:20:42 +0300

    CSSType. Initial `FontWeight` [generated]

--- kotlin-csstype/src/main/generated/csstype/FontWeight.kt
+++ kotlin-csstype/src/main/generated/csstype/FontWeight.kt
@@ -1,6 +1,23 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
+)
+
 package csstype
 
-// Globals | DataType.FontWeightAbsolute | "bolder" | "lighter"
-sealed external interface FontWeight
+// language=JavaScript
+@JsName("""({bold: 'bold', normal: 'normal', bolder: 'bolder', lighter: 'lighter'})""")
+sealed external interface FontWeight {
+    companion object {
+        val bold: FontWeight
+        val normal: FontWeight
+        val bolder: FontWeight
+        val lighter: FontWeight
+    }
+}
+
+inline fun FontWeight(value: Int): FontWeight =
+    value.unsafeCast<FontWeight>()
