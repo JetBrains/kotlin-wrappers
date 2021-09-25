a39007e378e6e2a67f995a0e7b84618d18bfbe89
Victor Turansky <victor.turansky@gmail.com>
2021-09-25 20:56:28 +0300

    CSSType. Initial `PseudosRuleBuilder` [generated]

--- kotlin-csstype/src/main/generated/csstype/Pseudos.kt
+++
@@ -1,6 +0,0 @@
-// Automatically generated - do not modify!
-
-package csstype
-
-// AdvancedPseudos | SimplePseudos
-sealed external interface Pseudos
---
+++ kotlin-csstype/src/main/generated/csstype/SimplePseudosRuleBuilder.kt
@@ -0,0 +1,381 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "DECLARATION_CANT_BE_INLINED",
+)
+
+package csstype
+
+interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
+    inline fun after(
+        block: T.() -> Unit,
+    ) {
+        "::after"(block)
+    }
+
+    inline fun backdrop(
+        block: T.() -> Unit,
+    ) {
+        "::backdrop"(block)
+    }
+
+    inline fun before(
+        block: T.() -> Unit,
+    ) {
+        "::before"(block)
+    }
+
+    inline fun cue(
+        block: T.() -> Unit,
+    ) {
+        "::cue"(block)
+    }
+
+    inline fun cueRegion(
+        block: T.() -> Unit,
+    ) {
+        "::cue-region"(block)
+    }
+
+    inline fun firstLetter(
+        block: T.() -> Unit,
+    ) {
+        "::first-letter"(block)
+    }
+
+    inline fun firstLine(
+        block: T.() -> Unit,
+    ) {
+        "::first-line"(block)
+    }
+
+    inline fun grammarError(
+        block: T.() -> Unit,
+    ) {
+        "::grammar-error"(block)
+    }
+
+    inline fun marker(
+        block: T.() -> Unit,
+    ) {
+        "::marker"(block)
+    }
+
+    inline fun placeholder(
+        block: T.() -> Unit,
+    ) {
+        "::placeholder"(block)
+    }
+
+    inline fun selection(
+        block: T.() -> Unit,
+    ) {
+        "::selection"(block)
+    }
+
+    inline fun spellingError(
+        block: T.() -> Unit,
+    ) {
+        "::spelling-error"(block)
+    }
+
+    inline fun targetText(
+        block: T.() -> Unit,
+    ) {
+        "::target-text"(block)
+    }
+
+    inline fun active(
+        block: T.() -> Unit,
+    ) {
+        ":active"(block)
+    }
+
+    inline fun anyLink(
+        block: T.() -> Unit,
+    ) {
+        ":any-link"(block)
+    }
+
+    inline fun blank(
+        block: T.() -> Unit,
+    ) {
+        ":blank"(block)
+    }
+
+    inline fun checked(
+        block: T.() -> Unit,
+    ) {
+        ":checked"(block)
+    }
+
+    inline fun current(
+        block: T.() -> Unit,
+    ) {
+        ":current"(block)
+    }
+
+    inline fun default(
+        block: T.() -> Unit,
+    ) {
+        ":default"(block)
+    }
+
+    inline fun defined(
+        block: T.() -> Unit,
+    ) {
+        ":defined"(block)
+    }
+
+    inline fun disabled(
+        block: T.() -> Unit,
+    ) {
+        ":disabled"(block)
+    }
+
+    inline fun empty(
+        block: T.() -> Unit,
+    ) {
+        ":empty"(block)
+    }
+
+    inline fun enabled(
+        block: T.() -> Unit,
+    ) {
+        ":enabled"(block)
+    }
+
+    inline fun first(
+        block: T.() -> Unit,
+    ) {
+        ":first"(block)
+    }
+
+    inline fun firstChild(
+        block: T.() -> Unit,
+    ) {
+        ":first-child"(block)
+    }
+
+    inline fun firstOfType(
+        block: T.() -> Unit,
+    ) {
+        ":first-of-type"(block)
+    }
+
+    inline fun focus(
+        block: T.() -> Unit,
+    ) {
+        ":focus"(block)
+    }
+
+    inline fun focusVisible(
+        block: T.() -> Unit,
+    ) {
+        ":focus-visible"(block)
+    }
+
+    inline fun focusWithin(
+        block: T.() -> Unit,
+    ) {
+        ":focus-within"(block)
+    }
+
+    inline fun fullscreen(
+        block: T.() -> Unit,
+    ) {
+        ":fullscreen"(block)
+    }
+
+    inline fun future(
+        block: T.() -> Unit,
+    ) {
+        ":future"(block)
+    }
+
+    inline fun hover(
+        block: T.() -> Unit,
+    ) {
+        ":hover"(block)
+    }
+
+    inline fun inRange(
+        block: T.() -> Unit,
+    ) {
+        ":in-range"(block)
+    }
+
+    inline fun indeterminate(
+        block: T.() -> Unit,
+    ) {
+        ":indeterminate"(block)
+    }
+
+    inline fun invalid(
+        block: T.() -> Unit,
+    ) {
+        ":invalid"(block)
+    }
+
+    inline fun lastChild(
+        block: T.() -> Unit,
+    ) {
+        ":last-child"(block)
+    }
+
+    inline fun lastOfType(
+        block: T.() -> Unit,
+    ) {
+        ":last-of-type"(block)
+    }
+
+    inline fun left(
+        block: T.() -> Unit,
+    ) {
+        ":left"(block)
+    }
+
+    inline fun link(
+        block: T.() -> Unit,
+    ) {
+        ":link"(block)
+    }
+
+    inline fun localLink(
+        block: T.() -> Unit,
+    ) {
+        ":local-link"(block)
+    }
+
+    inline fun nthCol(
+        block: T.() -> Unit,
+    ) {
+        ":nth-col"(block)
+    }
+
+    inline fun nthLastCol(
+        block: T.() -> Unit,
+    ) {
+        ":nth-last-col"(block)
+    }
+
+    inline fun onlyChild(
+        block: T.() -> Unit,
+    ) {
+        ":only-child"(block)
+    }
+
+    inline fun onlyOfType(
+        block: T.() -> Unit,
+    ) {
+        ":only-of-type"(block)
+    }
+
+    inline fun optional(
+        block: T.() -> Unit,
+    ) {
+        ":optional"(block)
+    }
+
+    inline fun outOfRange(
+        block: T.() -> Unit,
+    ) {
+        ":out-of-range"(block)
+    }
+
+    inline fun past(
+        block: T.() -> Unit,
+    ) {
+        ":past"(block)
+    }
+
+    inline fun paused(
+        block: T.() -> Unit,
+    ) {
+        ":paused"(block)
+    }
+
+    inline fun pictureInPicture(
+        block: T.() -> Unit,
+    ) {
+        ":picture-in-picture"(block)
+    }
+
+    inline fun placeholderShown(
+        block: T.() -> Unit,
+    ) {
+        ":placeholder-shown"(block)
+    }
+
+    inline fun readOnly(
+        block: T.() -> Unit,
+    ) {
+        ":read-only"(block)
+    }
+
+    inline fun readWrite(
+        block: T.() -> Unit,
+    ) {
+        ":read-write"(block)
+    }
+
+    inline fun required(
+        block: T.() -> Unit,
+    ) {
+        ":required"(block)
+    }
+
+    inline fun right(
+        block: T.() -> Unit,
+    ) {
+        ":right"(block)
+    }
+
+    inline fun root(
+        block: T.() -> Unit,
+    ) {
+        ":root"(block)
+    }
+
+    inline fun scope(
+        block: T.() -> Unit,
+    ) {
+        ":scope"(block)
+    }
+
+    inline fun target(
+        block: T.() -> Unit,
+    ) {
+        ":target"(block)
+    }
+
+    inline fun targetWithin(
+        block: T.() -> Unit,
+    ) {
+        ":target-within"(block)
+    }
+
+    inline fun userInvalid(
+        block: T.() -> Unit,
+    ) {
+        ":user-invalid"(block)
+    }
+
+    inline fun userValid(
+        block: T.() -> Unit,
+    ) {
+        ":user-valid"(block)
+    }
+
+    inline fun valid(
+        block: T.() -> Unit,
+    ) {
+        ":valid"(block)
+    }
+
+    inline fun visited(
+        block: T.() -> Unit,
+    ) {
+        ":visited"(block)
+    }
+}
