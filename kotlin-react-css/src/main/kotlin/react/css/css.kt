eecf91c7a5518c7c7945421ce96c74e0643b2dff
Victor Turansky <victor.turansky@gmail.com>
2021-09-26 12:34:12 +0300

    React CSS. Add `css` extensions (#819)

--- kotlin-react-css/src/main/kotlin/react/css/css.kt
+++ kotlin-react-css/src/main/kotlin/react/css/css.kt
@@ -1,12 +1,44 @@
 package react.css
 
+import emotion.css.cx
 import kotlinext.js.jso
 import react.PropsWithClassName
 import react.RElementBuilder
 
+@PublishedApi
+internal inline fun className(
+    crossinline block: PropertiesBuilder.() -> Unit,
+): String =
+    emotion.css.css(jso(block))
+
+// Props
+
+inline fun PropsWithClassName.css(
+    crossinline block: PropertiesBuilder.() -> Unit,
+) {
+    className = className(block)
+}
+
+inline fun PropsWithClassName.css(
+    vararg classNames: String,
+    crossinline block: PropertiesBuilder.() -> Unit,
+) {
+    className = cx(classNames = classNames + className(block))
+}
+
+// Builder
+
 inline fun RElementBuilder<PropsWithClassName>.css(
     crossinline block: PropertiesBuilder.() -> Unit,
 ) {
-    attrs.className = emotion.css.css(jso(block))
+    attrs.css(block)
 }
+
+inline fun RElementBuilder<PropsWithClassName>.css(
+    vararg classNames: String,
+    crossinline block: PropertiesBuilder.() -> Unit,
+) {
+    attrs.css(classNames = classNames, block = block)
+}
+
 
