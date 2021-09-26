4d7134850a425a9f591d6550dbb1691468818905
Victor Turansky <victor.turansky@gmail.com>
2021-09-26 12:28:26 +0300

    CSSType. Remove `@viewport` types [generated] (#816)

--- kotlin-csstype/src/main/generated/csstype/MinZoom.kt
+++
@@ -1,6 +0,0 @@
-// Automatically generated - do not modify!
-
-package csstype
-
-// "auto" | (string & {}) | (number & {})
-sealed external interface MinZoom
--- kotlin-csstype/src/main/generated/csstype/ViewportLength.kt
+++
@@ -1,6 +0,0 @@
-// Automatically generated - do not modify!
-
-package csstype
-
-// TLength | "auto" | (string & {})
-sealed external interface ViewportLength
