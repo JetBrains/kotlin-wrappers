f53d65fd3e341ef60061ad3038aa968fdfbf5e3b
Victor Turansky <victor.turansky@gmail.com>
2021-09-26 12:25:18 +0300

    CSSType. Double-like types [generated] (#814)

--- kotlin-csstype/src/main/generated/csstype/Scale.kt
+++ kotlin-csstype/src/main/generated/csstype/Scale.kt
@@ -1,6 +1,20 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
+)
+
 package csstype
 
-// Globals | "none" | (string & {}) | (number & {})
-sealed external interface Scale
+// language=JavaScript
+@JsName("""({none: 'none'})""")
+sealed external interface Scale {
+    companion object {
+        val none: Scale
+    }
+}
+
+inline fun Scale(value: Double): Scale =
+    value.unsafeCast<Scale>()
