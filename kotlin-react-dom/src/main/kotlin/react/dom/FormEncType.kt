5d464c88a9e3c709e82ba97fc30f550eb7396a20
Victor Turansky <victor.turansky@gmail.com>
2021-10-06 20:46:46 +0300

    React. Add `FormEncType` (#853)

---
+++ kotlin-react-dom/src/main/kotlin/react/dom/FormEncType.kt
@@ -0,0 +1,7 @@
+package react.dom
+
+object FormEncType {
+    const val applicationFormUrlEncoded = "application/x-www-form-urlencoded"
+    const val multipartFormData = "multipart/form-data"
+    const val textPlain = "text/plain"
+}
