5eee1fbdcecf990afc7d5ece42b0f27aad0ad527
Victor Turansky <victor.turansky@gmail.com>
2021-10-13 03:15:14 +0300

    `devNpm` for dev dependencies

---
+++ buildSrc/src/main/kotlin/NpmDependencies.kt
@@ -0,0 +1,18 @@
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.the
+import org.jetbrains.kotlin.gradle.targets.js.npm.DevNpmDependencyExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
+import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependencyExtension
+
+private fun Project.npmVersion(name: String): String =
+    "^${version(name)}"
+
+fun Project.npmv(name: String): NpmDependency {
+    val npm = dependencies.the<NpmDependencyExtension>()
+    return npm(name, npmVersion(name))
+}
+
+fun Project.devNpmv(name: String): NpmDependency {
+    val devNpm = dependencies.the<DevNpmDependencyExtension>()
+    return devNpm(name, npmVersion(name))
+}
