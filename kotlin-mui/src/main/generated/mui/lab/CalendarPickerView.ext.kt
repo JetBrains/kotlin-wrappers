62d0791eb1bd0d5c24a39dd83ee1b0a4005952ca
Victor Turansky <victor.turansky@gmail.com>
2021-10-23 13:51:06 +0300

    MUI. `CalendarPickerView` [generated]

---
+++ kotlin-mui/src/main/generated/mui/lab/CalendarPickerView.ext.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+package mui.lab
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{year: 'year', day: 'day', month: 'month'}/*union*/)""")
+external enum class CalendarPickerView {
+    year,
+    day,
+    month,
+
+    ;
+}
