fa882e8c89e4b713ab62844426835b2502ac1ca3
Victor Turansky <victor.turansky@gmail.com>
2021-10-24 16:20:10 +0300

    MUI. `TreeViewPropsBase` and params [generated] (#904)

--- kotlin-mui/src/main/generated/mui/lab/TreeView.kt
+++ kotlin-mui/src/main/generated/mui/lab/TreeView.kt
@@ -9,7 +9,95 @@ import kotlinext.js.ReadonlyArray
 
 external interface TreeViewProps : react.Props
 
-external interface MultiSelectTreeViewProps : react.Props {
+external interface TreeViewPropsBase :
+    mui.system.StandardProps,
+    react.dom.html.HTMLAttributes<org.w3c.dom.HTMLUListElement>,
+    react.PropsWithChildren {
+    /**
+     * The content of the component.
+     */
+    override var children: ReadonlyArray<react.ReactNode>?
+
+    /**
+     * Override or extend the styles applied to the component.
+     */
+    var classes: TreeViewClasses?
+
+    /**
+     * The default icon used to collapse the node.
+     */
+    var defaultCollapseIcon: react.ReactNode?
+
+    /**
+     * The default icon displayed next to a end node. This is applied to all
+     * tree nodes and can be overridden by the TreeItem `icon` prop.
+     */
+    var defaultEndIcon: react.ReactNode?
+
+    /**
+     * Expanded node ids. (Uncontrolled)
+     * @default []
+     */
+    var defaultExpanded: ReadonlyArray<String>?
+
+    /**
+     * The default icon used to expand the node.
+     */
+    var defaultExpandIcon: react.ReactNode?
+
+    /**
+     * The default icon displayed next to a parent node. This is applied to all
+     * parent nodes and can be overridden by the TreeItem `icon` prop.
+     */
+    var defaultParentIcon: react.ReactNode?
+
+    /**
+     * If `true`, will allow focus on disabled items.
+     * @default false
+     */
+    var disabledItemsFocusable: Boolean?
+
+    /**
+     * If `true` selection is disabled.
+     * @default false
+     */
+    var disableSelection: Boolean?
+
+    /**
+     * Expanded node ids. (Controlled)
+     */
+    var expanded: ReadonlyArray<String>?
+
+    /**
+     * This prop is used to help implement the accessibility logic.
+     * If you don't provide this prop. It falls back to a randomly generated id.
+     */
+    override var id: String?
+
+    /**
+     * Callback fired when tree items are focused.
+     *
+     * @param {React.SyntheticEvent} event The event source of the callback **Warning**: This is a generic event not a focus event.
+     * @param {string} value of the focused node.
+     */
+    var onNodeFocus: ((event: react.dom.events.SyntheticEvent<*, *>, nodeId: String) -> Unit)?
+
+    /**
+     * Callback fired when tree items are expanded/collapsed.
+     *
+     * @param {React.SyntheticEvent} event The event source of the callback.
+     * @param {array} nodeIds The ids of the expanded nodes.
+     */
+    var onNodeToggle: ((event: react.dom.events.SyntheticEvent<*, *>, nodeIds: ReadonlyArray<String>) -> Unit)?
+
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    var sx: mui.system.SxProps<mui.system.Theme>?
+}
+
+external interface MultiSelectTreeViewProps :
+    TreeViewPropsBase {
     /**
      * Selected node ids. (Uncontrolled)
      * When `multiSelect` is true this takes an array of strings; when false (default) a string.
@@ -39,7 +127,8 @@ external interface MultiSelectTreeViewProps : react.Props {
     var onNodeSelect: ((event: react.dom.events.SyntheticEvent<*, *>, nodeIds: ReadonlyArray<String>) -> Unit)?
 }
 
-external interface SingleSelectTreeViewProps : react.Props {
+external interface SingleSelectTreeViewProps :
+    TreeViewPropsBase {
     /**
      * Selected node ids. (Uncontrolled)
      * When `multiSelect` is true this takes an array of strings; when false (default) a string.
