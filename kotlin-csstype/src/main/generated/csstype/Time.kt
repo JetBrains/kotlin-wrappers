423588d2c343cd745f1d01de8ca21c25fc9e3178
Victor Turansky <victor.turansky@gmail.com>
2021-11-04 02:33:51 +0300

    CSSType. `Time` factories [generated]

--- kotlin-csstype/src/main/generated/csstype/Time.kt
+++ kotlin-csstype/src/main/generated/csstype/Time.kt
@@ -3,3 +3,9 @@
 package csstype
 
 sealed external interface Time : TimeProperty
+
+inline val Number.ms: Time
+    get() = "${this}ms".unsafeCast<Time>()
+
+inline val Number.s: Time
+    get() = "${this}s".unsafeCast<Time>()
