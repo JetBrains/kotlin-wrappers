2b6a926e43409ec8f40907034c2d8ae6e30a77cc
Leonid Khachaturov <leonidkhachaturov@gmail.com>
2021-11-16 09:27:17 +0100

    React 18: useId

---
+++ kotlin-react/src/main/kotlin/react/useId.kt
@@ -0,0 +1,12 @@
+@file:JsModule("react")
+@file:JsNonModule
+
+package react
+
+/**
+ * Available from 18.0
+ *
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+external fun useId(): String
