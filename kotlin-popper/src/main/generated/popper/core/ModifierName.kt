ccac60feef0f09faf192849e608fc8b8880a8859
Victor Turansky <victor.turansky@gmail.com>
2021-11-23 02:18:01 +0300

    Popper. `Modifier` factory [generated]

--- kotlin-popper/src/main/generated/popper/core/ModifierName.kt
+++ kotlin-popper/src/main/generated/popper/core/ModifierName.kt
@@ -2,4 +2,14 @@
 
 package popper.core
 
+import kotlinext.js.jso
+
 external interface ModifierName<Options> : Name
+
+inline fun <Options> ModifierName<Options>.modifier(
+    block: Modifier<Options>.() -> Unit,
+): Modifier<Options> =
+    jso {
+        name = this@modifier
+        block()
+    }
