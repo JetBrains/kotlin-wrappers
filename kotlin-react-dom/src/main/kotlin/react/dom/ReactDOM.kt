9c092c1a8fd85c1b4046ec3c9635143abe4775ac
Victor Turansky <victor.turansky@gmail.com>
2021-11-24 01:28:11 +0300

    React DOM. Sync `createRoot` naming/contract

--- kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
+++ kotlin-react-dom/src/main/kotlin/react/dom/ReactDOM.kt
@@ -1,7 +1,6 @@
 package react.dom
 
 import org.w3c.dom.Element
-import react.ReactElement
 import react.Render
 import react.createElement
 
@@ -10,8 +9,14 @@ fun render(container: Element?, callback: () -> Unit = {}, handler: Render) =
 
 // 18.0+
 // TODO rename to render() after React 18 is released
-fun renderIntoRoot(container: Element?, options: ReactRootOptions? = null, handler: Render): ReactElement =
-    createRoot(container, options).render(createElement(handler))
+fun renderIntoRoot(
+    container: Element,
+    options: RootOptions? = null,
+    handler: Render,
+) {
+    createRoot(container, options)
+        .render(createElement(handler))
+}
 
 fun hydrate(container: Element?, callback: () -> Unit = {}, handler: Render) =
     hydrate(createElement(handler), container, callback)
