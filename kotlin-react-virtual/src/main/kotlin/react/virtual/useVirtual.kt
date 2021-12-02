4c7616a22beeb2f24eea659c186049a939591b19
Victor Turansky <victor.turansky@gmail.com>
2021-12-03 00:30:20 +0300

    React Vurtual. Mark optional properties

--- kotlin-react-virtual/src/main/kotlin/react/virtual/useVirtual.kt
+++ kotlin-react-virtual/src/main/kotlin/react/virtual/useVirtual.kt
@@ -11,20 +11,20 @@ import react.Ref
 
 external interface VirtualOptions<T : Any> {
     var size: Int
-    var parentRef: Ref<T>
-    var estimateSize: (index: Index) -> Int
-    var overscan: Int
-    var horizontal: Boolean
-    var scrollToFn: (offset: Int, defaultScrollToFn: (offset: Int) -> Unit) -> Unit
-    var useObserver: (parentRef: Ref<T>, initialRect: Size?) -> Size
-    var initialRect: Size
-    var paddingStart: Int
-    var paddingEnd: Int
-    var onScrollElement: Ref<HTMLElement>
-    var scrollOffsetFn: (event: Event?) -> Int
-    var keyExtractor: (index: Index) -> Key
-    var measureSize: (el: HTMLElement, horizontal: Boolean) -> Int
-    var rangeExtractor: RangeExtractor
+    var parentRef: Ref<T>?
+    var estimateSize: ((index: Index) -> Int)?
+    var overscan: Int?
+    var horizontal: Boolean?
+    var scrollToFn: ((offset: Int, defaultScrollToFn: (offset: Int) -> Unit) -> Unit)?
+    var useObserver: ((parentRef: Ref<T>, initialRect: Size?) -> Size)?
+    var initialRect: Size?
+    var paddingStart: Int?
+    var paddingEnd: Int?
+    var onScrollElement: Ref<HTMLElement>?
+    var scrollOffsetFn: ((event: Event?) -> Int)?
+    var keyExtractor: ((index: Index) -> Key)?
+    var measureSize: ((el: HTMLElement, horizontal: Boolean) -> Int)?
+    var rangeExtractor: RangeExtractor?
 }
 
 external interface VirtualInstance {
