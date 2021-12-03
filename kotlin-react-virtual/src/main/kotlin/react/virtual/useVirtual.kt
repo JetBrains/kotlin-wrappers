bdbec46af17f714e656e79b248a04499f83eef15
Victor Turansky <victor.turansky@gmail.com>
2021-12-03 10:22:21 +0300

    React Query `2.10.0` (#1009)

--- kotlin-react-virtual/src/main/kotlin/react/virtual/useVirtual.kt
+++ kotlin-react-virtual/src/main/kotlin/react/virtual/useVirtual.kt
@@ -4,27 +4,18 @@
 package react.virtual
 
 import kotlinext.js.ReadonlyArray
-import org.w3c.dom.HTMLElement
-import org.w3c.dom.events.Event
 import react.Key
-import react.Ref
 
-external interface VirtualOptions<T : Any> {
+external interface VirtualOptions<T : Any> : ScrollOptions<T> {
     var size: Int
-    var parentRef: Ref<T>?
     var estimateSize: ((index: Index) -> Int)?
     var overscan: Int?
-    var horizontal: Boolean?
     var scrollToFn: ((offset: Int, defaultScrollToFn: (offset: Int) -> Unit) -> Unit)?
-    var useObserver: ((parentRef: Ref<T>, initialRect: Size?) -> Size)?
-    var initialRect: Size?
     var paddingStart: Int?
     var paddingEnd: Int?
-    var onScrollElement: Ref<HTMLElement>?
-    var scrollOffsetFn: ((event: Event?) -> Int)?
     var keyExtractor: ((index: Index) -> Key)?
-    var measureSize: ((el: HTMLElement, horizontal: Boolean) -> Int)?
     var rangeExtractor: RangeExtractor?
+    var useScroll: ((options: ScrollOptions<T>) -> ScrollInstance)?
 }
 
 external interface VirtualInstance {
