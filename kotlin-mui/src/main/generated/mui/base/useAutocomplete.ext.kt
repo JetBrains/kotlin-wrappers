69f396dcafa6da7d3bdbe4f8258ee3ba0ca6fe63
Victor Turansky <victor.turansky@gmail.com>
2021-12-04 23:01:49 +0300

    MUI. `useAutocomplete` types [generated] (#1017)

---
+++ kotlin-mui/src/main/generated/mui/base/useAutocomplete.ext.kt
@@ -0,0 +1,52 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{keyboard: 'keyboard', mouse: 'mouse', auto: 'auto'}/*union*/)""")
+external enum class AutocompleteHighlightChangeReason {
+    keyboard,
+    mouse,
+    auto,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{createOption: 'createOption', selectOption: 'selectOption', removeOption: 'removeOption', clear: 'clear', blur: 'blur'}/*union*/)""")
+external enum class AutocompleteChangeReason {
+    createOption,
+    selectOption,
+    removeOption,
+    clear,
+    blur,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{createOption: 'createOption', toggleInput: 'toggleInput', escape: 'escape', selectOption: 'selectOption', removeOption: 'removeOption', blur: 'blur'}/*union*/)""")
+external enum class AutocompleteCloseReason {
+    createOption,
+    toggleInput,
+    escape,
+    selectOption,
+    removeOption,
+    blur,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{input: 'input', reset: 'reset', clear: 'clear'}/*union*/)""")
+external enum class AutocompleteInputChangeReason {
+    input,
+    reset,
+    clear,
+
+    ;
+}
