71c15d68c0974884df4480bfae5bc74d4f136ae7
Victor Turansky <victor.turansky@gmail.com>
2021-12-04 19:35:49 +0300

    React. Restore `RefObject` signature

--- kotlin-react/src/main/kotlin/react/Ref.kt
+++ kotlin-react/src/main/kotlin/react/Ref.kt
@@ -3,8 +3,10 @@ package react
 // Refs (16.3+)
 sealed external interface Ref<in T : Any>
 
-typealias RefObject<T> = MutableRefObject<out T>
+external interface RefObject<T : Any> : Ref<T> {
+    val current: T?
+}
 
-sealed external interface MutableRefObject<T : Any> : Ref<T> {
-    var current: T?
+external interface MutableRefObject<T : Any> : RefObject<T> {
+    override var current: T?
 }
