b04836546c53c117d5d311e8079efe2fd041ed1d
Victor Turansky <victor.turansky@gmail.com>
2021-12-05 15:05:12 +0300

    React. `AutocompleteProps` parent types [generated] (#1021)

--- kotlin-mui/src/main/generated/mui/material/Autocomplete.kt
+++ kotlin-mui/src/main/generated/mui/material/Autocomplete.kt
@@ -7,7 +7,10 @@ package mui.material
 
 import kotlinext.js.ReadonlyArray
 
-external interface AutocompleteProps<T> : react.Props {
+external interface AutocompleteProps<T> :
+    mui.base.UseAutocompleteProps<T>,
+    mui.system.StandardProps,
+    react.dom.html.HTMLAttributes<org.w3c.dom.HTMLDivElement> {
     /**
      * Props applied to the [`Chip`](/api/chip/) element.
      */
