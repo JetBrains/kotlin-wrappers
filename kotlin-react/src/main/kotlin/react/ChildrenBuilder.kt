1478c6fe492644812908eb1c1d08cf1105fd3ec1
Victor Turansky <victor.turansky@gmail.com>
2021-12-07 21:52:34 +0300

    React. `ChildrenBuilder` (#1034)

---
+++ kotlin-react/src/main/kotlin/react/ChildrenBuilder.kt
@@ -0,0 +1,77 @@
+package react
+
+import kotlinext.js.Object
+import kotlinext.js.ReadonlyArray
+
+private val CHILDREN = js("(Symbol('@@children'))")
+
+internal inline var ChildrenBuilder.children: ReadonlyArray<ReactNode>?
+    get() = asDynamic()[CHILDREN]
+    private set(value) {
+        asDynamic()[CHILDREN] = value
+    }
+
+@ReactDsl
+sealed interface ChildrenBuilder {
+    operator fun Props?.unaryPlus() {
+        Object.assign(this@ChildrenBuilder, this)
+    }
+
+    fun child(
+        element: ReactNode,
+    ) {
+        if (children != null) {
+            children.asDynamic().push(element)
+        } else {
+            children = arrayOf(element)
+        }
+    }
+
+    operator fun ReactNode.unaryPlus() {
+        child(this)
+    }
+
+    operator fun String.unaryPlus() {
+        +ReactNode(this)
+    }
+
+    fun <P : Props> child(
+        type: ElementType<P>,
+        props: P,
+    ) {
+        +createElement(type, props)
+    }
+
+    operator fun <P : Props> ElementType<P>.invoke() {
+        +createElement(this)
+    }
+
+    operator fun <P> ElementType<P>.invoke(
+        block: @ReactDsl P.() -> Unit,
+    ) where P : Props,
+            P : ChildrenBuilder {
+        +create(block)
+    }
+
+    operator fun <T> Provider<T>.invoke(
+        value: T,
+        block: ChildrenBuilder.() -> Unit,
+    ) {
+        this {
+            this.value = value
+
+            block()
+        }
+    }
+
+    fun PropsWithChildren.children() {
+        Children.toArray(children)
+            .forEach(::child)
+    }
+}
+
+@JsName("createChildrenBuilder")
+internal fun ChildrenBuilder(): ChildrenBuilder =
+    ChildrenBuilderImpl()
+
+private class ChildrenBuilderImpl : ChildrenBuilder
