d11dc9cf1301e0fc09f0f05efa48164bab01cdf1
Leonid Khachaturov <leonidkhachaturov@gmail.com>
2021-12-08 08:05:18 +0100

    Set Node and Yarn versions explicitly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+
 plugins {
     kotlin("js") apply false
 }
@@ -5,3 +8,7 @@ plugins {
 tasks.wrapper {
     gradleVersion = "7.3.1"
 }
+
+rootProject.the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+
+YarnPlugin.apply(rootProject).version = "1.22.17"
