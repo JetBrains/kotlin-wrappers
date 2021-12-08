25acd588e6d0e3ce32d963ff7ff3c43e1ffd48fb
Victor Turansky <victor.turansky@gmail.com>
2021-12-08 14:17:26 +0300

    `noce-conventions` plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,8 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
-
 plugins {
     kotlin("js") apply false
+    `node-conventions`
 }
 
 tasks.wrapper {
     gradleVersion = "7.3.1"
 }
-
-YarnPlugin.apply(rootProject).version = "1.22.17"
-
-rootProject.the<NodeJsRootExtension>().nodeVersion = "16.13.1"
