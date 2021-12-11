33650b9d05ea6c11397917acb6f84d25907e1c29
Victor Turansky <victor.turansky@gmail.com>
2021-12-11 22:42:27 +0300

    React Router `6.1.1` [generated]

--- kotlin-react-router-dom/src/main/generated/react/router/dom/HistoryRouter.kt
+++ kotlin-react-router-dom/src/main/generated/react/router/dom/HistoryRouter.kt
@@ -5,4 +5,11 @@
 
 package react.router.dom
 
+/**
+ * A `<Router>` that accepts a pre-instantiated history object. It's important
+ * to note that using your own history object is highly discouraged and may add
+ * two versions of the history library to your bundles unless you use the same
+ * version of the history library that React Router uses internally.
+ */
+@JsName("unstable_HistoryRouter")
 external val HistoryRouter: react.FC<HistoryRouterProps>
