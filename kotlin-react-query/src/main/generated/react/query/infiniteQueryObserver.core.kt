f5e740963eb406db9b025cce424cd5fbbf3216c7
Victor Turansky <victor.turansky@gmail.com>
2021-12-12 10:17:52 +0300

    React Query `3.34.2` [generated] (#1046)

--- kotlin-react-query/src/main/generated/react/query/infiniteQueryObserver.core.kt
+++ kotlin-react-query/src/main/generated/react/query/infiniteQueryObserver.core.kt
@@ -20,7 +20,11 @@ open external class InfiniteQueryObserver<TQueryFnData, TError, TData, TQueryDat
     open var getCurrentResult: () -> InfiniteQueryObserverResult<TData, TError>
     protected open var fetch: (fetchOptions: ObserverFetchOptions?) -> kotlin.js.Promise<InfiniteQueryObserverResult<TData, TError>>
     override fun bindMethods()
-    open fun setOptions(options: InfiniteQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, *> = definedExternally)
+    open fun setOptions(
+        options: InfiniteQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, *> = definedExternally,
+        notifyOptions: NotifyOptions = definedExternally,
+    )
+
     open fun getOptimisticResult(options: InfiniteQueryObserverOptions<TQueryFnData, TError, TData, TQueryData, *>): InfiniteQueryObserverResult<TData, TError>
     open fun fetchNextPage(options: FetchNextPageOptions = definedExternally): kotlin.js.Promise<InfiniteQueryObserverResult<TData, TError>>
     open fun fetchPreviousPage(options: FetchPreviousPageOptions = definedExternally): kotlin.js.Promise<InfiniteQueryObserverResult<TData, TError>>
