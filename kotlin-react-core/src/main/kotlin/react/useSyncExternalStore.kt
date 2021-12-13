222229e6c6530473e7b4da65705df2e8461125bc
Victor Turansky <victor.turansky@gmail.com>
2021-12-13 12:16:57 +0300

    React. `useSyncExternalStore` (#1050)

---
+++ kotlin-react-core/src/main/kotlin/react/useSyncExternalStore.kt
@@ -0,0 +1,15 @@
+@file:JsModule("react")
+@file:JsNonModule
+
+package react
+
+/**
+ * Available from 18.0
+ *
+ * @see <a href="https://github.com/reactwg/react-18/discussions/86">Discussion</a>
+ */
+external fun <Snapshot> useSyncExternalStore(
+    subscribe: (onStoreChange: () -> Unit) -> Cleanup,
+    getSnapshot: () -> Snapshot,
+    getServerSnapshot: () -> Snapshot = definedExternally,
+): Snapshot
