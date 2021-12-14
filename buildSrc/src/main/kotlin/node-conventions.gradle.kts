915d8d6c3e17b2396c5b0a9a01a31b4281065121
Victor Turansky <victor.turansky@gmail.com>
2021-12-14 14:17:00 +0300

    Kotlin `1.6.10` (#1052)
    * Kotlin `1.6.10`
    
    * Allow scripts by default (for `puppeteer`)
    
    * Safe extension configuration

--- buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/node-conventions.gradle.kts
@@ -1,6 +1,13 @@
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
-YarnPlugin.apply(project).version = "1.22.17"
+plugins.withType<YarnPlugin> {
+    the<YarnRootExtension>().version = "1.22.17"
+    the<YarnRootExtension>().ignoreScripts = false
+}
 
-project.the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+plugins.withType<NodeJsRootPlugin> {
+    the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+}
