2b14ffc90f1b3aeb5697a42b13bace149615cabf
Victor Turansky <victor.turansky@gmail.com>
2021-12-14 13:32:43 +0300

    Allow scripts by default (for `puppeteer`)

--- buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/node-conventions.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
-import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
-YarnPlugin.apply(project).version = "1.22.17"
+the<YarnRootExtension>().version = "1.22.17"
+the<YarnRootExtension>().ignoreScripts = false
 
-project.the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+the<NodeJsRootExtension>().nodeVersion = "16.13.1"
