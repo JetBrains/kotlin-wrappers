a4cd04cf7aa124958a012bada54ea05d1556c7d2
Victor Turansky <victor.turansky@gmail.com>
2021-12-14 13:53:07 +0300

    Safe extension configuration

--- buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/node-conventions.gradle.kts
@@ -1,7 +1,13 @@
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 
-the<YarnRootExtension>().version = "1.22.17"
-the<YarnRootExtension>().ignoreScripts = false
+plugins.withType<YarnPlugin> {
+    the<YarnRootExtension>().version = "1.22.17"
+    the<YarnRootExtension>().ignoreScripts = false
+}
 
-the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+plugins.withType<NodeJsRootPlugin> {
+    the<NodeJsRootExtension>().nodeVersion = "16.13.1"
+}
