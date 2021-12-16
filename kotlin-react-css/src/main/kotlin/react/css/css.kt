edd91a5056daf727e0ee2d96fbf4d1ac1b8afc44
Victor Turansky <victor.turansky@gmail.com>
2021-12-16 03:09:01 +0300

    React CSS. Remove `RElementBuilder` adapters

--- kotlin-react-css/src/main/kotlin/react/css/css.kt
+++ kotlin-react-css/src/main/kotlin/react/css/css.kt
@@ -3,7 +3,6 @@ package react.css
 import emotion.css.cx
 import kotlinext.js.jso
 import react.PropsWithClassName
-import react.RElementBuilder
 
 @PublishedApi
 internal inline fun className(
@@ -25,20 +24,3 @@ inline fun PropsWithClassName.css(
 ) {
     className = cx(classNames = classNames + className(block))
 }
-
-// Builder
-
-inline fun RElementBuilder<PropsWithClassName>.css(
-    crossinline block: PropertiesBuilder.() -> Unit,
-) {
-    attrs.css(block)
-}
-
-inline fun RElementBuilder<PropsWithClassName>.css(
-    vararg classNames: String?,
-    crossinline block: PropertiesBuilder.() -> Unit,
-) {
-    attrs.css(classNames = classNames, block = block)
-}
-
-
