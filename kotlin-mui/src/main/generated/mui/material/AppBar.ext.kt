f784edd3e7061455ece43dc7e91a9b8a416f5670
Victor Turansky <victor.turansky@gmail.com>
2022-01-02 03:10:32 +0300

    MUI. Strict color types [generated] (#1088)

--- kotlin-mui/src/main/generated/mui/material/AppBar.ext.kt
+++ kotlin-mui/src/main/generated/mui/material/AppBar.ext.kt
@@ -4,6 +4,19 @@ package mui.material
 
 @Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
+@JsName("""(/*union*/{inherit: 'inherit', primary: 'primary', secondary: 'secondary', default: 'default', transparent: 'transparent'}/*union*/)""")
+external enum class AppBarColor {
+    inherit,
+    primary,
+    secondary,
+    default,
+    transparent,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
 @JsName("""(/*union*/{fixed: 'fixed', absolute: 'absolute', sticky: 'sticky', static: 'static', relative: 'relative'}/*union*/)""")
 external enum class AppBarPosition {
     fixed,
