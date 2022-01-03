469105154efd6e4e7c8bf23d3ee5f8e8a1f6ed1c
Victor Turansky <victor.turansky@gmail.com>
2022-01-03 05:35:34 +0300

    MUI. `useTheme` hook [generated]

---
+++ kotlin-mui/src/main/generated/mui/system/useTheme.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/system/useTheme")
+@file:JsNonModule
+
+package mui.system
+
+@JsName("default")
+external fun <T : Theme> useTheme(
+    defaultTheme: T? = definedExternally,
+): T
