3a3d66c01456b174e116193f9da2960558a2efd3
Victor Turansky <victor.turansky@gmail.com>
2022-01-03 10:52:30 +0300

    MUI. `usePagination` hook [generated] (#1093)

---
+++ kotlin-mui/src/main/generated/mui/material/usePagination.ext.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package mui.material
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{page: 'page', first: 'first', last: 'last', next: 'next', previous: 'previous', startEllipsis: 'start-ellipsis', endEllipsis: 'end-ellipsis'}/*union*/)""")
+external enum class UsePaginationItemType {
+    page,
+    first,
+    last,
+    next,
+    previous,
+    startEllipsis,
+    endEllipsis,
+
+    ;
+}
---
+++ kotlin-mui/src/main/generated/mui/material/usePagination.kt
@@ -0,0 +1,103 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/material/usePagination")
+@file:JsNonModule
+
+package mui.material
+
+import kotlinext.js.ReadonlyArray
+
+external interface UsePaginationProps : react.Props {
+    /**
+     * Number of always visible pages at the beginning and end.
+     * @default 1
+     */
+    var boundaryCount: Number?
+
+    /**
+     * The name of the component where this hook is used.
+     */
+    var componentName: String?
+
+    /**
+     * The total number of pages.
+     * @default 1
+     */
+    var count: Number?
+
+    /**
+     * The page selected by default when the component is uncontrolled.
+     * @default 1
+     */
+    var defaultPage: Number?
+
+    /**
+     * If `true`, the component is disabled.
+     * @default false
+     */
+    var disabled: Boolean?
+
+    /**
+     * If `true`, hide the next-page button.
+     * @default false
+     */
+    var hideNextButton: Boolean?
+
+    /**
+     * If `true`, hide the previous-page button.
+     * @default false
+     */
+    var hidePrevButton: Boolean?
+
+    /**
+     * Callback fired when the page is changed.
+     *
+     * @param {React.ChangeEvent<unknown>} event The event source of the callback.
+     * @param {number} page The page selected.
+     */
+    var onChange: ((event: react.dom.events.ChangeEvent<*>, page: Number) -> Unit)?
+
+    /**
+     * The current page.
+     */
+    var page: Number?
+
+    /**
+     * If `true`, show the first-page button.
+     * @default false
+     */
+    var showFirstButton: Boolean?
+
+    /**
+     * If `true`, show the last-page button.
+     * @default false
+     */
+    var showLastButton: Boolean?
+
+    /**
+     * Number of always visible pages before and after the current page.
+     * @default 1
+     */
+    var siblingCount: Number?
+}
+
+external interface UsePaginationItem {
+    var onClick: react.dom.events.ReactEventHandler<*>
+
+    var type: UsePaginationItemType
+
+    var page: Number
+
+    var selected: Boolean
+
+    var disabled: Boolean
+}
+
+external interface UsePaginationResult {
+    var items: ReadonlyArray<UsePaginationItem>
+}
+
+@JsName("default")
+external fun usePagination(
+    props: UsePaginationProps,
+): UsePaginationResult
