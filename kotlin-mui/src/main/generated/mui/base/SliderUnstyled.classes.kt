8a08afd006f4a2dd3f27ba813842e92a140f80e5
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 10:38:21 +0300

    MUI. `SliderUnstyled` [generated] (#1110)

---
+++ kotlin-mui/src/main/generated/mui/base/SliderUnstyled.classes.kt
@@ -0,0 +1,65 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+external interface SliderUnstyledClasses {
+    /** Class name applied to the root element. */
+    var root: String
+
+    /** Class name applied to the root element if `marks` is provided with at least one label. */
+    var marked: String
+
+    /** Class name applied to the root element if `orientation="vertical"`. */
+    var vertical: String
+
+    /** State class applied to the root and thumb element if `disabled={true}`. */
+    var disabled: String
+
+    /** State class applied to the root if a thumb is being dragged. */
+    var dragging: String
+
+    /** Class name applied to the rail element. */
+    var rail: String
+
+    /** Class name applied to the track element. */
+    var track: String
+
+    /** Class name applied to the root element if `track={false}`. */
+    var trackFalse: String
+
+    /** Class name applied to the root element if `track="inverted"`. */
+    var trackInverted: String
+
+    /** Class name applied to the thumb element. */
+    var thumb: String
+
+    /** State class applied to the thumb element if it's active. */
+    var active: String
+
+    /** State class applied to the thumb element if keyboard focused. */
+    var focusVisible: String
+
+    /** Class name applied to the thumb label element. */
+    var valueLabel: String
+
+    /** Class name applied to the thumb label element if it's open. */
+    var valueLabelOpen: String
+
+    /** Class name applied to the thumb label's circle element. */
+    var valueLabelCircle: String
+
+    /** Class name applied to the thumb label's label element. */
+    var valueLabelLabel: String
+
+    /** Class name applied to the mark element. */
+    var mark: String
+
+    /** Class name applied to the mark element if active (depending on the value). */
+    var markActive: String
+
+    /** Class name applied to the mark label element. */
+    var markLabel: String
+
+    /** Class name applied to the mark label element if active (depending on the value). */
+    var markLabelActive: String
+}
