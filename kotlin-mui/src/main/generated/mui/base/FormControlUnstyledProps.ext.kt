021de819b8ebdb7abcc0a30ab9cdedf290b14884
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 19:05:32 +0300

    MUI. `FormControlUnstyled` [generated] (#1115)

---
+++ kotlin-mui/src/main/generated/mui/base/FormControlUnstyledProps.ext.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+typealias NativeFormControlElement = org.w3c.dom.HTMLElement /* HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement */
