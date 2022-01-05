7c47b83ac37cb63c9ae1e24fe3c2e1c9be106d10
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 18:03:59 +0300

    MUI. Factory method for `ResponsiveStyleValue` [generated] (#1114)

---
+++ kotlin-mui/src/main/generated/mui/system/ResponsiveStyleValue.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+package mui.system
+
+external interface ResponsiveStyleValue<T : Any>
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <T : Any> ResponsiveStyleValue(
+    value: T,
+): ResponsiveStyleValue<T> =
+    value.unsafeCast<ResponsiveStyleValue<T>>()
---
+++ kotlin-mui/src/main/generated/mui/system/SxProps.kt
@@ -0,0 +1,6 @@
+// Automatically generated - do not modify!
+
+package mui.system
+
+@Suppress("UNUSED_TYPEALIAS_PARAMETER")
+typealias SxProps<T> = react.CSSProperties
