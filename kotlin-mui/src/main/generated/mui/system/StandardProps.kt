d4ca6f85043d423f2bff37930e4fe00016004f3f
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 17:43:42 +0300

    MUI. Factory method for `ResponsiveStyleValue` [generated]

--- kotlin-mui/src/main/generated/mui/system/StandardProps.kt
+++ kotlin-mui/src/main/generated/mui/system/StandardProps.kt
@@ -2,11 +2,6 @@
 
 package mui.system
 
-@Suppress("UNUSED_TYPEALIAS_PARAMETER")
-typealias SxProps<T> = react.CSSProperties
-
-external interface ResponsiveStyleValue<T : Any>
-
 external interface StandardProps :
     react.PropsWithStyle,
     react.PropsWithClassName
