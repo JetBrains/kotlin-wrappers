903a58b3711f23bbda9d547153e7bd4b35186db3
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 10:38:47 +0300

    MUI. Fix `TableCellBaseProps` alias [generated] (#1111)

--- kotlin-mui/src/main/generated/mui/material/Stubs.kt
+++
@@ -1,8 +0,0 @@
-// Automatically generated - do not modify!
-
-package mui.material
-
-import org.w3c.dom.HTMLTableCellElement
-import react.dom.html.HTMLAttributes
-
-typealias TableCellBaseProps = HTMLAttributes<HTMLTableCellElement>
