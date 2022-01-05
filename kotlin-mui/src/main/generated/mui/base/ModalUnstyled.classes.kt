0d23b274f5cf9ba2de3fd7f21464238edc84cdff
Victor Turansky <victor.turansky@gmail.com>
2022-01-05 21:45:51 +0300

    MUI. `ModalUnstyled` [generated] (#1117)

---
+++ kotlin-mui/src/main/generated/mui/base/ModalUnstyled.classes.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+external interface ModalUnstyledClasses {
+    /** Styles applied to the root element. */
+    var root: String
+
+    /** Styles applied to the root element if the `Modal` has exited. */
+    var hidden: String
+}
