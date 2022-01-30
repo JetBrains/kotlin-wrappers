87d697a960cf5b05d9779908d4d5de4cd0e89aa3
Victor Turansky <victor.turansky@gmail.com>
2022-01-31 01:39:55 +0300

    TypeScript. `Array` children [generated] (#1160)

--- kotlin-typescript/src/main/generated/typescript/NodeArray.kt
+++ kotlin-typescript/src/main/generated/typescript/NodeArray.kt
@@ -1,13 +1,7 @@
 // Automatically generated - do not modify!
 
-@file:Suppress(
-    "EXTERNAL_TYPE_EXTENDS_NON_EXTERNAL_TYPE",
-    "INTERFACE_WITH_SUPERCLASS",
-    "FINAL_SUPERTYPE",
-)
-
 package typescript
 
-sealed external interface NodeArray<out T : Node> : ReadonlyArray<T>, ReadonlyTextRange {
+sealed external interface NodeArray<out T : Node> : ReadonlyArrayAdapter<T>, ReadonlyTextRange {
     val hasTrailingComma: Boolean
 }
--- kotlin-typescript/src/main/generated/typescript/SortedArray.kt
+++ kotlin-typescript/src/main/generated/typescript/SortedArray.kt
@@ -1,13 +1,7 @@
 // Automatically generated - do not modify!
 
-@file:Suppress(
-    "EXTERNAL_TYPE_EXTENDS_NON_EXTERNAL_TYPE",
-    "INTERFACE_WITH_SUPERCLASS",
-    "FINAL_SUPERTYPE",
-)
-
 package typescript
 
-sealed external interface SortedArray<T> : Array<T> {
+sealed external interface SortedArray<T> : ReadonlyArrayAdapter<T> {
     // " __sortedArrayBrand": any
 }
