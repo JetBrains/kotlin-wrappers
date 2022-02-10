6136dde97abf95a9893eade2e8e3bdb96ecdf915
Victor Turansky <victor.turansky@gmail.com>
2022-02-11 01:51:26 +0300

    CSSType. `FilterFunction` [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/FilterFunction.kt
@@ -0,0 +1,112 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+sealed external interface FilterFunction :
+    Filter,
+    BackdropFilter
+
+inline fun blur(
+    value: Number,
+): FilterFunction =
+    "blur($value)".unsafeCast<FilterFunction>()
+
+inline fun blur(
+    value: Length,
+): FilterFunction =
+    "blur($value)".unsafeCast<FilterFunction>()
+
+inline fun brightness(
+    value: Number,
+): FilterFunction =
+    "brightness($value)".unsafeCast<FilterFunction>()
+
+inline fun brightness(
+    value: Percent,
+): FilterFunction =
+    "brightness($value)".unsafeCast<FilterFunction>()
+
+inline fun contrast(
+    value: Number,
+): FilterFunction =
+    "contrast($value)".unsafeCast<FilterFunction>()
+
+inline fun contrast(
+    value: Percent,
+): FilterFunction =
+    "contrast($value)".unsafeCast<FilterFunction>()
+
+inline fun dropShadow(
+    value: String,
+): FilterFunction =
+    "drop-shadow($value)".unsafeCast<FilterFunction>()
+
+inline fun grayscale(
+    value: Number,
+): FilterFunction =
+    "grayscale($value)".unsafeCast<FilterFunction>()
+
+inline fun grayscale(
+    value: Percent,
+): FilterFunction =
+    "grayscale($value)".unsafeCast<FilterFunction>()
+
+inline fun hueRotate(
+    value: Number,
+): FilterFunction =
+    "hue-rotate($value)".unsafeCast<FilterFunction>()
+
+inline fun hueRotate(
+    value: Angle,
+): FilterFunction =
+    "hue-rotate($value)".unsafeCast<FilterFunction>()
+
+inline fun invert(
+    value: Number,
+): FilterFunction =
+    "invert($value)".unsafeCast<FilterFunction>()
+
+inline fun invert(
+    value: Percent,
+): FilterFunction =
+    "invert($value)".unsafeCast<FilterFunction>()
+
+inline fun opacity(
+    value: Number,
+): FilterFunction =
+    "opacity($value)".unsafeCast<FilterFunction>()
+
+inline fun opacity(
+    value: Percent,
+): FilterFunction =
+    "opacity($value)".unsafeCast<FilterFunction>()
+
+inline fun saturate(
+    value: Number,
+): FilterFunction =
+    "saturate($value)".unsafeCast<FilterFunction>()
+
+inline fun saturate(
+    value: Percent,
+): FilterFunction =
+    "saturate($value)".unsafeCast<FilterFunction>()
+
+inline fun sepia(
+    value: Number,
+): FilterFunction =
+    "sepia($value)".unsafeCast<FilterFunction>()
+
+inline fun sepia(
+    value: Percent,
+): FilterFunction =
+    "sepia($value)".unsafeCast<FilterFunction>()
+
+fun FilterFunction(
+    vararg values: FilterFunction,
+): FilterFunction =
+    values.joinToString(" ")
+        .unsafeCast<FilterFunction>()
