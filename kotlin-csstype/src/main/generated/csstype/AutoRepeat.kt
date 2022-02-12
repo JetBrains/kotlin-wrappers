51817d27b7da6085e2ef3e87cae27e672a899ac7
Victor Turansky <victor.turansky@gmail.com>
2022-02-12 21:33:36 +0300

    CSSType. `AutoRepeat` [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/AutoRepeat.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package csstype
+
+// language=JavaScript
+@JsName("""(/*union*/{autoFill: 'auto-fill', autoFit: 'auto-fit'}/*union*/)""")
+external enum class AutoRepeat {
+    autoFill,
+    autoFit,
+
+    ;
+}
