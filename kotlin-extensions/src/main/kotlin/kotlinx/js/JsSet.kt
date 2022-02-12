15ddca992a22f8e480ff47c802365c1c03fd0d58
Pavel Matusevich <neitex@protonmail.com>
2022-02-13 02:43:06 +0300

    Applied suggested changes (-_-;)

--- kotlin-extensions/src/main/kotlin/kotlinx/js/JsSet.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/JsSet.kt
@@ -3,12 +3,11 @@ package kotlinx.js
 @JsName("Set")
 external class JsSet<T> {
     val size: Int
-        get() = definedExternally
     fun add(value: T): JsSet<T>
     fun clear()
     fun delete(value: T)
-    fun entries(): JsIterator<T>
+    fun entries(): JsIterator<JsPair<T, T>>
     fun has(value: T): Boolean
-    fun forEach(callback: (value: T, key: T, set: JsSet<T>) -> Unit)
+    fun forEach(callback: (value: T, key: T) -> Unit)
     fun values(): JsIterator<T>
 }
