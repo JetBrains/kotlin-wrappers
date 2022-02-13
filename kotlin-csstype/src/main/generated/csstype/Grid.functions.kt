a60539aa92e567d1187687900ba80ec8a9c25480
Victor Turansky <victor.turansky@gmail.com>
2022-02-13 10:19:34 +0300

    CSSType. `repeat` [generated] (#1199)

---
+++ kotlin-csstype/src/main/generated/csstype/Grid.functions.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+inline fun repeat(
+    times: Int,
+    value: GridTemplateStripes,
+): GridTemplateStripes =
+    "repeat($times, $value)".unsafeCast<GridTemplateStripes>()
+
+inline fun repeat(
+    repeat: AutoRepeat,
+    value: GridTemplateStripes,
+): GridTemplateStripes =
+    "repeat($repeat, $value)".unsafeCast<GridTemplateStripes>()
