e5e67ecc298bad7381df56b9a40df6651b6babc9
Pavel Matusevich <neitex@protonmail.com>
2022-02-13 03:26:58 +0300

    Added optional `iterable` constructor, renamed `callback` to `action`

--- kotlin-extensions/src/main/kotlin/kotlinx/js/JsSet.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/JsSet.kt
@@ -2,12 +2,15 @@ package kotlinx.js
 
 @JsName("Set")
 external class JsSet<T> {
+    constructor()
+    constructor(iterable: JsIterator<T>)
+
     val size: Int
     fun add(value: T): JsSet<T>
     fun clear()
     fun delete(value: T)
     fun entries(): JsIterator<JsPair<T, T>>
     fun has(value: T): Boolean
-    fun forEach(callback: (value: T, key: T) -> Unit)
+    fun forEach(action: (value: T, key: T) -> Unit)
     fun values(): JsIterator<T>
 }
