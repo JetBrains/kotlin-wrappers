50da800318004dbdda789e8d3c8570f2b53850f9
Victor Turansky <victor.turansky@gmail.com>
2022-02-16 00:48:59 +0300

    CSSType. `attr` and `url` [generated] (#1210)

---
+++ kotlin-csstype/src/main/generated/csstype/Reference.functions.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+inline fun attr(
+    name: String,
+): Content =
+    "attr($name)".unsafeCast<Content>()
+
+inline fun url(
+    value: String,
+): Url =
+    "url($value)".unsafeCast<Url>()
