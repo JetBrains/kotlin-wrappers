6509e2f8d1a2ece3e5aeaa6e7e34744633f85c55
Victor Turansky <victor.turansky@gmail.com>
2022-02-15 23:48:45 +0300

    CSSType. `Url` [generated] (#1209)

---
+++ kotlin-csstype/src/main/generated/csstype/Url.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package csstype
+
+sealed external interface Url :
+    Background,
+    BackgroundImage,
+    Border,
+    BorderImage,
+    BorderImageSource,
+    Content,
+    Cursor,
+    Filter,
+    ListStyle,
+    ListStyleImage,
+    Mask,
+    MaskImage,
+    OffsetPath
