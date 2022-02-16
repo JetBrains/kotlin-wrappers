337e6be18c40978ea39a1e09b6dc6a24593dde67
Victor Turansky <victor.turansky@gmail.com>
2022-02-16 08:27:40 +0300

    CSSType. `BoxShadow` factories [generated] (#1213)

--- kotlin-csstype/src/main/generated/csstype/BoxShadow.kt
+++ kotlin-csstype/src/main/generated/csstype/BoxShadow.kt
@@ -3,6 +3,7 @@
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
 )
 
 package csstype
@@ -14,3 +15,62 @@ sealed external interface BoxShadow {
         val none: BoxShadow
     }
 }
+
+// language=JavaScript
+@JsName("""(/*union*/{inset: 'inset'}/*union*/)""")
+external enum class BoxShadowInset {
+    inset,
+
+    ;
+}
+
+inline fun BoxShadow(
+    offsetX: Length,
+    offsetY: Length,
+    color: Color,
+): BoxShadow =
+    "$offsetX $offsetY $color".unsafeCast<BoxShadow>()
+
+inline fun BoxShadow(
+    offsetX: Length,
+    offsetY: Length,
+    blurRadius: Length,
+    color: Color,
+): BoxShadow =
+    "$offsetX $offsetY $blurRadius $color".unsafeCast<BoxShadow>()
+
+inline fun BoxShadow(
+    offsetX: Length,
+    offsetY: Length,
+    blurRadius: Length,
+    spreadRadius: Length,
+    color: Color,
+): BoxShadow =
+    "$offsetX $offsetY $blurRadius $spreadRadius $color".unsafeCast<BoxShadow>()
+
+inline fun BoxShadow(
+    inset: BoxShadowInset,
+    offsetX: Length,
+    offsetY: Length,
+    color: Color,
+): BoxShadow =
+    "$inset $offsetX $offsetY $color".unsafeCast<BoxShadow>()
+
+inline fun BoxShadow(
+    inset: BoxShadowInset,
+    offsetX: Length,
+    offsetY: Length,
+    blurRadius: Length,
+    color: Color,
+): BoxShadow =
+    "$inset $offsetX $offsetY $blurRadius $color".unsafeCast<BoxShadow>()
+
+inline fun BoxShadow(
+    inset: BoxShadowInset,
+    offsetX: Length,
+    offsetY: Length,
+    blurRadius: Length,
+    spreadRadius: Length,
+    color: Color,
+): BoxShadow =
+    "$inset $offsetX $offsetY $blurRadius $spreadRadius $color".unsafeCast<BoxShadow>()
