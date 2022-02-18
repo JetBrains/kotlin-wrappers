42131d65aa769019a9115935d4dc9cdebbd47f12
Victor Turansky <victor.turansky@gmail.com>
2022-02-18 04:18:42 +0300

    CSSType. `Gradient` and `Image` [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/Gradient.kt
@@ -0,0 +1,6 @@
+// Automatically generated - do not modify!
+
+package csstype
+
+sealed external interface Gradient :
+    Image
---
+++ kotlin-csstype/src/main/generated/csstype/Image.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+package csstype
+
+sealed external interface Image :
+    Background,
+    BackgroundImage,
+    Border,
+    BorderImage,
+    BorderImageSource,
+    Content,
+    ListStyle,
+    ListStyleImage,
+    Mask,
+    MaskImage
