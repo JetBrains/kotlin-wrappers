418421c4cc20f0b22096592ad6ebbd22eac58e1a
Victor Turansky <victor.turansky@gmail.com>
2022-02-19 09:11:29 +0300

    CSSType. Layout factories [generated] (#1219)

--- kotlin-csstype/src/main/generated/csstype/Margin.kt
+++ kotlin-csstype/src/main/generated/csstype/Margin.kt
@@ -1,5 +1,30 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package csstype
 
 typealias Margin = AutoLengthProperty
+
+inline fun Margin(
+    vertical: AutoLengthProperty,
+    horizontal: AutoLengthProperty,
+): Margin =
+    "$vertical $horizontal".unsafeCast<Margin>()
+
+inline fun Margin(
+    top: AutoLengthProperty,
+    horizontal: AutoLengthProperty,
+    bottom: AutoLengthProperty,
+): Margin =
+    "$top $horizontal $bottom".unsafeCast<Margin>()
+
+inline fun Margin(
+    top: AutoLengthProperty,
+    right: AutoLengthProperty,
+    bottom: AutoLengthProperty,
+    left: AutoLengthProperty,
+): Margin =
+    "$top $right $bottom $left".unsafeCast<Margin>()
