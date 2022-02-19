dc46dba0aea43125636ebf8e60ecfdfde451f6e0
Victor Turansky <victor.turansky@gmail.com>
2022-02-19 03:04:41 +0300

    CSSType. Layout factories [generated]

--- kotlin-csstype/src/main/generated/csstype/Inset.kt
+++ kotlin-csstype/src/main/generated/csstype/Inset.kt
@@ -1,5 +1,30 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package csstype
 
 typealias Inset = AutoLengthProperty
+
+inline fun Inset(
+    vertical: AutoLengthProperty,
+    horizontal: AutoLengthProperty,
+): Inset =
+    "$vertical $horizontal".unsafeCast<Inset>()
+
+inline fun Inset(
+    top: AutoLengthProperty,
+    horizontal: AutoLengthProperty,
+    bottom: AutoLengthProperty,
+): Inset =
+    "$top $horizontal $bottom".unsafeCast<Inset>()
+
+inline fun Inset(
+    top: AutoLengthProperty,
+    right: AutoLengthProperty,
+    bottom: AutoLengthProperty,
+    left: AutoLengthProperty,
+): Inset =
+    "$top $right $bottom $left".unsafeCast<Inset>()
