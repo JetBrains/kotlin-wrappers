7ef1028ba3e0982dc93edcdfa6ee1edb334ddf35
Victor Turansky <victor.turansky@gmail.com>
2022-02-23 15:25:42 +0300

    `Object.entries` and `Object.values` (#1252)

--- kotlin-extensions/src/main/kotlin/kotlinx/js/Object.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/Object.kt
@@ -12,6 +12,8 @@ external class Object internal constructor() {
         fun <T : Any> create(o: T?, properties: dynamic = definedExternally): T
         fun <T : Any> defineProperties(o: T, properties: dynamic): T
         fun <T : Any, P> defineProperty(o: T, p: String, attributes: PropertyDescriptor<P>): T
+        fun <T : Any> entries(o: Record<String, T>): ReadonlyArray<JsPair<String, T>>
+        fun entries(o: Any): ReadonlyArray<JsPair<String, Any?>>
         fun <R, T : R> freeze(o: T): R
         fun <T> getOwnPropertyDescriptor(o: Any, p: String): PropertyDescriptor<T>
         fun getOwnPropertyNames(o: Any): Array<String>
@@ -22,5 +24,7 @@ external class Object internal constructor() {
         fun keys(o: Any): Array<String>
         fun <T> preventExtensions(o: T): T
         fun <T> seal(o: T): T
+        fun <T : Any> values(o: Record<*, T>): ReadonlyArray<T>
+        fun values(o: Any): ReadonlyArray<Any?>
     }
 }
