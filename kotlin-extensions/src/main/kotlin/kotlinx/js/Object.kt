d7fe88badb1467e0119096383b4716c5374b602b
Victor Turansky <victor.turansky@gmail.com>
2022-02-23 09:42:08 +0300

    `Object` - sort methods (#1247)

--- kotlin-extensions/src/main/kotlin/kotlinx/js/Object.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/Object.kt
@@ -1,26 +1,26 @@
 package kotlinx.js
 
 external class Object internal constructor() {
-    fun toLocaleString(): String
-    fun valueOf(): dynamic
     fun hasOwnProperty(v: String): Boolean
     fun isPrototypeOf(v: Any): Boolean
     fun propertyIsEnumerable(v: String): Boolean
+    fun toLocaleString(): String
+    fun valueOf(): dynamic
 
     companion object {
-        fun <P : Any, T : P> getPrototypeOf(o: T): P
+        fun <T : Any, R : T> assign(dest: R, vararg src: T?): R
+        fun <T : Any> create(o: T?, properties: dynamic = definedExternally): T
+        fun <T : Any> defineProperties(o: T, properties: dynamic): T
+        fun <T : Any, P> defineProperty(o: T, p: String, attributes: PropertyDescriptor<P>): T
+        fun <R, T : R> freeze(o: T): R
         fun <T> getOwnPropertyDescriptor(o: Any, p: String): PropertyDescriptor<T>
         fun getOwnPropertyNames(o: Any): Array<String>
-        fun <T : Any> create(o: T?, properties: dynamic = definedExternally): T
-        fun <T : Any, P> defineProperty(o: T, p: String, attributes: PropertyDescriptor<P>): T
-        fun <T : Any> defineProperties(o: T, properties: dynamic): T
-        fun <T> seal(o: T): T
-        fun <R, T : R> freeze(o: T): R
-        fun <T> preventExtensions(o: T): T
-        fun isSealed(o: Any): Boolean
-        fun isFrozen(o: Any): Boolean
+        fun <P : Any, T : P> getPrototypeOf(o: T): P
         fun isExtensible(o: Any): Boolean
+        fun isFrozen(o: Any): Boolean
+        fun isSealed(o: Any): Boolean
         fun keys(o: Any): Array<String>
-        fun <T : Any, R : T> assign(dest: R, vararg src: T?): R
+        fun <T> preventExtensions(o: T): T
+        fun <T> seal(o: T): T
     }
 }
