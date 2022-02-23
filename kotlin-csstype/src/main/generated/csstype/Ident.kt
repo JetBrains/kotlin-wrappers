151d793d490c04aa8feef235bd2e1b9ed12eba61
Victor Turansky <victor.turansky@gmail.com>
2022-02-23 09:41:51 +0300

    CSSType. `Ident` [generated] (#1246)

---
+++ kotlin-csstype/src/main/generated/csstype/Ident.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+sealed external interface Ident :
+    IdentType
+
+sealed external interface IdentType :
+    AnimationName,
+    GridArea
+
+inline fun ident(
+    value: String,
+): Ident =
+    value.unsafeCast<Ident>()
