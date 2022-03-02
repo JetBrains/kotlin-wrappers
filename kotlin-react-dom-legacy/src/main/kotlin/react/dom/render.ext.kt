6a77ce3dfbf2c5a4708531c1138d2f550a4f9991
Victor Turansky <victor.turansky@gmail.com>
2022-03-03 00:33:35 +0300

    React `18.0.0-rc.1` support

--- kotlin-react-dom-legacy/src/main/kotlin/react/dom/render.ext.kt
+++ kotlin-react-dom-legacy/src/main/kotlin/react/dom/render.ext.kt
@@ -4,7 +4,6 @@ import org.w3c.dom.Element
 import react.Props
 import react.Render
 import react.createElement
-import react.dom.client.createRoot
 
 fun render(
     container: Element,
@@ -16,17 +15,4 @@ fun render(
         container = container,
         callback = callback,
     )
-}
-
-// 18.0+
-// TODO rename to render() after React 18 is released
-fun renderIntoRoot(
-    container: Element,
-    block: Render,
-) {
-    val children = createElement<Props>(block)
-        ?: return
-
-    createRoot(container)
-        .render(children)
 }
