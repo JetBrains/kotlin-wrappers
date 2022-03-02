51f22c5257740d7962583020a97260a144f5c196
Victor Turansky <victor.turansky@gmail.com>
2022-03-03 00:33:48 +0300

    Extensions. Update `delete` package (#1283)

---
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/delete.kt
@@ -0,0 +1,8 @@
+package kotlinx.js
+
+/**
+ * [MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete)
+ */
+external fun delete(
+    expression: dynamic,
+): Boolean
