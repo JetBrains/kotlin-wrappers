6674bea5d55ae2fcc1c5a0b3d0d0c1f7e441522f
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2022-03-06 23:11:10 +0300

    CacheProvider is introduced (#1297)

---
+++ kotlin-emotion/src/main/kotlin/emotion/react/CacheProvider.kt
@@ -0,0 +1,9 @@
+@file:JsModule("@emotion/react")
+@file:JsNonModule
+
+package emotion.react
+
+import emotion.cache.EmotionCache
+import react.Provider
+
+external val CacheProvider: Provider<EmotionCache>
