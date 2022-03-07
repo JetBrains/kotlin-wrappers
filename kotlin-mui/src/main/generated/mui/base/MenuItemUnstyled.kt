504452d859b25a14b9fea25fd419fea0e0d99149
Victor Turansky <victor.turansky@gmail.com>
2022-03-07 19:37:03 +0300

    MUI `5.5.0` [generated]

---
+++ kotlin-mui/src/main/generated/mui/base/MenuItemUnstyled.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/base/MenuItemUnstyled")
+@file:JsNonModule
+
+package mui.base
+
+/**
+ *
+ * Demos:
+ *
+ * - [Menus](https://mui.com/components/menus/)
+ *
+ * API:
+ *
+ * - [MenuItemUnstyled API](https://mui.com/api/menu-item-unstyled/)
+ */
+@JsName("default")
+external val MenuItemUnstyled: react.FC<MenuItemUnstyledProps>
---
+++ kotlin-mui/src/main/generated/mui/base/MenuUnstyled.types.kt
@@ -0,0 +1,50 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+external interface MenuUnstyledProps :
+    react.PropsWithChildren,
+    react.PropsWithClassName {
+    /**
+     * A ref with imperative actions.
+     * It allows to select the first or last menu item.
+     */
+    var actions: react.Ref<MenuUnstyledActions>?
+
+    /**
+     * An HTML element, [virtualElement](https://popper.js.org/docs/v2/virtual-elements/),
+     * or a function that returns either.
+     * It's used to set the position of the popper.
+     */
+    var anchorEl: dynamic /* PopperUnstyledProps['anchorEl'] */
+
+    override var children: react.ReactNode?
+
+    override var className: String?
+
+    var component: react.ElementType<*>?
+
+    var components: dynamic
+
+    var componentsProps: dynamic
+
+    /**
+     * Triggered when focus leaves the menu and the menu should close.
+     */
+    var onClose: (() -> Unit)?
+
+    /**
+     * Controls whether the menu is displayed.
+     * @default false
+     */
+    var open: Boolean?
+}
+
+external interface MenuUnstyledActions {
+    fun highlightFirstItem()
+    fun highlightLastItem()
+}
+
+external interface MenuUnstyledOwnerState {
+    var open: Boolean
+}
