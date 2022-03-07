8fefaaa86e795df2a8eb785a78161a79ddf6e453
Victor Turansky <victor.turansky@gmail.com>
2022-03-07 19:51:36 +0300

    MUI. `sx` extension [generated] (#1303)
    * MUI. `sx` extension [generated]
    
    * MUI `5.5.0` [generated]
    
    * Revert "MUI `5.5.0` [generated]"
    
    This reverts commit f7c70f90de6a246b3a2e888e423bf2bcd01006d6.

---
+++ kotlin-mui/src/main/generated/mui/system/sx.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+package mui.system
+
+import csstype.PropertiesBuilder
+import kotlinx.js.jso
+
+inline fun PropsWithSx.sx(
+    crossinline block: PropertiesBuilder.() -> Unit,
+) {
+    sx = jso(block)
+}
