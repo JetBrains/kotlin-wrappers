e4632753ef473026353c6cb45c49686056c1c13c
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2022-03-07 18:47:47 +0300

    Emotion. Keyframes helper is introduced (#1299)
    * Keyframes helper is introduced
    
    * Formatting is fixed
    
    * Keyframes type alias is introduced, from/to keywords are added to DSL
    
    * External keyframes function is made public
    
    * Keyframes params are renamed

---
+++ kotlin-emotion/src/main/kotlin/emotion/css/keyframes.ext.kt
@@ -0,0 +1,37 @@
+@file:Suppress(
+    "DECLARATION_CANT_BE_INLINED",
+)
+
+package emotion.css
+
+import csstype.*
+import kotlinx.js.Record
+import kotlinx.js.jso
+
+typealias Keyframes = Record<Percentage, Properties>
+
+@CssDsl
+interface KeyframesBuilder {
+    inline operator fun Percentage.invoke(
+        block: Properties.() -> Unit
+    ) {
+        unsafeCast<Keyframes>()[this] = jso(block)
+    }
+
+    inline fun from(
+        block: Properties.() -> Unit
+    ) {
+        0.pct(block)
+    }
+
+    inline fun to(
+        block: Properties.() -> Unit
+    ) {
+        100.pct(block)
+    }
+}
+
+inline fun keyframes(
+    block: KeyframesBuilder.() -> Unit,
+): AnimationName =
+    keyframes(jso(block).unsafeCast<Keyframes>())
---
+++ kotlin-emotion/src/main/kotlin/emotion/css/keyframes.kt
@@ -0,0 +1,10 @@
+@file:JsModule("@emotion/css")
+@file:JsNonModule
+
+package emotion.css
+
+import csstype.AnimationName
+
+external fun keyframes(
+    keyframes: Keyframes,
+): AnimationName
