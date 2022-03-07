42ef8a3dda882407635be4aa39a8e140e8108af9
Victor Turansky <victor.turansky@gmail.com>
2022-03-07 19:35:58 +0300

    Revert "MUI `5.5.0` [generated]"
    This reverts commit f7c70f90de6a246b3a2e888e423bf2bcd01006d6.

--- kotlin-mui/src/main/generated/mui/base/MenuItemUnstyled.classes.kt
+++
@@ -1,9 +0,0 @@
-// Automatically generated - do not modify!
-
-package mui.base
-
-external interface MenuItemUnstyledClasses {
-    var root: String
-    var disabled: String
-    var focusVisible: String
-}
