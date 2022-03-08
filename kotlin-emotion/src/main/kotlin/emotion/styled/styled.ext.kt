3093dbe8fb298de147888ba81bbfcc0621b0371a
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2022-03-08 20:56:37 +0300

    Emotion. Styled API is added (#1310)
    * Styled API is added to Emotion
    
    * `styled` signature is more general
    
    * `StyledOptions` are added
    
    * `StyledOptions` are added - revert
    
    * Redundant lambda is removed
    
    * Wrong return type is fixed

---
+++ kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
@@ -0,0 +1,23 @@
+package emotion.styled
+
+import csstype.PropertiesBuilder
+import emotion.react.Theme
+import kotlinx.js.jso
+import react.ElementType
+import react.FC
+import react.Props
+
+private inline val Props.theme: Theme
+    get() = asDynamic().theme
+
+fun <P : Props> ElementType<P>.styled(
+    block: PropertiesBuilder.(P, Theme) -> Unit,
+): FC<P> {
+    val style = { props: P ->
+        val builder: PropertiesBuilder = jso()
+        block(builder, props, props.theme)
+        builder
+    }
+
+    return styled(this)(style)
+}
