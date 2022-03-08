e96bc441df2f89ad43140d13841ea56e5514a4bd
Victor Turansky <victor.turansky@gmail.com>
2022-03-08 13:53:16 +0300

    CSSType. Strict pseudo selectors [generated]

--- kotlin-csstype/src/main/generated/csstype/AdvancedPseudosRuleBuilder.kt
+++ kotlin-csstype/src/main/generated/csstype/AdvancedPseudosRuleBuilder.kt
@@ -8,10 +8,24 @@ package csstype
 
 interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     inline fun cue(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        "::cue($selector)"(block)
+    }
+
+    inline fun cue(
         selector: String,
         block: T.() -> Unit,
     ) {
         "::cue($selector)"(block)
+    }
+
+    inline fun cueRegion(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        "::cue-region($selector)"(block)
     }
 
     inline fun cueRegion(
@@ -22,10 +36,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun part(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        "::part($selector)"(block)
+    }
+
+    inline fun part(
         selector: String,
         block: T.() -> Unit,
     ) {
         "::part($selector)"(block)
+    }
+
+    inline fun slotted(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        "::slotted($selector)"(block)
     }
 
     inline fun slotted(
@@ -36,10 +64,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun dir(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":dir($selector)"(block)
+    }
+
+    inline fun dir(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":dir($selector)"(block)
+    }
+
+    inline fun has(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":has($selector)"(block)
     }
 
     inline fun has(
@@ -50,10 +92,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun host(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":host($selector)"(block)
+    }
+
+    inline fun host(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":host($selector)"(block)
+    }
+
+    inline fun hostContext(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":host-context($selector)"(block)
     }
 
     inline fun hostContext(
@@ -64,10 +120,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun `is`(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":is($selector)"(block)
+    }
+
+    inline fun `is`(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":is($selector)"(block)
+    }
+
+    inline fun lang(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":lang($selector)"(block)
     }
 
     inline fun lang(
@@ -78,10 +148,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun matches(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":matches()($selector)"(block)
+    }
+
+    inline fun matches(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":matches()($selector)"(block)
+    }
+
+    inline fun not(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":not($selector)"(block)
     }
 
     inline fun not(
@@ -92,10 +176,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun nthChild(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":nth-child($selector)"(block)
+    }
+
+    inline fun nthChild(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":nth-child($selector)"(block)
+    }
+
+    inline fun nthLastChild(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":nth-last-child($selector)"(block)
     }
 
     inline fun nthLastChild(
@@ -106,6 +204,13 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun nthLastOfType(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":nth-last-of-type($selector)"(block)
+    }
+
+    inline fun nthLastOfType(
         selector: String,
         block: T.() -> Unit,
     ) {
@@ -113,10 +218,24 @@ interface AdvancedPseudosRuleBuilder<T : Any> : RuleBuilder<T> {
     }
 
     inline fun nthOfType(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":nth-of-type($selector)"(block)
+    }
+
+    inline fun nthOfType(
         selector: String,
         block: T.() -> Unit,
     ) {
         ":nth-of-type($selector)"(block)
+    }
+
+    inline fun where(
+        selector: Selector,
+        block: T.() -> Unit,
+    ) {
+        ":where($selector)"(block)
     }
 
     inline fun where(
