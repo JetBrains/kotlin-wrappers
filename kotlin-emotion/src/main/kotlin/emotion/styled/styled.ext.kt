41022ccc09e361dd3c9fb2d23a2d955ff6136f32
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-03-08 20:28:27 +0300

    Wrong return type is fixed

--- kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
+++ kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
@@ -1,6 +1,5 @@
 package emotion.styled
 
-import csstype.Properties
 import csstype.PropertiesBuilder
 import emotion.react.Theme
 import kotlinx.js.jso
@@ -8,19 +7,16 @@ import react.ElementType
 import react.FC
 import react.Props
 
-external interface PropsWithTheme : Props {
-    val theme: Theme
-}
+private inline val Props.theme: Theme
+    get() = asDynamic().theme
 
 fun <P : Props> ElementType<P>.styled(
     block: PropertiesBuilder.(P, Theme) -> Unit,
 ): FC<P> {
     val style = { props: P ->
-        block(
-            jso(),
-            props,
-            props.unsafeCast<PropsWithTheme>().theme
-        ).unsafeCast<Properties>()
+        val builder: PropertiesBuilder = jso()
+        block(builder, props, props.theme)
+        builder
     }
 
     return styled(this)(style)
