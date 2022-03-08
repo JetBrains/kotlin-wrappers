09df508d505c4eba0034fea16b96658424d7f9de
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-03-08 18:21:32 +0300

    Styled API is added to Emotion

--- kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
+++ kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
@@ -3,21 +3,22 @@ package emotion.styled
 import csstype.PropertiesBuilder
 import emotion.react.Theme
 import kotlinx.js.jso
-import react.ElementType
 import react.FC
+import react.IntrinsicType
 import react.Props
 import react.PropsWithClassName
 
-private inline val Props.theme: Theme
-    get() = asDynamic().theme
+external interface PropsWithTheme : Props {
+    val theme: Theme
+}
 
-fun <P : PropsWithClassName> ElementType<P>.styled(
+fun <P : PropsWithClassName> IntrinsicType<P>.styled(
     block: PropertiesBuilder.(P, Theme) -> Unit,
 ): FC<P> {
     val style = { props: P ->
-        val builder: PropertiesBuilder = jso()
-        block(builder, props, props.theme)
-        builder
+        jso<PropertiesBuilder> {
+            block(props, props.unsafeCast<PropsWithTheme>().theme)
+        }
     }
 
     return styled(this)(style)
