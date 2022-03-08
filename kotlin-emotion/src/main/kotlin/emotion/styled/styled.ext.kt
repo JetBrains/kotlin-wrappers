3af5af75d04c9319c22d75813bb4ec5b9ad2a427
sergeigrishchenko <s.i.grishchenko@gmail.com>
2022-03-08 18:26:40 +0300

    `styled` signature is more general

--- kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
+++ kotlin-emotion/src/main/kotlin/emotion/styled/styled.ext.kt
@@ -3,15 +3,15 @@ package emotion.styled
 import csstype.PropertiesBuilder
 import emotion.react.Theme
 import kotlinx.js.jso
+import react.ElementType
 import react.FC
-import react.IntrinsicType
 import react.Props
 
 external interface PropsWithTheme : Props {
     val theme: Theme
 }
 
-fun <P : Props> IntrinsicType<P>.styled(
+fun <P : Props> ElementType<P>.styled(
     block: PropertiesBuilder.(P, Theme) -> Unit,
 ): FC<P> {
     val style = { props: P ->
