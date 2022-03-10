0e0b6808b19898cbf06162dadb71251369e73caa
Victor Turansky <victor.turansky@gmail.com>
2022-03-10 18:37:58 +0300

    Revert "CSSType. Selector factories [generated] (#1319)" (#1326)
    This reverts commit ea2ac178ad5a105d0ee018e7e70f46bf1675a3e5.

--- kotlin-csstype/src/main/generated/csstype/SimplePseudosRuleBuilder.kt
+++ kotlin-csstype/src/main/generated/csstype/SimplePseudosRuleBuilder.kt
@@ -13,17 +13,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::after"(block)
     }
 
-    inline val Selector.after
-        get() = Selector("${this}::after")
-
     inline fun backdrop(
         block: T.() -> Unit,
     ) {
         "::backdrop"(block)
     }
-
-    inline val Selector.backdrop
-        get() = Selector("${this}::backdrop")
 
     inline fun before(
         block: T.() -> Unit,
@@ -31,17 +25,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::before"(block)
     }
 
-    inline val Selector.before
-        get() = Selector("${this}::before")
-
     inline fun cue(
         block: T.() -> Unit,
     ) {
         "::cue"(block)
     }
-
-    inline val Selector.cue
-        get() = Selector("${this}::cue")
 
     inline fun cueRegion(
         block: T.() -> Unit,
@@ -49,17 +37,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::cue-region"(block)
     }
 
-    inline val Selector.cueRegion
-        get() = Selector("${this}::cue-region")
-
     inline fun firstLetter(
         block: T.() -> Unit,
     ) {
         "::first-letter"(block)
     }
-
-    inline val Selector.firstLetter
-        get() = Selector("${this}::first-letter")
 
     inline fun firstLine(
         block: T.() -> Unit,
@@ -67,17 +49,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::first-line"(block)
     }
 
-    inline val Selector.firstLine
-        get() = Selector("${this}::first-line")
-
     inline fun grammarError(
         block: T.() -> Unit,
     ) {
         "::grammar-error"(block)
     }
-
-    inline val Selector.grammarError
-        get() = Selector("${this}::grammar-error")
 
     inline fun marker(
         block: T.() -> Unit,
@@ -85,17 +61,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::marker"(block)
     }
 
-    inline val Selector.marker
-        get() = Selector("${this}::marker")
-
     inline fun placeholder(
         block: T.() -> Unit,
     ) {
         "::placeholder"(block)
     }
-
-    inline val Selector.placeholder
-        get() = Selector("${this}::placeholder")
 
     inline fun selection(
         block: T.() -> Unit,
@@ -103,17 +73,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::selection"(block)
     }
 
-    inline val Selector.selection
-        get() = Selector("${this}::selection")
-
     inline fun spellingError(
         block: T.() -> Unit,
     ) {
         "::spelling-error"(block)
     }
-
-    inline val Selector.spellingError
-        get() = Selector("${this}::spelling-error")
 
     inline fun targetText(
         block: T.() -> Unit,
@@ -121,17 +85,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         "::target-text"(block)
     }
 
-    inline val Selector.targetText
-        get() = Selector("${this}::target-text")
-
     inline fun active(
         block: T.() -> Unit,
     ) {
         ":active"(block)
     }
-
-    inline val Selector.active
-        get() = Selector("${this}:active")
 
     inline fun anyLink(
         block: T.() -> Unit,
@@ -139,17 +97,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":any-link"(block)
     }
 
-    inline val Selector.anyLink
-        get() = Selector("${this}:any-link")
-
     inline fun blank(
         block: T.() -> Unit,
     ) {
         ":blank"(block)
     }
-
-    inline val Selector.blank
-        get() = Selector("${this}:blank")
 
     inline fun checked(
         block: T.() -> Unit,
@@ -157,17 +109,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":checked"(block)
     }
 
-    inline val Selector.checked
-        get() = Selector("${this}:checked")
-
     inline fun current(
         block: T.() -> Unit,
     ) {
         ":current"(block)
     }
-
-    inline val Selector.current
-        get() = Selector("${this}:current")
 
     inline fun default(
         block: T.() -> Unit,
@@ -175,17 +121,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":default"(block)
     }
 
-    inline val Selector.default
-        get() = Selector("${this}:default")
-
     inline fun defined(
         block: T.() -> Unit,
     ) {
         ":defined"(block)
     }
-
-    inline val Selector.defined
-        get() = Selector("${this}:defined")
 
     inline fun disabled(
         block: T.() -> Unit,
@@ -193,17 +133,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":disabled"(block)
     }
 
-    inline val Selector.disabled
-        get() = Selector("${this}:disabled")
-
     inline fun empty(
         block: T.() -> Unit,
     ) {
         ":empty"(block)
     }
-
-    inline val Selector.empty
-        get() = Selector("${this}:empty")
 
     inline fun enabled(
         block: T.() -> Unit,
@@ -211,17 +145,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":enabled"(block)
     }
 
-    inline val Selector.enabled
-        get() = Selector("${this}:enabled")
-
     inline fun first(
         block: T.() -> Unit,
     ) {
         ":first"(block)
     }
-
-    inline val Selector.first
-        get() = Selector("${this}:first")
 
     inline fun firstChild(
         block: T.() -> Unit,
@@ -229,17 +157,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":first-child"(block)
     }
 
-    inline val Selector.firstChild
-        get() = Selector("${this}:first-child")
-
     inline fun firstOfType(
         block: T.() -> Unit,
     ) {
         ":first-of-type"(block)
     }
-
-    inline val Selector.firstOfType
-        get() = Selector("${this}:first-of-type")
 
     inline fun focus(
         block: T.() -> Unit,
@@ -247,17 +169,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":focus"(block)
     }
 
-    inline val Selector.focus
-        get() = Selector("${this}:focus")
-
     inline fun focusVisible(
         block: T.() -> Unit,
     ) {
         ":focus-visible"(block)
     }
-
-    inline val Selector.focusVisible
-        get() = Selector("${this}:focus-visible")
 
     inline fun focusWithin(
         block: T.() -> Unit,
@@ -265,17 +181,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":focus-within"(block)
     }
 
-    inline val Selector.focusWithin
-        get() = Selector("${this}:focus-within")
-
     inline fun fullscreen(
         block: T.() -> Unit,
     ) {
         ":fullscreen"(block)
     }
-
-    inline val Selector.fullscreen
-        get() = Selector("${this}:fullscreen")
 
     inline fun future(
         block: T.() -> Unit,
@@ -283,17 +193,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":future"(block)
     }
 
-    inline val Selector.future
-        get() = Selector("${this}:future")
-
     inline fun hover(
         block: T.() -> Unit,
     ) {
         ":hover"(block)
     }
-
-    inline val Selector.hover
-        get() = Selector("${this}:hover")
 
     inline fun inRange(
         block: T.() -> Unit,
@@ -301,17 +205,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":in-range"(block)
     }
 
-    inline val Selector.inRange
-        get() = Selector("${this}:in-range")
-
     inline fun indeterminate(
         block: T.() -> Unit,
     ) {
         ":indeterminate"(block)
     }
-
-    inline val Selector.indeterminate
-        get() = Selector("${this}:indeterminate")
 
     inline fun invalid(
         block: T.() -> Unit,
@@ -319,17 +217,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":invalid"(block)
     }
 
-    inline val Selector.invalid
-        get() = Selector("${this}:invalid")
-
     inline fun lastChild(
         block: T.() -> Unit,
     ) {
         ":last-child"(block)
     }
-
-    inline val Selector.lastChild
-        get() = Selector("${this}:last-child")
 
     inline fun lastOfType(
         block: T.() -> Unit,
@@ -337,17 +229,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":last-of-type"(block)
     }
 
-    inline val Selector.lastOfType
-        get() = Selector("${this}:last-of-type")
-
     inline fun left(
         block: T.() -> Unit,
     ) {
         ":left"(block)
     }
-
-    inline val Selector.left
-        get() = Selector("${this}:left")
 
     inline fun link(
         block: T.() -> Unit,
@@ -355,17 +241,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":link"(block)
     }
 
-    inline val Selector.link
-        get() = Selector("${this}:link")
-
     inline fun localLink(
         block: T.() -> Unit,
     ) {
         ":local-link"(block)
     }
-
-    inline val Selector.localLink
-        get() = Selector("${this}:local-link")
 
     inline fun nthCol(
         block: T.() -> Unit,
@@ -373,17 +253,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":nth-col"(block)
     }
 
-    inline val Selector.nthCol
-        get() = Selector("${this}:nth-col")
-
     inline fun nthLastCol(
         block: T.() -> Unit,
     ) {
         ":nth-last-col"(block)
     }
-
-    inline val Selector.nthLastCol
-        get() = Selector("${this}:nth-last-col")
 
     inline fun onlyChild(
         block: T.() -> Unit,
@@ -391,17 +265,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":only-child"(block)
     }
 
-    inline val Selector.onlyChild
-        get() = Selector("${this}:only-child")
-
     inline fun onlyOfType(
         block: T.() -> Unit,
     ) {
         ":only-of-type"(block)
     }
-
-    inline val Selector.onlyOfType
-        get() = Selector("${this}:only-of-type")
 
     inline fun optional(
         block: T.() -> Unit,
@@ -409,17 +277,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":optional"(block)
     }
 
-    inline val Selector.optional
-        get() = Selector("${this}:optional")
-
     inline fun outOfRange(
         block: T.() -> Unit,
     ) {
         ":out-of-range"(block)
     }
-
-    inline val Selector.outOfRange
-        get() = Selector("${this}:out-of-range")
 
     inline fun past(
         block: T.() -> Unit,
@@ -427,17 +289,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":past"(block)
     }
 
-    inline val Selector.past
-        get() = Selector("${this}:past")
-
     inline fun paused(
         block: T.() -> Unit,
     ) {
         ":paused"(block)
     }
-
-    inline val Selector.paused
-        get() = Selector("${this}:paused")
 
     inline fun pictureInPicture(
         block: T.() -> Unit,
@@ -445,17 +301,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":picture-in-picture"(block)
     }
 
-    inline val Selector.pictureInPicture
-        get() = Selector("${this}:picture-in-picture")
-
     inline fun placeholderShown(
         block: T.() -> Unit,
     ) {
         ":placeholder-shown"(block)
     }
-
-    inline val Selector.placeholderShown
-        get() = Selector("${this}:placeholder-shown")
 
     inline fun readOnly(
         block: T.() -> Unit,
@@ -463,17 +313,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":read-only"(block)
     }
 
-    inline val Selector.readOnly
-        get() = Selector("${this}:read-only")
-
     inline fun readWrite(
         block: T.() -> Unit,
     ) {
         ":read-write"(block)
     }
-
-    inline val Selector.readWrite
-        get() = Selector("${this}:read-write")
 
     inline fun required(
         block: T.() -> Unit,
@@ -481,17 +325,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":required"(block)
     }
 
-    inline val Selector.required
-        get() = Selector("${this}:required")
-
     inline fun right(
         block: T.() -> Unit,
     ) {
         ":right"(block)
     }
-
-    inline val Selector.right
-        get() = Selector("${this}:right")
 
     inline fun root(
         block: T.() -> Unit,
@@ -499,17 +337,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":root"(block)
     }
 
-    inline val Selector.root
-        get() = Selector("${this}:root")
-
     inline fun scope(
         block: T.() -> Unit,
     ) {
         ":scope"(block)
     }
-
-    inline val Selector.scope
-        get() = Selector("${this}:scope")
 
     inline fun target(
         block: T.() -> Unit,
@@ -517,17 +349,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":target"(block)
     }
 
-    inline val Selector.target
-        get() = Selector("${this}:target")
-
     inline fun targetWithin(
         block: T.() -> Unit,
     ) {
         ":target-within"(block)
     }
-
-    inline val Selector.targetWithin
-        get() = Selector("${this}:target-within")
 
     inline fun userInvalid(
         block: T.() -> Unit,
@@ -535,17 +361,11 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":user-invalid"(block)
     }
 
-    inline val Selector.userInvalid
-        get() = Selector("${this}:user-invalid")
-
     inline fun userValid(
         block: T.() -> Unit,
     ) {
         ":user-valid"(block)
     }
-
-    inline val Selector.userValid
-        get() = Selector("${this}:user-valid")
 
     inline fun valid(
         block: T.() -> Unit,
@@ -553,15 +373,9 @@ interface SimplePseudosRuleBuilder<T : Any> : RuleBuilder<T> {
         ":valid"(block)
     }
 
-    inline val Selector.valid
-        get() = Selector("${this}:valid")
-
     inline fun visited(
         block: T.() -> Unit,
     ) {
         ":visited"(block)
     }
-
-    inline val Selector.visited
-        get() = Selector("${this}:visited")
 }
