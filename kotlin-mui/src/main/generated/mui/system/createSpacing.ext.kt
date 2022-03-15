1f3e95fa9bb77e6647e4812adebcf95130eb8bca
Victor Turansky <victor.turansky@gmail.com>
2022-03-16 02:04:09 +0300

    MUI. `SpacingOptions` [generated]

--- kotlin-mui/src/main/generated/mui/system/createSpacing.ext.kt
+++ kotlin-mui/src/main/generated/mui/system/createSpacing.ext.kt
@@ -1,5 +1,38 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package mui.system
 
-typealias SpacingOptions = Any
+import kotlinx.js.ReadonlyArray
+
+sealed external interface SpacingOptions
+
+inline fun SpacingOptions(
+    value: Int,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: Spacing,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: (abs: Int) -> csstype.Length,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: (abs: csstype.Length) -> csstype.Length,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: ReadonlyArray<Int>,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+typealias SpacingArgument = Int /* csstype.Auto */
