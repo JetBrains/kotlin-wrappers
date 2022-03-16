50313be228a3ead619da53a067a95ae61be32683
Victor Turansky <victor.turansky@gmail.com>
2022-03-16 10:12:33 +0300

    MUI. `Spacing` [generated] (#1345)

--- kotlin-mui/src/main/generated/mui/system/createSpacing.ext.kt
+++ kotlin-mui/src/main/generated/mui/system/createSpacing.ext.kt
@@ -1,5 +1,65 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "DECLARATION_CANT_BE_INLINED",
+    "NOTHING_TO_INLINE",
+)
+
 package mui.system
 
-typealias SpacingOptions = Any
+import kotlinx.js.ReadonlyArray
+
+sealed external interface SpacingOptions
+
+inline fun SpacingOptions(
+    value: Int,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: Spacing,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: (abs: Int) -> csstype.Length,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: (abs: csstype.Length) -> csstype.Length,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+inline fun SpacingOptions(
+    value: ReadonlyArray<Int>,
+): SpacingOptions =
+    value.unsafeCast<SpacingOptions>()
+
+typealias SpacingArgument = Int /* csstype.Auto */
+
+sealed interface Spacing {
+    inline operator fun invoke(): csstype.Length =
+        asDynamic()()
+
+    inline operator fun invoke(value: Int): csstype.Length =
+        asDynamic()(value)
+
+    inline operator fun invoke(topBottom: SpacingArgument, rightLeft: SpacingArgument): csstype.Length =
+        asDynamic()(topBottom, rightLeft)
+
+    inline operator fun invoke(
+        top: SpacingArgument,
+        rightLeft: SpacingArgument,
+        bottom: SpacingArgument,
+    ): csstype.Length =
+        asDynamic()(top, rightLeft, bottom)
+
+    inline operator fun invoke(
+        top: SpacingArgument,
+        right: SpacingArgument,
+        bottom: SpacingArgument,
+        left: SpacingArgument,
+    ): csstype.Length =
+        asDynamic()(top, right, bottom, left)
+}
