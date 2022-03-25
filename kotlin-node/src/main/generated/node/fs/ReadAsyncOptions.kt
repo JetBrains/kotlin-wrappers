b21d79aa2275dd0448e5a82652334167da131fe4
Victor Turansky <victor.turansky@gmail.com>
2022-03-25 23:34:44 +0300

    Node `17.0.23` [generated]

---
+++ kotlin-node/src/main/generated/node/fs/ReadAsyncOptions.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package node.fs
+
+sealed external interface ReadAsyncOptions<TBuffer : org.khronos.webgl.ArrayBufferView> : ReadSyncOptions {
+    var buffer: TBuffer?
+}
