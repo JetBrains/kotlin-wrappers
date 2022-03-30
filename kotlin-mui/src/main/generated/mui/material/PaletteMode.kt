6e8e0b29bd6284f1ddcb8333ee1ca0e594213dde
Victor Turansky <victor.turansky@gmail.com>
2022-03-31 00:07:52 +0400

    MUI. Strict `Palette` interfaces [generated]

---
+++ kotlin-mui/src/main/generated/mui/material/PaletteMode.kt
@@ -0,0 +1,13 @@
+// Automatically generated - do not modify!
+
+package mui.material
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{light: 'light', dark: 'dark'}/*union*/)""")
+external enum class PaletteMode {
+    light,
+    dark,
+
+    ;
+}
