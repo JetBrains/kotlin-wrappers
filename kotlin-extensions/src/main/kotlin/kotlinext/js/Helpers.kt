328bf9308fb57691a8b5fcf52e0c2b12591861e6
Victor Turansky <victor.turansky@gmail.com>
2022-03-30 21:43:39 +0400

    Regular cleanup

--- kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/Helpers.kt
@@ -2,37 +2,18 @@
 
 package kotlinext.js
 
-@Deprecated(
-    message = "Will be removed soon!",
-    replaceWith = ReplaceWith("kotlinx.js.jso")
-)
-inline fun <T : Any> jso(): T =
-    kotlinx.js.jso()
-
-@Deprecated(
-    message = "Will be removed soon!",
-    replaceWith = ReplaceWith("kotlinx.js.jso")
-)
-inline fun <T : Any> jso(builder: T.() -> Unit): T =
-    kotlinx.js.jso(builder)
+import kotlinx.js.Object
 
 inline fun js(builder: dynamic.() -> Unit): dynamic =
     kotlinx.js.jso(builder)
 
-fun <T : Any> clone(obj: T) = kotlinx.js.Object.assign(kotlinx.js.jso(), obj)
+fun <T : Any> clone(obj: T) = Object.assign(kotlinx.js.jso(), obj)
 
 inline fun <T : Any> assign(obj: T, builder: T.() -> Unit) = clone(obj).apply(builder)
 
 fun toPlainObjectStripNull(obj: Any) = js {
-    for (key in kotlinx.js.Object.keys(obj)) {
+    for (key in Object.keys(obj)) {
         val value = obj.asDynamic()[key]
         if (value != null) this[key] = value
     }
 }
-
-@Deprecated(
-    message = "Will be removed soon!",
-    replaceWith = ReplaceWith("kotlinx.js.delete"),
-)
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
-external fun delete(p: dynamic): Boolean = definedExternally
