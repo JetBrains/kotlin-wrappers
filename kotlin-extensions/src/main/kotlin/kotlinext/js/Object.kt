2aec2591aeabe1f7188411f18bcb76667f17bd24
Victor Turansky <victor.turansky@gmail.com>
2022-03-31 00:10:07 +0400

    Regular cleanup (#1375)

--- kotlin-extensions/src/main/kotlin/kotlinext/js/Object.kt
+++ kotlin-extensions/src/main/kotlin/kotlinext/js/Object.kt
@@ -1,20 +1,9 @@
 package kotlinext.js
 
+import kotlinx.js.Object
 import kotlinx.js.ReadonlyArray
 
-@Deprecated(
-    message = "Will be removed soon!",
-    replaceWith = ReplaceWith("kotlinx.js.PropertyDescriptor")
-)
-typealias PropertyDescriptor<T> = kotlinx.js.PropertyDescriptor<T>
-
-fun Any.asJsObject(): kotlinx.js.Object = unsafeCast<kotlinx.js.Object>()
-
-@Deprecated(
-    message = "Will be removed soon!",
-    replaceWith = ReplaceWith("kotlinx.js.Object")
-)
-typealias Object = kotlinx.js.Object
+fun Any.asJsObject(): Object = unsafeCast<kotlinx.js.Object>()
 
 fun Any.getOwnPropertyNames(): ReadonlyArray<String> =
-    kotlinx.js.Object.getOwnPropertyNames(this)
+    Object.getOwnPropertyNames(this)
--- kotlin-extensions/src/main/kotlin/kotlinext/js/ReadonlyArray.kt
+++
@@ -1,4 +0,0 @@
-package kotlinext.js
-
-@Deprecated(message = "Will be removed soon!")
-typealias ReadonlyArray<T> = kotlinx.js.ReadonlyArray<T>
