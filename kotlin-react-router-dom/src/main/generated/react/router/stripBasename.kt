3faa19a5391600e30f644e5ed6dfb6bf883ce81f
Victor Turansky <victor.turansky@gmail.com>
2022-04-01 01:19:35 +0400

    React Router `6.3.0` [generated]

---
+++ kotlin-react-router-dom/src/main/generated/react/router/stripBasename.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("react-router")
+@file:JsNonModule
+
+package react.router
+
+external fun stripBasename(
+    pathname: String,
+    basename: String,
+): String
