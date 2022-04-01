a5afeb5c1a9a00f7583d78d69525068c76d27573
Victor Turansky <victor.turansky@gmail.com>
2022-04-01 11:31:09 +0400

    React. Fix `Root` contract [generated] (#1381)

--- kotlin-react-dom/src/main/kotlin/react/dom/client/Root.kt
+++ kotlin-react-dom/src/main/kotlin/react/dom/client/Root.kt
@@ -1,10 +1,8 @@
 package react.dom.client
 
-import kotlinx.js.ReadonlyArray
 import react.ReactNode
 
 external interface Root {
-    fun render(children: ReactNode)
-    fun render(children: ReadonlyArray<ReactNode>)
+    fun render(children: ReactNode?)
     fun unmount()
 }
