9ee1119e981466d6c76155275034b3c000b09bc0
Victor Turansky <victor.turansky@gmail.com>
2022-04-01 11:30:23 +0400

    React Router `6.3.0` [generated] (#1379)

---
+++ kotlin-react-router-dom/src/main/generated/react/router/getToPathname.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("react-router")
+@file:JsNonModule
+
+package react.router
+
+external fun getToPathname(
+    to: history.To,
+): String?
---
+++ kotlin-react-router-dom/src/main/generated/react/router/resolveTo.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("react-router")
+@file:JsNonModule
+
+package react.router
+
+external fun resolveTo(
+    toArg: history.To,
+    routePathnames: kotlinx.js.ReadonlyArray<String>,
+    locationPathname: String,
+): history.Path
