53a4397d9fe590de8c5d18f13d21b1dde032dd44
Victor Turansky <victor.turansky@gmail.com>
2022-04-03 14:02:22 +0400

    JS. `HighResTimeStamp` docs (#1387)

--- kotlin-extensions/src/main/kotlin/kotlinx/js/TimeStamp.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/TimeStamp.kt
@@ -1,4 +1,11 @@
 package kotlinx.js
 
+/**
+ * Common alias for [DOMHighResTimeStamp](https://developer.mozilla.org/en-US/docs/Web/API/DOMHighResTimeStamp)
+ */
 typealias HighResTimeStamp = Double
+
+/**
+ * Common alias for [DOMTimeStamp](https://developer.mozilla.org/en-US/docs/Web/API/DOMTimeStamp)
+ */
 typealias TimeStamp = Double // Int64
