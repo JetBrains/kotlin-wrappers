8f2c46402c75c199551397f171ba5efd3aa66dda
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2022-04-05 18:39:44 +0200

    Restore `useInsertionEffect` detection
    This reverts commit 00161dea58842d41195a19231a5a3bc73ce112de.

--- kotlin-styled-next/src/main/kotlin/styled/StructEffects.kt
+++ kotlin-styled-next/src/main/kotlin/styled/StructEffects.kt
@@ -1,15 +1,26 @@
 package styled
 
-import react.Cleanup
-import react.MutableRefObject
-import react.rawUseMemo
-import react.useRef
+import react.*
 
 internal fun <T> useStructMemo(
     vararg dependencies: dynamic,
     callback: () -> T,
 ): T {
     return rawUseMemo(getMemoizedCallback(dependencies, callback), dependencies)
+}
+
+@JsModule("react")
+@JsNonModule
+external object ReactModule
+
+private val supportsInsertionEffect by kotlin.lazy { ReactModule.asDynamic().useInsertionEffect != undefined }
+
+internal fun useCustomInsertionEffect(vararg dependencies: dynamic, effect: EffectBuilder.() -> Unit) {
+    if (supportsInsertionEffect) {
+        useInsertionEffect(*dependencies, effect = effect)
+    } else {
+        useLayoutEffect(*dependencies, effect = effect)
+    }
 }
 
 private data class MemoizedResult<T>(
