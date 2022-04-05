00161dea58842d41195a19231a5a3bc73ce112de
Victor Turansky <victor.turansky@gmail.com>
2022-04-05 09:53:28 +0400

    Styled. Remove `useInsertionEffect` workaround (#1398)

--- kotlin-styled-next/src/main/kotlin/styled/StyledComponents.kt
+++ kotlin-styled-next/src/main/kotlin/styled/StyledComponents.kt
@@ -122,7 +122,7 @@ internal fun customStyled(type: dynamic): ElementType<StyledProps> {
 
         // className and isFresh are used as dependencies because they have primitive types and are easily comparable in JS.
         // This hook works because className changes every time when [css] is changed, so every [css] is captured exactly once.
-        useCustomInsertionEffect(isFresh, className) {
+        useInsertionEffect(isFresh, className) {
             if (isFresh) {
                 GlobalStyles.injectScheduled()
             }
@@ -131,7 +131,7 @@ internal fun customStyled(type: dynamic): ElementType<StyledProps> {
             }
         }
 
-        useCustomInsertionEffect(classes) {
+        useInsertionEffect(classes) {
             // A heuristic for tracking the usage of a new stylesheet.
             // If the element's stylesheets have changed, inject all pending stylesheets.
             GlobalStyles.injectScheduled()
