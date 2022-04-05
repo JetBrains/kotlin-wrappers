adadade46e4c55d9098fed4f32f1d0ffb04763e9
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2022-04-05 10:17:58 +0200

    React 18. Mark deprecated API

--- kotlin-react-dom-legacy/src/main/kotlin/react/dom/hydrate.ext.kt
+++ kotlin-react-dom-legacy/src/main/kotlin/react/dom/hydrate.ext.kt
@@ -5,6 +5,10 @@ import react.Props
 import react.Render
 import react.createElement
 
+/**
+ * [Deprecation details](https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#deprecations)
+ */
+@Deprecated("Legacy API")
 fun hydrate(
     container: Element,
     callback: () -> Unit = {},
