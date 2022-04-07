2f4a4a3f2d795a62140ebb091e5a9c07725264c8
Stefan Thaler <stefan@thaler-it.net>
2022-04-07 18:10:49 +0200

    React-Query. Add safe `QueryKey` factories

---
+++ kotlin-react-query/src/main/kotlin/react/query/QueryKey.ext.kt
@@ -0,0 +1,13 @@
+@file:Suppress(
+    "NOTHING_TO_INLINE"
+)
+
+import react.query.QueryKey
+
+inline fun <T : QueryKey> QueryKey(
+    key: String,
+): QueryKey = key
+
+inline fun <T : QueryKey> QueryKey(
+    vararg keys: Any,
+): QueryKey = keys
