caa965b7cf9d84439c012a8d8346f9bed0aecf50
Victor Turansky <victor.turansky@gmail.com>
2022-04-16 04:44:10 +0400

    Browser. Strict event handling

---
+++ kotlin-browser/src/main/kotlin/org/w3c/dom/events/EventTarget.kt
@@ -0,0 +1,54 @@
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package org.w3c.dom.events
+
+import kotlinx.js.jso
+import org.w3c.fetch.AbortSignal
+
+@JsName("null") // IR workaround
+sealed external interface EventHandlerOptions {
+    var capture: Boolean?
+    var once: Boolean?
+    var passive: Boolean?
+    var signal: AbortSignal?
+
+    companion object
+}
+
+inline val EventHandlerOptions.Companion.ACTIVE: EventHandlerOptions
+    get() = jso { passive = false }
+
+inline val EventHandlerOptions.Companion.CAPTURE: EventHandlerOptions
+    get() = jso { capture = true }
+
+fun <T : Event> EventTarget.addEventHandler(
+    type: EventType<T>,
+    handler: (T) -> Unit,
+): () -> Unit =
+    addEventHandler(
+        type = type,
+        handler = handler,
+        options = undefined,
+    )
+
+fun <T : Event> EventTarget.addEventHandler(
+    type: EventType<T>,
+    options: EventHandlerOptions?,
+    handler: (T) -> Unit,
+): () -> Unit {
+    addEventListener(
+        type = type.unsafeCast<String>(),
+        callback = handler.unsafeCast<(Event) -> Unit>(),
+        options = options,
+    )
+
+    return {
+        removeEventListener(
+            type = type.unsafeCast<String>(),
+            callback = handler.unsafeCast<(Event) -> Unit>(),
+            options = options,
+        )
+    }
+}
