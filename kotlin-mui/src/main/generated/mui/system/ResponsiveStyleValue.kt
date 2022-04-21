0a469c7ca2ded6fbf1e13d2194b4dd5b560f36eb
Victor Turansky <victor.turansky@gmail.com>
2022-04-21 10:08:27 +0400

    MUI. `responsive` helper [generated] (#1452)

--- kotlin-mui/src/main/generated/mui/system/ResponsiveStyleValue.kt
+++ kotlin-mui/src/main/generated/mui/system/ResponsiveStyleValue.kt
@@ -2,10 +2,23 @@
 
 package mui.system
 
+import kotlinx.js.Record
+
 external interface ResponsiveStyleValue<T : Any>
 
 @Suppress("NOTHING_TO_INLINE")
-inline fun <T : Any> ResponsiveStyleValue(
+inline fun <T : Any> responsive(
     value: T,
 ): ResponsiveStyleValue<T> =
     value.unsafeCast<ResponsiveStyleValue<T>>()
+
+@Suppress("BOUNDS_NOT_ALLOWED_IF_BOUNDED_BY_TYPE_PARAMETER")
+fun <T : Any, R> responsive(
+    vararg values: Pair<Breakpoint, T>,
+): R where R : T,
+           R : ResponsiveStyleValue<T> =
+    Record<Breakpoint, T> {
+        for ((key, value) in values) {
+            set(key, value)
+        }
+    }.unsafeCast<R>()
