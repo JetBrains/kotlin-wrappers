645653c2462c02fee1da4afce638266e68db8ba6
Victor Turansky <victor.turansky@gmail.com>
2022-04-23 23:11:21 +0400

    `Symbol` static properties

--- kotlin-extensions/src/main/kotlin/kotlinx/js/Symbol.kt
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/Symbol.kt
@@ -1,4 +1,6 @@
-@file:Suppress("NESTED_CLASS_IN_EXTERNAL_INTERFACE")
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
 
 package kotlinx.js
 
@@ -6,6 +8,20 @@ sealed external interface Symbol {
     val description: String?
 
     companion object {
+        val asyncIterator: Symbol
+        val hasInstance: Symbol
+        val isConcatSpreadable: Symbol
+        val iterator: Symbol
+        val match: Symbol
+        val matchAll: Symbol
+        val replace: Symbol
+        val search: Symbol
+        val species: Symbol
+        val split: Symbol
+        val toPrimitive: Symbol
+        val toStringTag: Symbol
+        val unscopables: Symbol
+
         fun `for`(key: String): Symbol
 
         fun keyFor(symbol: Symbol): String?
