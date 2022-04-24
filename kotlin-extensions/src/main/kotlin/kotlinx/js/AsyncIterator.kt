e7b700802bbd3d1c6608f32cbf5d3eed977d844a
Victor Turansky <victor.turansky@gmail.com>
2022-04-24 04:03:57 +0400

    JS. `AsyncIterator`

---
+++ kotlin-extensions/src/main/kotlin/kotlinx/js/AsyncIterator.kt
@@ -0,0 +1,19 @@
+package kotlinx.js
+
+import kotlin.js.Promise
+
+sealed external interface AsyncIterator<T, TReturn> {
+    val next: (/* ...args: [] | [TNext] */) -> Promise<JsIterator.Result<T, TReturn>>
+    val `return`: ((value: Promise<TReturn>?) -> Promise<JsIterator.Result<T, TReturn>>)?
+    val `throw`: ((e: Throwable?) -> Promise<JsIterator.Result<T, TReturn>>)?
+}
+
+sealed external interface AsyncIterable<T>
+
+fun <T> AsyncIterable<T>.asyncIterator(): AsyncIterator<T, Void> =
+    asDynamic()[Symbol.asyncIterator]()
+
+sealed external interface AsyncIterableIterator<T> : AsyncIterator<T, Void>
+
+fun <T> AsyncIterableIterator<T>.asyncIterator(): AsyncIterableIterator<T> =
+    asDynamic()[Symbol.asyncIterator]()
