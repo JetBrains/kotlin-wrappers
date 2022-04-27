b1158a05da644e55b0d05993bd3b6dfc0b0be438
Victor Turansky <victor.turansky@gmail.com>
2022-04-28 03:33:54 +0400

    Browser. Actualize `Clipboard` API

---
+++ kotlin-browser/src/main/kotlin/org/w3c/dom/clipboard/ClipboardItem.kt
@@ -0,0 +1,18 @@
+package org.w3c.dom.clipboard
+
+import kotlinx.js.ReadonlyArray
+import kotlinx.js.Record
+import org.w3c.files.Blob
+import kotlin.js.Promise
+
+sealed external interface ClipboardItemOptions {
+    var presentationStyle: PresentationStyle?
+}
+
+external class ClipboardItem(
+    items: Record<String, ClipboardItemDataType>,
+    options: ClipboardItemOptions = definedExternally,
+) {
+    val types: ReadonlyArray<String>
+    fun getType(type: String): Promise<Blob>
+}
---
+++ kotlin-browser/src/main/kotlin/org/w3c/dom/clipboard/PresentationStyle.kt
@@ -0,0 +1,15 @@
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package org.w3c.dom.clipboard
+
+// language=JavaScript
+@JsName("""(/*union*/{attachment: 'attachment', inline: 'inline', unspecified: 'unspecified'}/*union*/)""")
+external enum class PresentationStyle {
+    attachment,
+    inline,
+    unspecified,
+
+    ;
+}
