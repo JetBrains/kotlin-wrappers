bb3eb449ca32aefdd8502fb43eaef73513b12e18
Victor Turansky <victor.turansky@gmail.com>
2022-05-07 22:48:13 +0300

    React Types `18.0.9` [generated]

--- kotlin-react-dom/src/main/generated/react/dom/html/DialogHTMLAttributes.kt
+++ kotlin-react-dom/src/main/generated/react/dom/html/DialogHTMLAttributes.kt
@@ -3,7 +3,10 @@
 package react.dom.html
 
 import org.w3c.dom.Element
+import react.dom.events.ReactEventHandler
 
 external interface DialogHTMLAttributes<T : Element> : HTMLAttributes<T> {
+    var onCancel: ReactEventHandler<T>?
+    var onClose: ReactEventHandler<T>?
     var open: Boolean?
 }
