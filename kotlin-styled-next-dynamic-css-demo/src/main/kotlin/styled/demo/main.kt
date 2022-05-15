c8ba9f4d1c39a92cb874f323c1c8356983f3e553
Andrey Koshmanskiy <andrew.k.21.12@gmail.com>
2022-05-15 08:22:46 +0300

    kotlin-styled-next: dynamic CSS usage demo

--- kotlin-styled-next-dynamic-css-demo/src/main/kotlin/styled/demo/main.kt
+++ kotlin-styled-next-dynamic-css-demo/src/main/kotlin/styled/demo/main.kt
@@ -2,11 +2,14 @@ package styled.demo
 
 import kotlinx.browser.document
 import kotlinx.browser.window
-import react.Props
-import react.create
+import kotlinx.css.fontSize
+import kotlinx.css.rem
+import org.w3c.dom.events.Event
+import react.*
 import react.dom.client.createRoot
-import react.dom.span
-import react.fc
+import styled.StyleSheet
+import styled.css
+import styled.styledP
 
 fun main() {
     window.onload = {
@@ -16,7 +19,37 @@ fun main() {
 }
 
 private val demoComponent = fc<Props> {
-    span {
-        +"Hello there!"
+
+    val (screenSize, setScreenSize) = useState(ScreenSize.fromRawWidth(window.innerWidth))
+
+    useEffectOnce {
+        val eventListener: (Event) -> Unit = {
+            setScreenSize(ScreenSize.fromRawWidth(window.innerWidth))
+        }
+        window.addEventListener("resize", eventListener)
+        cleanup {
+            window.removeEventListener("resize", eventListener)
+        }
     }
+
+    styledP {
+
+        css(DemoComponentStyles.demoDynamicStyle(screenSize))
+
+        +"Hi, it seems like I'm running on the ${screenSize.name.lowercase()} screen!"
+
+    }
+
+}
+
+private object DemoComponentStyles : StyleSheet() {
+
+    val demoDynamicStyle by dynamicCss<ScreenSize> {
+        fontSize = when (it) {
+            ScreenSize.DESKTOP -> 2.rem
+            ScreenSize.TABLET  -> 1.5.rem
+            ScreenSize.PHONE   -> 1.rem
+        }
+    }
+
 }
