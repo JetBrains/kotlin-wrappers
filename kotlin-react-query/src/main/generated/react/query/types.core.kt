60408fbeda5f96da9845e694721a4c3fecc6292f
Victor Turansky <victor.turansky@gmail.com>
2022-06-02 09:28:01 +0300

    React Query `3.39.1` [generated] (#1504)

--- kotlin-react-query/src/main/generated/react/query/types.core.kt
+++ kotlin-react-query/src/main/generated/react/query/types.core.kt
@@ -315,7 +315,7 @@ external interface MutationOptions<TData, TError, TVariables, TContext> {
     var mutationKey: MutationKey
     var variables: TVariables
     var onMutate: (variables: TVariables) -> Promise<TContext?>?
-    var onSuccess: (data: TData, variables: TVariables, context: TContext) -> Promise<Any>?
+    var onSuccess: (data: TData, variables: TVariables, context: TContext?) -> Promise<Any>?
     var onError: (error: TError, variables: TVariables, context: TContext?) -> Promise<Any>?
     var onSettled: (data: TData?, error: TError?, variables: TVariables, context: TContext?) -> Promise<Any>?
     var retry: RetryValue<TError>
