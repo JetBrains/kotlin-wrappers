90c297c433b8dfbea406bc30d991321e685389ab
Victor Turansky <victor.turansky@gmail.com>
2022-06-04 16:15:50 +0300

    React Types `18.0.11` [generated] (#1511)

---
+++ kotlin-react-dom/src/main/generated/react/dom/html/TableAlign.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+package react.dom.html
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{left: 'left', center: 'center', right: 'right'}/*union*/)""")
+external enum class TableAlign {
+    left,
+    center,
+    right,
+
+    ;
+}
---
+++ kotlin-react-dom/src/main/generated/react/dom/html/TableRules.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+package react.dom.html
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{none: 'none', groups: 'groups', rows: 'rows', columns: 'columns', all: 'all'}/*union*/)""")
+external enum class TableRules {
+    none,
+    groups,
+    rows,
+    columns,
+    all,
+
+    ;
+}
