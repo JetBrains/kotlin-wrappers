9ac13ec61635aa3c0632e599e1f4e6938ae71f2e
Victor Turansky <victor.turansky@gmail.com>
2022-06-04 15:58:55 +0300

    React Types `18.0.11` [generated]

--- kotlin-react-dom/src/main/generated/react/dom/html/TableHTMLAttributes.kt
+++ kotlin-react-dom/src/main/generated/react/dom/html/TableHTMLAttributes.kt
@@ -5,8 +5,13 @@ package react.dom.html
 import org.w3c.dom.Element
 
 external interface TableHTMLAttributes<T : Element> : HTMLAttributes<T> {
+    var align: TableAlign?
+    var bgcolor: String?
+    var border: Int?
     var cellPadding: String?
     var cellSpacing: String?
+    var frame: Boolean?
+    var rules: TableRules?
     var summary: String?
     var width: Double?
 }
