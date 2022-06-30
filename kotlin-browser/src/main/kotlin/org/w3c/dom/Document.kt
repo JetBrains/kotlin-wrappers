1d6ea07f434f721b5ab3b89c501f3c5dfe5e2fe4
Victor Turansky <victor.turansky@gmail.com>
2022-06-30 13:05:04 +0300

    Strict `Document.createElement(tagName)` (#1557)

--- kotlin-browser/src/main/kotlin/org/w3c/dom/Document.kt
+++ kotlin-browser/src/main/kotlin/org/w3c/dom/Document.kt
@@ -2,3 +2,9 @@ package org.w3c.dom
 
 inline val Document.fonts: FontFaceSet
     get() = asDynamic().fonts
+
+inline fun <T : Element> Document.createElement(
+    tagName: TagName<T>,
+): T =
+    createElement(tagName.unsafeCast<String>())
+        .unsafeCast<T>()
