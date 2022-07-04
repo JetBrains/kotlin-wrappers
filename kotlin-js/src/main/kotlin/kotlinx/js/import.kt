f33f1824c53b02bbebd5eb5e3398cf0c017a44a4
Victor Turansky <victor.turansky@gmail.com>
2022-07-04 17:06:25 +0300

    JS. `import.meta` declarations

--- kotlin-js/src/main/kotlin/kotlinx/js/import.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/import.kt
@@ -5,3 +5,23 @@ import kotlin.js.Promise
 external fun <T : Any> import(
     path: String,
 ): Promise<T>
+
+external object import {
+    object meta {
+        object env
+
+        val url: String
+    }
+}
+
+inline val import.meta.env.MODE: String?
+    get() = process.env.NODE_ENV
+
+inline val import.meta.env.PROD: Boolean
+    get() = process.env.NODE_ENV === "production"
+
+inline val import.meta.env.DEV: Boolean
+    get() = process.env.NODE_ENV !== "production"
+
+@PublishedApi
+internal external val process: dynamic
