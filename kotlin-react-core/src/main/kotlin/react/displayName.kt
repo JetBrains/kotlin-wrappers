862e59cca73e9ab94818ad5015b96c377e3056b5
Victor Turansky <victor.turansky@gmail.com>
2022-07-04 12:17:47 +0300

    React. `displayName` - regular extension property (#1566)

--- kotlin-react-core/src/main/kotlin/react/displayName.kt
+++ kotlin-react-core/src/main/kotlin/react/displayName.kt
@@ -1,22 +1,19 @@
 package react
 
 inline var FC<*>.displayName: String?
-    @Deprecated(message = "Write-only property", level = DeprecationLevel.HIDDEN)
-    get() = error("")
+    get() = asDynamic().displayName
     set(value) {
         asDynamic().displayName = value
     }
 
 inline var NamedExoticComponent<*>.displayName: String?
-    @Deprecated(message = "Write-only property", level = DeprecationLevel.HIDDEN)
-    get() = error("")
+    get() = asDynamic().displayName
     set(value) {
         asDynamic().displayName = value
     }
 
 inline var Context<*>.displayName: String?
-    @Deprecated(message = "Write-only property", level = DeprecationLevel.HIDDEN)
-    get() = error("")
+    get() = asDynamic().displayName
     set(value) {
         asDynamic().displayName = value
     }
