bc9f58bea632e4bf4d52162bab13198a6f37c4a4
Victor Turansky <victor.turansky@gmail.com>
2022-07-08 08:28:31 +0300

    React Table. `TableOptions` [generated] (#1583)

--- kotlin-tanstack-react-table/src/main/generated/tanstack/table/core/TableOptions.kt
+++ kotlin-tanstack-react-table/src/main/generated/tanstack/table/core/TableOptions.kt
@@ -2,4 +2,13 @@
 
 package tanstack.table.core
 
-external interface TableOptions<TData : RowData> /* PartialKeys<TableOptionsResolved<TData>, 'state' | 'onStateChange' | 'renderFallbackValue'> */
+external interface TableOptions<TData : RowData> : TableOptionsResolved<TData> {
+    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
+    override var state: dynamic
+
+    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
+    override var onStateChange: dynamic
+
+    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
+    override var renderFallbackValue: dynamic
+}
