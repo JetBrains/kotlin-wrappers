1e10e28fd32006ef6c4d9460df3b9f8451920a18
Victor Turansky <victor.turansky@gmail.com>
2022-07-09 17:59:43 +0300

    Popper. State [generated] (#1587)

--- kotlin-popper/src/main/generated/popper/core/State.kt
+++ kotlin-popper/src/main/generated/popper/core/State.kt
@@ -2,47 +2,47 @@
 
 package popper.core
 
+import kotlinx.js.ReadonlyArray
+
 external interface State {
-/*
-elements: {
-    reference: Element | VirtualElement;
-    popper: HTMLElement;
-    arrow?: HTMLElement;
-};
-options: OptionsGeneric<any>;
-placement: Placement;
-strategy: PositioningStrategy;
-orderedModifiers: Array<Modifier<any, any>>;
-rects: StateRects;
-scrollParents: {
-    reference: Array<Element | Window | VisualViewport>;
-    popper: Array<Element | Window | VisualViewport>;
-};
-styles: {
-    [key: string]: Partial<CSSStyleDeclaration>;
-};
-attributes: {
-    [key: string]: {
-        [key: string]: string | boolean;
-    };
-};
-modifiersData: {
-    arrow?: {
-        x?: number;
-        y?: number;
-        centerOffset: number;
-    };
-    hide?: {
-        isReferenceHidden: boolean;
-        hasPopperEscaped: boolean;
-        referenceClippingOffsets: SideObject;
-        popperEscapeOffsets: SideObject;
-    };
-    offset?: OffsetData;
-    preventOverflow?: Offsets;
-    popperOffsets?: Offsets;
-    [key: string]: any;
-};
-reset: boolean
-*/
+    val elements: dynamic /* {
+        reference: Element | VirtualElement;
+        popper: HTMLElement;
+        arrow?: HTMLElement;
+    }; */
+    val options: OptionsGeneric<*>
+    val placement: Placement
+    val strategy: PositioningStrategy
+    val orderedModifiers: ReadonlyArray<Modifier<*>>
+    val rects: StateRects
+    val scrollParents: dynamic /* {
+        reference: Array<Element | Window | VisualViewport>;
+        popper: Array<Element | Window | VisualViewport>;
+    }; */
+    val styles: dynamic /* {
+        [key: string]: Partial<CSSStyleDeclaration>;
+    }; */
+    val attributes: dynamic /* {
+        [key: string]: {
+            [key: string]: string | boolean;
+        };
+    }; */
+    val modifiersData: dynamic /* {
+        arrow?: {
+            x?: number;
+            y?: number;
+            centerOffset: number;
+        };
+        hide?: {
+            isReferenceHidden: boolean;
+            hasPopperEscaped: boolean;
+            referenceClippingOffsets: SideObject;
+            popperEscapeOffsets: SideObject;
+        };
+        offset?: OffsetData;
+        preventOverflow?: Offsets;
+        popperOffsets?: Offsets;
+        [key: string]: any;
+    }; */
+    val reset: Boolean
 }
