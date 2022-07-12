4f2e28e5bd7463977bb3a1c850325e5ab7d28933
Victor Turansky <victor.turansky@gmail.com>
2022-07-12 19:24:53 +0300

    React Table. Fix `ColumnDefTemplate` usage (#1597)

--- kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/ColumnDefTemplate.kt
+++ kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/ColumnDefTemplate.kt
@@ -7,7 +7,7 @@ package tanstack.react.table
 import react.FC
 import tanstack.table.core.ColumnDefTemplate
 
-inline fun <T> ColumnDefTemplate(
+inline fun <T : Any> ColumnDefTemplate(
     source: FC<* /* T & Props */>,
-): ColumnDefTemplate<() -> T> =
-    source.unsafeCast<ColumnDefTemplate<() -> T>>()
+): ColumnDefTemplate<T> =
+    source.unsafeCast<ColumnDefTemplate<T>>()
