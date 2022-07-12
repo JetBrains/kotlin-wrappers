6707b3f1dc50a55cdc498bd57b59037a2560c83d
Stefan Thaler <stefan@thaler-it.net>
2022-07-12 13:18:36 +0200

    React Table. Add `ColumnDefTemplate` react adapter

---
+++ kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/ColumnDefTemplate.kt
@@ -0,0 +1,13 @@
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package tanstack.react.table
+
+import react.FC
+import tanstack.table.core.ColumnDefTemplate
+
+inline fun <T> ColumnDefTemplate(
+    source: FC<* /* T & Props */>,
+): ColumnDefTemplate<() -> T> =
+    source.unsafeCast<ColumnDefTemplate<() -> T>>()
