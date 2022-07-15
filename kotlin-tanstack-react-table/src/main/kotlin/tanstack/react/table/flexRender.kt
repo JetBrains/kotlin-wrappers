9f321bca42084a098bf038df9fc87268e45a12b4
Victor Turansky <victor.turansky@gmail.com>
2022-07-15 12:30:37 +0300

    React Table. Fix `flexRender` signature

--- kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/flexRender.kt
+++ kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/flexRender.kt
@@ -7,6 +7,6 @@ import react.ReactNode
 import tanstack.table.core.ColumnDefTemplate
 
 external fun <T : Any> flexRender(
-    Comp: ColumnDefTemplate<out () -> T>?,
+    Comp: ColumnDefTemplate<out T>?,
     props: T
 ): ReactNode?
