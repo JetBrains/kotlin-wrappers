541cb6ac35508ba99f28ace4f463cbe00c4b7ad5
Victor Turansky <victor.turansky@gmail.com>
2022-07-16 17:15:06 +0300

    CSSType. `+Properties` support for `PropertiesBuilder`

--- kotlin-csstype/src/main/kotlin/csstype/PropertiesBuilder.kt
+++ kotlin-csstype/src/main/kotlin/csstype/PropertiesBuilder.kt
@@ -1,6 +1,13 @@
 package csstype
 
+import kotlinx.js.Object
+
 sealed class PropertiesBuilder :
     Properties,
     RuleBuilder<PropertiesBuilder>,
-    PseudosRuleBuilder<PropertiesBuilder>
+    PseudosRuleBuilder<PropertiesBuilder> {
+
+    inline operator fun Properties?.unaryPlus() {
+        Object.assign(this@PropertiesBuilder, this)
+    }
+}
