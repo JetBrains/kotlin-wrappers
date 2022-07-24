2714080b9b2ae4ae30244e7c7a043b3e19ba1fda
Victor Turansky <victor.turansky@gmail.com>
2022-07-24 16:08:00 +0300

    CSSType. `Transition` factories [generated] (#1621)

--- kotlin-csstype/src/main/generated/csstype/Transition.kt
+++ kotlin-csstype/src/main/generated/csstype/Transition.kt
@@ -3,6 +3,7 @@
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NOTHING_TO_INLINE",
 )
 
 package csstype
@@ -21,3 +22,25 @@ sealed external interface Transition {
         val all: Transition
     }
 }
+
+inline fun Transition(
+    property: TransitionProperty,
+    duration: Time,
+    timingFunction: TransitionTimingFunction,
+): Transition =
+    "$property $duration $timingFunction".unsafeCast<Transition>()
+
+inline fun Transition(
+    property: TransitionProperty,
+    duration: Time,
+    delay: Time,
+): Transition =
+    "$property $duration $delay".unsafeCast<Transition>()
+
+inline fun Transition(
+    property: TransitionProperty,
+    duration: Time,
+    timingFunction: TransitionTimingFunction,
+    delay: Time,
+): Transition =
+    "$property $duration $timingFunction $delay".unsafeCast<Transition>()
