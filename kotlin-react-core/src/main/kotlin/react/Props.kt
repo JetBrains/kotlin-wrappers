39e10d5b0959107d378b4ecdaf00bc807312df23
Victor Turansky <victor.turansky@gmail.com>
2022-07-31 20:51:49 +0300

    React. `Props.ref` extension - legacy

--- kotlin-react-core/src/main/kotlin/react/Props.kt
+++ kotlin-react-core/src/main/kotlin/react/Props.kt
@@ -3,10 +3,3 @@ package react
 external interface Props {
     var key: Key?
 }
-
-inline var Props.ref: Ref<*>?
-    @Deprecated(message = "Write-only property", level = DeprecationLevel.HIDDEN)
-    get() = error("")
-    set(value) {
-        asDynamic().ref = value
-    }
---
+++ kotlin-react-legacy/src/main/kotlin/react/Props.ext.kt
@@ -0,0 +1,8 @@
+package react
+
+inline var Props.ref: Ref<*>?
+    @Deprecated(message = "Write-only property", level = DeprecationLevel.HIDDEN)
+    get() = error("")
+    set(value) {
+        asDynamic().ref = value
+    }
