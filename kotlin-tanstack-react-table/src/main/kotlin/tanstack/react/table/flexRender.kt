8b228d9a75865e866286adc49336d3e0e6f37d00
Victor Turansky <victor.turansky@gmail.com>
2022-08-04 16:04:08 +0300

    Tanstack Table. Missed `flexRender` declaration (#1656)

--- kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/flexRender.kt
+++ kotlin-tanstack-react-table/src/main/kotlin/tanstack/react/table/flexRender.kt
@@ -5,6 +5,14 @@ package tanstack.react.table
 
 import react.ReactNode
 import tanstack.table.core.ColumnDefTemplate
+import tanstack.table.core.HeaderContext
+import tanstack.table.core.RowData
+import tanstack.table.core.StringOrTemplateHeader
+
+external fun <TData : RowData, TValue> flexRender(
+    Comp: StringOrTemplateHeader<TData, TValue>?,
+    props: HeaderContext<TData, TValue>,
+): ReactNode?
 
 external fun <T : Any> flexRender(
     Comp: ColumnDefTemplate<out T>?,
