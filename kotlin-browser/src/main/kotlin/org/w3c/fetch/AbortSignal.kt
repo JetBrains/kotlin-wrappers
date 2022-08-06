2bf86c3b8151866f7882810ce8ffd4091b58a8b0
Victor Turansky <victor.turansky@gmail.com>
2022-08-06 15:24:57 +0300

    JS. Common `AbortController` and `AbortSignal` (#1663)

--- kotlin-browser/src/main/kotlin/org/w3c/fetch/AbortSignal.kt
+++ kotlin-browser/src/main/kotlin/org/w3c/fetch/AbortSignal.kt
@@ -1,13 +1,12 @@
 package org.w3c.fetch
 
-import org.w3c.dom.events.Event
 import org.w3c.dom.events.EventTarget
 
 /** A signal object that allows you to communicate with a DOM request (such as a Fetch) and abort it if required via an AbortController object. */
-external class AbortSignal : EventTarget {
+external class AbortSignal : kotlinx.js.AbortSignal, EventTarget {
     /**
      * Returns true if this AbortSignal's AbortController has signaled to abort, and false otherwise.
      */
-    val aborted: Boolean
-    var onabort: ((event: Event) -> Unit)?
+    override val aborted: Boolean
+    override var onabort: Function<Unit>? /* ((event: Event) -> Unit)? */
 }
---
+++ kotlin-js/src/main/kotlin/kotlinx/js/AbortController.kt
@@ -0,0 +1,20 @@
+package kotlinx.js
+
+external interface AbortController {
+    val signal: AbortSignal
+    fun abort()
+}
+
+// CONSTRUCTOR EMULATION START
+
+@JsName("AbortController")
+private external class AbortControllerImpl : AbortController {
+    override val signal: AbortSignal
+    override fun abort()
+
+}
+
+fun AbortController(): AbortController =
+    AbortControllerImpl()
+
+// CONSTRUCTOR EMULATION END
