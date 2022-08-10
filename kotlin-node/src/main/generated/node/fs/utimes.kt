aa76a13a22f1edf1df7c03a3af8fd2f5d4d9a836
Victor Turansky <victor.turansky@gmail.com>
2022-08-10 17:32:14 +0300

    Node. Use `TimeLike` alias [generated] (#1678)

--- kotlin-node/src/main/generated/node/fs/utimes.kt
+++ kotlin-node/src/main/generated/node/fs/utimes.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.await
 
 suspend fun utimes(
     path: PathLike,
-    atime: Any, /* string | number | Date */
-    mtime: Any, /* string | number | Date */
+    atime: TimeLike,
+    mtime: TimeLike,
 ) {
     utimesAsync(
         path = path,
