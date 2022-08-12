e84b678cdda7e85f1c12a4b7a26fa76aaa527546
Victor Turansky <victor.turansky@gmail.com>
2022-08-12 17:33:44 +0300

    Node. `events` interface [generated] (#1697)

---
+++ kotlin-node/src/main/generated/node/events/DOMEventTarget.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+package node.events
+
+sealed external interface DOMEventTarget {
+
+    // HIDDEN METHOD START
+    /*
+    addEventListener(
+        eventName: string,
+        listener: (...args: any[]) => void,
+        opts?: {
+            once: boolean;
+        }
+    ): any
+    */
+    // HIDDEN METHOD END
+
+}
