958570258f09b5f682c2e51f781a21a410622b71
Victor Turansky <victor.turansky@gmail.com>
2022-08-13 17:31:32 +0300

    JS. Suspend `fetch` (#1700)

--- kotlin-js/src/main/kotlin/kotlinx/js/fetchAsync.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/fetchAsync.kt
@@ -6,6 +6,12 @@ import kotlin.js.Promise
 
 @JsName("fetch")
 external fun fetchAsync(
+    input: Request,
+    init: RequestInit = definedExternally,
+): Promise<Response>
+
+@JsName("fetch")
+external fun fetchAsync(
     input: String,
     init: RequestInit = definedExternally,
 ): Promise<Response>
@@ -13,11 +19,5 @@ external fun fetchAsync(
 @JsName("fetch")
 external fun fetchAsync(
     input: URL,
-    init: RequestInit = definedExternally,
-): Promise<Response>
-
-@JsName("fetch")
-external fun fetchAsync(
-    input: Request,
     init: RequestInit = definedExternally,
 ): Promise<Response>
