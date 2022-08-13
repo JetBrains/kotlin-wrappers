b6e16c9c0c92f9d5f29b26552b21041d1c656cd4
Victor Turansky <victor.turansky@gmail.com>
2022-08-13 14:21:08 +0300

    JS. `fetchAsync`

--- kotlin-js/src/main/kotlin/kotlinx/js/fetchAsync.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/fetchAsync.kt
@@ -1,21 +1,23 @@
 package kotlinx.js
 
 import org.w3c.dom.url.URL
+import org.w3c.fetch.Response
+import kotlin.js.Promise
 
 @JsName("fetch")
 external fun fetchAsync(
     input: String,
     init: RequestInit = definedExternally,
-)
+): Promise<Response>
 
 @JsName("fetch")
 external fun fetchAsync(
     input: URL,
     init: RequestInit = definedExternally,
-)
+): Promise<Response>
 
 @JsName("fetch")
 external fun fetchAsync(
     input: Request,
     init: RequestInit = definedExternally,
-)
+): Promise<Response>
