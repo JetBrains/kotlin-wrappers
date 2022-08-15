f0f61d884b6c011683573af5fe46b9f3650d16df
Victor Turansky <victor.turansky@gmail.com>
2022-08-15 20:11:11 +0300

    JS. Unique symbols (#1710)

--- kotlin-js/src/main/kotlin/kotlinx/js/Symbol.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/Symbol.kt
@@ -6,22 +6,23 @@ package kotlinx.js
 
 sealed external interface Symbol {
     val description: String?
+        get() = definedExternally
+
+    object asyncIterator : Symbol
+    object hasInstance : Symbol
+    object isConcatSpreadable : Symbol
+    object iterator : Symbol
+    object match : Symbol
+    object matchAll : Symbol
+    object replace : Symbol
+    object search : Symbol
+    object species : Symbol
+    object split : Symbol
+    object toPrimitive : Symbol
+    object toStringTag : Symbol
+    object unscopables : Symbol
 
     companion object {
-        val asyncIterator: Symbol
-        val hasInstance: Symbol
-        val isConcatSpreadable: Symbol
-        val iterator: Symbol
-        val match: Symbol
-        val matchAll: Symbol
-        val replace: Symbol
-        val search: Symbol
-        val species: Symbol
-        val split: Symbol
-        val toPrimitive: Symbol
-        val toStringTag: Symbol
-        val unscopables: Symbol
-
         fun `for`(key: String): Symbol
 
         fun keyFor(symbol: Symbol): String?
