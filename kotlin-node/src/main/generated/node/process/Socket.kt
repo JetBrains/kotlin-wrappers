c2819382af9823bd7efd4b02583e30cc3b56a7d8
Victor Turansky <victor.turansky@gmail.com>
2022-08-17 04:15:19 +0300

    Node. Non-sealed `ReadWriteStream` [generated] (#1716)

--- kotlin-node/src/main/generated/node/process/Socket.kt
+++ kotlin-node/src/main/generated/node/process/Socket.kt
@@ -2,6 +2,6 @@
 
 package node.process
 
-sealed external interface Socket /* : ReadWriteStream */ {
+sealed external interface Socket : node.ReadWriteStream {
     var isTTY: Boolean? /* true */
 }
