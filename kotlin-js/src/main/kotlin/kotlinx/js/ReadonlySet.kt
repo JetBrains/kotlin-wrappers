a484653323f4a2142514109dea79c9b352ddc4ff
Victor Turansky <victor.turansky@gmail.com>
2022-08-22 01:14:23 +0300

    JS. `ReadonlySet` (#1744)

---
+++ kotlin-js/src/main/kotlin/kotlinx/js/ReadonlySet.kt
@@ -0,0 +1,13 @@
+package kotlinx.js
+
+external interface ReadonlySet<T> :
+    HasSymbol.iterator<() -> JsIterable.Iterator<T>> {
+
+    val size: Int
+
+    fun entries(): JsIterable.Iterator<JsPair<T, T>>
+    fun forEach(action: (value: T) -> Unit)
+    fun has(value: T): Boolean
+    fun keys(): JsIterable.Iterator<T>
+    fun values(): JsIterable.Iterator<T>
+}
