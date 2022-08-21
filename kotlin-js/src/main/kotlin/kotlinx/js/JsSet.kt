578d69c67f74e6af71b75043d98bd1a7ef7aa995
Victor Turansky <victor.turansky@gmail.com>
2022-08-22 01:13:55 +0300

    JS. `ReadonlySet`

--- kotlin-js/src/main/kotlin/kotlinx/js/JsSet.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/JsSet.kt
@@ -1,17 +1,18 @@
 package kotlinx.js
 
 @JsName("Set")
-external class JsSet<T> {
+external class JsSet<T> : ReadonlySet<T> {
     constructor()
     constructor(values: JsIterator<T>?)
     constructor(values: ReadonlyArray<T>?)
 
-    val size: Int
+    override val size: Int
     fun add(value: T): JsSet<T>
     fun clear()
     fun delete(value: T)
-    fun entries(): JsIterator<JsPair<T, T>>
-    fun has(value: T): Boolean
-    fun forEach(action: (value: T, key: T) -> Unit)
-    fun values(): JsIterator<T>
+    override fun entries(): JsIterable.Iterator<JsPair<T, T>>
+    override fun has(value: T): Boolean
+    override fun forEach(action: (value: T) -> Unit)
+    override fun keys(): JsIterable.Iterator<T>
+    override fun values(): JsIterable.Iterator<T>
 }
