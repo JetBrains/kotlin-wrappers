3ac4673e8d3db58b8847b1b8857cc96b1382f152
Victor Turansky <victor.turansky@gmail.com>
2022-08-26 21:58:44 +0300

    Node. `KeyFormat` and `KeyType` child types [generated]

--- kotlin-node/src/main/generated/node/crypto/KeyFormat.kt
+++ kotlin-node/src/main/generated/node/crypto/KeyFormat.kt
@@ -2,15 +2,14 @@
 
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package node.crypto
 
 // language=JavaScript
 @JsName("""(/*union*/{pem: 'pem', der: 'der'}/*union*/)""")
-external enum class KeyFormat {
-    pem,
-    der,
-
-    ;
+sealed external interface KeyFormat {
+    object pem : KeyFormat
+    object der : KeyFormat
 }
