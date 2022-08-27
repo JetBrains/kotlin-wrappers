d8b7b39beb1663b935bf08ac7656361b5cc2dbee
Victor Turansky <victor.turansky@gmail.com>
2022-08-27 13:12:02 +0300

    Node. `KeyFormat` and `KeyType` child types [generated] (#1758)

--- kotlin-node/src/main/generated/node/crypto/KeyType.kt
+++ kotlin-node/src/main/generated/node/crypto/KeyType.kt
@@ -2,21 +2,20 @@
 
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package node.crypto
 
 // language=JavaScript
 @JsName("""(/*union*/{rsa: 'rsa', rsaPss: 'rsa-pss', dsa: 'dsa', ec: 'ec', ed25519: 'ed25519', ed448: 'ed448', x25519: 'x25519', x448: 'x448'}/*union*/)""")
-external enum class KeyType {
-    rsa,
-    rsaPss,
-    dsa,
-    ec,
-    ed25519,
-    ed448,
-    x25519,
-    x448,
-
-    ;
+sealed external interface KeyType {
+    object rsa : KeyType
+    object rsaPss : KeyType
+    object dsa : KeyType
+    object ec : KeyType
+    object ed25519 : KeyType
+    object ed448 : KeyType
+    object x25519 : KeyType
+    object x448 : KeyType
 }
