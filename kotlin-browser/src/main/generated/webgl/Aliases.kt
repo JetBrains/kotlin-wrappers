0296334fda3a70b8b7bf1e8f18040fd2159365df
Victor Turansky <victor.turansky@gmail.com>
2022-08-28 18:16:36 +0300

    WebGL. Aliases [generated] (#1768)

---
+++ kotlin-browser/src/main/generated/webgl/Aliases.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+typealias TexImageSource = org.khronos.webgl.TexImageSource
+typealias WebGLActiveInfo = org.khronos.webgl.WebGLActiveInfo
+typealias WebGLBuffer = org.khronos.webgl.WebGLBuffer
+typealias WebGLContextAttributes = org.khronos.webgl.WebGLContextAttributes
+typealias WebGLContextEvent = org.khronos.webgl.WebGLContextEvent
+typealias WebGLContextEventInit = org.khronos.webgl.WebGLContextEventInit
+typealias WebGLFramebuffer = org.khronos.webgl.WebGLFramebuffer
+typealias WebGLObject = org.khronos.webgl.WebGLObject
+typealias WebGLProgram = org.khronos.webgl.WebGLProgram
+typealias WebGLRenderbuffer = org.khronos.webgl.WebGLRenderbuffer
+typealias WebGLRenderingContext = org.khronos.webgl.WebGLRenderingContext
+typealias WebGLRenderingContextBase = org.khronos.webgl.WebGLRenderingContextBase
+typealias WebGLShader = org.khronos.webgl.WebGLShader
+typealias WebGLShaderPrecisionFormat = org.khronos.webgl.WebGLShaderPrecisionFormat
+typealias WebGLTexture = org.khronos.webgl.WebGLTexture
+typealias WebGLUniformLocation = org.khronos.webgl.WebGLUniformLocation
