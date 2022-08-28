6a4b1edc634539ba42e10ba94b0bd529b398fe6d
Victor Turansky <victor.turansky@gmail.com>
2022-08-28 14:09:08 +0300

    JS. Cleanup `AsyncIterator`

--- kotlin-js/src/main/kotlin/kotlinx/js/AsyncIterator.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/AsyncIterator.kt
@@ -2,8 +2,8 @@ package kotlinx.js
 
 import kotlin.js.Promise
 
-sealed external interface AsyncIterator<out T, TReturn> {
-    val next: (/* ...args: [] | [TNext] */) -> Promise<JsIterator.Result<T, TReturn>>
-    val `return`: ((value: Promise<TReturn>?) -> Promise<JsIterator.Result<T, TReturn>>)?
-    val `throw`: ((e: Throwable?) -> Promise<JsIterator.Result<T, TReturn>>)?
+sealed external interface AsyncIterator<out T> {
+    val next: (/* ...args: [] | [TNext] */) -> Promise<JsIterator.Result<T, Void>>
+    val `return`: ((value: PromiseResult<Void>?) -> Promise<JsIterator.Result<T, Void>>)?
+    val `throw`: ((e: Throwable?) -> Promise<JsIterator.Result<T, Void>>)?
 }
