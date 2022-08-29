af483d2319a3b9506ff8ea304d0c1275599623f6
Victor Turansky <victor.turansky@gmail.com>
2022-08-29 18:56:45 +0300

    JS. Fix `JsIterator.iterator()`

--- kotlin-js/src/main/kotlin/kotlinx/js/JsIterator.iterator.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/JsIterator.iterator.kt
@@ -2,12 +2,14 @@ package kotlinx.js
 
 operator fun <T> JsIterator<T>.iterator(): Iterator<T> =
     iterator {
-        val result = next()
-        if (result.done) {
+        var result = next()
+        while (!result.done) {
             val value = result
                 .unsafeCast<JsIterator.YieldResult<T>>()
                 .value
 
             yield(value)
+
+            result = next()
         }
     }
