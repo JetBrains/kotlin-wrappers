9cd4808e958b6cfa50013e5d556dec432e55af07
Victor Turansky <victor.turansky@gmail.com>
2022-09-01 12:25:06 +0300

    React. `setRef` helper

--- kotlin-react-core/src/main/kotlin/react/setRef.kt
+++ kotlin-react-core/src/main/kotlin/react/setRef.kt
@@ -1,5 +1,8 @@
 package react
 
+/**
+ * [Original](https://github.com/mui/material-ui/blob/f0f33c6038f874d2f3ecc251a8ca5bc640424992/packages/mui-utils/src/setRef.ts#L16
+ */
 fun <T : Any> setRef(
     ref: Ref<T>?,
     value: T?,
