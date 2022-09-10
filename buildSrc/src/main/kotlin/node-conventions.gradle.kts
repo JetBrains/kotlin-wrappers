b48e497878706e7bb5942f441ce598a3a567ae4f
Victor Turansky <victor.turansky@gmail.com>
2022-09-10 20:03:56 +0400

    Node runtime `18.9.0`

--- buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/node-conventions.gradle.kts
@@ -13,6 +13,6 @@ plugins.withType<YarnPlugin> {
 
 plugins.withType<NodeJsRootPlugin> {
     the<NodeJsRootExtension>().apply {
-        nodeVersion = "16.17.0"
+        nodeVersion = "18.9.0"
     }
 }
