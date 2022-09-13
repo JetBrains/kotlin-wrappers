d3c76cc0d80645be6c30ae5ec366d0faf11424ca
Victor Turansky <victor.turansky@gmail.com>
2022-09-13 22:33:33 +0300

    CSSType. `MathDepth` [generated] (#1786)

--- kotlin-csstype/src/main/generated/csstype/MathDepth.kt
+++ kotlin-csstype/src/main/generated/csstype/MathDepth.kt
@@ -1,6 +1,16 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package csstype
 
-// Globals | "auto-add" | (string & {}) | (number & {})
-sealed external interface MathDepth
+// language=JavaScript
+@JsName("""(/*union*/{autoAdd: 'auto-add'}/*union*/)""")
+sealed external interface MathDepth {
+    companion object {
+        val autoAdd: MathDepth
+    }
+}
