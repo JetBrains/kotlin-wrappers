e1b7afe4ff5438f7798c7bf42bd8ba6b80556f32
Victor Turansky <victor.turansky@gmail.com>
2022-09-17 05:04:51 +0300

    JS. `JsArray.from`

--- kotlin-js/src/main/kotlin/kotlinx/JsArray.kt
+++ kotlin-js/src/main/kotlin/kotlinx/JsArray.kt
@@ -1,4 +1,12 @@
 package kotlinx
 
+import kotlinx.js.JsIterable
+import kotlinx.js.JsIterator
+
 @JsName("Array")
-sealed external class JsArray
+sealed external class JsArray {
+    companion object {
+        fun <T> from(source: JsIterator<T>): Array<T>
+        fun <T> from(source: JsIterable<T>): Array<T>
+    }
+}
