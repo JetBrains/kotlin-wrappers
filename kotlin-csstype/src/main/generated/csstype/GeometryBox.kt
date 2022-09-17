14d68aa584363422f2332865ef0438f2bfb8557f
Victor Turansky <victor.turansky@gmail.com>
2022-09-18 01:54:15 +0300

    CSSType. `BasicShape` types [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/GeometryBox.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package csstype
+
+// language=JavaScript
+@JsName("""(/*union*/{borderBox: 'border-box', paddingBox: 'padding-box', contentBox: 'content-box', marginBox: 'margin-box'}/*union*/)""")
+external enum class GeometryBox : BasicShape {
+    borderBox,
+    paddingBox,
+    contentBox,
+    marginBox,
+
+    ;
+}
