adc09391824fb9baa486238d8dd545751eabe55f
Victor Turansky <victor.turansky@gmail.com>
2022-09-18 15:28:43 +0300

    CSSType. `inset` function [generated]

---
+++ kotlin-csstype/src/main/generated/csstype/Shape.functions.kt
@@ -0,0 +1,62 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package csstype
+
+inline fun inset(
+    vertical: Length,
+    horizontal: Length,
+): BasicShape =
+    "inset($vertical $horizontal)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    top: Length,
+    horizontal: Length,
+    bottom: Length,
+): BasicShape =
+    "inset($top $horizontal $bottom)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    top: Length,
+    right: Length,
+    bottom: Length,
+    left: Length,
+): BasicShape =
+    "inset($top $right $bottom $left)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    value: Length,
+    round: Round,
+    borderRadius: Length,
+): BasicShape =
+    "inset($value $round $borderRadius)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    vertical: Length,
+    horizontal: Length,
+    round: Round,
+    borderRadius: Length,
+): BasicShape =
+    "inset($vertical $horizontal $round $borderRadius)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    top: Length,
+    horizontal: Length,
+    bottom: Length,
+    round: Round,
+    borderRadius: Length,
+): BasicShape =
+    "inset($top $horizontal $bottom $round $borderRadius)".unsafeCast<BasicShape>()
+
+inline fun inset(
+    top: Length,
+    right: Length,
+    bottom: Length,
+    left: Length,
+    round: Round,
+    borderRadius: Length,
+): BasicShape =
+    "inset($top $right $bottom $left $round $borderRadius)".unsafeCast<BasicShape>()
