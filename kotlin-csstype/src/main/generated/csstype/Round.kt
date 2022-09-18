ac26c3debc982792996c7a5a25e88bfcf24d4b16
Victor Turansky <victor.turansky@gmail.com>
2022-09-18 15:47:46 +0300

    CSSType. `inset` function [generated] (#1800)

---
+++ kotlin-csstype/src/main/generated/csstype/Round.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package csstype
+
+// language=JavaScript
+@JsName("""(/*union*/{round: 'round'}/*union*/)""")
+external enum class Round {
+    round,
+
+    ;
+}
