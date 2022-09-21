9437e25cd17fc9d78efb18e58d950a880cecf2d7
Victor Turansky <victor.turansky@gmail.com>
2022-09-21 11:58:31 +0300

    JS. `get` operator for typed arrays (#1807)

--- kotlin-js/src/main/kotlin/kotlinx/js/TypedArray.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/TypedArray.kt
@@ -11,3 +11,13 @@ typealias Uint16Array = org.khronos.webgl.Uint16Array
 typealias Uint32Array = org.khronos.webgl.Uint32Array
 typealias Uint8Array = org.khronos.webgl.Uint8Array
 typealias Uint8ClampedArray = org.khronos.webgl.Uint8ClampedArray
+
+inline operator fun Float32Array.get(index: Int): Float = asDynamic()[index]
+inline operator fun Float64Array.get(index: Int): Double = asDynamic()[index]
+inline operator fun Int16Array.get(index: Int): Short = asDynamic()[index]
+inline operator fun Int32Array.get(index: Int): Int = asDynamic()[index]
+inline operator fun Int8Array.get(index: Int): Byte = asDynamic()[index]
+inline operator fun Uint16Array.get(index: Int): Short = asDynamic()[index]
+inline operator fun Uint32Array.get(index: Int): Int = asDynamic()[index]
+inline operator fun Uint8Array.get(index: Int): Byte = asDynamic()[index]
+inline operator fun Uint8ClampedArray.get(index: Int): Byte = asDynamic()[index]
