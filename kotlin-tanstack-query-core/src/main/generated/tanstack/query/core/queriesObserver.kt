4f829e93b646cee13ff609b56e50d36e34eb286b
Victor Turansky <victor.turansky@gmail.com>
2022-09-25 20:23:23 +0300

    Tanstack Query `4.6.0`

--- kotlin-tanstack-query-core/src/main/generated/tanstack/query/core/queriesObserver.kt
+++ kotlin-tanstack-query-core/src/main/generated/tanstack/query/core/queriesObserver.kt
@@ -26,5 +26,6 @@ open external class QueriesObserver(
     )
 
     open fun getCurrentResult(): ReadonlyArray<QueryObserverResult<*, *>>
+    open fun getQueries(): ReadonlyArray<Query<*, *, *, QueryKey>>
     open fun getOptimisticResult(queries: ReadonlyArray<QueryObserverOptions<*, *, *, *, *>>): ReadonlyArray<QueryObserverResult<*, *>>
 }
---
+++ kotlin-tanstack-react-query/src/main/generated/tanstack/react/query/useSyncExternalStore.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@tanstack/react-query")
+@file:JsNonModule
+
+package tanstack.react.query
+
+external val useSyncExternalStore: Any /* typeof uSES */
