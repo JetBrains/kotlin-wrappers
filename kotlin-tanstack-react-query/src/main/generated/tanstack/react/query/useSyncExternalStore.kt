a25e0d180aa33621c7224734ec4f7f429ce0307c
Victor Turansky <victor.turansky@gmail.com>
2022-09-25 22:48:18 +0300

    Tanstack Query. Remove `useSyncExternalStore` shim [generated]

--- kotlin-tanstack-react-query/src/main/generated/tanstack/react/query/useSyncExternalStore.kt
+++
@@ -1,8 +0,0 @@
-// Automatically generated - do not modify!
-
-@file:JsModule("@tanstack/react-query")
-@file:JsNonModule
-
-package tanstack.react.query
-
-external val useSyncExternalStore: Any /* typeof uSES */
