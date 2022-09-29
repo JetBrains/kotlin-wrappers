a98449f5e18e683e40932937aa2283716504541a
Victor Turansky <victor.turansky@gmail.com>
2022-09-30 02:09:24 +0300

    Browser. `SecurityPolicyViolationEvent` types removed

--- kotlin-browser/src/main/generated/org/w3c/dom/events/SecurityPolicyViolationEvent.types.kt
+++
@@ -1,9 +0,0 @@
-// Automatically generated - do not modify!
-
-package org.w3c.dom.events
-
-import web.events.EventType
-import org.w3c.dom.events.Event as SecurityPolicyViolationEvent
-
-inline val SecurityPolicyViolationEvent.Companion.SECURITY_POLICY_VIOLATION: EventType<SecurityPolicyViolationEvent>
-    get() = EventType("securitypolicyviolation")
