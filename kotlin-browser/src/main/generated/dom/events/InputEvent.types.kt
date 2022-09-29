c2f2cae830e4831b09aeaab276af8885c34e7f1b
Victor Turansky <victor.turansky@gmail.com>
2022-09-30 02:00:33 +0300

    DOM. `InputEvent` and `SubmitEvent` types

--- kotlin-browser/src/main/generated/dom/events/InputEvent.types.kt
+++ kotlin-browser/src/main/generated/dom/events/InputEvent.types.kt
@@ -1,7 +1,8 @@
 // Automatically generated - do not modify!
 
-package org.w3c.dom.events
+package dom.events
 
+import org.w3c.dom.events.InputEvent
 import web.events.EventType
 
 inline val InputEvent.Companion.BEFORE_INPUT: EventType<InputEvent>
--- kotlin-browser/src/main/generated/dom/events/SubmitEvent.types.kt
+++ kotlin-browser/src/main/generated/dom/events/SubmitEvent.types.kt
@@ -1,6 +1,6 @@
 // Automatically generated - do not modify!
 
-package org.w3c.dom.events
+package dom.events
 
 import web.events.EventType
 import org.w3c.dom.events.Event as SubmitEvent
