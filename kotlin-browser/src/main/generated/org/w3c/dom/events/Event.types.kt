8c3a4f780066cc577858bc2275a2fe2e0b8f089f
Victor Turansky <victor.turansky@gmail.com>
2022-09-30 02:35:15 +0300

    WebRTC. More event types

--- kotlin-browser/src/main/generated/org/w3c/dom/events/Event.types.kt
+++ kotlin-browser/src/main/generated/org/w3c/dom/events/Event.types.kt
@@ -229,27 +229,6 @@ inline val Event.Companion.BUFFERED_AMOUNT_LOW: EventType<Event>
 inline val Event.Companion.CLOSING: EventType<Event>
     get() = EventType("closing")
 
-inline val Event.Companion.GATHERING_STATE_CHANGE: EventType<Event>
-    get() = EventType("gatheringstatechange")
-
-inline val Event.Companion.CONNECTION_STATE_CHANGE: EventType<Event>
-    get() = EventType("connectionstatechange")
-
-inline val Event.Companion.ICE_CANDIDATE_ERROR: EventType<Event>
-    get() = EventType("icecandidateerror")
-
-inline val Event.Companion.ICE_CONNECTION_STATE_CHANGE: EventType<Event>
-    get() = EventType("iceconnectionstatechange")
-
-inline val Event.Companion.ICE_GATHERING_STATE_CHANGE: EventType<Event>
-    get() = EventType("icegatheringstatechange")
-
-inline val Event.Companion.NEGOTIATION_NEEDED: EventType<Event>
-    get() = EventType("negotiationneeded")
-
-inline val Event.Companion.SIGNALING_STATE_CHANGE: EventType<Event>
-    get() = EventType("signalingstatechange")
-
 inline val Event.Companion.CONNECT: EventType<Event>
     get() = EventType("connect")
 
---
+++ kotlin-browser/src/main/generated/webrtc/Event.types.kt
@@ -0,0 +1,27 @@
+// Automatically generated - do not modify!
+
+package webrtc
+
+import org.w3c.dom.events.Event
+import web.events.EventType
+
+inline val Event.Companion.GATHERING_STATE_CHANGE: EventType<Event>
+    get() = EventType("gatheringstatechange")
+
+inline val Event.Companion.CONNECTION_STATE_CHANGE: EventType<Event>
+    get() = EventType("connectionstatechange")
+
+inline val Event.Companion.ICE_CANDIDATE_ERROR: EventType<Event>
+    get() = EventType("icecandidateerror")
+
+inline val Event.Companion.ICE_CONNECTION_STATE_CHANGE: EventType<Event>
+    get() = EventType("iceconnectionstatechange")
+
+inline val Event.Companion.ICE_GATHERING_STATE_CHANGE: EventType<Event>
+    get() = EventType("icegatheringstatechange")
+
+inline val Event.Companion.NEGOTIATION_NEEDED: EventType<Event>
+    get() = EventType("negotiationneeded")
+
+inline val Event.Companion.SIGNALING_STATE_CHANGE: EventType<Event>
+    get() = EventType("signalingstatechange")
