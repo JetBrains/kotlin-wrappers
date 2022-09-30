8c2084ef42731469267c401ed0b83c5d44efd8e0
Victor Turansky <victor.turansky@gmail.com>
2022-09-30 13:57:38 +0300

    Browser. `PopStateEvent`

---
+++ kotlin-browser/src/main/kotlin/browser/events/PopStateEvent.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package browser.events
+
+
+typealias PopStateEventInit = org.w3c.dom.PopStateEventInit
+
+typealias PopStateEvent = org.w3c.dom.PopStateEvent
