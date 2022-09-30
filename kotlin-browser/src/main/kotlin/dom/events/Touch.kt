8385be1132862bdfb99ec4b329963b2eeed6f10f
Victor Turansky <victor.turansky@gmail.com>
2022-10-01 00:35:00 +0300

    DOM. `TouchList.get`

---
+++ kotlin-browser/src/main/kotlin/dom/events/Touch.kt
@@ -0,0 +1,3 @@
+package dom.events
+
+typealias Touch = org.w3c.dom.Touch
--- kotlin-browser/src/main/kotlin/dom/events/TouchList.kt
+++ kotlin-browser/src/main/kotlin/dom/events/TouchList.kt
@@ -1,5 +1,8 @@
-// Automatically generated - do not modify!
-
 package dom.events
 
 typealias TouchList = org.w3c.dom.TouchList
+
+inline operator fun TouchList.get(
+    index: Int,
+): Touch? =
+    asDynamic()[index]
