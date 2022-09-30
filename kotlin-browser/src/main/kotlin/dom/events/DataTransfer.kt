1e2daa13633368d8d2a8cf8b845309a97a9fdfca
Victor Turansky <victor.turansky@gmail.com>
2022-09-30 23:50:33 +0300

    DOM. `DataTransfer` classes

---
+++ kotlin-browser/src/main/kotlin/dom/events/DataTransfer.kt
@@ -0,0 +1,5 @@
+package dom.events
+
+typealias DataTransfer = org.w3c.dom.DataTransfer
+typealias DataTransferItem = org.w3c.dom.DataTransferItem
+typealias DataTransferItemList = org.w3c.dom.DataTransferItemList
