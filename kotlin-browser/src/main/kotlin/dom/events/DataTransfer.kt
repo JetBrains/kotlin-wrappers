44806fc364044ddd0951cfa97a4e70ba31914bc4
Victor Turansky <victor.turansky@gmail.com>
2022-10-01 00:46:24 +0300

    DOM. `DataTransferItemList.get`

--- kotlin-browser/src/main/kotlin/dom/events/DataTransfer.kt
+++ kotlin-browser/src/main/kotlin/dom/events/DataTransfer.kt
@@ -3,3 +3,8 @@ package dom.events
 typealias DataTransfer = org.w3c.dom.DataTransfer
 typealias DataTransferItem = org.w3c.dom.DataTransferItem
 typealias DataTransferItemList = org.w3c.dom.DataTransferItemList
+
+inline operator fun DataTransferItemList.get(
+    index: Int,
+): DataTransferItem? =
+    asDynamic()[index]
