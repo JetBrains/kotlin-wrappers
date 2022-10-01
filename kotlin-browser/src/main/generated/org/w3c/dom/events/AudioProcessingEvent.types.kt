50f3397339dd392277093e1baf973c4a89198b01
Victor Turansky <victor.turansky@gmail.com>
2022-10-01 21:56:01 +0300

    `AudioProcessingEvent` types removed

--- kotlin-browser/src/main/generated/org/w3c/dom/events/AudioProcessingEvent.types.kt
+++
@@ -1,9 +0,0 @@
-// Automatically generated - do not modify!
-
-package org.w3c.dom.events
-
-import web.events.EventType
-import org.w3c.dom.events.Event as AudioProcessingEvent
-
-inline val AudioProcessingEvent.Companion.AUDIO_PROCESS: EventType<AudioProcessingEvent>
-    get() = EventType("audioprocess")
