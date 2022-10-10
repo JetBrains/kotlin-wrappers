8d624544af587551f96b645fb095b4c23d8176a9
Victor Turansky <victor.turansky@gmail.com>
2022-10-10 07:20:40 +0300

    Web. `AbortSignal` factories (#1833)

--- kotlin-web/src/main/kotlin/web/abort/AbortSignal.kt
+++ kotlin-web/src/main/kotlin/web/abort/AbortSignal.kt
@@ -6,4 +6,16 @@ import web.events.EventTarget
 sealed external class AbortSignal : EventTarget {
     val aborted: Boolean
     var onabort: ((event: Event) -> Unit)?
+    val reason: Any?
+    fun throwIfAborted()
+
+    companion object {
+        fun abort(
+            reason: Any? = definedExternally,
+        ): AbortSignal
+
+        fun timeout(
+            ms: Int,
+        ): AbortSignal
+    }
 }
