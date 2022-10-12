2188822df10bbd332e96650b881392a1b1572c8a
Victor Turansky <victor.turansky@gmail.com>
2022-10-12 13:07:15 +0300

    Cesium. Cleanup [generated]

--- kotlin-cesium/src/main/generated/cesium/requestAnimationFrameCallback.kt
+++
@@ -1,9 +0,0 @@
-// Automatically generated - do not modify!
-
-package cesium
-
-/**
- * A function that will be called when the next frame should be drawn.
- * @param [timestamp] A timestamp for the frame, in milliseconds.
- */
-typealias requestAnimationFrameCallback = (timestamp: Double) -> Unit
