a286d6d47d73e6c10282721ebb4098ccf8204fee
Victor Turansky <victor.turansky@gmail.com>
2022-10-14 10:35:49 +0300

    React Query. Redundant `unstable_batchedUpdates` removed [generated]

--- kotlin-tanstack-react-query/src/main/generated/tanstack/react/query/reactBatchedUpdates.kt
+++
@@ -1,8 +0,0 @@
-// Automatically generated - do not modify!
-
-@file:JsModule("@tanstack/react-query")
-@file:JsNonModule
-
-package tanstack.react.query
-
-external val unstable_batchedUpdates: Any /* typeof ReactDOM.unstable_batchedUpdates */
