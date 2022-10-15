b45cc3361e8ffb1a6de3963ac3b063bd8ccb8ad3
porotkin <preload737773@gmail.com>
2022-10-15 22:55:28 +0400

    Tanstack Query Devtools. Subprojects

---
+++ kotlin-tanstack-react-query-devtools/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("js")
+    `kotlin-conventions`
+    `publish-conventions`
+}
+
+dependencies {
+    api(project(":kotlin-react-core"))
+    api(project(":kotlin-tanstack-query-core"))
+
+    api(npmv("@tanstack/react-query-devtools"))
+}
