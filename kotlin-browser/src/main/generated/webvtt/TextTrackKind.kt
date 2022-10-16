27ac774ba6afc177c6159b12ef0245c6ca00bbaa
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 22:55:32 +0300

    WebVTT. Unions [generated]

---
+++ kotlin-browser/src/main/generated/webvtt/TextTrackKind.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package webvtt
+
+// language=JavaScript
+@JsName("""(/*union*/{captions: 'captions', chapters: 'chapters', descriptions: 'descriptions', metadata: 'metadata', subtitles: 'subtitles'}/*union*/)""")
+external enum class TextTrackKind {
+    captions,
+    chapters,
+    descriptions,
+    metadata,
+    subtitles,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/webvtt/TextTrackMode.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package webvtt
+
+// language=JavaScript
+@JsName("""(/*union*/{disabled: 'disabled', hidden: 'hidden', showing: 'showing'}/*union*/)""")
+external enum class TextTrackMode {
+    disabled,
+    hidden,
+    showing,
+
+    ;
+}
