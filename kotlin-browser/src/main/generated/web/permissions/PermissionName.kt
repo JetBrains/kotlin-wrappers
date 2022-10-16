af2d98901430f6c6428049094312c9c8e282ed23
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 22:56:26 +0300

    Permissions. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/permissions/PermissionName.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.permissions
+
+// language=JavaScript
+@JsName("""(/*union*/{geolocation: 'geolocation', notifications: 'notifications', persistentStorage: 'persistent-storage', push: 'push', screenWakeLock: 'screen-wake-lock', xrSpatialTracking: 'xr-spatial-tracking'}/*union*/)""")
+external enum class PermissionName {
+    geolocation,
+    notifications,
+    persistentStorage,
+    push,
+    screenWakeLock,
+    xrSpatialTracking,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/web/permissions/PermissionState.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.permissions
+
+// language=JavaScript
+@JsName("""(/*union*/{denied: 'denied', granted: 'granted', prompt: 'prompt'}/*union*/)""")
+external enum class PermissionState {
+    denied,
+    granted,
+    prompt,
+
+    ;
+}
