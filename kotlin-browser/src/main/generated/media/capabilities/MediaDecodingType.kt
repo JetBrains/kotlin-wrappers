e10c40368364cfc6f376e8d04c656948ac90e0e1
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 02:57:58 +0300

    Media Capabilities. Unions [generated]

---
+++ kotlin-browser/src/main/generated/media/capabilities/MediaDecodingType.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package media.capabilities
+
+// language=JavaScript
+@JsName("""(/*union*/{file: 'file', mediaSource: 'media-source', webrtc: 'webrtc'}/*union*/)""")
+external enum class MediaDecodingType {
+    file,
+    mediaSource,
+    webrtc,
+
+    ;
+}
