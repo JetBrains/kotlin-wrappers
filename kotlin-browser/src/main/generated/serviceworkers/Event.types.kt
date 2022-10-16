841117c1c3e0f92ca7551ef8776039ffa257afc5
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:51:23 +0300

    Web Notifications. Event types [generated]

--- kotlin-browser/src/main/generated/serviceworkers/Event.types.kt
+++ kotlin-browser/src/main/generated/serviceworkers/Event.types.kt
@@ -5,20 +5,8 @@ package serviceworkers
 import org.w3c.dom.events.Event
 import web.events.EventType
 
-inline val Event.Companion.CLICK: EventType<Event>
-    get() = EventType("click")
-
-inline val Event.Companion.CLOSE: EventType<Event>
-    get() = EventType("close")
-
 inline val Event.Companion.CONTROLLER_CHANGE: EventType<Event>
     get() = EventType("controllerchange")
-
-inline val Event.Companion.ERROR: EventType<Event>
-    get() = EventType("error")
-
-inline val Event.Companion.SHOW: EventType<Event>
-    get() = EventType("show")
 
 inline val Event.Companion.STATE_CHANGE: EventType<Event>
     get() = EventType("statechange")
---
+++ kotlin-browser/src/main/generated/web/notifications/Event.types.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package web.notifications
+
+import org.w3c.dom.events.Event
+import web.events.EventType
+
+inline val Event.Companion.CLICK: EventType<Event>
+    get() = EventType("click")
+
+inline val Event.Companion.CLOSE: EventType<Event>
+    get() = EventType("close")
+
+inline val Event.Companion.ERROR: EventType<Event>
+    get() = EventType("error")
+
+inline val Event.Companion.SHOW: EventType<Event>
+    get() = EventType("show")
