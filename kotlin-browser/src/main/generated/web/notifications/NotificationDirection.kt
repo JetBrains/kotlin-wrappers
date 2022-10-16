6e99e5095a6ed73a01aa39c8effe62d7f8010798
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:54:49 +0300

    Web Notifications. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/notifications/NotificationDirection.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.notifications
+
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto', ltr: 'ltr', rtl: 'rtl'}/*union*/)""")
+external enum class NotificationDirection {
+    auto,
+    ltr,
+    rtl,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/web/notifications/NotificationPermission.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.notifications
+
+// language=JavaScript
+@JsName("""(/*union*/{default: 'default', denied: 'denied', granted: 'granted'}/*union*/)""")
+external enum class NotificationPermission {
+    default,
+    denied,
+    granted,
+
+    ;
+}
