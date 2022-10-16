b3e82b574ecf0d7edbe9b8840338b98d92a27b2b
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:46:54 +0300

    Web Animations. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/animations/AnimationPlayState.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.animations
+
+// language=JavaScript
+@JsName("""(/*union*/{finished: 'finished', idle: 'idle', paused: 'paused', running: 'running'}/*union*/)""")
+external enum class AnimationPlayState {
+    finished,
+    idle,
+    paused,
+    running,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/web/animations/AnimationReplaceState.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.animations
+
+// language=JavaScript
+@JsName("""(/*union*/{active: 'active', persisted: 'persisted', removed: 'removed'}/*union*/)""")
+external enum class AnimationReplaceState {
+    active,
+    persisted,
+    removed,
+
+    ;
+}
