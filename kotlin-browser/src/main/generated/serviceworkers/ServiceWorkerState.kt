b4977a6c4c426031ea98f209fd25362011afab9c
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 02:58:55 +0300

    Service Workers. Unions [generated]

---
+++ kotlin-browser/src/main/generated/serviceworkers/ServiceWorkerState.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package serviceworkers
+
+// language=JavaScript
+@JsName("""(/*union*/{activated: 'activated', activating: 'activating', installed: 'installed', installing: 'installing', parsed: 'parsed', redundant: 'redundant'}/*union*/)""")
+external enum class ServiceWorkerState {
+    activated,
+    activating,
+    installed,
+    installing,
+    parsed,
+    redundant,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/serviceworkers/ServiceWorkerUpdateViaCache.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package serviceworkers
+
+// language=JavaScript
+@JsName("""(/*union*/{all: 'all', imports: 'imports', none: 'none'}/*union*/)""")
+external enum class ServiceWorkerUpdateViaCache {
+    all,
+    imports,
+    none,
+
+    ;
+}
