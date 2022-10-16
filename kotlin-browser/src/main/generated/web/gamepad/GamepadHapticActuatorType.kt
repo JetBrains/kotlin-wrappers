f8b0d90b1380d6e86fe3e201abf324c70acec76f
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 22:54:03 +0300

    Gamepad. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/gamepad/GamepadHapticActuatorType.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.gamepad
+
+// language=JavaScript
+@JsName("""(/*union*/{vibration: 'vibration'}/*union*/)""")
+external enum class GamepadHapticActuatorType {
+    vibration,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/web/gamepad/GamepadMappingType.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.gamepad
+
+// language=JavaScript
+@JsName("""(/*union*/{none: '', standard: 'standard', xrStandard: 'xr-standard'}/*union*/)""")
+external enum class GamepadMappingType {
+    none,
+    standard,
+    xrStandard,
+
+    ;
+}
