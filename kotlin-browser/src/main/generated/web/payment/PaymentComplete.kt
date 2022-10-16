8978e10587c33a5298836a707e7dfc493aee6321
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 22:56:41 +0300

    Payment. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/payment/PaymentComplete.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.payment
+
+// language=JavaScript
+@JsName("""(/*union*/{fail: 'fail', success: 'success', unknown: 'unknown'}/*union*/)""")
+external enum class PaymentComplete {
+    fail,
+    success,
+    unknown,
+
+    ;
+}
