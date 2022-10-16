7620ad6a12a930ac9335251f7ce646d86622fbe8
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:45:39 +0300

    WebVTT. Unions [generated]

---
+++ kotlin-browser/src/main/generated/webvtt/AlignSetting.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package webvtt
+
+// language=JavaScript
+@JsName("""(/*union*/{center: 'center', end: 'end', left: 'left', right: 'right', start: 'start'}/*union*/)""")
+external enum class AlignSetting {
+    center,
+    end,
+    left,
+    right,
+    start,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/webvtt/AutoKeyword.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package webvtt
+
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto'}/*union*/)""")
+external enum class AutoKeyword {
+    auto,
+
+    ;
+}
