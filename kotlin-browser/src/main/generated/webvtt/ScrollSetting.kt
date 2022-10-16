015cff38362f35a46e9ec116fcfda1d7dae3be79
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 23:54:51 +0300

    WebVTT. Unions [generated]

---
+++ kotlin-browser/src/main/generated/webvtt/ScrollSetting.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package webvtt
+
+// language=JavaScript
+@JsName("""(/*union*/{none: '', up: 'up'}/*union*/)""")
+external enum class ScrollSetting {
+    none,
+    up,
+
+    ;
+}
