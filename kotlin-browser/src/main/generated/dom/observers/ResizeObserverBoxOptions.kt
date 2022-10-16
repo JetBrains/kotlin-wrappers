5d91083ed52f87890aac27f53d50b6db83cb5bd2
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:02:42 +0300

    DOM Observers. `ResizeObserverBoxOptions` [generated]

---
+++ kotlin-browser/src/main/generated/dom/observers/ResizeObserverBoxOptions.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.observers
+
+// language=JavaScript
+@JsName("""(/*union*/{borderBox: 'border-box', contentBox: 'content-box', devicePixelContentBox: 'device-pixel-content-box'}/*union*/)""")
+external enum class ResizeObserverBoxOptions {
+    borderBox,
+    contentBox,
+    devicePixelContentBox,
+
+    ;
+}
