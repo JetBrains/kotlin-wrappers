8c2c9a18f9882723d0aa3e0884423d4b05d367a6
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 00:03:36 +0300

    HTML. `CanPlayTypeResult` [generated]

---
+++ kotlin-browser/src/main/generated/dom/html/CanPlayTypeResult.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.html
+
+// language=JavaScript
+@JsName("""(/*union*/{none: '', maybe: 'maybe', probably: 'probably'}/*union*/)""")
+external enum class CanPlayTypeResult :
+/* legacy adapter */ org.w3c.dom.CanPlayTypeResult {
+    none,
+    maybe,
+    probably,
+
+    ;
+}
