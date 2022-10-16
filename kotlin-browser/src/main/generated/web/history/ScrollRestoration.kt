54a18b8f934389ee11a4081192b353f8ccd769bd
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 23:55:12 +0300

    History. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/history/ScrollRestoration.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.history
+
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto', manual: 'manual'}/*union*/)""")
+external enum class ScrollRestoration :
+/* legacy adapter */ org.w3c.dom.ScrollRestoration {
+    auto,
+    manual,
+
+    ;
+}
