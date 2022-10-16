3aa9c9244991bbd7e165ac34eca067c5764486a0
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 02:56:52 +0300

    DOM. `TouchType` [generated]

---
+++ kotlin-browser/src/main/generated/dom/events/TouchType.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.events
+
+// language=JavaScript
+@JsName("""(/*union*/{direct: 'direct', stylus: 'stylus'}/*union*/)""")
+external enum class TouchType {
+    direct,
+    stylus,
+
+    ;
+}
