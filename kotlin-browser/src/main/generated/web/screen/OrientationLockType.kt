b8e6fd38f5b080b9adfcba227a00eb65e410ed4d
Victor Turansky <victor.turansky@gmail.com>
2022-10-16 22:55:57 +0300

    Screen. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/screen/OrientationLockType.kt
@@ -0,0 +1,22 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.screen
+
+// language=JavaScript
+@JsName("""(/*union*/{any: 'any', landscape: 'landscape', landscapePrimary: 'landscape-primary', landscapeSecondary: 'landscape-secondary', natural: 'natural', portrait: 'portrait', portraitPrimary: 'portrait-primary', portraitSecondary: 'portrait-secondary'}/*union*/)""")
+external enum class OrientationLockType {
+    any,
+    landscape,
+    landscapePrimary,
+    landscapeSecondary,
+    natural,
+    portrait,
+    portraitPrimary,
+    portraitSecondary,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/web/screen/OrientationType.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.screen
+
+// language=JavaScript
+@JsName("""(/*union*/{landscapePrimary: 'landscape-primary', landscapeSecondary: 'landscape-secondary', portraitPrimary: 'portrait-primary', portraitSecondary: 'portrait-secondary'}/*union*/)""")
+external enum class OrientationType {
+    landscapePrimary,
+    landscapeSecondary,
+    portraitPrimary,
+    portraitSecondary,
+
+    ;
+}
