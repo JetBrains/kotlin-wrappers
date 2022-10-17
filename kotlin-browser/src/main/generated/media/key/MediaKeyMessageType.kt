71ad565b99f2526c8f43de662308c15665edb5f8
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 03:02:19 +0300

    Media Key. Unions [generated]

---
+++ kotlin-browser/src/main/generated/media/key/MediaKeyMessageType.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package media.key
+
+// language=JavaScript
+@JsName("""(/*union*/{individualizationRequest: 'individualization-request', licenseRelease: 'license-release', licenseRenewal: 'license-renewal', licenseRequest: 'license-request'}/*union*/)""")
+external enum class MediaKeyMessageType {
+    individualizationRequest,
+    licenseRelease,
+    licenseRenewal,
+    licenseRequest,
+
+    ;
+}
