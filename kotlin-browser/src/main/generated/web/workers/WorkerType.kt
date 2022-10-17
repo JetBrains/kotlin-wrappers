5fb7a5ca426e1ead39894fed6d67b289b45f6bee
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 03:08:48 +0300

    Web Workers. `WorkerType` [generated]

---
+++ kotlin-browser/src/main/generated/web/workers/WorkerType.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.workers
+
+// language=JavaScript
+@JsName("""(/*union*/{classic: 'classic', module: 'module'}/*union*/)""")
+external enum class WorkerType {
+    classic,
+    module,
+
+    ;
+}
