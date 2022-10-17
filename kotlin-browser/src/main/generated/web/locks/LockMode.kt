bade4ec12cd29dd2e2f4c6512fc900c26b31f030
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 03:01:58 +0300

    Web Locks. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/locks/LockMode.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.locks
+
+// language=JavaScript
+@JsName("""(/*union*/{exclusive: 'exclusive', shared: 'shared'}/*union*/)""")
+external enum class LockMode {
+    exclusive,
+    shared,
+
+    ;
+}
