a3aa3c013fdfe7de7b08d11c2ba8c02b72e17ddf
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 12:51:32 +0300

    Web Messaging. `MessageChannel`

---
+++ kotlin-web/src/main/kotlin/web/messaging/MessageChannel.kt
@@ -0,0 +1,9 @@
+package web.messaging
+
+external class MessageChannel {
+    /** Returns the first MessagePort object. */
+    val port1: MessagePort
+
+    /** Returns the second MessagePort object. */
+    val port2: MessagePort
+}
