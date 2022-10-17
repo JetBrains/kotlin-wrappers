3b2023a43ebb9775aedb78035c34c4ccc8e458ea
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 03:01:09 +0300

    Web Speech. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/speech/SpeechSynthesisErrorCode.kt
@@ -0,0 +1,26 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.speech
+
+// language=JavaScript
+@JsName("""(/*union*/{audioBusy: 'audio-busy', audioHardware: 'audio-hardware', canceled: 'canceled', interrupted: 'interrupted', invalidArgument: 'invalid-argument', languageUnavailable: 'language-unavailable', network: 'network', notAllowed: 'not-allowed', synthesisFailed: 'synthesis-failed', synthesisUnavailable: 'synthesis-unavailable', textTooLong: 'text-too-long', voiceUnavailable: 'voice-unavailable'}/*union*/)""")
+external enum class SpeechSynthesisErrorCode {
+    audioBusy,
+    audioHardware,
+    canceled,
+    interrupted,
+    invalidArgument,
+    languageUnavailable,
+    network,
+    notAllowed,
+    synthesisFailed,
+    synthesisUnavailable,
+    textTooLong,
+    voiceUnavailable,
+
+    ;
+}
