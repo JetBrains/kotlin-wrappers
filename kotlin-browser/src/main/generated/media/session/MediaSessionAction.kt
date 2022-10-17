d2ffb4525f7316571462da2ba49de91e27da80c4
Victor Turansky <victor.turansky@gmail.com>
2022-10-17 03:02:54 +0300

    Media Session. Unions [generated]

---
+++ kotlin-browser/src/main/generated/media/session/MediaSessionAction.kt
@@ -0,0 +1,26 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package media.session
+
+// language=JavaScript
+@JsName("""(/*union*/{hangup: 'hangup', nexttrack: 'nexttrack', pause: 'pause', play: 'play', previoustrack: 'previoustrack', seekbackward: 'seekbackward', seekforward: 'seekforward', seekto: 'seekto', skipad: 'skipad', stop: 'stop', togglecamera: 'togglecamera', togglemicrophone: 'togglemicrophone'}/*union*/)""")
+external enum class MediaSessionAction {
+    hangup,
+    nexttrack,
+    pause,
+    play,
+    previoustrack,
+    seekbackward,
+    seekforward,
+    seekto,
+    skipad,
+    stop,
+    togglecamera,
+    togglemicrophone,
+
+    ;
+}
