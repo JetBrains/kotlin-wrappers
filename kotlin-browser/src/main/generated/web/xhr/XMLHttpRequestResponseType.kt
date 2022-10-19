8f65b0a667392b7e1225fb97882c8c058f99a4a9
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 00:23:06 +0300

    XHR. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/xhr/XMLHttpRequestResponseType.kt
@@ -0,0 +1,21 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.xhr
+
+// language=JavaScript
+@JsName("""(/*union*/{none: '', arraybuffer: 'arraybuffer', blob: 'blob', document: 'document', json: 'json', text: 'text'}/*union*/)""")
+external enum class XMLHttpRequestResponseType :
+/* legacy adapter */ org.w3c.xhr.XMLHttpRequestResponseType {
+    none,
+    arraybuffer,
+    blob,
+    document,
+    json,
+    text,
+
+    ;
+}
--- kotlin-browser/src/main/kotlin/web/xhr/xhr.aliases.kt
+++ kotlin-browser/src/main/kotlin/web/xhr/xhr.aliases.kt
@@ -3,4 +3,3 @@ package web.xhr
 typealias XMLHttpRequest = org.w3c.xhr.XMLHttpRequest
 typealias XMLHttpRequestEventTarget = org.w3c.xhr.XMLHttpRequestEventTarget
 typealias XMLHttpRequestUpload = org.w3c.xhr.XMLHttpRequestUpload
-typealias XMLHttpRequestResponseType = org.w3c.xhr.XMLHttpRequestResponseType
