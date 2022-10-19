e53b39570c89ad7dc2dbdc99cc7357610777dfa4
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 00:59:58 +0300

    File System. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/filesystem/FileSystemHandleKind.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.filesystem
+
+// language=JavaScript
+@JsName("""(/*union*/{directory: 'directory', file: 'file'}/*union*/)""")
+external enum class FileSystemHandleKind {
+    directory,
+    file,
+
+    ;
+}
