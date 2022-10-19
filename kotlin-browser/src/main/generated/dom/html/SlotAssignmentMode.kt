1ef09931199552079d901f6559043648c034ed7e
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 02:36:39 +0300

    HTML. Unions [generated]

---
+++ kotlin-browser/src/main/generated/dom/html/SlotAssignmentMode.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.html
+
+// language=JavaScript
+@JsName("""(/*union*/{manual: 'manual', named: 'named'}/*union*/)""")
+external enum class SlotAssignmentMode {
+    manual,
+    named,
+
+    ;
+}
