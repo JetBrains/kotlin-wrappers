58b0dd3028e6b09e461b651f3bfea1a4f0ce52e8
Victor Turansky <victor.turansky@gmail.com>
2022-10-19 13:37:40 +0300

    JS. `TimeStamp` -> `EpochTimeStamp`

---
+++ kotlin-js/src/main/kotlin/kotlinx/js/EpochTimeStamp.kt
@@ -0,0 +1,6 @@
+package kotlinx.js
+
+/**
+ * Common alias for [EpochTimeStamp](https://developer.mozilla.org/en-US/docs/Web/API/EpochTimeStamp)
+ */
+typealias EpochTimeStamp = Double // Int64
--- kotlin-js/src/main/kotlin/kotlinx/js/TimeStamp.kt
+++
@@ -1,6 +0,0 @@
-package kotlinx.js
-
-/**
- * Common alias for [DOMTimeStamp](https://developer.mozilla.org/en-US/docs/Web/API/DOMTimeStamp)
- */
-typealias TimeStamp = Number // Int64
