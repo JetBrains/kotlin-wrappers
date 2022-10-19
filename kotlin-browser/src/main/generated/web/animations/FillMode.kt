dbe336d7c27cba85b3d6fb5c5fe6fc1f779f7383
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:21:46 +0300

    Web Animations. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/animations/FillMode.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.animations
+
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto', backwards: 'backwards', both: 'both', forwards: 'forwards', none: 'none'}/*union*/)""")
+external enum class FillMode {
+    auto,
+    backwards,
+    both,
+    forwards,
+    none,
+
+    ;
+}
