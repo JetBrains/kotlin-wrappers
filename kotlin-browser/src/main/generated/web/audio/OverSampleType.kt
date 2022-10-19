7463ab35c231b1e6d0c6411f02097d5e01340652
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 02:35:27 +0300

    Web Audio. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/audio/OverSampleType.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.audio
+
+// language=JavaScript
+@JsName("""(/*union*/{x2: '2x', x4: '4x', none: 'none'}/*union*/)""")
+external enum class OverSampleType {
+    x2,
+    x4,
+    none,
+
+    ;
+}
