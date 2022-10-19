20df29baaa30925683018c1b4a62d0774ee010c0
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 02:36:06 +0300

    DOM Parsing. Unions [generated]

---
+++ kotlin-browser/src/main/generated/dom/parsing/DOMParserSupportedType.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.parsing
+
+// language=JavaScript
+@JsName("""(/*union*/{applicationXhtmlXml: 'application/xhtml+xml', applicationXml: 'application/xml', imageSvgXml: 'image/svg+xml', textHtml: 'text/html', textXml: 'text/xml'}/*union*/)""")
+external enum class DOMParserSupportedType {
+    applicationXhtmlXml,
+    applicationXml,
+    imageSvgXml,
+    textHtml,
+    textXml,
+
+    ;
+}
