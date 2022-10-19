16f53aa3fe6e56e7984a8a6e095db57302a20cdd
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 00:57:17 +0300

    DOM. Font unions [generated]

---
+++ kotlin-browser/src/main/generated/dom/css/FontFaceLoadStatus.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.css
+
+// language=JavaScript
+@JsName("""(/*union*/{error: 'error', loaded: 'loaded', loading: 'loading', unloaded: 'unloaded'}/*union*/)""")
+external enum class FontFaceLoadStatus {
+    error,
+    loaded,
+    loading,
+    unloaded,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/dom/css/FontFaceSetLoadStatus.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom.css
+
+// language=JavaScript
+@JsName("""(/*union*/{loaded: 'loaded', loading: 'loading'}/*union*/)""")
+external enum class FontFaceSetLoadStatus {
+    loaded,
+    loading,
+
+    ;
+}
