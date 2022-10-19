8bc598076434caf9c175a57cee1bf7ed3e6e3f3f
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:53:22 +0300

    Remote Playback. Unions [generated]

---
+++ kotlin-browser/src/main/generated/remoteplayback/RemotePlaybackState.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package remoteplayback
+
+// language=JavaScript
+@JsName("""(/*union*/{connected: 'connected', connecting: 'connecting', disconnected: 'disconnected'}/*union*/)""")
+external enum class RemotePlaybackState {
+    connected,
+    connecting,
+    disconnected,
+
+    ;
+}
