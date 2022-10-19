f823fd3e4099db8ab5a9e66c46cc043b9abed710
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:34:03 +0300

    Media Source. Unions [generated]

---
+++ kotlin-browser/src/main/generated/media/source/EndOfStreamError.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package media.source
+
+// language=JavaScript
+@JsName("""(/*union*/{decode: 'decode', network: 'network'}/*union*/)""")
+external enum class EndOfStreamError {
+    decode,
+    network,
+
+    ;
+}
