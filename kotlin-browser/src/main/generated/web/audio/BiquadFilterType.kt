e47dd7534f68432cb730df31828cf13ae7a3e394
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:53:40 +0300

    Web Audio. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/audio/BiquadFilterType.kt
@@ -0,0 +1,22 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.audio
+
+// language=JavaScript
+@JsName("""(/*union*/{allpass: 'allpass', bandpass: 'bandpass', highpass: 'highpass', highshelf: 'highshelf', lowpass: 'lowpass', lowshelf: 'lowshelf', notch: 'notch', peaking: 'peaking'}/*union*/)""")
+external enum class BiquadFilterType {
+    allpass,
+    bandpass,
+    highpass,
+    highshelf,
+    lowpass,
+    lowshelf,
+    notch,
+    peaking,
+
+    ;
+}
