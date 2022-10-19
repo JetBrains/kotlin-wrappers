6cc944fd498ad4faa3cd25f3d2bddf3b8534037d
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 00:59:38 +0300

    DOM. Unions [generated]

---
+++ kotlin-browser/src/main/generated/dom/FullscreenNavigationUI.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom
+
+// language=JavaScript
+@JsName("""(/*union*/{auto: 'auto', hide: 'hide', show: 'show'}/*union*/)""")
+external enum class FullscreenNavigationUI {
+    auto,
+    hide,
+    show,
+
+    ;
+}
---
+++ kotlin-browser/src/main/generated/dom/InsertPosition.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package dom
+
+// language=JavaScript
+@JsName("""(/*union*/{beforebegin: 'beforebegin', afterbegin: 'afterbegin', beforeend: 'beforeend', afterend: 'afterend'}/*union*/)""")
+external enum class InsertPosition {
+    beforebegin,
+    afterbegin,
+    beforeend,
+    afterend,
+
+    ;
+}
