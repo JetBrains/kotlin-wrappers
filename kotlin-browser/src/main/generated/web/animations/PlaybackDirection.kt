ed32439372dbed559f290f608f4e071ec74af454
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 01:54:00 +0300

    Web Animations. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/animations/PlaybackDirection.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.animations
+
+// language=JavaScript
+@JsName("""(/*union*/{alternate: 'alternate', alternateReverse: 'alternate-reverse', normal: 'normal', reverse: 'reverse'}/*union*/)""")
+external enum class PlaybackDirection {
+    alternate,
+    alternateReverse,
+    normal,
+    reverse,
+
+    ;
+}
