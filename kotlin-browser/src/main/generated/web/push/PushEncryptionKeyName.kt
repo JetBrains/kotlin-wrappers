6364a2125e260161806f6d8788cdebc7227cd387
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 02:36:26 +0300

    Push. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/push/PushEncryptionKeyName.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.push
+
+// language=JavaScript
+@JsName("""(/*union*/{auth: 'auth', p256dh: 'p256dh'}/*union*/)""")
+external enum class PushEncryptionKeyName {
+    auth,
+    p256dh,
+
+    ;
+}
