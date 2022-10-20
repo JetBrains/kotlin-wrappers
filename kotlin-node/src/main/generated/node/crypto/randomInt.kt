485ca839bbcb48ebb0e17800780beb49d721ecb7
Victor Turansky <victor.turansky@gmail.com>
2022-10-21 00:35:35 +0300

    Node `18.11.3` [generated]

--- kotlin-node/src/main/generated/node/crypto/randomInt.kt
+++ kotlin-node/src/main/generated/node/crypto/randomInt.kt
@@ -9,7 +9,7 @@ package node.crypto
  * Return a random integer `n` such that `min <= n < max`.  This
  * implementation avoids [modulo bias](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modulo_bias).
  *
- * The range (`max - min`) must be less than 248. `min` and `max` must
+ * The range (`max - min`) must be less than 2^48. `min` and `max` must
  * be [safe integers](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger).
  *
  * If the `callback` function is not provided, the random integer is
@@ -60,7 +60,7 @@ external fun randomInt(
  * Return a random integer `n` such that `min <= n < max`.  This
  * implementation avoids [modulo bias](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modulo_bias).
  *
- * The range (`max - min`) must be less than 248. `min` and `max` must
+ * The range (`max - min`) must be less than 2^48. `min` and `max` must
  * be [safe integers](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger).
  *
  * If the `callback` function is not provided, the random integer is
@@ -112,7 +112,7 @@ external fun randomInt(
  * Return a random integer `n` such that `min <= n < max`.  This
  * implementation avoids [modulo bias](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modulo_bias).
  *
- * The range (`max - min`) must be less than 248. `min` and `max` must
+ * The range (`max - min`) must be less than 2^48. `min` and `max` must
  * be [safe integers](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger).
  *
  * If the `callback` function is not provided, the random integer is
@@ -167,7 +167,7 @@ external fun randomInt(
  * Return a random integer `n` such that `min <= n < max`.  This
  * implementation avoids [modulo bias](https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#Modulo_bias).
  *
- * The range (`max - min`) must be less than 248. `min` and `max` must
+ * The range (`max - min`) must be less than 2^48. `min` and `max` must
  * be [safe integers](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isSafeInteger).
  *
  * If the `callback` function is not provided, the random integer is
