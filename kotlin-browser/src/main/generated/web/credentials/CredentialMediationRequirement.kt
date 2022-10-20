fa2c42d7f9d6c0246e9cd728d736b9fd6d8576e6
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 15:48:49 +0300

    Web Credentials. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/credentials/CredentialMediationRequirement.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.credentials
+
+// language=JavaScript
+@JsName("""(/*union*/{optional: 'optional', required: 'required', silent: 'silent'}/*union*/)""")
+external enum class CredentialMediationRequirement {
+    optional,
+    required,
+    silent,
+
+    ;
+}
