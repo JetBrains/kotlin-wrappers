f285ec63532fe169a3bf0ba32e18868ff6dff077
Victor Turansky <victor.turansky@gmail.com>
2022-10-20 15:49:09 +0300

    Web CSP. Unions [generated]

---
+++ kotlin-browser/src/main/generated/web/csp/SecurityPolicyViolationEventDisposition.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.csp
+
+// language=JavaScript
+@JsName("""(/*union*/{enforce: 'enforce', report: 'report'}/*union*/)""")
+external enum class SecurityPolicyViolationEventDisposition {
+    enforce,
+    report,
+
+    ;
+}
