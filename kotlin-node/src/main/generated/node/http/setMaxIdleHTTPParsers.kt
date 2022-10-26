53218d23310d91d376b335d54faeed850d5983f2
Victor Turansky <victor.turansky@gmail.com>
2022-10-26 20:59:24 +0300

    Node `18.11.6` [generated]

---
+++ kotlin-node/src/main/generated/node/http/setMaxIdleHTTPParsers.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("node:http")
+@file:JsNonModule
+
+package node.http
+
+/**
+ * Set the maximum number of idle HTTP parsers. Default: 1000.
+ * @param count
+ * @since v18.8.0, v16.18.0
+ */
+external fun setMaxIdleHTTPParsers(
+    count: Number,
+)
