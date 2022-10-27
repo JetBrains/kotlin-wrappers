054ab1e1fb56579f62fccd4b8cbe3593205af8cd
Victor Turansky <victor.turansky@gmail.com>
2022-10-28 01:33:17 +0300

    HTML. `HTMLCanvasElement` [generated]

---
+++ kotlin-browser/src/main/generated/dom/html/CanvasContextId.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package dom.html
+
+// language=JavaScript
+@JsName("""(/*union*/{canvas: '2d', bitmaprenderer: 'bitmaprenderer', webgl: 'webgl', webgl2: 'webgl2'}/*union*/)""")
+sealed external interface CanvasContextId {
+    // `2d`
+    object canvas : CanvasContextId
+    object bitmaprenderer : CanvasContextId
+    object webgl : CanvasContextId
+    object webgl2 : CanvasContextId
+}
--- kotlin-browser/src/main/generated/dom/html/HTMLCanvasElement.kt
+++ kotlin-browser/src/main/generated/dom/html/HTMLCanvasElement.kt
@@ -2,4 +2,60 @@
 
 package dom.html
 
-typealias HTMLCanvasElement = org.w3c.dom.HTMLCanvasElement
+import canvas.CanvasRenderingContext2D
+import canvas.CanvasRenderingContext2DSettings
+import canvas.ImageBitmapRenderingContext
+import canvas.ImageBitmapRenderingContextSettings
+import media.stream.MediaStream
+import webgl.WebGL2RenderingContext
+import webgl.WebGLContextAttributes
+import webgl.WebGLRenderingContext
+
+abstract external class HTMLCanvasElement :
+    HTMLElement {
+    /** Gets or sets the height of a canvas element on a document. */
+    var height: Double
+
+    /** Gets or sets the width of a canvas element on a document. */
+    var width: Double
+    fun captureStream(frameRequestRate: Number = definedExternally): MediaStream
+
+    /**
+     * Returns an object that provides methods and properties for drawing and manipulating images and graphics on a canvas element in a document. A context object includes information about colors, line widths, fonts, and other graphic parameters that can be drawn on a canvas.
+     * @param contextId The identifier (ID) of the type of canvas to create. Internet Explorer 9 and Internet Explorer 10 support only a 2-D context using canvas.getContext("2d"); IE11 Preview also supports 3-D or WebGL context using canvas.getContext("experimental-webgl");
+     */
+    fun getContext(
+        contextId: CanvasContextId.canvas,
+        options: CanvasRenderingContext2DSettings = definedExternally,
+    ): CanvasRenderingContext2D?
+
+    fun getContext(
+        contextId: CanvasContextId.bitmaprenderer,
+        options: ImageBitmapRenderingContextSettings = definedExternally,
+    ): ImageBitmapRenderingContext?
+
+    fun getContext(
+        contextId: CanvasContextId.webgl,
+        options: WebGLContextAttributes = definedExternally,
+    ): WebGLRenderingContext?
+
+    fun getContext(
+        contextId: CanvasContextId.webgl2,
+        options: WebGLContextAttributes = definedExternally,
+    ): WebGL2RenderingContext?
+
+    fun toBlob(
+        callback: BlobCallback,
+        type: String = definedExternally,
+        quality: Number = definedExternally,
+    )
+
+    /**
+     * Returns the content of the current canvas as an image that you can use as a source for another canvas or an HTML element.
+     * @param type The standard MIME type for the image format to return. If you do not specify this parameter, the default value is a PNG format image.
+     */
+    fun toDataURL(
+        type: String = definedExternally,
+        quality: Number = definedExternally,
+    ): String
+}
