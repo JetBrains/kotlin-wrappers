bc6dec1c7d3f1dc469302b459c91058eff200ddf
Victor Turansky <victor.turansky@gmail.com>
2022-10-28 01:41:06 +0300

    HTML. `HTMLIFrameElement` [generated]

--- kotlin-browser/src/main/generated/dom/html/HTMLIFrameElement.kt
+++ kotlin-browser/src/main/generated/dom/html/HTMLIFrameElement.kt
@@ -2,4 +2,36 @@
 
 package dom.html
 
-typealias HTMLIFrameElement = org.w3c.dom.HTMLIFrameElement
+import dom.DOMTokenList
+import dom.Document
+import web.http.ReferrerPolicy
+
+abstract external class HTMLIFrameElement :
+    HTMLElement {
+    var allow: String
+    var allowFullscreen: Boolean
+
+    /** Retrieves the document object of the page or frame. */
+    val contentDocument: Document?
+
+    /** Retrieves the object of the specified. */
+    val contentWindow: WindowProxy?
+
+    /** Sets or retrieves the height of the object. */
+    var height: String
+
+    /** Sets or retrieves the frame name. */
+    var name: String
+    var referrerPolicy: ReferrerPolicy
+    val sandbox: DOMTokenList
+
+    /** Sets or retrieves a URL to be loaded by the object. */
+    var src: String
+
+    /** Sets or retrives the content of the page that is to contain. */
+    var srcdoc: String
+
+    /** Sets or retrieves the width of the object. */
+    var width: String
+    fun getSVGDocument(): Document?
+}
