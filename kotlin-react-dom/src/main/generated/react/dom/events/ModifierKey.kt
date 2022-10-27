a6d79273593aa1fa0bee330684ee2f1f8ddd22d0
Victor Turansky <victor.turansky@gmail.com>
2022-10-27 16:30:11 +0300

    React Types `18.0.24` [generated]

---
+++ kotlin-react-dom/src/main/generated/react/dom/events/ModifierKey.kt
@@ -0,0 +1,25 @@
+// Automatically generated - do not modify!
+
+package react.dom.events
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{Alt: 'Alt', AltGraph: 'AltGraph', CapsLock: 'CapsLock', Control: 'Control', Fn: 'Fn', FnLock: 'FnLock', Hyper: 'Hyper', Meta: 'Meta', NumLock: 'NumLock', ScrollLock: 'ScrollLock', Shift: 'Shift', Super: 'Super', Symbol: 'Symbol', SymbolLock: 'SymbolLock'}/*union*/)""")
+external enum class ModifierKey {
+    Alt,
+    AltGraph,
+    CapsLock,
+    Control,
+    Fn,
+    FnLock,
+    Hyper,
+    Meta,
+    NumLock,
+    ScrollLock,
+    Shift,
+    Super,
+    Symbol,
+    SymbolLock,
+
+    ;
+}
