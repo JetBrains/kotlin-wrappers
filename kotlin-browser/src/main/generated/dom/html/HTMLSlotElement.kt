917189c3636cc20b4ec2dea091032ba281549962
Victor Turansky <victor.turansky@gmail.com>
2022-10-28 02:02:13 +0300

    HTML. `HTMLSlotElement` [generated]

--- kotlin-browser/src/main/generated/dom/html/HTMLSlotElement.kt
+++ kotlin-browser/src/main/generated/dom/html/HTMLSlotElement.kt
@@ -2,4 +2,14 @@
 
 package dom.html
 
-typealias HTMLSlotElement = org.w3c.dom.HTMLSlotElement
+import dom.Element
+import dom.Node
+import kotlinx.js.ReadonlyArray
+
+abstract external class HTMLSlotElement :
+    HTMLElement {
+    var name: String
+    fun assign(vararg nodes: Element /* | Text */)
+    fun assignedElements(options: AssignedNodesOptions = definedExternally): ReadonlyArray<Element>
+    fun assignedNodes(options: AssignedNodesOptions = definedExternally): ReadonlyArray<Node>
+}
