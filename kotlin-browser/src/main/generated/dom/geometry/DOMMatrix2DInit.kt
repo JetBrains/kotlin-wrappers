9a441c7333bc1698bf8ab5e32a828547e5629824
Victor Turansky <victor.turansky@gmail.com>
2022-10-28 05:20:25 +0300

    DOM Geometry. `DOMMatrix2DInit` [generated]

---
+++ kotlin-browser/src/main/generated/dom/geometry/DOMMatrix2DInit.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package dom.geometry
+
+sealed external interface DOMMatrix2DInit {
+    var a: Number?
+    var b: Number?
+    var c: Number?
+    var d: Number?
+    var e: Number?
+    var f: Number?
+    var m11: Number?
+    var m12: Number?
+    var m21: Number?
+    var m22: Number?
+    var m41: Number?
+    var m42: Number?
+}
