65b2445e10ce73fdb0f392ab55d2c3398d80a265
Victor Turansky <victor.turansky@gmail.com>
2022-10-28 13:26:18 +0300

    URL. Update formatting

--- kotlin-web/src/main/kotlin/web/url/URL.kt
+++ kotlin-web/src/main/kotlin/web/url/URL.kt
@@ -1,11 +1,18 @@
 package web.url
 
-import web.buffer.Blob
 import org.w3c.dom.mediasource.MediaSource
+import web.buffer.Blob
 
 external class URL : org.w3c.dom.url.URL {
-    constructor(url: String, base: String = definedExternally)
-    constructor(url: URL, base: URL = definedExternally)
+    constructor(
+        url: String,
+        base: String = definedExternally,
+    )
+
+    constructor(
+        url: URL,
+        base: URL = definedExternally,
+    )
 
     companion object {
         fun createObjectURL(obj: Blob): String
