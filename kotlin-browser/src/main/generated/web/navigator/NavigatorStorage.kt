8a4a9137e1ea3968f0e6dd39627711d95618e802
Victor Turansky <victor.turansky@gmail.com>
2022-10-30 01:22:23 +0300

    Navigator. `NavigatorStorage` [generated]

---
+++ kotlin-browser/src/main/generated/web/navigator/NavigatorStorage.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package web.navigator
+
+import web.storage.StorageManager
+
+sealed external interface NavigatorStorage {
+    val storage: StorageManager
+}
