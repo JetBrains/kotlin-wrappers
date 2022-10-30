18b8694e47f383dfac1cb27e95e0ba0a8a5da6f6
Victor Turansky <victor.turansky@gmail.com>
2022-10-30 22:10:55 +0200

    CSSOM. Types [generated]

---
+++ kotlin-browser/src/main/generated/cssom/CSSTransition.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package cssom
+
+import web.animations.Animation
+
+sealed external class CSSTransition :
+    Animation {
+    val transitionProperty: String
+}
