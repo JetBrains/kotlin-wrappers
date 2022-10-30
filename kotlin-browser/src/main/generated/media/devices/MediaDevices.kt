4cc2309537a8f36ca96b4f07c6d6df63453c535d
Victor Turansky <victor.turansky@gmail.com>
2022-10-30 23:23:50 +0200

    Media. Aliases [generated]

---
+++ kotlin-browser/src/main/generated/media/devices/MediaDevices.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package media.devices
+
+typealias MediaDevices = org.w3c.dom.mediacapture.MediaDevices
---
+++ kotlin-browser/src/main/generated/media/key/MediaKeySystemAccess.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package media.key
+
+typealias MediaKeySystemAccess = org.w3c.dom.encryptedmedia.MediaKeySystemAccess
