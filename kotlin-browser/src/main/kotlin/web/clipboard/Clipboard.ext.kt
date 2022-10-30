085620bde1a11830e677e1e9aee059484cc8b3cd
Victor Turansky <victor.turansky@gmail.com>
2022-10-30 23:22:32 +0200

    Clipboard. Extensions in separate file

---
+++ kotlin-browser/src/main/kotlin/web/clipboard/Clipboard.ext.kt
@@ -0,0 +1,14 @@
+package web.clipboard
+
+import kotlinx.js.Void
+import org.w3c.dom.DataTransfer
+import kotlin.js.Promise
+
+// Details - https://youtrack.jetbrains.com/issue/KT-52211
+inline fun Promise<DataTransfer>.apply_KT_52211_workaround(): Promise<ClipboardItems> =
+    unsafeCast<Promise<ClipboardItems>>()
+
+inline fun Clipboard.write(
+    data: ClipboardItems,
+): Promise<Void> =
+    asDynamic().write(data)
