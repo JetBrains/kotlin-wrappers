a9492bce6408b431168391f000d8a417627e7528
Victor Turansky <victor.turansky@gmail.com>
2022-11-01 15:17:24 +0200

    Node `18.11.9` [generated]

---
+++ kotlin-node/src/main/generated/node/test/after.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("node:test")
+@file:JsNonModule
+
+package node.test
+
+/**
+ * This function is used to create a hook running after running a suite.
+ * @param fn The hook function. If the hook uses callbacks, the callback function is passed as
+ *    the second argument. Default: A no-op function.
+ * @param options Configuration options for the hook.
+ * @since v18.8.0
+ */
+external fun after(
+    fn: HookFn = definedExternally,
+    options: HookOptions = definedExternally,
+)
