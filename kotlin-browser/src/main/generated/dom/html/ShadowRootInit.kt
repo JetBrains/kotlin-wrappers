160d6de9e83d899f7cfb35c335ac5c93328da3ff
Victor Turansky <victor.turansky@gmail.com>
2022-11-05 00:15:08 +0200

    HTML. `ShadowRootInit` [generated]

---
+++ kotlin-browser/src/main/generated/dom/html/ShadowRootInit.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+package dom.html
+
+sealed external interface ShadowRootInit {
+    var delegatesFocus: Boolean?
+    var mode: ShadowRootMode
+    var slotAssignment: SlotAssignmentMode?
+}
