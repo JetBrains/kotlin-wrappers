1bff8b1fdc1714faa231ca73d97091cf9edc55d6
Victor Turansky <victor.turansky@gmail.com>
2022-11-04 08:16:53 +0200

    JS. `EpochTimeStamp` - alias of `JsLong`

--- kotlin-js/src/main/kotlin/kotlinx/js/EpochTimeStamp.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/EpochTimeStamp.kt
@@ -3,4 +3,4 @@ package kotlinx.js
 /**
  * Common alias for [EpochTimeStamp](https://developer.mozilla.org/en-US/docs/Web/API/EpochTimeStamp)
  */
-typealias EpochTimeStamp = Double // Int64
+typealias EpochTimeStamp = JsLong
