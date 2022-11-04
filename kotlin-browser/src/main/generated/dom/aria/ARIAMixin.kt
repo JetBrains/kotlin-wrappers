0b8199845223f7803b6f24b9f116fb639c068366
Victor Turansky <victor.turansky@gmail.com>
2022-11-04 20:38:25 +0200

    ARIA. Types [generated]

---
+++ kotlin-browser/src/main/generated/dom/aria/ARIAMixin.kt
@@ -0,0 +1,42 @@
+// Automatically generated - do not modify!
+
+package dom.aria
+
+external interface ARIAMixin {
+    var ariaAtomic: String?
+    var ariaAutoComplete: String?
+    var ariaBusy: String?
+    var ariaChecked: String?
+    var ariaColCount: String?
+    var ariaColIndex: String?
+    var ariaColSpan: String?
+    var ariaCurrent: String?
+    var ariaDisabled: String?
+    var ariaExpanded: String?
+    var ariaHasPopup: String?
+    var ariaHidden: String?
+    var ariaKeyShortcuts: String?
+    var ariaLabel: String?
+    var ariaLevel: String?
+    var ariaLive: String?
+    var ariaModal: String?
+    var ariaMultiLine: String?
+    var ariaMultiSelectable: String?
+    var ariaOrientation: String?
+    var ariaPlaceholder: String?
+    var ariaPosInSet: String?
+    var ariaPressed: String?
+    var ariaReadOnly: String?
+    var ariaRequired: String?
+    var ariaRoleDescription: String?
+    var ariaRowCount: String?
+    var ariaRowIndex: String?
+    var ariaRowSpan: String?
+    var ariaSelected: String?
+    var ariaSetSize: String?
+    var ariaSort: String?
+    var ariaValueMax: String?
+    var ariaValueMin: String?
+    var ariaValueNow: String?
+    var ariaValueText: String?
+}
