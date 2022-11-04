241425e0088c48ef27140f028a469047a89406ee
Victor Turansky <victor.turansky@gmail.com>
2022-11-04 16:24:42 +0200

    Events. Data transfer [generated]

--- kotlin-browser/src/main/kotlin/dom/events/DataTransfer.kt
+++
@@ -1,5 +0,0 @@
-package dom.events
-
-typealias DataTransfer = org.w3c.dom.DataTransfer
-typealias DataTransferItem = org.w3c.dom.DataTransferItem
-typealias DataTransferItemList = org.w3c.dom.DataTransferItemList
