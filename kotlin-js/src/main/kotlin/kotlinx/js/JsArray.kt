5a4d9a819127fd4adfe468ab03837c5c111920a5
Victor Turansky <victor.turansky@gmail.com>
2022-11-08 00:12:56 +0200

    JS. More `Array` static methods

--- kotlin-js/src/main/kotlin/kotlinx/js/JsArray.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/JsArray.kt
@@ -3,7 +3,12 @@ package kotlinx.js
 @JsName("Array")
 sealed external class JsArray {
     companion object {
-        fun <T> from(source: JsIterator<T>): Array<T>
-        fun <T> from(source: JsIterable<T>): Array<T>
+        fun <T> from(source: JsIterator<T>): ReadonlyArray<T>
+        fun <T> from(source: JsIterable<T>): ReadonlyArray<T>
+        fun <T> from(source: ArrayLike<T>): ReadonlyArray<T>
+        fun <T, U> from(
+            source: ArrayLike<T>,
+            map: (item: T, index: Int) -> U,
+        ): ReadonlyArray<U>
     }
 }
