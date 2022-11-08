a9712cc9c3e84fc639049ae7b279fbfe322cd1d1
Victor Turansky <victor.turansky@gmail.com>
2022-11-08 10:35:24 +0200

    JS. Add `ArrayLike.asList` extension

--- kotlin-js/src/main/kotlin/kotlinx/js/ArrayLike.kt
+++ kotlin-js/src/main/kotlin/kotlinx/js/ArrayLike.kt
@@ -8,3 +8,15 @@ inline operator fun <T> ArrayLike<T>.get(
     index: Int,
 ): T =
     asDynamic()[index]
+
+fun <T> ArrayLike<T>.asList(): List<T> =
+    object : AbstractList<T>() {
+        override val size: Int
+            get() = this@asList.length
+
+        override fun get(index: Int): T =
+            when (index) {
+                in 0..lastIndex -> this@asList[index]
+                else -> throw IndexOutOfBoundsException("index $index is not in range [0..$lastIndex]")
+            }
+    }
