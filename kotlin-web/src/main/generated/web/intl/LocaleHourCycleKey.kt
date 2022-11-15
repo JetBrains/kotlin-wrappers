10a486abe73048229dbe8c162e2df95c65713566
Victor Turansky <victor.turansky@gmail.com>
2022-11-15 21:12:04 +0200

    Intl. Unions [generated]

---
+++ kotlin-web/src/main/generated/web/intl/LocaleHourCycleKey.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.intl
+
+// language=JavaScript
+@JsName("""(/*union*/{h12: 'h12', h23: 'h23', h11: 'h11', h24: 'h24'}/*union*/)""")
+external enum class LocaleHourCycleKey {
+    h12,
+    h23,
+    h11,
+    h24,
+
+    ;
+}
---
+++ kotlin-web/src/main/generated/web/intl/RelativeTimeFormatStyle.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.intl
+
+// language=JavaScript
+@JsName("""(/*union*/{long: 'long', short: 'short', narrow: 'narrow'}/*union*/)""")
+external enum class RelativeTimeFormatStyle {
+    long,
+    short,
+    narrow,
+
+    ;
+}
