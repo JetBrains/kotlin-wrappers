b28a58b466b76aec99aacbbbdaec6c8e90e79dff
Victor Turansky <victor.turansky@gmail.com>
2022-11-16 10:08:16 +0200

    Intl. Unions [generated]

---
+++ kotlin-web/src/main/generated/web/intl/DisplayNamesLanguageDisplay.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package web.intl
+
+// language=JavaScript
+@JsName("""(/*union*/{dialect: 'dialect', standard: 'standard'}/*union*/)""")
+external enum class DisplayNamesLanguageDisplay {
+    dialect,
+    standard,
+
+    ;
+}
