6868d9a0de6d8f41110b0ceab5f8688663169a15
Victor Turansky <victor.turansky@gmail.com>
2022-11-17 09:57:14 +0200

    DOM. Geometry extensions [generated]

---
+++ kotlin-browser/src/main/generated/dom/geometry/DOMMatrixReadOnly.ext.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package dom.geometry
+
+inline fun DOMMatrixReadOnly.asInit(): DOMMatrixInit =
+    unsafeCast<DOMMatrixInit>()
---
+++ kotlin-browser/src/main/generated/dom/geometry/DOMRectReadOnly.ext.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package dom.geometry
+
+inline fun DOMRectReadOnly.asInit(): DOMRectInit =
+    unsafeCast<DOMRectInit>()
