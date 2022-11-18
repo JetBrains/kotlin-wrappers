0ee05d5d0f1c574b75049a87d69b039d0d02bb93
Victor Turansky <victor.turansky@gmail.com>
2022-11-18 16:16:23 +0200

    HTTP. `FormData` [generated]

--- kotlin-web/src/main/generated/web/http/FormData.kt
+++ kotlin-web/src/main/generated/web/http/FormData.kt
@@ -2,4 +2,40 @@
 
 package web.http
 
-typealias FormData = org.w3c.xhr.FormData
+import kotlinx.js.ReadonlyArray
+import web.buffer.Blob
+import web.events.EventTarget
+
+external class FormData(
+    form: EventTarget /* HTMLFormElement */ = definedExternally,
+) {
+    fun append(
+        name: String,
+        value: String,
+        fileName: String = definedExternally,
+    )
+
+    fun append(
+        name: String,
+        value: Blob,
+        fileName: String = definedExternally,
+    )
+
+    fun delete(name: String)
+    fun get(name: String): FormDataEntryValue?
+    fun getAll(name: String): ReadonlyArray<FormDataEntryValue>
+    fun has(name: String): Boolean
+    fun set(
+        name: String,
+        value: String,
+        fileName: String = definedExternally,
+    )
+
+    fun set(
+        name: String,
+        value: Blob,
+        fileName: String = definedExternally,
+    )
+
+    fun forEach(action: (item: FormDataEntryValue) -> Unit)
+}
