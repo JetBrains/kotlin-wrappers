7db3157475b59dbceee658b5e5ccd01fb6fcfcfa
Victor Turansky <victor.turansky@gmail.com>
2022-11-19 02:38:38 +0200

    WebGL. `TexImageSource` [generated]

--- kotlin-browser/src/main/generated/webgl/Aliases.kt
+++
@@ -1,5 +0,0 @@
-// Automatically generated - do not modify!
-
-package webgl
-
-typealias TexImageSource = org.khronos.webgl.TexImageSource
---
+++ kotlin-browser/src/main/generated/webgl/TexImageSource.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package webgl
+
+typealias TexImageSource = Any /* ImageBitmap | ImageData | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement | OffscreenCanvas */
