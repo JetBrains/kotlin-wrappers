ccee0ca678dafdaa2f28735eb859d7335619e6d1
Victor Turansky <victor.turansky@gmail.com>
2022-11-20 01:53:04 +0200

    DOM. `MutationObserver` [generated]

---
+++ kotlin-browser/src/main/generated/dom/observers/MutationCallback.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package dom.observers
+
+import kotlinx.js.ReadonlyArray
+
+typealias MutationCallback = (
+    mutations: ReadonlyArray<MutationRecord>,
+    observer: MutationObserver,
+) -> Unit
---
+++ kotlin-browser/src/main/generated/dom/observers/MutationObserver.kt
@@ -0,0 +1,26 @@
+// Automatically generated - do not modify!
+
+package dom.observers
+
+import dom.Node
+import kotlinx.js.ReadonlyArray
+
+external class MutationObserver(
+    callback: MutationCallback,
+) {
+    /** Stops observer from observing any mutations. Until the observe() method is used again, observer's callback will not be invoked. */
+    fun disconnect()
+
+    /**
+     * Instructs the user agent to observe a given target (a node) and report any mutations based on the criteria given by options (an object).
+     *
+     * The options argument allows for setting mutation observation options via object members.
+     */
+    fun observe(
+        target: Node,
+        options: MutationObserverInit = definedExternally,
+    )
+
+    /** Empties the record queue and returns what was in there. */
+    fun takeRecords(): ReadonlyArray<MutationRecord>
+}
