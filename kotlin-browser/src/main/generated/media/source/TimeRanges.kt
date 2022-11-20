a0bbd1c982fc0d48949605fb31b3880a83611024
Victor Turansky <victor.turansky@gmail.com>
2022-11-20 19:44:41 +0200

    Media Source. `TimeRanges` [generated]

--- kotlin-browser/src/main/generated/media/source/TimeRanges.kt
+++ kotlin-browser/src/main/generated/media/source/TimeRanges.kt
@@ -2,4 +2,21 @@
 
 package media.source
 
-typealias TimeRanges = org.w3c.dom.TimeRanges
+sealed external class TimeRanges {
+    /** Returns the number of ranges in the object. */
+    val length: Int
+
+    /**
+     * Returns the time for the end of the range with the given index.
+     *
+     * Throws an "IndexSizeError" DOMException if the index is out of range.
+     */
+    fun end(index: Int): Number
+
+    /**
+     * Returns the time for the start of the range with the given index.
+     *
+     * Throws an "IndexSizeError" DOMException if the index is out of range.
+     */
+    fun start(index: Int): Number
+}
