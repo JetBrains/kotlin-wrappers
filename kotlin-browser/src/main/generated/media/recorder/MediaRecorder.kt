e066309a78e58a383993ab58ac854be77f33ee55
Victor Turansky <victor.turansky@gmail.com>
2022-11-22 00:19:49 +0200

    Media Recorder. `MediaRecorder` [generated]

---
+++ kotlin-browser/src/main/generated/media/recorder/MediaRecorder.kt
@@ -0,0 +1,30 @@
+// Automatically generated - do not modify!
+
+package media.recorder
+
+import media.streams.MediaStream
+import web.events.Event
+import web.events.EventHandler
+import web.events.EventTarget
+
+external class MediaRecorder(
+    stream: MediaStream,
+    options: MediaRecorderOptions = definedExternally,
+) : EventTarget {
+    val audioBitsPerSecond: Int
+    val mimeType: String
+    var ondataavailable: EventHandler<BlobEvent>?
+    var onerror: EventHandler<Event>?
+    var onpause: EventHandler<Event>?
+    var onresume: EventHandler<Event>?
+    var onstart: EventHandler<Event>?
+    var onstop: EventHandler<Event>?
+    val state: RecordingState
+    val stream: MediaStream
+    val videoBitsPerSecond: Int
+    fun pause()
+    fun requestData()
+    fun resume()
+    fun start(timeslice: Number = definedExternally)
+    fun stop()
+}
