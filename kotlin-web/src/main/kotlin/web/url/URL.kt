dc24bce3208145e100a43069876e214db4b77723
Victor Turansky <victor.turansky@gmail.com>
2022-11-22 11:54:53 +0200

    URL. `URL` and `URLSearchParams`

--- kotlin-web/src/main/kotlin/web/url/URL.kt
+++ kotlin-web/src/main/kotlin/web/url/URL.kt
@@ -2,7 +2,7 @@ package web.url
 
 import web.buffer.Blob
 
-external class URL : org.w3c.dom.url.URL {
+external class URL {
     constructor(
         url: String,
         base: String = definedExternally,
@@ -12,6 +12,20 @@ external class URL : org.w3c.dom.url.URL {
         url: URL,
         base: URL = definedExternally,
     )
+
+    var hash: String
+    var host: String
+    var hostname: String
+    var href: String
+    val origin: String
+    var password: String
+    var pathname: String
+    var port: String
+    var protocol: String
+    var search: String
+    val searchParams: URLSearchParams
+    var username: String
+    fun toJSON(): String
 
     companion object {
         fun createObjectURL(obj: Blob): String
--- kotlin-web/src/main/kotlin/web/url/URLSearchParams.kt
+++ kotlin-web/src/main/kotlin/web/url/URLSearchParams.kt
@@ -3,13 +3,39 @@ package web.url
 import kotlinx.js.*
 
 external class URLSearchParams() :
-    org.w3c.dom.url.URLSearchParams,
     HasSymbol.iterator<JsTuple2<String, String>> {
 
     constructor(init: ReadonlyArray<JsTuple2<String, String>>)
     constructor(init: ReadonlyRecord<String, String>)
     constructor(init: String)
     constructor(init: URLSearchParams)
+
+    /** Appends a specified key/value pair as a new search parameter. */
+    fun append(
+        name: String,
+        value: String,
+    )
+
+    /** Deletes the given search parameter, and its associated value, from the list of all search parameters. */
+    fun delete(name: String)
+
+    /** Returns the first value associated to the given search parameter. */
+    fun get(name: String): String?
+
+    /** Returns all the values association with a given search parameter. */
+    fun getAll(name: String): ReadonlyArray<String>
+
+    /** Returns a Boolean indicating if such a search parameter exists. */
+    fun has(name: String): Boolean
+
+    /** Sets the value associated to a given search parameter to the given value. If there were several values, delete the others. */
+    fun set(
+        name: String,
+        value: String
+    )
+
+    fun sort()
+    fun forEach(action: (item: String) -> Unit)
 
     fun entries(): JsIterable.Iterator<JsTuple2<String, String>>
     fun keys(): JsIterable.Iterator<String>
