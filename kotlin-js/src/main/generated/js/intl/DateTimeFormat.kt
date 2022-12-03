a32f02ea53f47a46a4b267dc2c1d942a73a6dfe2
Victor Turansky <victor.turansky@gmail.com>
2022-12-03 23:14:54 +0200

    Intl. Split union types [generated]

--- kotlin-js/src/main/generated/js/intl/DateTimeFormat.kt
+++ kotlin-js/src/main/generated/js/intl/DateTimeFormat.kt
@@ -2,17 +2,40 @@
 
 package js.intl
 
+import js.core.BigInt
 import js.core.ReadonlyArray
+import kotlin.js.Date
 
 sealed external interface DateTimeFormat {
-    fun formatToParts(date: Any /* Date | number */ = definedExternally): ReadonlyArray<DateTimeFormatPart>
+    fun formatToParts(date: Date = definedExternally): ReadonlyArray<DateTimeFormatPart>
+    fun formatToParts(date: Number): ReadonlyArray<DateTimeFormatPart>
     fun formatRange(
-        startDate: Any, /* Date | number | bigint */
-        endDate: Any, /* Date | number | bigint */
+        startDate: Date,
+        endDate: Date,
+    ): String
+
+    fun formatRange(
+        startDate: Number,
+        endDate: Number,
+    ): String
+
+    fun formatRange(
+        startDate: BigInt,
+        endDate: BigInt,
     ): String
 
     fun formatRangeToParts(
-        startDate: Any, /* Date | number | bigint */
-        endDate: Any, /* Date | number | bigint */
+        startDate: Date,
+        endDate: Date,
+    ): ReadonlyArray<DateTimeRangeFormatPart>
+
+    fun formatRangeToParts(
+        startDate: Number,
+        endDate: Number,
+    ): ReadonlyArray<DateTimeRangeFormatPart>
+
+    fun formatRangeToParts(
+        startDate: BigInt,
+        endDate: BigInt,
     ): ReadonlyArray<DateTimeRangeFormatPart>
 }
--- kotlin-js/src/main/generated/js/intl/NumberFormat.kt
+++ kotlin-js/src/main/generated/js/intl/NumberFormat.kt
@@ -2,17 +2,29 @@
 
 package js.intl
 
+import js.core.BigInt
 import js.core.ReadonlyArray
 
 sealed external interface NumberFormat {
-    fun formatToParts(number: Number /* | BigInt */ = definedExternally): ReadonlyArray<NumberFormatPart>
+    fun formatToParts(number: Number = definedExternally): ReadonlyArray<NumberFormatPart>
+    fun formatToParts(number: BigInt): ReadonlyArray<NumberFormatPart>
     fun formatRange(
-        start: Number, /* | BigInt */
-        end: Number, /* | BigInt */
+        start: Number,
+        end: Number,
+    ): String
+
+    fun formatRange(
+        start: BigInt,
+        end: BigInt,
     ): String
 
     fun formatRangeToParts(
-        start: Number, /* | BigInt */
-        end: Number, /* | BigInt */
+        start: Number,
+        end: Number,
+    ): ReadonlyArray<NumberRangeFormatPart>
+
+    fun formatRangeToParts(
+        start: BigInt,
+        end: BigInt,
     ): ReadonlyArray<NumberRangeFormatPart>
 }
