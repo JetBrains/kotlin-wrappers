865cef6a773286d04ec57bb7b0c4e0ddd8078fdb
Victor Turansky <victor.turansky@gmail.com>
2022-12-11 00:01:34 +0200

    HTML. Custom elements support [generated]

---
+++ kotlin-browser/src/main/generated/dom/html/CustomElementRegistry.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package dom.html
+
+import dom.Node
+import kotlin.js.Promise
+
+sealed external class CustomElementRegistry {
+    fun define(
+        name: String,
+        constructor: CustomElementConstructor,
+        options: ElementDefinitionOptions = definedExternally,
+    )
+
+    fun get(name: String): CustomElementConstructor?
+    fun upgrade(root: Node)
+    fun whenDefined(name: String): Promise<CustomElementConstructor>
+}
---
+++ kotlin-browser/src/main/generated/dom/html/ElementDefinitionOptions.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package dom.html
+
+sealed external interface ElementDefinitionOptions {
+    var extends: String?
+}
