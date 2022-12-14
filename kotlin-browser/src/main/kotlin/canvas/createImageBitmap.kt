f865d3b093225b3279fb3edd8fd5a41baa183255
Victor Turansky <victor.turansky@gmail.com>
2022-12-14 21:29:31 +0200

    Canvas. `createImageBitmap`

---
+++ kotlin-browser/src/main/kotlin/canvas/createImageBitmap.kt
@@ -0,0 +1,17 @@
+package canvas
+
+import kotlin.js.Promise
+
+external fun createImageBitmap(
+    image: ImageBitmapSource,
+    options: ImageBitmapOptions = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: ImageBitmapSource,
+    sx: Int,
+    sy: Int,
+    sw: Int,
+    sh: Int,
+    options: ImageBitmapOptions = definedExternally,
+): Promise<ImageBitmap>
