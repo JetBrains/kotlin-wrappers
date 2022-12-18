3603eeaf132a5f04deb998fb33615e5c1425c39f
Victor Turansky <victor.turansky@gmail.com>
2022-12-18 21:52:09 +0200

    Web Workers. Event types [generated]

---
+++ kotlin-browser/src/main/generated/web/workers/Event.types.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+package web.workers
+
+import web.events.Event
+import web.events.EventType
+
+inline val Event.Companion.LANGUAGE_CHANGE: EventType<Event>
+    get() = EventType("languagechange")
+
+inline val Event.Companion.OFFLINE: EventType<Event>
+    get() = EventType("offline")
+
+inline val Event.Companion.ONLINE: EventType<Event>
+    get() = EventType("online")
