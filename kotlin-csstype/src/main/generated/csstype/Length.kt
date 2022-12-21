ea13c0bfd11c5264a65fab629bd200fe1e50cabb
Victor Turansky <victor.turansky@gmail.com>
2022-12-21 04:11:03 +0200

    CSSType. `Length` factories [generated]

--- kotlin-csstype/src/main/generated/csstype/Length.kt
+++ kotlin-csstype/src/main/generated/csstype/Length.kt
@@ -32,11 +32,83 @@ inline val Number.ex: Length
 inline val Number.rem: Length
     get() = "${this}rem".unsafeCast<Length>()
 
-inline val Number.vh: Length
-    get() = "${this}vh".unsafeCast<Length>()
+inline val Number.cqw: Length
+    get() = "${this}cqw".unsafeCast<Length>()
+
+inline val Number.cqh: Length
+    get() = "${this}cqh".unsafeCast<Length>()
+
+inline val Number.cqi: Length
+    get() = "${this}cqi".unsafeCast<Length>()
+
+inline val Number.cqb: Length
+    get() = "${this}cqb".unsafeCast<Length>()
+
+inline val Number.cqmin: Length
+    get() = "${this}cqmin".unsafeCast<Length>()
+
+inline val Number.cqmax: Length
+    get() = "${this}cqmax".unsafeCast<Length>()
+
+inline val Number.dvw: Length
+    get() = "${this}dvw".unsafeCast<Length>()
+
+inline val Number.dvh: Length
+    get() = "${this}dvh".unsafeCast<Length>()
+
+inline val Number.dvi: Length
+    get() = "${this}dvi".unsafeCast<Length>()
+
+inline val Number.dvb: Length
+    get() = "${this}dvb".unsafeCast<Length>()
+
+inline val Number.dvmin: Length
+    get() = "${this}dvmin".unsafeCast<Length>()
+
+inline val Number.dvmax: Length
+    get() = "${this}dvmax".unsafeCast<Length>()
+
+inline val Number.lvw: Length
+    get() = "${this}lvw".unsafeCast<Length>()
+
+inline val Number.lvh: Length
+    get() = "${this}lvh".unsafeCast<Length>()
+
+inline val Number.lvi: Length
+    get() = "${this}lvi".unsafeCast<Length>()
+
+inline val Number.lvb: Length
+    get() = "${this}lvb".unsafeCast<Length>()
+
+inline val Number.lvmin: Length
+    get() = "${this}lvmin".unsafeCast<Length>()
+
+inline val Number.lvmax: Length
+    get() = "${this}lvmax".unsafeCast<Length>()
+
+inline val Number.svw: Length
+    get() = "${this}svw".unsafeCast<Length>()
+
+inline val Number.svh: Length
+    get() = "${this}svh".unsafeCast<Length>()
+
+inline val Number.svi: Length
+    get() = "${this}svi".unsafeCast<Length>()
+
+inline val Number.svb: Length
+    get() = "${this}svb".unsafeCast<Length>()
+
+inline val Number.svmin: Length
+    get() = "${this}svmin".unsafeCast<Length>()
+
+inline val Number.svmax: Length
+    get() = "${this}svmax".unsafeCast<Length>()
 
 inline val Number.vw: Length
     get() = "${this}vw".unsafeCast<Length>()
+
+inline val Number.vh: Length
+    get() = "${this}vh".unsafeCast<Length>()
 
 inline val Number.vmin: Length
     get() = "${this}vmin".unsafeCast<Length>()
