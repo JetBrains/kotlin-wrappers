e5ed36fd90b8244f3c2019166db86e5d25b1b7f3
Victor Turansky <victor.turansky@gmail.com>
2022-12-27 00:40:32 +0200

    Node `18.11.18` [generated]

--- kotlin-node/src/main/generated/node/childProcess/CommonOptions.kt
+++ kotlin-node/src/main/generated/node/childProcess/CommonOptions.kt
@@ -4,7 +4,7 @@ package node.childProcess
 
 sealed external interface CommonOptions : ProcessEnvOptions {
     /**
-     * @default true
+     * @default false
      */
     var windowsHide: Boolean?
 
