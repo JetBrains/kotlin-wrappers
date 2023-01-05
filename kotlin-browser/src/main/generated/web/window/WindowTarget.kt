8db5d698751c9e5ecbefa38f3de08bbffec083ea
Victor Turansky <victor.turansky@gmail.com>
2023-01-05 22:58:28 +0200

    Window. `WindowTarget` - sealed interface [generated]

--- kotlin-browser/src/main/generated/web/window/WindowTarget.kt
+++ kotlin-browser/src/main/generated/web/window/WindowTarget.kt
@@ -1,15 +1,19 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package web.window
 
 @Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
 @JsName("""(/*union*/{_self: '_self', _blank: '_blank', _parent: '_parent', _top: '_top'}/*union*/)""")
-external enum class WindowTarget {
-    _self,
-    _blank,
-    _parent,
-    _top,
-
-    ;
+sealed external interface WindowTarget {
+    companion object {
+        val _self: WindowTarget
+        val _blank: WindowTarget
+        val _parent: WindowTarget
+        val _top: WindowTarget
+    }
 }
