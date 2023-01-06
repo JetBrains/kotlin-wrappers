35578b9bc1bcd0f1c558e47eefca8db4be627b0f
Victor Turansky <victor.turansky@gmail.com>
2023-01-06 06:10:09 +0200

    JS. `Object.hasOwn`

--- kotlin-js/src/main/kotlin/js/core/Object.kt
+++ kotlin-js/src/main/kotlin/js/core/Object.kt
@@ -18,6 +18,9 @@ external class Object internal constructor() {
         fun <T> getOwnPropertyDescriptor(o: Any, p: String): PropertyDescriptor<T>
         fun getOwnPropertyNames(o: Any): ReadonlyArray<String>
         fun <P : Any, T : P> getPrototypeOf(o: T): P
+        fun hasOwn(o: Any, v: String /* PropertyKey */): Boolean
+        fun hasOwn(o: Any, v: Number /* PropertyKey */): Boolean
+        fun hasOwn(o: Any, v: Symbol /* PropertyKey */): Boolean
         fun `is`(value1: Any?, value2: Any?): Boolean
         fun isExtensible(o: Any): Boolean
         fun isFrozen(o: Any): Boolean
