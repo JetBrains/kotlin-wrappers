8861953c610ddcdc7b3aba9a5af884eced831d57
Victor Turansky <victor.turansky@gmail.com>
2023-01-06 05:33:57 +0200

    JS. `WeakRef`

---
+++ kotlin-js/src/main/kotlin/js/core/WeakRef.kt
@@ -0,0 +1,11 @@
+package js.core
+
+external class WeakRef<T : Any>(
+    target: T
+) {
+    /**
+     * Returns the WeakRef instance's target object, or undefined if the target object has been
+     * reclaimed.
+     */
+    fun deref(): T?
+}
