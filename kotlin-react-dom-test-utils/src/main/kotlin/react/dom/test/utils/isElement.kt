04dde15b69fc4ecb2da17bf1c742432b167760a7
Victor Turansky <victor.turansky@gmail.com>
2023-01-09 00:32:46 +0200

    React DOM Test Utilities. `isElement`

---
+++ kotlin-react-dom-test-utils/src/main/kotlin/react/dom/test/utils/isElement.kt
@@ -0,0 +1,11 @@
+@file:JsModule("react-dom/test-utils")
+@file:JsNonModule
+
+package react.dom.test.utils
+
+/**
+ * Returns `true` if `element` is any React element.
+ */
+external fun isElement(
+    element: Any?
+): Boolean
