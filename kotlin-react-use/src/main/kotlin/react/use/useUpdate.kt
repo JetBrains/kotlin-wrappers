6e5822f4b99c19ae5a1b7c1dbbddf8d240de9776
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 02:46:34 +0200

    React Use. `useUpdate`

---
+++ kotlin-react-use/src/main/kotlin/react/use/useUpdate.kt
@@ -0,0 +1,17 @@
+package react.use
+
+import js.core.Void
+import react.useReducer
+
+private val updateReducer: (Int, Void) -> Int = { num, _ ->
+    (num + 1) % 1_000_000
+}
+
+/**
+ * [Original](https://github.com/streamich/react-use/blob/master/src/useUpdate.ts)
+ */
+fun useUpdate(): () -> Unit {
+    val (_, update) = useReducer(updateReducer, 0)
+
+    return update.unsafeCast<() -> Unit>()
+}
