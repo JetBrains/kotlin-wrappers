d7b58f6b66001fe3bbea605d8c7b3192fdcef66a
Victor Turansky <victor.turansky@gmail.com>
2023-01-13 03:10:54 +0200

    React Use. `useUnmount` -> `useUnmountEffect`

--- kotlin-react-use/src/main/kotlin/react/use/useUnmountEffect.kt
+++ kotlin-react-use/src/main/kotlin/react/use/useUnmountEffect.kt
@@ -5,7 +5,7 @@ import react.useEffectOnce
 /**
  * [Original](https://github.com/streamich/react-use/blob/master/src/useUnmount.ts)
  */
-fun useUnmount(
+fun useUnmountEffect(
     callback: () -> Unit,
 ) {
     val latestCallback by useLatest(callback)
