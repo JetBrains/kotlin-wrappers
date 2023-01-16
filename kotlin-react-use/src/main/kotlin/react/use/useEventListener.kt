b255e50b4f3bb2e714a30daa7d329c3a88d65551
Victor Turansky <victor.turansky@gmail.com>
2023-01-16 03:37:48 +0200

    React Use. `useEventListener`

---
+++ kotlin-react-use/src/main/kotlin/react/use/useEventListener.kt
@@ -0,0 +1,28 @@
+package react.use
+
+import react.useEffect
+import web.events.*
+
+/**
+ * [Original](https://usehooks-ts.com/react-hook/use-event-listener)
+ */
+fun <T : Event> useEventListener(
+    target: EventTarget?,
+    type: EventType<T>,
+    options: AddEventListenerOptions? = undefined,
+    handler: EventHandler<T>,
+) {
+    val latestHandler by useLatest(handler)
+
+    useEffect(target, type) {
+        target ?: return@useEffect
+
+        cleanup(
+            target.addEventHandler(
+                type = type,
+                options = options,
+                handler = { latestHandler(it) },
+            )
+        )
+    }
+}
