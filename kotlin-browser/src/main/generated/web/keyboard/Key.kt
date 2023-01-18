cdce1183328f0c68f64eca1980c1d044a202d08d
Victor Turansky <victor.turansky@gmail.com>
2023-01-19 01:55:33 +0200

    Keyboard. `Key` [generated]

---
+++ kotlin-browser/src/main/generated/web/keyboard/Key.kt
@@ -0,0 +1,491 @@
+// Automatically generated - do not modify!
+
+package web.keyboard
+
+object Key {
+    inline val Backquote: KeyCode
+        get() = KeyCode("Backquote")
+
+    inline val Backslash: KeyCode
+        get() = KeyCode("Backslash")
+
+    inline val Backspace: KeyCode
+        get() = KeyCode("Backspace")
+
+    inline val BracketLeft: KeyCode
+        get() = KeyCode("BracketLeft")
+
+    inline val BracketRight: KeyCode
+        get() = KeyCode("BracketRight")
+
+    inline val Comma: KeyCode
+        get() = KeyCode("Comma")
+
+    inline val Digit0: KeyCode
+        get() = KeyCode("Digit0")
+
+    inline val Digit1: KeyCode
+        get() = KeyCode("Digit1")
+
+    inline val Digit2: KeyCode
+        get() = KeyCode("Digit2")
+
+    inline val Digit3: KeyCode
+        get() = KeyCode("Digit3")
+
+    inline val Digit4: KeyCode
+        get() = KeyCode("Digit4")
+
+    inline val Digit5: KeyCode
+        get() = KeyCode("Digit5")
+
+    inline val Digit6: KeyCode
+        get() = KeyCode("Digit6")
+
+    inline val Digit7: KeyCode
+        get() = KeyCode("Digit7")
+
+    inline val Digit8: KeyCode
+        get() = KeyCode("Digit8")
+
+    inline val Digit9: KeyCode
+        get() = KeyCode("Digit9")
+
+    inline val Equal: KeyCode
+        get() = KeyCode("Equal")
+
+    inline val IntlBackslash: KeyCode
+        get() = KeyCode("IntlBackslash")
+
+    inline val IntlRo: KeyCode
+        get() = KeyCode("IntlRo")
+
+    inline val IntlYen: KeyCode
+        get() = KeyCode("IntlYen")
+
+    inline val KeyA: KeyCode
+        get() = KeyCode("KeyA")
+
+    inline val KeyB: KeyCode
+        get() = KeyCode("KeyB")
+
+    inline val KeyC: KeyCode
+        get() = KeyCode("KeyC")
+
+    inline val KeyD: KeyCode
+        get() = KeyCode("KeyD")
+
+    inline val KeyE: KeyCode
+        get() = KeyCode("KeyE")
+
+    inline val KeyF: KeyCode
+        get() = KeyCode("KeyF")
+
+    inline val KeyG: KeyCode
+        get() = KeyCode("KeyG")
+
+    inline val KeyH: KeyCode
+        get() = KeyCode("KeyH")
+
+    inline val KeyI: KeyCode
+        get() = KeyCode("KeyI")
+
+    inline val KeyJ: KeyCode
+        get() = KeyCode("KeyJ")
+
+    inline val KeyK: KeyCode
+        get() = KeyCode("KeyK")
+
+    inline val KeyL: KeyCode
+        get() = KeyCode("KeyL")
+
+    inline val KeyM: KeyCode
+        get() = KeyCode("KeyM")
+
+    inline val KeyN: KeyCode
+        get() = KeyCode("KeyN")
+
+    inline val KeyO: KeyCode
+        get() = KeyCode("KeyO")
+
+    inline val KeyP: KeyCode
+        get() = KeyCode("KeyP")
+
+    inline val KeyQ: KeyCode
+        get() = KeyCode("KeyQ")
+
+    inline val KeyR: KeyCode
+        get() = KeyCode("KeyR")
+
+    inline val KeyS: KeyCode
+        get() = KeyCode("KeyS")
+
+    inline val KeyT: KeyCode
+        get() = KeyCode("KeyT")
+
+    inline val KeyU: KeyCode
+        get() = KeyCode("KeyU")
+
+    inline val KeyV: KeyCode
+        get() = KeyCode("KeyV")
+
+    inline val KeyW: KeyCode
+        get() = KeyCode("KeyW")
+
+    inline val KeyX: KeyCode
+        get() = KeyCode("KeyX")
+
+    inline val KeyY: KeyCode
+        get() = KeyCode("KeyY")
+
+    inline val KeyZ: KeyCode
+        get() = KeyCode("KeyZ")
+
+    inline val Minus: KeyCode
+        get() = KeyCode("Minus")
+
+    inline val Period: KeyCode
+        get() = KeyCode("Period")
+
+    inline val Quote: KeyCode
+        get() = KeyCode("Quote")
+
+    inline val Semicolon: KeyCode
+        get() = KeyCode("Semicolon")
+
+    inline val Slash: KeyCode
+        get() = KeyCode("Slash")
+
+    inline val AltLeft: KeyCode
+        get() = KeyCode("AltLeft")
+
+    inline val AltRight: KeyCode
+        get() = KeyCode("AltRight")
+
+    inline val CapsLock: KeyCode
+        get() = KeyCode("CapsLock")
+
+    inline val ContextMenu: KeyCode
+        get() = KeyCode("ContextMenu")
+
+    inline val ControlLeft: KeyCode
+        get() = KeyCode("ControlLeft")
+
+    inline val ControlRight: KeyCode
+        get() = KeyCode("ControlRight")
+
+    inline val Enter: KeyCode
+        get() = KeyCode("Enter")
+
+    inline val MetaLeft: KeyCode
+        get() = KeyCode("MetaLeft")
+
+    inline val MetaRight: KeyCode
+        get() = KeyCode("MetaRight")
+
+    inline val ShiftLeft: KeyCode
+        get() = KeyCode("ShiftLeft")
+
+    inline val ShiftRight: KeyCode
+        get() = KeyCode("ShiftRight")
+
+    inline val Space: KeyCode
+        get() = KeyCode("Space")
+
+    inline val Tab: KeyCode
+        get() = KeyCode("Tab")
+
+    inline val Convert: KeyCode
+        get() = KeyCode("Convert")
+
+    inline val KanaMode: KeyCode
+        get() = KeyCode("KanaMode")
+
+    inline val Lang1: KeyCode
+        get() = KeyCode("Lang1")
+
+    inline val Lang2: KeyCode
+        get() = KeyCode("Lang2")
+
+    inline val Lang3: KeyCode
+        get() = KeyCode("Lang3")
+
+    inline val Lang4: KeyCode
+        get() = KeyCode("Lang4")
+
+    inline val Lang5: KeyCode
+        get() = KeyCode("Lang5")
+
+    inline val NonConvert: KeyCode
+        get() = KeyCode("NonConvert")
+
+    inline val OSLeft: KeyCode
+        get() = KeyCode("OSLeft")
+
+    inline val OSRight: KeyCode
+        get() = KeyCode("OSRight")
+
+    inline val Delete: KeyCode
+        get() = KeyCode("Delete")
+
+    inline val End: KeyCode
+        get() = KeyCode("End")
+
+    inline val Help: KeyCode
+        get() = KeyCode("Help")
+
+    inline val Home: KeyCode
+        get() = KeyCode("Home")
+
+    inline val Insert: KeyCode
+        get() = KeyCode("Insert")
+
+    inline val PageDown: KeyCode
+        get() = KeyCode("PageDown")
+
+    inline val PageUp: KeyCode
+        get() = KeyCode("PageUp")
+
+    inline val ArrowDown: KeyCode
+        get() = KeyCode("ArrowDown")
+
+    inline val ArrowLeft: KeyCode
+        get() = KeyCode("ArrowLeft")
+
+    inline val ArrowRight: KeyCode
+        get() = KeyCode("ArrowRight")
+
+    inline val ArrowUp: KeyCode
+        get() = KeyCode("ArrowUp")
+
+    inline val NumLock: KeyCode
+        get() = KeyCode("NumLock")
+
+    inline val Numpad0: KeyCode
+        get() = KeyCode("Numpad0")
+
+    inline val Numpad1: KeyCode
+        get() = KeyCode("Numpad1")
+
+    inline val Numpad2: KeyCode
+        get() = KeyCode("Numpad2")
+
+    inline val Numpad3: KeyCode
+        get() = KeyCode("Numpad3")
+
+    inline val Numpad4: KeyCode
+        get() = KeyCode("Numpad4")
+
+    inline val Numpad5: KeyCode
+        get() = KeyCode("Numpad5")
+
+    inline val Numpad6: KeyCode
+        get() = KeyCode("Numpad6")
+
+    inline val Numpad7: KeyCode
+        get() = KeyCode("Numpad7")
+
+    inline val Numpad8: KeyCode
+        get() = KeyCode("Numpad8")
+
+    inline val Numpad9: KeyCode
+        get() = KeyCode("Numpad9")
+
+    inline val NumpadAdd: KeyCode
+        get() = KeyCode("NumpadAdd")
+
+    inline val NumpadBackspace: KeyCode
+        get() = KeyCode("NumpadBackspace")
+
+    inline val NumpadClear: KeyCode
+        get() = KeyCode("NumpadClear")
+
+    inline val NumpadClearEntry: KeyCode
+        get() = KeyCode("NumpadClearEntry")
+
+    inline val NumpadComma: KeyCode
+        get() = KeyCode("NumpadComma")
+
+    inline val NumpadDecimal: KeyCode
+        get() = KeyCode("NumpadDecimal")
+
+    inline val NumpadDivide: KeyCode
+        get() = KeyCode("NumpadDivide")
+
+    inline val NumpadEnter: KeyCode
+        get() = KeyCode("NumpadEnter")
+
+    inline val NumpadEqual: KeyCode
+        get() = KeyCode("NumpadEqual")
+
+    inline val NumpadHash: KeyCode
+        get() = KeyCode("NumpadHash")
+
+    inline val NumpadMemoryAdd: KeyCode
+        get() = KeyCode("NumpadMemoryAdd")
+
+    inline val NumpadMemoryClear: KeyCode
+        get() = KeyCode("NumpadMemoryClear")
+
+    inline val NumpadMemoryRecall: KeyCode
+        get() = KeyCode("NumpadMemoryRecall")
+
+    inline val NumpadMemoryStore: KeyCode
+        get() = KeyCode("NumpadMemoryStore")
+
+    inline val NumpadMemorySubtract: KeyCode
+        get() = KeyCode("NumpadMemorySubtract")
+
+    inline val NumpadMultiply: KeyCode
+        get() = KeyCode("NumpadMultiply")
+
+    inline val NumpadParenLeft: KeyCode
+        get() = KeyCode("NumpadParenLeft")
+
+    inline val NumpadParenRight: KeyCode
+        get() = KeyCode("NumpadParenRight")
+
+    inline val NumpadStar: KeyCode
+        get() = KeyCode("NumpadStar")
+
+    inline val NumpadSubtract: KeyCode
+        get() = KeyCode("NumpadSubtract")
+
+    inline val Escape: KeyCode
+        get() = KeyCode("Escape")
+
+    inline val F1: KeyCode
+        get() = KeyCode("F1")
+
+    inline val F2: KeyCode
+        get() = KeyCode("F2")
+
+    inline val F3: KeyCode
+        get() = KeyCode("F3")
+
+    inline val F4: KeyCode
+        get() = KeyCode("F4")
+
+    inline val F5: KeyCode
+        get() = KeyCode("F5")
+
+    inline val F6: KeyCode
+        get() = KeyCode("F6")
+
+    inline val F7: KeyCode
+        get() = KeyCode("F7")
+
+    inline val F8: KeyCode
+        get() = KeyCode("F8")
+
+    inline val F9: KeyCode
+        get() = KeyCode("F9")
+
+    inline val F10: KeyCode
+        get() = KeyCode("F10")
+
+    inline val F11: KeyCode
+        get() = KeyCode("F11")
+
+    inline val F12: KeyCode
+        get() = KeyCode("F12")
+
+    inline val F13: KeyCode
+        get() = KeyCode("F13")
+
+    inline val F14: KeyCode
+        get() = KeyCode("F14")
+
+    inline val F15: KeyCode
+        get() = KeyCode("F15")
+
+    inline val Fn: KeyCode
+        get() = KeyCode("Fn")
+
+    inline val FnLock: KeyCode
+        get() = KeyCode("FnLock")
+
+    inline val PrintScreen: KeyCode
+        get() = KeyCode("PrintScreen")
+
+    inline val ScrollLock: KeyCode
+        get() = KeyCode("ScrollLock")
+
+    inline val Pause: KeyCode
+        get() = KeyCode("Pause")
+
+    inline val BrowserBack: KeyCode
+        get() = KeyCode("BrowserBack")
+
+    inline val BrowserFavorites: KeyCode
+        get() = KeyCode("BrowserFavorites")
+
+    inline val BrowserForward: KeyCode
+        get() = KeyCode("BrowserForward")
+
+    inline val BrowserHome: KeyCode
+        get() = KeyCode("BrowserHome")
+
+    inline val BrowserRefresh: KeyCode
+        get() = KeyCode("BrowserRefresh")
+
+    inline val BrowserSearch: KeyCode
+        get() = KeyCode("BrowserSearch")
+
+    inline val BrowserStop: KeyCode
+        get() = KeyCode("BrowserStop")
+
+    inline val Eject: KeyCode
+        get() = KeyCode("Eject")
+
+    inline val LaunchApp1: KeyCode
+        get() = KeyCode("LaunchApp1")
+
+    inline val LaunchApp2: KeyCode
+        get() = KeyCode("LaunchApp2")
+
+    inline val LaunchMail: KeyCode
+        get() = KeyCode("LaunchMail")
+
+    inline val MediaPlayPause: KeyCode
+        get() = KeyCode("MediaPlayPause")
+
+    inline val MediaSelect: KeyCode
+        get() = KeyCode("MediaSelect")
+
+    inline val MediaStop: KeyCode
+        get() = KeyCode("MediaStop")
+
+    inline val MediaTrackNext: KeyCode
+        get() = KeyCode("MediaTrackNext")
+
+    inline val MediaTrackPrevious: KeyCode
+        get() = KeyCode("MediaTrackPrevious")
+
+    inline val Power: KeyCode
+        get() = KeyCode("Power")
+
+    inline val Sleep: KeyCode
+        get() = KeyCode("Sleep")
+
+    inline val AudioVolumeDown: KeyCode
+        get() = KeyCode("AudioVolumeDown")
+
+    inline val AudioVolumeMute: KeyCode
+        get() = KeyCode("AudioVolumeMute")
+
+    inline val AudioVolumeUp: KeyCode
+        get() = KeyCode("AudioVolumeUp")
+
+    inline val WakeUp: KeyCode
+        get() = KeyCode("WakeUp")
+
+    inline val Hiragana: KeyCode
+        get() = KeyCode("Hiragana")
+
+    inline val Katakana: KeyCode
+        get() = KeyCode("Katakana")
+
+    inline val Unidentified: KeyCode
+        get() = KeyCode("Unidentified")
+}
