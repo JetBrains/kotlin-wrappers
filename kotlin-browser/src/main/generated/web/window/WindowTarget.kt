0648d5b71e51524cfa4053ef0cc851eb56492432
Victor Turansky <victor.turansky@gmail.com>
2023-01-18 21:38:02 +0200

    Browser. Regenerate `WindowTarget`

--- kotlin-browser/src/main/generated/web/window/WindowTarget.kt
+++ kotlin-browser/src/main/generated/web/window/WindowTarget.kt
@@ -1,12 +1,12 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package web.window
 
-@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
 @JsName("""(/*union*/{_self: '_self', _blank: '_blank', _parent: '_parent', _top: '_top'}/*union*/)""")
 sealed external interface WindowTarget {
