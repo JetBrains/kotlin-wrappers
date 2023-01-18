d1504e00508dc6dfd82dcf7c2ef087661d7bdb94
Victor Turansky <victor.turansky@gmail.com>
2023-01-18 23:46:43 +0200

    Keyboard. `ModifierKey` object [generated]

---
+++ kotlin-browser/src/main/generated/web/keyboard/ModifierKey.kt
@@ -0,0 +1,41 @@
+// Automatically generated - do not modify!
+
+package web.keyboard
+
+object ModifierKey {
+    inline val Alt: ModifierKeyCode
+        get() = ModifierKeyCode("Alt")
+
+    inline val AltGraph: ModifierKeyCode
+        get() = ModifierKeyCode("AltGraph")
+
+    inline val CapsLock: ModifierKeyCode
+        get() = ModifierKeyCode("CapsLock")
+
+    inline val Control: ModifierKeyCode
+        get() = ModifierKeyCode("Control")
+
+    inline val Fn: ModifierKeyCode
+        get() = ModifierKeyCode("Fn")
+
+    inline val FnLock: ModifierKeyCode
+        get() = ModifierKeyCode("FnLock")
+
+    inline val Meta: ModifierKeyCode
+        get() = ModifierKeyCode("Meta")
+
+    inline val NumLock: ModifierKeyCode
+        get() = ModifierKeyCode("NumLock")
+
+    inline val ScrollLock: ModifierKeyCode
+        get() = ModifierKeyCode("ScrollLock")
+
+    inline val Shift: ModifierKeyCode
+        get() = ModifierKeyCode("Shift")
+
+    inline val Symbol: ModifierKeyCode
+        get() = ModifierKeyCode("Symbol")
+
+    inline val SymbolLock: ModifierKeyCode
+        get() = ModifierKeyCode("SymbolLock")
+}
--- kotlin-browser/src/main/generated/web/keyboard/ModifierKeyCode.kt
+++ kotlin-browser/src/main/generated/web/keyboard/ModifierKeyCode.kt
@@ -1,26 +1,14 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
-    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NOTHING_TO_INLINE",
 )
 
 package web.keyboard
 
-// language=JavaScript
-@JsName("""(/*union*/{Alt: 'Alt', AltGraph: 'AltGraph', CapsLock: 'CapsLock', Control: 'Control', Fn: 'Fn', FnLock: 'FnLock', Meta: 'Meta', NumLock: 'NumLock', ScrollLock: 'ScrollLock', Shift: 'Shift', Symbol: 'Symbol', SymbolLock: 'SymbolLock'}/*union*/)""")
-external enum class ModifierKeyCode {
-    Alt,
-    AltGraph,
-    CapsLock,
-    Control,
-    Fn,
-    FnLock,
-    Meta,
-    NumLock,
-    ScrollLock,
-    Shift,
-    Symbol,
-    SymbolLock,
+sealed external interface ModifierKeyCode
 
-    ;
-}
+inline fun ModifierKeyCode(
+    code: String,
+): ModifierKeyCode =
+    code.unsafeCast<ModifierKeyCode>()
