7d9b50258ee046c621511e803287cd2e2956e273
Victor Turansky <victor.turansky@gmail.com>
2023-01-22 03:27:51 +0200

    HTML. Update formatting [generated]

--- kotlin-browser/src/main/generated/web/html/CrossOrigin.kt
+++ kotlin-browser/src/main/generated/web/html/CrossOrigin.kt
@@ -1,8 +1,11 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
 package web.html
 
-@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
 @JsName("""(/*union*/{anonymous: 'anonymous', useCredentials: 'use-credentials', none: ''}/*union*/)""")
 external enum class CrossOrigin {
