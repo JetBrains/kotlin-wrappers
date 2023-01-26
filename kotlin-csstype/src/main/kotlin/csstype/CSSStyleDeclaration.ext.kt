345d4899b28f8cb7f2baca7e39e82c9f35169ee6
Victor Turansky <victor.turansky@gmail.com>
2023-01-26 03:12:44 +0200

    CSSType. `CSSStyleDeclaration` extensions

---
+++ kotlin-csstype/src/main/kotlin/csstype/CSSStyleDeclaration.ext.kt
@@ -0,0 +1,25 @@
+package csstype
+
+import web.cssom.CSSStyleDeclaration
+
+operator fun <T : Any> CSSStyleDeclaration.get(
+    variable: Variable<T>,
+): T? {
+    val value = getPropertyValue(
+        property = variable.unsafeCast<String>(),
+    )
+
+    return if (value.isNotEmpty()) {
+        value.unsafeCast<T?>()
+    } else null
+}
+
+inline operator fun <T : Any> CSSStyleDeclaration.set(
+    variable: Variable<T>,
+    value: T?,
+) {
+    setProperty(
+        property = variable.unsafeCast<String>(),
+        value = value.unsafeCast<String?>(),
+    )
+}
