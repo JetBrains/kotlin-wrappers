680c56647e8f27de0c04f7dd022628171b05c090
tretikoff <37001594+tretikoff@users.noreply.github.com>
2023-01-27 14:23:40 +0200

    Fix failing tests in ci (#1886)
    * Update tests and CSS access typing
    
    * Optimized karma runs
    
    Co-authored-by: tretikoff <Konstantin.Tretiakov@jetbrains.com>

--- kotlin-styled-next/karma.config.d/config.js
+++ kotlin-styled-next/karma.config.d/config.js
@@ -14,5 +14,5 @@ config.client.mocha = config.client.mocha || {}
 config.client.mocha.timeout = 60000
 
 config.browserNoActivityTimeout = 100000
-config.browserDisconnectTimeout = 50000
-config.browserDisconnectTolerance = 3
+config.browserDisconnectTimeout = 100000
+config.browserDisconnectTolerance = 2
