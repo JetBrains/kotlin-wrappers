342e5d96eeb65449e0634dd860a99de3502c3cea
Victor Turansky <victor.turansky@gmail.com>
2023-01-28 20:46:18 +0200

    JS. Use `ReadonlyRecord` in static methods

--- kotlin-js/src/main/kotlin/js/core/Object.kt
+++ kotlin-js/src/main/kotlin/js/core/Object.kt
@@ -12,7 +12,7 @@ external class Object internal constructor() {
         fun <T : Any> create(o: T?, properties: dynamic = definedExternally): T
         fun <T : Any> defineProperties(o: T, properties: dynamic): T
         fun <T : Any, P> defineProperty(o: T, p: String, attributes: PropertyDescriptor<P>): T
-        fun <T : Any> entries(o: Record<String, T>): ReadonlyArray<JsTuple2<String, T>>
+        fun <T : Any> entries(o: ReadonlyRecord<String, T>): ReadonlyArray<JsTuple2<String, T>>
         fun entries(o: Any): ReadonlyArray<JsTuple2<String, Any?>>
         fun <R, T : R> freeze(o: T): R
         fun <T> getOwnPropertyDescriptor(o: Any, p: String): PropertyDescriptor<T>
@@ -29,7 +29,7 @@ external class Object internal constructor() {
         fun keys(o: Any): ReadonlyArray<String>
         fun <T> preventExtensions(o: T): T
         fun <T> seal(o: T): T
-        fun <T : Any> values(o: Record<*, T>): ReadonlyArray<T>
+        fun <T : Any> values(o: ReadonlyRecord<*, T>): ReadonlyArray<T>
         fun values(o: Any): ReadonlyArray<Any?>
     }
 }
