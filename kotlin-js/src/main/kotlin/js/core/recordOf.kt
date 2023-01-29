fe8c0b1f08d958c4a928d5ccf1c18b9ecec07ae7
Victor Turansky <victor.turansky@gmail.com>
2023-01-29 03:31:44 +0200

    JS. `recordOf` returns `Record`

--- kotlin-js/src/main/kotlin/js/core/recordOf.kt
+++ kotlin-js/src/main/kotlin/js/core/recordOf.kt
@@ -2,7 +2,7 @@ package js.core
 
 fun <K : Any, V : Any> recordOf(
     vararg pairs: Pair<K, V>
-): ReadonlyRecord<K, V> {
+): Record<K, V> {
     val record = Record<K, V>()
 
     for ((key, value) in pairs) {
