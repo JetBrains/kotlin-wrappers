4524aaf0151ac7b1f04c79eb4eaeabcfdef1cbe8
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2023-01-30 22:23:29 +0200

    React Router: `RenderErrorBoundary` is converted (#1895)

---
+++ kotlin-react-router/src/main/generated/react/router/RenderErrorBoundaryProps.kt
@@ -0,0 +1,20 @@
+@file:JsModule("react-router")
+@file:JsNonModule
+
+@file:Suppress(
+    "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package react.router
+
+import remix.run.router.Location
+
+
+external interface RenderErrorBoundaryProps : react.PropsWithChildren {
+    var location: Location
+    var error: Any?
+    var component: react.ReactNode
+    var routeContext: RouteContextObject
+}
+
