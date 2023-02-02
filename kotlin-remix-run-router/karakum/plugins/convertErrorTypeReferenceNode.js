b35de27fd0123dc692b32242905ebe46d7bdd88b
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-02-02 22:16:18 +0200

    React Router: remove redundant parent checks

--- kotlin-remix-run-router/karakum/plugins/convertErrorTypeReferenceNode.js
+++ kotlin-remix-run-router/karakum/plugins/convertErrorTypeReferenceNode.js
@@ -3,9 +3,7 @@ const ts = require("typescript");
 module.exports = function (node) {
     if (
         ts.isIdentifier(node)
-        && node.parent
         && ts.isExpressionWithTypeArguments(node.parent)
-        && node.parent.parent
         && ts.isHeritageClause(node.parent.parent)
         && node.text === "Error"
     ) {
