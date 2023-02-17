1b0f6de082b66890390f31b0be75c16f19eb9f1b
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-02-17 16:57:29 +0200

    React Router: JsNonModule annotation is removed

--- kotlin-react-router/src/jsMain/generated/react/router/useHref.kt
+++ kotlin-react-router/src/jsMain/generated/react/router/useHref.kt
@@ -1,5 +1,4 @@
 @file:JsModule("react-router")
-@file:JsNonModule
 
 @file:Suppress(
     "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
--- kotlin-remix-run-router/src/jsMain/generated/remix/run/router/BlockerProceeding.kt
+++ kotlin-remix-run-router/src/jsMain/generated/remix/run/router/BlockerProceeding.kt
@@ -1,5 +1,4 @@
 @file:JsModule("@remix-run/router")
-@file:JsNonModule
 
 @file:Suppress(
     "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
