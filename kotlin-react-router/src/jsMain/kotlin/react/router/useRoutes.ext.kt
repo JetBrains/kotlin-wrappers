1972627881fa2ab710e7f31ee5ba75deb9cc7741
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-02-18 20:58:19 +0200

    React Router: declarations improvements

--- kotlin-react-router/src/jsMain/kotlin/react/router/useRoutes.ext.kt
+++ kotlin-react-router/src/jsMain/kotlin/react/router/useRoutes.ext.kt
@@ -1,10 +1,7 @@
-@file:Suppress(
-    "NOTHING_TO_INLINE",
-)
+@file:JsModule("react-router")
 
 package react.router
 
 import js.core.ReadonlyArray
 
-inline fun useRoutes(routes: ReadonlyArray<RouteObject>): react.ReactElement<*>? =
-    useRoutes(routes, undefined.unsafeCast<String>())
+external fun useRoutes(routes: ReadonlyArray<RouteObject>): react.ReactElement<*>?
