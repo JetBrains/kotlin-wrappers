39ca6e8325d95870686f5209c605a71ade995550
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2023-02-21 16:06:39 +0200

    React Router: `DataRouteObject` interface is fixed (#1926)

--- kotlin-react-router/src/jsMain/generated/react/router/RouteObject.kt
+++ kotlin-react-router/src/jsMain/generated/react/router/RouteObject.kt
@@ -7,5 +7,23 @@
 
 package react.router
 
+import remix.run.router.ActionFunction
+import remix.run.router.LoaderFunction
+import remix.run.router.ShouldRevalidateFunction
 
-external interface RouteObject /* export declare type RouteObject = IndexRouteObject | NonIndexRouteObject; */
+
+external interface RouteObject {
+    var caseSensitive: Boolean?
+    var path: String?
+    var id: String?
+    var loader: LoaderFunction?
+    var action: ActionFunction?
+    var hasErrorBoundary: Boolean?
+    var shouldRevalidate: ShouldRevalidateFunction?
+    var handle: Any?
+    var index: Boolean?
+    var children: js.core.ReadonlyArray<RouteObject>?
+    var element: react.ReactNode?
+    var errorElement: react.ReactNode?
+}
+
