a72f6d6120d3c2f317e087cb07ba807f85311e23
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-02-22 23:18:36 +0200

    React Router: unneeded `@JsModule` annotations are removed

--- kotlin-react-router/src/jsMain/generated/react/router/RelativeRoutingType.kt
+++ kotlin-react-router/src/jsMain/generated/react/router/RelativeRoutingType.kt
@@ -1,13 +1,11 @@
-@file:JsModule("react-router")
-
 @file:Suppress(
     "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
-    "NAME_CONTAINS_ILLEGAL_CHARS",
 )
 
 package react.router
 
 
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 @JsName("""(/*union*/{route: 'route', path: 'path'}/*union*/)""")
 external enum class RelativeRoutingType {
     route,
