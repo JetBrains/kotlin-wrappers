6b2feecb3f4a8cf4e1dbeb6cad95d3665eedd96b
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2023-02-23 00:03:13 +0200

    React Router: unneeded `@JsModule` annotations are removed (#1932)
    * React Router: unneeded `@JsModule` annotations are removed
    
    * React Router: unneeded `@JsModule` annotations are removed

--- kotlin-remix-run-router/src/jsMain/generated/remix/run/router/FormEncType.kt
+++ kotlin-remix-run-router/src/jsMain/generated/remix/run/router/FormEncType.kt
@@ -1,13 +1,11 @@
-@file:JsModule("@remix-run/router")
-
 @file:Suppress(
     "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
-    "NAME_CONTAINS_ILLEGAL_CHARS",
 )
 
 package remix.run.router
 
 
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 @JsName("""(/*union*/{applicationXWwwFormUrlencoded: 'application/x-www-form-urlencoded', multipartFormData: 'multipart/form-data'}/*union*/)""")
 external enum class FormEncType {
     applicationXWwwFormUrlencoded,
