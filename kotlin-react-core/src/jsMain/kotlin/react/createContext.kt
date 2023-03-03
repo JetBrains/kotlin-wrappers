6b1897cbac343a86ddc1a70cb887671346f5bbe3
Victor Turansky <victor.turansky@gmail.com>
2023-03-03 18:21:26 +0200

    React. Strict `createContext` signature

--- kotlin-react-core/src/jsMain/kotlin/react/createContext.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/createContext.kt
@@ -4,6 +4,8 @@
 package react
 
 // Context (16.3+)
+external fun <T : Any> createContext(): Context<T?>
+
 external fun <T> createContext(
-    defaultValue: T = definedExternally,
+    defaultValue: T,
 ): Context<T>
