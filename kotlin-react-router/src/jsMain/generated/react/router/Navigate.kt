1e484c0d9f9ddda008bf53e0ba4a817cce26fd61
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-03-03 11:14:13 +0200

    React Router: bump router versions, align component conversion

---
+++ kotlin-react-router/src/jsMain/generated/react/router/Navigate.kt
@@ -0,0 +1,19 @@
+@file:JsModule("react-router")
+
+@file:Suppress(
+    "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
+)
+
+package react.router
+
+
+/**
+ * Changes the current location.
+ *
+ * Note: This API is mostly useful in React.Component subclasses that are not
+ * able to use hooks. In functional components, we recommend you use the
+ * `useNavigate` hook instead.
+ *
+ * @see https://reactrouter.com/components/navigate
+ */
+external val Navigate: react.FC<NavigateProps>
---
+++ kotlin-react-router/src/jsMain/generated/react/router/NavigateProps.kt
@@ -0,0 +1,16 @@
+@file:Suppress(
+    "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
+)
+
+package react.router
+
+import remix.run.router.To
+
+
+external interface NavigateProps : react.Props {
+    var to: To
+    var replace: Boolean?
+    var state: Any?
+    var relative: RelativeRoutingType?
+}
+
