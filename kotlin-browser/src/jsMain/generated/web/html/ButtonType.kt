a521f6552c10ccfad0e231063d462ec1799d9ed7
Victor Turansky <victor.turansky@gmail.com>
2023-03-04 18:22:26 +0200

    Browser. Fix formatting

--- kotlin-browser/src/jsMain/generated/web/html/ButtonType.kt
+++ kotlin-browser/src/jsMain/generated/web/html/ButtonType.kt
@@ -1,8 +1,11 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
 package web.html
 
-@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
 @JsName("""(/*union*/{submit: 'submit', reset: 'reset', button: 'button'}/*union*/)""")
 external enum class ButtonType {
