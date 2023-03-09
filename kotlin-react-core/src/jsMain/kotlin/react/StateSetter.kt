b25382338fe40d00dabff7f05203f3beee16e08b
Victor Turansky <victor.turansky@gmail.com>
2023-03-09 18:13:08 +0200

    React. Update formatting

--- kotlin-react-core/src/jsMain/kotlin/react/StateSetter.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/StateSetter.kt
@@ -6,11 +6,15 @@ package react
 
 // TODO: make external in IR
 sealed class StateSetter<T> {
-    inline operator fun invoke(value: T) {
+    inline operator fun invoke(
+        value: T,
+    ) {
         asDynamic()(value)
     }
 
-    inline operator fun invoke(noinline transform: (T) -> T) {
+    inline operator fun invoke(
+        noinline transform: (T) -> T,
+    ) {
         asDynamic()(transform)
     }
 }
