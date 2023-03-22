09eba2440c1362111353542c7cd78d8a3e67cbd6
Victor Turansky <victor.turansky@gmail.com>
2023-03-22 17:58:09 +0200

    CSSType. Remove "external enum" usages [generated]

--- kotlin-csstype/src/jsMain/generated/csstype/NamedColor.kt
+++ kotlin-csstype/src/jsMain/generated/csstype/NamedColor.kt
@@ -2,6 +2,7 @@
 
 @file:Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package csstype
@@ -10,156 +11,156 @@ package csstype
 @JsName(
     """(/*union*/{aliceblue: '#F0F8FF', antiquewhite: '#FAEBD7', aqua: '#00FFFF', aquamarine: '#7FFFD4', azure: '#F0FFFF', beige: '#F5F5DC', bisque: '#FFE4C4', black: '#000000', blanchedalmond: '#FFEBCD', blue: '#0000FF', blueviolet: '#8A2BE2', brown: '#A52A2A', burlywood: '#DEB887', cadetblue: '#5F9EA0', chartreuse: '#7FFF00', chocolate: '#D2691E', coral: '#FF7F50', cornflowerblue: '#6495ED', cornsilk: '#FFF8DC', crimson: '#DC143C', cyan: '#00FFFF', darkblue: '#00008B', darkcyan: '#008B8B', darkgoldenrod: '#B8860B', darkgray: '#A9A9A9', darkgreen: '#006400', darkgrey: '#A9A9A9', darkkhaki: '#BDB76B', darkmagenta: '#8B008B', darkolivegreen: '#556B2F', darkorange: '#FF8C00', darkorchid: '#9932CC', darkred: '#8B0000', darksalmon: '#E9967A', darkseagreen: '#8FBC8F', darkslateblue: '#483D8B', darkslategray: '#2F4F4F', darkslategrey: '#2F4F4F', darkturquoise: '#00CED1', darkviolet: '#9400D3', deeppink: '#FF1493', deepskyblue: '#00BFFF', dimgray: '#696969', dimgrey: '#696969', dodgerblue: '#1E90FF', firebrick: '#B22222', floralwhite: '#FFFAF0', forestgreen: '#228B22', fuchsia: '#FF00FF', gainsboro: '#DCDCDC', ghostwhite: '#F8F8FF', gold: '#FFD700', goldenrod: '#DAA520', gray: '#808080', green: '#008000', greenyellow: '#ADFF2F', grey: '#808080', honeydew: '#F0FFF0', hotpink: '#FF69B4', indianred: '#CD5C5C', indigo: '#4B0082', ivory: '#FFFFF0', khaki: '#F0E68C', lavender: '#E6E6FA', lavenderblush: '#FFF0F5', lawngreen: '#7CFC00', lemonchiffon: '#FFFACD', lightblue: '#ADD8E6', lightcoral: '#F08080', lightcyan: '#E0FFFF', lightgoldenrodyellow: '#FAFAD2', lightgray: '#D3D3D3', lightgreen: '#90EE90', lightgrey: '#D3D3D3', lightpink: '#FFB6C1', lightsalmon: '#FFA07A', lightseagreen: '#20B2AA', lightskyblue: '#87CEFA', lightslategray: '#778899', lightslategrey: '#778899', lightsteelblue: '#B0C4DE', lightyellow: '#FFFFE0', lime: '#00FF00', limegreen: '#32CD32', linen: '#FAF0E6', magenta: '#FF00FF', maroon: '#800000', mediumaquamarine: '#66CDAA', mediumblue: '#0000CD', mediumorchid: '#BA55D3', mediumpurple: '#9370D8', mediumseagreen: '#3CB371', mediumslateblue: '#7B68EE', mediumspringgreen: '#00FA9A', mediumturquoise: '#48D1CC', mediumvioletred: '#C71585', midnightblue: '#191970', mintcream: '#F5FFFA', mistyrose: '#FFE4E1', moccasin: '#FFE4B5', navajowhite: '#FFDEAD', navy: '#000080', oldlace: '#FDF5E6', olive: '#808000', olivedrab: '#6B8E23', orange: '#FFA500', orangered: '#FF4500', orchid: '#DA70D6', palegoldenrod: '#EEE8AA', palegreen: '#98FB98', paleturquoise: '#AFEEEE', palevioletred: '#DB7093', papayawhip: '#FFEFD5', peachpuff: '#FFDAB9', peru: '#CD853F', pink: '#FFC0CB', plum: '#DDA0DD', powderblue: '#B0E0E6', purple: '#800080', rebeccapurple: '#663399', red: '#FF0000', rosybrown: '#BC8F8F', royalblue: '#4169E1', saddlebrown: '#8B4513', salmon: '#FA8072', sandybrown: '#F4A460', seagreen: '#2E8B57', seashell: '#FFF5EE', sienna: '#A0522D', silver: '#C0C0C0', skyblue: '#87CEEB', slateblue: '#6A5ACD', slategray: '#708090', slategrey: '#708090', snow: '#FFFAFA', springgreen: '#00FF7F', steelblue: '#4682B4', tan: '#D2B48C', teal: '#008080', thistle: '#D8BFD8', tomato: '#FF6347', transparent: 'rgba(0,0,0,0)', turquoise: '#40E0D0', violet: '#EE82EE', wheat: '#F5DEB3', white: '#FFFFFF', whitesmoke: '#F5F5F5', yellow: '#FFFF00', yellowgreen: '#9ACD32'}/*union*/)"""
 )
-external enum class NamedColor : Color {
-    aliceblue,
-    antiquewhite,
-    aqua,
-    aquamarine,
-    azure,
-    beige,
-    bisque,
-    black,
-    blanchedalmond,
-    blue,
-    blueviolet,
-    brown,
-    burlywood,
-    cadetblue,
-    chartreuse,
-    chocolate,
-    coral,
-    cornflowerblue,
-    cornsilk,
-    crimson,
-    cyan,
-    darkblue,
-    darkcyan,
-    darkgoldenrod,
-    darkgray,
-    darkgreen,
-    darkgrey,
-    darkkhaki,
-    darkmagenta,
-    darkolivegreen,
-    darkorange,
-    darkorchid,
-    darkred,
-    darksalmon,
-    darkseagreen,
-    darkslateblue,
-    darkslategray,
-    darkslategrey,
-    darkturquoise,
-    darkviolet,
-    deeppink,
-    deepskyblue,
-    dimgray,
-    dimgrey,
-    dodgerblue,
-    firebrick,
-    floralwhite,
-    forestgreen,
-    fuchsia,
-    gainsboro,
-    ghostwhite,
-    gold,
-    goldenrod,
-    gray,
-    green,
-    greenyellow,
-    grey,
-    honeydew,
-    hotpink,
-    indianred,
-    indigo,
-    ivory,
-    khaki,
-    lavender,
-    lavenderblush,
-    lawngreen,
-    lemonchiffon,
-    lightblue,
-    lightcoral,
-    lightcyan,
-    lightgoldenrodyellow,
-    lightgray,
-    lightgreen,
-    lightgrey,
-    lightpink,
-    lightsalmon,
-    lightseagreen,
-    lightskyblue,
-    lightslategray,
-    lightslategrey,
-    lightsteelblue,
-    lightyellow,
-    lime,
-    limegreen,
-    linen,
-    magenta,
-    maroon,
-    mediumaquamarine,
-    mediumblue,
-    mediumorchid,
-    mediumpurple,
-    mediumseagreen,
-    mediumslateblue,
-    mediumspringgreen,
-    mediumturquoise,
-    mediumvioletred,
-    midnightblue,
-    mintcream,
-    mistyrose,
-    moccasin,
-    navajowhite,
-    navy,
-    oldlace,
-    olive,
-    olivedrab,
-    orange,
-    orangered,
-    orchid,
-    palegoldenrod,
-    palegreen,
-    paleturquoise,
-    palevioletred,
-    papayawhip,
-    peachpuff,
-    peru,
-    pink,
-    plum,
-    powderblue,
-    purple,
-    rebeccapurple,
-    red,
-    rosybrown,
-    royalblue,
-    saddlebrown,
-    salmon,
-    sandybrown,
-    seagreen,
-    seashell,
-    sienna,
-    silver,
-    skyblue,
-    slateblue,
-    slategray,
-    slategrey,
-    snow,
-    springgreen,
-    steelblue,
-    tan,
-    teal,
-    thistle,
-    tomato,
-    transparent,
-    turquoise,
-    violet,
-    wheat,
-    white,
-    whitesmoke,
-    yellow,
-    yellowgreen,
-
-    ;
+sealed external interface NamedColor : Color {
+    companion object {
+        val aliceblue: NamedColor
+        val antiquewhite: NamedColor
+        val aqua: NamedColor
+        val aquamarine: NamedColor
+        val azure: NamedColor
+        val beige: NamedColor
+        val bisque: NamedColor
+        val black: NamedColor
+        val blanchedalmond: NamedColor
+        val blue: NamedColor
+        val blueviolet: NamedColor
+        val brown: NamedColor
+        val burlywood: NamedColor
+        val cadetblue: NamedColor
+        val chartreuse: NamedColor
+        val chocolate: NamedColor
+        val coral: NamedColor
+        val cornflowerblue: NamedColor
+        val cornsilk: NamedColor
+        val crimson: NamedColor
+        val cyan: NamedColor
+        val darkblue: NamedColor
+        val darkcyan: NamedColor
+        val darkgoldenrod: NamedColor
+        val darkgray: NamedColor
+        val darkgreen: NamedColor
+        val darkgrey: NamedColor
+        val darkkhaki: NamedColor
+        val darkmagenta: NamedColor
+        val darkolivegreen: NamedColor
+        val darkorange: NamedColor
+        val darkorchid: NamedColor
+        val darkred: NamedColor
+        val darksalmon: NamedColor
+        val darkseagreen: NamedColor
+        val darkslateblue: NamedColor
+        val darkslategray: NamedColor
+        val darkslategrey: NamedColor
+        val darkturquoise: NamedColor
+        val darkviolet: NamedColor
+        val deeppink: NamedColor
+        val deepskyblue: NamedColor
+        val dimgray: NamedColor
+        val dimgrey: NamedColor
+        val dodgerblue: NamedColor
+        val firebrick: NamedColor
+        val floralwhite: NamedColor
+        val forestgreen: NamedColor
+        val fuchsia: NamedColor
+        val gainsboro: NamedColor
+        val ghostwhite: NamedColor
+        val gold: NamedColor
+        val goldenrod: NamedColor
+        val gray: NamedColor
+        val green: NamedColor
+        val greenyellow: NamedColor
+        val grey: NamedColor
+        val honeydew: NamedColor
+        val hotpink: NamedColor
+        val indianred: NamedColor
+        val indigo: NamedColor
+        val ivory: NamedColor
+        val khaki: NamedColor
+        val lavender: NamedColor
+        val lavenderblush: NamedColor
+        val lawngreen: NamedColor
+        val lemonchiffon: NamedColor
+        val lightblue: NamedColor
+        val lightcoral: NamedColor
+        val lightcyan: NamedColor
+        val lightgoldenrodyellow: NamedColor
+        val lightgray: NamedColor
+        val lightgreen: NamedColor
+        val lightgrey: NamedColor
+        val lightpink: NamedColor
+        val lightsalmon: NamedColor
+        val lightseagreen: NamedColor
+        val lightskyblue: NamedColor
+        val lightslategray: NamedColor
+        val lightslategrey: NamedColor
+        val lightsteelblue: NamedColor
+        val lightyellow: NamedColor
+        val lime: NamedColor
+        val limegreen: NamedColor
+        val linen: NamedColor
+        val magenta: NamedColor
+        val maroon: NamedColor
+        val mediumaquamarine: NamedColor
+        val mediumblue: NamedColor
+        val mediumorchid: NamedColor
+        val mediumpurple: NamedColor
+        val mediumseagreen: NamedColor
+        val mediumslateblue: NamedColor
+        val mediumspringgreen: NamedColor
+        val mediumturquoise: NamedColor
+        val mediumvioletred: NamedColor
+        val midnightblue: NamedColor
+        val mintcream: NamedColor
+        val mistyrose: NamedColor
+        val moccasin: NamedColor
+        val navajowhite: NamedColor
+        val navy: NamedColor
+        val oldlace: NamedColor
+        val olive: NamedColor
+        val olivedrab: NamedColor
+        val orange: NamedColor
+        val orangered: NamedColor
+        val orchid: NamedColor
+        val palegoldenrod: NamedColor
+        val palegreen: NamedColor
+        val paleturquoise: NamedColor
+        val palevioletred: NamedColor
+        val papayawhip: NamedColor
+        val peachpuff: NamedColor
+        val peru: NamedColor
+        val pink: NamedColor
+        val plum: NamedColor
+        val powderblue: NamedColor
+        val purple: NamedColor
+        val rebeccapurple: NamedColor
+        val red: NamedColor
+        val rosybrown: NamedColor
+        val royalblue: NamedColor
+        val saddlebrown: NamedColor
+        val salmon: NamedColor
+        val sandybrown: NamedColor
+        val seagreen: NamedColor
+        val seashell: NamedColor
+        val sienna: NamedColor
+        val silver: NamedColor
+        val skyblue: NamedColor
+        val slateblue: NamedColor
+        val slategray: NamedColor
+        val slategrey: NamedColor
+        val snow: NamedColor
+        val springgreen: NamedColor
+        val steelblue: NamedColor
+        val tan: NamedColor
+        val teal: NamedColor
+        val thistle: NamedColor
+        val tomato: NamedColor
+        val transparent: NamedColor
+        val turquoise: NamedColor
+        val violet: NamedColor
+        val wheat: NamedColor
+        val white: NamedColor
+        val whitesmoke: NamedColor
+        val yellow: NamedColor
+        val yellowgreen: NamedColor
+    }
 }
