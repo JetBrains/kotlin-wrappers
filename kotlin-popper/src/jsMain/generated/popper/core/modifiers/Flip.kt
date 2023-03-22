864e502d153598cee3f338333c25ffe8ef108aa7
Victor Turansky <victor.turansky@gmail.com>
2023-03-22 12:07:34 +0200

    Popper. Update formatting [generated]

--- kotlin-popper/src/jsMain/generated/popper/core/modifiers/Flip.kt
+++ kotlin-popper/src/jsMain/generated/popper/core/modifiers/Flip.kt
@@ -7,18 +7,17 @@
 package popper.core.modifiers
 
 import js.core.ReadonlyArray
-import popper.core.Placement
 
 external interface FlipOptions {
     var mainAxis: Boolean
     var altAxis: Boolean
-    var fallbackPlacements: ReadonlyArray<Placement>
+    var fallbackPlacements: ReadonlyArray<popper.core.Placement>
     var padding: popper.core.Padding
     var boundary: dynamic /* Boundary */
     var rootBoundary: dynamic /* RootBoundary */
     var altBoundary: Boolean
     var flipVariations: Boolean
-    var allowedAutoPlacements: ReadonlyArray<Placement>
+    var allowedAutoPlacements: ReadonlyArray<popper.core.Placement>
 }
 
 @JsName("'flip'")
