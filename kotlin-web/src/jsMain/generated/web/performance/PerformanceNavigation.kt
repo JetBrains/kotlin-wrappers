26b25171de3dce3a981386626b94036191e80f57
Victor Turansky <victor.turansky@gmail.com>
2023-03-22 02:50:30 +0200

    Web. Update formatting [generated]

--- kotlin-web/src/jsMain/generated/web/performance/PerformanceNavigation.kt
+++ kotlin-web/src/jsMain/generated/web/performance/PerformanceNavigation.kt
@@ -3,15 +3,15 @@
 package web.performance
 
 sealed external class PerformanceNavigation {
-    val TYPE_BACK_FORWARD: Short
     val TYPE_NAVIGATE: Short
     val TYPE_RELOAD: Short
+    val TYPE_BACK_FORWARD: Short
     val TYPE_RESERVED: Short
 
     companion object {
-        val TYPE_BACK_FORWARD: Short
         val TYPE_NAVIGATE: Short
         val TYPE_RELOAD: Short
+        val TYPE_BACK_FORWARD: Short
         val TYPE_RESERVED: Short
     }
 }
