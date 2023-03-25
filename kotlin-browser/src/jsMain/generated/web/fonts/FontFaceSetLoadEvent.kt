ef8427d188d57cfe1b25410afb88e3756ab2c032
Victor Turansky <victor.turansky@gmail.com>
2023-03-25 20:36:57 +0200

    Fonts. Events [generated]

---
+++ kotlin-browser/src/jsMain/generated/web/fonts/FontFaceSetLoadEvent.kt
@@ -0,0 +1,25 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
+)
+
+package web.fonts
+
+import js.core.ReadonlyArray
+import web.events.Event
+import web.events.EventInit
+import web.events.EventType
+
+external interface FontFaceSetLoadEventInit : EventInit {
+    var fontfaces: ReadonlyArray<FontFace>?
+}
+
+open external class FontFaceSetLoadEvent(
+    override val type: EventType<FontFaceSetLoadEvent>,
+    init: FontFaceSetLoadEventInit = definedExternally,
+) : Event {
+    val fontfaces: ReadonlyArray<FontFace>
+
+    companion object
+}
