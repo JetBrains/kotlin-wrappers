f2d167ffb780456df5d00524ebd679b9855c5666
Victor Turansky <victor.turansky@gmail.com>
2023-03-26 00:15:55 +0200

    React Test Utils. External `EventSimulator`

--- kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/EventSimulator.kt
+++ kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/EventSimulator.kt
@@ -1,31 +1,27 @@
-@file:Suppress(
-    "NOTHING_TO_INLINE",
-)
-
 package react.dom.test
 
 import js.core.jso
 import web.dom.Element
 import web.events.EventInit
 
-sealed class EventSimulator<O : EventInit> {
-    inline operator fun invoke(
+sealed external interface EventSimulator<O : EventInit> {
+    @nativeInvoke
+    @Suppress("DEPRECATION")
+    operator fun invoke(
         element: Element, /* | Component<any> */
-    ) {
-        asDynamic()(element)
-    }
+    )
 
-    inline operator fun invoke(
+    @nativeInvoke
+    @Suppress("DEPRECATION")
+    operator fun invoke(
         element: Element, /* | Component<any> */
         options: O,
-    ) {
-        asDynamic()(element, options)
-    }
+    )
+}
 
-    inline operator fun invoke(
-        element: Element, /* | Component<any> */
-        block: O.() -> Unit,
-    ) {
-        this(element, options = jso(block))
-    }
+inline operator fun <O : EventInit> EventSimulator<O>.invoke(
+    element: Element, /* | Component<any> */
+    block: O.() -> Unit,
+) {
+    this(element, options = jso(block))
 }
