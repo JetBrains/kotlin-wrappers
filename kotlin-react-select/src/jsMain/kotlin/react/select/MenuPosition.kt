e112a03c8edabb66b16559e0049007836a24143c
Victor Turansky <victor.turansky@gmail.com>
2023-04-11 05:00:28 +0300

    React Select. `MenuPosition` union

--- kotlin-react-select/src/jsMain/kotlin/react/select/MenuPosition.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/MenuPosition.kt
@@ -1,3 +1,15 @@
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package react.select
 
-sealed external interface MenuPosition
+// language=JavaScript
+@JsName("""(/*union*/{absolute: 'absolute', fixed: 'fixed'}/*union*/)""")
+sealed external interface MenuPosition {
+    companion object {
+        val absolute: MenuPosition
+        val fixed: MenuPosition
+    }
+}
