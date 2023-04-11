d30ba7e5bb535ead0cd51d4eeeac9c19a4899534
Victor Turansky <victor.turansky@gmail.com>
2023-04-11 04:53:34 +0300

    React Select. `ActionMeta`

--- kotlin-react-select/src/jsMain/kotlin/react/select/ActionMeta.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/ActionMeta.kt
@@ -1,3 +1,8 @@
 package react.select
 
-sealed external interface ActionMeta<Option>
+sealed external interface ActionMeta<Option> {
+    val option: Option?
+    val removedValue: Option?
+    val removedValues: Options<Option>?
+    val name: String?
+}
