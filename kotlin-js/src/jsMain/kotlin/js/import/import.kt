562138eb0db08f287f6339290609bcdc7b7d9778
Victor Turansky <victor.turansky@gmail.com>
2023-04-16 14:36:25 +0300

    Import. Remove `env` from `import.meta`

--- kotlin-js/src/jsMain/kotlin/js/import/import.kt
+++ kotlin-js/src/jsMain/kotlin/js/import/import.kt
@@ -8,20 +8,6 @@ external fun <T : Any> import(
 
 external object import {
     object meta {
-        object env
-
         val url: String
     }
 }
-
-inline val import.meta.env.MODE: String?
-    get() = process.env.NODE_ENV
-
-inline val import.meta.env.PROD: Boolean
-    get() = process.env.NODE_ENV === "production"
-
-inline val import.meta.env.DEV: Boolean
-    get() = process.env.NODE_ENV !== "production"
-
-@PublishedApi
-internal external val process: dynamic
