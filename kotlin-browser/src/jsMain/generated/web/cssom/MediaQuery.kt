c4fbd1aedd046c2bbc13d028a1a9e8d2433ccf9d
Victor Turansky <victor.turansky@gmail.com>
2023-04-17 04:48:29 +0300

    Media Queries. `MediaQuery` operators [generated]

--- kotlin-browser/src/jsMain/generated/web/cssom/MediaQuery.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/MediaQuery.kt
@@ -12,3 +12,18 @@ inline fun MediaQuery(
     value: String,
 ): MediaQuery =
     value.unsafeCast<MediaQuery>()
+
+infix fun MediaQuery.and(
+    other: MediaQuery,
+): MediaQuery =
+    MediaQuery("$this and $other")
+
+infix fun MediaQuery.or(
+    other: MediaQuery,
+): MediaQuery =
+    MediaQuery("$this or $other")
+
+inline fun not(
+    query: MediaQuery,
+): MediaQuery =
+    MediaQuery("(not ($query))")
