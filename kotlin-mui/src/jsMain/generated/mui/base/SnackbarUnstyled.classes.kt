9a0c4fc472a9637754f4eae845cc1b9c0d040e8e
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-04-18 10:41:20 +0400

    MUI Material `5.10.8` & MUI Lab `5.0.0-alpha.102` [generated] (#1985)
    * MUI Material `5.10.8` & MUI Lab `5.0.0-alpha.102` [generated]
    
    * MUI Material `5.10.8` & MUI Lab `5.0.0-alpha.102` [generated]

---
+++ kotlin-mui/src/jsMain/generated/mui/base/SnackbarUnstyled.classes.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+import csstype.ClassName
+
+external interface SnackbarUnstyledClasses {
+    /** Class name applied to the root element. */
+    var root: ClassName
+}
---
+++ kotlin-mui/src/jsMain/generated/mui/base/useSnackbar.types.ext.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+@Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+// language=JavaScript
+@JsName("""(/*union*/{timeout: 'timeout', clickaway: 'clickaway', escapeKeyDown: 'escapeKeyDown'}/*union*/)""")
+sealed external interface SnackbarCloseReason {
+    companion object {
+        val timeout: SnackbarCloseReason
+        val clickaway: SnackbarCloseReason
+        val escapeKeyDown: SnackbarCloseReason
+    }
+}
