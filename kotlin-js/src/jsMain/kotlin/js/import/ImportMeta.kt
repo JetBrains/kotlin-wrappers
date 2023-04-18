a2207ba599ccee6619f73ce3fd250a19bf61159e
Victor Turansky <victor.turansky@gmail.com>
2023-04-19 02:54:09 +0300

    Import. Synchronous `ImportMeta.resolve`

--- kotlin-js/src/jsMain/kotlin/js/import/ImportMeta.kt
+++ kotlin-js/src/jsMain/kotlin/js/import/ImportMeta.kt
@@ -1,11 +1,9 @@
 package js.import
 
-import kotlin.js.Promise
-
 external interface ImportMeta {
     val url: String
 
     fun resolve(
         moduleName: String,
-    ): Promise<String>
+    ): String
 }
