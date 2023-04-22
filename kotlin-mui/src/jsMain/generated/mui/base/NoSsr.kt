f328c59abe8ad2fb75166a3fc97f299fd026d87a
Alexander Popoff <alexander.popoff@flysto.net>
2023-04-22 21:44:39 +0300

    MUI. Material `5.10.10` & Lab `5.0.0-alpha.104`[generated]

--- kotlin-mui/src/jsMain/generated/mui/base/NoSsr.kt
+++ kotlin-mui/src/jsMain/generated/mui/base/NoSsr.kt
@@ -5,26 +5,6 @@
 
 package mui.base
 
-external interface NoSsrProps : react.PropsWithChildren {
-    /**
-     * You can wrap a node.
-     */
-    override var children: react.ReactNode?
-
-    /**
-     * If `true`, the component will not only prevent server-side rendering.
-     * It will also defer the rendering of the children into a different screen frame.
-     * @default false
-     */
-    var defer: Boolean?
-
-    /**
-     * The fallback content to display.
-     * @default null
-     */
-    var fallback: react.ReactNode?
-}
-
 /**
  * NoSsr purposely removes components from the subject of Server Side Rendering (SSR).
  *
