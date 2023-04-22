c7c9aa72d91aacae8ef0c9b9addc5bbea1a9e0d3
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-04-22 22:51:29 +0400

    MUI. Material `5.10.10` & Lab `5.0.0-alpha.104`[generated] (#1990)

---
+++ kotlin-mui/src/jsMain/generated/mui/base/NoSsr.types.kt
@@ -0,0 +1,23 @@
+// Automatically generated - do not modify!
+
+package mui.base
+
+external interface NoSsrProps : react.PropsWithChildren {
+    /**
+     * You can wrap a node.
+     */
+    override var children: react.ReactNode?
+
+    /**
+     * If `true`, the component will not only prevent server-side rendering.
+     * It will also defer the rendering of the children into a different screen frame.
+     * @default false
+     */
+    var defer: Boolean?
+
+    /**
+     * The fallback content to display.
+     * @default null
+     */
+    var fallback: react.ReactNode?
+}
