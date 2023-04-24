3acab472833a8e3157c18ed9bb3dddf9a0601dcc
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-04-24 22:56:40 +0400

    MUI. Material `5.10.15` & Lab `5.0.0-alpha.109` [generated] (#1995)

--- kotlin-mui/src/jsMain/generated/mui/base/InputUnstyled.types.kt
+++ kotlin-mui/src/jsMain/generated/mui/base/InputUnstyled.types.kt
@@ -70,6 +70,12 @@ external interface InputUnstyledOwnProps :
     var endAdornment: react.ReactNode?
 
     /**
+     * If `true`, the `input` will indicate an error by setting the `aria-invalid` attribute on the input and the `Mui-error` class on the root element.
+     * The prop defaults to the value (`false`) inherited from the parent FormControl component.
+     */
+    var error: Boolean?
+
+    /**
      * The id of the `input` element.
      */
     var id: String?
