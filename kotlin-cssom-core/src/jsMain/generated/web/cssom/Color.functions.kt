5562be6b8454680c0cbef990c24d121c5dd6e341
Victor Turansky <victor.turansky@gmail.com>
2023-04-26 02:36:44 +0300

    CSSOM Core. `oklch` factories [generated]

--- kotlin-cssom-core/src/jsMain/generated/web/cssom/Color.functions.kt
+++ kotlin-cssom-core/src/jsMain/generated/web/cssom/Color.functions.kt
@@ -25,3 +25,18 @@ inline fun rgb(
     alpha: Double,
 ): Color =
     "rgb($red $green $blue / $alpha)".unsafeCast<Color>()
+
+inline fun oklch(
+    l: Percentage,
+    c: Double,
+    h: Double,
+): Color =
+    "oklch($l $c $h)".unsafeCast<Color>()
+
+inline fun oklch(
+    l: Percentage,
+    c: Double,
+    h: Double,
+    a: Double,
+): Color =
+    "oklch($l $c $h $a)".unsafeCast<Color>()
