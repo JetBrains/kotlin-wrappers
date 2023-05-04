575c8c33f2970852f32c666f7c703a3d00597515
Victor Turansky <victor.turansky@gmail.com>
2023-05-04 04:15:25 +0300

    React Router `6.11.1` [generated]

--- kotlin-react-router/src/jsMain/generated/react/router/RouteContextObject.kt
+++ kotlin-react-router/src/jsMain/generated/react/router/RouteContextObject.kt
@@ -8,4 +8,5 @@ package react.router
 sealed external interface RouteContextObject {
     var outlet: react.ReactElement<*>?
     var matches: js.core.ReadonlyArray<RouteMatch<*>>
+    var isDataRoute: Boolean
 }
