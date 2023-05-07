6df358cd46424b7c97557d77d8a2c0215e6baafd
Victor Turansky <victor.turansky@gmail.com>
2023-05-07 16:35:34 +0300

    Emotion. `AspectRatio` workaround removed

--- kotlin-cssom-core/src/jsMain/kotlin/web/cssom/AspectRatio.wa.kt
+++
@@ -1,8 +0,0 @@
-package web.cssom
-
-// TODO: remove after fix in emotion
-//  https://github.com/emotion-js/emotion/issues/3011
-fun AspectRatio(
-    value: Double,
-): AspectRatio =
-    value.toString().unsafeCast<AspectRatio>()
