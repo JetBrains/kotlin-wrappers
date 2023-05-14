6a62c113937327acfc3ab74a3606c17603eb58a9
Victor Turansky <victor.turansky@gmail.com>
2023-05-15 02:38:04 +0300

    React. Add `kotlinMemo` HOC

---
+++ kotlin-react-core/src/jsMain/kotlin/react/kotlinMemo.kt
@@ -0,0 +1,21 @@
+package react
+
+import js.core.Object
+
+fun <P : Props> kotlinMemo(
+    type: ComponentType<P>,
+): FC<P> =
+    memo(type, ::propsAreEqual)
+
+private fun <P : Props> propsAreEqual(
+    oldProps: P,
+    newProps: P,
+): Boolean =
+    sequenceOf(oldProps, newProps)
+        .flatMap { Object.keys(it).asSequence() }
+        .distinct()
+        .all { key ->
+            val oldValue: Any? = oldProps.asDynamic()[key]
+            val newValue: Any? = newProps.asDynamic()[key]
+            newValue == oldValue
+        }
