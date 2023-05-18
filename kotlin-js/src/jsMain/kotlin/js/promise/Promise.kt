f468acff92a0d2a846274c4cd47fd6cfabac200a
Victor Turansky <victor.turansky@gmail.com>
2023-05-18 14:25:40 +0300

    Promise. Add `Promise.all` overload

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -1,7 +1,13 @@
 package js.promise
 
+import js.core.ReadonlyArray
 import js.core.Void
 import kotlin.js.Promise
 
 inline fun Promise.Companion.resolve(): Promise<Void> =
     resolve(undefined)
+
+inline fun <S> Promise.Companion.all(
+    values: ReadonlyArray<PromiseResult<S>>,
+): Promise<ReadonlyArray<S>> =
+    all(promise = values.unsafeCast<ReadonlyArray<Promise<S>>>())
