90bd4e9da7b053979633656f92f0c326defeb3ec
Victor Turansky <victor.turansky@gmail.com>
2023-05-18 15:49:40 +0300

    Promise. Fix `PromiseLike.then` return type

--- kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.kt
@@ -1,15 +1,13 @@
 package js.promise
 
-import kotlin.js.Promise
-
 sealed external interface PromiseLike<out T> :
     PromiseResult<T> {
     fun <S> then(
         onFulfilled: ((T) -> S)?,
-    ): Promise<S>
+    ): PromiseLike<S>
 
     fun <S> then(
         onFulfilled: ((T) -> S)?,
         onRejected: ((Throwable) -> S)?,
-    ): Promise<S>
+    ): PromiseLike<S>
 }
