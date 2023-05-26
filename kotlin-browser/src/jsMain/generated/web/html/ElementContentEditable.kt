c56ad2429e2473a61524d4cc5b547e1e744c4e47
Victor Turansky <victor.turansky@gmail.com>
2023-05-27 00:58:26 +0300

    Web Components. `ElementContentEditable` without abstract members (#2007)

--- kotlin-browser/src/jsMain/generated/web/html/ElementContentEditable.kt
+++ kotlin-browser/src/jsMain/generated/web/html/ElementContentEditable.kt
@@ -1,10 +1,24 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.html
 
 sealed external interface ElementContentEditable {
     var contentEditable: String
+        get() = definedExternally
+        set(value) = definedExternally
+
     var enterKeyHint: EnterKeyHint
+        get() = definedExternally
+        set(value) = definedExternally
+
     var inputMode: InputMode
+        get() = definedExternally
+        set(value) = definedExternally
+
     val isContentEditable: Boolean
+        get() = definedExternally
 }
