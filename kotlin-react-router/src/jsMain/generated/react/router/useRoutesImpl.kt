9787c8086944e376c34272b46f9bf8b6d7880486
Victor Turansky <victor.turansky@gmail.com>
2023-05-26 12:48:15 +0300

    React Router. Fix Karakum config

--- kotlin-react-router/src/jsMain/generated/react/router/useRoutesImpl.kt
+++ kotlin-react-router/src/jsMain/generated/react/router/useRoutesImpl.kt
@@ -8,7 +8,6 @@ package react.router
 import remix.run.router.Location
 import remix.run.router.RouterState
 
-
 external fun useRoutesImpl(routes: js.core.ReadonlyArray<RouteObject>, locationArg: Location = definedExternally, dataRouterState: RouterState = definedExternally): react.ReactElement<*>?
 
 external fun useRoutesImpl(routes: js.core.ReadonlyArray<RouteObject>, locationArg: String = definedExternally, dataRouterState: RouterState = definedExternally): react.ReactElement<*>?
