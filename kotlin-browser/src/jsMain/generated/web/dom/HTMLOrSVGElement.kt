a9911b8eb5e0cc2063960e29bdc676031770180f
Victor Turansky <victor.turansky@gmail.com>
2023-05-27 00:56:03 +0300

    Web Components. `HTMLOrSVGElement` without abstract members (#2007)

--- kotlin-browser/src/jsMain/generated/web/dom/HTMLOrSVGElement.kt
+++ kotlin-browser/src/jsMain/generated/web/dom/HTMLOrSVGElement.kt
@@ -1,12 +1,27 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.dom
 
 external interface HTMLOrSVGElement {
     var autofocus: Boolean
+        get() = definedExternally
+        set(value) = definedExternally
+
     val dataset: DOMStringMap
+        get() = definedExternally
+
     var nonce: String?
+        get() = definedExternally
+        set(value) = definedExternally
+
     var tabIndex: Int
-    fun blur()
-    fun focus(options: FocusOptions = definedExternally)
+        get() = definedExternally
+        set(value) = definedExternally
+
+    fun blur(): Unit = definedExternally
+    fun focus(options: FocusOptions = definedExternally): Unit = definedExternally
 }
