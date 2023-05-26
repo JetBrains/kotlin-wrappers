2fa07726203c8278c725cef8561d7e927516f9c2
Victor Turansky <victor.turansky@gmail.com>
2023-05-27 01:03:26 +0300

    Web Components. `NonDocumentTypeChildNode` without abstract members (#2007)

--- kotlin-browser/src/jsMain/generated/web/dom/NonDocumentTypeChildNode.kt
+++ kotlin-browser/src/jsMain/generated/web/dom/NonDocumentTypeChildNode.kt
@@ -5,7 +5,9 @@ package web.dom
 sealed external interface NonDocumentTypeChildNode {
     /** Returns the first following sibling that is an element, and null otherwise. */
     val nextElementSibling: Element?
+        get() = definedExternally
 
     /** Returns the first preceding sibling that is an element, and null otherwise. */
     val previousElementSibling: Element?
+        get() = definedExternally
 }
