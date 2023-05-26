3532ff600b0efe05fd5247d88d682b279523404f
Victor Turansky <victor.turansky@gmail.com>
2023-05-27 00:59:34 +0300

    Web Components. `ElementCSSInlineStyle` without abstract members (#2007)

--- kotlin-browser/src/jsMain/generated/web/cssom/ElementCSSInlineStyle.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/ElementCSSInlineStyle.kt
@@ -1,8 +1,15 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.cssom
 
 external interface ElementCSSInlineStyle {
     val attributeStyleMap: StylePropertyMap
+        get() = definedExternally
+
     val style: CSSStyleDeclaration
+        get() = definedExternally
 }
