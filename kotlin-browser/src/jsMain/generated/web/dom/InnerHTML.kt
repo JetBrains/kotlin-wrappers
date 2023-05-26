f3ba9e0d9c300a15bf976e487931ee003e2a20a6
Victor Turansky <victor.turansky@gmail.com>
2023-05-27 01:02:24 +0300

    Web Components. `InnerHTML` without abstract members (#2007)

--- kotlin-browser/src/jsMain/generated/web/dom/InnerHTML.kt
+++ kotlin-browser/src/jsMain/generated/web/dom/InnerHTML.kt
@@ -1,7 +1,13 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.dom
 
 external interface InnerHTML {
     var innerHTML: String
+        get() = definedExternally
+        set(value) = definedExternally
 }
