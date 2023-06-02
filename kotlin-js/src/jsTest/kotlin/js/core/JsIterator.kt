b756f709ce05ca1300b3f33a3e09a11616a47464
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-06-02 18:22:13 +0300

    Test for iterator is added

---
+++ kotlin-js/src/jsTest/kotlin/js/core/JsIterator.kt
@@ -0,0 +1,20 @@
+package js.core
+
+import js.collections.JsSet
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JsIteratorTest {
+    @Test
+    fun operatorIterator() {
+        val expectedSet = setOf(1, 2, 3)
+        val jsSet = JsSet(expectedSet.toTypedArray())
+
+        val actualSet = mutableSetOf<Int>()
+        for (element in jsSet[Symbol.iterator]()) {
+            actualSet.add(element)
+        }
+
+        assertEquals(expectedSet, actualSet)
+    }
+}
