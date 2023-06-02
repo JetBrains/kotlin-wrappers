7c5205a24e53300ae0a3828517b68822653421f0
Victor Turansky <victor.turansky@gmail.com>
2023-06-03 00:22:00 +0300

    Cesium `1.106.1` [generated]

--- kotlin-cesium/src/jsMain/generated/cesium/DataSourceDisplay.kt
+++ kotlin-cesium/src/jsMain/generated/cesium/DataSourceDisplay.kt
@@ -101,15 +101,16 @@ external class DataSourceDisplay(options: ConstructorOptions) {
 /**
  * A function which creates an array of visualizers used for visualization.
  * ```
- * function createVisualizers(scene, dataSource) {
- *     return [new BillboardVisualizer(scene, dataSource.entities)];
+ * function createVisualizers(scene, entityCluster, dataSource) {
+ *     return [new BillboardVisualizer(entityCluster, dataSource.entities)];
  * }
  * ```
  * @param [scene] The scene to create visualizers for.
+ * @param [entityCluster] The entity cluster to create visualizers for.
  * @param [dataSource] The data source to create visualizers for.
  * @see <a href="https://cesium.com/docs/cesiumjs-ref-doc/DataSourceDisplay.html#.VisualizersCallback">Online Documentation</a>
  */
-typealias VisualizersCallback = (scene: Scene, dataSource: DataSource) -> ReadonlyArray<Visualizer>
+typealias VisualizersCallback = (scene: Scene, entityCluster: EntityCluster, dataSource: DataSource) -> ReadonlyArray<Visualizer>
 
 inline fun DataSourceDisplay(
     block: DataSourceDisplay.ConstructorOptions.() -> Unit,
