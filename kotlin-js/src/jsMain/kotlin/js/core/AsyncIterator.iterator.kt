9259474a54e06e016672c6c0befa68a47828832e
Victor Turansky <victor.turansky@gmail.com>
2023-06-02 03:30:02 +0300

    JS. Add `AsyncIterator.iterator` operator extension (#2009)

---
+++ kotlin-js/src/jsMain/kotlin/js/core/AsyncIterator.iterator.kt
@@ -0,0 +1,27 @@
+package js.core
+
+import js.collections.SuspendableIterator
+import kotlinx.coroutines.await
+
+operator fun <T> AsyncIterator<T>.iterator(): SuspendableIterator<T> =
+    AsyncIteratorAdapter(this)
+
+private class AsyncIteratorAdapter<T>(
+    private val source: AsyncIterator<T>,
+) : SuspendableIterator<T> {
+    private lateinit var lastResult: JsIterator.Result<T, Void>
+
+    override fun next(): T {
+        check(!lastResult.done)
+
+        return lastResult
+            .unsafeCast<JsIterator.ReturnResult<T>>()
+            .value
+    }
+
+    override suspend fun hasNext(): Boolean {
+        lastResult = source.next().await()
+
+        return !lastResult.done
+    }
+}
