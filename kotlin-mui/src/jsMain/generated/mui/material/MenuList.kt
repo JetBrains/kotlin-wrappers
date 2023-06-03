1eb1535f8cefea4c2f4a952deed3ab018e218ba6
Alexander Popoff <alexander.popoff@flysto.net>
2023-06-03 17:32:39 +0300

    MUI. Material `5.11.8` & Lab `5.0.0-alpha.119` [generated]

--- kotlin-mui/src/jsMain/generated/mui/material/MenuList.kt
+++ kotlin-mui/src/jsMain/generated/mui/material/MenuList.kt
@@ -47,7 +47,7 @@ external interface MenuListProps :
 }
 
 /**
- * A permanently displayed menu following https://www.w3.org/WAI/ARIA/apg/patterns/menubutton/.
+ * A permanently displayed menu following https://www.w3.org/WAI/ARIA/apg/patterns/menu-button/.
  * It's exposed to help customization of the [`Menu`](https://mui.com/material-ui/api/menu/) component if you
  * use it separately you need to move focus into the component manually. Once
  * the focus is placed inside the component it is fully keyboard accessible.
