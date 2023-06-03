778a7526f117c66438d6ace5e4bac077c9d19b54
Alexander Popoff <alexander.popoff@flysto.net>
2023-06-03 11:35:42 +0300

    MUI. Material `5.11.4` & Lab `5.0.0-alpha.115` [generated]

--- kotlin-mui/src/jsMain/generated/mui/base/Portal.kt
+++ kotlin-mui/src/jsMain/generated/mui/base/Portal.kt
@@ -5,30 +5,6 @@
 
 package mui.base
 
-import web.dom.Element
-
-external interface PortalProps : react.PropsWithChildren {
-    /**
-     * The children to render into the `container`.
-     */
-    override var children: react.ReactNode?
-
-    /**
-     * An HTML element or function that returns one.
-     * The `container` will have the portal children appended to it.
-     *
-     * By default, it uses the body of the top-level document object,
-     * so it's simply `document.body` most of the time.
-     */
-    var container: Element?
-
-    /**
-     * The `children` will be under the DOM hierarchy of the parent component.
-     * @default false
-     */
-    var disablePortal: Boolean?
-}
-
 /**
  * Portals provide a first-class way to render children into a DOM node
  * that exists outside the DOM hierarchy of the parent component.
