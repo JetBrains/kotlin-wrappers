db5d79e3e5ab746bec60bf1406a6e919ec3a197b
Victor Turansky <victor.turansky@gmail.com>
2023-06-07 22:55:36 +0300

    React. Modern `ForwardRef` factory (#2021)

---
+++ kotlin-react/src/jsMain/kotlin/react/ForwardRef.kt
@@ -0,0 +1,26 @@
+package react
+
+import js.core.Object
+import js.core.jso
+
+fun <T : Any, P : PropsWithRef<T>> ForwardRef(
+    block: ChildrenBuilder.(props: P) -> Unit,
+): ForwardRefExoticComponent<P> =
+    rawForwardRef { props, forwardedRef ->
+        val finalProps = forwardRefProps(props, forwardedRef)
+        createElementOrNull { block(finalProps) }
+    }
+
+private fun <T : Any, P : PropsWithRef<T>> forwardRefProps(
+    props: P,
+    forwardedRef: Ref<T>?,
+): P {
+    forwardedRef ?: return props
+
+    return Object.freeze(
+        Object.assign(
+            jso { ref = forwardedRef },
+            props,
+        )
+    )
+}
