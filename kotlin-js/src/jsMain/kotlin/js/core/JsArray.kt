3acf4190b88abc49dc534aa9b4e2a6c9a0fd663b
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-06-14 15:17:46 +0300

    JS. Fix `JsArray` overload priority resolution (#2041)

--- kotlin-js/src/jsMain/kotlin/js/core/JsArray.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/JsArray.kt
@@ -1,11 +1,22 @@
+@file:Suppress(
+    "INVISIBLE_MEMBER",
+    "INVISIBLE_REFERENCE",
+)
+
 package js.core
+
+import kotlin.internal.LowPriorityInOverloadResolution
 
 @JsName("Array")
 sealed external class JsArray {
     companion object {
         fun <T> from(source: JsIterator<T>): ReadonlyArray<T>
+
         fun <T> from(source: JsIterable<T>): ReadonlyArray<T>
+
+        @LowPriorityInOverloadResolution
         fun <T> from(source: ArrayLike<T>): ReadonlyArray<T>
+
         fun <T, U> from(
             source: ArrayLike<T>,
             map: (item: T, index: Int) -> U,
