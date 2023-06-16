73aafc5dc04131c20059d753dcde31a0f47638c2
Victor Turansky <victor.turansky@gmail.com>
2023-06-17 00:24:16 +0300

    Tanstack Query `4.29.14` [generated]

--- kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/notifyManager.kt
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/notifyManager.kt
@@ -3,6 +3,7 @@
 @file:JsModule("@tanstack/query-core")
 
 @file:Suppress(
+    "UNUSED_TYPEALIAS_PARAMETER",
     "NON_EXTERNAL_DECLARATION_IN_INAPPROPRIATE_FILE",
 )
 
@@ -13,6 +14,8 @@ typealias NotifyCallback = () -> Unit
 typealias NotifyFunction = (callback: () -> Unit) -> Unit
 
 typealias BatchNotifyFunction = (callback: () -> Unit) -> Unit
+
+typealias BatchCallsCallback<T> = Function<Unit> /* (...args: T) => void */
 
 external fun createNotifyManager(): dynamic /* { */
 
