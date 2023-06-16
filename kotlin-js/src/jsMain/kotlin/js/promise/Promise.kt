689a5237b71667eadfe2acc0e734f4ba75840096
Alexander Popoff <alexander.popoff@flysto.net>
2023-06-16 12:57:48 +0300

    Trigger

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -13,6 +13,7 @@ inline fun <S> Promise.Companion.all(
 ): Promise<ReadonlyArray<S>> =
     all(promise = values.unsafeCast<ReadonlyArray<Promise<S>>>())
 
+
 suspend fun <T> Promise<T>.asResult(): Result<T> =
     then(
         onFulfilled = { Result.success(it) },
