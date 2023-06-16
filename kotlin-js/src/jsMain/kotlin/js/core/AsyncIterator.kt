9d3acb5a08eda671dc53ba3c6df6b338a15825ff
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-06-16 17:05:11 +0300

    Generator-specific methods are removed from `AsyncIterator`

--- kotlin-js/src/jsMain/kotlin/js/core/AsyncIterator.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/AsyncIterator.kt
@@ -1,10 +1,7 @@
 package js.core
 
-import js.promise.PromiseResult
 import kotlin.js.Promise
 
 sealed external interface AsyncIterator<out T> {
     val next: (/* ...args: [] | [TNext] */) -> Promise<JsIterator.Result<T, Void>>
-    val `return`: ((value: PromiseResult<Void>?) -> Promise<JsIterator.Result<T, Void>>)?
-    val `throw`: ((e: Throwable?) -> Promise<JsIterator.Result<T, Void>>)?
 }
