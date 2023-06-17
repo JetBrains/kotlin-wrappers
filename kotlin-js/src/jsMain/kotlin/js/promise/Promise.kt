80db9a51ae6534b88162e9faa482ae1a25863b51
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-06-17 13:50:20 +0300

    JS. Add `Promise.toResult` extension (#2044)
    * JS. Add `Promise.asResult` extension
    
    * JS. Add `Promise.asResult` extension
    
    * Revert "JS. Add `Promise.asResult` extension"
    
    This reverts commit 007b2bbf282406f6cf64ab8d029b6a6564696cb2.
    
    * Trigger
    
    * Revert "Trigger"
    
    This reverts commit 689a5237b71667eadfe2acc0e734f4ba75840096.
    
    * JS. Add `Promise.toResult` extension

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -2,6 +2,7 @@ package js.promise
 
 import js.core.ReadonlyArray
 import js.core.Void
+import kotlinx.coroutines.await
 import kotlin.js.Promise
 
 inline fun Promise.Companion.resolve(): Promise<Void> =
@@ -11,3 +12,9 @@ inline fun <S> Promise.Companion.all(
     values: ReadonlyArray<PromiseResult<S>>,
 ): Promise<ReadonlyArray<S>> =
     all(promise = values.unsafeCast<ReadonlyArray<Promise<S>>>())
+
+suspend fun <T> Promise<T>.toResult(): Result<T> =
+    then(
+        onFulfilled = { Result.success(it) },
+        onRejected = { Result.failure(it) },
+    ).await()
