ddb4fa1a2100d64c1d4a6c39c6759a72160e7445
Victor Turansky <victor.turansky@gmail.com>
2023-06-18 02:33:16 +0300

    CSSOM. More `ListLike` children [generated]

--- kotlin-browser/src/jsMain/generated/web/cssom/CSSTransformValue.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/CSSTransformValue.kt
@@ -2,18 +2,22 @@
 
 package web.cssom
 
-import js.core.ArrayLike
+import js.collections.ListLike
 import js.core.JsIterable
+import js.core.JsTuple2
 import js.core.ReadonlyArray
 import web.geometry.DOMMatrix
 
 external class CSSTransformValue(
     transforms: ReadonlyArray<CSSTransformComponent>,
 ) : CSSStyleValue,
-    ArrayLike<CSSTransformComponent>,
-    JsIterable<CSSTransformComponent> {
+    ListLike<CSSTransformComponent> {
     override val length: Int
     val is2D: Boolean
     fun toMatrix(): DOMMatrix
-    fun forEach(action: (item: CSSTransformComponent) -> Unit)
+
+    override fun entries(): JsIterable.Iterator<JsTuple2<Int, CSSTransformComponent>>
+    override fun keys(): JsIterable.Iterator<Int>
+    override fun values(): JsIterable.Iterator<CSSTransformComponent>
+    override fun forEach(action: (item: CSSTransformComponent) -> Unit)
 }
--- kotlin-browser/src/jsMain/generated/web/cssom/CSSUnparsedValue.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/CSSUnparsedValue.kt
@@ -2,15 +2,19 @@
 
 package web.cssom
 
-import js.core.ArrayLike
+import js.collections.ListLike
 import js.core.JsIterable
+import js.core.JsTuple2
 import js.core.ReadonlyArray
 
 external class CSSUnparsedValue(
     members: ReadonlyArray<CSSUnparsedSegment>,
 ) : CSSStyleValue,
-    ArrayLike<CSSUnparsedSegment>,
-    JsIterable<CSSUnparsedSegment> {
+    ListLike<CSSUnparsedSegment> {
     override val length: Int
-    fun forEach(action: (item: CSSUnparsedSegment) -> Unit)
+
+    override fun entries(): JsIterable.Iterator<JsTuple2<Int, CSSUnparsedSegment>>
+    override fun keys(): JsIterable.Iterator<Int>
+    override fun values(): JsIterable.Iterator<CSSUnparsedSegment>
+    override fun forEach(action: (item: CSSUnparsedSegment) -> Unit)
 }
