ea506870e02678b36c8ffb71d414895483b7cca4
Victor Turansky <victor.turansky@gmail.com>
2023-06-18 22:24:25 +0300

    Compression Streams. Types [generated]

---
+++ kotlin-web/src/jsMain/generated/web/compression/DecompressionStream.kt
@@ -0,0 +1,13 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "SEALED_INHERITOR_IN_DIFFERENT_PACKAGE",
+)
+
+package web.compression
+
+import web.streams.GenericTransformStream
+
+external class DecompressionStream(
+    format: String,
+) : GenericTransformStream
