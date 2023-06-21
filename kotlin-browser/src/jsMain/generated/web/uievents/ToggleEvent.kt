f50e5b2861d2544a326a9e314eef7c633bb2f42f
Victor Turansky <victor.turansky@gmail.com>
2023-06-22 00:13:31 +0300

    UI Events. `ToggleEvent` [generated] (#2057)

---
+++ kotlin-browser/src/jsMain/generated/web/uievents/ToggleEvent.kt
@@ -0,0 +1,26 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
+)
+
+package web.uievents
+
+import web.events.Event
+import web.events.EventInit
+import web.events.EventType
+
+external interface ToggleEventInit : EventInit {
+    var newState: String?
+    var oldState: String?
+}
+
+open external class ToggleEvent(
+    override val type: EventType<ToggleEvent>,
+    init: ToggleEventInit = definedExternally,
+) : Event {
+    val newState: String
+    val oldState: String
+
+    companion object
+}
