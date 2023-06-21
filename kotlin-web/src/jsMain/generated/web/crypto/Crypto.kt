8f9bc27fb64b0b6be535051e19664634c9160bb8
Victor Turansky <victor.turansky@gmail.com>
2023-06-21 23:54:58 +0300

    Crypto. MDN references [generated] (#2057)

--- kotlin-web/src/jsMain/generated/web/crypto/Crypto.kt
+++ kotlin-web/src/jsMain/generated/web/crypto/Crypto.kt
@@ -5,10 +5,18 @@ package web.crypto
 import js.buffer.ArrayBufferView
 
 sealed external class Crypto {
-    /** Available only in secure contexts. */
+    /**
+     * Available only in secure contexts.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Crypto/subtle)
+     */
     val subtle: SubtleCrypto
     fun <T : ArrayBufferView?> getRandomValues(array: T): T
 
-    /** Available only in secure contexts. */
+    /**
+     * Available only in secure contexts.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Crypto/randomUUID)
+     */
     fun randomUUID(): String
 }
