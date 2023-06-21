abe357dae11de897564f9abd3ee0b97f738d84ee
Victor Turansky <victor.turansky@gmail.com>
2023-06-22 00:14:03 +0300

    Media Source. MDN references [generated] (#2057)

--- kotlin-browser/src/jsMain/generated/web/media/source/TimeRanges.kt
+++ kotlin-browser/src/jsMain/generated/web/media/source/TimeRanges.kt
@@ -3,13 +3,19 @@
 package web.media.source
 
 sealed external class TimeRanges {
-    /** Returns the number of ranges in the object. */
+    /**
+     * Returns the number of ranges in the object.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/TimeRanges/length)
+     */
     val length: Int
 
     /**
      * Returns the time for the end of the range with the given index.
      *
      * Throws an "IndexSizeError" DOMException if the index is out of range.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/TimeRanges/end)
      */
     fun end(index: Int): Number
 
@@ -17,6 +23,8 @@ sealed external class TimeRanges {
      * Returns the time for the start of the range with the given index.
      *
      * Throws an "IndexSizeError" DOMException if the index is out of range.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/TimeRanges/start)
      */
     fun start(index: Int): Number
 }
