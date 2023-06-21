4f2db7e727ddf40035519c1f7c697c39b469768d
Victor Turansky <victor.turansky@gmail.com>
2023-06-22 00:08:06 +0300

    WebTransport. Unions [generated] (#2057)

---
+++ kotlin-browser/src/jsMain/generated/web/transport/WebTransportCongestionControl.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package web.transport
+
+// language=JavaScript
+@JsName("""(/*union*/{default: 'default', lowLatency: 'low-latency', throughput: 'throughput'}/*union*/)""")
+sealed external interface WebTransportCongestionControl {
+    companion object {
+        val default: WebTransportCongestionControl
+        val lowLatency: WebTransportCongestionControl
+        val throughput: WebTransportCongestionControl
+    }
+}
---
+++ kotlin-browser/src/jsMain/generated/web/transport/WebTransportErrorSource.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package web.transport
+
+// language=JavaScript
+@JsName("""(/*union*/{session: 'session', stream: 'stream'}/*union*/)""")
+sealed external interface WebTransportErrorSource {
+    companion object {
+        val session: WebTransportErrorSource
+        val stream: WebTransportErrorSource
+    }
+}
