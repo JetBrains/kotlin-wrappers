dba1a23cacffc9388f14ec10efc84cccc637e94f
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-06-23 17:35:28 +0300

    User `*` instead of `Void` in `JsIterator.next` and `AsyncIterator.next` (preparation for generators)

--- kotlin-js/src/jsMain/kotlin/js/core/AsyncIterator.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/AsyncIterator.kt
@@ -3,5 +3,5 @@ package js.core
 import kotlin.js.Promise
 
 sealed external interface AsyncIterator<out T> {
-    val next: (/* ...args: [] | [TNext] */) -> Promise<JsIterator.Result<T, Void>>
+    fun next(): Promise<JsIterator.Result<T, *>>
 }
