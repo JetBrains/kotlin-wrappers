6fcd1aff5df161013788e26616a59ce61528ad03
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 23:30:46 +0300

    Promise. Sync parameter names (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -14,13 +14,13 @@ open external class Promise<out T>(
     open fun finally(onFinally: () -> Unit): Promise<T>
 
     companion object {
-        fun <S> all(promise: ReadonlyArray<Promise<S>>): Promise<ReadonlyArray<S>>
+        fun <S> all(values: ReadonlyArray<Promise<S>>): Promise<ReadonlyArray<S>>
 
-        fun <S> race(promise: ReadonlyArray<Promise<S>>): Promise<S>
+        fun <S> race(values: ReadonlyArray<Promise<S>>): Promise<S>
 
-        fun reject(e: Throwable): Promise<Nothing>
+        fun reject(reason: Throwable): Promise<Nothing>
 
-        fun <S> resolve(e: S): Promise<S>
-        fun <S> resolve(e: Promise<S>): Promise<S>
+        fun <S> resolve(value: S): Promise<S>
+        fun <S> resolve(value: Promise<S>): Promise<S>
     }
 }
