1bc003bc3399d7fe95a8f6169c5e6854d3638d7f
Victor Turansky <victor.turansky@gmail.com>
2023-06-29 01:31:26 +0300

    Promise. Strict type for `PromiseSettledResult.status` (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/PromiseSettledResult.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/PromiseSettledResult.kt
@@ -3,16 +3,16 @@ package js.promise
 import js.errors.JsError
 
 sealed external interface PromiseSettledResult<T> {
-    val status: String
+    val status: PromiseSettledResultStatus
 }
 
 private external interface PromiseFulfilledResult<T>
     : PromiseSettledResult<T> {
-    // status: "fulfilled"
+    // status: PromiseSettledResultStatus.fulfilled
     val value: T
 }
 
 private external interface PromiseRejectedResult {
-    // status: "rejected";
+    // status: PromiseSettledResultStatus.rejected
     val reason: JsError
 }
---
+++ kotlin-js/src/jsMain/kotlin/js/promise/PromiseSettledResultStatus.kt
@@ -0,0 +1,17 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package js.promise
+
+// language=JavaScript
+@JsName("""(/*union*/{fulfilled: 'fulfilled', rejected: 'rejected'}/*union*/)""")
+sealed external interface PromiseSettledResultStatus {
+    companion object {
+        val fulfilled: PromiseSettledResultStatus
+        val rejected: PromiseSettledResultStatus
+    }
+}
