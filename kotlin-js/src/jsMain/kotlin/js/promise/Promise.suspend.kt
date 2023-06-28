00cd9da43befd5bcf192ad545f3aba04d95c2df7
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 23:04:17 +0300

    Promise. Add `Promise.await` alias (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.suspend.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.suspend.kt
@@ -2,6 +2,9 @@ package js.promise
 
 import kotlinx.coroutines.await
 
+suspend inline fun <T> Promise<T>.await(): T =
+    unsafeCast<LegacyPromise<T>>().await()
+
 suspend fun <T> Promise<T>.toResult(): Result<T> =
     then(
         onFulfilled = { Result.success(it) },
