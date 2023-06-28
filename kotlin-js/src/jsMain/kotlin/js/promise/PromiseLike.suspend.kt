15d97095daba41cc6abf2886053d478c2f329b7f
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 23:54:29 +0300

    Promise. Update `PromiseLike` extensions (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.suspend.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.suspend.kt
@@ -2,10 +2,10 @@ package js.promise
 
 import kotlinx.coroutines.await
 
-suspend inline fun <T> Promise<T>.await(): T =
+suspend inline fun <T> PromiseLike<T>.await(): T =
     unsafeCast<LegacyPromise<T>>().await()
 
-suspend fun <T> Promise<T>.toResult(): Result<T> =
+suspend fun <T> PromiseLike<T>.toResult(): Result<T> =
     then(
         onFulfilled = { Result.success(it) },
         onRejected = { Result.failure(it) },
