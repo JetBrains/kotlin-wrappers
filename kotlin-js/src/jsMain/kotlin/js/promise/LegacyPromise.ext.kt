ba8f62af6864d70d1d6f527395e7f6a0e69e9e2a
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 22:55:03 +0300

    Promise. Update `LegacyPromise` extensions (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/LegacyPromise.ext.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/LegacyPromise.ext.kt
@@ -3,10 +3,10 @@ package js.promise
 import js.core.ReadonlyArray
 import js.core.Void
 
-inline fun kotlin.js.Promise.Companion.resolve(): Promise<Void> =
+inline fun kotlin.js.Promise.Companion.resolve(): LegacyPromise<Void> =
     resolve(undefined)
 
 inline fun <S> kotlin.js.Promise.Companion.all(
     values: ReadonlyArray<PromiseResult<S>>,
-): Promise<ReadonlyArray<S>> =
+): LegacyPromise<ReadonlyArray<S>> =
     all(promise = values.unsafeCast<ReadonlyArray<Promise<S>>>())
