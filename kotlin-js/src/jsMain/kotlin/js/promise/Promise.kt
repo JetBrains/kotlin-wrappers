3003db859f933d2f9e68b664e3d3042f9dd35eca
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 23:16:45 +0300

    Promise. Copy legacy `Promise` implementation (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -1,3 +1,24 @@
 package js.promise
 
-typealias Promise<T> = kotlin.js.Promise<T>
+open external class Promise<out T>(
+    executor: (resolve: (T) -> Unit, reject: (Throwable) -> Unit) -> Unit,
+) {
+    open fun <S> then(onFulfilled: ((T) -> S)?): Promise<S>
+
+    open fun <S> then(onFulfilled: ((T) -> S)?, onRejected: ((Throwable) -> S)?): Promise<S>
+
+    open fun <S> catch(onRejected: (Throwable) -> S): Promise<S>
+
+    open fun finally(onFinally: () -> Unit): Promise<T>
+
+    companion object {
+        fun <S> all(promise: Array<out Promise<S>>): Promise<Array<out S>>
+
+        fun <S> race(promise: Array<out Promise<S>>): Promise<S>
+
+        fun reject(e: Throwable): Promise<Nothing>
+
+        fun <S> resolve(e: S): Promise<S>
+        fun <S> resolve(e: Promise<S>): Promise<S>
+    }
+}
