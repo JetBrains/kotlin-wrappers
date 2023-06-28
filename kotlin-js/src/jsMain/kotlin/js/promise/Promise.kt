b7b30e548910e69f49b31486cae0eaa5f38a627b
Victor Turansky <victor.turansky@gmail.com>
2023-06-28 23:36:56 +0300

    Promise. Sync formatting (#2068)

--- kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/Promise.kt
@@ -5,13 +5,22 @@ import js.core.ReadonlyArray
 open external class Promise<out T>(
     executor: (resolve: (T) -> Unit, reject: (Throwable) -> Unit) -> Unit,
 ) {
-    open fun <R> then(onFulfilled: ((T) -> R)?): Promise<R>
+    open fun <R> then(
+        onFulfilled: ((T) -> R)?,
+    ): Promise<R>
 
-    open fun <R> then(onFulfilled: ((T) -> R)?, onRejected: ((Throwable) -> R)?): Promise<R>
+    open fun <R> then(
+        onFulfilled: ((T) -> R)?,
+        onRejected: ((Throwable) -> R)?,
+    ): Promise<R>
 
-    open fun <R> catch(onRejected: (Throwable) -> R): Promise<R>
+    open fun <R> catch(
+        onRejected: (Throwable) -> R,
+    ): Promise<R>
 
-    open fun finally(onFinally: () -> Unit): Promise<T>
+    open fun finally(
+        onFinally: () -> Unit,
+    ): Promise<T>
 
     companion object {
         fun <T> all(values: ReadonlyArray<Promise<T>>): Promise<ReadonlyArray<T>>
