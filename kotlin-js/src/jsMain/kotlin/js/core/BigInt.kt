c501542435aca9c5b480b7a59e24ca8922e4272a
Victor Turansky <victor.turansky@gmail.com>
2023-07-05 03:00:00 +0300

    JS. `BigInt` static methods

--- kotlin-js/src/jsMain/kotlin/js/core/BigInt.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/BigInt.kt
@@ -1,6 +1,35 @@
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package js.core
 
-sealed external interface BigInt
+sealed external interface BigInt {
+    /**
+     * Returns a string representation of an object.
+     * @param radix Specifies a radix for converting numeric values to strings.
+     */
+    fun toString(radix: Int): String
+
+    companion object {
+
+        /**
+         * Interprets the low bits of a BigInt as a 2's-complement signed integer.
+         * All higher bits are discarded.
+         * @param bits The number of low bits to use
+         * @param int The BigInt whose bits to extract
+         */
+        fun asIntN(bits: Int, int: BigInt): BigInt
+
+        /**
+         * Interprets the low bits of a BigInt as an unsigned integer.
+         * All higher bits are discarded.
+         * @param bits The number of low bits to use
+         * @param int The BigInt whose bits to extract
+         */
+        fun asUintN(bits: Int, int: BigInt): BigInt
+    }
+}
 
 external fun BigInt(
     value: Number,
