50859d21768d297b720ada757af4d5c5050f5fc3
Victor Turansky <victor.turansky@gmail.com>
2023-07-09 00:39:12 +0300

    JS. Fix `PropertyDescriptor` properties nullability

--- kotlin-js/src/jsMain/kotlin/js/core/PropertyDescriptor.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/PropertyDescriptor.kt
@@ -1,10 +1,10 @@
 package js.core
 
 external interface PropertyDescriptor<T> {
-    var configurable: Boolean
-    var enumerable: Boolean
-    var value: T
-    var writable: Boolean
-    var get: () -> T
-    var set: (v: T) -> Unit
+    var configurable: Boolean?
+    var enumerable: Boolean?
+    var value: T?
+    var writable: Boolean?
+    var get: (() -> T)?
+    var set: ((v: T) -> Unit)?
 }
