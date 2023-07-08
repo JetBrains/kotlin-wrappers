33552ce66f31c8b5f79234224a3d4915e12db521
Victor Turansky <victor.turansky@gmail.com>
2023-07-09 00:50:33 +0300

    JS. Use `PropertyKey` typealias

--- kotlin-js/src/jsMain/kotlin/js/core/Object.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/Object.kt
@@ -11,19 +11,15 @@ external class Object internal constructor() {
         fun <T : Any, R : T> assign(dest: R, vararg src: T?): R
         fun <T : Any> create(o: T?, properties: dynamic = definedExternally): T
         fun <T : Any> defineProperties(o: T, properties: dynamic): T
-        fun <T : Any, P> defineProperty(o: T, p: String, attributes: PropertyDescriptor<P>): T
-        fun <T : Any, P> defineProperty(o: T, p: Symbol, attributes: PropertyDescriptor<P>): T
+        fun <T : Any, P> defineProperty(o: T, p: PropertyKey, attributes: PropertyDescriptor<P>): T
         fun <T : Any> entries(o: ReadonlyRecord<String, T>): ReadonlyArray<JsTuple2<String, T>>
         fun entries(o: Any): ReadonlyArray<JsTuple2<String, Any?>>
         fun <R, T : R> freeze(o: T): R
-        fun <T> getOwnPropertyDescriptor(o: Any, p: String): PropertyDescriptor<T>?
-        fun <T> getOwnPropertyDescriptor(o: Any, p: Symbol): PropertyDescriptor<T>?
+        fun <T> getOwnPropertyDescriptor(o: Any, p: PropertyKey): PropertyDescriptor<T>?
         fun getOwnPropertyNames(o: Any): ReadonlyArray<String>
         fun getOwnPropertySymbols(o: Any): ReadonlyArray<Symbol>
         fun <P : Any, T : P> getPrototypeOf(o: T): P
-        fun hasOwn(o: Any, v: String /* PropertyKey */): Boolean
-        fun hasOwn(o: Any, v: Number /* PropertyKey */): Boolean
-        fun hasOwn(o: Any, v: Symbol /* PropertyKey */): Boolean
+        fun hasOwn(o: Any, v: PropertyKey): Boolean
         fun `is`(value1: Any?, value2: Any?): Boolean
         fun isExtensible(o: Any): Boolean
         fun isFrozen(o: Any): Boolean
