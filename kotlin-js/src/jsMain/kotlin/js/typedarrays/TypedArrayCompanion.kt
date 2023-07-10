d8dda1d6d4d2e5d2b713b05f5a41164db2485dee
Victor Turansky <victor.turansky@gmail.com>
2023-07-10 22:05:48 +0300

    JS. Add `TypedArrayCompanion` class

---
+++ kotlin-js/src/jsMain/kotlin/js/typedarrays/TypedArrayCompanion.kt
@@ -0,0 +1,4 @@
+package js.typedarrays
+
+@JsName("DONT_USE_IS_AS_OPERATORS_WITH_SYNTHETIC_TYPED_ARRAY_COMPANION")
+sealed external class TypedArrayCompanion<S : TypedArray<S, T>, T : Any /* Number */>
