847166c1f4ad71d5ef4e303aec3cb963ab142ab6
Victor Turansky <victor.turansky@gmail.com>
2023-07-10 22:14:34 +0300

    JS. Add `TypedArray.of` common method

--- kotlin-js/src/jsMain/kotlin/js/typedarrays/TypedArrayCompanion.kt
+++ kotlin-js/src/jsMain/kotlin/js/typedarrays/TypedArrayCompanion.kt
@@ -1,4 +1,6 @@
 package js.typedarrays
 
 @JsName("DONT_USE_IS_AS_OPERATORS_WITH_SYNTHETIC_TYPED_ARRAY_COMPANION")
-sealed external class TypedArrayCompanion<S : TypedArray<S, T>, T : Any /* Number */>
+sealed external class TypedArrayCompanion<S : TypedArray<S, T>, T : Any /* Number */> {
+    fun of(vararg items: T): S
+}
