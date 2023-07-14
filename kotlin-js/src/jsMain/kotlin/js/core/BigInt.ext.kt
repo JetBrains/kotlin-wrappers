4f80afca61724966364a34640d2012ccc6cd0b72
Victor Turansky <victor.turansky@gmail.com>
2023-07-14 18:10:50 +0300

    JS. Move `BigInt` factories and extensions in separate file

---
+++ kotlin-js/src/jsMain/kotlin/js/core/BigInt.ext.kt
@@ -0,0 +1,30 @@
+package js.core
+
+external fun BigInt(
+    value: Number,
+): BigInt
+
+external fun BigInt(
+    value: String,
+): BigInt
+
+inline val Number.n: BigInt
+    get() = BigInt(this)
+
+inline val String.n: BigInt
+    get() = BigInt(this)
+
+inline operator fun BigInt.unaryMinus(): BigInt =
+    (-unsafeCast<Double>()).unsafeCast<BigInt>()
+
+inline operator fun BigInt.plus(other: BigInt): BigInt =
+    (unsafeCast<Double>() + other.unsafeCast<Double>()).unsafeCast<BigInt>()
+
+inline operator fun BigInt.minus(other: BigInt): BigInt =
+    (unsafeCast<Double>() - other.unsafeCast<Double>()).unsafeCast<BigInt>()
+
+inline operator fun BigInt.times(other: BigInt): BigInt =
+    (unsafeCast<Double>() * other.unsafeCast<Double>()).unsafeCast<BigInt>()
+
+inline operator fun BigInt.div(other: BigInt): BigInt =
+    (unsafeCast<Double>() / other.unsafeCast<Double>()).unsafeCast<BigInt>()
