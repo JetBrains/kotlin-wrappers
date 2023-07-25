34dfb0346a69a35061a5ae63958edf6775befeb8
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2023-07-25 09:33:36 +0200

    Update require signature

--- kotlin-extensions/src/jsMain/kotlin/kotlinext/js/CommonJS.kt
+++ kotlin-extensions/src/jsMain/kotlin/kotlinext/js/CommonJS.kt
@@ -2,19 +2,19 @@ package kotlinext.js
 
 import kotlin.js.RegExp
 
-external interface Context : JsFunction1<String, dynamic> {
+external interface Context<T> : JsFunction1<String, T> {
     fun resolve(module: String): String
     fun keys(): Array<String>
     val id: Int
 }
 
-fun requireAll(context: Context) = context.keys().forEach(context::invoke)
+fun <T> requireAll(context: Context<T>) = context.keys().forEach(context::invoke)
 
 external object require {
     fun resolve(module: String): String
 
     // Note: require.context is a webpack-specific function
-    fun context(directory: String, useSubdirectories: Boolean = definedExternally, regExp: RegExp = definedExternally, mode: String = definedExternally): Context
+    fun <T> context(directory: String, useSubdirectories: Boolean = definedExternally, regExp: RegExp = definedExternally, mode: String = definedExternally): Context<T>
 }
 
-external fun require(module: String): dynamic
+external fun <T> require(module: String): T
