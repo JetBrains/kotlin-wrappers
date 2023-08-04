5c9fd697efbddc7d7c37cb08a499afb7dfc1f5d8
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-08-04 12:17:55 +0300

    Karakum. Refresh declarations

--- kotlin-remix-run-router/src/jsMain/generated/remix/run/router/getStaticContextFromError.kt
+++ kotlin-remix-run-router/src/jsMain/generated/remix/run/router/getStaticContextFromError.kt
@@ -7,4 +7,8 @@ package remix.run.router
  * Given an existing StaticHandlerContext and an error thrown at render time,
  * provide an updated StaticHandlerContext suitable for a second SSR render
  */
-external fun getStaticContextFromError(routes: js.core.ReadonlyArray<AgnosticDataRouteObject>, context: StaticHandlerContext, error: Any?): StaticHandlerContext
+external fun getStaticContextFromError(
+    routes: js.core.ReadonlyArray<AgnosticDataRouteObject>,
+    context: StaticHandlerContext,
+    error: Any?,
+): StaticHandlerContext
