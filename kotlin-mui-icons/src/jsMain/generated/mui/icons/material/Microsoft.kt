75bb2530774ec184112dca574b305ee5c49640a0
Alexander Popoff <alexander.popoff@flysto.net>
2023-08-07 21:08:05 +0300

    MUI. Icons Material `5.13.7` [generated]

---
+++ kotlin-mui-icons/src/jsMain/generated/mui/icons/material/Microsoft.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/icons-material/Microsoft")
+
+package mui.icons.material
+
+@JsName("default")
+external val Microsoft: SvgIconComponent
