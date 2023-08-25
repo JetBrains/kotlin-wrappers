da6400fd758e2c5f77818152900f114de8bb28be
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-08-25 09:34:10 +0300

    Node. generated -> generatedLegacy

--- kotlin-node/build.gradle.kts
+++ kotlin-node/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
+
 plugins {
     kotlin("multiplatform")
     `kotlin-conventions`
@@ -10,4 +13,13 @@ dependencies {
     jsMainImplementation(kotlinxCoroutines("core"))
 
     jsMainApi(devNpmv("@types/node"))
+}
+
+plugins.withType<KotlinMultiplatformPluginWrapper> {
+    extensions.configure<KotlinMultiplatformExtension> {
+        val generatedLegacyDir = projectDir.resolve("src/jsMain/generatedLegacy")
+        if (generatedLegacyDir.exists()) {
+            sourceSets["jsMain"].kotlin.srcDir(generatedLegacyDir)
+        }
+    }
 }
