f1c4cc287d875308b8815069dc956840298ba250
Alexander Popoff <alexander.popoff@flysto.net>
2023-09-17 19:07:02 +0300

    MUIX Tree View `6.0.0-alpha.3` [generated]

---
+++ kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeItem.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/x-tree-view/TreeItem")
+
+package muix.tree.view
+
+
+@JsName("default")
+external val TreeItem: react.FC<TreeItemProps>
