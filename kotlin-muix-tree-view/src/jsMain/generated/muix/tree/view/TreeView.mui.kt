d6f4c75a7506105b41994286b03133c76edded91
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-09-18 21:29:01 +0400

    MUIX Tree View `6.0.0-alpha.3` [generated] (#2122)

---
+++ kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeView.mui.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
+package muix.tree.view
+
+import web.cssom.ClassName
+
+// language=JavaScript
+@JsName("""(/*union*/{root: 'MuiTreeView-root'}/*union*/)""")
+sealed external interface MuiTreeView {
+    companion object {
+        val root: ClassName
+    }
+}
---
+++ kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeViewContext.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/x-tree-view/TreeViewContext")
+
+package muix.tree.view
+
+external interface TreeViewContextProps : react.Props, TreeViewContextValue
+
+
+@JsName("default")
+external val TreeViewContext: react.FC<TreeViewContextProps>
