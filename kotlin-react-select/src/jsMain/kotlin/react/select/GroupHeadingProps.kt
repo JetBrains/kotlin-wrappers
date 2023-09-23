1cd8da9f5c585a41abd1a45f945a7250f432415e
Alexander Popoff <alexander.popoff@flysto.net>
2023-09-23 13:40:30 +0300

    React Select. Support `GroupHeading` & `Group` components

---
+++ kotlin-react-select/src/jsMain/kotlin/react/select/GroupHeadingProps.kt
@@ -0,0 +1,15 @@
+package react.select
+
+import js.core.ReadonlyArray
+import react.PropsWithChildren
+import react.dom.html.HTMLAttributes
+import web.html.HTMLDivElement
+
+external interface GroupHeadingProps<Option : Any, Group : GroupBase<Option>> : PropsWithChildren {
+    var isDisabled: Boolean
+    var innerProps: HTMLAttributes<HTMLDivElement>?
+    var getValue: () -> ReadonlyArray<Option>
+    var options: ReadonlyArray<Option>
+    var selectOption: (Option) -> Unit
+    var selectProps: SelectProps<Option, Group>
+}
