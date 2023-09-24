f9f8445d75ea9adbca895b438219561cc4a0228f
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-09-24 16:53:53 +0300

    Node. `fs` module, improve naming of options

--- kotlin-node/karakum/nameResolvers/resolveFsOptionsName.js
+++ kotlin-node/karakum/nameResolvers/resolveFsOptionsName.js
@@ -2,14 +2,20 @@ import ts from "typescript";
 import * as karakum from "karakum";
 
 export default (node, context) => {
+    const sourceFileName = node.getSourceFile()?.fileName ?? "generated.d.ts"
+    if (!sourceFileName.endsWith("fs.d.ts")) return null
+
     const typeScriptService = context.lookupService(karakum.typeScriptServiceKey)
     const getParent = typeScriptService?.getParent.bind(typeScriptService) ?? (node => node.parent)
 
-    const union = getParent(node)
-    if (!union) return null
-    if (!ts.isUnionTypeNode(union)) return null
+    let parameter = getParent(node)
+    if (ts.isParenthesizedTypeNode(parameter)) {
+        parameter = getParent(parameter)
+    }
+    if (ts.isUnionTypeNode(parameter)) {
+        parameter = getParent(parameter)
+    }
 
-    const parameter = getParent(union)
     if (!parameter) return null
     if (!ts.isParameter(parameter)) return null
     if (!ts.isIdentifier(parameter.name)) return null
@@ -26,6 +32,7 @@ export default (node, context) => {
     if (
         parentName === "readdir"
         || parentName === "readdirSync"
+        || parentName === "watch"
     ) {
         let infix = "String"
 
@@ -43,10 +50,29 @@ export default (node, context) => {
             infix = "Buffer"
         }
 
+        if (
+            ts.isIntersectionTypeNode(node)
+            && node.types[1]
+            && ts.isTypeLiteralNode(node.types[1])
+            && node.types[1].members.some(member => (
+                ts.isPropertySignature(member)
+                && ts.isIdentifier(member.name)
+                && member.name.text === "encoding"
+                && ts.isLiteralTypeNode(member.type)
+                && ts.isStringLiteral(member.type.literal)
+                && member.type.literal.text === "buffer"
+            ))
+        ) {
+            infix = "Buffer"
+        }
+
         return `${karakum.capitalize(parentName)}${infix}${karakum.capitalize(parameterName)}`
     }
 
-    if (parentName === "readFileSync") {
+    if (
+        parentName === "readFile"
+        || parentName === "readFileSync"
+    ) {
         let infix = "Buffer"
 
         if (
@@ -61,6 +87,84 @@ export default (node, context) => {
             ))
         ) {
             infix = "String"
+        }
+
+        if (
+            ts.isIntersectionTypeNode(node)
+            && node.types[0]
+            && ts.isTypeLiteralNode(node.types[0])
+            && node.types[0].members.some(member => (
+                ts.isPropertySignature(member)
+                && ts.isIdentifier(member.name)
+                && member.name.text === "encoding"
+                && ts.isTypeReferenceNode(member.type)
+                && ts.isIdentifier(member.type.typeName)
+                && member.type.typeName.text === "BufferEncoding"
+            ))
+        ) {
+            infix = "String"
+        }
+
+        if (
+            ts.isIntersectionTypeNode(node)
+            && node.types[0]
+            && ts.isTypeReferenceNode(node.types[0])
+            && ts.isIdentifier(node.types[0].typeName)
+            && node.types[0].typeName.text === "ObjectEncodingOptions"
+        ) {
+            infix = ""
+        }
+
+        return `${karakum.capitalize(parentName)}${infix}${karakum.capitalize(parameterName)}`
+    }
+
+    if (
+        parentName === "stat"
+        || parentName === "statSync"
+        || parentName === "statfs"
+        || parentName === "statfsSync"
+        || parentName === "fstat"
+        || parentName === "fstatSync"
+        || parentName === "lstat"
+        || parentName === "watchFile"
+    ) {
+        let infix = parentName === "stat" ? "Simple" : ""
+
+        if (
+            ts.isIntersectionTypeNode(node)
+            && node.types[1]
+            && ts.isTypeLiteralNode(node.types[1])
+            && node.types[1].members.some(member => (
+                ts.isPropertySignature(member)
+                && ts.isIdentifier(member.name)
+                && member.name.text === "bigint"
+                && ts.isLiteralTypeNode(member.type)
+                && member.type.literal.kind === ts.SyntaxKind.TrueKeyword
+            ))
+        ) {
+            infix = "BigInt"
+        }
+
+        return `${karakum.capitalize(parentName)}${infix}${karakum.capitalize(parameterName)}`
+    }if (
+        parentName === "mkdir"
+        || parentName === "mkdirSync"
+    ) {
+        let infix = ""
+
+        if (
+            ts.isIntersectionTypeNode(node)
+            && node.types[1]
+            && ts.isTypeLiteralNode(node.types[1])
+            && node.types[1].members.some(member => (
+                ts.isPropertySignature(member)
+                && ts.isIdentifier(member.name)
+                && member.name.text === "recursive"
+                && ts.isLiteralTypeNode(member.type)
+                && member.type.literal.kind === ts.SyntaxKind.TrueKeyword
+            ))
+        ) {
+            infix = "Recursive"
         }
 
         return `${karakum.capitalize(parentName)}${infix}${karakum.capitalize(parameterName)}`
