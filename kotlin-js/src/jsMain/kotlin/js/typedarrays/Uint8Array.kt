982d2e6ad17d2fb913340a897f86aa7f9067d61f
Edoardo Luppi <lp.edoardo@gmail.com>
2023-09-25 22:26:31 +0200

    feat: allow converting Uint8Array-s to Kotlin's types (#2129)

--- kotlin-js/src/jsMain/kotlin/js/typedarrays/Uint8Array.kt
+++ kotlin-js/src/jsMain/kotlin/js/typedarrays/Uint8Array.kt
@@ -1,5 +1,9 @@
 @file:Suppress(
     "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
+    "INLINE_EXTERNAL_DECLARATION",
+    "INLINE_CLASS_IN_EXTERNAL_DECLARATION_WARNING",
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "NOTHING_TO_INLINE",
 )
 
 package js.typedarrays
@@ -17,6 +21,12 @@ open external class Uint8Array(
     constructor(length: Int)
     constructor(elements: JsIterable<Byte>)
     constructor(elements: ReadonlyArray<Byte>)
+
+    inline fun toByteArray(): ByteArray =
+        Int8Array(buffer, byteOffset, length).asByteArray()
+
+    inline fun toUByteArray(): UByteArray =
+        toByteArray().asUByteArray()
 
     companion object : TypedArrayCompanion<Uint8Array, Byte>
 }
