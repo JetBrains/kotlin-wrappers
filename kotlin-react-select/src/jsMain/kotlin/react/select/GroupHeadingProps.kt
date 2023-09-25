5348b1b3356db96277a467c0826061749d657e65
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-09-26 00:25:29 +0400

    React Select. Support `GroupHeading.data` (#2130)

--- kotlin-react-select/src/jsMain/kotlin/react/select/GroupHeadingProps.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/GroupHeadingProps.kt
@@ -9,6 +9,7 @@ external interface GroupHeadingProps<Option : Any, Group : GroupBase<Option>> : 
     var isDisabled: Boolean
     var innerProps: HTMLAttributes<HTMLDivElement>?
     var getValue: () -> ReadonlyArray<Option>
+    var data: Group
     var options: ReadonlyArray<Option>
     var selectOption: (Option) -> Unit
     var selectProps: SelectProps<Option, Group>
