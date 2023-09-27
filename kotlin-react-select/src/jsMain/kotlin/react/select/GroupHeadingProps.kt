022b324d4567cb7109fc0e974eaaa342d6fbf3ff
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-09-27 13:20:32 +0400

    React Select. Improved `GroupHeading` component (#2136)

--- kotlin-react-select/src/jsMain/kotlin/react/select/GroupHeadingProps.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/GroupHeadingProps.kt
@@ -2,15 +2,11 @@ package react.select
 
 import js.core.ReadonlyArray
 import react.PropsWithChildren
-import react.dom.html.HTMLAttributes
-import web.html.HTMLDivElement
 
 external interface GroupHeadingProps<Option : Any, Group : GroupBase<Option>> : PropsWithChildren {
-    var isDisabled: Boolean
-    var innerProps: HTMLAttributes<HTMLDivElement>?
-    var getValue: () -> ReadonlyArray<Option>
+    var id: String
     var data: Group
     var options: ReadonlyArray<Option>
-    var selectOption: (Option) -> Unit
     var selectProps: SelectProps<Option, Group>
+    var theme: ThemeConfig?
 }
