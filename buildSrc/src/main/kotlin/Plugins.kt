b0eb75fea3615205664eeb136c96531d3d895670
Victor Turansky <victor.turansky@gmail.com>
2023-10-15 19:10:30 +0300

    Initial Seskar plugin configuration

--- buildSrc/src/main/kotlin/Plugins.kt
+++ buildSrc/src/main/kotlin/Plugins.kt
@@ -3,3 +3,7 @@ import org.gradle.kotlin.dsl.PluginDependenciesSpecScope
 fun PluginDependenciesSpecScope.karakum() {
     id("io.github.sgrishchenko.karakum")
 }
+
+fun PluginDependenciesSpecScope.seskar() {
+    id("io.github.turansky.seskar")
+}
