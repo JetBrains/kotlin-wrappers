189524ac376c6949e203b120215753a9e60a7bd9
Victor Turansky <victor.turansky@gmail.com>
2023-11-06 01:55:44 +0200

    React DOM. Apply Seskar Plugin

--- kotlin-react-dom/build.gradle.kts
+++ kotlin-react-dom/build.gradle.kts
@@ -2,12 +2,15 @@ plugins {
     kotlin("multiplatform")
     `kotlin-conventions`
     `publish-conventions`
+    seskar()
 }
 
 dependencies {
     jsMainApi(project(":kotlin-js"))
     jsMainApi(project(":kotlin-browser"))
     jsMainApi(project(":kotlin-react-core"))
+
+    jsMainImplementation(seskarCore())
 
     jsMainApi(npmv("react-dom"))
 }
