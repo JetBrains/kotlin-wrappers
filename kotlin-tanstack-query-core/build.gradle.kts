e1f7dfe778a9dbf5a4683caa7a6cf242f3c798d8
Victor Turansky <victor.turansky@gmail.com>
2023-11-06 00:17:23 +0200

    Tanstack Query. Apply Seskar Plugin

--- kotlin-tanstack-query-core/build.gradle.kts
+++ kotlin-tanstack-query-core/build.gradle.kts
@@ -2,10 +2,13 @@ plugins {
     kotlin("multiplatform")
     `kotlin-conventions`
     `publish-conventions`
+    seskar()
 }
 
 dependencies {
     jsMainApi(project(":kotlin-browser"))
+
+    jsMainImplementation(seskarCore())
 
     jsMainApi(npmv("@tanstack/query-core"))
 }
