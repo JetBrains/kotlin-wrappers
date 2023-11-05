9bab2bcaa3a2a5efcf5d2faecfa3b67a442628d2
Victor Turansky <victor.turansky@gmail.com>
2023-11-06 00:37:18 +0200

    CSSOM. Apply Seskar Plugin

--- kotlin-cssom-core/build.gradle.kts
+++ kotlin-cssom-core/build.gradle.kts
@@ -2,10 +2,13 @@ plugins {
     kotlin("multiplatform")
     `kotlin-conventions`
     `publish-conventions`
+    seskar()
 }
 
 dependencies {
     jsMainApi(project(":kotlin-js"))
+
+    jsMainImplementation(seskarCore())
 
     jsMainApi(devNpmv("csstype"))
 }
