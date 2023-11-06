3928c10ea88081ca82c14d9953b7935a9f4ee122
Victor Turansky <victor.turansky@gmail.com>
2023-11-06 23:35:39 +0200

    JS. Use Seskar inline values

--- kotlin-js/src/jsMain/kotlin/js/core/PrimitiveHint.kt
+++ kotlin-js/src/jsMain/kotlin/js/core/PrimitiveHint.kt
@@ -1,16 +1,22 @@
 @file:Suppress(
-    "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package js.core
 
-// language=JavaScript
-@JsName("""(/*union*/{number: 'number', string: 'string', default: 'default'}/*union*/)""")
+import seskar.js.JsUnion
+import seskar.js.JsValue
+
+@JsUnion
 sealed external interface PrimitiveHint {
     companion object {
+        @JsValue("number")
         val number: PrimitiveHint
+
+        @JsValue("string")
         val string: PrimitiveHint
+
+        @JsValue("default")
         val default: PrimitiveHint
     }
 }
