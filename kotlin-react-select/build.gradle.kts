40a3c5ed42c0ed35ecec90f654694893d5dd9fb4
Victor Turansky <victor.turansky@gmail.com>
2023-11-07 01:38:22 +0200

    React Select. Apply Seskar Plugin

--- kotlin-react-select/build.gradle.kts
+++ kotlin-react-select/build.gradle.kts
@@ -2,12 +2,15 @@ plugins {
     kotlin("multiplatform")
     `kotlin-conventions`
     `publish-conventions`
+    seskar()
 }
 
 dependencies {
     jsMainApi(project(":kotlin-emotion"))
     jsMainApi(project(":kotlin-react"))
     jsMainApi(project(":kotlin-react-dom"))
+
+    jsMainImplementation(seskarCore())
 
     jsMainApi(npmv("react-select"))
 }
