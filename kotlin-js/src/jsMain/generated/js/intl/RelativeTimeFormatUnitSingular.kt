4a30a06a50c403fe4b01a88143f5d3c96ad80ddb
Victor Turansky <victor.turansky@gmail.com>
2023-11-22 20:59:41 +0200

    Intl. `RelativeTimeFormatUnitSingular` [generated]

---
+++ kotlin-js/src/jsMain/generated/js/intl/RelativeTimeFormatUnitSingular.kt
@@ -0,0 +1,39 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package js.intl
+
+import seskar.js.JsValue
+import seskar.js.JsVirtual
+
+@JsVirtual
+sealed external interface RelativeTimeFormatUnitSingular {
+    companion object {
+        @JsValue("year")
+        val year: RelativeTimeFormatUnitSingular
+
+        @JsValue("quarter")
+        val quarter: RelativeTimeFormatUnitSingular
+
+        @JsValue("month")
+        val month: RelativeTimeFormatUnitSingular
+
+        @JsValue("week")
+        val week: RelativeTimeFormatUnitSingular
+
+        @JsValue("day")
+        val day: RelativeTimeFormatUnitSingular
+
+        @JsValue("hour")
+        val hour: RelativeTimeFormatUnitSingular
+
+        @JsValue("minute")
+        val minute: RelativeTimeFormatUnitSingular
+
+        @JsValue("second")
+        val second: RelativeTimeFormatUnitSingular
+    }
+}
