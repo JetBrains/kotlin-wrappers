e7170128dfe5396f2b0f96ddc7dc6a34ce76a8dc
Victor Turansky <victor.turansky@gmail.com>
2023-11-22 20:08:01 +0200

    React Select. Update formatting

--- kotlin-react-select/src/jsMain/kotlin/react/select/InputActionMeta.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/InputActionMeta.kt
@@ -2,6 +2,7 @@ package react.select
 
 sealed external interface InputActionMeta {
     var action: InputAction
+
     /** The previous value of the search input. */
     var prevInputValue: String
 }
