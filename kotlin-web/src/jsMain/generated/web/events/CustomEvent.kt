63cc52747c49e777b1bc8136640926ea8f505095
Victor Turansky <victor.turansky@gmail.com>
2023-11-26 16:49:27 +0200

    Events. CustomEvent [generated]

--- kotlin-web/src/jsMain/generated/web/events/CustomEvent.kt
+++ kotlin-web/src/jsMain/generated/web/events/CustomEvent.kt
@@ -1,3 +1,5 @@
+// Automatically generated - do not modify!
+
 @file:Suppress(
     "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
 )
@@ -5,12 +7,20 @@
 package web.events
 
 external interface CustomEventInit<T> : EventInit {
-    var detail: T
+    var detail: T?
 }
 
-external class CustomEvent<T>(
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent) */
+open external class CustomEvent<out T>(
     override val type: EventType<CustomEvent<T>>,
     init: CustomEventInit<T> = definedExternally,
 ) : Event {
+    /**
+     * Returns any custom data event was created with. Typically used for synthetic events.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CustomEvent/detail)
+     */
     val detail: T
+
+    companion object
 }
