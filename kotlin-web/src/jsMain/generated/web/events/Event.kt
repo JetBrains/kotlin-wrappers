128030c2a7e5a6ff2f5594092edde2221d58eda7
Victor Turansky <victor.turansky@gmail.com>
2023-11-26 17:34:03 +0200

    Events. `Event` [generated]

--- kotlin-web/src/jsMain/generated/web/events/Event.kt
+++ kotlin-web/src/jsMain/generated/web/events/Event.kt
@@ -1,3 +1,5 @@
+// Automatically generated - do not modify!
+
 @file:Suppress(
     "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
 )
@@ -17,24 +19,110 @@ open external class Event(
     open val type: EventType<Event>,
     init: EventInit = definedExternally,
 ) {
-    open val target: EventTarget?
-    val currentTarget: EventTarget?
-    val eventPhase: Short
+    /**
+     * Returns true or false depending on how event was initialized. True if event goes through its target's ancestors in reverse tree order, and false otherwise.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/bubbles)
+     */
     val bubbles: Boolean
+
+    /**
+     * Returns true or false depending on how event was initialized. Its return value does not always carry meaning, but true can indicate that part of the operation during which event was dispatched, can be canceled by invoking the preventDefault() method.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/cancelable)
+     */
     val cancelable: Boolean
-    val defaultPrevented: Boolean
+
+    /**
+     * Returns true or false depending on how event was initialized. True if event invokes listeners past a ShadowRoot node that is the root of its target, and false otherwise.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/composed)
+     */
     val composed: Boolean
+
+    /**
+     * Returns the object whose event listener's callback is currently being invoked.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/currentTarget)
+     */
+    val currentTarget: EventTarget?
+
+    /**
+     * Returns true if preventDefault() was invoked successfully to indicate cancelation, and false otherwise.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/defaultPrevented)
+     */
+    val defaultPrevented: Boolean
+
+    /**
+     * Returns the event's phase, which is one of NONE, CAPTURING_PHASE, AT_TARGET, and BUBBLING_PHASE.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/eventPhase)
+     */
+    val eventPhase: Number
+
+    /**
+     * Returns true if event was dispatched by the user agent, and false otherwise.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/isTrusted)
+     */
     val isTrusted: Boolean
+
+    /**
+     * Returns the object to which event is dispatched (its target).
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/target)
+     */
+    open val target: EventTarget?
+
+    /**
+     * Returns the event's timestamp as the number of milliseconds measured relative to the time origin.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/timeStamp)
+     */
     val timeStamp: DOMHighResTimeStamp
+    /**
+     * Returns the type of event, e.g. "click", "hashchange", or "submit".
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/type)
+     */
+    // val type: String
+    /**
+     * Returns the invocation target objects of event's path (objects on which listeners will be invoked), except for any nodes in shadow trees of which the shadow root's mode is "closed" that are not reachable from event's currentTarget.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/composedPath)
+     */
     fun composedPath(): ReadonlyArray<EventTarget>
-    fun stopPropagation()
-    fun stopImmediatePropagation()
+
+    /**
+     * If invoked when the cancelable attribute value is true, and while executing a listener for the event with passive set to false, signals to the operation that caused event to be dispatched that it needs to be canceled.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/preventDefault)
+     */
     fun preventDefault()
 
+    /**
+     * Invoking this method prevents event from reaching any registered event listeners after the current one finishes running and, when dispatched in a tree, also prevents event from reaching any other objects.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/stopImmediatePropagation)
+     */
+    fun stopImmediatePropagation()
+
+    /**
+     * When dispatched in a tree, invoking this method prevents event from reaching any objects other than the current object.
+     *
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Event/stopPropagation)
+     */
+    fun stopPropagation()
+    val NONE: Short
+    val CAPTURING_PHASE: Short
+    val AT_TARGET: Short
+    val BUBBLING_PHASE: Short
+
     companion object {
+        val NONE: Short
+        val CAPTURING_PHASE: Short
         val AT_TARGET: Short
         val BUBBLING_PHASE: Short
-        val CAPTURING_PHASE: Short
-        val NONE: Short
     }
 }
