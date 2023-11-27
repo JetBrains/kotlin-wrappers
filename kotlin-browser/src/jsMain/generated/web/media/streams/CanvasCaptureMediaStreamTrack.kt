75e4991361fe39be4ab8ca27c3444042bb9eb596
Victor Turansky <victor.turansky@gmail.com>
2023-11-27 23:44:17 +0200

    Media Streams. `CanvasCaptureMediaStreamTrack` [generated]

---
+++ kotlin-browser/src/jsMain/generated/web/media/streams/CanvasCaptureMediaStreamTrack.kt
@@ -0,0 +1,15 @@
+// Automatically generated - do not modify!
+
+package web.media.streams
+
+import web.html.HTMLCanvasElement
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/CanvasCaptureMediaStreamTrack) */
+sealed external class CanvasCaptureMediaStreamTrack :
+    MediaStreamTrack {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/CanvasCaptureMediaStreamTrack/canvas) */
+    val canvas: HTMLCanvasElement
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/CanvasCaptureMediaStreamTrack/requestFrame) */
+    fun requestFrame()
+}
