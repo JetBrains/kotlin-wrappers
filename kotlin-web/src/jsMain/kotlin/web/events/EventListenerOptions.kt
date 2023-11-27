4d430fef7f56fd1ab59cb99f0b2358ca1a3c144e
Victor Turansky <victor.turansky@gmail.com>
2023-11-28 00:50:48 +0200

    Events. Remove `AddEventListenerOptions` constants

--- kotlin-web/src/jsMain/kotlin/web/events/EventListenerOptions.kt
+++ kotlin-web/src/jsMain/kotlin/web/events/EventListenerOptions.kt
@@ -1,32 +1,14 @@
-@file:Suppress(
-    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
-)
-
 package web.events
 
-import js.core.jso
 import web.abort.AbortSignal
 
 sealed external interface EventListenerOptions {
     var capture: Boolean?
 }
 
-@JsName("null") // IR workaround
 sealed external interface AddEventListenerOptions :
     EventListenerOptions {
     var once: Boolean?
     var passive: Boolean?
     var signal: AbortSignal?
-
-    companion object
 }
-
-inline val AddEventListenerOptions.Companion.ACTIVE: AddEventListenerOptions
-    get() = jso {
-        passive = false
-    }
-
-inline val AddEventListenerOptions.Companion.CAPTURE: AddEventListenerOptions
-    get() = jso {
-        capture = true
-    }
