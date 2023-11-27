235e7072253f4bda47a7b53f5aa33481186ba7d8
Victor Turansky <victor.turansky@gmail.com>
2023-11-27 23:21:15 +0200

    DOM. `StaticRange` [generated]

---
+++ kotlin-browser/src/jsMain/generated/web/dom/StaticRange.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package web.dom
+
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/StaticRange) */
+external class StaticRange(
+    init: StaticRangeInit,
+) : AbstractRange
---
+++ kotlin-browser/src/jsMain/generated/web/dom/StaticRangeInit.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package web.dom
+
+sealed external interface StaticRangeInit {
+    var endContainer: Node
+    var endOffset: Int
+    var startContainer: Node
+    var startOffset: Int
+}
