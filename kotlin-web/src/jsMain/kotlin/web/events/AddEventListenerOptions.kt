a64ee168832619d233ccb3bddb879321206645e9
Victor Turansky <victor.turansky@gmail.com>
2023-11-28 00:51:51 +0200

    Events. Seperate file for `AddEventListenerOptions`

---
+++ kotlin-web/src/jsMain/kotlin/web/events/AddEventListenerOptions.kt
@@ -0,0 +1,10 @@
+package web.events
+
+import web.abort.AbortSignal
+
+sealed external interface AddEventListenerOptions :
+    EventListenerOptions {
+    var once: Boolean?
+    var passive: Boolean?
+    var signal: AbortSignal?
+}
--- kotlin-web/src/jsMain/kotlin/web/events/EventListenerOptions.kt
+++ kotlin-web/src/jsMain/kotlin/web/events/EventListenerOptions.kt
@@ -1,14 +1,5 @@
 package web.events
 
-import web.abort.AbortSignal
-
 sealed external interface EventListenerOptions {
     var capture: Boolean?
-}
-
-sealed external interface AddEventListenerOptions :
-    EventListenerOptions {
-    var once: Boolean?
-    var passive: Boolean?
-    var signal: AbortSignal?
 }
