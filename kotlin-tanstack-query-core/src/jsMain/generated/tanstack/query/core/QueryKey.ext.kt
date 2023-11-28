3e4feb027fb9e0e6225072bac401e1ce9fbcb5fe
Alexander Popoff <32072831+aerialist7@users.noreply.github.com>
2023-11-28 14:35:48 +0400

    Tanstack Query. `QueryKey` factories (#2176)

---
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/QueryKey.ext.kt
@@ -0,0 +1,20 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package tanstack.query.core
+
+import js.core.ReadonlyArray
+
+inline fun <T : QueryKey> QueryKey(
+    vararg keys: Comparable<*>,
+): T =
+    keys.unsafeCast<T>()
+
+fun <T : QueryKey> QueryKey(
+    parentKey: QueryKey,
+    vararg keys: Comparable<*>,
+): T =
+    (parentKey.unsafeCast<ReadonlyArray<*>>() + keys).unsafeCast<T>()
