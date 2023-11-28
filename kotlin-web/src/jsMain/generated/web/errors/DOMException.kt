d5f732983cb2fcfc84aa31cf4a591320d95c97ef
Victor Turansky <victor.turansky@gmail.com>
2023-11-28 22:42:37 +0200

    Errors. `DOMException` [generated]

--- kotlin-web/src/jsMain/generated/web/errors/DOMException.kt
+++ kotlin-web/src/jsMain/generated/web/errors/DOMException.kt
@@ -1,19 +1,29 @@
+// Automatically generated - do not modify!
+
 package web.errors
 
 import js.errors.JsError
 import js.errors.JsErrorName
 import seskar.js.JsValue
 
+/**
+ * An abnormal event (called an exception) which occurs as a result of calling a method or accessing a property of a web API.
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMException)
+ */
 open external class DOMException(
-    message: String? = definedExternally,
-    name: JsErrorName? = definedExternally,
+    message: String = definedExternally,
+    name: JsErrorName = definedExternally,
 ) : JsError {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMException/message) */
     override val message: String
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/DOMException/name) */
     val name: JsErrorName
 
     companion object {
         /**
-         * The index is not in the allowed range. For example, this can be thrown by the Range object
+         * The index is not in the allowed range. For example, this can be thrown by the `Range` object
          */
         @JsValue("IndexSizeError")
         val IndexSizeError: JsErrorName
@@ -25,7 +35,7 @@ open external class DOMException(
         val HierarchyRequestError: JsErrorName
 
         /**
-         * The object is in the wrong Document
+         * The object is in the wrong `Document`
          */
         @JsValue("WrongDocumentError")
         val WrongDocumentError: JsErrorName
