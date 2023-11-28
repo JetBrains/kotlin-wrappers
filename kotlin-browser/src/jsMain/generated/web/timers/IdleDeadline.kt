76b26467309904f8ff74badf17358f326fe8c4ad
Victor Turansky <victor.turansky@gmail.com>
2023-11-29 00:40:21 +0200

    Timers. Callbacks [generated]

--- kotlin-browser/src/jsMain/generated/web/timers/IdleDeadline.kt
+++ kotlin-browser/src/jsMain/generated/web/timers/IdleDeadline.kt
@@ -1,8 +1,14 @@
+// Automatically generated - do not modify!
+
 package web.timers
 
 import js.core.DOMHighResTimeStamp
 
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline) */
 sealed external class IdleDeadline {
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline/didTimeout) */
     val didTimeout: Boolean
+
+    /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/IdleDeadline/timeRemaining) */
     fun timeRemaining(): DOMHighResTimeStamp
 }
---
+++ kotlin-browser/src/jsMain/generated/web/timers/IdleRequestOptions.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+package web.timers
+
+sealed external interface IdleRequestOptions {
+    var timeout: Int?
+}
