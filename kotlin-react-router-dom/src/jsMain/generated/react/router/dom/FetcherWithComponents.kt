086a5873fccfd01845f8e7d9de98cf1040cd1718
Victor Turansky <victor.turansky@gmail.com>
2023-12-02 03:59:02 +0200

    React Router `6.20.1`

--- kotlin-react-router-dom/src/jsMain/generated/react/router/dom/FetcherWithComponents.kt
+++ kotlin-react-router-dom/src/jsMain/generated/react/router/dom/FetcherWithComponents.kt
@@ -3,12 +3,11 @@
 
 package react.router.dom
 
-import react.ForwardRefExoticComponent
 import remix.run.router.Fetcher
 
 
 sealed external interface FetcherWithComponents<TData> : Fetcher<TData> {
-    var Form: ForwardRefExoticComponent<FetcherFormProps>
+    var Form: react.ForwardRefExoticComponent<FetcherFormProps>
     var submit: FetcherSubmitFunction
     var load: (href: String, opts: (FetcherWithComponentsLoadOpts)? /* use undefined for default */) -> Unit
 }
