d27f6b3373491515a5b6ada85e673f020cd24b0b
Victor Turansky <victor.turansky@gmail.com>
2023-12-03 23:55:47 +0200

    Messaging. Support `Transferable` <-> `ArrayBuffer` conversion [generated]

--- kotlin-web/src/jsMain/generated/web/messaging/Transferable.kt
+++ kotlin-web/src/jsMain/generated/web/messaging/Transferable.kt
@@ -2,5 +2,13 @@
 
 package web.messaging
 
-// OffscreenCanvas | ImageBitmap | MessagePort | ReadableStream | WritableStream | TransformStream | VideoFrame | ArrayBuffer
-external interface Transferable            
+import js.buffer.ArrayBuffer
+
+// OffscreenCanvas | ImageBitmap | MessagePort | ReadableStream | WritableStream | TransformStream | VideoFrame | ArrayBuffer    
+external interface Transferable
+
+inline fun ArrayBuffer.asTransferable(): Transferable =
+    unsafeCast<Transferable>()
+
+inline fun Transferable.asArrayBufferOrNull(): ArrayBuffer? =
+    asDynamic() as? ArrayBuffer    
