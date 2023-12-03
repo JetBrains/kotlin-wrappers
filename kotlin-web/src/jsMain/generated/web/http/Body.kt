d3848d9a2edd26b5423e913e4bb2ee1ba5b65e8d
Victor Turansky <victor.turansky@gmail.com>
2023-12-03 04:18:55 +0200

    HTTP. `Body` - mixin [generated]

--- kotlin-web/src/jsMain/generated/web/http/Body.kt
+++ kotlin-web/src/jsMain/generated/web/http/Body.kt
@@ -1,5 +1,9 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.http
 
 import js.buffer.ArrayBuffer
@@ -8,26 +12,27 @@ import js.typedarrays.Uint8Array
 import web.buffer.Blob
 import web.streams.ReadableStream
 
-@JsName("Object")
-sealed external class /* interface */ Body {
+sealed external interface Body {
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/body) */
     val body: ReadableStream<Uint8Array>?
+        get() = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/bodyUsed) */
     val bodyUsed: Boolean
+        get() = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/arrayBuffer) */
-    fun arrayBuffer(): Promise<ArrayBuffer>
+    fun arrayBuffer(): Promise<ArrayBuffer> = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/blob) */
-    fun blob(): Promise<Blob>
+    fun blob(): Promise<Blob> = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/formData) */
-    fun formData(): Promise<FormData>
+    fun formData(): Promise<FormData> = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/json) */
-    fun json(): Promise<*>
+    fun json(): Promise<*> = definedExternally
 
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Request/text) */
-    fun text(): Promise<String>
+    fun text(): Promise<String> = definedExternally
 }
