d2b5ff7fb28de7578afcc1d6494c39c2023015b2
Victor Turansky <victor.turansky@gmail.com>
2023-12-04 22:02:50 +0200

    JS. `MutableMapLike` - regular interface

--- kotlin-js/src/jsMain/kotlin/js/collections/MutableMapLike.kt
+++ kotlin-js/src/jsMain/kotlin/js/collections/MutableMapLike.kt
@@ -1,24 +1,7 @@
-@file:Suppress(
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-)
-
 package js.collections
 
-import js.core.JsTuple2
-import js.iterable.IterableIterator
-
 external interface MutableMapLike<K, V> : ReadonlyMap<K, V> {
-    override val size: Int
-        get() = definedExternally
-
-    fun clear(): Unit = definedExternally
-    fun delete(key: K): Boolean = definedExternally
-    operator fun set(key: K, value: V): Unit /* this */ = definedExternally
-
-    override operator fun get(key: K): V? = definedExternally
-    override fun entries(): IterableIterator<JsTuple2<K, V>> = definedExternally
-    override fun forEach(action: (value: V, key: K) -> Unit): Unit = definedExternally
-    override fun has(key: K): Boolean = definedExternally
-    override fun keys(): IterableIterator<K> = definedExternally
-    override fun values(): IterableIterator<V> = definedExternally
+    fun clear()
+    fun delete(key: K): Boolean
+    operator fun set(key: K, value: V)
 }
