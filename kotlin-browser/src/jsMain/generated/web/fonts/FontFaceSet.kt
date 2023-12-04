0750e87aed0370f6156a4e8231081ead3f1ac9ef
Victor Turansky <victor.turansky@gmail.com>
2023-12-04 22:19:26 +0200

    Fonts. `FontFaceSet` - interface

--- kotlin-browser/src/jsMain/generated/web/fonts/FontFaceSet.kt
+++ kotlin-browser/src/jsMain/generated/web/fonts/FontFaceSet.kt
@@ -1,20 +1,21 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "INTERFACE_WITH_SUPERCLASS",
+)
+
 package web.fonts
 
 import js.collections.MutableSetLike
-import js.core.JsTuple2
 import js.core.ReadonlyArray
-import js.iterable.IterableIterator
 import js.promise.Promise
 import web.events.Event
 import web.events.EventHandler
 import web.events.EventTarget
 
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/FontFaceSet) */
-external class FontFaceSet(
-    initialFaces: ReadonlyArray<FontFace>,
-) : EventTarget,
+sealed external interface FontFaceSet :
+    EventTarget,
     MutableSetLike<FontFace> {
     /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/FontFaceSet/loading_event) */
     var onloading: EventHandler<Event>?
@@ -42,15 +43,4 @@ external class FontFaceSet(
         font: String,
         text: String = definedExternally,
     ): Promise<ReadonlyArray<FontFace>>
-
-    override val size: Int
-
-    override fun add(value: FontFace)
-    override fun clear()
-    override fun delete(value: FontFace): Boolean
-    override fun entries(): IterableIterator<JsTuple2<FontFace, FontFace>>
-    override fun forEach(action: (value: FontFace) -> Unit)
-    override fun has(value: FontFace): Boolean
-    override fun keys(): IterableIterator<FontFace>
-    override fun values(): IterableIterator<FontFace>
 }
