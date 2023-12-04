e0558097d5f1b9fe3a8b495862a5e035cf96ae9f
Victor Turansky <victor.turansky@gmail.com>
2023-12-04 21:50:52 +0200

    JS. Add `TypedArrayTest`

---
+++ kotlin-js/src/jsTest/kotlin/js/typedarrays/TypedArrayTest.kt
@@ -0,0 +1,35 @@
+package js.typedarrays
+
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class TypedArrayTest {
+    @Test
+    fun Int32Array_is_TypedArray() {
+        val a: Any = Int32Array()
+
+        assertIs<TypedArray<*, *>>(a)
+    }
+
+    @Test
+    fun BigInt64Array_is_TypedArray() {
+        val a: Any = BigInt64Array()
+
+        assertIs<TypedArray<*, *>>(a)
+    }
+
+    @Test
+    fun Int32Array_companion_is_TypedArrayCompanion() {
+        val a: Any = Int32Array
+
+        assertIs<TypedArrayCompanion<*, *>>(a)
+    }
+
+    @Test
+    fun BigInt64Array_companion_is_TypedArrayCompanion() {
+        val a: Any = BigInt64Array
+
+        assertIs<TypedArrayCompanion<*, *>>(a)
+    }
+
+}
