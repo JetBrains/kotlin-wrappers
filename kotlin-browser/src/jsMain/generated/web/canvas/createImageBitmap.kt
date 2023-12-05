af339c96ceb333a459bff12d34067c857520460b
Victor Turansky <victor.turansky@gmail.com>
2023-12-05 03:09:56 +0200

    Canvas. Inline `ImageBitmapSource` union [generated]

--- kotlin-browser/src/jsMain/generated/web/canvas/createImageBitmap.kt
+++ kotlin-browser/src/jsMain/generated/web/canvas/createImageBitmap.kt
@@ -3,15 +3,44 @@
 package web.canvas
 
 import js.promise.Promise
+import web.buffer.Blob
 
 /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/createImageBitmap) */
 external fun createImageBitmap(
-    image: ImageBitmapSource,
+    image: CanvasImageSource,
     options: ImageBitmapOptions? = definedExternally,
 ): Promise<ImageBitmap>
 
 external fun createImageBitmap(
-    image: ImageBitmapSource,
+    image: Blob,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: ImageData,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: CanvasImageSource,
+    sx: Int,
+    sy: Int,
+    sw: Int,
+    sh: Int,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: Blob,
+    sx: Int,
+    sy: Int,
+    sw: Int,
+    sh: Int,
+    options: ImageBitmapOptions? = definedExternally,
+): Promise<ImageBitmap>
+
+external fun createImageBitmap(
+    image: ImageData,
     sx: Int,
     sy: Int,
     sw: Int,
