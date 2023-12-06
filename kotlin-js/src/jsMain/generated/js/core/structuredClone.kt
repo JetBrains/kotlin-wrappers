9aaecf9ed67f513904056fc410394e546c0eb131
Victor Turansky <victor.turansky@gmail.com>
2023-12-06 20:15:33 +0200

    JS. `structuredClone` [generated]

--- kotlin-js/src/jsMain/generated/js/core/structuredClone.kt
+++ kotlin-js/src/jsMain/generated/js/core/structuredClone.kt
@@ -1,5 +1,8 @@
+// Automatically generated - do not modify!
+
 package js.core
 
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/structuredClone) */
 external fun <T> structuredClone(
     value: T,
     options: StructuredSerializeOptions? = definedExternally,
