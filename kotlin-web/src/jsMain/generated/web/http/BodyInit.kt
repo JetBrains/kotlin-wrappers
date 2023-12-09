4a8671b180e6c32f68d7886d4c88f782ed3f3e98
Victor Turansky <victor.turansky@gmail.com>
2023-12-10 00:50:32 +0200

    HTTP. Strict `BodyInit` [generated]

--- kotlin-web/src/jsMain/generated/web/http/BodyInit.kt
+++ kotlin-web/src/jsMain/generated/web/http/BodyInit.kt
@@ -1,5 +1,44 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package web.http
 
-typealias BodyInit = Any /* ReadableStream | Blob | BufferSource | FormData | URLSearchParams | string */
+import js.buffer.BufferSource
+import web.buffer.Blob
+import web.streams.ReadableStream
+import web.url.URLSearchParams
+
+sealed external interface BodyInit
+
+inline fun BodyInit(
+    value: ReadableStream<*>,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
+
+inline fun BodyInit(
+    value: Blob,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
+
+inline fun BodyInit(
+    value: BufferSource,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
+
+inline fun BodyInit(
+    value: FormData,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
+
+inline fun BodyInit(
+    value: URLSearchParams,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
+
+inline fun BodyInit(
+    value: String,
+): BodyInit =
+    value.unsafeCast<BodyInit>()
