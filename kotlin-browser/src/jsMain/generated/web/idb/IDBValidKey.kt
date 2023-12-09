aa08f396f01dd703f42af8a8c0890d8cf1b9764e
Victor Turansky <victor.turansky@gmail.com>
2023-12-10 00:58:36 +0200

    IDB. Strict `IDBValidKey` [generated]

--- kotlin-browser/src/jsMain/generated/web/idb/IDBValidKey.kt
+++ kotlin-browser/src/jsMain/generated/web/idb/IDBValidKey.kt
@@ -1,5 +1,38 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package web.idb
 
-typealias IDBValidKey = Any /* number | string | Date | BufferSource | IDBValidKey[] */
+import js.buffer.BufferSource
+import js.core.ReadonlyArray
+import kotlin.js.Date
+
+sealed external interface IDBValidKey
+
+inline fun IDBValidKey(
+    value: Number,
+): IDBValidKey =
+    value.unsafeCast<IDBValidKey>()
+
+inline fun IDBValidKey(
+    value: String,
+): IDBValidKey =
+    value.unsafeCast<IDBValidKey>()
+
+inline fun IDBValidKey(
+    value: Date,
+): IDBValidKey =
+    value.unsafeCast<IDBValidKey>()
+
+inline fun IDBValidKey(
+    value: BufferSource,
+): IDBValidKey =
+    value.unsafeCast<IDBValidKey>()
+
+inline fun IDBValidKey(
+    value: ReadonlyArray<IDBValidKey>,
+): IDBValidKey =
+    value.unsafeCast<IDBValidKey>()
