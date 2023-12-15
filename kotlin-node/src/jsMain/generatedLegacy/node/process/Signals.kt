56e03a9ceafedb0cdc48c909dd7306a21a914a81
Victor Turansky <victor.turansky@gmail.com>
2023-12-15 20:15:45 +0200

    Node. Use `@JsValue`

--- kotlin-node/src/jsMain/generatedLegacy/node/process/Signals.kt
+++ kotlin-node/src/jsMain/generatedLegacy/node/process/Signals.kt
@@ -1,52 +1,126 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
-    "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package node.process
 
-// language=JavaScript
-@JsName("""(/*union*/{SIGABRT: 'SIGABRT', SIGALRM: 'SIGALRM', SIGBUS: 'SIGBUS', SIGCHLD: 'SIGCHLD', SIGCONT: 'SIGCONT', SIGFPE: 'SIGFPE', SIGHUP: 'SIGHUP', SIGILL: 'SIGILL', SIGINT: 'SIGINT', SIGIO: 'SIGIO', SIGIOT: 'SIGIOT', SIGKILL: 'SIGKILL', SIGPIPE: 'SIGPIPE', SIGPOLL: 'SIGPOLL', SIGPROF: 'SIGPROF', SIGPWR: 'SIGPWR', SIGQUIT: 'SIGQUIT', SIGSEGV: 'SIGSEGV', SIGSTKFLT: 'SIGSTKFLT', SIGSTOP: 'SIGSTOP', SIGSYS: 'SIGSYS', SIGTERM: 'SIGTERM', SIGTRAP: 'SIGTRAP', SIGTSTP: 'SIGTSTP', SIGTTIN: 'SIGTTIN', SIGTTOU: 'SIGTTOU', SIGUNUSED: 'SIGUNUSED', SIGURG: 'SIGURG', SIGUSR1: 'SIGUSR1', SIGUSR2: 'SIGUSR2', SIGVTALRM: 'SIGVTALRM', SIGWINCH: 'SIGWINCH', SIGXCPU: 'SIGXCPU', SIGXFSZ: 'SIGXFSZ', SIGBREAK: 'SIGBREAK', SIGLOST: 'SIGLOST', SIGINFO: 'SIGINFO'}/*union*/)""")
+import seskar.js.JsValue
+import seskar.js.JsVirtual
+
+@JsVirtual
 sealed external interface Signals {
     companion object {
+        @JsValue("SIGABRT")
         val SIGABRT: Signals
+
+        @JsValue("SIGALRM")
         val SIGALRM: Signals
+
+        @JsValue("SIGBUS")
         val SIGBUS: Signals
+
+        @JsValue("SIGCHLD")
         val SIGCHLD: Signals
+
+        @JsValue("SIGCONT")
         val SIGCONT: Signals
+
+        @JsValue("SIGFPE")
         val SIGFPE: Signals
+
+        @JsValue("SIGHUP")
         val SIGHUP: Signals
+
+        @JsValue("SIGILL")
         val SIGILL: Signals
+
+        @JsValue("SIGINT")
         val SIGINT: Signals
+
+        @JsValue("SIGIO")
         val SIGIO: Signals
+
+        @JsValue("SIGIOT")
         val SIGIOT: Signals
+
+        @JsValue("SIGKILL")
         val SIGKILL: Signals
+
+        @JsValue("SIGPIPE")
         val SIGPIPE: Signals
+
+        @JsValue("SIGPOLL")
         val SIGPOLL: Signals
+
+        @JsValue("SIGPROF")
         val SIGPROF: Signals
+
+        @JsValue("SIGPWR")
         val SIGPWR: Signals
+
+        @JsValue("SIGQUIT")
         val SIGQUIT: Signals
+
+        @JsValue("SIGSEGV")
         val SIGSEGV: Signals
+
+        @JsValue("SIGSTKFLT")
         val SIGSTKFLT: Signals
+
+        @JsValue("SIGSTOP")
         val SIGSTOP: Signals
+
+        @JsValue("SIGSYS")
         val SIGSYS: Signals
+
+        @JsValue("SIGTERM")
         val SIGTERM: Signals
+
+        @JsValue("SIGTRAP")
         val SIGTRAP: Signals
+
+        @JsValue("SIGTSTP")
         val SIGTSTP: Signals
+
+        @JsValue("SIGTTIN")
         val SIGTTIN: Signals
+
+        @JsValue("SIGTTOU")
         val SIGTTOU: Signals
+
+        @JsValue("SIGUNUSED")
         val SIGUNUSED: Signals
+
+        @JsValue("SIGURG")
         val SIGURG: Signals
+
+        @JsValue("SIGUSR1")
         val SIGUSR1: Signals
+
+        @JsValue("SIGUSR2")
         val SIGUSR2: Signals
+
+        @JsValue("SIGVTALRM")
         val SIGVTALRM: Signals
+
+        @JsValue("SIGWINCH")
         val SIGWINCH: Signals
+
+        @JsValue("SIGXCPU")
         val SIGXCPU: Signals
+
+        @JsValue("SIGXFSZ")
         val SIGXFSZ: Signals
+
+        @JsValue("SIGBREAK")
         val SIGBREAK: Signals
+
+        @JsValue("SIGLOST")
         val SIGLOST: Signals
+
+        @JsValue("SIGINFO")
         val SIGINFO: Signals
     }
 }
