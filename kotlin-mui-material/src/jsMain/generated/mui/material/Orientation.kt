60e653082a61e6af4c5fbbc8175420bff671afa8
Victor Turansky <victor.turansky@gmail.com>
2023-12-16 00:07:55 +0200

    MUI Material. `Orientation` - typealias

--- kotlin-mui-material/src/jsMain/generated/mui/material/Orientation.kt
+++ kotlin-mui-material/src/jsMain/generated/mui/material/Orientation.kt
@@ -2,19 +2,4 @@
 
 package mui.material
 
-import seskar.js.JsValue
-import seskar.js.JsVirtual
-
-@Suppress(
-    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
-)
-@JsVirtual
-sealed external interface Orientation {
-    companion object {
-        @JsValue("horizontal")
-        val horizontal: Orientation
-
-        @JsValue("vertical")
-        val vertical: Orientation
-    }
-}
+typealias Orientation = mui.base.Orientation
