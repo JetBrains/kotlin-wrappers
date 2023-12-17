9db1784dd57a6757759df3f58f417f70eae0d1d5
sergeigrishchenko <s.i.grishchenko@gmail.com>
2023-12-17 20:10:06 +0200

    Node. Use `@JsValue` fix

--- kotlin-node/src/jsMain/generatedLegacy/node/inspector/RuntimeEvent.kt
+++ kotlin-node/src/jsMain/generatedLegacy/node/inspector/RuntimeEvent.kt
@@ -1,37 +1,37 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
-    "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package node.inspector
 
 import node.events.EventType
+import seskar.js.JsValue
 import seskar.js.JsVirtual
 
 @JsVirtual
 sealed external interface RuntimeEvent {
     companion object {
-        @JsName("Runtime.consoleAPICalled")
+        @JsValue("Runtime.consoleAPICalled")
         val CONSOLE_APICALLED: CONSOLE_APICALLED
 
-        @JsName("Runtime.exceptionRevoked")
+        @JsValue("Runtime.exceptionRevoked")
         val EXCEPTION_REVOKED: EXCEPTION_REVOKED
 
-        @JsName("Runtime.exceptionThrown")
+        @JsValue("Runtime.exceptionThrown")
         val EXCEPTION_THROWN: EXCEPTION_THROWN
 
-        @JsName("Runtime.executionContextCreated")
+        @JsValue("Runtime.executionContextCreated")
         val EXECUTION_CONTEXT_CREATED: EXECUTION_CONTEXT_CREATED
 
-        @JsName("Runtime.executionContextDestroyed")
+        @JsValue("Runtime.executionContextDestroyed")
         val EXECUTION_CONTEXT_DESTROYED: EXECUTION_CONTEXT_DESTROYED
 
-        @JsName("Runtime.executionContextsCleared")
+        @JsValue("Runtime.executionContextsCleared")
         val EXECUTION_CONTEXTS_CLEARED: EXECUTION_CONTEXTS_CLEARED
 
-        @JsName("Runtime.inspectRequested")
+        @JsValue("Runtime.inspectRequested")
         val INSPECT_REQUESTED: INSPECT_REQUESTED
     }
 
