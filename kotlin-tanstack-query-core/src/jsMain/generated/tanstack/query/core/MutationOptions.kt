6b4d2ef3d31bf10532c9463be75e371582885783
Victor Turansky <victor.turansky@gmail.com>
2023-12-17 22:09:29 +0200

    Tanstack Query `5.14.1` [generated]

--- kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/MutationOptions.kt
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/MutationOptions.kt
@@ -8,7 +8,7 @@ external interface MutationOptions<TData, TError, TVariables, TContext> {
     var mutationFn: MutationFunction<TData, TVariables>
     var mutationKey: MutationKey
     var onMutate: (variables: TVariables) -> Promise<TContext?>?
-    var onSuccess: (data: TData, variables: TVariables, context: TContext?) -> Promise<*>?
+    var onSuccess: (data: TData, variables: TVariables, context: TContext) -> Promise<*>?
     var onError: (error: TError, variables: TVariables, context: TContext?) -> Promise<*>?
     var onSettled: (data: TData?, error: TError?, variables: TVariables, context: TContext?) -> Promise<*>?
     var retry: RetryValue<TError>
