034750f66d3c510918c0d779f2dd119680bf5c64
Victor Turansky <victor.turansky@gmail.com>
2023-12-24 01:09:03 +0200

    Web Components. `CustomElementCompanion` [generated]

---
+++ kotlin-browser/src/jsMain/generated/web/components/CustomElementCompanion.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+package web.components
+
+import js.core.ReadonlyArray
+
+external interface CustomElementCompanion {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#responding_to_attribute_changes)
+     */
+    val observedAttributes: ReadonlyArray<String>
+}
