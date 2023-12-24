c551b0eeeb3cec874268b9c30987c72c7549bb18
Victor Turansky <victor.turansky@gmail.com>
2023-12-24 16:57:17 +0200

    Web Components. `CustomElementCompanion.formAssociated` [generated]

--- kotlin-browser/src/jsMain/generated/web/components/CustomElementCompanion.kt
+++ kotlin-browser/src/jsMain/generated/web/components/CustomElementCompanion.kt
@@ -5,8 +5,12 @@ package web.components
 import js.core.ReadonlyArray
 
 external interface CustomElementCompanion {
+    val formAssociated: Boolean?
+        get() = definedExternally
+
     /**
      * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements#responding_to_attribute_changes)
      */
-    val observedAttributes: ReadonlyArray<String>
+    val observedAttributes: ReadonlyArray<String>?
+        get() = definedExternally
 }
