d497d20cd8af1444388ee9b67b77a2b70c7be2f0
Victor Turansky <victor.turansky@gmail.com>
2023-12-30 01:54:00 +0200

    Media Streams. `OverconstrainedError` [generated]

---
+++ kotlin-browser/src/jsMain/generated/web/media/streams/OverconstrainedError.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+package web.media.streams
+
+import web.errors.DOMException
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/OverconstrainedError)
+ */
+external class OverconstrainedError(
+    constraint: String,
+    message: String = definedExternally,
+) : DOMException {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/OverconstrainedError/constraint)
+     */
+    val constraint: String
+}
