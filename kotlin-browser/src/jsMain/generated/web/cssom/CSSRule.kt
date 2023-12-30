87038ac678c64a8bf0193410e7be1a4a304f5096
Victor Turansky <victor.turansky@gmail.com>
2023-12-30 04:02:49 +0200

    Browser. Legacy constants removed [generated]

--- kotlin-browser/src/jsMain/generated/web/cssom/CSSRule.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/CSSRule.kt
@@ -22,31 +22,4 @@ sealed external class CSSRule {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRule/parentStyleSheet)
      */
     val parentStyleSheet: CSSStyleSheet?
-    val STYLE_RULE: Short
-    val CHARSET_RULE: Short
-    val IMPORT_RULE: Short
-    val MEDIA_RULE: Short
-    val FONT_FACE_RULE: Short
-    val PAGE_RULE: Short
-    val NAMESPACE_RULE: Short
-    val KEYFRAMES_RULE: Short
-    val KEYFRAME_RULE: Short
-    val SUPPORTS_RULE: Short
-    val COUNTER_STYLE_RULE: Short
-    val FONT_FEATURE_VALUES_RULE: Short
-
-    companion object {
-        val STYLE_RULE: Short
-        val CHARSET_RULE: Short
-        val IMPORT_RULE: Short
-        val MEDIA_RULE: Short
-        val FONT_FACE_RULE: Short
-        val PAGE_RULE: Short
-        val NAMESPACE_RULE: Short
-        val KEYFRAMES_RULE: Short
-        val KEYFRAME_RULE: Short
-        val SUPPORTS_RULE: Short
-        val COUNTER_STYLE_RULE: Short
-        val FONT_FEATURE_VALUES_RULE: Short
-    }
 }
