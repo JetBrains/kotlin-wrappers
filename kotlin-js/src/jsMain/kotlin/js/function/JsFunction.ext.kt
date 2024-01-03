a7ccb768945ad8e06f29352a87ca1fa812934e06
sergeigrishchenko <s.i.grishchenko@gmail.com>
2024-01-03 18:15:27 +0200

    JS. Rework `JsFunction` interface

--- kotlin-js/src/jsMain/kotlin/js/function/JsFunction.ext.kt
+++
@@ -1,8 +0,0 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
-package js.function
-
-import js.core.JsTuple
-
-inline fun <R> JsFunction(noinline fn: () -> R): JsFunction<Nothing?, JsTuple, R> =
-    fn.unsafeCast<JsFunction<Nothing?, JsTuple, R>>()
