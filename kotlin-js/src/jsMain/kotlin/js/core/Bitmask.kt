b5f59f8dcd241f52c4b45fa9379dbbde3ffced36
Victor Turansky <victor.turansky@gmail.com>
2024-01-03 03:09:02 +0200

    JS. `Bitmask` type [generated]

---
+++ kotlin-js/src/jsMain/kotlin/js/core/Bitmask.kt
@@ -0,0 +1,21 @@
+@file:Suppress(
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "INLINE_EXTERNAL_DECLARATION",
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+    "DECLARATION_CANT_BE_INLINED",
+    "NOTHING_TO_INLINE",
+)
+
+package js.core
+
+@JsExternalInheritorsOnly
+external interface Bitmask<T : Bitmask<T>> {
+    inline val value: Int
+        get() = unsafeCast<Int>()
+
+    inline infix fun or(other: T): T =
+        (value or other.value).unsafeCast<T>()
+
+    inline operator fun contains(other: T): Boolean =
+        (value and other.value) == other.value
+}
