146d0e7b7557fa0b82a3fd70ae4d5ff9a2ab14bc
Victor Turansky <victor.turansky@gmail.com>
2024-01-06 00:53:13 +0200

    Arrays. Extensions

--- kotlin-js/src/jsMain/kotlin/js/arrays/Array.kt
+++ kotlin-js/src/jsMain/kotlin/js/arrays/Array.kt
@@ -2,7 +2,7 @@
     "NOTHING_TO_INLINE",
 )
 
-package js.core
+package js.arrays
 
 inline fun <T> Array<T>.push(
     item: T,
--- kotlin-js/src/jsMain/kotlin/js/arrays/Sequence.ext.kt
+++ kotlin-js/src/jsMain/kotlin/js/arrays/Sequence.ext.kt
@@ -1,4 +1,4 @@
-package js.core
+package js.arrays
 
 fun <T> Sequence<T>.toTypedArray(): Array<T> {
     val array = emptyArray<T>()
