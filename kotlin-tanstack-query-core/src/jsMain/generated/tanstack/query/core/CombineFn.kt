d2f3f2cd91d46d3face5eab92ad974a3e3ba6fe7
Alexander Popoff <alexander.popoff@flysto.net>
2024-01-09 15:28:33 +0200

    Tanstack Query `5.17.3` [generated]

---
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/CombineFn.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "UNUSED_TYPEALIAS_PARAMETER",
+)
+
+package tanstack.query.core
+
+import js.array.ReadonlyArray
+
+typealias CombineFn<TCombinedResult> = (result: ReadonlyArray<QueryObserverResult<*, *>>) -> TCombinedResult
