00e71727a8f28e4ca142dda8bafff6c857a0698a
Alexander Popoff <alexander.popoff@flysto.net>
2024-01-13 22:03:41 +0200

    MUI System `5.15.4` [generated]

---
+++ kotlin-mui-system/src/jsMain/generated/mui/system/Box.classes.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+package mui.system
+
+import web.cssom.ClassName
+
+external interface BoxClasses {
+    /** Styles applied to the root element. */
+    var root: ClassName
+}
