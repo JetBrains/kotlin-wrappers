d236a60e7218fed72eb4475f2d1179435cb7658c
sergeigrishchenko <s.i.grishchenko@gmail.com>
2024-01-15 22:36:35 +0200

    Node. Remove all mentions of `generatedLegacy`

--- kotlin-node/build.gradle.kts
+++ kotlin-node/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.KotlinMultiplatformPluginWrapper
-
 plugins {
     `kotlin-library-conventions`
     karakum()
@@ -13,13 +10,4 @@ dependencies {
     jsMainImplementation(kotlinxCoroutines("core"))
 
     jsMainApi(devNpmv("@types/node"))
-}
-
-plugins.withType<KotlinMultiplatformPluginWrapper> {
-    extensions.configure<KotlinMultiplatformExtension> {
-        val generatedLegacyDir = projectDir.resolve("src/jsMain/generatedLegacy")
-        if (generatedLegacyDir.exists()) {
-            sourceSets["jsMain"].kotlin.srcDir(generatedLegacyDir)
-        }
-    }
 }
