7be6a3fa888aaeb6cf6b8acf8ff247011a6c185f
Victor Turansky <victor.turansky@gmail.com>
2024-01-21 23:01:35 +0200

    Abort. `Abortable` [generated]

--- kotlin-web/src/jsMain/generated/web/abort/Abortable.kt
+++ kotlin-web/src/jsMain/generated/web/abort/Abortable.kt
@@ -2,6 +2,7 @@
 
 package web.abort
 
+@JsExternalInheritorsOnly
 external interface Abortable {
     /**
      * When provided the corresponding `AbortController` can be used to cancel an asynchronous action.
