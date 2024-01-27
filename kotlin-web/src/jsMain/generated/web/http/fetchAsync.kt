4e000d8b5a77fcc142b920a4e99387c2fc5fa39e
Victor Turansky <victor.turansky@gmail.com>
2024-01-28 01:26:19 +0200

    HTTP. `fetchAsync` [generated]

--- kotlin-web/src/jsMain/generated/web/http/fetchAsync.kt
+++ kotlin-web/src/jsMain/generated/web/http/fetchAsync.kt
@@ -1,22 +1,27 @@
+// Automatically generated - do not modify!
+
 package web.http
 
 import js.promise.Promise
 import web.url.URL
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/fetch)
+ */
 @JsName("fetch")
 external fun fetchAsync(
     input: Request,
-    init: RequestInit = definedExternally,
+    init: RequestInit? = definedExternally,
 ): Promise<Response>
 
 @JsName("fetch")
 external fun fetchAsync(
     input: String,
-    init: RequestInit = definedExternally,
+    init: RequestInit? = definedExternally,
 ): Promise<Response>
 
 @JsName("fetch")
 external fun fetchAsync(
     input: URL,
-    init: RequestInit = definedExternally,
+    init: RequestInit? = definedExternally,
 ): Promise<Response>
