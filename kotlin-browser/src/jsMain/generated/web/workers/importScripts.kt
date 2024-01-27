f7f25af59e3cd40d597b807c0d16d570c22d98ee
Victor Turansky <victor.turansky@gmail.com>
2024-01-28 01:43:46 +0200

    Workers. `importScripts` [generated]

--- kotlin-browser/src/jsMain/generated/web/workers/importScripts.kt
+++ kotlin-browser/src/jsMain/generated/web/workers/importScripts.kt
@@ -1,5 +1,18 @@
+// Automatically generated - do not modify!
+
 package web.workers
 
+import web.url.URL
+
+/**
+ * Fetches each URL in urls, executes them one-by-one in the order they are passed, and then returns (or throws if something went amiss).
+ *
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/importScripts)
+ */
 external fun importScripts(
     vararg urls: String,
+)
+
+external fun importScripts(
+    vararg urls: URL,
 )
