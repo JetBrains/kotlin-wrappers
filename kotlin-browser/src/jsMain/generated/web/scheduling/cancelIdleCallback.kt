85975401b8d200289a3a044a10d69a917f8f46e7
Victor Turansky <victor.turansky@gmail.com>
2024-01-28 02:31:53 +0200

    Scheduling. Idle functions [generated]

--- kotlin-browser/src/jsMain/generated/web/scheduling/cancelIdleCallback.kt
+++ kotlin-browser/src/jsMain/generated/web/scheduling/cancelIdleCallback.kt
@@ -1,5 +1,10 @@
+// Automatically generated - do not modify!
+
 package web.scheduling
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/cancelIdleCallback)
+ */
 external fun cancelIdleCallback(
-    requestId: IdleRequestId,
+    id: IdleRequestId,
 )
--- kotlin-browser/src/jsMain/generated/web/scheduling/requestIdleCallback.kt
+++ kotlin-browser/src/jsMain/generated/web/scheduling/requestIdleCallback.kt
@@ -1,6 +1,11 @@
+// Automatically generated - do not modify!
+
 package web.scheduling
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/requestIdleCallback)
+ */
 external fun requestIdleCallback(
     callback: IdleRequestCallback,
-    options: IdleRequestOptions = definedExternally,
+    options: IdleRequestOptions? = definedExternally,
 ): IdleRequestId
