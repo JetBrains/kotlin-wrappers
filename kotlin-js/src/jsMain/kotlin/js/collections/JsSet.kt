1807ed4748f8e78600fc0508ad884a735b5f7b42
Victor Turansky <victor.turansky@gmail.com>
2024-02-07 23:27:49 +0200

    JS. Fix `JsSet` method signatures

--- kotlin-js/src/jsMain/kotlin/js/collections/JsSet.kt
+++ kotlin-js/src/jsMain/kotlin/js/collections/JsSet.kt
@@ -16,16 +16,16 @@ external class JsSet<T>(
     override fun add(value: T)
     override fun clear()
     override fun delete(value: T): Boolean
-    fun difference(other: ReadonlySet<T>): JsSet<T>
+    fun difference(other: SetLike<T>): JsSet<T>
     override fun entries(): IterableIterator<JsTuple2<T, T>>
     override fun forEach(action: (value: T) -> Unit)
     override fun has(key: T): Boolean
-    fun intersection(other: ReadonlySet<T>): JsSet<T>
-    fun isDisjointFrom(other: ReadonlySet<T>): Boolean
-    fun isSubsetOf(other: ReadonlySet<T>): Boolean
-    fun isSupersetOf(other: ReadonlySet<T>): Boolean
+    fun intersection(other: SetLike<T>): JsSet<T>
+    fun isDisjointFrom(other: SetLike<T>): Boolean
+    fun isSubsetOf(other: SetLike<T>): Boolean
+    fun isSupersetOf(other: SetLike<T>): Boolean
     override fun keys(): IterableIterator<T>
-    fun symmetricDifference(other: ReadonlySet<T>): JsSet<T>
-    fun union(other: ReadonlySet<T>): JsSet<T>
+    fun symmetricDifference(other: SetLike<T>): JsSet<T>
+    fun union(other: SetLike<T>): JsSet<T>
     override fun values(): IterableIterator<T>
 }
