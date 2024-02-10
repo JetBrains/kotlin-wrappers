67cd86d42d7866cd994ba2023e6f234f0feecf9f
Victor Turansky <victor.turansky@gmail.com>
2024-02-11 01:04:05 +0200

    Events. `*Init` factories [generated]

---
+++ kotlin-web/src/jsMain/generated/web/events/CustomEventInit.factory.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+package web.events
+
+import js.objects.jso
+
+fun <T> CustomEventInit(
+    detail: T?,
+): CustomEventInit<T> =
+    jso {
+        asDynamic().detail = detail
+    }
---
+++ kotlin-web/src/jsMain/generated/web/messaging/MessageEventInit.factory.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+package web.messaging
+
+import js.objects.jso
+
+fun <T> MessageEventInit(
+    data: T?,
+): MessageEventInit<T> =
+    jso {
+        asDynamic().data = data
+    }
