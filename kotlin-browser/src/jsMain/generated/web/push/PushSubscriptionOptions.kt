2de9e57dec41e70c8d736a310e61e47d39615716
Victor Turansky <victor.turansky@gmail.com>
2024-02-20 02:18:44 +0200

    Browser. Remove invalid annotation

--- kotlin-browser/src/jsMain/generated/web/push/PushSubscriptionOptions.kt
+++ kotlin-browser/src/jsMain/generated/web/push/PushSubscriptionOptions.kt
@@ -3,14 +3,12 @@
 package web.push
 
 import js.buffer.ArrayBuffer
-import js.objects.JsPlainObject
 
 /**
  * Available only in secure contexts.
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PushSubscriptionOptions)
  */
-@JsPlainObject
 sealed external class PushSubscriptionOptions {
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PushSubscriptionOptions/applicationServerKey)
