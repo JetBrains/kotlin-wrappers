e2ff31272bb119b0cbc8e44e4b77a210de583744
Victor Turansky <victor.turansky@gmail.com>
2024-02-27 02:19:34 +0200

    Reflect. Separate file for `ProxyHandler`

--- kotlin-js/src/jsMain/kotlin/js/reflect/Proxy.kt
+++ kotlin-js/src/jsMain/kotlin/js/reflect/Proxy.kt
@@ -1,26 +1,5 @@
 package js.reflect
 
-import js.array.ReadonlyArray
-import js.objects.TypedPropertyDescriptor
-
-external interface ProxyHandler<T : Any> {
-    interface PropertyName /* string | symbol */
-
-    var apply: ((target: T, thisArg: Any, argArray: ReadonlyArray<Any>) -> Any)?
-    var construct: ((target: T, argArray: ReadonlyArray<Any>, newTarget: Function<*>) -> Any)?
-    var defineProperty: ((target: T, p: PropertyName, attributes: TypedPropertyDescriptor<*>) -> Boolean)?
-    var deleteProperty: ((target: T, p: PropertyName) -> Boolean)?
-    var get: ((target: T, p: PropertyName, receiver: Any) -> Any)?
-    var getOwnPropertyDescriptor: ((target: T, p: PropertyName) -> TypedPropertyDescriptor<*>)?
-    var getPrototypeOf: ((target: T) -> Any?)?
-    var has: ((target: T, p: PropertyName) -> Boolean)?
-    var isExtensible: ((target: T) -> Boolean)?
-    var ownKeys: ((target: T) -> ReadonlyArray<PropertyName>)?
-    var preventExtensions: ((target: T) -> Boolean)?
-    var set: ((target: T, p: PropertyName, value: Any, receiver: Any) -> Boolean)?
-    var setPrototypeOf: ((target: T, v: Any?) -> Boolean)?
-}
-
 external class Proxy<T : Any>(
     target: T,
     handler: ProxyHandler<T>,
