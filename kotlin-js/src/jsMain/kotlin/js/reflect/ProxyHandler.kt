2e2624b53895ea4bbb65ac9440c03f3749e42849
Victor Turansky <victor.turansky@gmail.com>
2024-02-27 02:22:23 +0200

    Reflect. Use `PropertyKey` type

--- kotlin-js/src/jsMain/kotlin/js/reflect/ProxyHandler.kt
+++ kotlin-js/src/jsMain/kotlin/js/reflect/ProxyHandler.kt
@@ -1,22 +1,21 @@
 package js.reflect
 
 import js.array.ReadonlyArray
+import js.objects.PropertyKey
 import js.objects.TypedPropertyDescriptor
 
 external interface ProxyHandler<T : Any> {
-    interface PropertyName /* string | symbol */
-
     var apply: ((target: T, thisArg: Any, argArray: ReadonlyArray<Any>) -> Any)?
     var construct: ((target: T, argArray: ReadonlyArray<Any>, newTarget: Function<*>) -> Any)?
-    var defineProperty: ((target: T, p: PropertyName, attributes: TypedPropertyDescriptor<*>) -> Boolean)?
-    var deleteProperty: ((target: T, p: PropertyName) -> Boolean)?
-    var get: ((target: T, p: PropertyName, receiver: Any) -> Any)?
-    var getOwnPropertyDescriptor: ((target: T, p: PropertyName) -> TypedPropertyDescriptor<*>)?
+    var defineProperty: ((target: T, p: PropertyKey, attributes: TypedPropertyDescriptor<*>) -> Boolean)?
+    var deleteProperty: ((target: T, p: PropertyKey) -> Boolean)?
+    var get: ((target: T, p: PropertyKey, receiver: Any) -> Any)?
+    var getOwnPropertyDescriptor: ((target: T, p: PropertyKey) -> TypedPropertyDescriptor<*>)?
     var getPrototypeOf: ((target: T) -> Any?)?
-    var has: ((target: T, p: PropertyName) -> Boolean)?
+    var has: ((target: T, p: PropertyKey) -> Boolean)?
     var isExtensible: ((target: T) -> Boolean)?
-    var ownKeys: ((target: T) -> ReadonlyArray<PropertyName>)?
+    var ownKeys: ((target: T) -> ReadonlyArray<PropertyKey>)?
     var preventExtensions: ((target: T) -> Boolean)?
-    var set: ((target: T, p: PropertyName, value: Any, receiver: Any) -> Boolean)?
+    var set: ((target: T, p: PropertyKey, value: Any, receiver: Any) -> Boolean)?
     var setPrototypeOf: ((target: T, v: Any?) -> Boolean)?
 }
