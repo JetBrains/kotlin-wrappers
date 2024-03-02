d4bc45aa0c289e4a02fd07fd6ec43c062ba51cd1
Victor Turansky <victor.turansky@gmail.com>
2024-03-02 20:46:08 +0200

    GPU. Fix type [generated] (#2219)

--- kotlin-web/src/jsMain/generated/web/gpu/GPUCanvasConfiguration.kt
+++ kotlin-web/src/jsMain/generated/web/gpu/GPUCanvasConfiguration.kt
@@ -5,6 +5,7 @@ package web.gpu
 import js.array.ReadonlyArray
 import js.core.JsLong
 import js.objects.JsPlainObject
+import web.images.PredefinedColorSpace
 
 @JsPlainObject
 sealed external interface GPUCanvasConfiguration {
@@ -12,7 +13,7 @@ sealed external interface GPUCanvasConfiguration {
     var format: GPUTextureFormat
     var usage: GPUTextureUsage?
     var viewFormats: ReadonlyArray<GPUTextureFormat>?
-    var colorSpace: String /* "srgb" | "display-p3" */?
+    var colorSpace: PredefinedColorSpace?
     var alphaMode: GPUCanvasAlphaMode?
     var width: JsLong
     var height: JsLong
