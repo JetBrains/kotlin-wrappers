1e4e1711e127e3f0cfaae65d619846595da1be15
Victor Turansky <victor.turansky@gmail.com>
2024-03-06 02:17:10 +0200

    Web Components. Separate form callback interfaces (#2222)

--- kotlin-browser/src/jsMain/generated/web/form/CustomFormControl.kt
+++ kotlin-browser/src/jsMain/generated/web/form/CustomFormControl.kt
@@ -2,6 +2,33 @@
 
 package web.form
 
+import web.html.HTMLFormElement
+
 external interface CustomFormControl :
-    FormControl,
-    CustomFormControlCallbacks
+    FormControl {
+
+    interface WithFormCallbacks :
+        WithFormAssociatedCallback,
+        WithFormDisabledCallback,
+        WithFormResetCallback,
+        WithFormStateRestoreCallback
+
+    interface WithFormAssociatedCallback {
+        fun formAssociatedCallback(form: HTMLFormElement?)
+    }
+
+    interface WithFormDisabledCallback {
+        fun formDisabledCallback(disabled: Boolean)
+    }
+
+    interface WithFormResetCallback {
+        fun formResetCallback()
+    }
+
+    interface WithFormStateRestoreCallback {
+        fun formStateRestoreCallback(
+            state: Any?, /* File | string | FormData */
+            mode: FormStateRestoreMode,
+        )
+    }
+}
