513ebd3f53cda7216d68a7d5a055bfc0b12b1da4
Victor Turansky <victor.turansky@gmail.com>
2024-03-06 02:30:14 +0200

    Web Components. Add `CustomElementData` (#2222)

---
+++ kotlin-browser/src/jsMain/generated/web/components/CustomElementData.kt
@@ -0,0 +1,13 @@
+// Automatically generated - do not modify!
+
+package web.components
+
+import web.html.HTMLElement
+import web.html.HtmlTagName
+import kotlin.reflect.KClass
+
+open class CustomElementData<T : HTMLElement>
+protected constructor(
+    val name: HtmlTagName<T>,
+    val clazz: KClass<T>,
+) : CustomElementCompanion
---
+++ kotlin-browser/src/jsMain/generated/web/components/CustomElementRegistry.define.kt
@@ -0,0 +1,14 @@
+// Automatically generated - do not modify!
+
+package web.components
+
+import web.html.HTMLElement
+
+fun <T : HTMLElement> CustomElementRegistry.define(
+    data: CustomElementData<T>,
+) {
+    define(
+        name = data.name,
+        constructor = data.clazz.js,
+    )
+}
