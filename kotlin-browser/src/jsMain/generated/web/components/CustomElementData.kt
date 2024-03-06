b1a4b59588081a07c222968c1ee52f85edb172b2
Victor Turansky <victor.turansky@gmail.com>
2024-03-06 02:39:43 +0200

    Web Components. Static properties (#2222)

--- kotlin-browser/src/jsMain/generated/web/components/CustomElementData.kt
+++ kotlin-browser/src/jsMain/generated/web/components/CustomElementData.kt
@@ -2,6 +2,9 @@
 
 package web.components
 
+import js.array.ReadonlyArray
+import js.objects.Object
+import js.objects.jso
 import web.html.HTMLElement
 import web.html.HtmlTagName
 import kotlin.reflect.KClass
@@ -10,4 +13,22 @@ open class CustomElementData<T : HTMLElement>
 protected constructor(
     val name: HtmlTagName<T>,
     val clazz: KClass<T>,
-) : CustomElementCompanion
+) : CustomElementCompanion {
+    init {
+        Object.defineProperty<_, Boolean?>(
+            clazz.js,
+            "formAssociated",
+            jso {
+                get = { this@CustomElementData.formAssociated }
+            }
+        )
+
+        Object.defineProperty<_, ReadonlyArray<String>?>(
+            clazz.js,
+            "observedAttributes",
+            jso {
+                get = { this@CustomElementData.observedAttributes }
+            }
+        )
+    }
+}
