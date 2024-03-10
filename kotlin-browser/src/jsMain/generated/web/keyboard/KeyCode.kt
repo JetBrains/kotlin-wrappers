1e85a1eb10b5880d0021f07c34bec9e2bf2aecf2
Victor Turansky <victor.turansky@gmail.com>
2024-03-10 03:21:26 +0200

    Keyboard. Update unions [generated]

--- kotlin-browser/src/jsMain/generated/web/keyboard/KeyCode.kt
+++ kotlin-browser/src/jsMain/generated/web/keyboard/KeyCode.kt
@@ -1,14 +1,501 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
-    "NOTHING_TO_INLINE",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package web.keyboard
 
-sealed external interface KeyCode
+import seskar.js.JsValue
+import seskar.js.JsVirtual
 
-inline fun KeyCode(
-    code: String,
-): KeyCode =
-    code.unsafeCast<KeyCode>()
+@JsVirtual
+sealed external interface KeyCode {
+    companion object {
+        @JsValue("Backquote")
+        val Backquote: KeyCode
+
+        @JsValue("Backslash")
+        val Backslash: KeyCode
+
+        @JsValue("Backspace")
+        val Backspace: KeyCode
+
+        @JsValue("BracketLeft")
+        val BracketLeft: KeyCode
+
+        @JsValue("BracketRight")
+        val BracketRight: KeyCode
+
+        @JsValue("Comma")
+        val Comma: KeyCode
+
+        @JsValue("Digit0")
+        val Digit0: KeyCode
+
+        @JsValue("Digit1")
+        val Digit1: KeyCode
+
+        @JsValue("Digit2")
+        val Digit2: KeyCode
+
+        @JsValue("Digit3")
+        val Digit3: KeyCode
+
+        @JsValue("Digit4")
+        val Digit4: KeyCode
+
+        @JsValue("Digit5")
+        val Digit5: KeyCode
+
+        @JsValue("Digit6")
+        val Digit6: KeyCode
+
+        @JsValue("Digit7")
+        val Digit7: KeyCode
+
+        @JsValue("Digit8")
+        val Digit8: KeyCode
+
+        @JsValue("Digit9")
+        val Digit9: KeyCode
+
+        @JsValue("Equal")
+        val Equal: KeyCode
+
+        @JsValue("IntlBackslash")
+        val IntlBackslash: KeyCode
+
+        @JsValue("IntlRo")
+        val IntlRo: KeyCode
+
+        @JsValue("IntlYen")
+        val IntlYen: KeyCode
+
+        @JsValue("KeyA")
+        val KeyA: KeyCode
+
+        @JsValue("KeyB")
+        val KeyB: KeyCode
+
+        @JsValue("KeyC")
+        val KeyC: KeyCode
+
+        @JsValue("KeyD")
+        val KeyD: KeyCode
+
+        @JsValue("KeyE")
+        val KeyE: KeyCode
+
+        @JsValue("KeyF")
+        val KeyF: KeyCode
+
+        @JsValue("KeyG")
+        val KeyG: KeyCode
+
+        @JsValue("KeyH")
+        val KeyH: KeyCode
+
+        @JsValue("KeyI")
+        val KeyI: KeyCode
+
+        @JsValue("KeyJ")
+        val KeyJ: KeyCode
+
+        @JsValue("KeyK")
+        val KeyK: KeyCode
+
+        @JsValue("KeyL")
+        val KeyL: KeyCode
+
+        @JsValue("KeyM")
+        val KeyM: KeyCode
+
+        @JsValue("KeyN")
+        val KeyN: KeyCode
+
+        @JsValue("KeyO")
+        val KeyO: KeyCode
+
+        @JsValue("KeyP")
+        val KeyP: KeyCode
+
+        @JsValue("KeyQ")
+        val KeyQ: KeyCode
+
+        @JsValue("KeyR")
+        val KeyR: KeyCode
+
+        @JsValue("KeyS")
+        val KeyS: KeyCode
+
+        @JsValue("KeyT")
+        val KeyT: KeyCode
+
+        @JsValue("KeyU")
+        val KeyU: KeyCode
+
+        @JsValue("KeyV")
+        val KeyV: KeyCode
+
+        @JsValue("KeyW")
+        val KeyW: KeyCode
+
+        @JsValue("KeyX")
+        val KeyX: KeyCode
+
+        @JsValue("KeyY")
+        val KeyY: KeyCode
+
+        @JsValue("KeyZ")
+        val KeyZ: KeyCode
+
+        @JsValue("Minus")
+        val Minus: KeyCode
+
+        @JsValue("Period")
+        val Period: KeyCode
+
+        @JsValue("Quote")
+        val Quote: KeyCode
+
+        @JsValue("Semicolon")
+        val Semicolon: KeyCode
+
+        @JsValue("Slash")
+        val Slash: KeyCode
+
+        @JsValue("AltLeft")
+        val AltLeft: KeyCode
+
+        @JsValue("AltRight")
+        val AltRight: KeyCode
+
+        @JsValue("CapsLock")
+        val CapsLock: KeyCode
+
+        @JsValue("ContextMenu")
+        val ContextMenu: KeyCode
+
+        @JsValue("ControlLeft")
+        val ControlLeft: KeyCode
+
+        @JsValue("ControlRight")
+        val ControlRight: KeyCode
+
+        @JsValue("Enter")
+        val Enter: KeyCode
+
+        @JsValue("MetaLeft")
+        val MetaLeft: KeyCode
+
+        @JsValue("MetaRight")
+        val MetaRight: KeyCode
+
+        @JsValue("ShiftLeft")
+        val ShiftLeft: KeyCode
+
+        @JsValue("ShiftRight")
+        val ShiftRight: KeyCode
+
+        @JsValue("Space")
+        val Space: KeyCode
+
+        @JsValue("Tab")
+        val Tab: KeyCode
+
+        @JsValue("Convert")
+        val Convert: KeyCode
+
+        @JsValue("KanaMode")
+        val KanaMode: KeyCode
+
+        @JsValue("Lang1")
+        val Lang1: KeyCode
+
+        @JsValue("Lang2")
+        val Lang2: KeyCode
+
+        @JsValue("Lang3")
+        val Lang3: KeyCode
+
+        @JsValue("Lang4")
+        val Lang4: KeyCode
+
+        @JsValue("Lang5")
+        val Lang5: KeyCode
+
+        @JsValue("NonConvert")
+        val NonConvert: KeyCode
+
+        @JsValue("OSLeft")
+        val OSLeft: KeyCode
+
+        @JsValue("OSRight")
+        val OSRight: KeyCode
+
+        @JsValue("Delete")
+        val Delete: KeyCode
+
+        @JsValue("End")
+        val End: KeyCode
+
+        @JsValue("Help")
+        val Help: KeyCode
+
+        @JsValue("Home")
+        val Home: KeyCode
+
+        @JsValue("Insert")
+        val Insert: KeyCode
+
+        @JsValue("PageDown")
+        val PageDown: KeyCode
+
+        @JsValue("PageUp")
+        val PageUp: KeyCode
+
+        @JsValue("ArrowDown")
+        val ArrowDown: KeyCode
+
+        @JsValue("ArrowLeft")
+        val ArrowLeft: KeyCode
+
+        @JsValue("ArrowRight")
+        val ArrowRight: KeyCode
+
+        @JsValue("ArrowUp")
+        val ArrowUp: KeyCode
+
+        @JsValue("NumLock")
+        val NumLock: KeyCode
+
+        @JsValue("Numpad0")
+        val Numpad0: KeyCode
+
+        @JsValue("Numpad1")
+        val Numpad1: KeyCode
+
+        @JsValue("Numpad2")
+        val Numpad2: KeyCode
+
+        @JsValue("Numpad3")
+        val Numpad3: KeyCode
+
+        @JsValue("Numpad4")
+        val Numpad4: KeyCode
+
+        @JsValue("Numpad5")
+        val Numpad5: KeyCode
+
+        @JsValue("Numpad6")
+        val Numpad6: KeyCode
+
+        @JsValue("Numpad7")
+        val Numpad7: KeyCode
+
+        @JsValue("Numpad8")
+        val Numpad8: KeyCode
+
+        @JsValue("Numpad9")
+        val Numpad9: KeyCode
+
+        @JsValue("NumpadAdd")
+        val NumpadAdd: KeyCode
+
+        @JsValue("NumpadBackspace")
+        val NumpadBackspace: KeyCode
+
+        @JsValue("NumpadClear")
+        val NumpadClear: KeyCode
+
+        @JsValue("NumpadClearEntry")
+        val NumpadClearEntry: KeyCode
+
+        @JsValue("NumpadComma")
+        val NumpadComma: KeyCode
+
+        @JsValue("NumpadDecimal")
+        val NumpadDecimal: KeyCode
+
+        @JsValue("NumpadDivide")
+        val NumpadDivide: KeyCode
+
+        @JsValue("NumpadEnter")
+        val NumpadEnter: KeyCode
+
+        @JsValue("NumpadEqual")
+        val NumpadEqual: KeyCode
+
+        @JsValue("NumpadHash")
+        val NumpadHash: KeyCode
+
+        @JsValue("NumpadMemoryAdd")
+        val NumpadMemoryAdd: KeyCode
+
+        @JsValue("NumpadMemoryClear")
+        val NumpadMemoryClear: KeyCode
+
+        @JsValue("NumpadMemoryRecall")
+        val NumpadMemoryRecall: KeyCode
+
+        @JsValue("NumpadMemoryStore")
+        val NumpadMemoryStore: KeyCode
+
+        @JsValue("NumpadMemorySubtract")
+        val NumpadMemorySubtract: KeyCode
+
+        @JsValue("NumpadMultiply")
+        val NumpadMultiply: KeyCode
+
+        @JsValue("NumpadParenLeft")
+        val NumpadParenLeft: KeyCode
+
+        @JsValue("NumpadParenRight")
+        val NumpadParenRight: KeyCode
+
+        @JsValue("NumpadStar")
+        val NumpadStar: KeyCode
+
+        @JsValue("NumpadSubtract")
+        val NumpadSubtract: KeyCode
+
+        @JsValue("Escape")
+        val Escape: KeyCode
+
+        @JsValue("F1")
+        val F1: KeyCode
+
+        @JsValue("F2")
+        val F2: KeyCode
+
+        @JsValue("F3")
+        val F3: KeyCode
+
+        @JsValue("F4")
+        val F4: KeyCode
+
+        @JsValue("F5")
+        val F5: KeyCode
+
+        @JsValue("F6")
+        val F6: KeyCode
+
+        @JsValue("F7")
+        val F7: KeyCode
+
+        @JsValue("F8")
+        val F8: KeyCode
+
+        @JsValue("F9")
+        val F9: KeyCode
+
+        @JsValue("F10")
+        val F10: KeyCode
+
+        @JsValue("F11")
+        val F11: KeyCode
+
+        @JsValue("F12")
+        val F12: KeyCode
+
+        @JsValue("F13")
+        val F13: KeyCode
+
+        @JsValue("F14")
+        val F14: KeyCode
+
+        @JsValue("F15")
+        val F15: KeyCode
+
+        @JsValue("Fn")
+        val Fn: KeyCode
+
+        @JsValue("FnLock")
+        val FnLock: KeyCode
+
+        @JsValue("PrintScreen")
+        val PrintScreen: KeyCode
+
+        @JsValue("ScrollLock")
+        val ScrollLock: KeyCode
+
+        @JsValue("Pause")
+        val Pause: KeyCode
+
+        @JsValue("BrowserBack")
+        val BrowserBack: KeyCode
+
+        @JsValue("BrowserFavorites")
+        val BrowserFavorites: KeyCode
+
+        @JsValue("BrowserForward")
+        val BrowserForward: KeyCode
+
+        @JsValue("BrowserHome")
+        val BrowserHome: KeyCode
+
+        @JsValue("BrowserRefresh")
+        val BrowserRefresh: KeyCode
+
+        @JsValue("BrowserSearch")
+        val BrowserSearch: KeyCode
+
+        @JsValue("BrowserStop")
+        val BrowserStop: KeyCode
+
+        @JsValue("Eject")
+        val Eject: KeyCode
+
+        @JsValue("LaunchApp1")
+        val LaunchApp1: KeyCode
+
+        @JsValue("LaunchApp2")
+        val LaunchApp2: KeyCode
+
+        @JsValue("LaunchMail")
+        val LaunchMail: KeyCode
+
+        @JsValue("MediaPlayPause")
+        val MediaPlayPause: KeyCode
+
+        @JsValue("MediaSelect")
+        val MediaSelect: KeyCode
+
+        @JsValue("MediaStop")
+        val MediaStop: KeyCode
+
+        @JsValue("MediaTrackNext")
+        val MediaTrackNext: KeyCode
+
+        @JsValue("MediaTrackPrevious")
+        val MediaTrackPrevious: KeyCode
+
+        @JsValue("Power")
+        val Power: KeyCode
+
+        @JsValue("Sleep")
+        val Sleep: KeyCode
+
+        @JsValue("AudioVolumeDown")
+        val AudioVolumeDown: KeyCode
+
+        @JsValue("AudioVolumeMute")
+        val AudioVolumeMute: KeyCode
+
+        @JsValue("AudioVolumeUp")
+        val AudioVolumeUp: KeyCode
+
+        @JsValue("WakeUp")
+        val WakeUp: KeyCode
+
+        @JsValue("Hiragana")
+        val Hiragana: KeyCode
+
+        @JsValue("Katakana")
+        val Katakana: KeyCode
+
+        @JsValue("Unidentified")
+        val Unidentified: KeyCode
+    }
+}
--- kotlin-browser/src/jsMain/generated/web/keyboard/ModifierKeyCode.kt
+++ kotlin-browser/src/jsMain/generated/web/keyboard/ModifierKeyCode.kt
@@ -1,14 +1,51 @@
 // Automatically generated - do not modify!
 
 @file:Suppress(
-    "NOTHING_TO_INLINE",
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
 
 package web.keyboard
 
-sealed external interface ModifierKeyCode
+import seskar.js.JsValue
+import seskar.js.JsVirtual
 
-inline fun ModifierKeyCode(
-    code: String,
-): ModifierKeyCode =
-    code.unsafeCast<ModifierKeyCode>()
+@JsVirtual
+sealed external interface ModifierKeyCode {
+    companion object {
+        @JsValue("Alt")
+        val Alt: ModifierKeyCode
+
+        @JsValue("AltGraph")
+        val AltGraph: ModifierKeyCode
+
+        @JsValue("CapsLock")
+        val CapsLock: ModifierKeyCode
+
+        @JsValue("Control")
+        val Control: ModifierKeyCode
+
+        @JsValue("Fn")
+        val Fn: ModifierKeyCode
+
+        @JsValue("FnLock")
+        val FnLock: ModifierKeyCode
+
+        @JsValue("Meta")
+        val Meta: ModifierKeyCode
+
+        @JsValue("NumLock")
+        val NumLock: ModifierKeyCode
+
+        @JsValue("ScrollLock")
+        val ScrollLock: ModifierKeyCode
+
+        @JsValue("Shift")
+        val Shift: ModifierKeyCode
+
+        @JsValue("Symbol")
+        val Symbol: ModifierKeyCode
+
+        @JsValue("SymbolLock")
+        val SymbolLock: ModifierKeyCode
+    }
+}
