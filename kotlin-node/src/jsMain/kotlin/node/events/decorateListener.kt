e7ef7566187d4b490dd549cb023eea8bae39f738
Victor Turansky <victor.turansky@gmail.com>
2024-03-25 00:30:26 +0200

    Node. Fix parameter formatting

--- kotlin-node/src/jsMain/kotlin/node/events/decorateListener.kt
+++ kotlin-node/src/jsMain/kotlin/node/events/decorateListener.kt
@@ -7,7 +7,9 @@ import js.symbol.Symbol
 
 private val decoratedListenerKey = Symbol("@@decorated-listener")
 
-fun <P : JsTuple> decorateListener(listener: (P) -> Unit): Function<Unit> {
+fun <P : JsTuple> decorateListener(
+    listener: (P) -> Unit,
+): Function<Unit> {
     if (listener.asDynamic() == null) return undefined.unsafeCast<Function<Unit>>()
 
     var decoratedListener = listener.asDynamic()[decoratedListenerKey]
