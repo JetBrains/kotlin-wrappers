d0db9ea0dbb206b85ff9fa674ce9391e24cc9e36
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 01:34:10 +0200

    JS. Use `AsyncGeneratorFunction` factory function

--- kotlin-js/src/jsTest/kotlin/js/iterable/AsyncIteratorTest.kt
+++ kotlin-js/src/jsTest/kotlin/js/iterable/AsyncIteratorTest.kt
@@ -1,31 +1,26 @@
 package js.iterable
 
+import js.function.invoke
+import js.generator.AsyncGenerator
+import js.generator.AsyncGeneratorFunction
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class AsyncIteratorTest {
-    val asyncGeneratorFn: () -> AsyncIterator<Int>
-
-    init {
-        @Suppress("UNUSED_VARIABLE")
-        // language=javascript
-        val body = """
-            return async function*() {
-              yield await Promise.resolve(1);
-              yield await Promise.resolve(2);
-              yield await Promise.resolve(3);
-            }
-        """.trimIndent()
-
-        // language=javascript
-        asyncGeneratorFn = js("new Function(body)()")
-    }
-
     @Test
     fun operatorIterator() = runTest {
         val expectedList = listOf(1, 2, 3)
-        val asyncGenerator = asyncGeneratorFn()
+
+        val createAsyncGenerator = AsyncGeneratorFunction<AsyncGenerator<Int, *, *>>(
+            // language=javascript
+            """
+            yield await Promise.resolve(1);
+            yield await Promise.resolve(2);
+            yield await Promise.resolve(3);
+            """
+        )
+        val asyncGenerator = createAsyncGenerator()
 
         val actualList = mutableListOf<Int>()
         for (element in asyncGenerator) {
