c4515baa8c9bace677ebae82622b11e42c54bdc9
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 00:10:48 +0200

    JS. Redundant `this` removed

--- kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
@@ -48,16 +48,16 @@ inline fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction(
     fn.unsafeCast<JsFunction<JsTuple, R>>()
 
 inline operator fun <R> JsFunction<JsTuple, R>.invoke(): R =
-    this.asDynamic()()
+    asDynamic()()
 
 inline operator fun <P1, R> JsFunction<JsTuple1<P1>, R>.invoke(p1: P1): R =
-    this.asDynamic()(p1)
+    asDynamic()(p1)
 
 inline operator fun <P1, P2, R> JsFunction<JsTuple2<P1, P2>, R>.invoke(p1: P1, p2: P2): R =
-    this.asDynamic()(p1, p2)
+    asDynamic()(p1, p2)
 
 inline operator fun <P1, P2, P3, R> JsFunction<JsTuple3<P1, P2, P3>, R>.invoke(p1: P1, p2: P2, p3: P3): R =
-    this.asDynamic()(p1, p2, p3)
+    asDynamic()(p1, p2, p3)
 
 inline operator fun <P1, P2, P3, P4, R> JsFunction<JsTuple4<P1, P2, P3, P4>, R>.invoke(
     p1: P1,
@@ -65,7 +65,7 @@ inline operator fun <P1, P2, P3, P4, R> JsFunction<JsTuple4<P1, P2, P3, P4>, R>.
     p3: P3,
     p4: P4,
 ): R =
-    this.asDynamic()(p1, p2, p3, p4)
+    asDynamic()(p1, p2, p3, p4)
 
 inline operator fun <P1, P2, P3, P4, P5, R> JsFunction<JsTuple5<P1, P2, P3, P4, P5>, R>.invoke(
     p1: P1,
@@ -74,7 +74,7 @@ inline operator fun <P1, P2, P3, P4, P5, R> JsFunction<JsTuple5<P1, P2, P3, P4, 
     p4: P4,
     p5: P5,
 ): R =
-    this.asDynamic()(p1, p2, p3, p4, p5)
+    asDynamic()(p1, p2, p3, p4, p5)
 
 inline operator fun <P1, P2, P3, P4, P5, P6, R> JsFunction<JsTuple6<P1, P2, P3, P4, P5, P6>, R>.invoke(
     p1: P1,
@@ -84,7 +84,7 @@ inline operator fun <P1, P2, P3, P4, P5, P6, R> JsFunction<JsTuple6<P1, P2, P3, 
     p5: P5,
     p6: P6,
 ): R =
-    this.asDynamic()(p1, p2, p3, p4, p5, p6)
+    asDynamic()(p1, p2, p3, p4, p5, p6)
 
 inline operator fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction<JsTuple7<P1, P2, P3, P4, P5, P6, P7>, R>.invoke(
     p1: P1,
@@ -95,4 +95,4 @@ inline operator fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction<JsTuple7<P1, P2, 
     p6: P6,
     p7: P7,
 ): R =
-    this.asDynamic()(p1, p2, p3, p4, p5, p6, p7)
+    asDynamic()(p1, p2, p3, p4, p5, p6, p7)
