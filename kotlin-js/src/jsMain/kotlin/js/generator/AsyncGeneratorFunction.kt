5310007f33fc7333a67bda6d10806acd84c6821c
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 01:31:44 +0200

    JS. Add `AsyncGeneratorFunction` factory function

--- kotlin-js/src/jsMain/kotlin/js/generator/AsyncGeneratorFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/generator/AsyncGeneratorFunction.kt
@@ -1,7 +1,19 @@
+// TEMP
+@file:Suppress(
+    "NAME_CONTAINS_ILLEGAL_CHARS",
+)
+
 package js.generator
 
 import js.array.JsTuple
 import js.function.JsFunction
 
+private const val ASYNC_GENERATOR_FUNCTION: String = "(async function* () {}.constructor)"
+
 sealed external interface AsyncGeneratorFunction<in A : JsTuple, out R : AsyncGenerator<*, *, *>>
     : JsFunction<A, R>
+
+@JsName(ASYNC_GENERATOR_FUNCTION)
+external fun <R : AsyncGenerator<*, *, *>> AsyncGeneratorFunction(
+    body: String,
+): AsyncGeneratorFunction<JsTuple, R>
