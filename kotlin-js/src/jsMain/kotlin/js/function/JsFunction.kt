edeb8e88dace75c69676726f9cb2c7c04ab344d1
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 00:11:42 +0200

    JS. Sync formatting

--- kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
@@ -50,13 +50,22 @@ inline fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction(
 inline operator fun <R> JsFunction<JsTuple, R>.invoke(): R =
     asDynamic()()
 
-inline operator fun <P1, R> JsFunction<JsTuple1<P1>, R>.invoke(p1: P1): R =
+inline operator fun <P1, R> JsFunction<JsTuple1<P1>, R>.invoke(
+    p1: P1,
+): R =
     asDynamic()(p1)
 
-inline operator fun <P1, P2, R> JsFunction<JsTuple2<P1, P2>, R>.invoke(p1: P1, p2: P2): R =
+inline operator fun <P1, P2, R> JsFunction<JsTuple2<P1, P2>, R>.invoke(
+    p1: P1,
+    p2: P2,
+): R =
     asDynamic()(p1, p2)
 
-inline operator fun <P1, P2, P3, R> JsFunction<JsTuple3<P1, P2, P3>, R>.invoke(p1: P1, p2: P2, p3: P3): R =
+inline operator fun <P1, P2, P3, R> JsFunction<JsTuple3<P1, P2, P3>, R>.invoke(
+    p1: P1,
+    p2: P2,
+    p3: P3,
+): R =
     asDynamic()(p1, p2, p3)
 
 inline operator fun <P1, P2, P3, P4, R> JsFunction<JsTuple4<P1, P2, P3, P4>, R>.invoke(
