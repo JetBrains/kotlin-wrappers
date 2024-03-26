6871248b0de57a0255b74d89db70fc8e1aa87632
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 02:05:10 +0200

    JS. `AsyncGeneratorFunction` - class

--- kotlin-js/src/jsMain/kotlin/js/generator/AsyncGeneratorFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/generator/AsyncGeneratorFunction.kt
@@ -7,11 +7,15 @@ package js.generator
 
 import js.array.JsTuple
 import js.function.JsFunction
+import seskar.js.JsSpecialName
 
 private const val ASYNC_GENERATOR_FUNCTION: String = "(async function* () {}.constructor)"
 
-sealed external interface AsyncGeneratorFunction<in A : JsTuple, out R : AsyncGenerator<*, *, *>>
-    : JsFunction<A, R>
+@JsSpecialName(ASYNC_GENERATOR_FUNCTION)
+open external class AsyncGeneratorFunction<in A : JsTuple, out R : AsyncGenerator<*, *, *>>(
+    vararg parameterNames: String,
+    body: String,
+) : JsFunction<A, R>
 
 @JsName(ASYNC_GENERATOR_FUNCTION)
 external fun <R : AsyncGenerator<*, *, *>> AsyncGeneratorFunction(
