f17c0fab73d3fd23f56461b167f259fbea790e26
Victor Turansky <victor.turansky@gmail.com>
2024-03-26 02:53:37 +0200

    JS. `GeneratorFunction` - class

--- kotlin-js/src/jsMain/kotlin/js/generator/GeneratorFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/generator/GeneratorFunction.kt
@@ -2,6 +2,12 @@ package js.generator
 
 import js.array.JsTuple
 import js.function.JsFunction
+import seskar.js.JsSpecialName
 
-sealed external interface GeneratorFunction<in A : JsTuple, out R : Generator<*, *, *>>
-    : JsFunction<A, R>
+private const val GENERATOR_FUNCTION: String = "(function* () {}.constructor)"
+
+@JsSpecialName(GENERATOR_FUNCTION)
+open external class GeneratorFunction<in A : JsTuple, out R : Generator<*, *, *>>(
+    vararg parameterNames: String,
+    body: String,
+) : JsFunction<A, R>
