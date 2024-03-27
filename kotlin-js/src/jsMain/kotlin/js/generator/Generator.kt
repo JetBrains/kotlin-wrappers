040f82cc7bcc7d4069843996862f999de0877b30
Victor Turansky <victor.turansky@gmail.com>
2024-03-28 01:49:10 +0200

    JS. `Generator` - hidden class

--- kotlin-js/src/jsMain/kotlin/js/generator/Generator.kt
+++ kotlin-js/src/jsMain/kotlin/js/generator/Generator.kt
@@ -4,7 +4,8 @@ import js.errors.JsError
 import js.iterable.IteratorResult
 import js.iterable.JsIterator
 
-sealed external interface Generator<out T, TReturn, in TNext> : JsIterator<T> {
+@JsName("__HIDDEN_JS_CLASS__")
+sealed external class Generator<out T, TReturn, in TNext> : JsIterator<T> {
     fun next(value: TNext): IteratorResult<T, TReturn>
 
     fun `return`(value: TReturn): IteratorResult<T, TReturn>
