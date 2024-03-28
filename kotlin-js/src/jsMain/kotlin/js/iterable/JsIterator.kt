93540b2ab13ba9206ee9a670db5572bd53cca6aa
Victor Turansky <victor.turansky@gmail.com>
2024-03-28 02:22:40 +0200

    JS. `JsIterator.from()`

--- kotlin-js/src/jsMain/kotlin/js/iterable/JsIterator.kt
+++ kotlin-js/src/jsMain/kotlin/js/iterable/JsIterator.kt
@@ -5,5 +5,6 @@ abstract external class JsIterator<out T> :
     JsIteratorLike<T> {
     companion object {
         fun <T> from(source: JsIteratorLike<T>): JsIterator<T>
+        fun <T> from(source: JsIterable<T>): JsIterator<T>
     }
 }
