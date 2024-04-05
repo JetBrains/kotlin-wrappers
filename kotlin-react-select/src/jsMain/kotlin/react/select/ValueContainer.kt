21450af082c7e093aa666f9b445f593bfe563382
Alexander Popoff <alexander.popoff@flysto.net>
2024-04-05 17:12:48 +0300

    React Select. Support `ValueContainer.getValue` prop

--- kotlin-react-select/src/jsMain/kotlin/react/select/ValueContainer.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/ValueContainer.kt
@@ -4,7 +4,8 @@ import react.PropsWithChildren
 import react.dom.html.HTMLAttributes
 import web.html.HTMLDivElement
 
-external interface ValueContainerProps : PropsWithChildren {
+external interface ValueContainerProps<Option, Group : GroupBase<Option>> : PropsWithChildren {
     var innerProps: HTMLAttributes<HTMLDivElement>?
     var isDisabled: Boolean
+    var getValue: () -> Options<Option>
 }
