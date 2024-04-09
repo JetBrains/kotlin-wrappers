ce0976b479a344f44c90f15b40f47130696511ff
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 00:54:52 +0300

    Temporal. `DurationLike`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/DurationLike.kt
@@ -0,0 +1,17 @@
+package js.temporal
+
+import js.objects.JsPlainObject
+
+@JsPlainObject
+sealed external interface DurationLike {
+    var years: Int?
+    var months: Int?
+    var weeks: Int?
+    var days: Int?
+    var hours: Int?
+    var minutes: Int?
+    var seconds: Int?
+    var milliseconds: Int?
+    var microseconds: Int?
+    var nanoseconds: Int?
+}
