b518bd0aa2874bdee0c5fa516165889025cc4e6c
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 02:44:35 +0300

    Temporal. `PlainDateTimeLike`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/PlainDateTimeLike.kt
@@ -0,0 +1,20 @@
+package js.temporal
+
+import js.objects.JsPlainObject
+
+@JsPlainObject
+sealed external interface PlainDateTimeLike {
+    var era: String?
+    var eraYear: Int?
+    var year: Int?
+    var month: Int?
+    var monthCode: String?
+    var day: Int?
+    var hour: Int?
+    var minute: Int?
+    var second: Int?
+    var millisecond: Int?
+    var microsecond: Int?
+    var nanosecond: Int?
+    var calendar: CalendarLike?
+}
