df39b4373ee20a8784c9692e8b14bd49fa35afe5
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 02:37:57 +0300

    Temporal. `CalendarLike`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/CalendarLike.kt
@@ -0,0 +1,14 @@
+package js.temporal
+
+/**
+ * Any of these types can be passed to Temporal methods instead of a Temporal.Calendar.
+ */
+typealias CalendarLike = Any /*
+    | string
+    | CalendarProtocol
+    | ZonedDateTime
+    | PlainDateTime
+    | PlainDate
+    | PlainYearMonth
+    | PlainMonthDay
+*/
