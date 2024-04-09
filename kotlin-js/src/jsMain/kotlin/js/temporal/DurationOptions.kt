ba0d0bb6e7a16ce0e96d6d2d5b08e730812d8c10
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 00:36:05 +0300

    Temporal. `DurationOptions`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/DurationOptions.kt
@@ -0,0 +1,23 @@
+package js.temporal
+
+import js.objects.JsPlainObject
+
+/**
+ * Options for assigning fields using `Duration.prototype.with()` or entire
+ * objects with `Duration.from()`, and for arithmetic with
+ * `Duration.prototype.add()` and `Duration.prototype.subtract()`.
+ */
+@JsPlainObject
+sealed external interface DurationOptions {
+    /**
+     * How to deal with out-of-range values
+     *
+     * - In `'constrain'` mode, out-of-range values are clamped to the nearest
+     *   in-range value.
+     * - In `'balance'` mode, out-of-range values are resolved by balancing them
+     *   with the next highest unit.
+     *
+     * The default is `'constrain'`.
+     */
+    var overflow: String? /* "constrain" | "balance" */
+}
