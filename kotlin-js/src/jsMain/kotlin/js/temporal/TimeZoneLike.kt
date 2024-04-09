1fcdd5a4fbd3010bcd08a03a9b4d3145fe51d9b2
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 02:32:12 +0300

    Temporal. `TimeZoneLike`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/TimeZoneLike.kt
@@ -0,0 +1,6 @@
+package js.temporal
+
+/**
+ * Any of these types can be passed to Temporal methods instead of a Temporal.TimeZone.
+ */
+typealias TimeZoneLike = Any /* String | TimeZoneProtocol | ZonedDateTime */
