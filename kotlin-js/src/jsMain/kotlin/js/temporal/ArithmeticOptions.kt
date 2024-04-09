6c4afc3658a832ced0663d95c60b5dbf4d1dd610
Victor Turansky <victor.turansky@gmail.com>
2024-04-10 00:42:25 +0300

    Temporal. `ArithmeticOptions`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/ArithmeticOptions.kt
@@ -0,0 +1,19 @@
+package js.temporal
+
+import js.objects.JsPlainObject
+
+/**
+ * Options for arithmetic operations like `add()` and `subtract()`
+ */
+@JsPlainObject
+sealed external interface ArithmeticOptions {
+    /**
+     * Controls handling of out-of-range arithmetic results.
+     *
+     * If a result is out of range, then `'constrain'` will clamp the result to
+     * the allowed range, while `'reject'` will throw a RangeError.
+     *
+     * The default is `'constrain'`.
+     */
+    var overflow: String? /* "constrain" | "reject" */
+}
