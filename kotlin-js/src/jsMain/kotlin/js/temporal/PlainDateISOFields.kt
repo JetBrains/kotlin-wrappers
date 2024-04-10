58d61d64066a71869b17cb14ca3f952885bf1edf
Victor Turansky <victor.turansky@gmail.com>
2024-04-11 00:59:38 +0300

    Temporal. `PlainDateISOFields`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/PlainDateISOFields.kt
@@ -0,0 +1,11 @@
+package js.temporal
+
+import js.objects.JsPlainObject
+
+@JsPlainObject
+sealed external interface PlainDateISOFields {
+    var isoYear: Int
+    var isoMonth: Int
+    var isoDay: Int
+    var calendar: String /* | CalendarProtocol */
+}
