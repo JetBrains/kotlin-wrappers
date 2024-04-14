7c934a771d521d4b437efcf6c4e91f0c37fa2bfc
Victor Turansky <victor.turansky@gmail.com>
2024-04-14 20:52:27 +0300

    Temporal. `Factory`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/Factory.kt
@@ -0,0 +1,13 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
+package js.temporal
+
+sealed external interface Factory<T : Any, in L : Any, in O : Any> {
+    fun from(value: T, options: O = definedExternally): T = definedExternally
+    fun from(value: L, options: O = definedExternally): T = definedExternally
+    fun from(value: String, options: O = definedExternally): T = definedExternally
+}
