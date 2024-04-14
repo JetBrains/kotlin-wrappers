a9d318a8bf1b763830f6c9a080c9e1f5734bb8bb
Victor Turansky <victor.turansky@gmail.com>
2024-04-15 02:17:23 +0300

    Temporal. `PlainYearMonth`

--- kotlin-js/src/jsMain/kotlin/js/temporal/PlainYearMonth.kt
+++ kotlin-js/src/jsMain/kotlin/js/temporal/PlainYearMonth.kt
@@ -2,5 +2,53 @@
 
 package js.temporal
 
-// TODO: implement
-external class PlainYearMonth
+import js.objects.JsPlainObject
+
+/**
+ * A `Temporal.PlainYearMonth` represents a particular month on the calendar. For
+ * example, it could be used to represent a particular instance of a monthly
+ * recurring event, like "the June 2019 meeting".
+ *
+ * See https://tc39.es/proposal-temporal/docs/yearmonth.html for more details.
+ *
+ * @category Temporal
+ */
+external class PlainYearMonth(
+    isoYear: Int,
+    isoMonth: Int,
+    calendar: CalendarLike = definedExternally,
+    referenceISODay: Int = definedExternally,
+) : HasArithmeticOperations<PlainYearMonth, PlainYearMonthLike, DateUnit<* /* year..month */>>,
+    LocalizableDateTime {
+    val era: String?
+    val eraYear: Int?
+    val year: Int
+    val month: Int
+    val monthCode: String
+    val calendarId: String
+    fun getCalendar(): CalendarProtocol
+    val daysInMonth: Int
+    val daysInYear: Int
+    val monthsInYear: Int
+    val inLeapYear: Boolean
+
+    // fun equals(other: Temporal.PlainYearMonth | PlainYearMonthLike | String): Boolean;
+    fun with(
+        yearMonthLike: PlainYearMonthLike,
+        options: AssignmentOptions = definedExternally,
+    ): PlainYearMonth
+
+    fun toPlainDate(options: ToPlainDateOptions): PlainDate
+
+    @JsPlainObject
+    sealed interface ToPlainDateOptions {
+        var day: Int
+    }
+
+    fun getISOFields(): PlainDateISOFields
+    fun toJSON(): String
+    fun toString(options: ShowCalendarOption = definedExternally): String
+
+    companion object :
+        PlainCompanion<PlainYearMonth, PlainYearMonthLike>
+}
