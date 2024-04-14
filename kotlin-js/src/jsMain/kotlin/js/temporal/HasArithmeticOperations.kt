f2880f13b90991a873d664f158e32c40f83843c0
Victor Turansky <victor.turansky@gmail.com>
2024-04-14 21:34:50 +0300

    Temporal. Update `HasArithmeticOperations`

--- kotlin-js/src/jsMain/kotlin/js/temporal/HasArithmeticOperations.kt
+++ kotlin-js/src/jsMain/kotlin/js/temporal/HasArithmeticOperations.kt
@@ -6,64 +6,64 @@
 
 package js.temporal
 
-sealed external interface HasArithmeticOperations<T : Any, in L : Any, U : DateTimeUnit<U>> {
+sealed external interface HasArithmeticOperations<T : HasArithmeticOperations<T, L, U>, in L : Any, U : DateTimeUnit<U>> {
     fun add(
         duration: Duration,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun add(
         duration: DurationLike,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun add(
         duration: String,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun subtract(
         duration: Duration,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun subtract(
         duration: DurationLike,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun subtract(
         duration: String,
         options: ArithmeticOptions = definedExternally,
-    ): T
+    ): T = definedExternally
 
     fun until(
         other: T,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 
     fun until(
         other: L,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 
     fun until(
         other: String,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 
     fun since(
         other: T,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 
     fun since(
         other: L,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 
     fun since(
         other: String,
         options: DifferenceOptions<U>,
-    ): Duration
+    ): Duration = definedExternally
 }
