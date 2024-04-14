53a15dd8d63c7b18e155518f5758ad1785adc946
Victor Turansky <victor.turansky@gmail.com>
2024-04-14 04:17:31 +0300

    Temporal. `TimeZoneProtocol`

---
+++ kotlin-js/src/jsMain/kotlin/js/temporal/TimeZoneProtocol.kt
@@ -0,0 +1,30 @@
+package js.temporal
+
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+
+@JsPlainObject
+sealed external interface TimeZoneProtocol {
+    var id: String
+    var getOffsetNanosecondsFor: (instant: Instant /* | string */) -> Int
+    var getOffsetStringFor: ((instant: Instant /* | string */) -> String)?
+    var getPlainDateTimeFor: ((
+        instant: Instant, /* | string */
+        calendar: CalendarLike?,
+    ) -> PlainDateTime)?
+    var getInstantFor: ((
+        dateTime: PlainDateTime, /* | PlainDateTimeLike | string */
+        options: ToInstantOptions?,
+    ) -> Instant)?
+    var getNextTransition: ((
+        startingPoint: Instant, /* | string */
+    ) -> Instant?)?
+    var getPreviousTransition: ((
+        startingPoint: Instant, /* | string */
+    ) -> Instant?)?
+    var getPossibleInstantsFor: (
+        dateTime: PlainDateTime, /* | PlainDateTimeLike | string */
+    ) -> ReadonlyArray<Instant>
+    var toString: (() -> String)?
+    var toJSON: (() -> String)?
+}
