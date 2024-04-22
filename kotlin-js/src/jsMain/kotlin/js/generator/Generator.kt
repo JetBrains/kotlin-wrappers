1e1631fa5a1d7dffa739f552dbc331e348905857
Victor Turansky <victor.turansky@gmail.com>
2024-04-23 00:59:02 +0300

    JS. `Generator`

--- kotlin-js/src/jsMain/kotlin/js/generator/Generator.kt
+++ kotlin-js/src/jsMain/kotlin/js/generator/Generator.kt
@@ -3,8 +3,11 @@ package js.generator
 import js.errors.JsError
 import js.iterable.IteratorResult
 import js.iterable.JsIterator
+import seskar.js.JsSpecialName
 
-@JsName("__HIDDEN_JS_CLASS__")
+private const val GENERATOR: String = "(function* () {}.constructor.prototype.prototype)"
+
+@JsSpecialName(GENERATOR)
 sealed external class Generator<out T, TReturn, in TNext> : JsIterator<T> {
     fun next(value: TNext): IteratorResult<T, TReturn>
 
