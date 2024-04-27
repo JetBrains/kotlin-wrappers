4c0dba504a1b51072fe1dbd1722c42a86845ab42
Victor Turansky <victor.turansky@gmail.com>
2024-04-27 05:02:22 +0300

    K2. Remove contract for `act`

--- kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/act.kt
+++ kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/act.kt
@@ -1,15 +1,9 @@
 package react.dom.test
 
 import js.promise.await
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
 
 suspend fun <T> act(
     block: () -> T,
 ): T {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-
     return actAsync(block).await()
 }
