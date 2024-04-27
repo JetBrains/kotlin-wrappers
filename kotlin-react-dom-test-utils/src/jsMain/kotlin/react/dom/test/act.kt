44469d0722058c56f27247ff83a5d7448add42b8
Victor Turansky <victor.turansky@gmail.com>
2024-04-27 05:04:04 +0300

    K2. Hide contract for `act`

--- kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/act.kt
+++ kotlin-react-dom-test-utils/src/jsMain/kotlin/react/dom/test/act.kt
@@ -5,5 +5,13 @@ import js.promise.await
 suspend fun <T> act(
     block: () -> T,
 ): T {
+    // TODO: report bug
+    //  doesn't work in K2
+    /*
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    */
+
     return actAsync(block).await()
 }
