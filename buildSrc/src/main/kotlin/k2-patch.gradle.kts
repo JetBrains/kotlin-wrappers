65b4773e957fdcb626c0ab86cd48ae26d8920d21
Victor Turansky <victor.turansky@gmail.com>
2024-04-27 05:31:11 +0300

    K2. Fix task configuration

--- buildSrc/src/main/kotlin/k2-patch.gradle.kts
+++ buildSrc/src/main/kotlin/k2-patch.gradle.kts
@@ -1,29 +1,31 @@
 val k2patch by tasks.registering {
-    patchFile("gradle.properties") {
-        it.replace(
-            "kotlin.version=1.9.23",
-            "kotlin.version=2.0.0-RC1",
-        )
-    }
+    doLast {
+        patchFile("gradle.properties") {
+            it.replace(
+                "kotlin.version=1.9.23",
+                "kotlin.version=2.0.0-RC1",
+            )
+        }
 
-    patchFile("buildSrc/src/main/kotlin/Plugins.kt") {
-        it.replace("""id("io.github.sgrishchenko.karakum")""", "")
-    }
+        patchFile("buildSrc/src/main/kotlin/Plugins.kt") {
+            it.replace("""id("io.github.sgrishchenko.karakum")""", "")
+        }
 
-    // WA for KT-67628
-    patchFile("kotlin-js/src/jsMain/kotlin/js/iterable/IteratorYieldResult.kt") {
-        it.replace(
-            "IteratorResult<TYield, Void>",
-            "IteratorResult<TYield, Nothing?>",
-        )
-    }
+        // WA for KT-67628
+        patchFile("kotlin-js/src/jsMain/kotlin/js/iterable/IteratorYieldResult.kt") {
+            it.replace(
+                "IteratorResult<TYield, Void>",
+                "IteratorResult<TYield, Nothing?>",
+            )
+        }
 
-    // WA for KT-67625
-    patchFile("kotlin-react/src/jsMain/kotlin/react/createElementOrNull.kt") {
-        it.replace(
-            "ReadonlyArray<ReactNode?>",
-            "Array<out ReactNode?>",
-        )
+        // WA for KT-67625
+        patchFile("kotlin-react/src/jsMain/kotlin/react/createElementOrNull.kt") {
+            it.replace(
+                "ReadonlyArray<ReactNode?>",
+                "Array<out ReactNode?>",
+            )
+        }
     }
 }
 
