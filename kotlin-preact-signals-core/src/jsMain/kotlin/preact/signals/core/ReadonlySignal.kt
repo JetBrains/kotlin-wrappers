a5dd372fb1aa63487287c5378bab4d4afa3f04e1
Victor Turansky <victor.turansky@gmail.com>
2024-05-01 01:17:58 +0300

    Preact Signals. `Signal` - delegate

--- kotlin-preact-signals-core/src/jsMain/kotlin/preact/signals/core/ReadonlySignal.kt
+++ kotlin-preact-signals-core/src/jsMain/kotlin/preact/signals/core/ReadonlySignal.kt
@@ -1,10 +1,27 @@
+@file:Suppress(
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "INLINE_EXTERNAL_DECLARATION",
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+    "DECLARATION_CANT_BE_INLINED",
+    "NOTHING_TO_INLINE",
+)
+
 package preact.signals.core
+
+import kotlin.reflect.KProperty
 
 /**
  * An interface for read-only signals.
  */
 sealed external interface ReadonlySignal<out T> {
     val value: T
+
+    inline operator fun getValue(
+        thisRef: Nothing?,
+        property: KProperty<*>,
+    ): T =
+        value
+
     fun subscribe(
         fn: (value: T) -> Unit,
     ): () -> Unit
--- kotlin-preact-signals-core/src/jsMain/kotlin/preact/signals/core/Signal.kt
+++ kotlin-preact-signals-core/src/jsMain/kotlin/preact/signals/core/Signal.kt
@@ -1,10 +1,17 @@
 @file:JsModule("@preact/signals-core")
 
 @file:Suppress(
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "INLINE_EXTERNAL_DECLARATION",
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+    "DECLARATION_CANT_BE_INLINED",
+    "NOTHING_TO_INLINE",
     "EXTERNAL_CLASS_CONSTRUCTOR_PROPERTY_PARAMETER",
 )
 
 package preact.signals.core
+
+import kotlin.reflect.KProperty
 
 /**
  * The base class for plain and computed signals.
@@ -12,6 +19,14 @@ package preact.signals.core
 external class Signal<T>(
     override var value: T = definedExternally,
 ) : ReadonlySignal<T> {
+    inline operator fun setValue(
+        thisRef: Nothing?,
+        property: KProperty<*>,
+        value: T,
+    ) {
+        this.value = value
+    }
+
     override fun subscribe(
         fn: (value: T) -> Unit,
     ): () -> Unit
