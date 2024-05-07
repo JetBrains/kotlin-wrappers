f8a95f7b5bfa5aad2aa1048c7d80020f838fc573
Victor Turansky <victor.turansky@gmail.com>
2024-05-08 02:53:45 +0300

    JS. Separate file for `JsFunction` adapters

---
+++ kotlin-js/src/jsMain/kotlin/js/function/JsFunction.adapter.kt
@@ -0,0 +1,45 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package js.function
+
+import js.array.*
+
+inline fun <R> JsFunction(
+    noinline fn: () -> R,
+): JsFunction<JsTuple, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, R> JsFunction(
+    noinline fn: (P1) -> R,
+): JsFunction<JsTuple1<P1>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, R> JsFunction(
+    noinline fn: (P1, P2) -> R,
+): JsFunction<JsTuple2<P1, P2>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, P3, R> JsFunction(
+    noinline fn: (P1, P2, P3) -> R,
+): JsFunction<JsTuple3<P1, P2, P3>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, P3, P4, R> JsFunction(
+    noinline fn: (P1, P2, P3, P4) -> R,
+): JsFunction<JsTuple4<P1, P2, P3, P4>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, P3, P4, P5, R> JsFunction(
+    noinline fn: (P1, P2, P3, P4, P5) -> R,
+): JsFunction<JsTuple5<P1, P2, P3, P4, P5>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, P3, P4, P5, P6, R> JsFunction(
+    noinline fn: (P1, P2, P3, P4, P5, P6) -> R,
+): JsFunction<JsTuple6<P1, P2, P3, P4, P5, P6>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
+
+inline fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction(
+    noinline fn: (P1, P2, P3, P4, P5, P6, P7) -> R,
+): JsFunction<JsTuple7<P1, P2, P3, P4, P5, P6, P7>, R> =
+    fn.unsafeCast<JsFunction<JsTuple, R>>()
--- kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
@@ -1,8 +1,6 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
 package js.function
 
-import js.array.*
+import js.array.JsTuple
 
 private const val FUNCTION: String = "Function"
 
@@ -12,43 +10,3 @@ open external class JsFunction<in A : JsTuple, out R>(
     vararg parameterNames: String,
     body: String,
 )
-
-inline fun <R> JsFunction(
-    noinline fn: () -> R,
-): JsFunction<JsTuple, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, R> JsFunction(
-    noinline fn: (P1) -> R,
-): JsFunction<JsTuple1<P1>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, R> JsFunction(
-    noinline fn: (P1, P2) -> R,
-): JsFunction<JsTuple2<P1, P2>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, P3, R> JsFunction(
-    noinline fn: (P1, P2, P3) -> R,
-): JsFunction<JsTuple3<P1, P2, P3>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, P3, P4, R> JsFunction(
-    noinline fn: (P1, P2, P3, P4) -> R,
-): JsFunction<JsTuple4<P1, P2, P3, P4>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, P3, P4, P5, R> JsFunction(
-    noinline fn: (P1, P2, P3, P4, P5) -> R,
-): JsFunction<JsTuple5<P1, P2, P3, P4, P5>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, P3, P4, P5, P6, R> JsFunction(
-    noinline fn: (P1, P2, P3, P4, P5, P6) -> R,
-): JsFunction<JsTuple6<P1, P2, P3, P4, P5, P6>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
-
-inline fun <P1, P2, P3, P4, P5, P6, P7, R> JsFunction(
-    noinline fn: (P1, P2, P3, P4, P5, P6, P7) -> R,
-): JsFunction<JsTuple7<P1, P2, P3, P4, P5, P6, P7>, R> =
-    fn.unsafeCast<JsFunction<JsTuple, R>>()
