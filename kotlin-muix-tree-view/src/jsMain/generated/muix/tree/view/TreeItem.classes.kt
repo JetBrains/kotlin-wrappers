794e5aea00d92933137a82fb8acf1f825793e458
Alexander Popoff <alexander.popoff@flysto.net>
2024-05-07 21:47:22 +0300

    MUIX Tree View. Full style customization support via components' classes [generated]

--- kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeItem.classes.kt
+++ kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeItem.classes.kt
@@ -1,34 +1,38 @@
 // Automatically generated - do not modify!
 
+@file:JsModule("@mui/x-tree-view/TreeItem")
+
 package muix.tree.view
 
 import web.cssom.ClassName
 
-external interface TreeItemClasses {
+sealed external interface TreeItemClasses {
     /** Styles applied to the root element. */
-    var root: ClassName
+    val root: ClassName
 
     /** Styles applied to the transition component. */
-    var group: ClassName
+    val group: ClassName
 
     /** Styles applied to the content element. */
-    var content: ClassName
+    val content: ClassName
 
     /** State class applied to the content element when expanded. */
-    var expanded: ClassName
+    val expanded: ClassName
 
     /** State class applied to the content element when selected. */
-    var selected: ClassName
+    val selected: ClassName
 
     /** State class applied to the content element when focused. */
-    var focused: ClassName
+    val focused: ClassName
 
     /** State class applied to the element when disabled. */
-    var disabled: ClassName
+    val disabled: ClassName
 
     /** Styles applied to the tree node icon. */
-    var iconContainer: ClassName
+    val iconContainer: ClassName
 
     /** Styles applied to the label element. */
-    var label: ClassName
+    val label: ClassName
 }
+
+external val treeItemClasses: TreeItemClasses
--- kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeView.classes.kt
+++ kotlin-muix-tree-view/src/jsMain/generated/muix/tree/view/TreeView.classes.kt
@@ -1,10 +1,14 @@
 // Automatically generated - do not modify!
 
+@file:JsModule("@mui/x-tree-view/TreeView")
+
 package muix.tree.view
 
 import web.cssom.ClassName
 
-external interface TreeViewClasses {
+sealed external interface TreeViewClasses {
     /** Styles applied to the root element. */
-    var root: ClassName
+    val root: ClassName
 }
+
+external val treeViewClasses: TreeViewClasses
