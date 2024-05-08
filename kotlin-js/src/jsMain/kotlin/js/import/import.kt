27f6a5242dfc9c1148ecca9e32199c31a298e9cb
Victor Turansky <victor.turansky@gmail.com>
2024-05-09 02:53:48 +0300

    JS. Suspend `import`

--- kotlin-js/src/jsMain/kotlin/js/import/import.kt
+++ kotlin-js/src/jsMain/kotlin/js/import/import.kt
@@ -2,9 +2,14 @@ package js.import
 
 import js.promise.Promise
 
-external fun <T : Any> import(
+@JsName("import")
+external fun <T : Any> importAsync(
     path: String,
 ): Promise<T>
+
+external suspend fun <T : Any> import(
+    path: String,
+): T
 
 external object import {
     val meta: ImportMeta
