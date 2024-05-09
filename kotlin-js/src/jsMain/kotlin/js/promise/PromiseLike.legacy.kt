87abf6ddfb894193d8eb144b124aeeae7fbd0459
Victor Turansky <victor.turansky@gmail.com>
2024-05-09 15:22:44 +0300

    Promise. Deprecate `PromiseLike.await` extension

--- kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.legacy.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/PromiseLike.legacy.kt
@@ -1,4 +1,9 @@
+@file:Suppress(
+    "EXTENSION_SHADOWED_BY_MEMBER",
+)
+
 package js.promise
 
+@Deprecated("Use `await` method instead")
 suspend inline fun <T> PromiseLike<T>.await(): T =
     awaitInternal(this)
