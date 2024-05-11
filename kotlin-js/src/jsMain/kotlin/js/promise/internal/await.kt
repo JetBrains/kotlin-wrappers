ad3ffab7cb8b7a979a090d129a9566117f4d780f
Victor Turansky <victor.turansky@gmail.com>
2024-05-11 19:19:14 +0300

    Promise. Add internal `awaitOptionalPromiseLike` function

--- kotlin-js/src/jsMain/kotlin/js/promise/internal/await.kt
+++ kotlin-js/src/jsMain/kotlin/js/promise/internal/await.kt
@@ -9,3 +9,10 @@ internal suspend fun <T> awaitPromiseLike(
     p: PromiseLike<T>,
 ): T =
     p.unsafeCast<LegacyPromise<T>>().await()
+
+internal suspend fun <T> awaitOptionalPromiseLike(
+    p: PromiseLike<T>?,
+): T? =
+    if (p != null) {
+        awaitPromiseLike(p)
+    } else null
