03ac5ac2476e33e5bd4807ed4d65795c0c95d0ff
Victor Turansky <victor.turansky@gmail.com>
2024-05-21 20:25:40 +0300

    K2. Kotlin `2.0.0`

--- buildSrc/src/main/kotlin/k2-patch.gradle.kts
+++ buildSrc/src/main/kotlin/k2-patch.gradle.kts
@@ -3,7 +3,7 @@ val k2patch by tasks.registering {
         patchFile("gradle.properties") {
             it.replace(
                 "kotlin.version=1.9.24",
-                "kotlin.version=2.0.0-RC3",
+                "kotlin.version=2.0.0",
             )
         }
     }
