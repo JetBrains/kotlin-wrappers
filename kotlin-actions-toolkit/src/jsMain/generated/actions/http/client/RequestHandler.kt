14d6b4282a5f4d1ed1180ba20388754abe22d581
Victor Turansky <victor.turansky@gmail.com>
2024-05-25 15:22:30 +0300

    Actions Toolkit. Fix `RequestHandler` declaration [generated]

--- kotlin-actions-toolkit/src/jsMain/generated/actions/http/client/RequestHandler.kt
+++ kotlin-actions-toolkit/src/jsMain/generated/actions/http/client/RequestHandler.kt
@@ -4,18 +4,12 @@ package actions.http.client
 
 import js.promise.Promise
 
-sealed external interface RequestHandler {
+external interface RequestHandler {
     fun prepareRequest(options: node.http.RequestOptions)
     fun canHandleAuthentication(response: HttpClientResponse): Boolean
     fun handleAuthentication(
         httpClient: HttpClient,
         requestInfo: RequestInfo,
-        data: String?,
-    ): Promise<HttpClientResponse>
-
-    fun handleAuthentication(
-        httpClient: HttpClient,
-        requestInfo: RequestInfo,
-        data: node.ReadableStream?,
+        data: Any /* string | node.ReadableStream */?,
     ): Promise<HttpClientResponse>
 }
