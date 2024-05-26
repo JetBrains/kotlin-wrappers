8d1f1d4135d50e86e21ae24611fb37c975a20a8e
Victor Turansky <victor.turansky@gmail.com>
2024-05-26 05:02:19 +0300

    JS. Fix formatting

--- kotlin-js/src/jsMain/kotlin/js/array/JsTuple.kt
+++ kotlin-js/src/jsMain/kotlin/js/array/JsTuple.kt
@@ -15,37 +15,44 @@ sealed external interface JsTuple {
     fun asArray(): ReadonlyArray<Any?>
 }
 
-sealed external interface JsTuple1<out A> : JsTuple {
+sealed external interface JsTuple1<out A> :
+    JsTuple {
     @JsAlias("[0]")
     operator fun component1(): A
 }
 
-sealed external interface JsTuple2<out A, out B> : JsTuple1<A> {
+sealed external interface JsTuple2<out A, out B> :
+    JsTuple1<A> {
     @JsAlias("[1]")
     operator fun component2(): B
 }
 
-sealed external interface JsTuple3<out A, out B, out C> : JsTuple2<A, B> {
+sealed external interface JsTuple3<out A, out B, out C> :
+    JsTuple2<A, B> {
     @JsAlias("[2]")
     operator fun component3(): C
 }
 
-sealed external interface JsTuple4<out A, out B, out C, out D> : JsTuple3<A, B, C> {
+sealed external interface JsTuple4<out A, out B, out C, out D> :
+    JsTuple3<A, B, C> {
     @JsAlias("[3]")
     operator fun component4(): D
 }
 
-sealed external interface JsTuple5<out A, out B, out C, out D, out E> : JsTuple4<A, B, C, D> {
+sealed external interface JsTuple5<out A, out B, out C, out D, out E> :
+    JsTuple4<A, B, C, D> {
     @JsAlias("[4]")
     operator fun component5(): E
 }
 
-sealed external interface JsTuple6<out A, out B, out C, out D, out E, out F> : JsTuple5<A, B, C, D, E> {
+sealed external interface JsTuple6<out A, out B, out C, out D, out E, out F> :
+    JsTuple5<A, B, C, D, E> {
     @JsAlias("[5]")
     operator fun component6(): F
 }
 
-sealed external interface JsTuple7<out A, out B, out C, out D, out E, out F, out G> : JsTuple6<A, B, C, D, E, F> {
+sealed external interface JsTuple7<out A, out B, out C, out D, out E, out F, out G> :
+    JsTuple6<A, B, C, D, E, F> {
     @JsAlias("[6]")
     operator fun component7(): G
 }
