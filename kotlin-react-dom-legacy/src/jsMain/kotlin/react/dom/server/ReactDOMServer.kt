f76b2205ecde0a237de5b08f4edc7773108a7e72
Victor Turansky <victor.turansky@gmail.com>
2024-06-03 02:09:12 +0300

    K2. Remove redundant `dynamic` usage

--- kotlin-react-dom-legacy/src/jsMain/kotlin/react/dom/server/ReactDOMServer.kt
+++ kotlin-react-dom-legacy/src/jsMain/kotlin/react/dom/server/ReactDOMServer.kt
@@ -4,6 +4,8 @@ import react.Props
 import react.Render
 import react.createElement
 
-fun renderToString(handler: Render) = rawRenderToString(createElement<Props>(handler))
+fun renderToString(handler: Render): String =
+    rawRenderToString(createElement<Props>(handler))
 
-fun renderToStaticMarkup(handler: Render) = rawRenderToStaticMarkup(createElement<Props>(handler))
+fun renderToStaticMarkup(handler: Render): String =
+    rawRenderToStaticMarkup(createElement<Props>(handler))
