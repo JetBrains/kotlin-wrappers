e1ce7e72c5ad2ed4d8567cf54072b6b439b4caac
Victor Turansky <victor.turansky@gmail.com>
2024-06-02 15:36:01 +0300

    Examples. Remove old examples

--- examples/old/build.gradle.kts
+++ examples/old/build.gradle.kts
@@ -8,7 +8,4 @@ dependencies {
     jsMainImplementation(projects.kotlinReact)
     jsMainImplementation(projects.kotlinReactDom)
     jsMainImplementation(projects.kotlinReactRouterDom)
-
-    jsMainImplementation(npm("react-quill", "1.3.5"))
-    jsMainImplementation(npm("axios", "0.21.1"))
 }
