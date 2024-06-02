9813425fd5487457dfd20a232e787cb084004c66
Victor Turansky <victor.turansky@gmail.com>
2024-06-02 15:16:49 +0300

    Cesium. Strict types [generated]

--- kotlin-cesium-engine/src/jsMain/generated/cesium/engine/SingleTileImageryProvider.kt
+++ kotlin-cesium-engine/src/jsMain/generated/cesium/engine/SingleTileImageryProvider.kt
@@ -196,25 +196,25 @@ external class SingleTileImageryProvider(options: ConstructorOptions) {
         @JsAsync
         suspend fun fromUrl(
             url: Resource,
-            options: Any /* SingleTileImageryProvider.fromUrlOptions */? = definedExternally,
+            options: fromUrlOptions? = definedExternally,
         ): SingleTileImageryProvider
 
         @JsName("fromUrl")
         fun fromUrlAsync(
             url: Resource,
-            options: Any /* SingleTileImageryProvider.fromUrlOptions */? = definedExternally,
+            options: fromUrlOptions? = definedExternally,
         ): Promise<SingleTileImageryProvider>
 
         @JsAsync
         suspend fun fromUrl(
             url: String,
-            options: Any /* SingleTileImageryProvider.fromUrlOptions */? = definedExternally,
+            options: fromUrlOptions? = definedExternally,
         ): SingleTileImageryProvider
 
         @JsName("fromUrl")
         fun fromUrlAsync(
             url: String,
-            options: Any /* SingleTileImageryProvider.fromUrlOptions */? = definedExternally,
+            options: fromUrlOptions? = definedExternally,
         ): Promise<SingleTileImageryProvider>
 
         /**
