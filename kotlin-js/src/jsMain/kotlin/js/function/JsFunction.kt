e77124a0907433eae6be656c78fda21ace423f0c
Victor Turansky <victor.turansky@gmail.com>
2024-06-05 21:25:58 +0300

    Function. `JsContextFunction` (#2267)

--- kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsFunction.kt
@@ -1,12 +1,5 @@
 package js.function
 
-import js.array.JsTuple
+import js.core.Void
 
-internal const val FUNCTION: String = "Function"
-
-@JsName(FUNCTION)
-@JsExternalInheritorsOnly
-open external class JsFunction<in A : JsTuple, out R>(
-    vararg parameterNames: String,
-    body: String,
-)
+typealias JsFunction<A, R> = JsContextFunction<Void, A, R>
