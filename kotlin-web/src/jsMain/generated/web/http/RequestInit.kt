c2ab43df7eec0a0cb060657868df922855709d62
Victor Turansky <victor.turansky@gmail.com>
2024-06-10 02:36:34 +0300

    HTTP. `Request.window` removed

--- kotlin-web/src/jsMain/generated/web/http/RequestInit.kt
+++ kotlin-web/src/jsMain/generated/web/http/RequestInit.kt
@@ -2,7 +2,6 @@
 
 package web.http
 
-import js.core.Void
 import kotlinx.js.JsPlainObject
 import web.abort.AbortSignal
 import web.abort.Abortable
@@ -70,9 +69,4 @@ external interface RequestInit :
      * An AbortSignal to set request's signal.
      */
     override var signal: AbortSignal?
-
-    /**
-     * Can only be null. Used to disassociate request from any Window.
-     */
-    var window: Void
 }
