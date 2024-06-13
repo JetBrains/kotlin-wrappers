b7657fdc89f7cb859a010ca5ed79e3bda1e61143
porotkin <preload737773@gmail.com>
2024-06-13 09:29:40 +0300

    TanStack Query Core `5.45.0`

--- kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/QueryObserverOptions.kt
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/QueryObserverOptions.kt
@@ -5,7 +5,7 @@ package tanstack.query.core
 external interface QueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey : QueryKey, TPageParam>
     : QueryOptions<TQueryFnData, TError, TQueryData, TQueryKey, TPageParam> {
     var enabled: Boolean
-    var staleTime: JsDuration
+    var staleTime: StaleTime<TQueryFnData, TError, TQueryData, TQueryKey>
     var refetchInterval: Any /* number | false | ((query: Query<TQueryFnData, TError, TQueryData, TQueryKey>) => number | false | undefined) */
     var refetchIntervalInBackground: Boolean
     var refetchOnWindowFocus: (query: Query<TQueryFnData, TError, TQueryData, TQueryKey>) -> Boolean /* | boolean | 'always' */
---
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/StaleTime.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "UNUSED_TYPEALIAS_PARAMETER",
+)
+
+package tanstack.query.core
+
+typealias StaleTime<TQueryFnData, TError, TData, TQueryKey> = Union /* number | ((query: Query<TQueryFnData, TError, TData, TQueryKey>) => number) */
