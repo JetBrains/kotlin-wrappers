6c7fff78e769c6d8449db32593f7f74a821a6f76
Alexander Popoff <alexander.popoff@flysto.net>
2024-06-22 15:53:58 +0300

    React Select. Fix `onInputChange` type

--- kotlin-react-select/src/jsMain/kotlin/react/select/InputChangeHandler.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/InputChangeHandler.kt
@@ -1,6 +1,7 @@
 package react.select
 
+// Details: https://github.com/JedWatson/react-select/issues/5677
 typealias InputChangeHandler = (
     newValue: String,
     actionMeta: InputActionMeta,
-) -> Unit
+) -> String?
