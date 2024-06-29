f3c82eb06aef811105aa1b45ab4fa4029ac74c90
Victor Turansky <victor.turansky@gmail.com>
2024-06-29 19:15:21 +0300

    TanStack Query `5.49.0`

--- kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/DehydrateOptions.kt
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/DehydrateOptions.kt
@@ -3,6 +3,7 @@
 package tanstack.query.core
 
 external interface DehydrateOptions {
+    var serializeData: TransformerFn
     var shouldDehydrateMutation: (mutation: Mutation<*, *, *, *>) -> Boolean
     var shouldDehydrateQuery: (query: Query<*, *, *, *>) -> Boolean
 }
