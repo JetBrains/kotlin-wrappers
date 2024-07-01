9f9170d7f42d5bec0b4da97911eff8de6855b0cc
Victor Turansky <victor.turansky@gmail.com>
2024-07-01 22:54:50 +0300

    Events. Add `EventInstance` example

--- examples/old/src/jsMain/kotlin/example/EventHandler.kt
+++ examples/old/src/jsMain/kotlin/example/EventHandler.kt
@@ -2,11 +2,23 @@ package example
 
 import web.dom.Document
 import web.dom.document
+import web.dom.dragOverEvent
 import web.events.addEventHandler
+import web.events.addHandler
 import web.uievents.DragEvent
 
-private fun preventDefaultExample() {
+private fun preventDefaultExample_handler() {
     document.addEventHandler(DragEvent.dragOver()) { event ->
+        val doc: Document = event.currentTarget
+
+        event.preventDefault()
+
+        console.log(doc === document)
+    }
+}
+
+private fun preventDefaultExample_instance() {
+    document.dragOverEvent.addHandler { event ->
         val doc: Document = event.currentTarget
 
         event.preventDefault()
