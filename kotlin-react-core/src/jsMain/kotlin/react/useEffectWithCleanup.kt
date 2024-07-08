fc6febc5d820670e9fa7bf31fb7d1846d8954dac
Victor Turansky <victor.turansky@gmail.com>
2024-07-09 00:30:43 +0300

    React. Add `useEffectWithCleanup`

---
+++ kotlin-react-core/src/jsMain/kotlin/react/useEffectWithCleanup.kt
@@ -0,0 +1,37 @@
+package react
+
+import react.internal.createCleanupCallback
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useEffectWithCleanup(
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseEffect(callback)
+}
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useEffectWithCleanup(
+    vararg dependencies: Any?,
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseEffect(callback, dependencies)
+}
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useEffectOnceWithCleanup(
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseEffect(callback, emptyArray())
+}
