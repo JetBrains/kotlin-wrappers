1ff719b542788c7e522125a844ea21424a9e460c
Victor Turansky <victor.turansky@gmail.com>
2024-07-09 00:37:12 +0300

    React. Add `useLayoutEffectWithCleanup`

---
+++ kotlin-react-core/src/jsMain/kotlin/react/useLayoutEffectWithCleanup.kt
@@ -0,0 +1,37 @@
+package react
+
+import react.internal.createCleanupCallback
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useLayoutEffectWithCleanup(
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseLayoutEffect(callback)
+}
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useLayoutEffectWithCleanup(
+    vararg dependencies: Any?,
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseLayoutEffect(callback, dependencies)
+}
+
+/**
+ * Only works inside [fc]
+ * @see <a href="https://reactjs.org/docs/hooks-state.html#hooks-and-function-components">Hooks and Function Components</a>
+ */
+fun useLayoutEffectOnceWithCleanup(
+    effect: CleanupBuilder.() -> Unit,
+) {
+    val callback = createCleanupCallback(effect)
+    rawUseLayoutEffect(callback, emptyArray())
+}
