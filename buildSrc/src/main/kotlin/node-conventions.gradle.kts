5e33179ae20cf8aa3b996162578cf9d0496fd207
Victor Turansky <victor.turansky@gmail.com>
2024-07-09 21:27:45 +0300

    Node runtime `20.15.1`

--- buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/node-conventions.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
 
 plugins.withType<NodeJsRootPlugin> {
     the<NodeJsRootExtension>().apply {
-        version = "20.15.0"
+        version = "20.15.1"
     }
 
     the<NpmExtension>().apply {
