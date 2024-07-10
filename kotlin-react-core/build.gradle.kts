bad709dd80fffd2175445c14f3d7456a2ac0bdf4
Victor Turansky <victor.turansky@gmail.com>
2024-07-10 10:58:38 +0300

    React. Add `coroutines` dependency

--- kotlin-react-core/build.gradle.kts
+++ kotlin-react-core/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
     jsMainApi(projects.kotlinJs)
     jsMainApi(projects.kotlinCsstype)
 
+    jsMainImplementation(kotlinxCoroutines("core"))
+
     jsTestImplementation(kotlin("test-js"))
 
     jsMainApi(npmv("react"))
