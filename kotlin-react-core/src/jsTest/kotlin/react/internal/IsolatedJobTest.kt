e8bc248a1551c9db9004688dd870bb4faa7a79a3
Victor Turansky <victor.turansky@gmail.com>
2024-07-20 00:47:07 +0300

    React. Additional test for `isolatedJob`

--- kotlin-react-core/src/jsTest/kotlin/react/internal/IsolatedJobTest.kt
+++ kotlin-react-core/src/jsTest/kotlin/react/internal/IsolatedJobTest.kt
@@ -1,6 +1,10 @@
 package react.internal
 
 import kotlinx.coroutines.test.runTest
+import web.events.subscribe
+import web.window.clickEvent
+import web.window.mouseUpEvent
+import web.window.window
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -10,6 +14,20 @@ class IsolatedJobTest {
         var a = 13
 
         isolatedJob {
+            a = 42
+        }
+
+        assertEquals(42, a)
+    }
+
+    @Test
+    fun launchWithSubscribe() = runTest {
+        var a = 13
+
+        isolatedJob {
+            window.clickEvent.subscribe { /* do nothing */ }
+            window.mouseUpEvent.subscribe { /* do nothing */ }
+
             a = 42
         }
 
