1dfa295cd695c23f597af733e1f598787048ab44
Victor Turansky <victor.turansky@gmail.com>
2024-07-23 23:38:52 +0300

    React DOM. `FormAction` - union

--- kotlin-react-dom/src/jsMain/generated/react/dom/FormAction.kt
+++ kotlin-react-dom/src/jsMain/generated/react/dom/FormAction.kt
@@ -1,7 +1,21 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package react.dom
 
 import web.form.FormData
 
-typealias FormAction = (data: FormData) -> Unit
+sealed external interface FormAction
+
+inline fun FormAction(
+    value: String,
+): FormAction =
+    value.unsafeCast<FormAction>()
+
+inline fun FormAction(
+    noinline value: (data: FormData) -> Unit,
+): FormAction =
+    value.unsafeCast<FormAction>()
