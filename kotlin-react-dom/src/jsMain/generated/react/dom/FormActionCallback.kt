fb4cc6361c95a41e3f1e86c3ad6b048844038f6d
Victor Turansky <victor.turansky@gmail.com>
2024-07-24 00:26:58 +0300

    React DOM. Suspend `FormActionCallback.invoke`

--- kotlin-react-dom/src/jsMain/generated/react/dom/FormActionCallback.kt
+++ kotlin-react-dom/src/jsMain/generated/react/dom/FormActionCallback.kt
@@ -2,5 +2,18 @@
 
 package react.dom
 
+import js.promise.Promise
+import web.form.FormData
+
 sealed external interface FormActionCallback
     : FormAction
+
+// Support `PromiseLike`?
+suspend operator fun FormActionCallback.invoke(
+    data: FormData,
+) {
+    val result = asDynamic()(data)
+    if (result is Promise<*>) {
+        result.await()
+    }
+}
