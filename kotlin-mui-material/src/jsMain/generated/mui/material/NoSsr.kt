73b560244bac9c1eef9aaace587b6ccadff8a7a0
Alexander Popoff <alexander.popoff@flysto.net>
2024-07-27 16:09:09 +0300

    MUI. Material `5.16.5` [generated]

---
+++ kotlin-mui-material/src/jsMain/generated/mui/material/NoSsr.kt
@@ -0,0 +1,26 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/material/NoSsr")
+
+package mui.material
+
+/**
+ * NoSsr purposely removes components from the subject of Server Side Rendering (SSR).
+ *
+ * This component can be useful in a variety of situations:
+ *
+ * *   Escape hatch for broken dependencies not supporting SSR.
+ * *   Improve the time-to-first paint on the client by only rendering above the fold.
+ * *   Reduce the rendering time on the server.
+ * *   Under too heavy server load, you can turn on service degradation.
+ *
+ * Demos:
+ *
+ * - [No SSR](https://mui.com/material-ui/react-no-ssr/)
+ *
+ * API:
+ *
+ * - [NoSsr API](https://mui.com/material-ui/api/no-ssr/)
+ */
+@JsName("default")
+external val NoSsr: react.FC<NoSsrProps>
