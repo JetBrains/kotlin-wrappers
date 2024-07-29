996e80ef2982072db5d7e3ff4649050db1b97405
Victor Turansky <victor.turansky@gmail.com>
2024-07-29 15:36:13 +0300

    Actions. GitHub [generated] (fix #2260)

---
+++ kotlin-actions-toolkit/src/jsMain/generated/actions/github/getProxyFetch.kt
@@ -0,0 +1,7 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@actions/github")
+
+package actions.github
+
+external fun getProxyFetch(destinationUrl: Any?): Any /* typeof fetch */
