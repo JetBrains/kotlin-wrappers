988212db289d8c384c52ed27883abe0f3e6f98e6
porotkin <preload737773@gmail.com>
2024-08-02 09:22:42 +0300

    Cesium `1.120.0`

--- kotlin-cesium-engine/src/jsMain/generated/cesium/engine/PointGraphics.kt
+++ kotlin-cesium-engine/src/jsMain/generated/cesium/engine/PointGraphics.kt
@@ -82,6 +82,12 @@ external class PointGraphics {
     var disableDepthTestDistance: Property?
 
     /**
+     * Gets or sets the Property specifying the [SplitDirection] of this point.
+     * @see <a href="https://cesium.com/docs/cesiumjs-ref-doc/PointGraphics.html#splitDirection">Online Documentation</a>
+     */
+    var splitDirection: Property?
+
+    /**
      * Duplicates this instance.
      * @param [result] The object onto which to store the result.
      * @return The modified result parameter or a new instance if one was not provided.
