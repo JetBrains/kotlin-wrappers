f7f331331aa3c3a1c5c9ce8091e6a40e081b8eca
porotkin <preload737773@gmail.com>
2024-08-03 23:00:36 +0300

    React. Use `web.form.FormEncType` [generated]

--- kotlin-react-dom/src/jsMain/generated/react/dom/html/FormHTMLAttributes.kt
+++ kotlin-react-dom/src/jsMain/generated/react/dom/html/FormHTMLAttributes.kt
@@ -4,6 +4,7 @@ package react.dom.html
 
 import react.dom.FormAction
 import web.dom.Element
+import web.form.FormEncType
 import web.form.FormMethod
 import web.window.WindowTarget
 
@@ -11,7 +12,7 @@ external interface FormHTMLAttributes<T : Element> : HTMLAttributes<T> {
     var acceptCharset: String?
     var action: FormAction?
     var autoComplete: AutoComplete?
-    var encType: web.form.FormEncType?
+    var encType: FormEncType?
     var method: FormMethod?
     var name: String?
     var noValidate: Boolean?
