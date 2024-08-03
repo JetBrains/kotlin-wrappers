ff191ad7e2725a0995ef6fee5fedcef4597fc2bd
Victor Turansky <victor.turansky@gmail.com>
2024-08-03 19:00:05 +0300

    Coroutines. Inline original `CoroutineScope.promise` implementation

--- kotlin-js/src/jsMain/kotlin/js/coroutines/CoroutineScope.ext.kt
+++ kotlin-js/src/jsMain/kotlin/js/coroutines/CoroutineScope.ext.kt
@@ -3,17 +3,13 @@ package js.coroutines
 import js.promise.Promise
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
-import kotlinx.coroutines.promise as legacyPromise
 
 fun <T> CoroutineScope.promise(
     context: CoroutineContext = EmptyCoroutineContext,
     start: CoroutineStart = CoroutineStart.DEFAULT,
     block: suspend CoroutineScope.() -> T,
 ): Promise<T> =
-    legacyPromise(
-        context = context,
-        start = start,
-        block = block,
-    ).unsafeCast<Promise<T>>()
+    async(context, start, block).asPromise()
