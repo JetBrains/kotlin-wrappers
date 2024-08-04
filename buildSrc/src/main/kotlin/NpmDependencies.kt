bfad0971c60730846f8ac232ef7bf51669dd37a4
Sergei Grishchenko <s.i.grishchenko@gmail.com>
2024-08-04 23:12:50 +0300

    Use explicit NPM versions (#2350)
    * Split two helpers:
    * npm dependency name -> version
    * kotlin wrapper subproject name -> publication version
    
    * Remove list of strict npm dependencies and use explicit npm versions in `gradle.properties`
    
    * Use valid versions
    
    ---------
    
    Co-authored-by: Victor Turansky <victor.turansky@gmail.com>

--- buildSrc/src/main/kotlin/NpmDependencies.kt
+++ buildSrc/src/main/kotlin/NpmDependencies.kt
@@ -4,21 +4,22 @@ import org.jetbrains.kotlin.gradle.targets.js.npm.DevNpmDependencyExtension
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependencyExtension
 
-private val STRICT_VERSION_REQUIRED = setOf(
-    "@mui/material",
-    "@mui/icons-material",
-    "@mui/lab",
-    "@mui/x-date-pickers",
-    "@mui/x-tree-view",
+private val NAME_ALIASES = mapOf(
+    "@mui/x-date-pickers" to "muix-date-pickers",
+    "@mui/x-tree-view" to "muix-tree-view",
+
+    "@popperjs/core" to "popper",
+
+    "@types/node" to "node",
 )
 
 private fun Project.npmVersion(name: String): String {
-    val version = version(name)
-    if (name in STRICT_VERSION_REQUIRED) {
-        return version
-    }
+    val finalName = NAME_ALIASES[name]
+        ?: name
+            .removePrefix("@")
+            .replace("/", "-")
 
-    return "^$version"
+    return version(finalName)
 }
 
 fun Project.npmv(name: String): NpmDependency {
--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -2,32 +2,27 @@ import org.gradle.api.Project
 
 private val TARGET_ALIASES = mapOf(
     "react-core" to "react",
-    "react-dom" to "react",
-    "react-dom-legacy" to "react",
-    "react-dom-test-utils" to "react",
+    "react-dom-legacy" to "react-dom",
+    "react-dom-test-utils" to "react-dom",
     "react-legacy" to "react",
 
     "emotion" to "emotion-react",
-
-    "@mui/x-date-pickers" to "muix-date-pickers",
-    "@mui/x-tree-view" to "muix-tree-view",
-
-    "@popperjs/core" to "popper",
-
-    "@types/node" to "node",
 )
 
 fun Project.version(target: String): String {
-    val finalTarget = TARGET_ALIASES[target]
-        ?: target.removePrefix("@")
-            .replace("/", "-")
-
-    return prop("$finalTarget.version")
+    return prop("$target.version")
 }
 
 internal fun Project.publishVersion(): String {
+    val target = name.removePrefix("kotlin-")
+    val finalTarget = TARGET_ALIASES[target] ?: target
+
     val build = prop("version.build")
         .let { if (it.isNotEmpty()) "-$it" else "" }
 
-    return version(name.removePrefix("kotlin-")) + build
+    val version = version(finalTarget)
+        .removePrefix("^")
+        .removePrefix("~")
+
+    return version + build
 }
