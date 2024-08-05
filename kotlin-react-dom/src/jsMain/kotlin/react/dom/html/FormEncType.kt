8e77636d12d716e2a7697f453d6183312bd830ce
porotkin <preload737773@gmail.com>
2024-08-05 10:06:14 +0300

    React DOM. Remove unused `FormEncType`

--- kotlin-react-dom/src/jsMain/kotlin/react/dom/html/FormEncType.kt
+++
@@ -1,7 +0,0 @@
-package react.dom.html
-
-object FormEncType {
-    const val applicationFormUrlEncoded = "application/x-www-form-urlencoded"
-    const val multipartFormData = "multipart/form-data"
-    const val textPlain = "text/plain"
-}
