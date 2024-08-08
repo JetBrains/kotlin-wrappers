f74f1807a1691d6623ce608b2de78317e527608f
Michael Porotkin <34237389+porotkin@users.noreply.github.com>
2024-08-08 19:24:33 +0300

    React DOM. Use `AutoFill` [generated] (#2367)

--- kotlin-react-dom/src/jsMain/generated/react/dom/html/SelectHTMLAttributes.kt
+++ kotlin-react-dom/src/jsMain/generated/react/dom/html/SelectHTMLAttributes.kt
@@ -3,10 +3,11 @@
 package react.dom.html
 
 import react.dom.events.ChangeEventHandler
+import web.autofill.AutoFill
 import web.dom.Element
 
 external interface SelectHTMLAttributes<T : Element> : HTMLAttributes<T> {
-    var autoComplete: AutoComplete?
+    var autoComplete: AutoFill?
     var disabled: Boolean?
     var form: String?
     var multiple: Boolean?
