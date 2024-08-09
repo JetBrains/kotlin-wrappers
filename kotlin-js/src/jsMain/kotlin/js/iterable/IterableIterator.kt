5bba2419c90a89f998cd5b3eb2088e018d2772bc
Victor Turansky <victor.turansky@gmail.com>
2024-08-10 01:49:46 +0300

    Iterable. `JsIterator` extends `JsIterable`

--- kotlin-js/src/jsMain/kotlin/js/iterable/IterableIterator.kt
+++ kotlin-js/src/jsMain/kotlin/js/iterable/IterableIterator.kt
@@ -1,25 +1,8 @@
 @file:Suppress(
     "INTERFACE_WITH_SUPERCLASS",
-    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
-    "INLINE_EXTERNAL_DECLARATION",
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-    "DECLARATION_CANT_BE_INLINED",
-    "NOTHING_TO_INLINE",
 )
 
 package js.iterable
 
-import js.symbol.Symbol
-import seskar.js.JsNative
-
 sealed external interface IterableIterator<out T> :
-    JsIterable<T>,
-    JsIterator<T> {
-    @JsNative
-    override operator fun get(
-        key: Symbol.iterator,
-    ): () -> IterableIterator<T> = definedExternally
-
-    override inline fun iterator(): Iterator<T> =
-        iteratorFromJsIteratorLike(this)
-}
+    JsIterator<T>
