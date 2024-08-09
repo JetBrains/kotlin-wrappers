2a211b3482688118248bf2dde5172f33a7e0e9e2
Victor Turansky <victor.turansky@gmail.com>
2024-08-10 02:15:01 +0300

    Iterable. Remove `*IteratorLike.iterator` operators

--- kotlin-js/src/jsMain/kotlin/js/iterable/JsIteratorLike.kt
+++ kotlin-js/src/jsMain/kotlin/js/iterable/JsIteratorLike.kt
@@ -1,16 +1,5 @@
-@file:Suppress(
-    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
-    "INLINE_EXTERNAL_DECLARATION",
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-    "DECLARATION_CANT_BE_INLINED",
-    "NOTHING_TO_INLINE",
-)
-
 package js.iterable
 
 external interface JsIteratorLike<out T> {
     fun next(): IteratorResult<T, *>
-
-    inline operator fun iterator(): Iterator<T> =
-        iteratorFromJsIteratorLike(this)
 }
