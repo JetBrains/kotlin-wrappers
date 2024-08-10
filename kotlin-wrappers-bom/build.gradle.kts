f37c375857f5658e5ebd631d8db4160f1e1727fc
Victor Turansky <victor.turansky@gmail.com>
2024-08-10 15:09:33 +0300

    BOM. Calculate constraints

--- kotlin-wrappers-bom/build.gradle.kts
+++ kotlin-wrappers-bom/build.gradle.kts
@@ -5,50 +5,20 @@ plugins {
 
 dependencies {
     constraints {
-        api(projects.kotlinActionsToolkit)
-        api(projects.kotlinBrowser)
-        api(projects.kotlinCesiumEngine)
-        api(projects.kotlinCesiumWidgets)
-        api(projects.kotlinCss)
-        api(projects.kotlinCssomCore)
-        api(projects.kotlinCsstype)
-        api(projects.kotlinElectron)
-        api(projects.kotlinEmotion)
-        api(projects.kotlinExtensions)
-        api(projects.kotlinJs)
-        api(projects.kotlinMuiBase)
-        api(projects.kotlinMuiIconsMaterial)
-        api(projects.kotlinMuiLab)
-        api(projects.kotlinMuiMaterial)
-        api(projects.kotlinMuiSystem)
-        api(projects.kotlinMuixDatePickers)
-        api(projects.kotlinMuixTreeView)
-        api(projects.kotlinNode)
-        api(projects.kotlinPopper)
-        api(projects.kotlinPreactSignalsCore)
-        api(projects.kotlinPreactSignalsReact)
-        api(projects.kotlinReact)
-        api(projects.kotlinReactBeautifulDnd)
-        api(projects.kotlinReactCore)
-        api(projects.kotlinReactDom)
-        api(projects.kotlinReactDomLegacy)
-        api(projects.kotlinReactDomTestUtils)
-        api(projects.kotlinReactLegacy)
-        api(projects.kotlinReactPopper)
-        api(projects.kotlinReactRouter)
-        api(projects.kotlinReactRouterDom)
-        api(projects.kotlinReactSelect)
-        api(projects.kotlinReactUse)
-        api(projects.kotlinRemixRunRouter)
-        api(projects.kotlinStyledNext)
-        api(projects.kotlinTanstackQueryCore)
-        api(projects.kotlinTanstackReactQuery)
-        api(projects.kotlinTanstackReactQueryDevtools)
-        api(projects.kotlinTanstackReactTable)
-        api(projects.kotlinTanstackReactVirtual)
-        api(projects.kotlinTanstackTableCore)
-        api(projects.kotlinTanstackVirtualCore)
-        api(projects.kotlinTypescript)
-        api(projects.kotlinWeb)
+        for (library in getLibraryProjects()) {
+            api(project(library.path))
+        }
     }
 }
+
+fun isLibraryProject(
+    project: Project,
+): Boolean =
+    sequenceOf(
+        KotlinLibraryConventionsPlugin::class,
+        KotlinLegacyLibraryConventionsPlugin::class
+    ).any(project.plugins::hasPlugin)
+
+fun getLibraryProjects(): Sequence<Project> =
+    rootProject.subprojects.asSequence()
+        .filter(::isLibraryProject)
