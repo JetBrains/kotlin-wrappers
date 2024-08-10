ee17e8aeace341044251cb1a50f7c7eb02cbe209
Victor Turansky <victor.turansky@gmail.com>
2024-08-10 16:56:46 +0300

    Version Catalog. Add catalog

--- kotlin-wrappers-catalog/build.gradle.kts
+++ kotlin-wrappers-catalog/build.gradle.kts
@@ -2,3 +2,15 @@ plugins {
     `version-catalog`
     `publish-conventions`
 }
+
+catalog {
+    versionCatalog {
+        for (library in getLibraryProjects(rootProject)) {
+            library(
+                "wrappers-" + library.name.removePrefix("kotlin-"),
+                library.group.toString(),
+                library.name,
+            ).version(library.publishVersion())
+        }
+    }
+}
