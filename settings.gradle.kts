bc05aab9bd54139f8fa4d16dc5d4f06dbd441d6b
Victor Turansky <victor.turansky@gmail.com>
2024-08-11 19:09:26 +0300

    Settings. Fix formatting

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,9 +27,8 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             val kotlinVersion = extra["kotlin.version"] as String
-            library("kotlin-test-annotations-common", "org.jetbrains.kotlin", "kotlin-test-annotations-common").version(
-                kotlinVersion
-            )
+            library("kotlin-test-annotations-common", "org.jetbrains.kotlin", "kotlin-test-annotations-common")
+                .version(kotlinVersion)
             library("kotlin-test-common", "org.jetbrains.kotlin", "kotlin-test-common").version(kotlinVersion)
             library("kotlin-test-junit", "org.jetbrains.kotlin", "kotlin-test-junit").version(kotlinVersion)
             library("kotlin-test-js", "org.jetbrains.kotlin", "kotlin-test-js").version(kotlinVersion)
