1441e73fffb36eec40a609aef5d30148997a1db2
Bernhard Posselt <bernhard@posselt.at>
2024-08-16 17:50:41 +0200

    allow passing parameters to newInstance

--- kotlin-js/src/jsMain/kotlin/js/reflect/JsClass.kt
+++ kotlin-js/src/jsMain/kotlin/js/reflect/JsClass.kt
@@ -1,7 +1,9 @@
 package js.reflect
 
-fun <T : Any> JsClass<T>.newInstance(): T =
+import js.array.ReadonlyArray
+
+fun <T : Any> JsClass<T>.newInstance(argumentList: ReadonlyArray<Any?> = emptyArray()): T =
     Reflect.construct(
         target = this,
-        argumentsList = emptyArray(),
+        argumentsList = argumentList,
     )
