49a069a88755df180fc3481c141ece56e5267d14
Bernhard Posselt <bernhard@posselt.at>
2024-08-17 12:53:54 +0200

    remove test code

--- kotlin-js/src/jsMain/kotlin/js/function/JsConstructorFunction.adapter.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsConstructorFunction.adapter.kt
@@ -5,9 +5,3 @@ import js.array.*
 fun <C: Any> JsConstructorFunction(
     clazz: JsClass<C>,
 ): JsConstructorFunction<JsTuple, C> = clazz.unsafeCast<JsConstructorFunction<JsTuple, C>>()
-
-class X
-
-fun main() {
-    JsConstructorFunction(X::class.js)
-}
