afa77710b14d22770dbcc280d076c8ac66f69c7d
Victor Turansky <victor.turansky@gmail.com>
2024-08-17 16:49:48 +0300

    Function. Fix formatting (#2400)

--- kotlin-js/src/jsMain/kotlin/js/function/JsConstructorFunction.adapter.kt
+++ kotlin-js/src/jsMain/kotlin/js/function/JsConstructorFunction.adapter.kt
@@ -4,4 +4,5 @@ import js.array.*
 
 fun <A : JsTuple, R : Any> JsConstructorFunction(
     clazz: JsClass<R>,
-): JsConstructorFunction<A, R> = clazz.unsafeCast<JsConstructorFunction<A, R>>()
+): JsConstructorFunction<A, R> =
+    clazz.unsafeCast<JsConstructorFunction<A, R>>()
