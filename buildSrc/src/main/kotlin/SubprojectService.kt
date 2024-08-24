0c87a4853709f5ab5c9d5884a379f9011f0522d1
Victor Turansky <victor.turansky@gmail.com>
2024-08-25 01:36:50 +0300

    Build. Add `Project.addSubprojectDependencies` extension (#2379)

--- buildSrc/src/main/kotlin/SubprojectService.kt
+++ buildSrc/src/main/kotlin/SubprojectService.kt
@@ -1,7 +1,31 @@
+import org.gradle.api.NamedDomainObjectProvider
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
 import org.gradle.api.provider.SetProperty
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
+import org.gradle.kotlin.dsl.get
 
 abstract class SubprojectService : BuildService<BuildServiceParameters.None> {
     abstract val data: SetProperty<SubprojectData>
+}
+
+fun Project.addSubprojectDependencies(
+    configuration: NamedDomainObjectProvider<Configuration>,
+    type: SubprojectType,
+) {
+    configuration.configure {
+        val subprojectService = extensions["subprojectService"] as SubprojectService
+
+        dependencyConstraints.addAllLater(
+            subprojectService.data.map { subprojects ->
+                subprojects.asSequence()
+                    .filter { it.type == type }
+                    .map { it.path }
+                    .sorted()
+                    .map { path -> project.dependencies.constraints.create(project(path)) }
+                    .toList()
+            }
+        )
+    }
 }
--- kotlin-wrappers-bom/build.gradle.kts
+++ kotlin-wrappers-bom/build.gradle.kts
@@ -4,15 +4,7 @@ plugins {
     id("dev.adamko.dokkatoo-html")
 }
 
-configurations.api.configure {
-    dependencyConstraints.addAllLater(
-        subprojectService.data.map { subprojects ->
-            subprojects.asSequence()
-                .filter { it.type == SubprojectType.LIBRARY }
-                .map { it.path }
-                .sorted()
-                .map { path ->project.dependencies.constraints.create(project(path)) }
-                .toList()
-        }
-    )
-}
+addSubprojectDependencies(
+    configuration = configurations.api,
+    type = SubprojectType.LIBRARY,
+)
