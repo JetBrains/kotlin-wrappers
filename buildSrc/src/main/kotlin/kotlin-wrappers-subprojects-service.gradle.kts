dfda3e506eba8058fda0105c86bca6ff8ce7f928
Adam <897017+aSemy@users.noreply.github.com>
2024-08-24 22:30:23 +0200

    Create Gradle BuildService for easier aggregation of kotlin-wrappers subprojects (#2379)
    The recent change to automatically aggregate subprojects is nice, but in upcoming Gradle versions they are looking at disabling cross-project configuration, as it harms build configuration performance (https://docs.gradle.org/current/userguide/isolated_projects.html).
    
    Using a BuildService will help with Gradle build performance, and is more future-proof.

---
+++ buildSrc/src/main/kotlin/kotlin-wrappers-subprojects-service.gradle.kts
@@ -0,0 +1,26 @@
+/**
+ * Create a service for collecting the coordinates of all kotlin-wrappers
+ * artifacts that should be included in `kotlin-wrappers-bom`.
+ */
+abstract class SubprojectsManagerService : BuildService<BuildServiceParameters.None> {
+    /** [Project.path]s of subprojects to include in `kotlin-wrappers-bom`. */
+    abstract val bomDependencies: SetProperty<String>
+
+    /** [Project.path]s of subprojects to include in the aggregated Dokka docs. */
+    abstract val dokkaDependencies: SetProperty<String>
+}
+
+val kotlinWrapperSubprojects: SubprojectsManagerService =
+    gradle.sharedServices.registerIfAbsent("SubprojectsManagerService", SubprojectsManagerService::class).get()
+
+extensions.add("kotlinWrapperSubprojects", kotlinWrapperSubprojects)
+
+/** Controls whether the current subproject will be included in `kotlin-wrappers-bom`. */
+val includeInKotlinWrappersBom: Property<Boolean> =
+    objects.property<Boolean>().convention(project.path != ":kotlin-wrappers-bom")
+extensions.add<Property<Boolean>>("includeInKotlinWrappersBom", includeInKotlinWrappersBom)
+
+kotlinWrapperSubprojects.bomDependencies
+    .addAll(provider { project.path }.zip(includeInKotlinWrappersBom) { path, enabled ->
+        if (enabled) listOf(path) else emptyList()
+    })
