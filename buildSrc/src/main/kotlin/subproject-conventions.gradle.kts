82863499c94caf0522819c10897b1a9ab93783ff
Victor Turansky <victor.turansky@gmail.com>
2024-08-25 01:43:25 +0300

    Build. Move service creation in class file (#2379)

--- buildSrc/src/main/kotlin/subproject-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/subproject-conventions.gradle.kts
@@ -1,22 +1,19 @@
-val subprojectService: SubprojectService =
-    gradle.sharedServices.registerIfAbsent("SubprojectService", SubprojectService::class).get()
-
-extensions.add("subprojectService", subprojectService)
-
 val subprojectType: Property<SubprojectType> =
     objects.property<SubprojectType>()
         .convention(provider { getSubprojectType() })
 
+val subprojectService = getOrCreateSubprojectService()
 subprojectService.data.add(
     provider { project.path }
         .zip(subprojectType, ::SubprojectData)
 )
 
-fun getSubprojectType() = when {
-    pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform") -> SubprojectType.LIBRARY
-    pluginManager.hasPlugin("java-platform") -> SubprojectType.BOM
-    pluginManager.hasPlugin("version-catalog") -> SubprojectType.VERSION_CATALOG
-    project.path == ":docs" -> SubprojectType.DOCS
+fun getSubprojectType(): SubprojectType =
+    when {
+        pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform") -> SubprojectType.LIBRARY
+        pluginManager.hasPlugin("java-platform") -> SubprojectType.BOM
+        pluginManager.hasPlugin("version-catalog") -> SubprojectType.VERSION_CATALOG
+        project.path == ":docs" -> SubprojectType.DOCS
 
-    else -> throw IllegalStateException("Unknown subproject type")
-}
+        else -> throw IllegalStateException("Unknown subproject type")
+    }
