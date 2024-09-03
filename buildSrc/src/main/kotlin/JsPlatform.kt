d5a6a93ea4df52bf3d9378e89a7d480199113fae
Victor Turansky <victor.turansky@gmail.com>
2024-09-04 00:26:40 +0300

    Revert "Build. No tests -> no JS targets"
    This reverts commit db0bfee9a6ce9c95ebc876cdbb490e0a1e0601b8.

--- buildSrc/src/main/kotlin/JsPlatform.kt
+++ buildSrc/src/main/kotlin/JsPlatform.kt
@@ -10,22 +10,8 @@ internal enum class JsPlatform {
     ;
 }
 
-private val TEST_DIRECTORIES = listOf(
-    "src/commonTest",
-    "src/jsTest",
-)
-
-private fun Project.hasTests(): Boolean =
-    TEST_DIRECTORIES.any {
-        projectDir.resolve(it).exists()
-    }
-
 internal val Project.jsPlatform: JsPlatform
     get() {
-        if (!hasTests()) {
-            JsPlatform.MISSED
-        }
-
         val platform = findProperty("js.platform") as String?
 
         return if (platform != null) {
