fcd2e63c5fab413a2c10fc7c5fbb7fc5c17d4721
Victor Turansky <victor.turansky@gmail.com>
2024-09-03 19:25:52 +0300

    React. Add `ComponentModule` factory

--- kotlin-react-core/src/jsMain/kotlin/react/ComponentModule.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/ComponentModule.kt
@@ -1,5 +1,14 @@
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
 package react
 
 import js.import.Module
 
 typealias ComponentModule<P> = Module<ComponentType<P>>
+
+inline fun <P : Props> ComponentModule(
+    default: ComponentType<P>,
+): ComponentModule<P> =
+    Module(default)
