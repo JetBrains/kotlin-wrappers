1e798131c23bd8ba5c78ca24b39d6ff2ee493eeb
Victor Turansky <victor.turansky@gmail.com>
2024-09-04 03:54:31 +0300

    Version Catalog. Remove redundant extension

--- buildSrc/src/main/kotlin/NpmDependencies.kt
+++ buildSrc/src/main/kotlin/NpmDependencies.kt
@@ -1,7 +1,5 @@
-import org.gradle.api.Project
 import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.provider.Provider
-import org.gradle.api.provider.ProviderConvertible
 import org.jetbrains.kotlin.gradle.targets.js.npm.DevNpmDependencyExtension
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependency
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmDependencyExtension
@@ -15,11 +13,6 @@ operator fun NpmDependencyExtension.invoke(
         version = dependency.version!!,
     )
 }
-
-operator fun NpmDependencyExtension.invoke(
-    dependencyNotation: ProviderConvertible<MinimalExternalModuleDependency>,
-): NpmDependency =
-    this(dependencyNotation.asProvider())
 
 operator fun DevNpmDependencyExtension.invoke(
     dependencyNotation: Provider<MinimalExternalModuleDependency>,
