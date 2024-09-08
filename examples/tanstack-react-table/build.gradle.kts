10166259401606a9913e3fdd6c2eef068725abdb
Victor Turansky <victor.turansky@gmail.com>
2024-09-08 19:33:17 +0300

    React Table Example. Add dependency on additional hooks

--- examples/tanstack-react-table/build.gradle.kts
+++ examples/tanstack-react-table/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     jsMainImplementation(libs.wrappers.react)
     jsMainImplementation(libs.wrappers.reactDom)
+    jsMainImplementation(libs.wrappers.reactUse)
     jsMainImplementation(libs.wrappers.emotion)
     jsMainImplementation(libs.wrappers.tanstack.reactQuery)
     jsMainImplementation(libs.wrappers.tanstack.reactTable)
