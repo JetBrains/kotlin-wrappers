0cd001a9a8ba3ef2c29f9bdca37e9edf09fbb209
Victor Turansky <victor.turansky@gmail.com>
2024-09-08 19:43:16 +0300

    React Table Example. Move selection module on app level

--- examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/app/AppModule.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/app/AppModule.kt
@@ -5,6 +5,7 @@ import react.PropsWithChildren
 import react.use.useConstant
 import tanstack.query.core.QueryClient
 import tanstack.react.query.QueryClientProvider
+import wrappers.example.selection.SelectionModule
 
 internal val AppModule = FC<PropsWithChildren> { props ->
     val queryClient = useConstant(::QueryClient)
@@ -12,6 +13,8 @@ internal val AppModule = FC<PropsWithChildren> { props ->
     QueryClientProvider {
         client = queryClient
 
-        +props.children
+        SelectionModule {
+            +props.children
+        }
     }
 }
--- examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/components/UserInfo.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/components/UserInfo.kt
@@ -6,7 +6,6 @@ import react.dom.html.ReactHTML.div
 import web.cssom.Display
 import web.cssom.GridAutoFlow
 import web.cssom.px
-import wrappers.example.selection.SelectionModule
 
 val UserInfo = FC {
     div {
@@ -16,9 +15,7 @@ val UserInfo = FC {
             gridAutoFlow = GridAutoFlow.column
         }
 
-        SelectionModule {
-            UserTable()
-            UserPanel()
-        }
+        UserTable()
+        UserPanel()
     }
 }
