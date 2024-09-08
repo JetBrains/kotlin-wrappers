a732c48f862eb2c3ea906972da91295116c753a7
Victor Turansky <victor.turansky@gmail.com>
2024-09-08 14:56:05 +0300

    React Table Example. Update dependencies

--- examples/tanstack-react-table/build.gradle.kts
+++ examples/tanstack-react-table/build.gradle.kts
@@ -3,10 +3,9 @@ plugins {
 }
 
 dependencies {
-    jsMainImplementation(libs.wrappers.emotion)
-    jsMainImplementation(libs.wrappers.js)
     jsMainImplementation(libs.wrappers.react)
-    jsMainImplementation(libs.wrappers.reactUse)
     jsMainImplementation(libs.wrappers.reactDom)
-    jsMainImplementation(libs.wrappers.reactRouterDom)
+    jsMainImplementation(libs.wrappers.emotion)
+    jsMainImplementation(libs.wrappers.tanstack.reactQuery)
+    jsMainImplementation(libs.wrappers.tanstack.reactTable)
 }
