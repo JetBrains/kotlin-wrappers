ac671420e4f097c1760819917a3fa94cc3492061
Victor Turansky <victor.turansky@gmail.com>
2024-09-14 15:37:48 +0300

    Revert "Lazy. WA against reexport in app"
    This reverts commit 096546528b5f76bd3be9767356c5212ff3a101f9.

--- examples/react-lazy-modules-webpack/build.gradle.kts
+++ examples/react-lazy-modules-webpack/build.gradle.kts
@@ -2,18 +2,6 @@ plugins {
     `kotlin-conventions`
 }
 
-// WA for reexport in app
-tasks.jsProductionExecutableCompileSync {
-    doLast {
-        val entryFile = destinationDirectory.get()
-            .resolve("react-lazy-modules-webpack.mjs")
-
-        val newContent = entryFile.readText()
-            .replace("\nexport * from './", "\n// export * from './")
-        entryFile.writeText(newContent)
-    }
-}
-
 dependencies {
     jsMainImplementation(kotlinWrappers.emotion)
     jsMainImplementation(kotlinWrappers.js)
