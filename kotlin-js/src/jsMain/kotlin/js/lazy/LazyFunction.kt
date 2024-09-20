78b2c69dc470e00fd4fdf067bdf2f9f9598772cd
Victor Turansky <victor.turansky@gmail.com>
2024-09-21 00:41:10 +0300

    Revert "Lazy. `LazyFunction.invoke` - extension"
    This reverts commit 3a3a59121620d2989ebf6a04159f9e305b046dad.

--- kotlin-js/src/jsMain/kotlin/js/lazy/LazyFunction.kt
+++ kotlin-js/src/jsMain/kotlin/js/lazy/LazyFunction.kt
@@ -1,12 +1,18 @@
+@file:Suppress(
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+    "INLINE_EXTERNAL_DECLARATION",
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+    "DECLARATION_CANT_BE_INLINED",
+)
+
 package js.lazy
 
-import js.promise.PromiseResult
+import js.lazy.internal.invokeLazyFunction
 
-sealed external interface LazyFunction<out R>
-
-suspend operator fun <R> LazyFunction<R>.invoke(): R =
-    unsafeCast<PromiseResult<LazyFunctionSource<R>>>()
-        .await()()
+sealed external interface LazyFunction<out R> {
+    suspend inline operator fun invoke(): R =
+        invokeLazyFunction(this)
+}
 
 fun <R> LazyFunction(
     value: LazyFunctionSource<R>,
