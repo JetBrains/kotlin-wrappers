93fa47f485341f42a9753f41d7b6a33bb5c3c083
Victor Turansky <victor.turansky@gmail.com>
2024-09-29 03:30:02 +0300

    Examples. Add missed example

--- examples/react/build.gradle.kts
+++ examples/react/build.gradle.kts
@@ -6,5 +6,6 @@ dependencies {
     jsMainImplementation(kotlinWrappers.emotion)
     jsMainImplementation(kotlinWrappers.js)
     jsMainImplementation(kotlinWrappers.react)
+    jsMainImplementation(kotlinWrappers.reactDom)
     jsMainImplementation(kotlinWrappers.reactUse)
 }
