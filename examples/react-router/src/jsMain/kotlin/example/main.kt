22dc7c62f5abf8c119d12e4bd5edecbbccea6654
Victor Turansky <victor.turansky@gmail.com>
2024-09-29 03:25:02 +0300

    Examples. Add `main` for "React Router"

---
+++ examples/react-router/src/jsMain/kotlin/example/main.kt
@@ -0,0 +1,24 @@
+package example
+
+import react.create
+import react.dom.client.createRoot
+import web.dom.document
+import web.html.HTML.div
+import web.html.HTMLElement
+
+fun main() {
+    val container = createContainer()
+    createRoot(container)
+        .render(ReactRouterDomApp.create())
+}
+
+private fun createContainer(): HTMLElement {
+    val container = document.createElement(div)
+    container.style.apply {
+        background = "red"
+        width = "100%"
+        height = "100%"
+    }
+    document.body.appendChild(container)
+    return container
+}
