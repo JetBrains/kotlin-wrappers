233d274f8834dbef0b1436c2fa2d717ce5e486a0
Victor Turansky <victor.turansky@gmail.com>
2024-10-07 02:56:54 +0300

    Objects. Immutable `TypedPropertyDescriptor`

--- kotlin-js/src/jsMain/kotlin/js/objects/TypedPropertyDescriptor.kt
+++ kotlin-js/src/jsMain/kotlin/js/objects/TypedPropertyDescriptor.kt
@@ -4,10 +4,10 @@ import kotlinx.js.JsPlainObject
 
 @JsPlainObject
 external interface TypedPropertyDescriptor<T> {
-    var configurable: Boolean?
-    var enumerable: Boolean?
-    var value: T?
-    var writable: Boolean?
-    var get: (() -> T)?
-    var set: ((v: T) -> Unit)?
+    val configurable: Boolean?
+    val enumerable: Boolean?
+    val value: T?
+    val writable: Boolean?
+    val get: (() -> T)?
+    val set: ((v: T) -> Unit)?
 }
