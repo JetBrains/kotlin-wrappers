ab614559f040a1b99d030d1f23182d86014cbe17
Victor Turansky <victor.turansky@gmail.com>
2024-11-04 23:54:53 +0200

    React Legacy. Update `HOC` implementation

--- kotlin-react-legacy/src/jsMain/kotlin/react/HOC.kt
+++ kotlin-react-legacy/src/jsMain/kotlin/react/HOC.kt
@@ -1,25 +1,39 @@
 package react
 
-import kotlinext.js.JsFunction
+external interface HOC<out P : Props, in R : Props> {
+}
 
-external interface HOC<out P : Props, in R : Props> : JsFunction<Nothing?, ComponentClass<R>>
+@Suppress("NOTHING_TO_INLINE")
+inline operator fun <P : Props, R : Props> HOC<P, R>.invoke(
+    component: ComponentType<P>,
+    vararg args: Any?,
+): ComponentClass<R> {
+    asDynamic()(component, args)
+}
 
-operator fun <P : Props, R : Props> HOC<P, R>.invoke(component: ComponentClass<P>) =
-    call(null, component)
+operator fun <P : Props, R : Props> HOC<P, R>.invoke(
+    component: RBuilder.(P) -> Unit,
+): ComponentClass<R> =
+    invoke(
+        component = fc { props: P ->
+            createElement<P> {
+                component(props)
+            }
+        },
+    )
 
-operator fun <P : Props, R : Props> HOC<P, R>.invoke(component: RBuilder.(P) -> Unit) =
-    call(null, { props: P ->
-        createElement<P> {
-            component(props)
-        }
-    })
-
-operator fun <P : Props, R : Props> HOC<P, R>.invoke(config: Any, component: RBuilder.(P) -> Unit) =
-    call(null, { props: P ->
-        createElement<P> {
-            component(props)
-        }
-    }, config)
+operator fun <P : Props, R : Props> HOC<P, R>.invoke(
+    config: Any,
+    component: RBuilder.(P) -> Unit,
+): ComponentClass<R> =
+    invoke(
+        component = fc { props: P ->
+            createElement<P> {
+                component(props)
+            }
+        },
+        config,
+    )
 
 fun <P : Props> allOf(vararg hocs: HOC<P, P>): (component: ComponentClass<P>) -> ComponentClass<P> =
     { hocs.fold(it) { acc, hoc -> hoc(acc) } }
