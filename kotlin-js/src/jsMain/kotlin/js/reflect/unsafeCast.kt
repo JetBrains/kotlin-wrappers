e455684cbcaea2c2edec9d00298a1f8951bebf92
Victor Turansky <victor.turansky@gmail.com>
2024-11-05 02:23:39 +0200

    JS. Add new `unsafeCast` (#2449)

---
+++ kotlin-js/src/jsMain/kotlin/js/reflect/unsafeCast.kt
@@ -0,0 +1,15 @@
+@file:Suppress(
+    "NOTHING_TO_INLINE",
+)
+
+package js.reflect
+
+inline fun <T: Any> unsafeCast(
+    value: String,
+): T =
+    value.unsafeCast<T>()
+
+inline fun <T: Any> unsafeCast(
+    value: Any,
+): T =
+    value.unsafeCast<T>()
