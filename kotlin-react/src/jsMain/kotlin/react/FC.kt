32eba2e4cd9ca30af9cbb998c80b380627a8d1f4
Victor Turansky <victor.turansky@gmail.com>
2024-11-05 02:55:34 +0200

    React. Use new `unsafeCast` (#2449)

--- kotlin-react/src/jsMain/kotlin/react/FC.kt
+++ kotlin-react/src/jsMain/kotlin/react/FC.kt
@@ -4,21 +4,17 @@ import js.reflect.unsafeCast
 
 fun FC(
     block: @ReactDsl ChildrenBuilder.() -> Unit,
-): FC<Props> {
-    val component = {
+): FC<Props> =
+    unsafeCast {
         createElementOrNull(block)
     }
-    return unsafeCast(component)
-}
 
 fun <P : Props> FC(
     block: @ReactDsl ChildrenBuilder.(props: P) -> Unit,
-): FC<P> {
-    val component = { props: P ->
+): FC<P> =
+    unsafeCast { props: P ->
         createElementOrNull { block(props) }
     }
-    return unsafeCast(component)
-}
 
 fun FC(
     displayName: String,
