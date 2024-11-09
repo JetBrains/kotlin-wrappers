e469c6f93a4309a2bb6093a42dd0962114eec846
Victor Turansky <victor.turansky@gmail.com>
2024-11-09 13:40:44 +0200

    Screen. `Screen` extends `EventTarget` [generated]

--- kotlin-browser/src/jsMain/generated/web/screen/Screen.kt
+++ kotlin-browser/src/jsMain/generated/web/screen/Screen.kt
@@ -2,12 +2,18 @@
 
 package web.screen
 
+import web.events.Event
+import web.events.EventInstance
+import web.events.EventTarget
+import web.events.JsEvent
+
 /**
  * A screen, usually the one on which the current window is being rendered, and is obtained using window.screen.
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Screen)
  */
-sealed external class Screen {
+sealed external class Screen :
+    EventTarget {
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Screen/availHeight)
      */
@@ -42,4 +48,10 @@ sealed external class Screen {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Screen/width)
      */
     val width: Int
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Screen/change_event)
+     */
+    @JsEvent("change")
+    val changeEvent: EventInstance<Event, Screen /* this */, Screen /* this */>
 }
