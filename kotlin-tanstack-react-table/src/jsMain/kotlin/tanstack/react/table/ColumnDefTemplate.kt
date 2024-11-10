067e4f09171df1b17f2298ed7170d91f5014b5e4
Victor Turansky <victor.turansky@gmail.com>
2024-11-11 01:01:40 +0200

    TanStack Table. Use new `unsafeCast` (#2449)

--- kotlin-tanstack-react-table/src/jsMain/kotlin/tanstack/react/table/ColumnDefTemplate.kt
+++ kotlin-tanstack-react-table/src/jsMain/kotlin/tanstack/react/table/ColumnDefTemplate.kt
@@ -4,10 +4,11 @@
 
 package tanstack.react.table
 
+import js.reflect.unsafeCast
 import react.FC
 import tanstack.table.core.ColumnDefTemplate
 
 inline fun <T : Any> ColumnDefTemplate(
     source: FC<* /* T & Props */>,
 ): ColumnDefTemplate<T> =
-    source.unsafeCast<ColumnDefTemplate<T>>()
+    unsafeCast(source)
