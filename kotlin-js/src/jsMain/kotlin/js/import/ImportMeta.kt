064981557e6bd85bf327e4118fb5a4105b991e1c
Victor Turansky <victor.turansky@gmail.com>
2024-11-22 02:03:48 +0200

    Import. `ImportMeta` doc

--- kotlin-js/src/jsMain/kotlin/js/import/ImportMeta.kt
+++ kotlin-js/src/jsMain/kotlin/js/import/ImportMeta.kt
@@ -1,5 +1,11 @@
 package js.import
 
+/**
+ * The type of `import.meta`.
+ *
+ * If you need to declare that a given property exists on `import.meta`,
+ * this type may be augmented via interface merging.
+ */
 external interface ImportMeta {
     val url: String
 
