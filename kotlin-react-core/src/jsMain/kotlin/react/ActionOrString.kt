bfb2a2cfc9afcdb36b21bc6a2aaae7a0c2f1ffe6
Victor Turansky <victor.turansky@gmail.com>
2024-11-24 21:19:45 +0200

    React. Remove `asDynamic()` usages (#2449)

--- kotlin-react-core/src/jsMain/kotlin/react/ActionOrString.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/ActionOrString.kt
@@ -4,13 +4,16 @@
 
 package react
 
+import js.reflect.unsafeCast
+import js.reflect.upcast
+
 @JsExternalInheritorsOnly
 sealed external interface ActionOrString<in T>
 
 inline fun ActionOrString<*>.asStringOrNull(): String? =
-    asDynamic() as? String
+    upcast<Any>() as? String
 
 inline fun <T> ActionOrString<T>.asActionOrNull(): Action<T>? =
-    if (asDynamic() is Function<*>) {
-        unsafeCast<Action<T>>()
+    if (upcast<Any>() is Function<*>) {
+        unsafeCast<Action<T>>(this)
     } else null
