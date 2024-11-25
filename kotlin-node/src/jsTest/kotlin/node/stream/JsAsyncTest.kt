c5c293069a1d4f90f9b99941cd64cceee4ebd3d6
Victor Turansky <victor.turansky@gmail.com>
2024-11-25 23:07:09 +0200

    Node. Remove `Array.push` usage (#2449)

--- kotlin-node/src/jsTest/kotlin/node/stream/JsAsyncTest.kt
+++ kotlin-node/src/jsTest/kotlin/node/stream/JsAsyncTest.kt
@@ -1,6 +1,5 @@
 package node.stream
 
-import js.array.push
 import kotlinx.coroutines.test.runTest
 import node.buffer.Buffer
 import kotlin.test.Test
@@ -14,17 +13,19 @@ class JsAsyncTest {
             Buffer.from("world"),
         )
 
-        val actual = arrayOf<Buffer>()
-        val destination = Writable(WritableOptions(
-            write = { chunk, encoding, callback ->
-                actual.push(chunk as Buffer)
-                callback(null)
-            }
-        ))
+        val actual = mutableListOf<Buffer>()
+        val destination = Writable(
+            opts = WritableOptions(
+                write = { chunk, encoding, callback ->
+                    actual.add(chunk as Buffer)
+                    callback(null)
+                }
+            ),
+        )
 
         pipelineAsync(source, destination).await()
         // TODO: pipeline(source, destination)
 
-        assertContentEquals(source, actual)
+        assertContentEquals(source, actual.toTypedArray())
     }
 }
