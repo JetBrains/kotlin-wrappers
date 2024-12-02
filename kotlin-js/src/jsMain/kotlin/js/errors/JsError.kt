3f09f5e5555e8674eeb7bf5d2c6bf07773a7b00a
Victor Turansky <victor.turansky@gmail.com>
2024-12-02 02:53:56 +0200

    JS. Remove redundant `asDynamic()` usages (#2449)

--- kotlin-js/src/jsMain/kotlin/js/errors/JsError.kt
+++ kotlin-js/src/jsMain/kotlin/js/errors/JsError.kt
@@ -1,9 +1,16 @@
 package js.errors
 
+import js.reflect.unsafeCast
+
 actual typealias JsError = Throwable
 
 inline var JsError.name: JsErrorName
-    get() = asDynamic().name
+    get() = unsafeCast<HasErrorName>(this).name
     set(value) {
-        asDynamic().name = value
+        unsafeCast<HasErrorName>(this).name = value
     }
+
+@PublishedApi
+internal external interface HasErrorName {
+    var name: JsErrorName
+}
