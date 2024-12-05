2485b493ba5d2849ea97a4f5ef5c8deae023af7d
Victor Turansky <victor.turansky@gmail.com>
2024-12-05 23:17:25 +0200

    React 19. Temp build workaround

--- kotlin-tanstack-react-table/build.gradle.kts
+++ kotlin-tanstack-react-table/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
 
 dependencies {
     jsMainApi(projects.kotlinReactCore)
+    // TODO: remove after migration on React 19
+    jsMainApi(projects.kotlinReactDom)
     jsMainApi(projects.kotlinTanstackTableCore)
 
     jsMainApi(npm(libs.npm.tanstack.reactTable))
