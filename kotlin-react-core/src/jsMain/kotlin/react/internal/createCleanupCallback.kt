77dc8d1132e57f0736d41cc9a3bf3c7ffc634838
Victor Turansky <victor.turansky@gmail.com>
2024-12-08 03:47:50 +0200

    React 19. Add additional `createCleanupCallback` factory

--- kotlin-react-core/src/jsMain/kotlin/react/internal/createCleanupCallback.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/internal/createCleanupCallback.kt
@@ -5,8 +5,18 @@ import react.Cleanup
 
 fun createCleanupCallback(
     block: suspend CoroutineScope.() -> Unit,
-): () -> Cleanup? = callback@{
+): () -> Cleanup = callback@{
     val job = isolatedJob(block)
+
+    return@callback {
+        job.cancel()
+    }
+}
+
+fun <T> createCleanupCallback(
+    block: suspend CoroutineScope.(T) -> Unit,
+): (T) -> Cleanup = callback@{ p1 ->
+    val job = isolatedJob { block(p1) }
 
     return@callback {
         job.cancel()
