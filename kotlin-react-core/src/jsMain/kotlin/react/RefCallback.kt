2df116195df0cc36b9279ca79585668ef79cfba4
Victor Turansky <victor.turansky@gmail.com>
2024-12-08 03:12:00 +0200

    React Legacy. Add `LegacyRefCallback`

--- kotlin-react-core/src/jsMain/kotlin/react/RefCallback.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/RefCallback.kt
@@ -3,7 +3,7 @@ package react
 import js.reflect.unsafeCast
 import react.raw.useCallbackRaw
 
-sealed external interface RefCallback<in T : Any> :
+external interface RefCallback<in T : Any> :
     Ref<T>
 
 inline fun <T : Any> RefCallback(
---
+++ kotlin-react-legacy/src/jsMain/kotlin/react/LegacyRefCallback.kt
@@ -0,0 +1,21 @@
+package react
+
+import js.reflect.unsafeCast
+import react.raw.useCallbackRaw
+
+external interface LegacyRefCallback<in T : Any> :
+    RefCallback<T>
+
+fun <T : Any> LegacyRefCallback(
+    callback: (T?) -> Unit,
+): LegacyRefCallback<T> =
+    unsafeCast { instance: T? ->
+        callback(instance)
+        undefined
+    }
+
+fun <T : Any> useLegacyRefCallback(
+    vararg dependencies: Any?,
+    callback: (T?) -> Unit,
+): LegacyRefCallback<T> =
+    LegacyRefCallback(useCallbackRaw(callback, dependencies))
