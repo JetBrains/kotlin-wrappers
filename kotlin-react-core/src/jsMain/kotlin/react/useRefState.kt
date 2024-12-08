c58e532417ea308cc19486715e06e97db837a60c
Victor Turansky <victor.turansky@gmail.com>
2024-12-09 00:18:13 +0200

    React 19. Actualize `useRefState` implementation

--- kotlin-react-core/src/jsMain/kotlin/react/useRefState.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/useRefState.kt
@@ -1,10 +1,21 @@
-@file:JsModule("react")
-
 package react
 
 import js.array.JsTuple2
+import js.array.tupleOf
+import kotlinx.coroutines.job
 
 // [Example](https://popper.js.org/react-popper/v2/#example)
 
-@JsName("useState")
-external fun <T : Any> useRefState(): JsTuple2<T?, RefCallback<T>>
+fun <T : Any> useRefState(): JsTuple2<T?, RefCallback<T>> {
+    var state by useState<T>()
+    val callback = RefCallback<T> { value ->
+        state = value
+
+        // cleanup
+        coroutineContext.job.invokeOnCompletion {
+            state = null
+        }
+    }
+
+    return tupleOf(state, callback)
+}
