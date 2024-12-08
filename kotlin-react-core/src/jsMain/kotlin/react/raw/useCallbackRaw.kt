791d42312b29c55efa802f770599e7a9ffadb6ec
Victor Turansky <victor.turansky@gmail.com>
2024-12-08 04:03:35 +0200

    React 19. Add additional `useCallbackRaw` overload

--- kotlin-react-core/src/jsMain/kotlin/react/raw/useCallbackRaw.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/raw/useCallbackRaw.kt
@@ -3,10 +3,18 @@
 package react.raw
 
 import js.array.ReadonlyArray
+import react.RefCallback
 
 // Callback Hook (16.8+)
 @JsName("useCallback")
 external fun <T : Function<*>> useCallbackRaw(
+    callback: T,
+    dependencies: ReadonlyArray<Any?>,
+): T
+
+// TODO: use common marker interface for JS functions?
+@JsName("useCallback")
+external fun <T : RefCallback<*>> useCallbackRaw(
     callback: T,
     dependencies: ReadonlyArray<Any?>,
 ): T
