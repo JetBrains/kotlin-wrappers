15edea28ebf40848362234a1df315db55428a819
Victor Turansky <victor.turansky@gmail.com>
2024-12-08 04:07:52 +0200

    React 19. Support `RefCallback` with cleanup

--- kotlin-react-core/src/jsMain/kotlin/react/RefCallback.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/RefCallback.kt
@@ -1,6 +1,8 @@
 package react
 
 import js.reflect.unsafeCast
+import kotlinx.coroutines.CoroutineScope
+import react.internal.createCleanupCallback
 import react.raw.useCallbackRaw
 
 /* sealed */
@@ -8,12 +10,12 @@ external interface RefCallback<in T : Any> :
     Ref<T>
 
 fun <T : Any> RefCallback(
-    callback: (T?) -> Unit,
+    block: suspend CoroutineScope.(T) -> Unit,
 ): RefCallback<T> =
-    unsafeCast(callback)
+    unsafeCast(createCleanupCallback(block))
 
 fun <T : Any> useRefCallback(
     vararg dependencies: Any?,
-    callback: (T?) -> Unit,
+    block: suspend CoroutineScope.(T) -> Unit,
 ): RefCallback<T> =
-    RefCallback(useCallbackRaw(callback, dependencies))
+    useCallbackRaw(RefCallback(block), dependencies)
