079c6b221ce4b5dc6b27ceed86787179a26c7a3c
Victor Turansky <victor.turansky@gmail.com>
2024-12-10 00:06:43 +0200

    React. Actualize `ErrorInfo`

--- kotlin-react-core/src/jsMain/kotlin/react/ErrorInfo.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/ErrorInfo.kt
@@ -1,5 +1,12 @@
 package react
 
+import js.objects.JsPlainObject
+
+@JsPlainObject
 external interface ErrorInfo {
-    val componentStack: String
+    /**
+     * Captures which component contained the exception, and its ancestors.
+     */
+    val componentStack: String?
+    val digest: String?
 }
