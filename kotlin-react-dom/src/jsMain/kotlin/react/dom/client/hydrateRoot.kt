d171b24dcff307c0763567d05675b7e88f0793d4
Victor Turansky <victor.turansky@gmail.com>
2024-12-09 23:41:34 +0200

    React. Additional `hydrateRoot` container

--- kotlin-react-dom/src/jsMain/kotlin/react/dom/client/hydrateRoot.kt
+++ kotlin-react-dom/src/jsMain/kotlin/react/dom/client/hydrateRoot.kt
@@ -3,6 +3,7 @@
 package react.dom.client
 
 import react.ReactNode
+import web.dom.Document
 import web.dom.Element
 
 // See https://reactjs.org/docs/react-dom.html
@@ -10,6 +11,12 @@ import web.dom.Element
 // 18.0+
 external fun hydrateRoot(
     container: Element,
+    initialChildren: ReactNode,
+    options: HydrationOptions = definedExternally,
+): Root
+
+external fun hydrateRoot(
+    container: Document,
     initialChildren: ReactNode,
     options: HydrationOptions = definedExternally,
 ): Root
