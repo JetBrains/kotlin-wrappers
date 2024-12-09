c2745cacde51255b52ebdafc6b7b0e2442892684
Victor Turansky <victor.turansky@gmail.com>
2024-12-09 16:35:14 +0200

    Revert "React 19. Actualize `useRefState` implementation"
    This reverts commit c58e532417ea308cc19486715e06e97db837a60c.

--- kotlin-react-core/src/jsMain/kotlin/react/useRefState.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/useRefState.kt
@@ -1,21 +1,10 @@
+@file:JsModule("react")
+
 package react
 
 import js.array.JsTuple2
-import js.array.tupleOf
-import kotlinx.coroutines.job
 
 // [Example](https://popper.js.org/react-popper/v2/#example)
 
-fun <T : Any> useRefState(): JsTuple2<T?, RefCallback<T>> {
-    var state by useState<T>()
-    val callback = RefCallback<T> { value ->
-        state = value
-
-        // cleanup
-        coroutineContext.job.invokeOnCompletion {
-            state = null
-        }
-    }
-
-    return tupleOf(state, callback)
-}
+@JsName("useState")
+external fun <T : Any> useRefState(): JsTuple2<T?, RefCallback<T>>
