3555acb7368f85584146df9187247aef3d01127a
Victor Turansky <victor.turansky@gmail.com>
2024-12-09 23:35:41 +0200

    React 19. Additional `createRoot` containers

--- kotlin-react-dom/src/jsMain/kotlin/react/dom/client/createRoot.kt
+++ kotlin-react-dom/src/jsMain/kotlin/react/dom/client/createRoot.kt
@@ -2,6 +2,8 @@
 
 package react.dom.client
 
+import web.dom.Document
+import web.dom.DocumentFragment
 import web.dom.Element
 
 // See https://reactjs.org/docs/react-dom.html
@@ -9,5 +11,16 @@ import web.dom.Element
 // 18.0+
 external fun createRoot(
     container: Element,
+    options: RootOptions = definedExternally,
+): Root
+
+external fun createRoot(
+    container: DocumentFragment,
+    options: RootOptions = definedExternally,
+): Root
+
+
+external fun createRoot(
+    container: Document,
     options: RootOptions = definedExternally,
 ): Root
