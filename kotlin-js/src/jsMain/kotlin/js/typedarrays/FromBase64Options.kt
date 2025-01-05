2e5714d9c9e2260a27385914473f248794cf1271
Victor Turansky <victor.turansky@gmail.com>
2025-01-05 02:54:29 +0200

    Typed arrays. Fix `FromBase64Options` declaration

--- kotlin-js/src/jsMain/kotlin/js/typedarrays/FromBase64Options.kt
+++ kotlin-js/src/jsMain/kotlin/js/typedarrays/FromBase64Options.kt
@@ -3,7 +3,7 @@ package js.typedarrays
 import js.objects.JsPlainObject
 
 @JsPlainObject
-interface FromBase64Options {
+external interface FromBase64Options {
     val alphabet: Alphabet?
     val lastChunkHandling: LastChunkHandling?
 }
