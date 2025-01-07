6c6c71d57d281ded94e20bc86619349bf43b606a
Victor Turansky <victor.turansky@gmail.com>
2025-01-07 03:51:44 +0200

    React DOM. Separate files for declarations

--- kotlin-react-dom/src/jsMain/kotlin/react/dom/server/renderToStaticMarkup.kt
+++ kotlin-react-dom/src/jsMain/kotlin/react/dom/server/renderToStaticMarkup.kt
@@ -6,8 +6,5 @@ import react.ReactNode
 
 // See https://reactjs.org/docs/react-dom-server.html
 
-@JsName("renderToString")
-external fun rawRenderToString(element: ReactNode?): String
-
 @JsName("renderToStaticMarkup")
 external fun rawRenderToStaticMarkup(element: ReactNode?): String
---
+++ kotlin-react-dom/src/jsMain/kotlin/react/dom/server/renderToString.kt
@@ -0,0 +1,10 @@
+@file:JsModule("react-dom/server")
+
+package react.dom.server
+
+import react.ReactNode
+
+// See https://reactjs.org/docs/react-dom-server.html
+
+@JsName("renderToString")
+external fun rawRenderToString(element: ReactNode?): String
