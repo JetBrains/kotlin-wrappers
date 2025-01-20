ccdd94e55e92c91020174f2b223e29015a7f3d46
Victor Turansky <victor.turansky@gmail.com>
2025-01-20 23:58:42 +0200

    JSON. `isRawJSON`, `rawJSON`

---
+++ kotlin-js/src/jsMain/kotlin/js/json/isRawJSON.kt
@@ -0,0 +1,12 @@
+@file:JsQualifier("JSON")
+
+package js.json
+
+import js.import.JsQualifier
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/isRawJSON)
+ */
+external fun isRawJSON(
+    value: Any,
+): Boolean
---
+++ kotlin-js/src/jsMain/kotlin/js/json/rawJSON.kt
@@ -0,0 +1,12 @@
+@file:JsQualifier("JSON")
+
+package js.json
+
+import js.import.JsQualifier
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/rawJSON)
+ */
+external fun rawJSON(
+    value: String,
+): Any
