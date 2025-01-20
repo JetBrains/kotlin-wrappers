90c4bb3701f69bb31887161551ec6b2bfd74a483
Victor Turansky <victor.turansky@gmail.com>
2025-01-20 23:48:34 +0200

    JSON. Top-level `stringify`

--- kotlin-js/src/jsMain/kotlin/js/json/JSON.kt
+++ kotlin-js/src/jsMain/kotlin/js/json/JSON.kt
@@ -1,41 +1,41 @@
+@file:JsQualifier("JSON")
+
 package js.json
 
 import js.array.ReadonlyArray
 
-external object JSON {
-    /**
-     * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-     * @param value A JavaScript value, usually an object or array, to be converted.
-     * @param replacer A function that transforms the results.
-     * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
-     */
-    fun stringify(
-        value: Any?,
-        replacer: (key: String, value: Any?) -> Any? = definedExternally,
-        space: String = definedExternally,
-    ): String
+/**
+ * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+ * @param value A JavaScript value, usually an object or array, to be converted.
+ * @param replacer A function that transforms the results.
+ * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+ */
+external fun stringify(
+    value: Any?,
+    replacer: (key: String, value: Any?) -> Any? = definedExternally,
+    space: String = definedExternally,
+): String
 
-    fun stringify(
-        value: Any?,
-        replacer: (key: String, value: Any?) -> Any? = definedExternally,
-        space: Int = definedExternally,
-    ): String
+external fun stringify(
+    value: Any?,
+    replacer: (key: String, value: Any?) -> Any? = definedExternally,
+    space: Int = definedExternally,
+): String
 
-    /**
-     * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
-     * @param value A JavaScript value, usually an object or array, to be converted.
-     * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
-     * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
-     */
-    fun stringify(
-        value: Any?,
-        replacer: ReadonlyArray<Any /* number | string */> = definedExternally,
-        space: String = definedExternally,
-    ): String
+/**
+ * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+ * @param value A JavaScript value, usually an object or array, to be converted.
+ * @param replacer An array of strings and numbers that acts as an approved list for selecting the object properties that will be stringified.
+ * @param space Adds indentation, white space, and line break characters to the return-value JSON text to make it easier to read.
+ */
+external fun stringify(
+    value: Any?,
+    replacer: ReadonlyArray<Any /* number | string */> = definedExternally,
+    space: String = definedExternally,
+): String
 
-    fun stringify(
-        value: Any?,
-        replacer: ReadonlyArray<Any /* number | string */> = definedExternally,
-        space: Int = definedExternally,
-    ): String
-}
+external fun stringify(
+    value: Any?,
+    replacer: ReadonlyArray<Any /* number | string */> = definedExternally,
+    space: Int = definedExternally,
+): String
