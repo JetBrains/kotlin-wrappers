082a9472a0f1c081525be13867eea64dadcdd550
Victor Turansky <victor.turansky@gmail.com>
2025-01-21 23:12:50 +0200

    JSON. Fix `isRawJSON` contract

--- kotlin-js/src/jsMain/kotlin/js/json/isRawJSON.kt
+++ kotlin-js/src/jsMain/kotlin/js/json/isRawJSON.kt
@@ -8,5 +8,5 @@ import js.import.JsQualifier
  * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/isRawJSON)
  */
 external fun isRawJSON(
-    value: Any,
+    value: Any?,
 ): Boolean
