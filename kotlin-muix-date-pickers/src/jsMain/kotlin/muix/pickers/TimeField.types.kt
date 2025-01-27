0843060fb8f29f2bf3284a0583c6183d1690bc69
Alexander Popoff <alexander.popoff@flysto.net>
2025-01-27 21:57:12 +0200

    MUI X. Date Pickers. Add `TimeField` component

---
+++ kotlin-muix-date-pickers/src/jsMain/kotlin/muix/pickers/TimeField.types.kt
@@ -0,0 +1,24 @@
+package muix.pickers
+
+import mui.material.BaseSize
+import react.PropsWithClassName
+import react.PropsWithRef
+import web.dom.Element
+
+external interface TimeFieldProps<TValue : Any> :
+    PropsWithClassName,
+    PropsWithRef<Element> {
+    var clearable: Boolean?
+    var shouldRespectLeadingZeros: Boolean?
+    var error: Boolean?
+
+    var size: BaseSize?
+
+    var label: String?
+    var format: String?
+    var defaultValue: TValue?
+    var value: TValue?
+
+    var onChange: ((TValue?, Nothing) -> Unit)?
+    var onError: ((String?, TValue?) -> Unit)?
+}
