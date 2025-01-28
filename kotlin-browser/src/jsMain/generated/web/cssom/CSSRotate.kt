e6d5cf41c723d54624b9f124b09175c05f6eb471
Victor Turansky <victor.turansky@gmail.com>
2025-01-28 03:16:58 +0200

    Browser. Property-parameters [generated]

--- kotlin-browser/src/jsMain/generated/web/cssom/CSSRotate.kt
+++ kotlin-browser/src/jsMain/generated/web/cssom/CSSRotate.kt
@@ -7,29 +7,22 @@ package web.cssom
  */
 open external class CSSRotate(
     /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/x)
+     */
+    var x: CSSNumberish,
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/y)
+     */
+    var y: CSSNumberish,
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/z)
+     */
+    var z: CSSNumberish,
+    /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/angle)
      */
     var angle: CSSNumericValue,
 ) : CSSTransformComponent {
-    constructor(
-        x: CSSNumberish,
-        y: CSSNumberish,
-        z: CSSNumberish,
-        angle: CSSNumericValue,
-    )
+    constructor(angle: CSSNumericValue)
 
-    /**
-     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/x)
-     */
-    var x: CSSNumberish
-
-    /**
-     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/y)
-     */
-    var y: CSSNumberish
-
-    /**
-     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/CSSRotate/z)
-     */
-    var z: CSSNumberish
 }
