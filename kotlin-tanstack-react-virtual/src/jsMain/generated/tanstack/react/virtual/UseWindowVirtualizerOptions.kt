fd4279fee17b5853d59a925a4081ddce69669267
Victor Turansky <victor.turansky@gmail.com>
2025-02-08 18:53:46 +0200

    TanStack Virtual. `UseWindowVirtualizerOptions` - JSO [generated]

--- kotlin-tanstack-react-virtual/src/jsMain/generated/tanstack/react/virtual/UseWindowVirtualizerOptions.kt
+++ kotlin-tanstack-react-virtual/src/jsMain/generated/tanstack/react/virtual/UseWindowVirtualizerOptions.kt
@@ -1,11 +1,37 @@
 package tanstack.react.virtual
 
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+import tanstack.virtual.core.*
 import web.dom.Element
+import web.dom.observers.ResizeObserverEntry
 import web.window.Window
 
-external interface UseWindowVirtualizerOptions<TItemElement : Element> :
-    UseVirtualizerOptions<Window, TItemElement> {
-
-    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
-    override var getScrollElement: dynamic
+@JsPlainObject
+external interface UseWindowVirtualizerOptions<TItemElement : Element> {
+    val count: Int
+    val estimateSize: (index: Int) -> Int
+    val debug: Boolean?
+    val initialRect: Rect?
+    val onChange: ((instance: Virtualizer<Window, TItemElement>, sync: Boolean) -> Unit)?
+    val measureElement: ((element: TItemElement, entry: ResizeObserverEntry?, instance: Virtualizer<Window, TItemElement>) -> Int)?
+    val overscan: Int?
+    val horizontal: Boolean?
+    val paddingStart: Int?
+    val paddingEnd: Int?
+    val scrollPaddingStart: Int?
+    val scrollPaddingEnd: Int?
+    val initialOffset: Int /* | (() -> Int) */?
+    val getItemKey: ((index: Int) -> Key)?
+    val rangeExtractor: ((range: Range) -> ReadonlyArray<Int>)?
+    val scrollMargin: Int?
+    val gap: Int?
+    val indexAttribute: String?
+    val initialMeasurementsCache: ReadonlyArray<VirtualItem>?
+    val lanes: Int?
+    val isScrollingResetDelay: Int?
+    val useScrollendEvent: Boolean?
+    val enabled: Boolean?
+    val isRtl: Boolean?
+    val useAnimationFrameWithResizeObserver: Boolean?
 }
