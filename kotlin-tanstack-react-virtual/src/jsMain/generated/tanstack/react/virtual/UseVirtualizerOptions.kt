0cb758307525c9a87b32ae66fbe52066b5620418
Victor Turansky <victor.turansky@gmail.com>
2025-02-08 18:50:44 +0200

    TanStack Virtual. `UseVirtualizerOptions` - JSO [generated]

--- kotlin-tanstack-react-virtual/src/jsMain/generated/tanstack/react/virtual/UseVirtualizerOptions.kt
+++ kotlin-tanstack-react-virtual/src/jsMain/generated/tanstack/react/virtual/UseVirtualizerOptions.kt
@@ -1,18 +1,38 @@
 package tanstack.react.virtual
 
-import tanstack.virtual.core.VirtualizerOptions
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+import tanstack.virtual.core.*
 import web.dom.Element
+import web.dom.observers.ResizeObserverEntry
 import web.events.EventTarget
 
-external interface UseVirtualizerOptions<TScrollElement : EventTarget, TItemElement : Element> :
-    VirtualizerOptions<TScrollElement, TItemElement> {
-
-    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
-    override var observeElementRect: dynamic
-
-    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
-    override var observeElementOffset: dynamic
-
-    @Deprecated(message = "Excluded property", level = DeprecationLevel.HIDDEN)
-    override var scrollToFn: dynamic
+@JsPlainObject
+external interface UseVirtualizerOptions<TScrollElement : EventTarget, TItemElement : Element> {
+    val count: Int
+    val getScrollElement: () -> TScrollElement?
+    val estimateSize: (index: Int) -> Int
+    val debug: Boolean?
+    val initialRect: Rect?
+    val onChange: ((instance: Virtualizer<TScrollElement, TItemElement>, sync: Boolean) -> Unit)?
+    val measureElement: ((element: TItemElement, entry: ResizeObserverEntry?, instance: Virtualizer<TScrollElement, TItemElement>) -> Int)?
+    val overscan: Int?
+    val horizontal: Boolean?
+    val paddingStart: Int?
+    val paddingEnd: Int?
+    val scrollPaddingStart: Int?
+    val scrollPaddingEnd: Int?
+    val initialOffset: Int /* | (() -> Int) */?
+    val getItemKey: ((index: Int) -> Key)?
+    val rangeExtractor: ((range: Range) -> ReadonlyArray<Int>)?
+    val scrollMargin: Int?
+    val gap: Int?
+    val indexAttribute: String?
+    val initialMeasurementsCache: ReadonlyArray<VirtualItem>?
+    val lanes: Int?
+    val isScrollingResetDelay: Int?
+    val useScrollendEvent: Boolean?
+    val enabled: Boolean?
+    val isRtl: Boolean?
+    val useAnimationFrameWithResizeObserver: Boolean?
 }
