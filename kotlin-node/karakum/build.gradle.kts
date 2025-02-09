bd60384f60712e9704288f64026cc0be7bc1008b
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-02-09 22:38:09 +0200

    Exclude transitive dependencies from Karakum to avoid dependencies conflicts, add dependency for @types/node

--- kotlin-node/karakum/build.gradle.kts
+++ kotlin-node/karakum/build.gradle.kts
@@ -22,7 +22,20 @@ kotlin {
 
     sourceSets {
         jsMain.dependencies {
-            implementation("io.github.sgrishchenko:karakum:1.0.0-alpha.50")
+            implementation(libs.npm.types.node.get().let {
+                npm(
+                    name = it.name,
+                    version = it.version!!,
+                )
+            })
+
+            implementation(projects.kotlinJs)
+            implementation(projects.kotlinNode)
+            implementation(projects.kotlinTypescript)
+
+            implementation("io.github.sgrishchenko:karakum:1.0.0-alpha.50") {
+                exclude(group = "org.jetbrains.kotlin-wrappers")
+            }
         }
     }
 }
