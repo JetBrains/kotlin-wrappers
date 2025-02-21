53bc15693e87fd16be6bebe994a43bcb1d3cc082
Victor Turansky <victor.turansky@gmail.com>
2025-02-22 01:22:17 +0200

    RegExp. Add `RegExpIndicesArray.asArray()`

--- kotlin-js/src/jsMain/kotlin/js/regexp/RegExpIndicesArray.kt
+++ kotlin-js/src/jsMain/kotlin/js/regexp/RegExpIndicesArray.kt
@@ -1,10 +1,16 @@
 package js.regexp
 
 import js.array.ArrayLike
+import js.array.ReadonlyArray
 import js.objects.ReadonlyRecord
+import seskar.js.JsAlias
+import seskar.js.JsAlias.Companion.THIS
 
 @JsExternalInheritorsOnly
 external interface RegExpIndicesArray :
     ArrayLike<RegExpIndex> {
     val groups: ReadonlyRecord<String, RegExpIndex>?
+
+    @JsAlias(THIS)
+    fun asArray(): ReadonlyArray<String>
 }
