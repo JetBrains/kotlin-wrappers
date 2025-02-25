288588aab3fbd1371372eb7cf5e163c3a42e4828
porotkin <preload737773@gmail.com>
2025-02-25 09:22:35 +0200

    Examples. `tanstack-react-table`: Cleanup `useUsersTable` hook

--- examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/selection/useTableMeta.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/selection/useTableMeta.kt
@@ -1,11 +1,13 @@
 package wrappers.example.table.selection
 
+import preact.signals.react.useSignal
 import react.use.useConstant
 import tanstack.table.core.TableMeta
 
-internal fun useTableMeta(
-    selection: Selection,
-): TableMeta =
-    useConstant {
+internal fun useTableMeta(): TableMeta {
+    val selection = useSignal(EMPTY_SELECTION)
+
+    return useConstant {
         createMeta(selection)
     }
+}
--- examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/useUsersTable.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/wrappers/example/table/useUsersTable.kt
@@ -2,7 +2,6 @@ package wrappers.example.table
 
 import js.array.ReadonlyArray
 import js.objects.jso
-import preact.signals.react.useSignal
 import tanstack.react.table.useReactTable
 import tanstack.table.core.ColumnDef
 import tanstack.table.core.StringOrTemplateHeader
@@ -10,7 +9,6 @@ import tanstack.table.core.Table
 import tanstack.table.core.getCoreRowModel
 import wrappers.example.entities.User
 import wrappers.example.hooks.useUsers
-import wrappers.example.table.selection.EMPTY_SELECTION
 import wrappers.example.table.selection.createSelectionColumn
 import wrappers.example.table.selection.useTableMeta
 
@@ -29,10 +27,8 @@ private val COLUMNS: ReadonlyArray<ColumnDef<User, String>> = arrayOf(
 )
 
 fun useUsersTable(): Table<User> {
-    val selection = useSignal(EMPTY_SELECTION)
-    val tableMeta = useTableMeta(selection)
-
     val users = useUsers()
+    val tableMeta = useTableMeta()
 
     return useReactTable(
         options = jso {
