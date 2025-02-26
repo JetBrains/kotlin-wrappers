b533dc36bf8ab33b756c300e414f16d838317a92
Victor Turansky <victor.turansky@gmail.com>
2025-02-26 02:35:22 +0200

    Revert "Separate tests for `definedExternally` (#2449)"
    This reverts commit ea81f479d126effbc2734a1d9fd896b97bf48f45.

--- kotlin-js/src/commonTest/kotlin/js/core/StringArray.kt
+++ kotlin-js/src/commonTest/kotlin/js/core/StringArray.kt
@@ -1,12 +1,28 @@
 package js.core
 
 @JsName("Array")
-external class StringArray(
-    a: String = definedExternally,
-    b: String = definedExternally,
-    c: String = definedExternally,
-    d: String = definedExternally,
-) {
+external class StringArray() {
+    constructor(
+        a: String,
+    )
+
+    constructor(
+        a: String,
+        b: String,
+    )
+
+    constructor(
+        a: String,
+        b: String,
+        c: String,
+    )
+
+    constructor(
+        a: String,
+        b: String,
+        c: String,
+        d: String,
+    )
 
     val length: Int
 }
