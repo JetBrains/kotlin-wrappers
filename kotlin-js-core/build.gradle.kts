0ba3fb6f8734ecbdf253e846e1510f74d80db873
Victor Turansky <victor.turansky@gmail.com>
2025-02-26 02:35:22 +0200

    Revert "Reapply "JS. Common `definedExternally` (#2449)""
    This reverts commit 84f246a1c248ddf887944a0e396ceeedec0e7a1b.

--- kotlin-js-core/build.gradle.kts
+++ kotlin-js-core/build.gradle.kts
@@ -1,14 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
     `kotlin-library-conventions`
-}
-
-// for `definedExternally`
-tasks.withType<KotlinCompilationTask<*>>().configureEach {
-    compilerOptions {
-        freeCompilerArgs.addAll(
-            "-Xallow-kotlin-package",
-        )
-    }
 }
--- kotlin-js-core/src/commonMain/kotlin/kotlin/js/definedExternally.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress(
-    "WRONG_JS_INTEROP_TYPE",
-)
-
-package kotlin.js
-
-external val definedExternally: Nothing
