ea81f479d126effbc2734a1d9fd896b97bf48f45
Victor Turansky <victor.turansky@gmail.com>
2025-02-26 02:20:41 +0200

    Separate tests for `definedExternally` (#2449)

--- kotlin-js/src/commonTest/kotlin/js/core/StringArray.kt
+++ kotlin-js/src/commonTest/kotlin/js/core/StringArray.kt
@@ -1,28 +1,12 @@
 package js.core
 
 @JsName("Array")
-external class StringArray() {
-    constructor(
-        a: String,
-    )
-
-    constructor(
-        a: String,
-        b: String,
-    )
-
-    constructor(
-        a: String,
-        b: String,
-        c: String,
-    )
-
-    constructor(
-        a: String,
-        b: String,
-        c: String,
-        d: String,
-    )
+external class StringArray(
+    a: String = definedExternally,
+    b: String = definedExternally,
+    c: String = definedExternally,
+    d: String = definedExternally,
+) {
 
     val length: Int
 }
