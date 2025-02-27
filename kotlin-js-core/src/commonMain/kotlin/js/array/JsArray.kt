52eb0ece8ae9b970ea697eddb970981d0a5e4f2a
Victor Turansky <victor.turansky@gmail.com>
2025-02-27 02:59:40 +0200

    JS. External common `JsArray` (#2449)

--- kotlin-js-core/src/commonMain/kotlin/js/array/JsArray.kt
+++ kotlin-js-core/src/commonMain/kotlin/js/array/JsArray.kt
@@ -1,7 +1,9 @@
 @file:Suppress(
+    "WRONG_JS_INTEROP_TYPE",
+    "EXPECTED_EXTERNAL_DECLARATION",
     "EXPECT_ACTUAL_INCOMPATIBILITY",
 )
 
 package js.array
 
-expect class JsArray<T /* : JsAny? */>
+expect external class JsArray<T /* : JsAny? */>
