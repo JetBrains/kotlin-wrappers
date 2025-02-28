1247316140697e5d32f361b3000279379b13f252
Victor Turansky <victor.turansky@gmail.com>
2025-02-28 19:40:43 +0200

    JS. Fix formatting (#2449)

--- kotlin-js/src/jsMain/kotlin/js/disposable/usingSync.kt
+++ kotlin-js/src/jsMain/kotlin/js/disposable/usingSync.kt
@@ -4,14 +4,19 @@ import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 @PublishedApi
-internal inline fun <T : Disposable, R> usingSync(disposable: T, block: (T) -> R): R {
+internal inline fun <T : Disposable, R> usingSync(
+    disposable: T,
+    block: (T) -> R,
+): R {
     contract {
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
     }
     return disposable.asClosable().use { block(disposable) }
 }
 
-inline fun <R> usingSync(block: DisposableStack.() -> R): R {
+inline fun <R> usingSync(
+    block: DisposableStack.() -> R,
+): R {
     contract {
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
     }
