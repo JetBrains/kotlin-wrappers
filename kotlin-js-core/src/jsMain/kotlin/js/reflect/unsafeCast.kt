49a79c0564e23835caece9dea2c5e11b8962142a
Victor Turansky <victor.turansky@gmail.com>
2025-03-01 03:39:56 +0200

    Revert "Reflect. Common `unsafeCast` (#2449)"
    This reverts commit 17d3e80581388d4b9d96783a4ac27597118e8d10.

--- kotlin-js-core/src/jsMain/kotlin/js/reflect/unsafeCast.kt
+++ kotlin-js-core/src/jsMain/kotlin/js/reflect/unsafeCast.kt
@@ -1,16 +1,16 @@
 package js.reflect
 
-actual inline fun <T : Any> unsafeCast(
+inline fun <T: Any> unsafeCast(
     value: String,
 ): T =
     value.unsafeCast<T>()
 
-actual inline fun <T : Any> unsafeCast(
+inline fun <T: Any> unsafeCast(
     value: String?,
 ): T? =
     value.unsafeCast<T?>()
 
-actual inline fun <T> unsafeCast(
+inline fun <T> unsafeCast(
     value: Any?,
 ): T =
     value.unsafeCast<T>()
