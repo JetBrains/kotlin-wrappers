9214ea45e616eb115f8007eb0adf28dfa2df94ab
Victor Turansky <victor.turansky@gmail.com>
2025-03-01 05:25:29 +0200

    Reflect. Common `HasInstanceSymbolHolder` (#2449)

--- kotlin-js/src/commonMain/kotlin/js/reflect/HasInstanceSymbolHolder.kt
+++ kotlin-js/src/commonMain/kotlin/js/reflect/HasInstanceSymbolHolder.kt
@@ -1,10 +1,12 @@
 @file:Suppress(
+    "WRONG_JS_INTEROP_TYPE",
     "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
 )
 
 package js.reflect
 
 import js.symbol.Symbol
+import kotlin.js.definedExternally
 import seskar.js.JsNativeGetter
 
 external interface HasInstanceSymbolHolder {
