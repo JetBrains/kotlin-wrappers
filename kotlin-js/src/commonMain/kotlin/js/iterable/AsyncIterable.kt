bdfb604b5967bf40e13a6dd07c1ba7154f138c49
Victor Turansky <victor.turansky@gmail.com>
2025-03-03 00:43:51 +0200

    Iterable. Common `AsyncIterable` (#2449)

--- kotlin-js/src/commonMain/kotlin/js/iterable/AsyncIterable.kt
+++ kotlin-js/src/commonMain/kotlin/js/iterable/AsyncIterable.kt
@@ -1,4 +1,5 @@
 @file:Suppress(
+    "WRONG_JS_INTEROP_TYPE",
     "WRONG_BODY_OF_EXTERNAL_DECLARATION",
     "INLINE_EXTERNAL_DECLARATION",
     "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
@@ -9,6 +10,7 @@ package js.iterable
 
 import js.iterable.internal.iteratorFromAsyncIterable
 import js.symbol.Symbol
+import kotlin.js.definedExternally
 import seskar.js.JsNativeGetter
 
 external interface AsyncIterable<out T> {
