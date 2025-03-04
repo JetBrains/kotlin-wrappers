445387b8bd6bdaa16b6e5cb11ec2ae4db22fd95e
Victor Turansky <victor.turansky@gmail.com>
2025-03-05 01:46:54 +0200

    Array. Use `JsAny` (#2449)

--- kotlin-js/src/commonMain/kotlin/js/array/JsArrays.kt
+++ kotlin-js/src/commonMain/kotlin/js/array/JsArrays.kt
@@ -1,5 +1,4 @@
 @file:Suppress(
-    "WRONG_JS_INTEROP_TYPE",
     "UPPER_BOUND_VIOLATED",
     "INVISIBLE_MEMBER",
     "INVISIBLE_REFERENCE",
@@ -7,6 +6,7 @@
 
 package js.array
 
+import js.core.JsAny
 import js.iterable.AsyncIterable
 import js.iterable.JsIterable
 import js.iterable.JsIterator
@@ -17,46 +17,46 @@ import kotlin.js.JsName
 
 @JsName("Array")
 external object JsArrays {
-    fun <T> from(source: JsIterator<T>): ReadonlyArray<T>
+    fun <T : JsAny?> from(source: JsIterator<T>): ReadonlyArray<T>
 
-    fun <T> from(source: JsIterable<T>): ReadonlyArray<T>
+    fun <T : JsAny?> from(source: JsIterable<T>): ReadonlyArray<T>
 
     @LowPriorityInOverloadResolution
-    fun <T> from(source: ArrayLike<T>): ReadonlyArray<T>
+    fun <T : JsAny?> from(source: ArrayLike<T>): ReadonlyArray<T>
 
-    fun <T, U> from(
+    fun <T : JsAny?, U : JsAny?> from(
         source: JsIterator<T>,
         transform: (item: T, index: Int) -> U,
     ): ReadonlyArray<U>
 
-    fun <T, U> from(
+    fun <T : JsAny?, U : JsAny?> from(
         source: JsIterable<T>,
         transform: (item: T, index: Int) -> U,
     ): ReadonlyArray<U>
 
     @LowPriorityInOverloadResolution
-    fun <T, U> from(
+    fun <T : JsAny?, U : JsAny?> from(
         source: ArrayLike<T>,
         transform: (item: T, index: Int) -> U,
     ): ReadonlyArray<U>
 
-    fun <T> fromAsync(source: AsyncIterable<T>): Promise<ReadonlyArray<T>>
+    fun <T : JsAny?> fromAsync(source: AsyncIterable<T>): Promise<ReadonlyArray<T>>
 
-    fun <T> fromAsync(source: JsIterable<PromiseResult<T>>): Promise<ReadonlyArray<T>>
+    fun <T : JsAny?> fromAsync(source: JsIterable<PromiseResult<T>>): Promise<ReadonlyArray<T>>
 
-    fun <T> fromAsync(source: ReadonlyArray<PromiseResult<T>> /* ArrayLike */): Promise<ReadonlyArray<T>>
+    fun <T : JsAny?> fromAsync(source: ReadonlyArray<PromiseResult<T>> /* ArrayLike */): Promise<ReadonlyArray<T>>
 
-    fun <T, U> fromAsync(
+    fun <T : JsAny?, U : JsAny?> fromAsync(
         source: AsyncIterable<T>,
         transform: (item: T, index: Int) -> U,
     ): Promise<ReadonlyArray<U>>
 
-    fun <T, U> fromAsync(
+    fun <T : JsAny?, U : JsAny?> fromAsync(
         source: JsIterable<PromiseResult<T>>,
         transform: (item: T, index: Int) -> U,
     ): Promise<ReadonlyArray<U>>
 
-    fun <T, U> fromAsync(
+    fun <T : JsAny?, U : JsAny?> fromAsync(
         source: ReadonlyArray<PromiseResult<T>>, /* ArrayLike */
         transform: (item: T, index: Int) -> U,
     ): Promise<ReadonlyArray<U>>
