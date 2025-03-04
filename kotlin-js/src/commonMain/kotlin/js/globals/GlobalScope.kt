b60ce446cc5ed0b624614fc5d3e3be016ea797d6
Victor Turansky <victor.turansky@gmail.com>
2025-03-04 04:38:47 +0200

    Globals. Use `JsAny` (#2449)

--- kotlin-js/src/commonMain/kotlin/js/globals/GlobalScope.kt
+++ kotlin-js/src/commonMain/kotlin/js/globals/GlobalScope.kt
@@ -1,8 +1,10 @@
 package js.globals
 
+import js.core.JsAny
+import js.core.JsString
 import js.objects.Record
 import js.reflect.JsExternalInheritorsOnly
 
 @JsExternalInheritorsOnly
 external interface GlobalScope :
-    Record<String, Any?>
+    Record<JsString, JsAny?>
