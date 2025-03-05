692b1b5f6a57e4ed180661b323a349ff4344864f
Victor Turansky <victor.turansky@gmail.com>
2025-03-05 02:57:53 +0200

    Array. Use `JsAny` (#2449)

--- kotlin-js/src/commonMain/kotlin/js/array/JsTuple.kt
+++ kotlin-js/src/commonMain/kotlin/js/array/JsTuple.kt
@@ -1,14 +1,13 @@
 @file:Suppress(
-    "WRONG_JS_INTEROP_TYPE",
     "INTERFACE_WITH_SUPERCLASS",
 )
 
 package js.array
 
 import js.core.JsAny
-import kotlin.js.JsName
 import seskar.js.JsAlias
 import seskar.js.JsAlias.Companion.THIS
+import kotlin.js.JsName
 
 sealed external interface JsTuple :
     JsAny {
@@ -19,43 +18,43 @@ sealed external interface JsTuple :
     fun asArray(): ReadonlyArray<JsAny?>
 }
 
-sealed external interface JsTuple1<out A> :
+sealed external interface JsTuple1<out A : JsAny?> :
     JsTuple {
     @JsAlias("[0]")
     operator fun component1(): A
 }
 
-sealed external interface JsTuple2<out A, out B> :
+sealed external interface JsTuple2<out A : JsAny?, out B : JsAny?> :
     JsTuple1<A> {
     @JsAlias("[1]")
     operator fun component2(): B
 }
 
-sealed external interface JsTuple3<out A, out B, out C> :
+sealed external interface JsTuple3<out A : JsAny?, out B : JsAny?, out C : JsAny?> :
     JsTuple2<A, B> {
     @JsAlias("[2]")
     operator fun component3(): C
 }
 
-sealed external interface JsTuple4<out A, out B, out C, out D> :
+sealed external interface JsTuple4<out A : JsAny?, out B : JsAny?, out C : JsAny?, out D : JsAny?> :
     JsTuple3<A, B, C> {
     @JsAlias("[3]")
     operator fun component4(): D
 }
 
-sealed external interface JsTuple5<out A, out B, out C, out D, out E> :
+sealed external interface JsTuple5<out A : JsAny?, out B : JsAny?, out C : JsAny?, out D : JsAny?, out E : JsAny?> :
     JsTuple4<A, B, C, D> {
     @JsAlias("[4]")
     operator fun component5(): E
 }
 
-sealed external interface JsTuple6<out A, out B, out C, out D, out E, out F> :
+sealed external interface JsTuple6<out A : JsAny?, out B : JsAny?, out C : JsAny?, out D : JsAny?, out E : JsAny?, out F : JsAny?> :
     JsTuple5<A, B, C, D, E> {
     @JsAlias("[5]")
     operator fun component6(): F
 }
 
-sealed external interface JsTuple7<out A, out B, out C, out D, out E, out F, out G> :
+sealed external interface JsTuple7<out A : JsAny?, out B : JsAny?, out C : JsAny?, out D : JsAny?, out E : JsAny?, out F : JsAny?, out G : JsAny?> :
     JsTuple6<A, B, C, D, E, F> {
     @JsAlias("[6]")
     operator fun component7(): G
--- kotlin-js/src/commonMain/kotlin/js/objects/Object.kt
+++ kotlin-js/src/commonMain/kotlin/js/objects/Object.kt
@@ -22,11 +22,11 @@ private constructor() {
         fun <T : JsAny> create(o: T?, properties: PropertyDescriptorMap = definedExternally): T
         fun <T : JsAny> defineProperties(o: T, properties: PropertyDescriptorMap): T
         fun <T : JsAny, P : JsAny?> defineProperty(o: T, p: PropertyKey, attributes: TypedPropertyDescriptor<P>): T
-        fun <T : JsAny?> entries(o: ReadonlyRecord<JsString, T>): ReadonlyArray<JsTuple2<String, T>>
-        fun entries(o: JsAny): ReadonlyArray<JsTuple2<String, JsAny?>>
+        fun <T : JsAny?> entries(o: ReadonlyRecord<JsString, T>): ReadonlyArray<JsTuple2<JsString, T>>
+        fun entries(o: JsAny): ReadonlyArray<JsTuple2<JsString, JsAny?>>
         fun <R : JsAny?, T : R> freeze(o: T): R
-        fun <T : JsAny?> fromEntries(entries: JsIterable<JsTuple2<String, T>>): ReadonlyRecord<JsString, T>
-        fun <T : JsAny?> fromEntries(entries: ReadonlyArray<JsTuple2<String, T>>): ReadonlyRecord<JsString, T>
+        fun <T : JsAny?> fromEntries(entries: JsIterable<JsTuple2<JsString, T>>): ReadonlyRecord<JsString, T>
+        fun <T : JsAny?> fromEntries(entries: ReadonlyArray<JsTuple2<JsString, T>>): ReadonlyRecord<JsString, T>
         fun <T : JsAny?> getOwnPropertyDescriptor(o: JsAny, p: PropertyKey): TypedPropertyDescriptor<T>?
         fun getOwnPropertyNames(o: JsAny): ReadonlyArray<JsString>
         fun getOwnPropertySymbols(o: JsAny): ReadonlyArray<Symbol>
