e393cc52be7bd10b13b32bf8d25d41186911dff9
Victor Turansky <victor.turansky@gmail.com>
2025-03-15 03:51:34 +0200

    Browser. WasmJS compatible type parameter bounds (#2449)

--- kotlin-web/src/commonMain/generated/web/serialization/structuredClone.kt
+++ kotlin-web/src/commonMain/generated/web/serialization/structuredClone.kt
@@ -2,12 +2,13 @@
 
 package web.serialization
 
+import js.core.JsAny
 import kotlin.js.definedExternally
 
 /**
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Window/structuredClone)
  */
-external fun <T> structuredClone(
+external fun <T : JsAny?> structuredClone(
     value: T,
     options: StructuredSerializeOptions? = definedExternally,
 ): T
