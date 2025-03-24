fd31fcd88f2e9437ad22c8c35e58c9874329c71e
Victor Turansky <victor.turansky@gmail.com>
2025-03-24 23:05:04 +0200

    Dokka. Declare version in properties

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,5 +22,5 @@ dependencies {
     implementation(kotlin("gradle-plugin", version("kotlin")))
     implementation("org.jetbrains.kotlin:js-plain-objects:${version("kotlin")}")
     implementation("io.github.turansky.seskar:seskar-gradle-plugin:${version("seskar")}")
-    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:2.0.0")
+    implementation("org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:${version("dokka")}")
 }
