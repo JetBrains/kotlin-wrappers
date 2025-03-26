59517e1fdeb58f164af3b7f5dda84e82a5a1e726
Victor Turansky <victor.turansky@gmail.com>
2025-03-26 04:42:56 +0200

    Console. Overloads for Wasm [generated]

--- kotlin-web/src/commonMain/generated/web/console/Console.kt
+++ kotlin-web/src/commonMain/generated/web/console/Console.kt
@@ -38,6 +38,7 @@ sealed external interface Console {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/debug_static)
      */
     fun debug(vararg data: JsAny?)
+    fun debug(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/dir_static)
@@ -51,21 +52,25 @@ sealed external interface Console {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/dirxml_static)
      */
     fun dirxml(vararg data: JsAny?)
+    fun dirxml(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/error_static)
      */
     fun error(vararg data: JsAny?)
+    fun error(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/group_static)
      */
     fun group(vararg data: JsAny?)
+    fun group(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/groupCollapsed_static)
      */
     fun groupCollapsed(vararg data: JsAny?)
+    fun groupCollapsed(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/groupEnd_static)
@@ -76,11 +81,13 @@ sealed external interface Console {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/info_static)
      */
     fun info(vararg data: JsAny?)
+    fun info(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/log_static)
      */
     fun log(vararg data: JsAny?)
+    fun log(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/table_static)
@@ -114,9 +121,11 @@ sealed external interface Console {
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/trace_static)
      */
     fun trace(vararg data: JsAny?)
+    fun trace(vararg data: String?)
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/console/warn_static)
      */
     fun warn(vararg data: JsAny?)
+    fun warn(vararg data: String?)
 }
