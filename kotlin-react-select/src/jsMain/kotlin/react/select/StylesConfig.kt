e171e5f88403ab303ed259e826a95dc0b065c5fa
Victor Turansky <victor.turansky@gmail.com>
2025-03-30 01:32:08 +0200

    React Select. Extract `StylesConfigFunction`

--- kotlin-react-select/src/jsMain/kotlin/react/select/StylesConfig.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/StylesConfig.kt
@@ -1,26 +1,6 @@
 package react.select
 
-import csstype.PropertiesBuilder
 import js.objects.JsPlainObject
-import js.objects.Object
-import js.objects.jso
-import js.reflect.unsafeCast
-import react.Props
-
-typealias StylesConfigFunction<P /* Props */> = (
-    base: CSSObjectWithLabel,
-    props: P,
-) -> CSSObjectWithLabel
-
-fun <P : Props> Styles(
-    block: /* CSSObjectWithLabel */ PropertiesBuilder.(props: P) -> Unit,
-): StylesConfigFunction<P> {
-    return { base, props ->
-        val result = Object.assign(jso(), base)
-        block(unsafeCast(result), props)
-        result
-    }
-}
 
 @JsPlainObject
 external interface StylesConfig<Option : Any, Group : GroupBase<Option>> {
