1ef019c2f1d92bc29f8aaffc937fc02ebf475707
Victor Turansky <victor.turansky@gmail.com>
2025-03-30 01:27:27 +0200

    React Select. Fix formatting

--- kotlin-react-select/src/jsMain/kotlin/react/select/ClearIndicator.kt
+++ kotlin-react-select/src/jsMain/kotlin/react/select/ClearIndicator.kt
@@ -4,7 +4,9 @@ import react.PropsWithChildren
 import react.dom.html.HTMLAttributes
 import web.html.HTMLDivElement
 
-external interface ClearIndicatorProps<Option : Any, Group : GroupBase<Option>> : PropsWithChildren {
+external interface ClearIndicatorProps<Option : Any, Group : GroupBase<Option>> :
+    PropsWithChildren {
+
     var isFocused: Boolean
     var innerProps: HTMLAttributes<HTMLDivElement>
     var selectProps: SelectProps<Option, Group>
