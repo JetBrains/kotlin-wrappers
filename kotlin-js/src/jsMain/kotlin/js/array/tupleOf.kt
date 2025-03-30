6377a4b07eb8413d04e4b91342057ab4927224b6
Victor Turansky <victor.turansky@gmail.com>
2025-03-30 18:13:47 +0300

    JS. Common `tupleOf` implementation

--- kotlin-js/src/jsMain/kotlin/js/array/tupleOf.kt
+++ kotlin-js/src/jsMain/kotlin/js/array/tupleOf.kt
@@ -1,27 +1,28 @@
 package js.array
 
+import js.core.JsAny
 import js.reflect.unsafeCast
 
 inline fun emptyTuple(): JsTuple =
-    unsafeCast(emptyArray<Any?>())
+    unsafeCast(jsArrayOf<JsAny?>())
 
 inline fun <A> tupleOf(
     first: A,
 ): JsTuple1<A> =
-    unsafeCast(arrayOf(first))
+    unsafeCast(jsArrayOf(first))
 
 inline fun <A, B> tupleOf(
     first: A,
     second: B,
 ): JsTuple2<A, B> =
-    unsafeCast(arrayOf(first, second))
+    unsafeCast(jsArrayOf(first, second))
 
 inline fun <A, B, C> tupleOf(
     first: A,
     second: B,
     third: C,
 ): JsTuple3<A, B, C> =
-    unsafeCast(arrayOf(first, second, third))
+    unsafeCast(jsArrayOf(first, second, third))
 
 inline fun <A, B, C, D> tupleOf(
     first: A,
@@ -29,7 +30,7 @@ inline fun <A, B, C, D> tupleOf(
     third: C,
     fourth: D,
 ): JsTuple4<A, B, C, D> =
-    unsafeCast(arrayOf(first, second, third, fourth))
+    unsafeCast(jsArrayOf(first, second, third, fourth))
 
 inline fun <A, B, C, D, E> tupleOf(
     first: A,
@@ -38,7 +39,7 @@ inline fun <A, B, C, D, E> tupleOf(
     fourth: D,
     fifth: E,
 ): JsTuple5<A, B, C, D, E> =
-    unsafeCast(arrayOf(first, second, third, fourth, fifth))
+    unsafeCast(jsArrayOf(first, second, third, fourth, fifth))
 
 inline fun <A, B, C, D, E, F> tupleOf(
     first: A,
@@ -48,7 +49,7 @@ inline fun <A, B, C, D, E, F> tupleOf(
     fifth: E,
     sixth: F,
 ): JsTuple6<A, B, C, D, E, F> =
-    unsafeCast(arrayOf(first, second, third, fourth, fifth, sixth))
+    unsafeCast(jsArrayOf(first, second, third, fourth, fifth, sixth))
 
 inline fun <A, B, C, D, E, F, G> tupleOf(
     first: A,
@@ -59,4 +60,4 @@ inline fun <A, B, C, D, E, F, G> tupleOf(
     sixth: F,
     seventh: G,
 ): JsTuple7<A, B, C, D, E, F, G> =
-    unsafeCast(arrayOf(first, second, third, fourth, fifth, sixth, seventh))
+    unsafeCast(jsArrayOf(first, second, third, fourth, fifth, sixth, seventh))
