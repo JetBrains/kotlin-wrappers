f56622e77a8479102665a6e896c095125378f770
Victor Turansky <victor.turansky@gmail.com>
2025-03-31 03:58:42 +0300

    Ajv. Preparing for WASM target

--- kotlin-ajv/src/commonMain/kotlin/ajv/Ajv.kt
+++ kotlin-ajv/src/commonMain/kotlin/ajv/Ajv.kt
@@ -3,10 +3,13 @@
 package ajv
 
 import js.core.JsAny
+import js.core.JsString
+import js.import.JsModule
 import js.objects.ReadonlyRecord
+import kotlin.js.definedExternally
 
 external class Ajv(
     options: Options = definedExternally,
 ) {
-    fun compile(schema: ReadonlyRecord<String, JsAny?>): ValidateFunction
+    fun compile(schema: ReadonlyRecord<JsString, JsAny?>): ValidateFunction
 }
