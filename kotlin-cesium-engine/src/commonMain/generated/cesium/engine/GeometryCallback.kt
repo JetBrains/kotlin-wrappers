e9341ca4015ed4bea5aa87b2b153991fb2ad36e9
Victor Turansky <victor.turansky@gmail.com>
2025-04-02 01:20:35 +0300

    Cesium. Strict `GeometryCallback` return type [generated]

--- kotlin-cesium-engine/src/commonMain/generated/cesium/engine/GeometryCallback.kt
+++ kotlin-cesium-engine/src/commonMain/generated/cesium/engine/GeometryCallback.kt
@@ -2,10 +2,13 @@
 
 package cesium.engine
 
+import js.promise.PromiseResult
+import js.typedarrays.TypedArray
+
 /**
  * @param [x] The X coordinate of the tile for which to request geometry.
  * @param [y] The Y coordinate of the tile for which to request geometry.
  * @param [level] The level of the tile for which to request geometry.
  * @see <a href="https://cesium.com/docs/cesiumjs-ref-doc/CustomHeightmapTerrainProvider.html#.GeometryCallback">Online Documentation</a>
  */
-typealias GeometryCallback = (x: Double, y: Double, level: Double) -> Any? /* Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | number[] | Promise<Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | number[]> | undefined */
+typealias GeometryCallback = (x: Double, y: Double, level: Double) -> PromiseResult<TypedArray<*, *, *, *>> /* Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | number[] | Promise<Int8Array | Uint8Array | Int16Array | Uint16Array | Int32Array | Uint32Array | Float32Array | Float64Array | number[]> | undefined */
