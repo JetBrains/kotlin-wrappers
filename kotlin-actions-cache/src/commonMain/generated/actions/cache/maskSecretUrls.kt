b3999387209476706fb75e1d10cc231d67fd27e1
Victor Turansky <victor.turansky@gmail.com>
2025-04-08 00:15:36 +0300

    Actions. Cache `4.0.3` [generated]

---
+++ kotlin-actions-cache/src/commonMain/generated/actions/cache/maskSecretUrls.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@actions/cache")
+
+package actions.cache
+
+import js.objects.ReadonlyRecord
+
+external fun maskSecretUrls(body: ReadonlyRecord<String, Any?>?)
