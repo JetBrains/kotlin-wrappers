73464434478029665fa4aa313188b4136ece5bfb
Victor Turansky <victor.turansky@gmail.com>
2025-04-10 01:08:38 +0300

    Observable. `Observable` methods with predicate (#2776)

--- kotlin-web/src/commonMain/kotlin/web/observable/Observable.kt
+++ kotlin-web/src/commonMain/kotlin/web/observable/Observable.kt
@@ -2,6 +2,7 @@ package web.observable
 
 import js.array.ReadonlyArray
 import js.core.JsAny
+import js.core.JsBoolean
 import js.iterable.AsyncIterable
 import js.iterable.JsIterable
 import js.promise.Promise
@@ -11,19 +12,38 @@ external class Observable<T : JsAny?>(
     callback: SubscribeCallback<T>,
 ) {
     // fun catch(CatchCallback callback) : Observable
+
     fun drop(n: Int): Observable<T>
-    // fun every(Predicate predicate, options: SubscribeOptions? = definedExternally) : Promise<boolean>
-    // fun filter(Predicate predicate) : Observable
+
+    fun every(
+        predicate: (T) -> Boolean,
+        options: SubscribeOptions? = definedExternally,
+    ): Promise<JsBoolean>
+
+    fun filter(predicate: (T) -> Boolean): Observable<T>
+
     fun finally(callback: () -> Unit): Observable<T>
-    // fun find(Predicate predicate, options: SubscribeOptions? = definedExternally) : Promise<any>
+
+    // fun find(predicate: (T) -> Boolean, options: SubscribeOptions? = definedExternally) : Promise<any>
+
     fun first(options: SubscribeOptions? = definedExternally): Promise<T>
+
     // fun flatMap(Mapper mapper) : Observable
+
     // fun forEach(Visitor callback, options: SubscribeOptions? = definedExternally) : Promise<undefined>
+
     // fun inspect(optional ObservableInspectorUnion inspectorUnion = {}) : Observable
+
     fun last(options: SubscribeOptions? = definedExternally): Promise<T>
+
     // fun map(Mapper mapper) : Observable
+
     // fun reduce(Reducer reducer, optional any initialValue, options: SubscribeOptions? = definedExternally) : Promise<any>
-    // fun some(Predicate predicate, options: SubscribeOptions? = definedExternally) : Promise<boolean>
+
+    fun some(
+        predicate: (T) -> Boolean,
+        options: SubscribeOptions? = definedExternally,
+    ): Promise<JsBoolean>
 
     /**
      * Subscribes to the sequence with an observer
@@ -42,8 +62,11 @@ external class Observable<T : JsAny?>(
     )
 
     // fun switchMap(Mapper mapper) : Observable
+
     fun take(n: Int): Observable<T>
+
     // fun takeUntil(any value) : Observable
+
     fun toArray(options: SubscribeOptions? = definedExternally): Promise<ReadonlyArray<T>>
 
     companion object {
