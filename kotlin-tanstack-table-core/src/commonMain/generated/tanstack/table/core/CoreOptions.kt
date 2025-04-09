79f68a56c19a117b10ec15d3f17631ab565977f7
Victor Turansky <victor.turansky@gmail.com>
2025-04-09 18:08:26 +0300

    TanStack Table. Fix `CoreOptions` declaration

--- kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/CoreOptions.kt
+++ kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/CoreOptions.kt
@@ -128,4 +128,26 @@ external interface CoreOptions<TData : RowData> {
      * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
      */
     val meta: TableMeta?
+
+    /**
+     * The `onStateChange` option can be used to optionally listen to state changes within the table.
+     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#onstatechange)
+     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
+     */
+    val onStateChange: ((updater: Updater<TableState>) -> Unit)?
+
+    /**
+     * Value used when the desired value is not found in the data.
+     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#renderfallbackvalue)
+     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
+     */
+    val renderFallbackValue: Any?
+
+    /**
+     * The `state` option can be used to optionally _control_ part or all of the table state. The state you pass here will merge with and overwrite the internal automatically-managed state to produce the final state for the table. You can also listen to state changes via the `onStateChange` option.
+     * > Note: Any state passed in here will override both the internal state and any other `initialState` you provide.
+     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#state)
+     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
+     */
+    val state: TableState? /* Partial */
 }
--- kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/CoreOptionsResolved.kt
+++ kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/CoreOptionsResolved.kt
@@ -7,25 +7,7 @@ import js.objects.JsPlainObject
 @JsPlainObject
 external interface CoreOptionsResolved<TData : RowData> :
     CoreOptions<TData> {
-    /**
-     * The `onStateChange` option can be used to optionally listen to state changes within the table.
-     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#onstatechange)
-     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
-     */
-    val onStateChange: (updater: Updater<TableState>) -> Unit
-
-    /**
-     * Value used when the desired value is not found in the data.
-     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#renderfallbackvalue)
-     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
-     */
-    val renderFallbackValue: Any?
-
-    /**
-     * The `state` option can be used to optionally _control_ part or all of the table state. The state you pass here will merge with and overwrite the internal automatically-managed state to produce the final state for the table. You can also listen to state changes via the `onStateChange` option.
-     * > Note: Any state passed in here will override both the internal state and any other `initialState` you provide.
-     * [API Docs](https://tanstack.com/table/v8/docs/api/core/table#state)
-     * [Guide](https://tanstack.com/table/v8/docs/guide/tables)
-     */
-    val state: TableState /* Partial */
+    override val onStateChange: (updater: Updater<TableState>) -> Unit
+    override val renderFallbackValue: Any?
+    override val state: TableState /* Partial */
 }
