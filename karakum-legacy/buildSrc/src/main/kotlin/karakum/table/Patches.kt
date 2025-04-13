a7c6e1f875c315d9decef7d0ea885424e7b4a52c
Victor Turansky <victor.turansky@gmail.com>
2025-04-14 01:51:57 +0300

    TanStack Table. Update `CoreOptionsResolved` generation

--- karakum-legacy/buildSrc/src/main/kotlin/karakum/table/Patches.kt
+++ karakum-legacy/buildSrc/src/main/kotlin/karakum/table/Patches.kt
@@ -1,13 +1,28 @@
 package karakum.table
 
 internal fun String.applyPatches(): String {
-    return replace(
-        "\n    meta?: TableMeta<TData>;",
-        "\n    meta?: TableMeta<TData>;" +
-                "\n}" +
+    return let {
+        val source = it.substringAfter("\n    meta?: TableMeta<TData>;")
+            .substringBefore("\n}")
+
+        val replacement = source
+            .replaceFirst(
+                "onStateChange: (updater: Updater<TableState>) => void;",
+                "onStateChange?: (updater: Updater<TableState>) => void;",
+            )
+            .replaceFirst(
+                "state: Partial<TableState>;",
+                "state?: Partial<TableState>;",
+            )
+
+        it.replaceFirst(source, replacement) +
                 "\n\n" +
-                "export interface CoreOptionsResolved<TData extends RowData> extends CoreOptions<TData> {",
-    )
+                "export interface CoreOptionsResolved<TData extends RowData> extends CoreOptions<TData> {\n" +
+                source.splitToSequence("\n")
+                    .filter { it.endsWith(";") }
+                    .joinToString("\n") +
+                "\n}\n"
+    }
         .let {
             val replacement = it
                 .substringAfter("TableOptionsResolved<TData extends RowData> extends ")
