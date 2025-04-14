6c0e51c368add69e3615ae98e50ad250b3281b9f
Victor Turansky <victor.turansky@gmail.com>
2025-04-15 00:32:43 +0300

    TanStack Table `8.21.3` [generated]

---
+++ kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/safelyAccessDocument.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@tanstack/table-core")
+
+package tanstack.table.core
+
+import web.dom.Document
+
+external fun safelyAccessDocument(_document: Document? = definedExternally): Document?
---
+++ kotlin-tanstack-table-core/src/commonMain/generated/tanstack/table/core/safelyAccessDocumentEvent.kt
@@ -0,0 +1,10 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@tanstack/table-core")
+
+package tanstack.table.core
+
+import web.dom.Document
+import web.events.Event
+
+external fun safelyAccessDocumentEvent(event: Event): Document?
