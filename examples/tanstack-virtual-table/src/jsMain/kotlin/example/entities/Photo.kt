94d30fbae543719a587d6964c1ca6eccb4733a6b
Victor Turansky <victor.turansky@gmail.com>
2025-04-21 01:13:18 +0300

    Examples. Immutable `Photo`

--- examples/tanstack-virtual-table/src/jsMain/kotlin/example/entities/Photo.kt
+++ examples/tanstack-virtual-table/src/jsMain/kotlin/example/entities/Photo.kt
@@ -1,13 +1,15 @@
 package wrappers.example.entities
 
 import js.array.ReadonlyArray
+import js.objects.JsPlainObject
 
+@JsPlainObject
 external interface Photo {
-    var id: Key
-    var albumId: Key
-    var title: String
-    var url: String
-    var thumbnailUrl: String
+    val id: Key
+    val albumId: Key
+    val title: String
+    val url: String
+    val thumbnailUrl: String
 }
 
 typealias PhotoList = ReadonlyArray<Photo>
