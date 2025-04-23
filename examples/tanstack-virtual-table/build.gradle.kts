9688046788ff83ee13fd27ab31b42743208f1558
porotkin <preload737773@gmail.com>
2025-04-23 15:35:33 +0300

    Examples. `virtual-table` Add virtual dependencies

--- examples/tanstack-virtual-table/build.gradle.kts
+++ examples/tanstack-virtual-table/build.gradle.kts
@@ -8,10 +8,16 @@ dependencies {
     jsMainImplementation(kotlinWrappers.reactUse)
     jsMainImplementation(kotlinWrappers.emotion.react)
     jsMainImplementation(kotlinWrappers.emotion.styled)
+
     jsMainImplementation(kotlinWrappers.tanstack.queryCore)
     jsMainImplementation(kotlinWrappers.tanstack.reactQuery)
+
     jsMainImplementation(kotlinWrappers.tanstack.tableCore)
     jsMainImplementation(kotlinWrappers.tanstack.reactTable)
+
+    jsMainImplementation(kotlinWrappers.tanstack.virtualCore)
+    jsMainImplementation(kotlinWrappers.tanstack.reactVirtual)
+
     jsMainImplementation(kotlinWrappers.preact.signalsCore)
     jsMainImplementation(kotlinWrappers.preact.signalsReact)
 }
