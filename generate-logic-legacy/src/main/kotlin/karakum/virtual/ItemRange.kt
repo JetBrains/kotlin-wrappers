f5faf9ce4339b5fbd343ed36b6820ac111779bac
porotkin <preload737773@gmail.com>
2025-05-05 19:59:26 +0300

    TanStack Virtual. Add language injection prefix for `ItemRange`

--- generate-logic-legacy/src/main/kotlin/karakum/virtual/ItemRange.kt
+++ generate-logic-legacy/src/main/kotlin/karakum/virtual/ItemRange.kt
@@ -2,7 +2,7 @@ package karakum.virtual
 
 internal const val ITEM_RANGE = "ItemRange"
 
-// language=typescript
+// language=typescript prefix="type ItemRange ="
 internal const val ITEM_RANGE_BODY = """{
     startIndex: number;
     endIndex: number;
