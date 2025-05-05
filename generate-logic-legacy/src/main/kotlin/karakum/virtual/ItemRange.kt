ba7a6bfcfec10b39d5550355c9a1e0bdd49f491a
porotkin <preload737773@gmail.com>
2025-05-05 19:14:24 +0300

    TanStack Virtual. Fix generator

--- generate-logic-legacy/src/main/kotlin/karakum/virtual/ItemRange.kt
+++ generate-logic-legacy/src/main/kotlin/karakum/virtual/ItemRange.kt
@@ -4,6 +4,6 @@ internal const val ITEM_RANGE = "ItemRange"
 
 // language=typescript
 internal const val ITEM_RANGE_BODY = """{
-    number;
-    number;
+    startIndex: number;
+    endIndex: number;
 }"""
