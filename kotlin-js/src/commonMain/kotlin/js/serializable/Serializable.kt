ba31a5e1bd2aea8ced83d7e9b153ca7759ca352d
Victor Turansky <victor.turansky@gmail.com>
2025-05-06 01:03:48 +0200

    Serializable. Add MDN reference

--- kotlin-js/src/commonMain/kotlin/js/serializable/Serializable.kt
+++ kotlin-js/src/commonMain/kotlin/js/serializable/Serializable.kt
@@ -1,3 +1,6 @@
 package js.serializable
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#supported_types)
+ */
 external interface Serializable
