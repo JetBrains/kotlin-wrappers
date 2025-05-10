50cd897c40656682b497786950553d9c5cbc6b45
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-05-10 22:21:55 +0300

    Karakum. Simplify Gradle configuration after migration to new plugin

--- karakum/buildSrc/src/main/kotlin/node-conventions.gradle.kts
+++
@@ -1,7 +0,0 @@
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
-
-plugins.withType<NodeJsPlugin> {
-    the<NodeJsEnvSpec>().version.set("22.14.0")
-}
--- karakum/settings.gradle.kts
+++ karakum/settings.gradle.kts
@@ -11,12 +11,6 @@ dependencyResolutionManagement {
             plugin("karakum", "io.github.sgrishchenko.karakum").version(karakumVersion)
             library("karakum", "io.github.sgrishchenko", "karakum").version(karakumVersion)
 
-            val arrowVersion = extra["arrow-kt.version"] as String
-            library("arrow-core", "io.arrow-kt", "arrow-core").version(arrowVersion)
-
-            val kotlinVersion = extra["kotlin.version"] as String
-            library("kotlin-test", "org.jetbrains.kotlin", "kotlin-test").version(kotlinVersion)
-
             val electronVersion = extra["electron.version"] as String
             library("electron", "npm", "electron").version(electronVersion)
 
