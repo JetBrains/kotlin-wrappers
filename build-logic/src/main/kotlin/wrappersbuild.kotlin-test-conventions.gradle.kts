ad76b3b168e5c1600a3e434bd2c22e1361c03481
porotkin <preload737773@gmail.com>
2025-05-15 15:52:50 +0300

    Wrappers Build. `kotlin-test-conventions`: Naming

--- build-logic/src/main/kotlin/wrappersbuild.kotlin-test-conventions.gradle.kts
+++ build-logic/src/main/kotlin/wrappersbuild.kotlin-test-conventions.gradle.kts
@@ -8,12 +8,12 @@ val TEST_SOURCE_SETS = listOf(
 afterEvaluate {
     val kotlin = project.extensions.getByName<KotlinProjectExtension>("kotlin")
 
-    val hasSupportedTestSourceSet = TEST_SOURCE_SETS
+    val hasTestSources = TEST_SOURCE_SETS
         .mapNotNull { name -> kotlin.sourceSets.findByName(name) }
         .flatMap { it.kotlin.sourceDirectories }
         .any { it.exists() }
 
     tasks.named("jsTestPackageJson") {
-        enabled = hasSupportedTestSourceSet
+        enabled = hasTestSources
     }
 }
