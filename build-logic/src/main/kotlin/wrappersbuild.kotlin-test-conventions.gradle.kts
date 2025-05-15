495b1af4e7f79622c3f9cded57a22ba10e25cf42
porotkin <preload737773@gmail.com>
2025-05-15 13:19:09 +0300

    Wrappers Build. Avoid using `project.extensions` in task confuguration

--- build-logic/src/main/kotlin/wrappersbuild.kotlin-test-conventions.gradle.kts
+++ build-logic/src/main/kotlin/wrappersbuild.kotlin-test-conventions.gradle.kts
@@ -1,19 +1,23 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
-val TEST_SOURCE_SETS = listOf(
+private val TEST_SOURCE_SETS = listOf(
     "jsTest",
     "commonTest",
 )
 
-afterEvaluate {
-    tasks.named("jsTestPackageJson") {
-        onlyIf {
-            val kotlin = project.extensions.getByName<KotlinProjectExtension>("kotlin")
+private val JS_TEST_TASK_NAME = "jsTestPackageJson"
 
-            TEST_SOURCE_SETS.asSequence()
-                .map { kotlin.sourceSets.getByName(it) }
-                .flatMap { it.kotlin.sourceDirectories }
-                .any { it.exists() }
+afterEvaluate {
+    val kotlin = project.extensions.getByName<KotlinProjectExtension>("kotlin")
+
+    val hasSupportedTestSourceSet = TEST_SOURCE_SETS
+        .mapNotNull { name -> kotlin.sourceSets.findByName(name) }
+        .flatMap { it.kotlin.sourceDirectories }
+        .any { it.exists() }
+
+    tasks.named(JS_TEST_TASK_NAME) {
+        onlyIf {
+            hasSupportedTestSourceSet
         }
     }
 }
