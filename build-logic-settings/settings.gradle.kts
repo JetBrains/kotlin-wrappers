071236afc7bca4dfeefbe1b5d2c0971aef4c8ba4
Victor Turansky <victor.turansky@gmail.com>
2025-06-08 14:06:24 +0200

    Settings. Add subproject for common logic

--- build-logic-settings/settings.gradle.kts
+++ build-logic-settings/settings.gradle.kts
@@ -1,10 +1,13 @@
 rootProject.name = "build-logic-settings"
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
     }
 }
 
+include("common")
 include("generatorbuild")
 include("wrappersbuild")
--- build-logic-settings/wrappersbuild/build.gradle.kts
+++ build-logic-settings/wrappersbuild/build.gradle.kts
@@ -1,3 +1,7 @@
 plugins {
     `kotlin-dsl`
 }
+
+dependencies {
+    implementation(projects.common)
+}
