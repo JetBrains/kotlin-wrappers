05c958cf4caaeaf076efa60ffedee47f9e766f2c
Victor Turansky <victor.turansky@gmail.com>
2025-06-08 16:51:11 +0200

    Build. Add common `commonbuild` subroject

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,11 +1,14 @@
 rootProject.name = "build-logic"
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 dependencyResolutionManagement {
     repositories {
         gradlePluginPortal()
     }
 }
 
+include("commonbuild")
 include("generatorbuild")
 include("generatorlegacybuild")
 include("wrappersbuild")
