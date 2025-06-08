15428d9523e236be0b30500445c6eec67f67a2c1
Victor Turansky <victor.turansky@gmail.com>
2025-06-08 04:41:20 +0200

    Version catalog. Create `jspkg` catalog in plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,36 +31,6 @@ dependencyResolutionManagement {
             val htmlVersion = extra["kotlinx-html.version"] as String
             library("kotlinx-html", "org.jetbrains.kotlinx", "kotlinx-html").version(htmlVersion)
         }
-
-        create("jspkg") {
-            npmLibraries()
-        }
-    }
-}
-
-fun VersionCatalogBuilder.npmLibraries() {
-    val lines = file("gradle.properties").readLines()
-
-    for ((commentLine, versionLine) in lines.windowed(2)) {
-        val version = versionLine
-            .substringAfter(".npm.version=", "")
-            .ifEmpty { null }
-            ?: continue
-
-        val packageName = commentLine
-            .removePrefix("# https://www.npmjs.com/package/")
-            .takeIf { it != commentLine }
-            ?: continue
-
-        val packageAlias = packageName
-            .removePrefix("@")
-            .replace(
-                regex = Regex("""-(\w)"""),
-                transform = { it.groupValues[1].uppercase() }
-            )
-            .replace("/", "-")
-
-        library(packageAlias, "npm", packageName).version(version)
     }
 }
 
