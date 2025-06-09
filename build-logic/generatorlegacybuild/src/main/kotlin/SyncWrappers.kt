0ede96563f157b69c6e85544403b0842d74c6ed2
Victor Turansky <victor.turansky@gmail.com>
2025-06-09 14:35:30 +0200

    Generator legacy. Restore sync logic

--- build-logic/generatorlegacybuild/src/main/kotlin/SyncWrappers.kt
+++ build-logic/generatorlegacybuild/src/main/kotlin/SyncWrappers.kt
@@ -21,7 +21,7 @@ open class SyncWrappers : Sync() {
         sourceSet: String,
     ): File =
         project.rootProject.layout.projectDirectory
-            .dir(if (project == project.rootProject) "generatorlegacybuild/src" else "..")
+            .dir(if (project == project.rootProject) "../src" else "..")
             .dir(projectName)
             .dir("src/$sourceSet/generated")
             .asFile
