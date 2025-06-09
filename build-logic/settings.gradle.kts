952308f2b97109fc7d082d3506e2d869ddaa2812
Victor Turansky <victor.turansky@gmail.com>
2025-06-09 14:00:26 +0200

    Build. Use version catalog

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,9 +1,33 @@
+import java.util.*
+
 rootProject.name = "build-logic"
 
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
+    }
+}
+
+val props = Properties().apply {
+    rootDir.resolve("../gradle.properties")
+        .inputStream()
+        .use { load(it) }
+}
+
+fun version(target: String): String {
+    val propName = "${target}.version"
+    return props.getProperty(propName)
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("gradlePlugins") {
+            library("kotlin", "org.jetbrains.kotlin", "kotlin-gradle-plugin").version(version("kotlin"))
+            library("jsPlainObjects", "org.jetbrains.kotlin", "js-plain-objects").version(version("kotlin"))
+            library("seskar", "io.github.turansky.seskar", "seskar-gradle-plugin").version(version("seskar"))
+            library("dokka", "org.jetbrains.dokka", "dokka-gradle-plugin").version(version("dokka"))
+        }
     }
 }
 
--- build-logic/wrappersbuild/build.gradle.kts
+++ build-logic/wrappersbuild/build.gradle.kts
@@ -1,22 +1,10 @@
-import java.util.*
-
 plugins {
     `kotlin-dsl`
 }
 
-val props = Properties().apply {
-    rootProject.file("../gradle.properties").inputStream().use { load(it) }
-}
-
-fun version(target: String): String {
-    val propName = "${target}.version"
-    return properties[propName] as? String
-        ?: props.getProperty(propName)
-}
-
 dependencies {
-    implementation(kotlin("gradle-plugin", version("kotlin")))
-    implementation("org.jetbrains.kotlin:js-plain-objects:${version("kotlin")}")
-    implementation("io.github.turansky.seskar:seskar-gradle-plugin:${version("seskar")}")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:${version("dokka")}")
+    implementation(gradlePlugins.kotlin)
+    implementation(gradlePlugins.jsPlainObjects)
+    implementation(gradlePlugins.seskar)
+    implementation(gradlePlugins.dokka)
 }
