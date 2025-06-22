379d7d50d0ffbc5d32fa46b4f316d163bd001ff4
Victor Turansky <victor.turansky@gmail.com>
2025-06-22 05:36:47 +0200

    Examples. Fix compilation

--- examples/browser/src/jsMain/kotlin/example/EventHandler.kt
+++ examples/browser/src/jsMain/kotlin/example/EventHandler.kt
@@ -5,6 +5,7 @@ import web.dom.Document
 import web.dom.document
 import web.events.addEventHandler
 import web.events.addHandler
+import web.uievents.DRAG_OVER
 import web.uievents.DragEvent
 
 private fun preventDefaultExample_handler() {
