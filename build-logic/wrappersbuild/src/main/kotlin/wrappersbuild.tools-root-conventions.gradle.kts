a8d3c9a77e27eab105269e966c2ef04384479177
Victor Turansky <victor.turansky@gmail.com>
2025-06-24 03:32:18 +0200

    Wasm. Hide `package-lock.json`

--- build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.tools-root-conventions.gradle.kts
+++ build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.tools-root-conventions.gradle.kts
@@ -1,28 +1,44 @@
+import org.jetbrains.kotlin.gradle.targets.js.NpmVersions
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
 import org.jetbrains.kotlin.gradle.targets.js.npm.LockFileMismatchReport
 import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.wasm.npm.WasmNpmExtension
 
 plugins {
     id("wrappersbuild.node-conventions")
 }
 
 plugins.withType<NodeJsRootPlugin> {
-    the<NodeJsRootExtension>().apply {
-        versions.apply {
-            webpack.version = "5.99.9" // https://www.npmjs.com/package/webpack
-            webpackCli.version = "6.0.1" // https://www.npmjs.com/package/webpack-cli
-            karma.version = "6.4.4" // https://www.npmjs.com/package/karma
-
-            // synchronize TypeScript version
-            // to simplify converting of kotlin-typescript subproject
-            typescript.version = prop("typescript.npm.version")
-        }
-    }
+    the<NodeJsRootExtension>().versions.configureVersions()
 
     the<NpmExtension>().apply {
         lockFileDirectory = projectDir
         packageLockMismatchReport = LockFileMismatchReport.NONE
-        ignoreScripts = false
     }
+}
+
+plugins.withType<WasmNodeJsRootPlugin> {
+    the<WasmNodeJsRootExtension>().versions.configureVersions()
+
+    the<WasmNpmExtension>().apply {
+        lockFileDirectory = project.layout.buildDirectory.file("wasm-package-lock").get().asFile
+        packageLockMismatchReport = LockFileMismatchReport.NONE
+    }
+}
+
+fun NpmVersions.configureVersions() {
+    // https://www.npmjs.com/package/webpack
+    webpack.version = "5.99.9"
+
+    // https://www.npmjs.com/package/webpack-cli
+    webpackCli.version = "6.0.1"
+
+    // https://www.npmjs.com/package/webpack-dev-server
+    webpackDevServer.version = "5.2.2"
+
+    // https://www.npmjs.com/package/karma
+    karma.version = "6.4.4"
 }
