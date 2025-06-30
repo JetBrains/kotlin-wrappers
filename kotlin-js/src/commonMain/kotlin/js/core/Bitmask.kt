a4e57a49029d097d7bc8989862d33be28623cbc3
Pantus Oleh <53688419+zibet27@users.noreply.github.com>
2025-06-30 19:23:46 +0200

    JS. Commonize `BigInt` and `Bitmask` (#2449) (#2797)

--- kotlin-js/src/commonMain/kotlin/js/core/Bitmask.kt
+++ kotlin-js/src/commonMain/kotlin/js/core/Bitmask.kt
@@ -1,10 +1,17 @@
-@file:Suppress(
-    "EXPECTED_EXTERNAL_DECLARATION",
-)
-
 package js.core
 
+import js.math.unsafeBitwiseAnd
+import js.math.unsafeBitwiseOr
 import js.reflect.JsExternalInheritorsOnly
 
 @JsExternalInheritorsOnly
-expect external interface Bitmask<T : Bitmask<T>>
+external interface Bitmask<T : Bitmask<T>>
+
+inline operator fun <T : Bitmask<T>> T.plus(other: T): T =
+    unsafeBitwiseOr(this, other)
+
+inline operator fun <T : Bitmask<T>> T.contains(other: T): Boolean =
+    unsafeBitwiseAnd(this, other) == other
+
+
+
--- kotlin-js/src/wasmJsMain/kotlin/js/core/Bitmask.kt
+++
@@ -1,6 +0,0 @@
-package js.core
-
-import js.reflect.JsExternalInheritorsOnly
-
-@JsExternalInheritorsOnly
-actual external interface Bitmask<T : Bitmask<T>>
