f0f64622461cd90c6f271b8220b5ae2214f93bcc
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-07-05 16:05:37 +0300

    Karakum. Migrate null-writable to actual Karakum configuration

---
+++ kotlin-node/karakum/src/jsMain/kotlin/node/karakum/util/nullable.kt
@@ -0,0 +1,34 @@
+@file:OptIn(ExperimentalContracts::class)
+
+package node.karakum.util
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.coroutines.cancellation.CancellationException
+
+object RaiseCancellationException : CancellationException()
+
+object Raise {
+    fun raise(): Nothing = throw RaiseCancellationException
+
+    fun ensure(condition: Boolean) {
+        contract { returns() implies condition }
+        return if (condition) Unit else raise()
+    }
+
+    fun <A> ensureNotNull(value: A?): A {
+        contract { returns() implies (value != null) }
+        return value ?: raise()
+    }
+}
+
+
+fun <T> nullable(block: Raise.() -> T): T? {
+    contract { callsInPlace(block, InvocationKind.AT_MOST_ONCE) }
+    return try {
+        block(Raise)
+    } catch (_: RaiseCancellationException) {
+        null
+    }
+}
