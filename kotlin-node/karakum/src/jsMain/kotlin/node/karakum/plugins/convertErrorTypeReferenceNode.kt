0b37a509d8b6e55703d78125e5f6b4fff61d7ca9
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-07-05 16:06:27 +0300

    Revert "Karakum. Migrate null-writable to actual Karakum configuration"
    This reverts commit f0f64622461cd90c6f271b8220b5ae2214f93bcc.

--- kotlin-node/karakum/src/jsMain/kotlin/node/karakum/plugins/convertErrorTypeReferenceNode.kt
+++ kotlin-node/karakum/src/jsMain/kotlin/node/karakum/plugins/convertErrorTypeReferenceNode.kt
@@ -1,6 +1,6 @@
 package node.karakum.plugins
 
-import node.karakum.util.nullable
+import arrow.core.raise.nullable
 import io.github.sgrishchenko.karakum.extension.createPlugin
 import typescript.isExpressionWithTypeArguments
 import typescript.isIdentifier
