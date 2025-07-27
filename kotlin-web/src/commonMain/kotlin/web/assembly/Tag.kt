1c3d2e1cbaa976f78b4e10474b3e5751b247db0a
Victor Turansky <victor.turansky@gmail.com>
2025-07-27 15:31:44 +0200

    WebAssembly. `Tag`

---
+++ kotlin-web/src/commonMain/kotlin/web/assembly/Tag.kt
@@ -0,0 +1,12 @@
+@file:JsQualifier("WebAssembly")
+
+package web.assembly
+
+import kotlin.js.JsQualifier
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/WebAssembly/Reference/JavaScript_interface/Tag)
+ */
+open external class Tag(
+    type: TagType,
+)
---
+++ kotlin-web/src/commonMain/kotlin/web/assembly/TagType.kt
@@ -0,0 +1,9 @@
+package web.assembly
+
+import js.array.ReadonlyArray
+import js.objects.JsPlainObject
+
+@JsPlainObject
+external interface TagType {
+    var parameters: ReadonlyArray<ValueType<*>>
+}
