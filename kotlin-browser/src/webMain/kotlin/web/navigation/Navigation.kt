3131adaf89f55ca1a27017f7c4701071b9562a6f
Victor Turansky <victor.turansky@gmail.com>
2025-08-04 02:06:17 +0200

    Navigation. Events

--- kotlin-browser/src/webMain/kotlin/web/navigation/Navigation.kt
+++ kotlin-browser/src/webMain/kotlin/web/navigation/Navigation.kt
@@ -4,6 +4,7 @@ import js.iterable.JsIterator
 import web.errors.ErrorEvent
 import web.events.Event
 import web.events.EventHandler
+import web.events.EventInstance
 import web.events.EventTarget
 import kotlin.js.definedExternally
 
@@ -99,3 +100,27 @@ private constructor() :
      */
     var oncurrententrychange: EventHandler<NavigationCurrentEntryChangeEvent, Navigation, Navigation>?
 }
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigation/navigate_event)
+ */
+inline val <C : Navigation> C.navigate: EventInstance<NavigateEvent, Navigation, Navigation>?
+    get() = EventInstance(this, "navigate")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigation/navigatesuccess_event)
+ */
+inline val <C : Navigation> C.navigateSuccess: EventInstance<Event, Navigation, Navigation>?
+    get() = EventInstance(this, "navigatesuccess")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigation/navigateerror_event)
+ */
+inline val <C : Navigation> C.navigateError: EventInstance<ErrorEvent, Navigation, Navigation>?
+    get() = EventInstance(this, "navigateerror")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigation/currententrychange_event)
+ */
+inline val <C : Navigation> C.currentEntryChange: EventInstance<NavigationCurrentEntryChangeEvent, Navigation, Navigation>?
+    get() = EventInstance(this, "currententrychange")
