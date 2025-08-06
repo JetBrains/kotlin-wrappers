3c59075e3cd449015dbd515cffdc40409edb1753
Victor Turansky <victor.turansky@gmail.com>
2025-08-06 03:19:34 +0200

    Typed Arrays. Optimize converting methods (#2809)

--- kotlin-js/src/webMain/kotlin/js/typedarrays/TypedArray.kt
+++ kotlin-js/src/webMain/kotlin/js/typedarrays/TypedArray.kt
@@ -365,17 +365,27 @@ sealed external class TypedArray<
     override fun values(): JsIterator<T>
 }
 
+private fun <A : TypedArray<*, *, *, *>> A.copyOfBuffer(): ArrayBufferLike =
+    buffer.slice(
+        begin = byteOffset,
+        end = byteOffset + byteLength,
+    )
+
 fun <A : TypedArray<*, *, *, *>> A.toInt8Array(): Int8Array<ArrayBuffer> {
-    val sourceBuffer = buffer.slice(begin = byteOffset, end = byteOffset + byteLength)
-    return when (sourceBuffer) {
+    if (this is Int8Array<*>)
+        return Int8Array(this)
+
+    return when (val sourceBuffer = copyOfBuffer()) {
         is ArrayBuffer -> Int8Array(sourceBuffer)
         else -> Int8Array(sourceBuffer).slice()
     }
 }
 
 fun <A : TypedArray<*, *, *, *>> A.toUint8Array(): Uint8Array<ArrayBuffer> {
-    val sourceBuffer = buffer.slice(begin = byteOffset, end = byteOffset + byteLength)
-    return when (sourceBuffer) {
+    if (this is Uint8Array<*>)
+        return Uint8Array(this)
+
+    return when (val sourceBuffer = copyOfBuffer()) {
         is ArrayBuffer -> Uint8Array(sourceBuffer)
         else -> Uint8Array(sourceBuffer).slice()
     }
