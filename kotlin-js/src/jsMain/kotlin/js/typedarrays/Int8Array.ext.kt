76cb4feb709b4a0d7907cd2b423d6343f84cd41f
Victor Turansky <victor.turansky@gmail.com>
2025-08-06 14:52:53 +0200

    Typed Arrays. Common `Int8Array.asByteArray()` extension (#2809)

--- kotlin-js/src/jsMain/kotlin/js/typedarrays/Int8Array.ext.kt
+++ kotlin-js/src/jsMain/kotlin/js/typedarrays/Int8Array.ext.kt
@@ -2,7 +2,7 @@ package js.typedarrays
 
 import js.reflect.unsafeCast
 
-inline fun Int8Array<*>.asByteArray(): ByteArray =
+actual inline fun Int8Array<*>.asByteArray(): ByteArray =
     unsafeCast(this)
 
 actual inline fun Int8Array<*>.toByteArray(): ByteArray =
--- kotlin-js/src/wasmJsMain/kotlin/js/typedarrays/Int8Array.ext.kt
+++ kotlin-js/src/wasmJsMain/kotlin/js/typedarrays/Int8Array.ext.kt
@@ -2,6 +2,9 @@ package js.typedarrays
 
 import js.core.JsPrimitives.toByte
 
+actual fun Int8Array<*>.asByteArray(): ByteArray =
+    toByteArray()
+
 actual fun Int8Array<*>.toByteArray(): ByteArray =
     ByteArray(size = length) { index ->
         get(index).toByte()
