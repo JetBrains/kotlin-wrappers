22e039d6f0d6ff5fc3d3a5ac6fb228762bff19a9
Victor Turansky <victor.turansky@gmail.com>
2025-08-08 02:19:08 +0200

    Examples. Remove internal API usages

--- examples/tanstack-react-table/src/jsMain/kotlin/example/query/createMutationFunction.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/example/query/createMutationFunction.kt
@@ -1,13 +1,14 @@
 package example.query
 
-import js.coroutines.internal.IsolatedCoroutineScope
 import js.coroutines.promise
+import kotlinx.coroutines.CoroutineScope
 import tanstack.query.core.MutationFunction
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun <D, V> createMutationFunction(
     action: suspend (V) -> D,
 ): MutationFunction<D, V> =
     { variables ->
-        IsolatedCoroutineScope()
+        CoroutineScope(EmptyCoroutineContext)
             .promise { action(variables) }
     }
--- examples/tanstack-react-table/src/jsMain/kotlin/example/query/createQueryFunction.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/example/query/createQueryFunction.kt
@@ -1,18 +1,19 @@
 package example.query
 
-import js.coroutines.internal.IsolatedCoroutineScope
 import js.coroutines.promise
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.cancel
 import tanstack.query.core.QueryFunction
 import tanstack.query.core.QueryKey
 import web.abort.abortEvent
 import web.events.addHandler
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun <D> createQueryFunction(
     block: suspend () -> D,
 ): QueryFunction<D, QueryKey, Nothing> =
     QueryFunction { context ->
-        val scope = IsolatedCoroutineScope()
+        val scope = CoroutineScope(EmptyCoroutineContext)
 
         context.signal.abortEvent.addHandler {
             scope.cancel()
