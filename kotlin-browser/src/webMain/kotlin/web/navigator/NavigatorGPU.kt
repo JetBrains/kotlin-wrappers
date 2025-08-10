b14c0442c1822e167c19a4c247eb61681c33d6b6
Victor Turansky <victor.turansky@gmail.com>
2025-08-10 23:21:08 +0200

    Navigator. Update `NavigatorGPU`

--- kotlin-browser/src/webMain/kotlin/web/navigator/NavigatorGPU.kt
+++ kotlin-browser/src/webMain/kotlin/web/navigator/NavigatorGPU.kt
@@ -1,12 +1,21 @@
+@file:Suppress(
+    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
+)
+
 package web.navigator
 
+import js.reflect.JsExternalInheritorsOnly
 import web.gpu.GPU
+import kotlin.js.definedExternally
 
-sealed external interface NavigatorGPU {
+/* mixin */
+@JsExternalInheritorsOnly
+external interface NavigatorGPU {
     /**
      * Available only in secure contexts.
      *
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Navigator/gpu)
      */
     val gpu: GPU
+        get() = definedExternally
 }
