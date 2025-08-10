81657c4ba325c6660f868e5173ae326bb7f20a59
Victor Turansky <victor.turansky@gmail.com>
2025-08-10 13:25:05 +0200

    Audio. Add `AudioWorkletModule` factory function (#2811)

--- kotlin-browser/src/webMain/kotlin/web/audio/AudioWorkletModule.kt
+++ kotlin-browser/src/webMain/kotlin/web/audio/AudioWorkletModule.kt
@@ -1,6 +1,15 @@
 package web.audio
 
 import web.worklets.WorkletModule
+import web.worklets.internal.createWorkletModule
 
 external interface AudioWorkletModule :
     WorkletModule
+
+fun AudioWorkletModule(
+    block: (self: AudioWorkletGlobalScope) -> Unit,
+): AudioWorkletModule =
+    createWorkletModule(
+        scopeClassName = "AudioWorkletGlobalScope",
+        block = block,
+    )
