74bbc56c187be46a95ab217e0fa16d028db305aa
Victor Turansky <victor.turansky@gmail.com>
2025-08-10 15:52:29 +0200

    WebGPU. Update `GPUError` [generated]

--- kotlin-web/src/webMain/generated/web/gpu/GPUError.kt
+++ kotlin-web/src/webMain/generated/web/gpu/GPUError.kt
@@ -1,13 +1,8 @@
 // Automatically generated - do not modify!
 
-@file:Suppress(
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-)
-
 package web.gpu
 
 import js.errors.JsErrorLike
-import kotlin.js.definedExternally
 
 /**
  * The **`GPUError`** interface of the WebGPU API is the base interface for errors surfaced by GPUDevice.popErrorScope and the GPUDevice.uncapturederror_event event.
@@ -24,5 +19,4 @@ private constructor() :
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/GPUError/message)
      */
     val message: String
-        get() = definedExternally
 }
