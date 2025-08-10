339a499899fa85328b93845f61a20a9bebe6a222
Victor Turansky <victor.turansky@gmail.com>
2025-08-10 13:29:08 +0200

    CSS Painting. Add `PaintWorkletModule` factory function (#2811)

--- kotlin-browser/src/webMain/kotlin/web/css/painting/PaintWorkletModule.kt
+++ kotlin-browser/src/webMain/kotlin/web/css/painting/PaintWorkletModule.kt
@@ -1,6 +1,15 @@
 package web.css.painting
 
 import web.worklets.WorkletModule
+import web.worklets.internal.createWorkletModule
 
 external interface PaintWorkletModule :
     WorkletModule
+
+fun PaintWorkletModule(
+    block: (self: PaintWorkletGlobalScope) -> Unit,
+): PaintWorkletModule =
+    createWorkletModule(
+        scopeClassName = "PaintWorkletGlobalScope",
+        block = block,
+    )
