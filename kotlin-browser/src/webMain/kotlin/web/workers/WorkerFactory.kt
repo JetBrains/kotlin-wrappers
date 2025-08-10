0b993bedaeadffbc2c0fb89222328ed97b26bc09
Victor Turansky <victor.turansky@gmail.com>
2025-08-10 14:40:15 +0200

    Workers. Wasm-compatible `WorkerFactory.invoke`

--- kotlin-browser/src/webMain/kotlin/web/workers/WorkerFactory.kt
+++ kotlin-browser/src/webMain/kotlin/web/workers/WorkerFactory.kt
@@ -1,11 +1,13 @@
 package web.workers
 
+import js.function.unsafeInvoke
 import kotlinx.coroutines.CoroutineScope
 
 sealed /* final */
-external interface WorkerFactory<T : AbstractWorker> {
-    operator fun invoke(): T
-}
+external interface WorkerFactory<T : AbstractWorker>
+
+inline operator fun <T : AbstractWorker> WorkerFactory<T>.invoke(): T =
+    unsafeInvoke(this)
 
 fun WorkerFactory(
     block: suspend CoroutineScope.(self: DedicatedWorkerGlobalScope) -> Unit,
