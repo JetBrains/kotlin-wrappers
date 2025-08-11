cc609021f10729bc61e85d3ea16c3dd9116415bd
Victor Turansky <victor.turansky@gmail.com>
2025-08-12 01:30:01 +0200

    Disposable. `SuspendAutoCloseable.use` (#2806)

--- kotlin-js/src/webMain/kotlin/js/disposable/SuspendAutoCloseable.kt
+++ kotlin-js/src/webMain/kotlin/js/disposable/SuspendAutoCloseable.kt
@@ -1,8 +1,44 @@
 package js.disposable
 
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * Suspend analog of [AutoCloseable]
  */
 fun interface SuspendAutoCloseable {
     suspend fun close()
+}
+
+suspend inline fun <T : SuspendAutoCloseable, R> T.use(
+    block: (T) -> R,
+): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    var exception: Throwable? = null
+    try {
+        return block(this)
+    } catch (e: Throwable) {
+        exception = e
+        throw e
+    } finally {
+        closeFinally(exception)
+    }
+}
+
+@PublishedApi
+internal suspend fun SuspendAutoCloseable.closeFinally(
+    cause: Throwable?,
+) {
+    when {
+        cause == null -> close()
+        else ->
+            try {
+                close()
+            } catch (closeException: Throwable) {
+                cause.addSuppressed(closeException)
+            }
+    }
 }
--- kotlin-js/src/webMain/kotlin/js/disposable/using.kt
+++ kotlin-js/src/webMain/kotlin/js/disposable/using.kt
@@ -5,41 +5,6 @@ import js.symbol.Symbol
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
-@PublishedApi
-internal suspend inline fun <T : AsyncDisposable, R> using(
-    disposable: T,
-    block: T.() -> R,
-): R {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-
-    var exception: Throwable? = null
-    try {
-        return block(disposable)
-    } catch (e: Throwable) {
-        exception = e
-        throw e
-    } finally {
-        disposable.closeFinally(exception)
-    }
-}
-
-@PublishedApi
-internal suspend fun AsyncDisposable.closeFinally(
-    cause: Throwable?,
-) {
-    when {
-        cause == null -> this[Symbol.asyncDispose]().await()
-        else ->
-            try {
-                this[Symbol.asyncDispose]().await()
-            } catch (closeException: Throwable) {
-                cause.addSuppressed(closeException)
-            }
-    }
-}
-
 suspend inline fun <R> using(
     block: AsyncDisposableStack.() -> R,
 ): R {
@@ -47,5 +12,12 @@ suspend inline fun <R> using(
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
     }
 
-    return using(AsyncDisposableStack(), block)
+    val stack = AsyncDisposableStack()
+    val closeable = SuspendAutoCloseable {
+        stack[Symbol.asyncDispose]().await()
+    }
+
+    return closeable.use {
+        block(stack)
+    }
 }
