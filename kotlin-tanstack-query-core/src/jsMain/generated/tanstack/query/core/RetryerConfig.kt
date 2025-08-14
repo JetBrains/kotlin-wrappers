e6af1be078aec96ddebd23e42bafdad2b0120d87
porotkin <preload737773@gmail.com>
2025-08-14 14:50:53 +0300

    TanStack Query `5.85.2` [generated]

--- kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/RetryerConfig.kt
+++ kotlin-tanstack-query-core/src/jsMain/generated/tanstack/query/core/RetryerConfig.kt
@@ -11,8 +11,6 @@ external interface RetryerConfig<TData, TError> {
     val fn: () -> PromiseResult<TData>
     val initialPromise: Promise<TData>?
     val abort: (() -> Unit)?
-    val onError: ((error: TError) -> Unit)?
-    val onSuccess: ((data: TData) -> Unit)?
     val onFail: ((failureCount: Int, error: TError) -> Unit)?
     val onPause: (() -> Unit)?
     val onContinue: (() -> Unit)?
