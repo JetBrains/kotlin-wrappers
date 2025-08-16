6f9e934a4b0b0cb695e22183bbdab630a0c96863
Victor Turansky <victor.turansky@gmail.com>
2025-08-16 16:28:32 +0200

    Web Components. Simplify `CustomElement` declaration

--- kotlin-browser/src/webMain/kotlin/web/components/CustomElement.kt
+++ kotlin-browser/src/webMain/kotlin/web/components/CustomElement.kt
@@ -8,25 +8,25 @@ external interface CustomElement {
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Web_components#connectedcallback)
      */
-    val connectedCallback: () -> Unit
+    val connectedCallback: (() -> Unit)?
         get() = definedExternally
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Web_components#disconnectedcallback)
      */
-    val disconnectedCallback: () -> Unit
+    val disconnectedCallback: (() -> Unit)?
         get() = definedExternally
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Web_components#adoptedcallback)
      */
-    val adoptedCallback: () -> Unit
+    val adoptedCallback: (() -> Unit)?
         get() = definedExternally
 
     /**
      * [MDN Reference](https://developer.mozilla.org/docs/Web/API/Web_components#attributechangedcallback)
      */
-    val attributeChangedCallback: AttributeChangedCallback
+    val attributeChangedCallback: AttributeChangedCallback?
         get() = definedExternally
 }
 
