a63ef0c77c128df32c9e890bf71c669b4274be91
Victor Turansky <victor.turansky@gmail.com>
2025-08-17 04:33:37 +0200

    Audio. Add `AudioWorkletProcessorName` interface (#2811)

---
+++ kotlin-browser/src/webMain/kotlin/web/audio/AudioWorkletProcessorName.kt
@@ -0,0 +1,10 @@
+package web.audio
+
+import js.reflect.unsafeCast
+
+sealed external interface AudioWorkletProcessorName
+
+inline fun AudioWorkletProcessorName(
+    value: String,
+): AudioParamName =
+    unsafeCast(value)
