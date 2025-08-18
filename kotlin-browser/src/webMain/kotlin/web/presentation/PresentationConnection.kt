f4d7431b2e694daf247b538b720d9a303a9adbba
Victor Turansky <victor.turansky@gmail.com>
2025-08-18 03:23:36 +0200

    Presentation. `PresentationConnection`

--- kotlin-browser/src/webMain/kotlin/web/presentation/PresentationConnection.kt
+++ kotlin-browser/src/webMain/kotlin/web/presentation/PresentationConnection.kt
@@ -1,6 +1,15 @@
 package web.presentation
 
+import js.buffer.ArrayBuffer
+import js.buffer.ArrayBufferView
+import js.core.JsAny
+import web.blob.Blob
+import web.events.Event
+import web.events.EventHandler
+import web.events.EventInstance
 import web.events.EventTarget
+import web.messaging.MessageEvent
+import web.sockets.BinaryType
 
 /**
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection)
@@ -9,5 +18,100 @@ open external class PresentationConnection
 private constructor() :
     EventTarget {
 
-    // TBD
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/id)
+     */
+    val id: String
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/url)
+     */
+    val url: String
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/state)
+     */
+    val state: PresentationConnectionState
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/close)
+     */
+    fun close()
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/terminate)
+     */
+    fun terminate()
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/connect_event)
+     */
+    var onconnect: EventHandler<Event, PresentationConnection, PresentationConnection>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/close_event)
+     */
+    var onclose: EventHandler<PresentationConnectionCloseEvent, PresentationConnection, PresentationConnection>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/terminate_event)
+     */
+    var onterminate: EventHandler<Event, PresentationConnection, PresentationConnection>
+
+    // Communication
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/binaryType)
+     */
+    var binaryType: BinaryType
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/message_event)
+     */
+    var onmessage: EventHandler<MessageEvent<JsAny>, PresentationConnection, PresentationConnection>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/send)
+     */
+    fun send(message: String)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/send)
+     */
+    fun send(data: Blob)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/send)
+     */
+    fun send(data: ArrayBuffer)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/send)
+     */
+    fun send(data: ArrayBufferView<*>)
 }
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/connect_event)
+ */
+inline val <C : PresentationConnection> C.connectEvent: EventInstance<Event, C, C>
+    get() = EventInstance(this, "connect")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/close_event)
+ */
+inline val <C : PresentationConnection> C.closeEvent: EventInstance<PresentationConnectionCloseEvent, C, C>
+    get() = EventInstance(this, "close")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/terminate_event)
+ */
+inline val <C : PresentationConnection> C.terminateEvent: EventInstance<Event, C, C>
+    get() = EventInstance(this, "terminate")
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/PresentationConnection/message_event)
+ */
+inline val <C : PresentationConnection> C.messageEvent: EventInstance<MessageEvent<JsAny>, C, C>
+    get() = EventInstance(this, "message")
+
