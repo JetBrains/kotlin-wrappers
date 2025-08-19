91c5bfbfba237885fe43f238b14ef5561254f011
Victor Turansky <victor.turansky@gmail.com>
2025-08-19 02:03:41 +0200

    USB. `USBDevice`

---
+++ kotlin-browser/src/webMain/kotlin/web/usb/USBDevice.kt
@@ -0,0 +1,12 @@
+package web.usb
+
+import web.experimental.ExperimentalWebApi
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/USBDevice)
+ */
+@ExperimentalWebApi
+open external class USBDevice
+private constructor() {
+    // TBD
+}
