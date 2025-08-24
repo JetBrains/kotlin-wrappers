24a80fc6047f7b514880fd391dba2b7f41f91fa5
Victor Turansky <victor.turansky@gmail.com>
2025-08-24 23:45:48 +0200

    React. Remove duplicated `isolatedPromise`

--- kotlin-react-core/src/jsMain/kotlin/react/internal/isolatedVoidPromise.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/internal/isolatedVoidPromise.kt
@@ -1,13 +1,14 @@
 package react.internal
 
 import js.core.Void
+import js.coroutines.internal.createIsolatedPromise
 import js.promise.Promise
 import kotlinx.coroutines.CoroutineScope
 
 internal fun isolatedVoidPromise(
     block: suspend CoroutineScope.() -> Unit,
 ): Promise<Void> =
-    isolatedPromise {
+    createIsolatedPromise {
         block()
         undefined
     }
