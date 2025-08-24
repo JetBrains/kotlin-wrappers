f3fbcb397357c973d6168b8721157ef8e313c4b1
Victor Turansky <victor.turansky@gmail.com>
2025-08-24 18:47:17 +0200

    Function. Factories for async functions

--- kotlin-js/src/webMain/kotlin/js/function/async.kt
+++ kotlin-js/src/webMain/kotlin/js/function/async.kt
@@ -6,82 +6,60 @@ import js.coroutines.promise
 import js.promise.Promise
 import kotlinx.coroutines.CoroutineStart.UNDISPATCHED
 
-fun <R : JsAny?> async(
+// TODO: use common utility instead
+private fun <R : JsAny?> createPromise(
     block: suspend () -> R,
-): () -> Promise<R> = {
+): Promise<R> =
     IsolatedCoroutineScope()
         .promise(
             start = UNDISPATCHED,
             block = { block() },
         )
+
+fun <R : JsAny?> async(
+    block: suspend () -> R,
+): () -> Promise<R> = {
+    createPromise { block() }
 }
 
 fun <P1 : JsAny?, R : JsAny?> async(
     block: suspend (P1) -> R,
 ): (P1) -> Promise<R> = { p1 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1) },
-        )
+    createPromise { block(p1) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2) -> R,
 ): (P1, P2) -> Promise<R> = { p1, p2 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2) },
-        )
+    createPromise { block(p1, p2) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2, P3) -> R,
 ): (P1, P2, P3) -> Promise<R> = { p1, p2, p3 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2, p3) },
-        )
+    createPromise { block(p1, p2, p3) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2, P3, P4) -> R,
 ): (P1, P2, P3, P4) -> Promise<R> = { p1, p2, p3, p4 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2, p3, p4) },
-        )
+    createPromise { block(p1, p2, p3, p4) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2, P3, P4, P5) -> R,
 ): (P1, P2, P3, P4, P5) -> Promise<R> = { p1, p2, p3, p4, p5 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2, p3, p4, p5) },
-        )
+    createPromise { block(p1, p2, p3, p4, p5) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, P6 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2, P3, P4, P5, P6) -> R,
 ): (P1, P2, P3, P4, P5, P6) -> Promise<R> = { p1, p2, p3, p4, p5, p6 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2, p3, p4, p5, p6) },
-        )
+    createPromise { block(p1, p2, p3, p4, p5, p6) }
 }
 
 fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, P6 : JsAny?, P7 : JsAny?, R : JsAny?> async(
     block: suspend (P1, P2, P3, P4, P5, P6, P7) -> R,
 ): (P1, P2, P3, P4, P5, P6, P7) -> Promise<R> = { p1, p2, p3, p4, p5, p6, p7 ->
-    IsolatedCoroutineScope()
-        .promise(
-            start = UNDISPATCHED,
-            block = { block(p1, p2, p3, p4, p5, p6, p7) },
-        )
+    createPromise { block(p1, p2, p3, p4, p5, p6, p7) }
 }
