0ebc9217571c94a09be54650df33d91889330dc6
Victor Turansky <victor.turansky@gmail.com>
2025-08-29 02:32:13 +0200

    Globals. Use `Record` operators

--- kotlin-js/src/webMain/kotlin/js/globals/internal/getGlobalScope.kt
+++ kotlin-js/src/webMain/kotlin/js/globals/internal/getGlobalScope.kt
@@ -1,9 +1,9 @@
 package js.globals.internal
 
-import js.core.JsPrimitives.toJsString
 import js.globals.GlobalScope
 import js.globals.globalThis
 import js.internal.InternalApi
+import js.objects.get
 import js.reflect.JsClass
 import js.reflect.cast
 import js.reflect.unsafeCast
@@ -13,7 +13,7 @@ fun <S : GlobalScope> getGlobalScope(
     scopeClassName: String,
 ): S {
     val jsClass = unsafeCast<JsClass<S>>(
-        globalThis[scopeClassName.toJsString()]
+        globalThis[scopeClassName]
             ?: error("Class `$scopeClassName` not found!")
     )
 
