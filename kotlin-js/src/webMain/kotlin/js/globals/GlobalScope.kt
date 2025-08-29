e91261f189ff89ad2d39662d2dea66f67f81f07b
Victor Turansky <victor.turansky@gmail.com>
2025-08-29 14:07:58 +0200

    Globals. `Symbol` keys for `GlobalScope`

--- kotlin-js/src/webMain/kotlin/js/globals/GlobalScope.kt
+++ kotlin-js/src/webMain/kotlin/js/globals/GlobalScope.kt
@@ -1,10 +1,10 @@
 package js.globals
 
 import js.core.JsAny
-import js.core.JsString
+import js.objects.PropertyKey
 import js.objects.Record
 import js.reflect.JsExternalInheritorsOnly
 
 @JsExternalInheritorsOnly
 external interface GlobalScope :
-    Record<JsString, JsAny?>
+    Record<PropertyKey, JsAny?>
