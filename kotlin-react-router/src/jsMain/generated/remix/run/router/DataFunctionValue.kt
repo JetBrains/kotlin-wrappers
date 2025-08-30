f09b05d32fc15c49ab868f0f4c977316f7d58d98
Victor Turansky <victor.turansky@gmail.com>
2025-08-30 11:49:52 +0200

    React Router. Strict `DataFunctionValue` alias (#2812)

--- kotlin-react-router/src/jsMain/generated/remix/run/router/DataFunctionValue.kt
+++ kotlin-react-router/src/jsMain/generated/remix/run/router/DataFunctionValue.kt
@@ -2,9 +2,11 @@
 
 package remix.run.router
 
+import web.http.Response
+
 /**
  * Loaders and actions can return anything except `undefined` (`null` is a
  * valid return value if there is no data to return).  Responses are preferred
  * and will ease any future migration to Remix
  */
-typealias DataFunctionValue = Any? /* Response | NonNullable<unknown> | null */
+typealias DataFunctionValue = Response?
