7c4c2442bdfba0ec2d08c53c0172ecbf789420a7
Victor Turansky <victor.turansky@gmail.com>
2025-09-05 01:58:01 +0200

    Examples. Use `CoroutineScope` factory

--- examples/tanstack-react-table/src/jsMain/kotlin/example/query/createQueryFunction.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/example/query/createQueryFunction.kt
@@ -1,23 +1,14 @@
 package example.query
 
 import js.coroutines.promise
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
 import tanstack.query.core.QueryFunction
 import tanstack.query.core.QueryKey
-import web.abort.abortEvent
-import web.events.addHandler
-import kotlin.coroutines.EmptyCoroutineContext
+import web.coroutines.CoroutineScope
 
 fun <D> createQueryFunction(
     block: suspend () -> D,
 ): QueryFunction<D, QueryKey, Nothing> =
     QueryFunction { context ->
-        val scope = CoroutineScope(EmptyCoroutineContext)
-
-        context.signal.abortEvent.addHandler {
-            scope.cancel()
-        }
-
-        scope.promise { block() }
+        CoroutineScope(context.signal)
+            .promise { block() }
     }
