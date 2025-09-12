1a8e29dd05b17746a5a66aa99be73602d1e7a7da
Victor Turansky <victor.turansky@gmail.com>
2025-09-12 17:57:11 +0200

    Iterable. Use `SuspendCloseable.use` for safe dispose

--- kotlin-js/src/webMain/kotlin/js/iterable/internal/AsyncIterator.flow.kt
+++ kotlin-js/src/webMain/kotlin/js/iterable/internal/AsyncIterator.flow.kt
@@ -1,12 +1,13 @@
 package js.iterable.internal
 
+import js.disposable.internal.SuspendCloseable
+import js.disposable.internal.use
 import js.internal.InternalApi
 import js.iterable.AsyncIterator
 import js.iterable.isYield
 import js.promise.await
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onCompletion
 import kotlin.js.JsAny
 
 @InternalApi
@@ -15,15 +16,19 @@ internal fun <T : JsAny?> flowFromAsyncIterator(
     source: AsyncIterator<T>,
 ): Flow<T> =
     flow {
-        do {
-            val result = source.next().await()
-            val done = if (isYield(result)) {
-                emit(result.value)
-                true
-            } else {
-                false
-            }
-        } while (done)
-    }.onCompletion {
-        source.`return`().await()
+        val closable = SuspendCloseable {
+            source.`return`().await()
+        }
+
+        closable.use {
+            do {
+                val result = source.next().await()
+                val done = if (isYield(result)) {
+                    emit(result.value)
+                    true
+                } else {
+                    false
+                }
+            } while (done)
+        }
     }
