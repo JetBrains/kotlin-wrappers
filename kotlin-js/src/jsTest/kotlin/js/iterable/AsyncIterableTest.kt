2aea993d38929eff5be6e0655bf593e9b34169ca
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-09-13 01:14:08 +0300

    Iterable. Add test for retry

--- kotlin-js/src/jsTest/kotlin/js/iterable/AsyncIterableTest.kt
+++ kotlin-js/src/jsTest/kotlin/js/iterable/AsyncIterableTest.kt
@@ -4,6 +4,8 @@ import js.function.JsFunction
 import js.function.invoke
 import js.generator.AsyncGenerator
 import js.generator.AsyncGeneratorFunction
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.retry
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
@@ -45,6 +47,7 @@ class AsyncIterableTest {
                 [Symbol.asyncIterator]() {
                     return ({
                         count: 5,
+
                         async next() {
                             return ({
                                 done: this.count-- === 0,
@@ -75,5 +78,48 @@ class AsyncIterableTest {
         assertEquals(expectedList, actualList)
         assertTrue(result.isFailure)
         assertTrue(isDisposeCalled)
+    }
+
+    @Test
+    fun `should start iteration from the beginning on flow retry`() = runTest {
+        var creationCount = 0L
+        val onCreate = { creationCount += 1 }
+
+        val createAsyncIterable = JsFunction<() -> Unit, AsyncIterable<Int>>(
+            "onCreate",
+            // language=javascript
+            """
+            return ({
+                [Symbol.asyncIterator]() {
+                    onCreate()
+                    return ({
+                        count: 5,
+
+                        async next() {
+                            return ({
+                                done: this.count-- === 0,
+                                value: this.count
+                            })
+                        },
+
+                        async return() { }
+                    })
+                }
+            })
+            """
+        )
+        val asyncIterable = createAsyncIterable(onCreate)
+
+        val retryCount = 3L
+
+        val result = runCatching {
+            asyncIterable.asFlow()
+                .map { error("Error") }
+                .retry(retryCount)
+                .collect {}
+        }
+
+        assertTrue(result.isFailure)
+        assertEquals(retryCount + 1, creationCount)
     }
 }
