def611fc3e66764efe4b1fc5cda70babdf805fea
Victor Turansky <victor.turansky@gmail.com>
2025-09-13 05:13:36 +0200

    Iterable. Use `BigInt` in tests

--- kotlin-js/src/jsTest/kotlin/js/iterable/JsIteratorTest.kt
+++ kotlin-js/src/jsTest/kotlin/js/iterable/JsIteratorTest.kt
@@ -1,6 +1,8 @@
 package js.iterable
 
 import js.collections.JsSet
+import js.core.BigInt
+import js.core.n
 import js.symbol.Symbol
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -8,10 +10,10 @@ import kotlin.test.assertEquals
 class JsIteratorTest {
     @Test
     fun operatorIterator() {
-        val expectedSet = setOf(1, 2, 3)
+        val expectedSet = setOf(1.n, 2.n, 3.n)
         val jsSet = JsSet(expectedSet.toTypedArray())
 
-        val actualSet = mutableSetOf<Int>()
+        val actualSet = mutableSetOf<BigInt>()
         for (element in jsSet[Symbol.iterator]()) {
             actualSet.add(element)
         }
