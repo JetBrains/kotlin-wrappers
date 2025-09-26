7eba83c8afa65990d56e782a6eb98d1a858a7e5a
Victor Turansky <victor.turansky@gmail.com>
2025-09-26 15:28:56 +0200

    Examples. Use `QueryClient` from context

--- examples/tanstack-react-table/src/jsMain/kotlin/example/hooks/useMutateUser.kt
+++ examples/tanstack-react-table/src/jsMain/kotlin/example/hooks/useMutateUser.kt
@@ -9,19 +9,17 @@ import tanstack.query.core.InvalidateQueryFilters
 import tanstack.query.core.QueryKey
 import tanstack.react.query.UseMutationOptions
 import tanstack.react.query.useMutation
-import tanstack.react.query.useQueryClient
 
 fun <O, R> useMutateUser(
     action: suspend CoroutineScope.(O) -> R,
 ): (O) -> Unit {
-    val queryClient = useQueryClient()
     val mutate = useMutation<R, JsError, O, QueryKey>(
         options = UseMutationOptions(
             mutationFn = unsafeAsync { options, _ ->
                 action(options)
             },
-            onSuccess = { _, _, _, _ ->
-                queryClient.invalidateQueries(
+            onSuccess = { _, _, _, context ->
+                context.client.invalidateQueries(
                     filters = InvalidateQueryFilters(
                         queryKey = USERS_QUERY_KEY
                     )
