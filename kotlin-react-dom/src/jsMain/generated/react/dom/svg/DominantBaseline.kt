317dbe63b6efba3fa1f572b25175adad840ad1b9
porotkin <preload737773@gmail.com>
2025-09-26 11:38:53 +0300

    React DOM. Sync generation

--- kotlin-react-dom/src/jsMain/generated/react/dom/svg/DominantBaseline.kt
+++ kotlin-react-dom/src/jsMain/generated/react/dom/svg/DominantBaseline.kt
@@ -4,8 +4,11 @@ package react.dom.svg
 
 import seskar.js.JsValue
 
-sealed external interface DominantBaseline : AlignmentBaseline {
+sealed external interface DominantBaseline {
     companion object {
+        @JsValue("auto")
+        val auto: DominantBaseline
+
         @JsValue("use-script")
         val useScript: DominantBaseline
 
@@ -14,5 +17,32 @@ sealed external interface DominantBaseline : AlignmentBaseline {
 
         @JsValue("reset-size")
         val resetSize: DominantBaseline
+
+        @JsValue("ideographic")
+        val ideographic: DominantBaseline
+
+        @JsValue("alphabetic")
+        val alphabetic: DominantBaseline
+
+        @JsValue("hanging")
+        val hanging: DominantBaseline
+
+        @JsValue("mathematical")
+        val mathematical: DominantBaseline
+
+        @JsValue("central")
+        val central: DominantBaseline
+
+        @JsValue("middle")
+        val middle: DominantBaseline
+
+        @JsValue("text-after-edge")
+        val textAfterEdge: DominantBaseline
+
+        @JsValue("text-before-edge")
+        val textBeforeEdge: DominantBaseline
+
+        @JsValue("inherit")
+        val inherit: DominantBaseline
     }
 }
