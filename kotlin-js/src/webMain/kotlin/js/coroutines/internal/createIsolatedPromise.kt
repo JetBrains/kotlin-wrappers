88b6fe112731df5d9c308275aa5478fec4f53586
Victor Turansky <victor.turansky@gmail.com>
2025-09-26 03:49:04 +0200

    JS. Internal `createIsolatedPromise`

--- kotlin-js/src/webMain/kotlin/js/coroutines/internal/createIsolatedPromise.kt
+++ kotlin-js/src/webMain/kotlin/js/coroutines/internal/createIsolatedPromise.kt
@@ -1,12 +1,10 @@
 package js.coroutines.internal
 
-import js.internal.InternalApi
 import js.promise.Promise
 import kotlinx.coroutines.CoroutineScope
 import kotlin.js.JsAny
 
-@InternalApi
-fun <R : JsAny?> createIsolatedPromise(
+internal fun <R : JsAny?> createIsolatedPromise(
     block: suspend CoroutineScope.() -> R,
 ): Promise<R> =
     IsolatedCoroutineScope()
