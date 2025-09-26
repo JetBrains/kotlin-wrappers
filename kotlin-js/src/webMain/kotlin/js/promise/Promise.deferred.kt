0b1fbbe58ae97ccf87bc73bfa89b394e422dec9d
Victor Turansky <victor.turansky@gmail.com>
2025-09-27 00:55:47 +0200

    Promise. Save `Deferred` in symbol

--- kotlin-js/src/webMain/kotlin/js/promise/Promise.deferred.kt
+++ kotlin-js/src/webMain/kotlin/js/promise/Promise.deferred.kt
@@ -1,6 +1,16 @@
 package js.promise
 
+import js.reflect.internal.kotlinGet
+import js.reflect.internal.kotlinSet
+import js.symbol.Symbol
 import kotlinx.coroutines.Deferred
 import kotlin.js.JsAny
 
-internal expect var <T : JsAny?> Promise<T>.deferred: Deferred<T>?
+private val DEFERRED: Symbol = Symbol("@@deferred")
+
+@Suppress("UNCHECKED_CAST")
+internal var <T : JsAny?> Promise<T>.deferred: Deferred<T>?
+    get() = kotlinGet(this, DEFERRED) as Deferred<T>?
+    set(value) {
+        kotlinSet(this, DEFERRED, value)
+    }
