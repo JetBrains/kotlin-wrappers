70c3f556be934d7c7a1c117d0de3bd6e4d356035
Victor Turansky <victor.turansky@gmail.com>
2025-10-02 02:22:20 +0200

    React. `captureOwnerStack`

---
+++ kotlin-react-core/src/jsMain/kotlin/react/captureOwnerStack.kt
@@ -0,0 +1,10 @@
+@file:JsModule("react")
+
+package react
+
+/**
+ * Warning: Only available in development builds.
+ *
+ * [Online Documentation](https://react.dev/reference/react/captureOwnerStack)
+ */
+external fun captureOwnerStack(): String?
