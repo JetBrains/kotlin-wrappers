2d7b93d4c2a7597fb7ad3cd0415833f372c48b8f
Victor Turansky <victor.turansky@gmail.com>
2025-10-05 20:59:28 +0200

    Actions Cache `4.1.0` [generated]

---
+++ kotlin-actions-cache/src/webMain/generated/actions/cache/FinalizeCacheError.kt
@@ -0,0 +1,11 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@actions/cache")
+
+package actions.cache
+
+import js.errors.JsError
+
+external class FinalizeCacheError : JsError {
+    constructor(message: String)
+}
