6d92257ad7a3058a0e1ce3a4e711fc4ba6bfbfa0
Victor Turansky <victor.turansky@gmail.com>
2025-10-11 03:00:16 +0200

    React. `addTransitionType`

---
+++ kotlin-react-core/src/jsMain/kotlin/react/addTransitionType.kt
@@ -0,0 +1,13 @@
+@file:JsModule("react")
+
+package react
+
+import react.canary.ReactCanary
+
+/**
+ * [`addTransitionType` reference documentation](https://react.dev/reference/react/addTransitionType)
+ */
+@ReactCanary
+external fun addTransitionType(
+    type: ViewTransitionType,
+)
