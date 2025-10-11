197fd1d997ee1d5e8836859a2316ab1ca42296b1
Victor Turansky <victor.turansky@gmail.com>
2025-10-12 01:39:23 +0200

    Abort. Use `AbortSignal.any` if possible

--- kotlin-web/src/webMain/kotlin/web/abort/internal/AbortSignal.safeAny.kt
+++ kotlin-web/src/webMain/kotlin/web/abort/internal/AbortSignal.safeAny.kt
@@ -1,6 +1,8 @@
 package web.abort.internal
 
+import js.array.jsArrayOf
 import js.errors.toJsError
+import js.reflect.Reflect
 import web.abort.AbortController
 import web.abort.AbortSignal
 import web.abort.abortEvent
@@ -21,6 +23,17 @@ internal fun safeAny(
     if (signal2.aborted)
         return signal2
 
+    return if (Reflect.has(AbortSignal, "any")) {
+        AbortSignal.any(jsArrayOf(signal1, signal2))
+    } else {
+        legacySafariAny(signal1, signal2)
+    }
+}
+
+private fun legacySafariAny(
+    signal1: AbortSignal,
+    signal2: AbortSignal,
+): AbortSignal {
     val controller = AbortController()
     val handlers = mutableListOf<() -> Unit>()
     val abortHandler = EventHandler<Event, AbortSignal, AbortSignal, _> { event ->
