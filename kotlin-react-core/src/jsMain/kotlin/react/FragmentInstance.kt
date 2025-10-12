5a4a45efe374a573119262d813e8bcf06ba75f64
Victor Turansky <victor.turansky@gmail.com>
2025-10-13 00:20:14 +0200

    React. `FragmentInstance` extends `EventTargetLike`

--- kotlin-react-core/src/jsMain/kotlin/react/FragmentInstance.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/FragmentInstance.kt
@@ -6,10 +6,7 @@ import web.dom.FocusOptions
 import web.dom.GetRootNodeOptions
 import web.dom.Node
 import web.dom.NodePosition
-import web.events.AddEventListenerOptions
-import web.events.Event
-import web.events.EventListenerOptions
-import web.events.EventType
+import web.events.EventTargetLike
 import web.geometry.DOMRect
 import web.intersection.IntersectionObserver
 import web.resize.ResizeObserver
@@ -18,7 +15,8 @@ import web.resize.ResizeObserver
  * [Online Documentation](https://react.dev/reference/react/Fragment#fragmentinstance)
  */
 @ReactCanary
-external interface FragmentInstance {
+external interface FragmentInstance :
+    EventTargetLike {
 
     // Focus management methods
 
@@ -38,23 +36,7 @@ external interface FragmentInstance {
 
     fun compareDocumentPosition(otherNode: Node /* ? */): NodePosition
     fun getClientRects(): ReadonlyArray<DOMRect>
-    fun getRootNode(getRootNodeOptions: GetRootNodeOptions = definedExternally): Any /* Document | ShadowRoot | FragmentInstance */
-
-    // Event handling methods
-
-    fun <E : Event> addEventListener(
-        type: EventType<E>,
-        listener: (E) -> Unit,
-        options: AddEventListenerOptions,
-    )
-
-    fun <E : Event> removeEventListener(
-        type: EventType<E>,
-        listener: (E) -> Unit,
-        options: EventListenerOptions,
-    )
-
-    fun dispatchEvent(event: Event): Boolean
+    fun getRootNode(getRootNodeOptions: GetRootNodeOptions = definedExternally): EventTargetLike /* Document | ShadowRoot | FragmentInstance */
 
     // Scroll methods
 
