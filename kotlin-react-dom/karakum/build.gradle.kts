84d5af7f08bb9d3178562800e779c1c6672b2812
Victor Turansky <victor.turansky@gmail.com>
2025-10-13 02:21:58 +0200

    React. Prepare `FragmentInstance` events generation

--- kotlin-react-dom/karakum/build.gradle.kts
+++ kotlin-react-dom/karakum/build.gradle.kts
@@ -4,9 +4,21 @@ plugins {
 
 dependencies {
     commonMainImplementation(npm(jspkg.types.react))
+    commonMainImplementation(npm(jspkg.webref.events))
+}
+
+val syncReact by tasks.registering(SyncWrappers::class) {
+    from(jsGeneratedDir) {
+        include("react/*.kt")
+    }
+
+    into(jsMainDir("kotlin-react"))
 }
 
 val syncReactDom by tasks.registering(SyncWrappers::class) {
-    from(jsGeneratedDir)
+    from(jsGeneratedDir) {
+        include("react/dom/**/*.kt")
+    }
+
     into(jsMainDir("kotlin-react-dom"))
 }
