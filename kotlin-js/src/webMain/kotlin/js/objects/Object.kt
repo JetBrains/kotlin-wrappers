9717cccd23ca77f4fe835461d5a728070347fe55
Victor Turansky <victor.turansky@gmail.com>
2025-10-15 01:28:42 +0200

    JS. Addition `Object` method overloads

--- kotlin-js/src/webMain/kotlin/js/objects/Object.kt
+++ kotlin-js/src/webMain/kotlin/js/objects/Object.kt
@@ -22,12 +22,14 @@ private constructor() {
         fun <T : JsAny> create(o: T?, properties: PropertyDescriptorMap = definedExternally): T
         fun <T : JsAny> defineProperties(o: T, properties: PropertyDescriptorMap): T
         fun <T : JsAny, P : JsAny?> defineProperty(o: T, p: PropertyKey, attributes: TypedPropertyDescriptor<P>): T
+        fun <T : JsAny, P : JsAny?> defineProperty(o: T, p: String, attributes: TypedPropertyDescriptor<P>): T
         fun <T : JsAny?> entries(o: ReadonlyRecord<JsString, T>): ReadonlyArray<Tuple2<JsString, T>>
         fun entries(o: JsAny): ReadonlyArray<Tuple2<JsString, JsAny?>>
         fun <R : JsAny?, T : R> freeze(o: T): R
         fun <T : JsAny?> fromEntries(entries: JsIterable<Tuple2<JsString, T>>): ReadonlyRecord<JsString, T>
         fun <T : JsAny?> fromEntries(entries: ReadonlyArray<Tuple2<JsString, T>>): ReadonlyRecord<JsString, T>
         fun <T : JsAny?> getOwnPropertyDescriptor(o: JsAny, p: PropertyKey): TypedPropertyDescriptor<T>?
+        fun <T : JsAny?> getOwnPropertyDescriptor(o: JsAny, p: String): TypedPropertyDescriptor<T>?
         fun getOwnPropertyNames(o: JsAny): ReadonlyArray<JsString>
         fun getOwnPropertySymbols(o: JsAny): ReadonlyArray<Symbol>
         fun <P : JsAny, T : P> getPrototypeOf(o: T): P
@@ -38,6 +40,7 @@ private constructor() {
         ): ReadonlyRecord<K, ReadonlyArray<T>>
 
         fun hasOwn(o: JsAny, v: PropertyKey): Boolean
+        fun hasOwn(o: JsAny, v: String): Boolean
         fun `is`(value1: JsAny?, value2: JsAny?): Boolean
         fun isExtensible(o: JsAny): Boolean
         fun isFrozen(o: JsAny): Boolean
