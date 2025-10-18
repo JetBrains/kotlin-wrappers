098f9cf35b7266964a142861bb8e080727cdcc0c
Victor Turansky <victor.turansky@gmail.com>
2025-10-18 13:58:49 +0200

    React. Strict `ViewTransitionClass`

--- kotlin-react-core/src/jsMain/kotlin/react/ViewTransitionClass.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/ViewTransitionClass.kt
@@ -1,10 +1,34 @@
 package react
 
+import js.objects.recordOf
+import js.reflect.unsafeCast
 import react.canary.ReactCanary
+import seskar.js.JsValue
 import web.cssom.ClassName
 
 // type ViewTransitionClassPerType = Record<"default" | (string & {}), "none" | "auto" | (string & {})>;
 // type ViewTransitionClass = ViewTransitionClassPerType | ViewTransitionClassPerType[string];
 
 @ReactCanary
-typealias ViewTransitionClass = ClassName
+sealed external interface ViewTransitionClass {
+    companion object {
+        @JsValue("auto")
+        val auto: ViewTransitionClass
+
+        @JsValue("none")
+        val none: ViewTransitionClass
+    }
+}
+
+@ReactCanary
+inline fun ViewTransitionClass(
+    value: ClassName,
+): ViewTransitionClass =
+    unsafeCast(value)
+
+// TODO: add "default" support
+@ReactCanary
+inline fun ViewTransitionClass(
+    vararg values: Pair<ViewTransitionType, ClassName>,
+): ViewTransitionClass =
+    unsafeCast(recordOf(pairs = values))
