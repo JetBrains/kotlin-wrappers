06b41583a7773e9fe0304e6db3f52fa4b0dbcb83
Victor Turansky <victor.turansky@gmail.com>
2025-10-18 20:24:59 +0200

    Closeable. `JsCloseable.use`

--- kotlin-js/src/webMain/kotlin/js/closeable/JsCloseable.kt
+++ kotlin-js/src/webMain/kotlin/js/closeable/JsCloseable.kt
@@ -1,5 +1,23 @@
 package js.closeable
 
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 external interface JsCloseable {
     fun close()
 }
+
+inline fun <T : JsCloseable, R> T.use(
+    block: (T) -> R,
+): R {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    val closeable = AutoCloseable(::close)
+
+    return closeable.use {
+        block(this)
+    }
+}
+
