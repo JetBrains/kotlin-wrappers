e8c40194b304db7b542c096f07f8f814621c5916
Victor Turansky <victor.turansky@gmail.com>
2025-11-04 02:07:04 +0100

    Internal. Allow `@InternalApi` for properties

--- kotlin-js-core/src/webMain/kotlin/js/internal/InternalApi.kt
+++ kotlin-js-core/src/webMain/kotlin/js/internal/InternalApi.kt
@@ -1,13 +1,13 @@
 package js.internal
 
 import kotlin.RequiresOptIn.Level.ERROR
-import kotlin.annotation.AnnotationTarget.CLASS
-import kotlin.annotation.AnnotationTarget.FUNCTION
+import kotlin.annotation.AnnotationTarget.*
 
 @RequiresOptIn(level = ERROR)
 @Retention(AnnotationRetention.BINARY)
 @Target(
     CLASS,
+    PROPERTY,
     FUNCTION,
 )
 annotation class InternalApi
