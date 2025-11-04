bdfd47edad4b7832f6e665a7625244061d63e808
Victor Turansky <victor.turansky@gmail.com>
2025-11-04 01:20:33 +0100

    React. Add `jsx` and `jsxs` declarations

---
+++ kotlin-react-core/src/jsMain/kotlin/react/jsx/runtime/jsxs.kt
@@ -0,0 +1,17 @@
+@file:JsModule("react/jsx-runtime")
+
+package react.jsx.runtime
+
+import react.ElementType
+import react.Key
+import react.Props
+import react.ReactElement
+
+/**
+ * [Online Documentation](https://react.dev/reference/react)
+ */
+external fun <P : Props> jsxs(
+    type: ElementType<P>,
+    props: P? = definedExternally,
+    key: Key? = definedExternally,
+): ReactElement<P>
