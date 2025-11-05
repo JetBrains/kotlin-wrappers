f9b37a699338eb4d3fcea2cbe9c3a4d27621e767
Victor Turansky <victor.turansky@gmail.com>
2025-11-05 02:18:26 +0100

    HTML. Fix `HTMLCanvasElement.toBlob()` extensions

--- kotlin-browser/src/webMain/kotlin/web/html/HTMLCanvasElement.ext.kt
+++ kotlin-browser/src/webMain/kotlin/web/html/HTMLCanvasElement.ext.kt
@@ -1,5 +1,6 @@
 package web.html
 
+import web.blob.Blob
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
@@ -8,13 +9,12 @@ import kotlin.coroutines.suspendCoroutine
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLCanvasElement/toBlob)
  */
-suspend fun HTMLCanvasElement.toBlob() {
+suspend fun HTMLCanvasElement.toBlob(): Blob? =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = continuation::resume,
         )
     }
-}
 
 /**
  * The **`HTMLCanvasElement.toBlob()`** method creates a Blob object representing the image contained in the canvas. This file may be cached on the disk or stored in memory at the discretion of the user agent.
@@ -23,14 +23,13 @@ suspend fun HTMLCanvasElement.toBlob() {
  */
 suspend fun HTMLCanvasElement.toBlob(
     type: String,
-) {
+): Blob? =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = continuation::resume,
             type = type,
         )
     }
-}
 
 /**
  * The **`HTMLCanvasElement.toBlob()`** method creates a Blob object representing the image contained in the canvas. This file may be cached on the disk or stored in memory at the discretion of the user agent.
@@ -40,7 +39,7 @@ suspend fun HTMLCanvasElement.toBlob(
 suspend fun HTMLCanvasElement.toBlob(
     type: String,
     quality: Double,
-) {
+): Blob? =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = continuation::resume,
@@ -48,4 +47,3 @@ suspend fun HTMLCanvasElement.toBlob(
             quality = quality,
         )
     }
-}
