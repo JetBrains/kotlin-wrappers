36e94764d124c32eab7b8d932fe83cec68df5563
Victor Turansky <victor.turansky@gmail.com>
2025-11-13 01:19:58 +0100

    Objects. `Destructible`

---
+++ kotlin-js/src/webMain/kotlin/js/objects/Destructible.kt
@@ -0,0 +1,33 @@
+package js.objects
+
+import js.reflect.JsExternalInheritorsOnly
+import js.reflect.Reflect.deleteProperty
+import kotlin.js.JsAny
+import kotlin.reflect.KProperty1
+
+@JsExternalInheritorsOnly
+external interface Destructible<R : Destructible<R>>
+
+fun <T : Destructible<R>, P1, R : JsAny> T.destruct(
+    p1: KProperty1<in T, P1>,
+): Pair<P1, R> {
+    val v1 = p1.get(this)
+
+    val rest: R = Object.assign(unsafeJso(), this)
+    deleteProperty(rest, p1.name)
+
+    return Pair(v1, rest)
+}
+
+fun <T : Destructible<R>, P1, P2, R : JsAny> T.destruct(
+    p1: KProperty1<in T, P1>,
+    p2: KProperty1<in T, P2>,
+): Triple<P1, P2, R> {
+    val v1 = p1.get(this)
+    val v2 = p2.get(this)
+
+    val rest: R = Object.assign(unsafeJso(), this)
+    deleteProperty(rest, p1.name)
+
+    return Triple(v1, v2, rest)
+}
