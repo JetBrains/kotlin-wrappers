3db9ee9a3f8f8ec3f7328f85a36a978e73240581
Victor Turansky <victor.turansky@gmail.com>
2025-11-13 01:30:10 +0100

    React. Add `PropsWithChildren.childrenAndRest()` extension

--- kotlin-react-core/src/jsMain/kotlin/react/PropsWithChildren.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/PropsWithChildren.kt
@@ -1,8 +1,17 @@
 package react
 
+import js.objects.destruct
+import kotlin.reflect.KProperty1
+
+val CHILDREN_PROP: KProperty1<in PropsWithChildren, ReactNode?> =
+    PropsWithChildren::children
+
 external interface PropsWithChildren :
     Props,
     // TODO: use as `MutablePropsWithChildren` parent type instead
     ChildrenBuilder {
     var children: ReactNode?
 }
+
+private fun PropsWithChildren.childrenAndRest(): Pair<ReactNode?, Props> =
+    destruct(CHILDREN_PROP)
