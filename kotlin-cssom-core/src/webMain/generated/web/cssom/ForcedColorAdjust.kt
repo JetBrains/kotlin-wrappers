cef5a6d6219ddcef79974ba12b8a292055c435d6
Victor Turansky <victor.turansky@gmail.com>
2025-11-15 21:23:38 +0100

    CSSType `3.2.0`. Update unions [generated]

--- kotlin-cssom-core/src/webMain/generated/web/cssom/ForcedColorAdjust.kt
+++ kotlin-cssom-core/src/webMain/generated/web/cssom/ForcedColorAdjust.kt
@@ -1,5 +1,16 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package web.cssom
 
-sealed external interface ForcedColorAdjust
+import seskar.js.JsValue
+
+sealed external interface ForcedColorAdjust {
+    companion object {
+        @JsValue("preserve-parent-color")
+        val preserveParentColor: ForcedColorAdjust
+    }
+}
