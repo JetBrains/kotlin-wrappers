2b045a3cc62a3a5b6b06877d1e5794c47e8357e8
Victor Turansky <victor.turansky@gmail.com>
2025-11-16 00:46:13 +0100

    CSSType `3.2.1`. Update `Background` [generated]

--- kotlin-cssom-core/src/webMain/generated/web/cssom/Background.kt
+++ kotlin-cssom-core/src/webMain/generated/web/cssom/Background.kt
@@ -1,64 +1,6 @@
 // Automatically generated - do not modify!
 
-@file:Suppress(
-    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
-)
-
 package web.cssom
 
-import seskar.js.JsValue
-
-sealed external interface Background {
-    companion object {
-        @JsValue("bottom")
-        val bottom: Background
-
-        @JsValue("center")
-        val center: Background
-
-        @JsValue("left")
-        val left: Background
-
-        @JsValue("right")
-        val right: Background
-
-        @JsValue("top")
-        val top: Background
-
-        @JsValue("no-repeat")
-        val noRepeat: Background
-
-        @JsValue("repeat")
-        val repeat: Background
-
-        @JsValue("repeat-x")
-        val repeatX: Background
-
-        @JsValue("repeat-y")
-        val repeatY: Background
-
-        @JsValue("round")
-        val round: Background
-
-        @JsValue("space")
-        val space: Background
-
-        @JsValue("fixed")
-        val fixed: Background
-
-        @JsValue("local")
-        val local: Background
-
-        @JsValue("scroll")
-        val scroll: Background
-
-        @JsValue("border-box")
-        val borderBox: Background
-
-        @JsValue("content-box")
-        val contentBox: Background
-
-        @JsValue("padding-box")
-        val paddingBox: Background
-    }
-}
+// Globals | (string & {})
+sealed external interface Background
