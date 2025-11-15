e05e434f3a4c1c81906486b93879fd167dff8803
Victor Turansky <victor.turansky@gmail.com>
2025-11-16 00:48:40 +0100

    CSSType `3.2.1`. New unions [generated]

---
+++ kotlin-cssom-core/src/webMain/generated/web/cssom/FieldSizing.kt
@@ -0,0 +1,19 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package web.cssom
+
+import seskar.js.JsValue
+
+sealed external interface FieldSizing {
+    companion object {
+        @JsValue("content")
+        val content: FieldSizing
+
+        @JsValue("fixed")
+        val fixed: FieldSizing
+    }
+}
