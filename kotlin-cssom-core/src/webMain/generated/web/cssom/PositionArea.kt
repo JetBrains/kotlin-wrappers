31adf01ed8dd0b7bd5e24c3b6b6fc589c5d65891
Victor Turansky <victor.turansky@gmail.com>
2025-11-15 23:59:07 +0100

    CSSType `3.2.1`. New unions [generated]

--- kotlin-cssom-core/src/webMain/generated/web/cssom/PositionArea.kt
+++ kotlin-cssom-core/src/webMain/generated/web/cssom/PositionArea.kt
@@ -1,5 +1,163 @@
 // Automatically generated - do not modify!
 
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package web.cssom
 
-sealed external interface PositionArea
+import seskar.js.JsValue
+
+sealed external interface PositionArea {
+    companion object {
+        @JsValue("block-end")
+        val blockEnd: PositionArea
+
+        @JsValue("block-start")
+        val blockStart: PositionArea
+
+        @JsValue("bottom")
+        val bottom: PositionArea
+
+        @JsValue("center")
+        val center: PositionArea
+
+        @JsValue("end")
+        val end: PositionArea
+
+        @JsValue("inline-end")
+        val inlineEnd: PositionArea
+
+        @JsValue("inline-start")
+        val inlineStart: PositionArea
+
+        @JsValue("left")
+        val left: PositionArea
+
+        @JsValue("right")
+        val right: PositionArea
+
+        @JsValue("self-block-end")
+        val selfBlockEnd: PositionArea
+
+        @JsValue("self-block-start")
+        val selfBlockStart: PositionArea
+
+        @JsValue("self-end")
+        val selfEnd: PositionArea
+
+        @JsValue("self-inline-end")
+        val selfInlineEnd: PositionArea
+
+        @JsValue("self-inline-start")
+        val selfInlineStart: PositionArea
+
+        @JsValue("self-start")
+        val selfStart: PositionArea
+
+        @JsValue("span-all")
+        val spanAll: PositionArea
+
+        @JsValue("span-block-end")
+        val spanBlockEnd: PositionArea
+
+        @JsValue("span-block-start")
+        val spanBlockStart: PositionArea
+
+        @JsValue("span-bottom")
+        val spanBottom: PositionArea
+
+        @JsValue("span-end")
+        val spanEnd: PositionArea
+
+        @JsValue("span-inline-end")
+        val spanInlineEnd: PositionArea
+
+        @JsValue("span-inline-start")
+        val spanInlineStart: PositionArea
+
+        @JsValue("span-left")
+        val spanLeft: PositionArea
+
+        @JsValue("span-right")
+        val spanRight: PositionArea
+
+        @JsValue("span-self-block-end")
+        val spanSelfBlockEnd: PositionArea
+
+        @JsValue("span-self-block-start")
+        val spanSelfBlockStart: PositionArea
+
+        @JsValue("span-self-end")
+        val spanSelfEnd: PositionArea
+
+        @JsValue("span-self-inline-end")
+        val spanSelfInlineEnd: PositionArea
+
+        @JsValue("span-self-inline-start")
+        val spanSelfInlineStart: PositionArea
+
+        @JsValue("span-self-start")
+        val spanSelfStart: PositionArea
+
+        @JsValue("span-start")
+        val spanStart: PositionArea
+
+        @JsValue("span-top")
+        val spanTop: PositionArea
+
+        @JsValue("span-x-end")
+        val spanXEnd: PositionArea
+
+        @JsValue("span-x-self-end")
+        val spanXSelfEnd: PositionArea
+
+        @JsValue("span-x-self-start")
+        val spanXSelfStart: PositionArea
+
+        @JsValue("span-x-start")
+        val spanXStart: PositionArea
+
+        @JsValue("span-y-end")
+        val spanYEnd: PositionArea
+
+        @JsValue("span-y-self-end")
+        val spanYSelfEnd: PositionArea
+
+        @JsValue("span-y-self-start")
+        val spanYSelfStart: PositionArea
+
+        @JsValue("span-y-start")
+        val spanYStart: PositionArea
+
+        @JsValue("start")
+        val start: PositionArea
+
+        @JsValue("top")
+        val top: PositionArea
+
+        @JsValue("x-end")
+        val xEnd: PositionArea
+
+        @JsValue("x-self-end")
+        val xSelfEnd: PositionArea
+
+        @JsValue("x-self-start")
+        val xSelfStart: PositionArea
+
+        @JsValue("x-start")
+        val xStart: PositionArea
+
+        @JsValue("y-end")
+        val yEnd: PositionArea
+
+        @JsValue("y-self-end")
+        val ySelfEnd: PositionArea
+
+        @JsValue("y-self-start")
+        val ySelfStart: PositionArea
+
+        @JsValue("y-start")
+        val yStart: PositionArea
+    }
+}
