7dee901dba439eeb2b0a1eb497ba757e0bd0ca85
Victor Turansky <victor.turansky@gmail.com>
2025-11-19 14:34:28 +0100

    JS. Remove redundant imports

--- kotlin-js/src/webMain/kotlin/js/disposable/internal/awaitFirst.kt
+++ kotlin-js/src/webMain/kotlin/js/disposable/internal/awaitFirst.kt
@@ -6,8 +6,6 @@ import js.objects.ReadonlyRecord
 import js.promise.Promise
 import js.promise.await
 import js.reflect.unsafeCast
-import kotlin.collections.filterNotNull
-import kotlin.collections.firstNotNullOf
 import kotlin.js.JsAny
 
 internal suspend fun JsAny.awaitFirst(
