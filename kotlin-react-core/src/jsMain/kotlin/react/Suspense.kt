fc08729b6186e83a676733ba67c2d0f8203e457d
Victor Turansky <victor.turansky@gmail.com>
2025-11-22 22:47:10 +0100

    React. Add `Suspense.defer` prop

--- kotlin-react-core/src/jsMain/kotlin/react/Suspense.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/Suspense.kt
@@ -6,6 +6,14 @@ external interface SuspenseProps :
     PropsWithChildren {
 
     var fallback: ReactNode?
+
+    /**
+     * The presence of this prop indicates that the content is computationally expensive to render.
+     * In other words, the tree is CPU bound and not I/O bound (e.g. due to fetching data).
+     *
+     * [https://github.com/facebook/react/pull/19936](https://github.com/facebook/react/pull/19936)
+     */
+    var defer: Boolean?
 }
 
 /**
