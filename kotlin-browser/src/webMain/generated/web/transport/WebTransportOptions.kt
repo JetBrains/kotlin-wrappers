3967571f93691a248d42778dcd09b7c9b8fdb092
Victor Turansky <victor.turansky@gmail.com>
2025-12-20 11:59:52 +0100

    Web Types `0.0.304` [generated]

--- kotlin-browser/src/webMain/generated/web/transport/WebTransportOptions.kt
+++ kotlin-browser/src/webMain/generated/web/transport/WebTransportOptions.kt
@@ -4,11 +4,13 @@ package web.transport
 
 import js.array.ReadonlyArray
 import js.objects.JsPlainObject
+import kotlin.js.JsString
 
 @JsPlainObject
 external interface WebTransportOptions {
     var allowPooling: Boolean?
     var congestionControl: WebTransportCongestionControl?
+    var protocols: ReadonlyArray<JsString>?
     var requireUnreliable: Boolean?
     var serverCertificateHashes: ReadonlyArray<WebTransportHash>?
 }
