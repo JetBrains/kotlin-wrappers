33b6e6a9124f507def0555a5de51728a5d6f08f6
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-12-26 18:40:06 +0200

    Karakum `1.0.0-alpha.84`

--- kotlin-node/karakum/build.gradle.kts
+++ kotlin-node/karakum/build.gradle.kts
@@ -3,7 +3,5 @@ plugins {
 }
 
 dependencies {
-    // TODO: temporal solution, bump Arrow version after stable release
-    jsMainImplementation("io.arrow-kt:arrow-core:2.2.0-beta.3")
     jsMainImplementation(npm(jspkg.types.node))
 }
