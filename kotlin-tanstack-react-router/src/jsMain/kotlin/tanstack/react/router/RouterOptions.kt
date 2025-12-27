8cbbd0c5d472d83335ed4a9885e01a546223a76c
Victor Turansky <victor.turansky@gmail.com>
2025-12-27 04:19:07 +0100

    TanStack React Router. `RouterOptions`

---
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/RouterOptions.kt
@@ -0,0 +1,75 @@
+package tanstack.react.router
+
+import js.errors.JsError
+import js.objects.JsPlainObject
+import react.ComponentType
+import react.ErrorInfo
+import react.PropsWithChildren
+
+@JsPlainObject
+external interface RouterOptions {
+    /**
+     * The default `component` a route should use if no component is provided.
+     *
+     * `default` - Outlet
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaultcomponent-property)
+     */
+    var defaultComponent: RouteComponent?
+
+    /**
+     * The default `errorComponent` a route should use if no error component is provided.
+     *
+     * `default` - ErrorComponent
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaulterrorcomponent-property)
+     * [Guide](https://tanstack.com/router/latest/docs/framework/react/guide/data-loading#handling-errors-with-routeoptionserrorcomponent)
+     */
+    var defaultErrorComponent: ErrorRouteComponent?
+
+    /**
+     * The default `pendingComponent` a route should use if no pending component is provided.
+     *
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaultpendingcomponent-property)
+     * [Guide](https://tanstack.com/router/latest/docs/framework/react/guide/data-loading#showing-a-pending-component)
+     */
+    var defaultPendingComponent: RouteComponent?
+
+    /**
+     * The default `notFoundComponent` a route should use if no notFound component is provided.
+     *
+     * `default` - NotFound
+     *
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaultnotfoundcomponent-property)
+     * [Guide](https://tanstack.com/router/latest/docs/framework/react/guide/not-found-errors#default-router-wide-not-found-handling)
+     */
+    var defaultNotFoundComponent: NotFoundRouteComponent?
+
+    /**
+     * A component that will be used to wrap the entire router.
+     *
+     * This is useful for providing a context to the entire router.
+     *
+     * Only non-DOM-rendering components like providers should be used, anything else will cause a hydration error.
+     *
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#wrap-property)
+     */
+    var Wrap: ComponentType<PropsWithChildren>?
+
+    /**
+     * A component that will be used to wrap the inner contents of the router.
+     *
+     * This is useful for providing a context to the inner contents of the router where you also need access to the router context and hooks.
+     *
+     * Only non-DOM-rendering components like providers should be used, anything else will cause a hydration error.
+     *
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#innerwrap-property)
+     */
+    var InnerWrap: ComponentType<PropsWithChildren>?
+
+    /**
+     * The default `onCatch` handler for errors caught by the Router ErrorBoundary
+     *
+     * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RouterOptionsType#defaultoncatch-property)
+     * [Guide](https://tanstack.com/router/latest/docs/framework/react/guide/data-loading#handling-errors-with-routeoptionsoncatch)
+     */
+    var defaultOnCatch: ((error: JsError, errorInfo: ErrorInfo) -> Unit)?
+}
