3337334be55299615d51e44204125f6449d56369
Victor Turansky <victor.turansky@gmail.com>
2025-12-28 03:21:39 +0100

    TanStack React Router. `useCanGoBack`

---
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useCanGoBack.kt
@@ -0,0 +1,8 @@
+@file:JsModule("@tanstack/react-router")
+
+package tanstack.react.router
+
+/**
+ * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/useCanGoBack)
+ */
+external fun useCanGoBack(): Boolean
