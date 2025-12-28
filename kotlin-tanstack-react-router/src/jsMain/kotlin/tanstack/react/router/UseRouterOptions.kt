291b3dc43066cc7161471440f16e0fda240422b7
Victor Turansky <victor.turansky@gmail.com>
2025-12-28 03:32:01 +0100

    TanStack React Router. `useRouter`

---
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/UseRouterOptions.kt
@@ -0,0 +1,8 @@
+package tanstack.react.router
+
+import js.objects.JsPlainObject
+
+@JsPlainObject
+external interface UseRouterOptions {
+    val warn: Boolean?
+}
---
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useRouter.kt
@@ -0,0 +1,18 @@
+@file:JsModule("@tanstack/react-router")
+
+package tanstack.react.router
+
+/**
+ * Access the current TanStack Router instance from React context.
+ * Must be used within a `RouterProvider`.
+ *
+ * Options:
+ * - `warn`: Log a warning if no router context is found (default: true).
+ *
+ * [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/useRouterHook)
+ *
+ * @return The registered router instance.
+ */
+external fun useRouter(
+    options: UseRouterOptions = definedExternally,
+): Router
