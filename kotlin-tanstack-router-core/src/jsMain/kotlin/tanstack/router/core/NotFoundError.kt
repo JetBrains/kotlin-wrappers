cfb3069cb7fa3267d1a7beb9640df89475e343fb
Victor Turansky <victor.turansky@gmail.com>
2025-12-29 03:47:48 +0100

    TanStack Router Core. Mark `NotFoundError` as error-like

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/NotFoundError.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/NotFoundError.kt
@@ -1,10 +1,12 @@
 package tanstack.router.core
 
+import js.errors.JsErrorLike
 import js.objects.JsPlainObject
 import web.http.Headers
 
 @JsPlainObject
-external interface NotFoundError {
+external interface NotFoundError :
+    JsErrorLike {
     val data: Any?
     val `throw`: Boolean?
     val routeId: RouteId?
