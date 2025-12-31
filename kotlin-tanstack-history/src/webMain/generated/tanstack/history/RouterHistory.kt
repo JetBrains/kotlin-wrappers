752a33eca6dfc9d481dce3e2412f249b5de3d449
Victor Turansky <victor.turansky@gmail.com>
2025-12-31 02:24:20 +0100

    TanStack History. Immutable `RouterHistory`

--- kotlin-tanstack-history/src/webMain/generated/tanstack/history/RouterHistory.kt
+++ kotlin-tanstack-history/src/webMain/generated/tanstack/history/RouterHistory.kt
@@ -5,28 +5,30 @@ package tanstack.history
 import js.collections.JsSet
 
 external interface RouterHistory {
-    var location: HistoryLocation
-    var length: Int
-    var subscribers: JsSet<(opts: SubscriberArgs) -> Unit>
-    var subscribe: (cb: (opts: SubscriberArgs) -> Unit) -> () -> Unit
-    var push: (
+    val location: HistoryLocation
+    val length: Int
+    val subscribers: JsSet<(opts: SubscriberArgs) -> Unit>
+    fun subscribe(cb: (opts: SubscriberArgs) -> Unit): () -> Unit
+    fun push(
         path: String,
-        state: Any?, /* use undefined for default */
-        navigateOpts: NavigateOptions?, // use undefined for default
-    ) -> Unit
-    var replace: (
+        state: Any? = definedExternally,
+        navigateOpts: NavigateOptions? = definedExternally,
+    )
+
+    fun replace(
         path: String,
-        state: Any?, /* use undefined for default */
-        navigateOpts: NavigateOptions?, // use undefined for default
-    ) -> Unit
-    var go: (index: Int, navigateOpts: NavigateOptions? /* use undefined for default */) -> Unit
-    var back: (navigateOpts: NavigateOptions? /* use undefined for default */) -> Unit
-    var forward: (navigateOpts: NavigateOptions? /* use undefined for default */) -> Unit
-    var canGoBack: () -> Boolean
-    var createHref: (href: String) -> String
-    var block: (blocker: NavigationBlocker) -> () -> Unit
-    var flush: () -> Unit
-    var destroy: () -> Unit
-    var notify: (action: SubscriberHistoryAction) -> Unit
-    var _ignoreSubscribers: Boolean?
+        state: Any? = definedExternally,
+        navigateOpts: NavigateOptions? = definedExternally,
+    )
+
+    fun go(index: Int, navigateOpts: NavigateOptions? = definedExternally)
+    fun back(navigateOpts: NavigateOptions? = definedExternally)
+    fun forward(navigateOpts: NavigateOptions? = definedExternally)
+    fun canGoBack(): Boolean
+    fun createHref(href: String): String
+    fun block(blocker: NavigationBlocker): () -> Unit
+    fun flush()
+    fun destroy()
+    fun notify(action: SubscriberHistoryAction)
+    val _ignoreSubscribers: Boolean?
 }
