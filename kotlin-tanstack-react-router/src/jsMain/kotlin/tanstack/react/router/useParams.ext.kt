0c7b9084c0f41a8f626816b47b66773d977c1b7e
Victor Turansky <victor.turansky@gmail.com>
2025-12-31 02:03:14 +0100

    TanStack React Router. `Select` in hooks

--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useParams.ext.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useParams.ext.kt
@@ -4,3 +4,8 @@ import tanstack.router.core.Params
 
 fun useParams(): Params =
     useParams(UseParamsOptions(strict = false))
+
+fun <T> useParams(
+    select: Select<Params, T>,
+): T =
+    useParams(UseParamsOptions(strict = false, select = select))
--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useSearch.ext.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useSearch.ext.kt
@@ -4,3 +4,8 @@ import tanstack.router.core.Search
 
 fun useSearch(): Search =
     useSearch(UseSearchOptions(strict = false))
+
+fun <T> useSearch(
+    select: Select<Search, T>,
+): T =
+    useSearch(UseSearchOptions(strict = false, select = select))
