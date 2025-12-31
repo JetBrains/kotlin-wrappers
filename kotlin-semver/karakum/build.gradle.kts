1da3802de86af982fdba432575802aab43862bc4
sergeigrishchenko <s.i.grishchenko@gmail.com>
2025-12-31 17:02:41 +0200

    Karakum `1.0.0-alpha.93`

--- kotlin-semver/karakum/build.gradle.kts
+++ kotlin-semver/karakum/build.gradle.kts
@@ -2,6 +2,6 @@ plugins {
     id("generatorbuild.karakum-conventions")
 }
 
-dependencies {
-    jsMainImplementation(npm(jspkg.types.semver))
+karakum {
+    library(jspkg.types.semver)
 }
--- kotlin-semver/karakum/src/jsMain/kotlin/semver/karakum/main.kt
+++ kotlin-semver/karakum/src/jsMain/kotlin/semver/karakum/main.kt
@@ -1,25 +1,16 @@
 package semver.karakum
 
-import io.github.sgrishchenko.karakum.configuration.InputResolutionStrategy
 import io.github.sgrishchenko.karakum.configuration.NamespaceStrategy
 import io.github.sgrishchenko.karakum.configuration.ignore
-import io.github.sgrishchenko.karakum.configuration.plain
 import io.github.sgrishchenko.karakum.generate
 import io.github.sgrishchenko.karakum.util.manyOf
 import js.array.ReadonlyArray
-import js.import.import
 import js.objects.recordOf
-import node.module.findPackageJSON
-import node.path.path
 import semver.karakum.annotations.annotateJsPlainObject
 import semver.karakum.inheritanceModifiers.modifyMethodInheritance
 import semver.karakum.plugins.*
 
 suspend fun main(args: ReadonlyArray<String>) {
-    val semverPackage = findPackageJSON("@types/semver", import.meta.url)
-        .let { requireNotNull(it) }
-        .let { path.dirname(it) }
-
     generate(args) {
         plugins = manyOf(
             convertCoerceParameterType,
@@ -35,16 +26,14 @@ suspend fun main(args: ReadonlyArray<String>) {
             ::modifyMethodInheritance
         )
 
-        inputResolutionStrategy = InputResolutionStrategy.plain
-        input = manyOf("$semverPackage/**/*.d.ts")
-        ignoreInput = manyOf("$semverPackage/internals/*")
+        input = manyOf("**/*.d.ts")
+        ignoreInput = manyOf("**/internals/*")
         ignoreOutput = manyOf(
             "**/semver/ComparatorOperator.kt",
             "**/semver/OutsideHilo.kt",
             "**/semver/Operator.kt",
             "**/semver/preload.kt",
         )
-        libraryName = "semver"
         libraryNameOutputPrefix = true
         packageNameMapper = recordOf(
             "^.*/([^/]+\\.kt)$" to "semver/$1",
