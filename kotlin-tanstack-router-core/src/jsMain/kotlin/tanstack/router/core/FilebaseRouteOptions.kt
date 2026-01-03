d1ece3d286cf10ffdeb03ae9e2a421628e971e85
Victor Turansky <victor.turansky@gmail.com>
2026-01-03 18:32:02 +0100

    TanStack Router Core. Extract `FilebaseRouteOptions`

---
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/FilebaseRouteOptions.kt
@@ -0,0 +1,16 @@
+package tanstack.router.core
+
+import js.objects.JsPlainObject
+import js.promise.PromiseResult
+
+@JsPlainObject
+sealed external interface FilebaseRouteOptions {
+    val validateSearch: ((rawSearch: Search) -> Search)?
+    val shouldReload: ((context: LoaderFnContext) -> Boolean?)? /* | Boolean */
+    val context: ((options: RouteContextOptions) -> Context?)?
+    val ssr: ((options: SsrContextOptions) -> PromiseResult<SSROption?>)?
+    val beforeLoad: ((options: BeforeLoadContextOptions) -> PromiseResult<Context?>?)?
+    val loaderDeps: ((options: FullSearchSchemaOption) -> LoaderDeps?)?
+    val remountDeps: ((options: RemountDepsOptions) -> Unit)?
+    val loader: ((options: LoaderFnContext) -> PromiseResult<Any? /* LoaderData */>?)?
+}
--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouteOptions.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouteOptions.kt
@@ -1,10 +1,10 @@
 package tanstack.router.core
 
 import js.objects.JsPlainObject
-import js.promise.PromiseResult
 
 @JsPlainObject
 external interface RouteOptions<TParentRoute : AnyRoute> :
+    FilebaseRouteOptions,
     UpdatableRouteOptions {
     // BaseRouteOptions
     val getParentRoute: () -> TParentRoute
@@ -21,14 +21,4 @@ external interface RouteOptions<TParentRoute : AnyRoute> :
         val parse: ((rawParams: String) -> Params)?
         val stringify: ((params: Params) -> String)?
     }
-
-    // FilebaseRouteOptionsInterface
-    val validateSearch: ((rawSearch: Search) -> Search)?
-    val shouldReload: ((context: LoaderFnContext) -> Boolean?)? /* | Boolean */
-    val context: ((options: RouteContextOptions) -> Context?)?
-    val ssr: ((options: SsrContextOptions) -> PromiseResult<SSROption?>)?
-    val beforeLoad: ((options: BeforeLoadContextOptions) -> PromiseResult<Context?>?)?
-    val loaderDeps: ((options: FullSearchSchemaOption) -> LoaderDeps?)?
-    val remountDeps: ((options: RemountDepsOptions) -> Unit)?
-    val loader: ((options: LoaderFnContext) -> PromiseResult<Any? /* LoaderData */>?)?
 }
