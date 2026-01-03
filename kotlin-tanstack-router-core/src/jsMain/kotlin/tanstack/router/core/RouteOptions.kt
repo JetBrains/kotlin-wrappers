014063100a0552e055cc3d820548dfdf34145215
Victor Turansky <victor.turansky@gmail.com>
2026-01-03 03:56:06 +0100

    TanStack Router Core. Update `RouteOptions`

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouteOptions.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouteOptions.kt
@@ -4,9 +4,21 @@ import js.objects.JsPlainObject
 
 @JsPlainObject
 external interface RouteOptions<TParentRoute : BaseRoute<*>> {
+    // BaseRouteOptions
     val getParentRoute: () -> TParentRoute
+
+    // RoutePathOptions
     val path: RoutePath?
     val id: RouteId?
+
+    // ParamsOptions
+    val params: Params?
+
+    @JsPlainObject
+    interface Params {
+        val parse: ((rawParams: String) -> Params)?
+        val stringify: ((params: Params) -> String)?
+    }
 
     // TBD
 }
