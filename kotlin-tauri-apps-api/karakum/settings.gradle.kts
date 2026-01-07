8ac783dcb02081306e9eb5ce92aad30ad166d075
mani123 <support@worldmandia.cc>
2026-01-07 19:22:16 +0100

    Tauri API. Empty Generator

---
+++ kotlin-tauri-apps-api/karakum/settings.gradle.kts
@@ -0,0 +1,11 @@
+rootProject.name = "kotlin-tauri-apps-api-karakum"
+
+pluginManagement {
+    includeBuild("../../build-logic-settings")
+}
+
+plugins {
+    id("generatorbuild.settings")
+}
+
+includeBuild("../../build-logic")
---
+++ kotlin-tauri-apps-api/karakum/src/webMain/kotlin/karakum/tauri/apps/api/main.kt
@@ -0,0 +1,7 @@
+package karakum.tauri.apps.api
+
+import js.array.ReadonlyArray
+
+suspend fun main(args: ReadonlyArray<String>) {
+    println("Generate!")
+}
