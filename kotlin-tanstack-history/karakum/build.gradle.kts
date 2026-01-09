8c40957bc2406969dc0589c0cd9fe14d4dbca89b
Victor Turansky <victor.turansky@gmail.com>
2026-01-09 21:53:00 +0100

    TanStack History. Temp generation fixes

--- kotlin-tanstack-history/karakum/build.gradle.kts
+++ kotlin-tanstack-history/karakum/build.gradle.kts
@@ -5,3 +5,49 @@ plugins {
 karakum {
     library(jspkg.tanstack.history)
 }
+
+tasks.generateKarakumExternals {
+    doLast {
+        addTempGenerationFixes()
+    }
+}
+
+fun addTempGenerationFixes() {
+    sequenceOf(
+        "BlockerFnArgs.kt",
+        "CreateBrowserHistoryOpts.kt",
+        "CreateHashHistoryOpts.kt",
+        "CreateHistoryOpts.kt",
+        "CreateMemoryHistoryOpts.kt",
+        "HistoryLocation.kt",
+        "NavigateOptions.kt",
+        "NavigationBlocker.kt",
+        "ParsedHistoryState.kt",
+        "ParsedPath.kt",
+        "RouterHistory.kt",
+        "SubscriberArgs.kt",
+    ).forEach { jsoPath ->
+        addFix(jsoPath) {
+            it.replaceFirst(
+                "\nexternal interface ",
+                "\nimport js.objects.JsPlainObject\n\n@JsPlainObject" +
+                        "\nexternal interface ",
+            )
+        }
+    }
+
+
+}
+
+fun addFix(
+    path: String,
+    transform: (String) -> String,
+) {
+    val file = file("../src/webMain/generated/tanstack/history/$path")
+    val oldContent = file.readText()
+    val newContent = transform(oldContent)
+    check(oldContent != newContent) {
+        "Transformation fo path `$path` changed nothing!"
+    }
+    file.writeText(newContent)
+}
