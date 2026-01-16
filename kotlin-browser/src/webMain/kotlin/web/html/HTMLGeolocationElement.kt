a0c78cfb356e0fba4d15a9fc7fbac1d7ee8ded0f
Victor Turansky <victor.turansky@gmail.com>
2026-01-16 02:11:05 +0100

    Permission. `HTMLGeolocationElement` and `HTMLPermissionElement`

---
+++ kotlin-browser/src/webMain/kotlin/web/html/HTMLGeolocationElement.kt
@@ -0,0 +1,10 @@
+package web.html
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLGeolocationElement)
+ */
+open external class HTMLGeolocationElement
+protected constructor() :
+    HTMLElement {
+    // ...
+}
---
+++ kotlin-browser/src/webMain/kotlin/web/html/HTMLPermissionElement.kt
@@ -0,0 +1,10 @@
+package web.html
+
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLPermissionElement)
+ */
+open external class HTMLPermissionElement
+protected constructor() :
+    HTMLElement {
+    // ...
+}
