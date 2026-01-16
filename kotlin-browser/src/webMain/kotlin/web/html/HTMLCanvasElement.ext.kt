3cebc18880a6fe30458a5c25fdef1762cccbfaed
Victor Turansky <victor.turansky@gmail.com>
2026-01-16 02:31:44 +0100

    HTML. Non-nullable result for `HTMLCanvasElement.toBlob()`

--- kotlin-browser/src/webMain/kotlin/web/html/HTMLCanvasElement.ext.kt
+++ kotlin-browser/src/webMain/kotlin/web/html/HTMLCanvasElement.ext.kt
@@ -1,21 +1,34 @@
 package web.html
 
+import js.coroutines.resumeWithError
 import web.blob.Blob
+import web.errors.DOMException
+import web.errors.OperationError
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
 private fun BlobCallback(
-    continuation: Continuation<Blob?>,
-): BlobCallback =
-    continuation::resume
+    continuation: Continuation<Blob>,
+): BlobCallback = { blob ->
+    if (blob != null) {
+        continuation.resume(blob)
+    } else {
+        val error = DOMException(
+            message = "Unable to create blob from canvas",
+            name = DOMException.OperationError,
+        )
+
+        continuation.resumeWithError(error)
+    }
+}
 
 /**
  * The **`HTMLCanvasElement.toBlob()`** method creates a Blob object representing the image contained in the canvas. This file may be cached on the disk or stored in memory at the discretion of the user agent.
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/HTMLCanvasElement/toBlob)
  */
-suspend fun HTMLCanvasElement.toBlob(): Blob? =
+suspend fun HTMLCanvasElement.toBlob(): Blob =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = BlobCallback(continuation),
@@ -29,7 +42,7 @@ suspend fun HTMLCanvasElement.toBlob(): Blob? =
  */
 suspend fun HTMLCanvasElement.toBlob(
     type: String,
-): Blob? =
+): Blob =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = BlobCallback(continuation),
@@ -45,7 +58,7 @@ suspend fun HTMLCanvasElement.toBlob(
 suspend fun HTMLCanvasElement.toBlob(
     type: String,
     quality: Double,
-): Blob? =
+): Blob =
     suspendCoroutine { continuation ->
         toBlobWithCallback(
             callback = BlobCallback(continuation),
