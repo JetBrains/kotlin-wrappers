385502e65e68a8ae53e6329d320c56a1db6db572
Victor Turansky <victor.turansky@gmail.com>
2026-01-23 12:58:49 +0100

    Service Worker Types `0.0.180` [generated]

--- kotlin-browser/src/webMain/generated/web/workers/importScripts.kt
+++ kotlin-browser/src/webMain/generated/web/workers/importScripts.kt
@@ -5,7 +5,7 @@ package web.workers
 import web.url.URL
 
 /**
- * The **`importScripts()`** method of the WorkerGlobalScope interface synchronously imports one or more scripts into the worker's scope.
+ * The **`importScripts()`** method of the WorkerGlobalScope interface synchronously imports one or more scripts into the scope of a classic worker (a worker constructed from a classic script).
  *
  * [MDN Reference](https://developer.mozilla.org/docs/Web/API/WorkerGlobalScope/importScripts)
  */
