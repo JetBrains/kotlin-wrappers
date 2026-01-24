0e63e5861e3f8d5f3392c5f4d18aa8a03f326481
Victor Turansky <victor.turansky@gmail.com>
2026-01-24 03:43:46 +0100

    WebXR. Types dependency

--- kotlin-browser/karakum/build.gradle.kts
+++ kotlin-browser/karakum/build.gradle.kts
@@ -7,6 +7,7 @@ dependencies {
     commonMainImplementation(npm(jspkg.types.webworker))
     commonMainImplementation(npm(jspkg.types.serviceworker))
     commonMainImplementation(npm(jspkg.types.audioworklet))
+    commonMainImplementation(npm(jspkg.types.webxr))
     commonMainImplementation(npm(jspkg.typescript))
     commonMainImplementation(npm(jspkg.webref.events))
     commonMainImplementation(npm(jspkg.webref.idl))
