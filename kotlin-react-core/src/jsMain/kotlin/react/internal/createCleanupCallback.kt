6833f9f5bc13d0ece817dbfe9449bdc33d5c82bd
Victor Turansky <victor.turansky@gmail.com>
2026-01-28 00:51:03 +0100

    React. `createCleanupCallback` overload for 2 parameters

--- kotlin-react-core/src/jsMain/kotlin/react/internal/createCleanupCallback.kt
+++ kotlin-react-core/src/jsMain/kotlin/react/internal/createCleanupCallback.kt
@@ -13,10 +13,20 @@ internal fun createCleanupCallback(
     }
 }
 
-internal fun <T> createCleanupCallback(
-    block: suspend CoroutineScope.(T) -> Unit,
-): (T) -> Cleanup = callback@{ p1 ->
+internal fun <P1> createCleanupCallback(
+    block: suspend CoroutineScope.(P1) -> Unit,
+): (P1) -> Cleanup = callback@{ p1 ->
     val job = isolatedJob { block(p1) }
+
+    return@callback {
+        job.cancel()
+    }
+}
+
+internal fun <P1, P2> createCleanupCallback(
+    block: suspend CoroutineScope.(P1, P2) -> Unit,
+): (P1, P2) -> Cleanup = callback@{ p1, p2 ->
+    val job = isolatedJob { block(p1, p2) }
 
     return@callback {
         job.cancel()
