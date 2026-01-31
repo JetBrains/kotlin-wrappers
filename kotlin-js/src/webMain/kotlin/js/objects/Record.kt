51d57c5e77d5e8d54ac7df993348e2611c1b0da2
Victor Turansky <victor.turansky@gmail.com>
2026-01-31 22:12:03 +0100

    Objects. Fix formatting

--- kotlin-js/src/webMain/kotlin/js/objects/Record.kt
+++ kotlin-js/src/webMain/kotlin/js/objects/Record.kt
@@ -38,22 +38,22 @@ fun <K : JsAny, V : JsAny?> Record(
 ): Record<K, V> =
     unsafeJso(block)
 
-fun <K: JsAny, V : JsAny?> Sequence<Tuple2<K, V>>.toRecord(): Record<K, V> =
+fun <K : JsAny, V : JsAny?> Sequence<Tuple2<K, V>>.toRecord(): Record<K, V> =
     Record {
         forEach { (key, value) -> set(key, value) }
     }
 
-fun <K: JsAny, V : JsAny?> Sequence<Pair<K, V>>.toRecord(): Record<K, V> =
+fun <K : JsAny, V : JsAny?> Sequence<Pair<K, V>>.toRecord(): Record<K, V> =
     Record {
         forEach { (key, value) -> set(key, value) }
     }
 
-fun <K: JsAny, V : JsAny?> Iterable<Pair<K, V>>.toRecord(): Record<K, V> =
+fun <K : JsAny, V : JsAny?> Iterable<Pair<K, V>>.toRecord(): Record<K, V> =
     Record {
         forEach { (key, value) -> set(key, value) }
     }
 
-fun <K: JsAny, V : JsAny?> Map<K, V>.toRecord(): Record<K, V> =
+fun <K : JsAny, V : JsAny?> Map<K, V>.toRecord(): Record<K, V> =
     Record {
         forEach { (key, value) -> set(key, value) }
     }
