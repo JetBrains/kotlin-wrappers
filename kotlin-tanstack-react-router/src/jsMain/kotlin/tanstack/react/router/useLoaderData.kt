38e42269433de2994b9f204d882ec17bbf900e7b
Victor Turansky <victor.turansky@gmail.com>
2026-02-02 04:30:33 +0100

    TanStack React Router. Extract `UseLoaderDataOptions`

--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useLoaderData.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useLoaderData.kt
@@ -2,18 +2,6 @@
 
 package tanstack.react.router
 
-import js.objects.JsPlainObject
-import tanstack.router.core.LoaderData
-import tanstack.router.core.RouteId
-
-@JsPlainObject
-internal external interface UseLoaderDataOptions<T> {
-    val from: RouteId? /* RoutePath */
-    val strict: Boolean?
-    val select: Select<LoaderData?, T>?
-    val structuralSharing: Boolean?
-}
-
 /**
  * Read and select the current route's loader data with type‑safety.
  *
