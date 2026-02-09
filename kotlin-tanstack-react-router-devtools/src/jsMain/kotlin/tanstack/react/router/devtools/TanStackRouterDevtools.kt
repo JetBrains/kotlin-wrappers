9fca83495a919a413b9b357bc575d76c5dc55c08
Alexander Popoff <alexander.popoff@flysto.net>
2026-02-09 21:06:44 +0200

    TanStack React Router Devtools

--- kotlin-tanstack-react-router-devtools/src/jsMain/kotlin/tanstack/react/router/devtools/TanStackRouterDevtools.kt
+++ kotlin-tanstack-react-router-devtools/src/jsMain/kotlin/tanstack/react/router/devtools/TanStackRouterDevtools.kt
@@ -4,16 +4,20 @@ package tanstack.react.router.devtools
 
 import react.FC
 import react.Props
+import react.dom.html.ButtonHTMLAttributes
+import react.dom.html.HTMLAttributes
 import tanstack.react.router.Router
 import web.components.ShadowRoot
+import web.html.HTMLButtonElement
+import web.html.HTMLDivElement
 
 external interface TanStackRouterDevtoolsProps :
     Props {
     var router: Router?
     var initialIsOpen: Boolean?
-    var panelProps: Props?
-    var closeButtonProps: Props?
-    var toggleButtonProps: Props?
+    var panelProps: HTMLAttributes<HTMLDivElement>?
+    var closeButtonProps: ButtonHTMLAttributes<HTMLButtonElement>?
+    var toggleButtonProps: ButtonHTMLAttributes<HTMLButtonElement>?
     var position: Position?
     var shadowDOMTarget: ShadowRoot?
     var containerElement: Any? /* or String */
