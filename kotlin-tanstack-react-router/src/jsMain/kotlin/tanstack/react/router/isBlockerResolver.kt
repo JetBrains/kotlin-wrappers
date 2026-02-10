91ce23fdac75e69399fed3d250c4adbf4924b819
Victor Turansky <victor.turansky@gmail.com>
2026-02-10 02:00:32 +0100

    TanStack React Router. Suppresses on top of file (#2836)

--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/isBlockerResolver.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/isBlockerResolver.kt
@@ -1,11 +1,12 @@
+@file:Suppress(
+    "CANNOT_CHECK_FOR_EXTERNAL_INTERFACE",
+    "CANNOT_CHECK_FOR_ERASED",
+)
+
 package tanstack.react.router
 
 import kotlin.contracts.contract
 
-@Suppress(
-    "CANNOT_CHECK_FOR_EXTERNAL_INTERFACE",
-    "CANNOT_CHECK_FOR_ERASED",
-)
 internal inline fun isBlockerResolver(
     value: HasBlockerResolverStatus,
 ): Boolean {
