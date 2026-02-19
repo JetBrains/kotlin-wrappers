009f295a7ef464471d0789eb57d84e373cbe3149
Victor Turansky <victor.turansky@gmail.com>
2026-02-19 04:39:24 +0100

    WebMCP. `ModelContextInput`

---
+++ kotlin-browser/src/webMain/kotlin/web/mcp/ModelContextInput.kt
@@ -0,0 +1,17 @@
+package web.mcp
+
+import js.array.ReadonlyArray
+import kotlinx.js.JsPlainObject
+import web.experimental.ExperimentalWebApi
+
+/**
+ * Context provided to models via provideContext().
+ */
+@JsPlainObject
+@ExperimentalWebApi
+external interface ModelContextInput {
+    /**
+     * Base tool descriptors to expose.
+     */
+    val tools: ReadonlyArray<ToolDescriptor>?
+}
