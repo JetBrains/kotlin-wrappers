1f8212962ecaad3e144a0eda238fd2d0d878c4e5
Victor Turansky <victor.turansky@gmail.com>
2026-02-19 04:00:08 +0100

    WebMCP. `ToolName`

--- kotlin-browser/src/webMain/kotlin/web/mcp/ToolName.kt
+++ kotlin-browser/src/webMain/kotlin/web/mcp/ToolName.kt
@@ -2,10 +2,13 @@ package web.mcp
 
 import js.internal.InternalApi
 import js.reflect.unsafeCast
+import web.experimental.ExperimentalWebApi
 
+@ExperimentalWebApi
 @SubclassOptInRequired(InternalApi::class)
 external interface ToolName
 
+@ExperimentalWebApi
 inline fun ToolName(
     value: String,
 ): ToolName =
