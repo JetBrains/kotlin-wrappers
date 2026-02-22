b5c7b1e8c9032f58176c8f1d26f260d7ab7f176c
Victor Turansky <victor.turansky@gmail.com>
2026-02-22 22:04:12 +0100

    TanStack React Router. Update `redirect` signature

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/redirect.fun.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/redirect.fun.kt
@@ -1,16 +1,13 @@
 package tanstack.router.core
 
-import js.objects.Object
 import js.objects.unsafeJso
 import tanstack.router.core.raw.redirectRaw
 
 fun redirect(
-    options: RedirectOptions,
-): Nothing =
-    redirectRaw(
-        options = Object.assign(
-            unsafeJso(),
-            options,
-            unsafeJso { `throw` = true },
-        ),
-    )
+    block: RedirectOptions.() -> Unit,
+): Nothing {
+    val options = unsafeJso(block)
+    options.`throw` = true
+
+    redirectRaw<Nothing>(options)
+}
