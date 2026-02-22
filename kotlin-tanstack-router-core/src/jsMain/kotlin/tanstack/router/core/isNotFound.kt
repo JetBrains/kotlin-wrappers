d36c3fe4466f4c443fbbaf705fa71877c4c4bea3
Victor Turansky <victor.turansky@gmail.com>
2026-02-22 22:56:43 +0100

    TanStack Router Core. Contract for `isNotFound`

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/isNotFound.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/isNotFound.kt
@@ -1,10 +1,23 @@
 @file:JsModule("@tanstack/router-core")
 
+@file:Suppress(
+    "CANNOT_CHECK_FOR_EXTERNAL_INTERFACE",
+    "WRONG_BODY_OF_EXTERNAL_DECLARATION",
+)
+
 package tanstack.router.core
+
+import kotlin.contracts.contract
 
 /**
  * Determine if a value is a TanStack Router not-found error.
  */
 external fun isNotFound(
-    o: Any?,
-): Boolean
+    value: Any?,
+): Boolean {
+    contract {
+        returns(true) implies (value is NotFoundError)
+    }
+
+    return definedExternally
+}
