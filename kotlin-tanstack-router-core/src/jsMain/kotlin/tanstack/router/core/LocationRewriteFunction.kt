334bdf6f6435a7d3ce2cddd9c4b01f39267be8f0
Victor Turansky <victor.turansky@gmail.com>
2026-02-22 20:52:54 +0100

    TanStack Router Core. `LocationRewriteFunction` - invoker

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/LocationRewriteFunction.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/LocationRewriteFunction.kt
@@ -2,12 +2,17 @@ package tanstack.router.core
 
 import web.url.URL
 
-/**
- * A function that will be called to rewrite the URL.
- *
- * @param options.url The URL to rewrite.
- * @return The rewritten URL (as a URL instance or full href string) or `undefined` if no rewrite is needed.
- */
-typealias LocationRewriteFunction = (
-    options: LocationRewriteFunctionOptions,
-) -> URL?
+value class LocationRewriteFunction(
+    private val function: (LocationRewriteFunctionOptions) -> URL?,
+) {
+    /**
+     * A function that will be called to rewrite the URL.
+     *
+     * @param url The URL to rewrite.
+     * @return The rewritten URL (as a URL instance or full href string) or `undefined` if no rewrite is needed.
+     */
+    operator fun invoke(
+        url: URL,
+    ): URL? =
+        function(LocationRewriteFunctionOptions(url = url))
+}
