c9ff0adbba6c4638f055129dcb3ea47944838a64
Victor Turansky <victor.turansky@gmail.com>
2026-02-25 16:26:03 +0100

    Iterable. WasmJS compatible `AsyncIterator`

--- kotlin-js/src/webMain/kotlin/js/iterable/AsyncIterator.kt
+++ kotlin-js/src/webMain/kotlin/js/iterable/AsyncIterator.kt
@@ -1,12 +1,13 @@
 package js.iterable
 
 import js.disposable.AsyncDisposable
+import js.internal.InternalApi
 import js.reflect.JsExternalInheritorsOnly
 import kotlin.js.JsAny
 
-// TODO: add `@JsName`
 @JsExternalInheritorsOnly
-abstract external class AsyncIterator<out T : JsAny?> :
+@SubclassOptInRequired(InternalApi::class)
+external interface /* class */ AsyncIterator<out T : JsAny?> :
     AsyncIteratorLike<T>,
     AsyncIterable<T>,
     AsyncDisposable
