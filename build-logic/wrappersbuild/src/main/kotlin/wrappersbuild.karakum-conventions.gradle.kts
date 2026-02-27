dea45787c98830d3945601297ee0d366a5ebe600
Victor Turansky <victor.turansky@gmail.com>
2026-02-27 13:26:48 +0100

    Build. Remove redundant `afterEvaluate`

--- build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.karakum-conventions.gradle.kts
+++ build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.karakum-conventions.gradle.kts
@@ -6,24 +6,23 @@ fun hasKarakumGenerator(): Boolean {
     return kotlinDir.exists()
 }
 
-afterEvaluate {
-    if (hasKarakumGenerator()) {
-        val karakumProject = gradle.includedBuild("${project.name}-karakum")
+val karakumProject: IncludedBuild
+    get() = gradle.includedBuild("${project.name}-karakum")
 
-        val deleteKarakumPackageLock by tasks.registering(Delete::class) {
-            val lockFile = project.layout.projectDirectory
-                .file("karakum/kotlin-js-store/package-lock.json")
+if (hasKarakumGenerator()) {
+    val deleteKarakumPackageLock by tasks.registering(Delete::class) {
+        val lockFile = project.layout.projectDirectory
+            .file("karakum/kotlin-js-store/package-lock.json")
 
-            delete(lockFile)
-        }
+        delete(lockFile)
+    }
 
-        val cleanKarakum by tasks.registering {
-            dependsOn(deleteKarakumPackageLock)
-            dependsOn(karakumProject.task(":clean"))
-        }
+    val cleanKarakum by tasks.registering {
+        dependsOn(deleteKarakumPackageLock)
+        dependsOn(karakumProject.task(":clean"))
+    }
 
-        val buildKarakum by tasks.registering {
-            dependsOn(karakumProject.task(":build"))
-        }
+    val buildKarakum by tasks.registering {
+        dependsOn(karakumProject.task(":build"))
     }
 }
