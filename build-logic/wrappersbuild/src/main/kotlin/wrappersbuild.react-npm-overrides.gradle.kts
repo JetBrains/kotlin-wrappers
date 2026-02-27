94e6db6de7b529f8e32d149e0f9a3907a3cc0719
Victor Turansky <victor.turansky@gmail.com>
2026-02-27 03:05:34 +0100

    Security. Temp fix for `@prantlf/jsonlint`

--- build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.react-npm-overrides.gradle.kts
+++ build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.react-npm-overrides.gradle.kts
@@ -5,5 +5,9 @@ plugins.withType<NodeJsRootPlugin> {
     the<NpmExtension>().apply {
         override("react", prop("react.npm.version"))
         override("react-dom", prop("react-dom.npm.version"))
+
+        // TODO: remove after
+        //  https://github.com/prantlf/jsonlint/pull/44
+        override("ajv", prop("ajv.npm.version"))
     }
 }
