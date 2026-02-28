424a35c945591ca7cd4ad62c33272da0e37aea36
Victor Turansky <victor.turansky@gmail.com>
2026-02-28 02:34:33 +0100

    Revert "TanStack Router Core. Non-inline `RouterCore.load`"
    This reverts commit 037ae57f7b42ce1fd23e5cc0709343b9037fc1f9.

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouterCore.ext.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouterCore.ext.kt
@@ -3,11 +3,11 @@ package tanstack.router.core
 import js.objects.unsafeJso
 import js.promise.await
 
-suspend fun RouterCore.load() {
+suspend inline fun RouterCore.load() {
     loadAsync().await()
 }
 
-suspend fun RouterCore.load(
+suspend inline fun RouterCore.load(
     block: LoadOptions.() -> Unit,
 ) {
     loadAsync(unsafeJso(block)).await()
