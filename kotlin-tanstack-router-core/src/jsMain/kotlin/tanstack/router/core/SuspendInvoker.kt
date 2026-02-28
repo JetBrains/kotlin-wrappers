9963181bdacbec0dd403f4e27a0e7d78d1c8caac
Victor Turansky <victor.turansky@gmail.com>
2026-02-28 02:34:33 +0100

    Revert "TanStack Router Core. `SuspendInvoker` removed"
    This reverts commit 5077b29d8bb323ff47b209f8fde179e94a3fe0b8.

---
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/SuspendInvoker.kt
@@ -0,0 +1,18 @@
+package tanstack.router.core
+
+import js.objects.unsafeJso
+import js.promise.Promise
+import js.promise.await
+
+value class SuspendInvoker<O : Any, R>
+private constructor(
+    private val function: (O) -> Promise<R>,
+) {
+    suspend operator fun invoke(): R =
+        function(unsafeJso()).await()
+
+    suspend operator fun invoke(
+        block: O.() -> Unit,
+    ): R =
+        function(unsafeJso(block)).await()
+}
