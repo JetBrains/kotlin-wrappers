037ae57f7b42ce1fd23e5cc0709343b9037fc1f9
Victor Turansky <victor.turansky@gmail.com>
2026-02-28 01:05:54 +0100

    TanStack Router Core. Non-inline `RouterCore.load`

--- kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouterCore.ext.kt
+++ kotlin-tanstack-router-core/src/jsMain/kotlin/tanstack/router/core/RouterCore.ext.kt
@@ -3,11 +3,11 @@ package tanstack.router.core
 import js.objects.unsafeJso
 import js.promise.await
 
-suspend inline fun RouterCore.load() {
+suspend fun RouterCore.load() {
     loadAsync().await()
 }
 
-suspend inline fun RouterCore.load(
+suspend fun RouterCore.load(
     block: LoadOptions.() -> Unit,
 ) {
     loadAsync(unsafeJso(block)).await()
