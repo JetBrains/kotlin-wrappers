91b164f2c8fe0f5e0d647e446c31783d3744936c
Victor Turansky <victor.turansky@gmail.com>
2026-03-01 01:01:11 +0100

    Testing Library. 2 `waitFor` overloads

--- kotlin-testing-library-dom/src/webMain/kotlin/testing/library/dom/waitFor.kt
+++ kotlin-testing-library-dom/src/webMain/kotlin/testing/library/dom/waitFor.kt
@@ -1,20 +1,21 @@
 package testing.library.dom
 
 import js.function.unsafeAsync
-import js.promise.Promise
-import js.promise.PromiseLike
-import js.promise.PromiseResult
 import js.promise.await
 import kotlinx.coroutines.CoroutineScope
 
-fun <T> waitForAsync(
-    options: waitForOptions = undefined.unsafeCast<Nothing>(),
-    fn: () -> PromiseResult<T>,
-): Promise<T> =
-    waitForRaw(fn, options)
+suspend fun <T> waitFor(
+    block: suspend CoroutineScope.() -> T,
+): T =
+    waitForRaw(
+        callback = unsafeAsync(block),
+    ).await()
 
 suspend fun <T> waitFor(
-    options: waitForOptions = undefined.unsafeCast<Nothing>(),
-    fn: suspend CoroutineScope.() -> T,
+    options: waitForOptions,
+    block: suspend CoroutineScope.() -> T,
 ): T =
-    waitForAsync(options, unsafeAsync(fn)).await()
+    waitForRaw(
+        options = options,
+        callback = unsafeAsync(block),
+    ).await()
