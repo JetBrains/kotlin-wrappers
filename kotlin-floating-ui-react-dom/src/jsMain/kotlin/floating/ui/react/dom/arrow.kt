ed4cd1e8d6ecc6c0a4a4c29b4b9cca17f2fc50b5
Victor Turansky <victor.turansky@gmail.com>
2026-03-01 13:28:58 +0100

    Floating UI React DOM. `arrow`

---
+++ kotlin-floating-ui-react-dom/src/jsMain/kotlin/floating/ui/react/dom/arrow.kt
@@ -0,0 +1,16 @@
+package floating.ui.react.dom
+
+import floating.ui.core.Middleware
+import react.DependencyList
+
+/**
+ * Provides data to position an inner element of the floating element so that it
+ * appears centered to the reference element.
+ * This wraps the core `arrow` middleware to allow React refs as the element.
+ *
+ * [API Docs](https://floating-ui.com/docs/arrow)
+ */
+external fun arrow(
+    options: ArrowOptions, /* | Derivable<ArrowOptions> */
+    dependencies: DependencyList = definedExternally,
+): Middleware
