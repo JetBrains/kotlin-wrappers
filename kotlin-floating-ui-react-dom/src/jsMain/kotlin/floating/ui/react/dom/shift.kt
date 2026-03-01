b26784992def1d7f6e5c6f47e87bea713214988e
Victor Turansky <victor.turansky@gmail.com>
2026-03-01 14:10:15 +0100

    Floating UI React DOM. `shift`

---
+++ kotlin-floating-ui-react-dom/src/jsMain/kotlin/floating/ui/react/dom/shift.kt
@@ -0,0 +1,17 @@
+@file:JsModule("@floating-ui/react-dom")
+
+package floating.ui.react.dom
+
+import floating.ui.core.Middleware
+import floating.ui.core.ShiftOptions
+import react.DependencyList
+
+/**
+ * Optimizes the visibility of the floating element by shifting it in order to
+ * keep it in view when it will overflow the clipping boundary.
+ * [API Docs](https://floating-ui.com/docs/shift)
+ */
+external fun shift(
+    options: ShiftOptions, /* | Derivable<ShiftOptions> */
+    dependencies: DependencyList = definedExternally,
+): Middleware
