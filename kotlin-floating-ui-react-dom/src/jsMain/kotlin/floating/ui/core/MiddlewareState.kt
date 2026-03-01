dfaa324a06040d6bbd2f3af2111b8a56943d370a
Victor Turansky <victor.turansky@gmail.com>
2026-03-01 21:14:26 +0100

    Floating UI Core. `MiddlewareState`

--- kotlin-floating-ui-react-dom/src/jsMain/kotlin/floating/ui/core/MiddlewareState.kt
+++ kotlin-floating-ui-react-dom/src/jsMain/kotlin/floating/ui/core/MiddlewareState.kt
@@ -1,6 +1,7 @@
 package floating.ui.core
 
 import floating.ui.utils.Coords
+import floating.ui.utils.ElementRects
 import floating.ui.utils.Placement
 import floating.ui.utils.Strategy
 import kotlinx.js.JsPlainObject
@@ -11,5 +12,15 @@ external interface MiddlewareState :
     val initialPlacement: Placement
     val placement: Placement
     val strategy: Strategy
-    // TBD
+    val middlewareData: MiddlewareData
+
+    // val elements: Elements
+    val rects: ElementRects
+    val platform: Platform
+
+    @JsPlainObject
+    interface Platform :
+        floating.ui.core.Platform {
+        // val detectOverflow: typeof detectOverflow
+    }
 }
