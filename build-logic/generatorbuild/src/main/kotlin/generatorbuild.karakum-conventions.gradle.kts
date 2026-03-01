1e7af58bb9448e3077a51071808147e4f4bdabdb
Victor Turansky <victor.turansky@gmail.com>
2026-03-02 00:48:48 +0100

    Security. Exclude redundant NPM test dependencies

--- build-logic/generatorbuild/src/main/kotlin/generatorbuild.karakum-conventions.gradle.kts
+++ build-logic/generatorbuild/src/main/kotlin/generatorbuild.karakum-conventions.gradle.kts
@@ -1,7 +1,15 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+
 plugins {
     id("io.github.sgrishchenko.karakum")
 }
 
 karakum {
     output = layout.projectDirectory.dir("../src/webMain/generated")
+}
+
+// Exclude redundant NPM test dependencies
+plugins.withType<NodeJsRootPlugin> {
+    the<NodeJsRootExtension>().versions.mocha.version = "npm:empty-npm-package@1.0.0"
 }
