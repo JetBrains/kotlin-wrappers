94e91126f6ec465aa9f6ceb651afe8ea29f5ed34
Victor Turansky <victor.turansky@gmail.com>
2026-03-03 00:17:32 +0100

    Build. Remove redundant NPM overrides for React

--- build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.react-npm-overrides.gradle.kts
+++ build-logic/wrappersbuild/src/main/kotlin/wrappersbuild.react-npm-overrides.gradle.kts
@@ -3,9 +3,6 @@ import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
 
 plugins.withType<NodeJsRootPlugin> {
     the<NpmExtension>().apply {
-        override("react", prop("react.npm.version"))
-        override("react-dom", prop("react-dom.npm.version"))
-
         // TODO: remove after
         //  https://github.com/prantlf/jsonlint/pull/44
         override("ajv", prop("ajv.npm.version"))
