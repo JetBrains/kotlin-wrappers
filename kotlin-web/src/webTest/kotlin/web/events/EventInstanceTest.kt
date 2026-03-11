d3e7bf05780397a222f625ee1dfc1142e5f07a42
sergeigrishchenko <s.i.grishchenko@gmail.com>
2026-03-11 22:53:41 +0200

    Web Events. Remove using `subscribe` from tests, remove duplicated tests, improve test descriptions

--- kotlin-web/src/webTest/kotlin/web/events/EventInstanceTest.kt
+++ kotlin-web/src/webTest/kotlin/web/events/EventInstanceTest.kt
@@ -8,33 +8,13 @@ import kotlin.test.assertEquals
 
 class EventInstanceTest {
     @Test
-    fun `1 subscribe`() = runJsTest {
-        var a = 13
-        val target = EventTarget()
-
-        val job1 = target.changeEvent()
-            .subscribe { a++ }
-
-        assertEquals(13, a)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(14, a)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(15, a)
-
-        // stop
-        job1.cancel()
-    }
-
-    @Test
-    fun `1 launch.collect`() = runJsTest {
+    fun `should notify listener synchronously after dispatch event`() = runJsTest {
         var a = 13
         val target = EventTarget()
 
         val job1 = launch(Dispatchers.Unconfined) {
             target.changeEvent()
-                .subscribe { a++ }
+                .collect { a++ }
         }
 
         assertEquals(13, a)
@@ -50,42 +30,18 @@ class EventInstanceTest {
     }
 
     @Test
-    fun `2 subscribe`() = runJsTest {
-        val payloads = mutableListOf<Int>()
-        val target = EventTarget()
-
-        val job1 = target.changeEvent()
-            .subscribe { payloads.add(1) }
-
-        val job2 = target.changeEvent()
-            .subscribe { payloads.add(2) }
-
-        assertEquals(listOf(), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 2), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 2, 1, 2), payloads)
-
-        // stop
-        job1.cancel()
-        job2.cancel()
-    }
-
-    @Test
-    fun `2 launch.collect`() = runJsTest {
+    fun `should notify multiple listeners synchronously after dispatch event`() = runJsTest {
         val payloads = mutableListOf<Int>()
         val target = EventTarget()
 
         val job1 = launch(Dispatchers.Unconfined) {
             target.changeEvent()
-                .subscribe { payloads.add(1) }
+                .collect { payloads.add(1) }
         }
 
         val job2 = launch(Dispatchers.Unconfined) {
             target.changeEvent()
-                .subscribe { payloads.add(2) }
+                .collect { payloads.add(2) }
         }
 
         assertEquals(listOf(), payloads)
@@ -102,104 +58,20 @@ class EventInstanceTest {
     }
 
     @Test
-    fun `subscribe -> handler -> subscribe`() = runJsTest {
-        val payloads = mutableListOf<Int>()
-        val target = EventTarget()
-
-        val job1 = target.changeEvent()
-            .subscribe { payloads.add(1) }
-
-        target.changeEvent.addHandler { payloads.add(48) }
-
-        val job2 = target.changeEvent()
-            .subscribe { payloads.add(2) }
-
-        assertEquals(listOf(), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2, 1, 48, 2), payloads)
-
-        // stop
-        job1.cancel()
-        job2.cancel()
-    }
-
-    @Test
-    fun `launch.collect -> handler -> launch.collect`() = runJsTest {
+    fun `should notify listeners synchronously in order of subscription`() = runJsTest {
         val payloads = mutableListOf<Int>()
         val target = EventTarget()
 
         val job1 = launch(Dispatchers.Unconfined) {
             target.changeEvent()
-                .subscribe { payloads.add(1) }
+                .collect { payloads.add(1) }
         }
 
         target.changeEvent.addHandler { payloads.add(48) }
 
         val job2 = launch(Dispatchers.Unconfined) {
             target.changeEvent()
-                .subscribe { payloads.add(2) }
-        }
-
-        assertEquals(listOf(), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2, 1, 48, 2), payloads)
-
-        // stop
-        job1.cancel()
-        job2.cancel()
-    }
-
-    @Test
-    fun `subscribe -> handler -> subscribe -> handler`() = runJsTest {
-        val payloads = mutableListOf<Int>()
-        val target = EventTarget()
-
-        val job1 = target.changeEvent()
-            .subscribe { payloads.add(1) }
-
-        target.changeEvent.addHandler { payloads.add(48) }
-
-        val job2 = target.changeEvent()
-            .subscribe { payloads.add(2) }
-
-        target.changeEvent.addHandler { payloads.add(17) }
-
-        assertEquals(listOf(), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2, 17), payloads)
-
-        target.dispatchEvent(Event(Event.CHANGE))
-        assertEquals(listOf(1, 48, 2, 17, 1, 48, 2, 17), payloads)
-
-        // stop
-        job1.cancel()
-        job2.cancel()
-    }
-
-    @Test
-    fun `launch.collect -> handler -> launch.collect -> handler`() = runJsTest {
-        val payloads = mutableListOf<Int>()
-        val target = EventTarget()
-
-        val job1 = launch(Dispatchers.Unconfined) {
-            target.changeEvent()
-                .subscribe { payloads.add(1) }
-        }
-
-        target.changeEvent.addHandler { payloads.add(48) }
-
-        val job2 = launch(Dispatchers.Unconfined) {
-            target.changeEvent()
-                .subscribe { payloads.add(2) }
+                .collect { payloads.add(2) }
         }
 
         target.changeEvent.addHandler { payloads.add(17) }
