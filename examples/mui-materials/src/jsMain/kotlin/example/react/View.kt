4aaa54c3471852ae4fe6803feee5b1aec21f11e6
Mike Kienenberger <mkienenb@gmail.com>
2026-03-13 22:23:19 -0400

    refactor: separate DetailItem as a component

--- examples/mui-materials/src/jsMain/kotlin/example/react/View.kt
+++ examples/mui-materials/src/jsMain/kotlin/example/react/View.kt
@@ -33,37 +33,17 @@ internal val View = FC {
             margin = auto
         }
 
-        fun detailItem(label: String, value: String?, keyValue: String) = Box {
-            key = Key("detail-item-$keyValue")
-            Box {
-                sx {
-                    display = Display.flex
-                    flexDirection = FlexDirection.column
-                    minWidth = 160.px
-                }
-                Typography {
-                    variant = TypographyVariant.caption
-                    sx {
-                        fontWeight = FontWeight.bolder
-                        color = Color("#BFD7EA")
-                        textTransform = TextTransform.uppercase
-                        letterSpacing = 0.5.px
-                    }
-                    +label
-                }
-                Typography {
-                    variant = TypographyVariant.body1
-                    sx {
-                        fontWeight = FontWeight.bold
-                        color = NamedColor.white
-                    }
-                    +value
-                }
-            }
-        }
 
-        detailItem("Name", "John Doe", "name")
-        detailItem("Name", "Winner", "title")
+        DetailItem {
+             label = "Name"
+             value = "John Doe"
+             keyValue = "name"
+        }
+        DetailItem {
+             label = "Title"
+             value = "Winner"
+             keyValue = "title"
+        }
     }
 
     Divider {
