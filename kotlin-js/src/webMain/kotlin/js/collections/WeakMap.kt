057f88bb5c86f4354f21c32741176844d11c5fee
Victor Turansky <victor.turansky@gmail.com>
2026-04-01 00:53:45 +0200

    Collections. `WeakMap.getOrInsert` and `WeakMap.getOrInsertComputed`

--- kotlin-js/src/webMain/kotlin/js/collections/WeakMap.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/WeakMap.kt
@@ -10,6 +10,17 @@ open external class WeakMap<in K : JsAny, V : JsAny?>(
 ) {
     fun delete(key: K): Boolean
     fun get(key: K): V?
+
+    fun getOrInsert(
+        key: K,
+        defaultValue: V,
+    ): V
+
+    fun getOrInsertComputed(
+        key: K,
+        callback: (key: @UnsafeVariance K) -> V,
+    ): V
+
     fun has(key: K): Boolean
     fun set(key: K, value: V): WeakMap<K, V>
 }
