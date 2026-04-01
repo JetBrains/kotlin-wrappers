4f5c959d20c88158cc9a8117f4f03f46fc5124d7
Victor Turansky <victor.turansky@gmail.com>
2026-04-01 02:45:51 +0200

    JS. Common `IteratorResultTest`

--- kotlin-js/src/webTest/kotlin/js/iterable/IteratorResultTest.kt
+++ kotlin-js/src/webTest/kotlin/js/iterable/IteratorResultTest.kt
@@ -2,7 +2,9 @@ package js.iterable
 
 import js.array.jsArrayOf
 import js.collections.JsSet
+import js.numbers.n
 import js.symbol.Symbol
+import kotlin.js.JsString
 import kotlin.test.Test
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -18,7 +20,7 @@ class IteratorResultTest {
 
     @Test
     fun yieldResult() {
-        val set = JsSet(jsArrayOf("1", "2"))
+        val set = JsSet(jsArrayOf(1.n, 2.n))
         val result = set[Symbol.iterator]().next()
 
         assertFalse(result.done)
