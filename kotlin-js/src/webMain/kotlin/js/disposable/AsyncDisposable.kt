f5fca72763d2c93d85933f7a79afea82161aa7c3
Victor Turansky <victor.turansky@gmail.com>
2026-04-03 01:23:22 +0200

    Revert "JS. `AsyncDisposable` - use `@JsSymbol`"
    This reverts commit 4364a34636bb12ab334b188034722dd95aa1eec4.

--- kotlin-js/src/webMain/kotlin/js/disposable/AsyncDisposable.kt
+++ kotlin-js/src/webMain/kotlin/js/disposable/AsyncDisposable.kt
@@ -1,15 +1,13 @@
-@file:Suppress(
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-)
-
 package js.disposable
 
 import js.core.Void
+import js.hacks.safeMethod
 import js.promise.PromiseLike
-import kotlin.js.JsSymbol
-import kotlin.js.definedExternally
+import js.symbol.Symbol
 
-external interface AsyncDisposable {
-    @JsSymbol("asyncDispose")
-    fun asyncDispose(): PromiseLike<Void> = definedExternally
-}
+external interface AsyncDisposable
+
+operator fun AsyncDisposable.get(
+    key: Symbol.asyncDispose,
+): () -> PromiseLike<Void> =
+    safeMethod(Symbol.asyncDispose)
