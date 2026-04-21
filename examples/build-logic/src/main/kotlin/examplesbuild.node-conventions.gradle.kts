88518ce6cfd5e1f8e740b145fed69ea3ab9bdfa4
Victor Turansky <victor.turansky@gmail.com>
2026-04-21 19:00:47 +0200

    Node runtime `24.15.0` - common version

--- examples/build-logic/src/main/kotlin/examplesbuild.node-conventions.gradle.kts
+++ examples/build-logic/src/main/kotlin/examplesbuild.node-conventions.gradle.kts
@@ -1,6 +1,14 @@
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsEnvSpec
 import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsPlugin
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsEnvSpec
+import org.jetbrains.kotlin.gradle.targets.wasm.nodejs.WasmNodeJsPlugin
+
+private val NODE_VERSION = "24.15.0"
 
 plugins.withType<NodeJsPlugin> {
-    the<NodeJsEnvSpec>().version.set("24.15.0")
+    the<NodeJsEnvSpec>().version.set(NODE_VERSION)
+}
+
+plugins.withType<WasmNodeJsPlugin> {
+    the<WasmNodeJsEnvSpec>().version.set(NODE_VERSION)
 }
