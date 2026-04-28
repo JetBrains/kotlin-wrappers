fbccab7ddb344d30bd5fbf47d74a2113788aa1c8
Victor Turansky <victor.turansky@gmail.com>
2026-04-28 21:05:01 +0200

    JS. Remove `kotlin.js.undefined` declaration

--- kotlin-js-core/build.gradle.kts
+++ kotlin-js-core/build.gradle.kts
@@ -1,14 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
-
 plugins {
     id("wrappersbuild.kotlin-library-conventions")
-}
-
-// for `definedExternally`
-tasks.withType<KotlinCompilationTask<*>>().configureEach {
-    compilerOptions {
-        freeCompilerArgs.addAll(
-            "-Xallow-kotlin-package",
-        )
-    }
 }
--- kotlin-js-core/src/wasmJsMain/kotlin/kotlin/js/undefined.kt
+++
@@ -1,7 +0,0 @@
-@file:Suppress(
-    "WRONG_JS_INTEROP_TYPE",
-)
-
-package kotlin.js
-
-actual external val undefined: Nothing?
