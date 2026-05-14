653e5de5808962f5345c2a5161d2f8b3477f3b49
Victor Turansky <victor.turansky@gmail.com>
2026-05-14 22:52:36 +0200

    Errors. Fix `stackTraceLimit` KDoc

--- kotlin-js/src/webMain/kotlin/js/errors/stackTraceLimit.kt
+++ kotlin-js/src/webMain/kotlin/js/errors/stackTraceLimit.kt
@@ -3,7 +3,7 @@ package js.errors
 import js.numbers.UInt53
 
 /**
- * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Error/captureStackTrace)
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Error/stackTraceLimit)
  */
 @JsQualifier("Error")
 external var stackTraceLimit: UInt53?
