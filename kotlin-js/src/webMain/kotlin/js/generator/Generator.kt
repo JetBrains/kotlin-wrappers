69ab0f62fb4ececb557a42421b0a78a4d20a417a
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 22:13:23 +0200

    Generator. `Generator` MDN links

--- kotlin-js/src/webMain/kotlin/js/generator/Generator.kt
+++ kotlin-js/src/webMain/kotlin/js/generator/Generator.kt
@@ -6,15 +6,30 @@ import js.iterable.JsIterator
 
 private const val GENERATOR: String = "(function* () {}.constructor.prototype.prototype)"
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Generator)
+ */
 @JsName(GENERATOR)
 open external class Generator<out T : JsAny?, TReturn : JsAny?, in TNext : JsAny?>
 private constructor() :
     JsIterator<T> {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Generator/next)
+     */
     override fun next(): IteratorResult<T, TReturn>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Generator/next)
+     */
     fun next(value: TNext): IteratorResult<T, TReturn>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Generator/return)
+     */
     fun `return`(value: TReturn): IteratorResult<T, TReturn>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Generator/throw)
+     */
     fun `throw`(error: JsError): IteratorResult<T, TReturn>
 }
