c0a93a83342350fe4545bd83132c0461a314cb0d
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 22:14:28 +0200

    Generator. `GeneratorFunction` MDN links

--- kotlin-js/src/webMain/kotlin/js/generator/GeneratorFunction.kt
+++ kotlin-js/src/webMain/kotlin/js/generator/GeneratorFunction.kt
@@ -5,6 +5,9 @@ import js.function.JsFunction
 
 private const val GENERATOR_FUNCTION: String = "(function* () {}.constructor)"
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/GeneratorFunction)
+ */
 @JsName(GENERATOR_FUNCTION)
 open external class GeneratorFunction<in A : Tuple, out R : Generator<*, *, *>>(
     vararg parameterNames: String,
