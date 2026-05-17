9f5d6cc48788c1d766b69c3695a7e91e00a00cb0
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 18:28:55 +0200

    Collections. `JsSet` MDN links

--- kotlin-js/src/webMain/kotlin/js/collections/JsSet.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/JsSet.kt
@@ -6,28 +6,96 @@ import js.iterable.JsIterable
 import js.iterable.JsIterator
 import js.serialization.Serializable
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set)
+ */
 @JsName("Set")
 open external class JsSet<T : JsAny?>(
     values: JsIterable<T> = definedExternally,
 ) : MutableSetLike<T>,
     Serializable {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/Set)
+     */
     constructor(values: ReadonlyArray<T>)
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/size)
+     */
     override val size: Int
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/add)
+     */
     override fun add(value: T)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/clear)
+     */
     override fun clear()
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/delete)
+     */
     override fun delete(value: T): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/difference)
+     */
     fun difference(other: ReadonlySetLike<T>): JsSet<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/entries)
+     */
     override fun entries(): JsIterator<Tuple2<T, T>>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/forEach)
+     */
     override fun forEach(action: (value: T) -> Unit)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/has)
+     */
     override fun has(key: T): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/intersection)
+     */
     fun intersection(other: ReadonlySetLike<T>): JsSet<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/isDisjointFrom)
+     */
     fun isDisjointFrom(other: ReadonlySetLike<T>): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/isSubsetOf)
+     */
     fun isSubsetOf(other: ReadonlySetLike<T>): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/isSupersetOf)
+     */
     fun isSupersetOf(other: ReadonlySetLike<T>): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/keys)
+     */
     override fun keys(): JsIterator<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/symmetricDifference)
+     */
     fun symmetricDifference(other: ReadonlySetLike<T>): JsSet<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/union)
+     */
     fun union(other: ReadonlySetLike<T>): JsSet<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Set/values)
+     */
     override fun values(): JsIterator<T>
 }
