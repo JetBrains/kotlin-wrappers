606ebda72c09c875abf747f76f0e62bad5c55661
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 22:20:16 +0200

    Import. `import` MDN link

--- kotlin-js/src/webMain/kotlin/js/import/import.kt
+++ kotlin-js/src/webMain/kotlin/js/import/import.kt
@@ -3,17 +3,26 @@ package js.import
 import js.promise.Promise
 import js.promise.await
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import)
+ */
 @JsName("import")
 external fun <T : JsAny?> importAsync(
     path: String,
     options: ImportCallOptions = definedExternally,
 ): Promise<T>
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import)
+ */
 suspend fun <T : JsAny?> import(
     path: String,
 ): T =
     importAsync<T>(path).await()
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import)
+ */
 suspend fun <T : JsAny?> import(
     path: String,
     options: ImportCallOptions,
@@ -21,5 +30,8 @@ suspend fun <T : JsAny?> import(
     importAsync<T>(path, options).await()
 
 external object import {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/import.meta)
+     */
     val meta: ImportMeta
 }
