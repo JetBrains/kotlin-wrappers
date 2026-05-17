63baad6711b1dc4d46b1d1c47dd7c68415b8d973
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 03:13:33 +0200

    Iterable. `Iterator` MDN links

--- kotlin-js/src/webMain/kotlin/js/iterable/JsIterator.kt
+++ kotlin-js/src/webMain/kotlin/js/iterable/JsIterator.kt
@@ -3,69 +3,117 @@ package js.iterable
 import js.array.ReadonlyArray
 import js.disposable.Disposable
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator)
+ */
 @JsName("Iterator")
 abstract external class JsIterator<out T : JsAny?> :
     JsIteratorLike<T>,
     JsIterable<T>,
     Disposable {
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/return)
+     */
     fun `return`(): IteratorResult<T, *>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/drop)
+     */
     fun drop(
         n: Int,
     ): JsIterator<T>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/every)
+     */
     fun every(
         predicate: (T) -> Boolean,
     ): Boolean
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/filter)
+     */
     fun filter(
         predicate: (T) -> Boolean,
     ): JsIterator<T>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/find)
+     */
     fun find(
         predicate: (T) -> Boolean,
     ): T?
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/flatMap)
+     */
     fun <R : JsAny?> flatMap(
         transform: (T) -> JsIterable<R>,
     ): JsIterator<R>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/flatMap)
+     */
     fun <R : JsAny?> flatMap(
         transform: (T) -> ReadonlyArray<R>,
     ): JsIterator<R>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/forEach)
+     */
     fun forEach(
         action: (item: T) -> Unit,
     )
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/map)
+     */
     fun <R : JsAny?> map(
         transform: (T) -> R,
     ): JsIterator<R>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/reduce)
+     */
     fun reduce(
         operation: (previousValue: T, currentValue: T, currentIndex: Int) -> @UnsafeVariance T,
     ): T
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/reduce)
+     */
     fun <U : JsAny?> reduce(
         operation: (previousValue: U, currentValue: T, currentIndex: Int) -> U,
         initialValue: U,
     ): U
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/some)
+     */
     fun some(
         predicate: (T) -> Boolean,
     ): Boolean
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/take)
+     */
     fun take(
         n: Int,
     ): JsIterator<T>
     // fun toArray(): ReadonlyArray<T>
 
     companion object {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/from)
+         */
         fun <T : JsAny?> from(
             source: JsIteratorLike<T>,
         ): JsIterator<T>
 
+        /**
+         * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Iterator/from)
+         */
         fun <T : JsAny?> from(
             source: JsIterable<T>,
         ): JsIterator<T>
