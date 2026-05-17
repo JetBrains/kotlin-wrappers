ff6f9b4f9ec69a23fe17c81bbb982329d91d68ea
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 21:45:34 +0200

    Disposable. `SuppressedError` MDN links

--- kotlin-js/src/webMain/kotlin/js/disposable/SuppressedError.kt
+++ kotlin-js/src/webMain/kotlin/js/disposable/SuppressedError.kt
@@ -6,7 +6,13 @@ import js.errors.JsError
  * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SuppressedError)
  */
 open external class SuppressedError(
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SuppressedError/error)
+     */
     val error: JsError,
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/SuppressedError/suppressed)
+     */
     val suppressed: JsError,
     override val message: String? = definedExternally,
 ) : JsError
