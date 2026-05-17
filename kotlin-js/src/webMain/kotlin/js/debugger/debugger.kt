0addf2943fdea340200f7c947416a4c0fc2a37b4
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 20:05:45 +0200

    Debugger. `debugger` MDN link

--- kotlin-js/src/webMain/kotlin/js/debugger/debugger.kt
+++ kotlin-js/src/webMain/kotlin/js/debugger/debugger.kt
@@ -6,4 +6,7 @@ package js.debugger
 
 import js.core.Void
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/debugger)
+ */
 external val debugger: Void
