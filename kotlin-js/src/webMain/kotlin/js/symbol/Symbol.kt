5df127720000cb2adb72ad31526e19b1888a09e7
Victor Turansky <victor.turansky@gmail.com>
2026-05-18 01:38:58 +0200

    Symbol. `Symbol` MDN links

--- kotlin-js/src/webMain/kotlin/js/symbol/Symbol.kt
+++ kotlin-js/src/webMain/kotlin/js/symbol/Symbol.kt
@@ -3,37 +3,115 @@ package js.symbol
 import js.core.JsPrimitive
 import js.objects.PropertyKey
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol)
+ */
 @JsPrimitive("symbol")
 open external class Symbol
 private constructor() :
     PropertyKey {
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/description)
+     */
     val description: String?
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/asyncDispose)
+     */
     object asyncDispose : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/asyncIterator)
+     */
     object asyncIterator : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/dispose)
+     */
     object dispose : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/hasInstance)
+     */
     object hasInstance : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/isConcatSpreadable)
+     */
     object isConcatSpreadable : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/iterator)
+     */
     object iterator : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/match)
+     */
     object match : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/matchAll)
+     */
     object matchAll : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/metadata)
+     */
     object metadata : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/replace)
+     */
     object replace : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/search)
+     */
     object search : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/species)
+     */
     object species : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/split)
+     */
     object split : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toPrimitive)
+     */
     object toPrimitive : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toStringTag)
+     */
     object toStringTag : Symbol
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/unscopables)
+     */
     object unscopables : Symbol
 
     companion object {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/for)
+         */
         fun `for`(key: String): Symbol
 
+        /**
+         * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/keyFor)
+         */
         fun keyFor(symbol: Symbol): String?
     }
 }
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/Symbol)
+ */
 external fun Symbol(
     description: String = definedExternally,
 ): Symbol
