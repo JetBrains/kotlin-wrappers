12ac24d7902af73503a46c1ca5217d1cb1ae2133
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 22:08:05 +0200

    Generator. `AsyncGenerator` MDN links

--- kotlin-js/src/webMain/kotlin/js/generator/AsyncGenerator.kt
+++ kotlin-js/src/webMain/kotlin/js/generator/AsyncGenerator.kt
@@ -8,18 +8,40 @@ import js.promise.PromiseLike
 
 private const val ASYNC_GENERATOR: String = "(async function* () {}.constructor.prototype.prototype)"
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator)
+ */
 @JsName(ASYNC_GENERATOR)
 open external class AsyncGenerator<out T : JsAny?, TReturn : JsAny?, in TNext : JsAny?>
 private constructor() :
     AsyncIterator<T> {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/next)
+     */
     override fun next(): Promise<IteratorResult<T, TReturn>>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/next)
+     */
     fun next(value: TNext = definedExternally): Promise<IteratorResult<T, TReturn>>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/return)
+     */
     fun `return`(): Promise<IteratorResult<T, TReturn?>>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/return)
+     */
     fun `return`(value: TReturn): Promise<IteratorResult<T, TReturn>>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/return)
+     */
     fun `return`(value: PromiseLike<TReturn>): Promise<IteratorResult<T, TReturn>>
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncGenerator/throw)
+     */
     fun `throw`(error: JsError): Promise<IteratorResult<T, TReturn>>
 }
