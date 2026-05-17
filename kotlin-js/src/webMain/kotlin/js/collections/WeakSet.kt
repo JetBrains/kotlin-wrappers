ae8017e897711a09b09d4d88259ab9d0b6bee560
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 19:21:47 +0200

    Collections. `WeakSet` MDN links

--- kotlin-js/src/webMain/kotlin/js/collections/WeakSet.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/WeakSet.kt
@@ -2,10 +2,24 @@ package js.collections
 
 import js.array.ReadonlyArray
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakSet)
+ */
 open external class WeakSet<in T : JsAny?>(
     values: ReadonlyArray<T>? = definedExternally,
 ) {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakSet/add)
+     */
     fun add(value: T): WeakSet<T>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakSet/delete)
+     */
     fun delete(value: T): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakSet/has)
+     */
     fun has(key: T): Boolean
 }
