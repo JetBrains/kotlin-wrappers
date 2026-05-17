1931ad0e624f7a1a2966dc48d46dc496e4f03301
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 22:27:09 +0200

    Iterable. `AsyncIterator` MDN links

--- kotlin-js/src/webMain/kotlin/js/iterable/AsyncIterator.kt
+++ kotlin-js/src/webMain/kotlin/js/iterable/AsyncIterator.kt
@@ -4,6 +4,9 @@ import js.disposable.AsyncDisposable
 import js.internal.InternalApi
 import js.reflect.JsExternalInheritorsOnly
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/AsyncIterator)
+ */
 @JsExternalInheritorsOnly
 @SubclassOptInRequired(InternalApi::class)
 external interface /* class */ AsyncIterator<out T : JsAny?> :
