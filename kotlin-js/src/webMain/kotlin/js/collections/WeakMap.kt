9a9283262ac872c0e030b277fcd7cd729607a6cc
Victor Turansky <victor.turansky@gmail.com>
2026-05-17 19:20:56 +0200

    Collections. `WeakMap` MDN links

--- kotlin-js/src/webMain/kotlin/js/collections/WeakMap.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/WeakMap.kt
@@ -3,22 +3,45 @@ package js.collections
 import js.array.ReadonlyArray
 import js.array.Tuple2
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap)
+ */
 open external class WeakMap<in K : JsAny, V : JsAny?>(
     entries: ReadonlyArray<Tuple2<K, V>>? = definedExternally,
 ) {
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/delete)
+     */
     fun delete(key: K): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/get)
+     */
     fun get(key: K): V?
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/getOrInsert)
+     */
     fun getOrInsert(
         key: K,
         defaultValue: V,
     ): V
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/getOrInsertComputed)
+     */
     fun getOrInsertComputed(
         key: K,
         callback: (key: @UnsafeVariance K) -> V,
     ): V
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/has)
+     */
     fun has(key: K): Boolean
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/WeakMap/set)
+     */
     fun set(key: K, value: V): WeakMap<K, V>
 }
