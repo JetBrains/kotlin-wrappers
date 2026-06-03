8504f4c88c4d3608d6d90ea4ecffde10c0a2a156
Victor Turansky <victor.turansky@gmail.com>
2026-06-03 23:57:26 +0200

    New name based destructuring

--- kotlin-react-use/src/jsMain/kotlin/react/use/useMediaQuery.kt
+++ kotlin-react-use/src/jsMain/kotlin/react/use/useMediaQuery.kt
@@ -24,7 +24,7 @@ fun useMediaQuery(
         setMatches(queryList.matches)
 
         queryList.changeEvent()
-            .collect { setMatches(regex.toRegex().matches(it)) }
+            .collect { setMatches(it.matches) }
     }
 
     return matches
