9bb650fd264360e3d971064d0e825bd50455b374
Victor Turansky <victor.turansky@gmail.com>
2026-06-06 01:38:32 +0200

    Array. `JsArray` extends `ListLike`

--- kotlin-js/src/wasmJsMain/kotlin/js/array/JsArray.kt
+++ kotlin-js/src/wasmJsMain/kotlin/js/array/JsArray.kt
@@ -1,14 +1,55 @@
 package js.array
 
-@JsName("Array")
-actual external class JsArray<T : JsAny?> : JsAny {
-    actual operator fun get(index: Int): T
-    actual operator fun set(index: Int, value: T)
+import js.collections.ListLike
+import js.iterable.JsIterator
+import js.numbers.JsInt
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array)
+ */
+@JsName("Array")
+actual external class JsArray<T : JsAny?> :
+    MutableArrayLike<T>,
+    ListLike<T> {
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/length)
+     */
     @JsName("length")
     actual val size: Int
 
+    actual override operator fun get(index: Int): T
+    actual override operator fun set(index: Int, value: T)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/length)
+     */
+    override val length: Int
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach)
+     */
+    override fun forEach(action: (item: T) -> Unit)
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/entries)
+     */
+    override fun entries(): JsIterator<Tuple2<JsInt, T>>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/keys)
+     */
+    override fun keys(): JsIterator<JsInt>
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/values)
+     */
+    override fun values(): JsIterator<T>
+
     companion object {
+        /**
+         * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/of)
+         */
         operator fun <T : JsAny?> of(
             vararg values: T,
         ): JsArray<T>
--- kotlin-js/src/webMain/kotlin/js/array/JsArray.kt
+++ kotlin-js/src/webMain/kotlin/js/array/JsArray.kt
@@ -4,9 +4,17 @@
 
 package js.array
 
-expect external class JsArray<T : JsAny?> : JsAny {
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array)
+ */
+expect external class JsArray<T : JsAny?> :
+    JsAny {
+
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array/length)
+     */
+    val size: Int
+
     operator fun get(index: Int): T
     operator fun set(index: Int, value: T)
-
-    val size: Int
 }
