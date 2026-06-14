8ffb0af9d8dc0f9d143a2ca92004dae17717addc
Victor Turansky <victor.turansky@gmail.com>
2026-06-14 02:14:14 +0200

    Function. More `async` factories

--- kotlin-web/src/webMain/kotlin/web/function/async.kt
+++ kotlin-web/src/webMain/kotlin/web/function/async.kt
@@ -46,3 +46,59 @@ fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, P6 : JsAny
 ): (P1, P2, P3, P4, P5, P6, P7) -> Promise<R> = { p1, p2, p3, p4, p5, p6, p7 ->
     createCancellablePromise(p7) { block(p1, p2, p3, p4, p5, p6, p7) }
 }
+
+fun <R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.() -> R,
+): () -> Promise<R> = {
+    createCancellablePromise(abortable, block)
+}
+
+fun <P1 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1) -> R,
+): (P1) -> Promise<R> = { p1 ->
+    createCancellablePromise(abortable) { block(p1) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2) -> R,
+): (P1, P2) -> Promise<R> = { p1, p2 ->
+    createCancellablePromise(abortable) { block(p1, p2) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2, P3) -> R,
+): (P1, P2, P3) -> Promise<R> = { p1, p2, p3 ->
+    createCancellablePromise(abortable) { block(p1, p2, p3) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2, P3, P4) -> R,
+): (P1, P2, P3, P4) -> Promise<R> = { p1, p2, p3, p4 ->
+    createCancellablePromise(abortable) { block(p1, p2, p3, p4) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2, P3, P4, P5) -> R,
+): (P1, P2, P3, P4, P5) -> Promise<R> = { p1, p2, p3, p4, p5 ->
+    createCancellablePromise(abortable) { block(p1, p2, p3, p4, p5) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, P6 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2, P3, P4, P5, P6) -> R,
+): (P1, P2, P3, P4, P5, P6) -> Promise<R> = { p1, p2, p3, p4, p5, p6 ->
+    createCancellablePromise(abortable) { block(p1, p2, p3, p4, p5, p6) }
+}
+
+fun <P1 : JsAny?, P2 : JsAny?, P3 : JsAny?, P4 : JsAny?, P5 : JsAny?, P6 : JsAny?, P7 : JsAny?, R : JsAny?> async(
+    abortable: AbortableLike,
+    block: suspend CoroutineScope.(P1, P2, P3, P4, P5, P6, P7) -> R,
+): (P1, P2, P3, P4, P5, P6, P7) -> Promise<R> = { p1, p2, p3, p4, p5, p6, p7 ->
+    createCancellablePromise(abortable) { block(p1, p2, p3, p4, p5, p6, p7) }
+}
