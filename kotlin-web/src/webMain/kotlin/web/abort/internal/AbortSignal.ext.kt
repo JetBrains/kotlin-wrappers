2e10beb2ae53d5ddf5fe78f93c435e31321b4e1b
Victor Turansky <victor.turansky@gmail.com>
2026-06-16 01:06:32 +0200

    Abort. Add `AbortSignal.or` extension

---
+++ kotlin-web/src/webMain/kotlin/web/abort/internal/AbortSignal.ext.kt
@@ -0,0 +1,13 @@
+package web.abort.internal
+
+import js.array.jsArrayOf
+import web.abort.AbortSignal
+
+internal fun AbortSignal?.or(
+    signal: AbortSignal,
+): AbortSignal =
+    if (this != null) {
+        AbortSignal.any(jsArrayOf(this, signal))
+    } else {
+        signal
+    }
