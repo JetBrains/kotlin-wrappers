ddfeeafb4022e49cea10ef222637cfaa6d6fd09c
Victor Turansky <victor.turansky@gmail.com>
2026-06-22 02:15:11 +0200

    Actions Cache `6.1.0` [generated]

---
+++ kotlin-actions-cache/src/webMain/generated/actions/cache/CACHE_WRITE_DENIED_PREFIX.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package actions.cache
+
+const val CACHE_WRITE_DENIED_PREFIX = "cache write denied:"
---
+++ kotlin-actions-cache/src/webMain/generated/actions/cache/CacheWriteDeniedError.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@actions/cache")
+
+package actions.cache
+
+open external class CacheWriteDeniedError : ReserveCacheError {
+    constructor(message: String)
+}
