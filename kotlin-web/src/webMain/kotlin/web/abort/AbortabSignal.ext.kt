4c19bcd24fe8736224eba21e1df40b870938f012
Victor Turansky <victor.turansky@gmail.com>
2026-06-25 02:35:38 +0200

    Abort. `AbortSignal or AbortSignal` extension

--- kotlin-web/src/webMain/kotlin/web/abort/AbortabSignal.ext.kt
+++ kotlin-web/src/webMain/kotlin/web/abort/AbortabSignal.ext.kt
@@ -7,13 +7,36 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import web.events.addHandler
 
+infix fun AbortSignal.or(
+    signal: AbortSignal,
+): AbortSignal =
+    AbortSignal.any(jsArrayOf(this, signal))
+
+infix fun AbortSignal.or(
+    signal: AbortSignal?,
+): AbortSignal =
+    if (signal != null) {
+        this or signal
+    } else {
+        this
+    }
+
 infix fun AbortSignal?.or(
     signal: AbortSignal,
 ): AbortSignal =
     if (this != null) {
-        AbortSignal.any(jsArrayOf(this, signal))
+        this or signal
     } else {
         signal
+    }
+
+infix fun AbortSignal?.or(
+    signal: AbortSignal?,
+): AbortSignal? =
+    if (this != null && signal != null) {
+        this or signal
+    } else {
+        this ?: signal
     }
 
 fun AbortSignal.asCoroutineScope(): CoroutineScope {
