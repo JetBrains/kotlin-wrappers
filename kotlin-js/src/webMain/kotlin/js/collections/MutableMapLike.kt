d3838dbb3b0bc6fc2f9799f40e2f25d51cfb004d
Victor Turansky <victor.turansky@gmail.com>
2026-07-21 01:48:42 +0200

    Collections. Mixins for `MutableMapLike`

--- kotlin-js/src/webMain/kotlin/js/collections/MutableMapLike.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/MutableMapLike.kt
@@ -1,9 +1,22 @@
 package js.collections
 
+import js.internal.InternalApi
+
 external interface MutableMapLike<K : JsAny?, V : JsAny?> :
     ReadonlyMap<K, V> {
 
     fun clear()
     fun delete(key: K): Boolean
     fun set(key: K, value: V)
+
+    @SubclassOptInRequired(InternalApi::class)
+    @Suppress("NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE")
+    interface Mixin<K : JsAny?, V : JsAny?> :
+        MutableMapLike<K, V>,
+        ReadonlyMap.Mixin<K, V> {
+
+        override fun clear(): Unit = definedExternally
+        override fun delete(key: K): Boolean = definedExternally
+        override fun set(key: K, value: V): Unit = definedExternally
+    }
 }
