64746f67228214b4397f06e1738189f34d82cbe6
Victor Turansky <victor.turansky@gmail.com>
2026-07-21 01:43:16 +0200

    Collections. Mixins for `MutableSetLike` and `ReadonlySet`

--- kotlin-js/src/webMain/kotlin/js/collections/MutableSetLike.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/MutableSetLike.kt
@@ -1,9 +1,22 @@
 package js.collections
 
+import js.internal.InternalApi
+
 external interface MutableSetLike<T : JsAny?> :
     ReadonlySet<T> {
 
     fun add(value: T)
     fun clear()
     fun delete(value: T): Boolean
+
+    @SubclassOptInRequired(InternalApi::class)
+    @Suppress("NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE")
+    interface Mixin<T : JsAny?> :
+        MutableSetLike<T>,
+        ReadonlySet.Mixin<T> {
+
+        override fun add(value: T): Unit = definedExternally
+        override fun clear(): Unit = definedExternally
+        override fun delete(value: T): Boolean = definedExternally
+    }
 }
--- kotlin-js/src/webMain/kotlin/js/collections/ReadonlySet.kt
+++ kotlin-js/src/webMain/kotlin/js/collections/ReadonlySet.kt
@@ -1,5 +1,8 @@
 package js.collections
 
+import js.internal.InternalApi
+import js.iterable.JsIterator
+
 external interface ReadonlySet<out T : JsAny?> :
     CollectionLike<@UnsafeVariance T, T>,
     ReadonlySetLike<T> {
@@ -7,4 +10,18 @@ external interface ReadonlySet<out T : JsAny?> :
     override val size: Int
 
     override fun has(key: @UnsafeVariance T): Boolean
+
+    @SubclassOptInRequired(InternalApi::class)
+    @Suppress("NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE")
+    interface Mixin<out T : JsAny?> :
+        ReadonlySet<T>,
+        CollectionLike.Mixin<@UnsafeVariance T, T>,
+        ReadonlySetLike.Mixin<T> {
+
+        override val size: Int
+            get() = definedExternally
+
+        override fun has(key: @UnsafeVariance T): Boolean = definedExternally
+        override fun keys(): JsIterator<T> = definedExternally
+    }
 }
