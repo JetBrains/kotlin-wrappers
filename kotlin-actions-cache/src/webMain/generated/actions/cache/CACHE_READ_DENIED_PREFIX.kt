dea04de7619ea85314b88a71577cae1e07a69cdd
Victor Turansky <victor.turansky@gmail.com>
2026-07-25 17:46:02 +0200

    Actions Cache `6.2.0` [generated]

---
+++ kotlin-actions-cache/src/webMain/generated/actions/cache/CACHE_READ_DENIED_PREFIX.kt
@@ -0,0 +1,5 @@
+// Automatically generated - do not modify!
+
+package actions.cache
+
+const val CACHE_READ_DENIED_PREFIX = "cache read denied:"
---
+++ kotlin-actions-cache/src/webMain/generated/actions/cache/CacheReadDeniedError.kt
@@ -0,0 +1,12 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@actions/cache")
+
+package actions.cache
+
+import js.errors.JsError
+
+open external class CacheReadDeniedError :
+    JsError {
+    constructor(message: String)
+}
