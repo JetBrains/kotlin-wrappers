2be55766e803b730a659ccf3eda16f4a5aeb2903
Victor Turansky <victor.turansky@gmail.com>
2026-07-27 00:12:33 +0200

    String. KDoc for `ToString`

--- kotlin-js/src/webMain/kotlin/js/string/ToString.kt
+++ kotlin-js/src/webMain/kotlin/js/string/ToString.kt
@@ -1,7 +1,13 @@
 package js.string
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toStringTag)
+ */
 external interface ToString {
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toStringTag)
+     */
     @JsSymbol("toStringTag")
     fun `[@@toStringTag]`(): String
 }
