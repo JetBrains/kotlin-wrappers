895817067b1b020ff0f319493fd991dcd200f614
Victor Turansky <victor.turansky@gmail.com>
2026-07-27 00:13:22 +0200

    Primitive. KDoc for `ToPrimitive`

--- kotlin-js/src/webMain/kotlin/js/primitive/ToPrimitive.kt
+++ kotlin-js/src/webMain/kotlin/js/primitive/ToPrimitive.kt
@@ -1,7 +1,13 @@
 package js.primitive
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toPrimitive)
+ */
 external interface ToPrimitive {
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/toPrimitive)
+     */
     @JsSymbol("toPrimitive")
     fun `[@@toPrimitive]`(
         hint: PrimitiveHint? = definedExternally,
