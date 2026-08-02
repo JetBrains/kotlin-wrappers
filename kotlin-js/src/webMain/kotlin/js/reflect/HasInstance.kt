7fa0ea6eaa6a9f0be402eca47bf3fe94d2430569
Victor Turansky <victor.turansky@gmail.com>
2026-08-02 18:30:20 +0200

    Reflect. KDoc for `HasInstance`

--- kotlin-js/src/webMain/kotlin/js/reflect/HasInstance.kt
+++ kotlin-js/src/webMain/kotlin/js/reflect/HasInstance.kt
@@ -1,7 +1,13 @@
 package js.reflect
 
+/**
+ * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/hasInstance)
+ */
 external interface HasInstance {
 
+    /**
+     * [MDN Reference](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Symbol/hasInstance)
+     */
     @JsSymbol("hasInstance")
     fun `[@@hasInstance]`(
         instance: JsAny?,
