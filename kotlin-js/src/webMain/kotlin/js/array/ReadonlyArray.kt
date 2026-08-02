c4706c7b2feef16b8e9724c0a6b1a7aa9ba54d10
Victor Turansky <victor.turansky@gmail.com>
2026-08-02 17:45:30 +0200

    Array. `ReadonlyArray` component operators

--- kotlin-js/src/webMain/kotlin/js/array/ReadonlyArray.kt
+++ kotlin-js/src/webMain/kotlin/js/array/ReadonlyArray.kt
@@ -2,6 +2,21 @@ package js.array
 
 typealias ReadonlyArray<T> = JsArray<out T>
 
+inline operator fun <T : JsAny?> ReadonlyArray<T>.component1(): T =
+    get(0)
+
+inline operator fun <T : JsAny?> ReadonlyArray<T>.component2(): T =
+    get(1)
+
+inline operator fun <T : JsAny?> ReadonlyArray<T>.component3(): T =
+    get(2)
+
+inline operator fun <T : JsAny?> ReadonlyArray<T>.component4(): T =
+    get(3)
+
+inline operator fun <T : JsAny?> ReadonlyArray<T>.component5(): T =
+    get(4)
+
 expect operator fun <T : JsAny?> ReadonlyArray<T>.iterator(): Iterator<T>
 
 expect fun <T : JsAny?> ReadonlyArray<T>.asSequence(): Sequence<T>
