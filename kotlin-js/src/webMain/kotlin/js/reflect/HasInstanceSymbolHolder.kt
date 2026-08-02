cac69f0aa5d0891f0658d201b30c020251837e56
Victor Turansky <victor.turansky@gmail.com>
2026-08-02 18:20:18 +0200

    Reflect. Cleanup `HasInstanceSymbolHolder` interface

--- kotlin-js/src/webMain/kotlin/js/reflect/HasInstanceSymbolHolder.kt
+++ kotlin-js/src/webMain/kotlin/js/reflect/HasInstanceSymbolHolder.kt
@@ -1,7 +1,3 @@
-@file:Suppress(
-    "NON_ABSTRACT_MEMBER_OF_EXTERNAL_INTERFACE",
-)
-
 package js.reflect
 
 import js.symbol.Symbol
@@ -9,5 +5,5 @@ import js.symbol.Symbol
 external interface HasInstanceSymbolHolder {
     operator fun get(
         key: Symbol.hasInstance,
-    ): HasInstance = definedExternally
+    ): HasInstance
 }
