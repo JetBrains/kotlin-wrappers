481ec6d8d4a0fae6365ff06d77b047f6acc8d9cc
Dominick Johnson <dominick.johnson@tylertech.com>
2026-08-28 12:44:16 -0600

    Expose 0-param JS version of useMatches; remove uneeded overloads

--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatches.ext.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatches.ext.kt
@@ -2,16 +2,7 @@ package tanstack.react.router
 
 import js.array.ReadonlyArray
 
-fun useMatches(): ReadonlyArray<RouteMatch> =
-    useMatches(UseMatchesOptions())
-
 fun <T> useMatches(
     select: Select<ReadonlyArray<RouteMatch>, T>,
 ): T =
     useMatches(UseMatchesOptions(select = select))
-
-fun <T> useMatches(
-    select: Select<ReadonlyArray<RouteMatch>, T>,
-    structuralSharing: Boolean,
-): T =
-    useMatches(UseMatchesOptions(select = select, structuralSharing = structuralSharing))
--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatches.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatches.kt
@@ -2,6 +2,8 @@
 
 package tanstack.react.router
 
+import js.array.ReadonlyArray
+
 /**
  * The useMatches hook returns the router's complete presented array of RouteMatch objects regardless of its caller's position in the component tree. The array can include still-loading descendants or matches below a pending/error/not-found render boundary.
  *
@@ -13,6 +15,8 @@ package tanstack.react.router
  *
  * @return An array of RouteMatch objects (or selected value) for the matched route.
  */
+external fun useMatches(): ReadonlyArray<RouteMatch>
+
 internal /* raw */
 external fun <T> useMatches(
     options: UseMatchesOptions<T>,
