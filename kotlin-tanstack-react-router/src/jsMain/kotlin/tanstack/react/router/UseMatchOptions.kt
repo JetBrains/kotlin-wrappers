60656f72a816a65dd6a2f9292fb437a529e3a8b4
Alexander Popoff <alexander.popoff@flysto.net>
2026-09-02 20:50:34 +0200

    RouteMatch -> UseMatchResult

--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/UseMatchOptions.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/UseMatchOptions.kt
@@ -2,14 +2,13 @@ package tanstack.react.router
 
 import kotlinx.js.JsPlainObject
 import tanstack.router.core.RouteId
-import tanstack.router.core.RouteMatch
 
 @JsPlainObject
 internal /* raw */
 external interface UseMatchOptions<T> {
     val from: RouteId? /* RoutePath */
     val strict: Boolean?
-    val select: Select<RouteMatch, T>?
+    val select: Select<UseMatchResult, T>?
     val structuralSharing: Boolean?
     val shouldThrow: Boolean?
 }
--- kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatch.ext.kt
+++ kotlin-tanstack-react-router/src/jsMain/kotlin/tanstack/react/router/useMatch.ext.kt
@@ -1,11 +1,9 @@
 package tanstack.react.router
 
-import tanstack.router.core.RouteMatch
-
 fun useMatch(): UseMatchResult =
     useMatch(UseMatchOptions(strict = false))
 
 fun <T> useMatch(
-    select: Select<RouteMatch, T>,
+    select: Select<UseMatchResult, T>,
 ): T =
     useMatch(UseMatchOptions(strict = false, select = select))
